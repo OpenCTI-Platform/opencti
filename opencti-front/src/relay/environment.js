ce39e516b5a17641ddec83495f85a633b46e126f
Julien Richard <richard.julien@gmail.com>
2018-10-19 15:00:18 +0200

    Adding cache in the network layer

--- opencti-front/src/relay/environment.js
+++ opencti-front/src/relay/environment.js
@@ -1,10 +1,26 @@
 import {Environment, Network, RecordSource, Store,} from 'relay-runtime';
 import {installRelayDevTools} from 'relay-devtools';
+import RelayQueryResponseCache from 'relay-runtime/lib/RelayQueryResponseCache';
 
 const __DEV__ = process.env.NODE_ENV === 'development';
 if (__DEV__) installRelayDevTools();
 
-function fetchQuery(operation, variables) {
+const oneMinute = 60 * 1000;
+const cache = new RelayQueryResponseCache({size: 250, ttl: oneMinute});
+
+function fetchQuery(operation, variables, cacheConfig,) {
+    const queryID = operation.text;
+    const isMutation = operation.operationKind === 'mutation';
+    const isQuery = operation.operationKind === 'query';
+    const forceFetch = cacheConfig && cacheConfig.force;
+
+    // Try to get data from cache on queries
+    const fromCache = cache.get(queryID, variables);
+    if (isQuery && fromCache !== null && !forceFetch) {
+        return fromCache;
+    }
+
+    // Otherwise, fetch data from server
     return fetch('/graphql', {
         method: 'POST',
         headers: {
@@ -16,6 +32,16 @@ function fetchQuery(operation, variables) {
         }),
     }).then(response => {
         return response.json();
+    }).then(json => {
+        // Update cache on queries
+        if (isQuery && json) {
+            cache.set(queryID, variables, json);
+        }
+        // Clear cache on mutations
+        if (isMutation) {
+            cache.clear();
+        }
+        return json;
     });
 }
 
