6789026b951a7a490fb89f5c06a4f583c1a4d99e
Samuel Hassine <samuel.hassine@ssi.gouv.fr>
2019-04-16 09:12:16 +0200

    Fix search URLs

--- opencti-clients/python/examples/stix2/import.py
+++ opencti-clients/python/examples/stix2/import.py
@@ -15,4 +15,12 @@ file_to_import = config['mitre']['repository_path_cti'] + '/enterprise-attack/en
 opencti = OpenCTI(config['opencti']['api_url'], config['opencti']['api_key'], config['opencti']['verbose'])
 
 # Import the bundle
-opencti.stix2_import_bundle_from_file(file_to_import)
+#opencti.stix2_import_bundle_from_file(file_to_import)
+
+report = opencti.create_external_reference_if_not_exists(
+    'Mitre',
+    'T1044',
+    'https://attack.mitre.org/techniques/T1044',
+    'fdsfsd'
+    'internal'
+)['id']
