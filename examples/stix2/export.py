b32dd23fe49acf31e3882529ad28560deec37a9b
Samuel Hassine <samuel.hassine@ssi.gouv.fr>
2019-06-26 15:16:09 +0200

    Enhance existing methods for creating STIX 2 entities
    (original commit: OpenCTI-Platform/client-python@77a0da5)

--- examples/stix2/export.py
+++ examples/stix2/export.py
@@ -4,7 +4,7 @@ import os
 import yaml
 import json
 
-from python.pycti.opencti import OpenCTI
+from opencti import OpenCTI
 
 # Load configuration
 config = yaml.load(open(os.path.dirname(__file__) + '/config.yml'))
@@ -16,7 +16,7 @@ export_file = './exports/IntrusionSets.json'
 opencti = OpenCTI(config['opencti']['api_url'], config['opencti']['api_key'], config['opencti']['log_file'], config['opencti']['verbose'])
 
 # Import the bundle
-bundle = opencti.stix2_export_entity('report', '466872bd-41d0-3d38-af2f-0b964b7ff993', 'full')
+bundle = opencti.stix2_export_entity('report', '{ENTITY_ID}', 'full')
 
 with open(export_file, 'w') as file:
     json.dump(bundle, file, indent=4)
