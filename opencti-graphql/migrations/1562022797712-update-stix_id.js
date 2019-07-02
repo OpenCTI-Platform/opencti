48873f0ec069fb8752a2266e0db932e8c80e5b69
Samuel Hassine <samuel@hassine.fr>
2019-07-02 07:14:25 +0200

    Migration for entities

--- opencti-graphql/migrations/1562022797712-update-stix_id.js
+++ opencti-graphql/migrations/1562022797712-update-stix_id.js
@@ -1,21 +1,38 @@
 import { queryMultiple, updateAttribute } from '../src/database/grakn';
 import { index } from '../src/database/elasticSearch';
+import { logger } from '../src/config/conf';
 
 module.exports.up = async next => {
-  ['sector', 'organization', 'user', 'region', 'country', 'city'].map(
-    async entityType => {
-      const query = `match $x isa entity; $x has stix_id $sid; $sid contains "${entityType}"; get $x;`;
-      const entities = await queryMultiple(query, ['x']);
-      entities.map(entity => {
-        return updateAttribute(entity.x.id, {
-          key: 'stix_id',
-          value: [entity.x.stix_id.replace(entityType, 'identity')]
-        }).then(stixDomainEntity => {
-          index('stix-domain-entities', 'stix_domain_entity', stixDomainEntity);
+  const resultPromise = Promise.all(
+    ['sector', 'organization', 'user', 'region', 'country', 'city'].map(
+      async entityType => {
+        const query = `match $x isa entity; $x has stix_id $sid; $sid contains "${entityType}"; get $x;`;
+        const entities = await queryMultiple(query, ['x']);
+        logger.info('Entities loaded');
+        const updatePromise = Promise.all(
+          entities.map(entity => {
+            return updateAttribute(entity.x.id, {
+              key: 'stix_id',
+              value: [entity.x.stix_id.replace(entityType, 'identity')]
+            }).then(stixDomainEntity => {
+              index(
+                'stix-domain-entities',
+                'stix_domain_entity',
+                stixDomainEntity
+              );
+            });
+          })
+        );
+        await Promise.resolve(updatePromise).then(() => {
+          logger.info('Entities updated');
+          return Promise.resolve(true);
         });
-      });
-    }
+      }
+    )
   );
+  await Promise.resolve(resultPromise).then(() => {
+    logger.info('Migration complete');
+  });
   next();
 };
 
