e6ff67d8d5b82a8aefae5153b82c350883d9b4b9
Samuel Hassine <samuel.hassine@ssi.gouv.fr>
2019-07-05 14:32:16 +0200

    Update connectors

--- opencti-worker/worker_import.py
+++ opencti-worker/worker_import.py
@@ -40,14 +40,11 @@ class WorkerImport:
         self.channel.queue_bind(exchange='opencti', queue='opencti-import', routing_key='import.*.*')
 
     def import_action(self, ch, method, properties, body):
-        try:
-            data = json.loads(body)
-            self.logger.log('Receiving new action of type: { ' + data['type'] + ' }')
-            if data['type'] == 'import.stix2.bundle':
-                self.opencti.stix2_import_bundle(base64.b64decode(data['content']).decode('utf-8'))
-        except Exception as e:
-            self.logger.log('An unexpected error occurred: { ' + str(e) + ' }')
-            return False
+        data = json.loads(body)
+        self.logger.log('Receiving new action of type: { ' + data['type'] + ' }')
+        if data['type'] == 'import.stix2.bundle':
+            print(base64.b64decode(data['content']).decode('utf-8'))
+            self.opencti.stix2_import_bundle(base64.b64decode(data['content']).decode('utf-8'))
 
     def consume(self):
         self.channel.basic_consume(queue='opencti-import', on_message_callback=self.import_action, auto_ack=True)
@@ -56,9 +53,5 @@ class WorkerImport:
 
 if __name__ == '__main__':
     while True:
-        try:
-            worker_import = WorkerImport()
-            worker_import.consume()
-        except Exception as e:
-            print(e)
-            time.sleep(5)
+        worker_import = WorkerImport()
+        worker_import.consume()
