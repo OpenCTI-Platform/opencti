11687eefd329f30aa8b8e279a0101f903e1e4932
Samuel Hassine <samuel@hassine.fr>
2019-07-17 22:09:06 +0200

    [connectors] Update metrics display

--- opencti-platform/opencti-front/src/private/components/connector/ConnectorsStatusContent.js
+++ opencti-platform/opencti-front/src/private/components/connector/ConnectorsStatusContent.js
@@ -5,7 +5,6 @@ import {
 } from 'ramda';
 import { interval } from 'rxjs';
 import graphql from 'babel-plugin-relay/macro';
-import SwipeableViews from 'react-swipeable-views';
 import { withStyles } from '@material-ui/core/styles/index';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -17,9 +16,11 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import IconButton from '@material-ui/core/IconButton';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
 import { createRefetchContainer } from 'react-relay';
+import { ExpandMore } from '@material-ui/icons';
 import NoContent from '../../../components/NoContent';
 import inject18n from '../../../components/i18n';
 import { FIVE_SECONDS } from '../../../utils/Time';
@@ -28,14 +29,10 @@ const interval$ = interval(FIVE_SECONDS);
 
 const styles = theme => ({
   card: {
-    width: '100%',
-    minHeight: '100%',
+    width: '60%',
+    margin: '0 auto',
     marginBottom: 20,
     borderRadius: 6,
-  },
-  tabContent: {
-    width: '100%',
-    overflow: 'hidden',
   },
   metric: {
     margin: '0 auto',
@@ -61,12 +58,32 @@ const styles = theme => ({
   avatar: {
     backgroundColor: theme.palette.primary.main,
   },
+  configuration: {
+    fontSize: 12,
+    padding: '4px 0 0 10px',
+    letterSpacing: '3px',
+    textTransform: 'uppercase',
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
 });
 
 class ConnectorsStatusComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = { tabs: {}, messages: {} };
+    this.state = { expanded: {}, messages: {} };
   }
 
   componentDidMount() {
@@ -75,8 +92,10 @@ class ConnectorsStatusComponent extends Component {
     });
   }
 
-  switchTab(key, event, value) {
-    this.setState({ tabs: assoc(key, value, this.state.tabs) });
+  handleExpand(key) {
+    this.setState({
+      expanded: assoc(key, !this.state.expanded[key], this.state.expanded),
+    });
   }
 
   render() {
@@ -86,130 +105,130 @@ class ConnectorsStatusComponent extends Component {
     const { queuesMetrics } = data;
     return (
       <div>
-        <Grid container={true} spacing={2}>
-          {queuesMetrics.length === 0 ? (
-            <NoContent
-              message={t('No connectors are enabled on this platform.')}
-            />
-          ) : (
-            queuesMetrics.map((queueMetric) => {
-              const config = JSON.parse(
-                Buffer.from(queueMetric.arguments.config, 'base64').toString(
-                  'ascii',
-                ),
-              );
-              return (
-                <Grid item={true} lg={6} xs={12} key={queueMetric.name}>
-                  <Card raised={true} classes={{ root: classes.card }}>
-                    <CardHeader
-                      avatar={
-                        <Avatar aria-label="Recipe" className={classes.avatar}>
-                          {propOr(' ', 'name', config).charAt(0)}
-                        </Avatar>
-                      }
-                      title={propOr('', 'name', config)}
-                    />
-                    <CardContent style={{ paddingTop: 0 }}>
-                      <Tabs
-                        value={propOr(0, queueMetric.name, this.state.tabs)}
-                        onChange={this.switchTab.bind(this, queueMetric.name)}
-                        indicatorColor="primary"
-                        textColor="primary"
-                        variant="fullWidth"
-                      >
-                        <Tab label={t('Metrics')} />
-                        <Tab label={t('Configuration')} />
-                      </Tabs>
-                      <SwipeableViews
-                        axis="x"
-                        index={propOr(0, queueMetric.name, this.state.tabs)}
-                      >
-                        <div className={classes.tabContent}>
-                          <Grid container={true} spacing={2}>
-                            <Grid item={true} lg={6} xs={12}>
-                              <div className={classes.metric}>
-                                <div className={classes.number}>
-                                  {queueMetric.messages_ready}
-                                </div>
-                                <div className={classes.title}>
-                                  {t('Queued messages')}
-                                </div>
-                              </div>
-                            </Grid>
-                            <Grid item={true} lg={6} xs={12}>
-                              <div className={classes.metric}>
-                                <div className={classes.number}>
-                                  {queueMetric.messages_unacknowledged}
-                                </div>
-                                <div className={classes.title}>
-                                  {t('In progress messages')}
-                                </div>
-                              </div>
-                            </Grid>
-                            <Grid item={true} lg={6} xs={12}>
-                              <div className={classes.metric}>
-                                <div className={classes.number}>
-                                  {pathOr(
-                                    0,
-                                    [
-                                      'deliver_details',
-                                      'message_stats',
-                                      'rate',
-                                    ],
-                                    queueMetric,
-                                  )}
-                                  /s
-                                </div>
-                                <div className={classes.title}>
-                                  {t('Messages processed')}
-                                </div>
-                              </div>
-                            </Grid>
-                            <Grid item={true} lg={6} xs={12}>
-                              <div className={classes.metric}>
-                                <div className={classes.date}>
-                                  {nsdt(queueMetric.idle_since)}
-                                </div>
-                                <div className={classes.title}>
-                                  {t('Last processed message')}
-                                </div>
-                              </div>
-                            </Grid>
-                          </Grid>
+        {queuesMetrics.length === 0 ? (
+          <NoContent
+            message={t('No connectors are enabled on this platform.')}
+          />
+        ) : (
+          queuesMetrics.map((queueMetric) => {
+            const config = JSON.parse(
+              Buffer.from(queueMetric.arguments.config, 'base64').toString(
+                'ascii',
+              ),
+            );
+            return (
+              <Card
+                raised={true}
+                classes={{ root: classes.card }}
+                key={queueMetric.name}
+              >
+                <CardHeader
+                  avatar={
+                    <Avatar aria-label="Recipe" className={classes.avatar}>
+                      {propOr(' ', 'name', config).charAt(0)}
+                    </Avatar>
+                  }
+                  title={propOr('', 'name', config)}
+                  style={{ paddingBottom: 0 }}
+                />
+                <CardContent style={{ paddingTop: 0 }}>
+                  <Grid container={true} spacing={2}>
+                    <Grid item={true} lg={6} xs={12}>
+                      <div className={classes.metric}>
+                        <div className={classes.number}>
+                          {queueMetric.messages_ready}
                         </div>
-                        <div className={classes.tabContent}>
-                          <Table>
-                            <TableHead>
-                              <TableRow>
-                                <TableCell align="left">{t('Key')}</TableCell>
-                                <TableCell align="left">{t('Value')}</TableCell>
-                              </TableRow>
-                            </TableHead>
-                            <TableBody>
-                              {filter(
-                                n => n[0] !== 'name',
-                                toPairs(config),
-                              ).map(conf => (
-                                <TableRow key={conf[0]} hover={true}>
-                                  <TableCell align="left">{conf[0]}</TableCell>
-                                  <TableCell align="left">
-                                    {Array.isArray(conf[1])
-                                      ? conf[1].join(',')
-                                      : conf[1]}
-                                  </TableCell>
-                                </TableRow>
-                              ))}
-                            </TableBody>
-                          </Table>
+                        <div className={classes.title}>
+                          {t('Queued messages')}
                         </div>
-                      </SwipeableViews>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              );
-            })
-          )}
-        </Grid>
+                      </div>
+                    </Grid>
+                    <Grid item={true} lg={6} xs={12}>
+                      <div className={classes.metric}>
+                        <div className={classes.number}>
+                          {queueMetric.messages_unacknowledged}
+                        </div>
+                        <div className={classes.title}>
+                          {t('In progress messages')}
+                        </div>
+                      </div>
+                    </Grid>
+                    <Grid item={true} lg={6} xs={12}>
+                      <div className={classes.metric}>
+                        <div className={classes.number}>
+                          {pathOr(
+                            0,
+                            ['deliver_details', 'message_stats', 'rate'],
+                            queueMetric,
+                          )}
+                          /s
+                        </div>
+                        <div className={classes.title}>
+                          {t('Messages processed')}
+                        </div>
+                      </div>
+                    </Grid>
+                    <Grid item={true} lg={6} xs={12}>
+                      <div className={classes.metric}>
+                        <div className={classes.date}>
+                          {nsdt(queueMetric.idle_since)}
+                        </div>
+                        <div className={classes.title}>
+                          {t('Last processed message')}
+                        </div>
+                      </div>
+                    </Grid>
+                  </Grid>
+                </CardContent>
+                <CardActions disableSpacing>
+                  <div className={classes.configuration}>
+                    {t('Configuration')}
+                  </div>
+                  <IconButton
+                    className={
+                      this.state.expanded[queueMetric.name]
+                        ? classes.expandOpen
+                        : classes.expand
+                    }
+                    onClick={this.handleExpand.bind(this, queueMetric.name)}
+                  >
+                    <ExpandMore />
+                  </IconButton>
+                </CardActions>
+                <Collapse
+                  in={this.state.expanded[queueMetric.name]}
+                  timeout="auto"
+                  unmountOnExit
+                >
+                  <CardContent style={{ paddingTop: 0 }}>
+                    <Table>
+                      <TableHead>
+                        <TableRow>
+                          <TableCell align="left">{t('Key')}</TableCell>
+                          <TableCell align="left">{t('Value')}</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {filter(n => n[0] !== 'name', toPairs(config)).map(
+                          conf => (
+                            <TableRow key={conf[0]} hover={true}>
+                              <TableCell align="left">{conf[0]}</TableCell>
+                              <TableCell align="left">
+                                {Array.isArray(conf[1])
+                                  ? conf[1].join(',')
+                                  : conf[1]}
+                              </TableCell>
+                            </TableRow>
+                          ),
+                        )}
+                      </TableBody>
+                    </Table>
+                  </CardContent>
+                </Collapse>
+              </Card>
+            );
+          })
+        )}
       </div>
     );
   }
