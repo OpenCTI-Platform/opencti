c31f3ab5764ec3f23a06f4cbe2d74f17f9f31165
Samuel Hassine <samuel.hassine@luatix.org>
2019-12-06 21:11:49 +0100

    [client] Prepare new release
    (original commit: OpenCTI-Platform/client-python@c00c070)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
     print("warning: pypandoc module not found, could not convert Markdown to RST")
     read_md = lambda f: open(f, 'r').read()
 
-VERSION = "2.1.3"
+VERSION = "2.1.4"
 
 
 class VerifyVersionCommand(install):
