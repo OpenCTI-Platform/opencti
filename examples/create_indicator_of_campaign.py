82b569357c36dd040639d8668081a0e3aeec5d22
Samuel Hassine <samuel.hassine@luatix.org>
2019-12-19 13:01:31 +0100

    [client] Fix indicator creation
    (original commit: OpenCTI-Platform/client-python@2396446)

--- examples/create_indicator_of_campaign.py
+++ examples/create_indicator_of_campaign.py
@@ -6,7 +6,7 @@ from dateutil.parser import parse
 from pycti import OpenCTIApiClient
 
 # Variables
-api_url = 'https://demo.opencti.io'
+api_url = 'http://localhost:4000'
 api_token = 'fa63eb1f-bf14-4777-9190-43b4571cbc8b'
 
 # OpenCTI initialization
