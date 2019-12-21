b7833e604c076491b38048aaadc3d3e0c3fd44e1
Samuel Hassine <samuel.hassine@luatix.org>
2019-12-21 10:43:19 +0100

    [examples] Fix some URLs
    (original commit: OpenCTI-Platform/client-python@d076aa1)

--- examples/create_incident_with_ttps_and_indicators.py
+++ examples/create_incident_with_ttps_and_indicators.py
@@ -6,7 +6,7 @@ from dateutil.parser import parse
 from pycti import OpenCTIApiClient
 
 # Variables
-api_url = 'https://reference.opencti.io'
+api_url = 'https://demo.opencti.io'
 api_token = 'bb4aca90-b98c-49ee-9582-7eac92b61b82'
 
 # OpenCTI initialization
