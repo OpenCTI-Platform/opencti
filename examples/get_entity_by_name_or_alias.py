e52aac37173f4d775cf8e4eb16658a894a167ebf
Samuel Hassine <samuel@hassine.fr>
2020-01-24 10:58:15 +0100

    [client] Fix black formatting
    (original commit: OpenCTI-Platform/client-python@695601b)

--- examples/get_entity_by_name_or_alias.py
+++ examples/get_entity_by_name_or_alias.py
@@ -10,7 +10,7 @@ api_token = "609caced-7610-4c84-80b4-f3a380d1939b"
 opencti_api_client = OpenCTIApiClient(api_url, api_token)
 
 # Get the ANSSI entity
-anssi = opencti_api_client.stix_domain_entity.get_by_stix_id_or_name(name='ANSSI')
+anssi = opencti_api_client.stix_domain_entity.get_by_stix_id_or_name(name="ANSSI")
 
 # Print
 print(anssi)
