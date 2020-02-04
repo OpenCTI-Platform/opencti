10b0d04083cafffa9a1a3d3b1eb089e74f57a7bd
Julien Richard <richard.julien@gmail.com>
2020-02-04 15:19:57 +0100

    Refactor login to pure function

--- opencti-platform/opencti-front/src/public/components/Login.js
+++ opencti-platform/opencti-front/src/public/components/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as PropTypes from 'prop-types';
 import graphql from 'babel-plugin-relay/macro';
 import {
@@ -77,82 +77,70 @@ const LoginMessage = ({ message, sso, t }) => <div>
 </div>;
 const Message = inject18n(LoginMessage);
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { width: 0, height: 0 };
-    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-  }
+const Login = ({ location, classes }) => {
+  const query = new URLSearchParams(location.search);
+  const message = query.get('message');
+  // eslint-disable-next-line max-len
+  const [dimension, setDimension] = useState({ width: window.innerWidth, height: window.innerHeight });
+  const marginTop = dimension.height / 2 - loginHeight / 2 - 120;
 
-  componentDidMount() {
-    this.updateWindowDimensions();
-    window.addEventListener('resize', this.updateWindowDimensions);
-  }
+  const updateWindowDimensions = () => {
+    setDimension({ width: window.innerWidth, height: window.innerHeight });
+  };
+  useEffect(() => {
+    window.addEventListener('resize', updateWindowDimensions);
+    return () => window.removeEventListener('resize', updateWindowDimensions);
+  });
 
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.updateWindowDimensions);
-  }
-
-  updateWindowDimensions() {
-    this.setState({ width: window.innerWidth, height: window.innerHeight });
-  }
-
-  renderExternalAuth(authButtons) {
-    return (
-      <div>
+  const renderExternalAuth = (authButtons) => <React.Fragment>
         <div style={{ marginTop: 20 }}>&nbsp;</div>
         {authButtons.map((value, index) => <Button
               key={`${value.provider}_${index}`}
-              className={this.props.classes.buttonGoogle}
+              type="submit"
               variant="contained"
+              color="primary"
               size="small"
+              style={{ marginRight: 10, minWidth: 100 }}
               component="a"
               href={`/auth/${value.provider}`}>
             {value.name}
           </Button>)}
-      </div>
-    );
-  }
+      </React.Fragment>;
 
-  render() {
-    const query = new URLSearchParams(this.props.location.search);
-    const message = query.get('message');
-    const marginTop = this.state.height / 2 - loginHeight / 2 - 120;
-    return (
-      <QueryRenderer query={LoginQuery} variables={{}}
-        render={({ props }) => {
-          if (props && props.settings) {
-            const providers = props.settings.platform_providers;
-            const isAuthForm = filter((p) => p.type === 'FORM', providers).length > 0;
-            const authSSOs = filter((p) => p.type === 'SSO', providers);
-            if (authSSOs.length === 1 && !message) {
-              const authSSO = head(authSSOs);
-              window.location.href = `/auth/${authSSO.provider}`;
-            }
-            // If not display form and buttons if configured
-            const sso = authSSOs.length === 1;
-            const auto = sso && !message;
-            const isAuthButtons = authSSOs.length > 1;
-            return (
-              <ConnectedIntlProvider settings={props.settings}>
-                <div className={this.props.classes.container} style={{ marginTop }}>
-                  <img src={logo} alt="logo" className={this.props.classes.logo} />
-                  { message && <Message message={message} sso={sso ? head(authSSOs) : null}/> }
-                  { auto && <Loader /> }
-                  { isAuthForm && !auto && <LoginForm demo={pathOr(false, ['settings', 'platform_demo'], props)} />}
-                  { isAuthButtons && !auto && pathOr(false, ['settings', 'platform_external_auth'], props) === true
-                      && this.renderExternalAuth(authSSOs)}
-                  { providers.length === 0 && <Message message={'No authentication providers available'} /> }
-                </div>
-              </ConnectedIntlProvider>
-            );
+  return (
+    <QueryRenderer query={LoginQuery} variables={{}}
+      render={({ props }) => {
+        if (props && props.settings) {
+          const providers = props.settings.platform_providers;
+          const isAuthForm = filter((p) => p.type === 'FORM', providers).length > 0;
+          const authSSOs = filter((p) => p.type === 'SSO', providers);
+          if (authSSOs.length === 1 && !message) {
+            const authSSO = head(authSSOs);
+            window.location.href = `/auth/${authSSO.provider}`;
           }
-          return <div />;
-        }}
-      />
-    );
-  }
-}
+          // If not display form and buttons if configured
+          const sso = authSSOs.length === 1;
+          const auto = sso && !message;
+          const isAuthButtons = authSSOs.length > 1;
+          return (
+            <ConnectedIntlProvider settings={props.settings}>
+              <div className={classes.container} style={{ marginTop }}>
+                <img src={logo} alt="logo" className={classes.logo} />
+                { message && <Message message={message} sso={sso ? head(authSSOs) : null}/> }
+                { auto && <Loader /> }
+                { isAuthForm && !auto && <LoginForm demo={pathOr(false, ['settings', 'platform_demo'], props)} />}
+                { isAuthButtons && !auto && pathOr(false, ['settings', 'platform_external_auth'], props) === true
+                    && renderExternalAuth(authSSOs)}
+                { providers.length === 0 && <Message message={'No authentication providers available'} /> }
+              </div>
+            </ConnectedIntlProvider>
+          );
+        }
+        return <div />;
+      }}
+    />
+  );
+};
 
 Login.propTypes = {
   classes: PropTypes.object,
