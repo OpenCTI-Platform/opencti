6b3d7367b4943746cc046d9e170f0108ee2b615a
Samuel Hassine <samuel.hassine@ssi.gouv.fr>
2020-02-05 11:44:12 +0100

    [client] Try to get indicator by stix id
    (original commit: OpenCTI-Platform/client-python@f230965)

---
+++ examples/update_entity_attribute.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+import json
+from pycti import OpenCTIApiClient
+
+# Variables
+api_url = "https://demo.opencti.io"
+api_token = "609caced-7610-4c84-80b4-f3a380d1939b"
+
+# OpenCTI initialization
+opencti_api_client = OpenCTIApiClient(api_url, api_token)
+
+# Get the intrusion set APT28
+intrusion_set = opencti_api_client.intrusion_set.read(
+    filters=[{"key": "name", "values": ["APT28"]}]
+)
+
+# Update the description
+opencti_api_client.stix_domain_entity.update_field(
+    id=intrusion_set['id'],
+    key='description',
+    value='This is APT28!'
+)
