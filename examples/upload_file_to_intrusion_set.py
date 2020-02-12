d39f3b141881c60d5c9088d10c5e1c070cadee3b
Samuel Hassine <samuel.hassine@ssi.gouv.fr>
2020-02-12 11:00:18 +0100

    [client] Fix the direction of exported relations in STIX2
    (original commit: OpenCTI-Platform/client-python@607c3d5)

--- examples/upload_file_to_intrusion_set.py
+++ examples/upload_file_to_intrusion_set.py
@@ -5,7 +5,7 @@ from pycti import OpenCTIApiClient
 
 # Variables
 api_url = "https://demo.opencti.io"
-api_token = "609caced-7610-4c84-80b4-f3a380d1939b"
+api_token = "2b4f29e3-5ea8-4890-8cf5-a76f61f1e2b2"
 
 # OpenCTI initialization
 opencti_api_client = OpenCTIApiClient(api_url, api_token)
