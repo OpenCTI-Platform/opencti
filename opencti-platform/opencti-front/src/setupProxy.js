7cbc4a173bed660842b05680c556f0961e218baf
Samuel Hassine <samuel@hassine.fr>
2020-03-24 23:16:39 +0100

    Fix test, prepare rollback of bump by fixing dependencies

--- opencti-platform/opencti-front/src/setupProxy.js
+++ opencti-platform/opencti-front/src/setupProxy.js
@@ -1,12 +1,12 @@
 /* eslint-disable */
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const onError = function(err, req, res) {
   console.log('Something went wrong... Ignoring');
 };
 
 module.exports = function(app) {
-  app.use(proxy("/graphql", { target: "http://localhost:4000", ws: true, onError }));
-  app.use(proxy("/storage", { target: "http://localhost:4000", onError }));
-  app.use(proxy("/auth/**", { target: "http://localhost:4000", onError }));
+  app.use(createProxyMiddleware("/graphql", { target: "http://localhost:4000", ws: true, onError }));
+  app.use(createProxyMiddleware("/storage", { target: "http://localhost:4000", onError }));
+  app.use(createProxyMiddleware("/auth/**", { target: "http://localhost:4000", onError }));
 };
