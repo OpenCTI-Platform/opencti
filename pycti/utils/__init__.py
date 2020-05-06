3013de27d07c26755176c47683bfa4dab18d0f93
Robert Haist <robert.haist@dcso.de>
2020-05-06 09:41:52 +0200

    Add basic sphinx doc structure for RTD

--- pycti/utils/__init__.py
+++ pycti/utils/__init__.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+from .opencti_stix2 import OpenCTIStix2
+from .constants import ObservableTypes
+from .constants import CustomProperties
+
+__all__ = [
+    "OpenCTIStix2",
+    "ObservableTypes",
+    "CustomProperties",
+]
