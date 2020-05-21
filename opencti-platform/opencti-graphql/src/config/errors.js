fe86471a75744a0453b9d822d02f76814f741c42
Julien Richard <richard.julien@gmail.com>
2020-05-21 18:09:23 +0200

    [api] Remove unused constants and export

--- opencti-platform/opencti-graphql/src/config/errors.js
+++ opencti-platform/opencti-graphql/src/config/errors.js
@@ -1,10 +1,7 @@
 import { createError } from 'apollo-errors';
 
-export const LEVEL_WARNING = 'warning';
-export const LEVEL_ERROR = 'error';
-export const TYPE_AUTH = 'authentication';
-export const TYPE_TECHNICAL = 'technical';
-const TYPE_BUSINESS = 'business';
+const CATEGORY_TECHNICAL = 'technical';
+const CATEGORY_BUSINESS = 'business';
 
 const error = (type, message, data) => {
   const Exception = createError(type, { data, message });
@@ -13,55 +10,55 @@ const error = (type, message, data) => {
 
 export const AuthenticationFailure = (reason, data) =>
   error('AuthFailure', 'Wrong name or password', {
-    category: TYPE_TECHNICAL,
+    category: CATEGORY_TECHNICAL,
     ...data,
   });
 
 // TYPE_AUTH
 export const AuthRequired = (reason, data) =>
   error('AuthRequired', 'You must be logged in to do this.', {
-    category: TYPE_TECHNICAL,
+    category: CATEGORY_TECHNICAL,
     ...data,
   });
 
 export const ForbiddenAccess = (reason, data) =>
   error('ForbiddenAccess', 'You are not allowed to do this.', {
-    category: TYPE_TECHNICAL,
+    category: CATEGORY_TECHNICAL,
     ...data,
   });
 
-// TYPE_TECHNICAL
+// CATEGORY_TECHNICAL
 export const DatabaseError = (reason, data) =>
   error('DatabaseError', 'A database error has occurred', {
     reason: reason || 'No reason specify',
-    category: TYPE_TECHNICAL,
+    category: CATEGORY_TECHNICAL,
     ...data,
   });
 
 export const ConfigurationError = (reason, data) =>
   error('ConfigurationError', 'A configuration error has occurred', {
     reason: reason || 'No reason specify',
-    category: TYPE_TECHNICAL,
+    category: CATEGORY_TECHNICAL,
     ...data,
   });
 
 export const UnknownError = (reason, data) =>
   error('UnknownError', 'An unknown error has occurred', {
     reason: reason || 'No reason specify',
-    category: TYPE_TECHNICAL,
+    category: CATEGORY_TECHNICAL,
     ...data,
   });
 
 export const FunctionalError = (reason, data) =>
   error('FunctionalError', 'Business validation', {
     reason: reason || 'No reason specify',
-    category: TYPE_BUSINESS,
+    category: CATEGORY_BUSINESS,
     ...data,
   });
 
 export const ValidationError = (field, data) =>
   error('ValidationError', 'Validation error', {
     reason: `Invalid field ${field}`,
-    category: TYPE_BUSINESS,
+    category: CATEGORY_BUSINESS,
     ...data,
   });
