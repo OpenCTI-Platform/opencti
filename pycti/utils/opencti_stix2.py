59195067c53f783709cad9383b196df22ff302c2
Samuel Hassine <samuel@hassine.fr>
2020-09-01 11:10:25 +0200

    [client] Fix external references of reports
    (original commit: OpenCTI-Platform/client-python@451706f)

--- pycti/utils/opencti_stix2.py
+++ pycti/utils/opencti_stix2.py
@@ -468,7 +468,7 @@ class OpenCTIStix2:
                         name=title,
                         createdBy=author["id"] if author is not None else None,
                         objectMarking=[object_marking_ref_result["id"]],
-                        external_references=[external_reference_id],
+                        externalReferences=[external_reference_id],
                         description=external_reference["description"]
                         if "description" in external_reference
                         else "",
