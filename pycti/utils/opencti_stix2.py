1d4a494057323779ea5d00ba89d75d64518e53cc
Samuel Hassine <samuel@hassine.fr>
2020-10-12 17:51:24 +0200

    [client] Fix orphan in worker
    (original commit: OpenCTI-Platform/client-python@4d3ac1c)

--- pycti/utils/opencti_stix2.py
+++ pycti/utils/opencti_stix2.py
@@ -174,7 +174,9 @@ class OpenCTIStix2:
             data = json.load(file)
         return self.import_bundle(data, update, types)
 
-    def import_bundle_from_json(self, json_data, update=False, types=None) -> List:
+    def import_bundle_from_json(
+        self, json_data, update=False, types=None, remove_orphan=True
+    ) -> List:
         """import a stix2 bundle from JSON data
 
         :param json_data: JSON data
@@ -187,7 +189,7 @@ class OpenCTIStix2:
         :rtype: List
         """
         data = json.loads(json_data)
-        return self.import_bundle(data, update, types)
+        return self.import_bundle(data, update, types, remove_orphan)
 
     def resolve_author(self, title):
         if "fireeye" in title.lower() or "mandiant" in title.lower():
@@ -1455,14 +1457,16 @@ class OpenCTIStix2:
 
         return bundle
 
-    def import_bundle(self, stix_bundle, update=False, types=None) -> List:
+    def import_bundle(
+        self, stix_bundle, update=False, types=None, remove_orphan=True
+    ) -> List:
         # Check if the bundle is correctly formatted
         if "type" not in stix_bundle or stix_bundle["type"] != "bundle":
             raise ValueError("JSON data type is not a STIX2 bundle")
         if "objects" not in stix_bundle or len(stix_bundle["objects"]) == 0:
             raise ValueError("JSON data objects is empty")
 
-        stix2_splitter = OpenCTIStix2Splitter()
+        stix2_splitter = OpenCTIStix2Splitter(remove_orphan=remove_orphan)
         bundles = stix2_splitter.split_bundle(stix_bundle, False)
         # Import every elements in a specific order
         imported_elements = []
