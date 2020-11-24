2e21f8cad71750dab1fa4f0925ce63dbd8bcfc93
Samuel Hassine <samuel@hassine.fr>
2020-11-24 01:46:08 +0100

    [client] Remove the orphans management

--- tests/01-unit/test_stix_split.py
+++ tests/01-unit/test_stix_split.py
@@ -8,14 +8,3 @@ def test_split_bundle():
         content = file.read()
     bundles = stix_splitter.split_bundle(content)
     assert len(bundles) == 7028
-
-    with open("./tests/data/enterprise-attack-orphan.json") as file_orphan:
-        content_orphan = file_orphan.read()
-    # With orphan removal
-    stix_no_orphan_splitter = OpenCTIStix2Splitter(remove_orphan=False)
-    bundles_orphan = stix_no_orphan_splitter.split_bundle(content_orphan)
-    assert len(bundles_orphan) == 7029
-    # With orphan removal
-    stix_no_orphan_splitter = OpenCTIStix2Splitter(remove_orphan=True)
-    bundles_orphan = stix_no_orphan_splitter.split_bundle(content_orphan)
-    assert len(bundles_orphan) == 7021
