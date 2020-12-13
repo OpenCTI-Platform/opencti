5206a96986286d42ed0eeb0f98a02a05c4c06d85
Samuel Hassine <samuel@hassine.fr>
2020-12-13 01:34:42 +0100

    [client] Bump dependencies

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     install_requires=[
         "requests==2.25.0",
         "PyYAML==5.3.1",
-        "python-dateutil==51.0.0",
+        "python-dateutil==2.8.1",
         "datefinder==0.7.0",
         "stix2==2.1.0",
         "pytz==2020.4",
