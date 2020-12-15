7bc186a5c2208e53445db9f17a89efc481a36824
Samuel Hassine <samuel@hassine.fr>
2020-12-15 16:36:27 +0100

    [frontend] Entities creation in report (#936)

--- opencti-platform/opencti-front/src/private/components/common/stix_domain_objects/StixDomainObjectCreation.js
+++ opencti-platform/opencti-front/src/private/components/common/stix_domain_objects/StixDomainObjectCreation.js
@@ -292,97 +292,121 @@ class StixDomainObjectCreation extends Component {
         containerstyle={{ width: '100%' }}
       >
         {targetStixDomainObjectTypes === undefined
-        || targetStixDomainObjectTypes.includes('Organization')
-        || targetStixDomainObjectTypes.includes('Identity') ? (
+        || targetStixDomainObjectTypes.some(
+          (r) => ['Stix-Domain-Object', 'Organization', 'Identity'].indexOf(r) >= 0,
+        ) ? (
           <MenuItem value="Organization">{t('Organization')}</MenuItem>
           ) : (
             ''
           )}
         {targetStixDomainObjectTypes === undefined
-        || targetStixDomainObjectTypes.includes('Sector')
-        || targetStixDomainObjectTypes.includes('Identity') ? (
+        || targetStixDomainObjectTypes.some(
+          (r) => ['Stix-Domain-Object', 'Sector', 'Identity'].indexOf(r) >= 0,
+        ) ? (
           <MenuItem value="Sector">{t('Sector')}</MenuItem>
           ) : (
             ''
           )}
         {targetStixDomainObjectTypes === undefined
-        || targetStixDomainObjectTypes.includes('Individual')
-        || targetStixDomainObjectTypes.includes('Identity') ? (
+        || targetStixDomainObjectTypes.some(
+          (r) => ['Stix-Domain-Object', 'Individual', 'Identity'].indexOf(r) >= 0,
+        ) ? (
           <MenuItem value="Individual">{t('Individual')}</MenuItem>
           ) : (
             ''
           )}
         {targetStixDomainObjectTypes === undefined
-        || targetStixDomainObjectTypes.includes('Threat-Actor') ? (
+        || targetStixDomainObjectTypes.some(
+          (r) => ['Stix-Domain-Object', 'Threat-Actor'].indexOf(r) >= 0,
+        ) ? (
           <MenuItem value="Threat-Actor">{t('Threat actor')}</MenuItem>
           ) : (
             ''
           )}
         {targetStixDomainObjectTypes === undefined
-        || targetStixDomainObjectTypes.includes('Intrusion-Set') ? (
+        || targetStixDomainObjectTypes.some(
+          (r) => ['Stix-Domain-Object', 'Intrusion-Set'].indexOf(r) >= 0,
+        ) ? (
           <MenuItem value="Intrusion-Set">{t('Intrusion set')}</MenuItem>
           ) : (
             ''
           )}
         {targetStixDomainObjectTypes === undefined
-        || targetStixDomainObjectTypes.includes('Campaign') ? (
+        || targetStixDomainObjectTypes.some(
+          (r) => ['Stix-Domain-Object', 'Campaign'].indexOf(r) >= 0,
+        ) ? (
           <MenuItem value="Campaign">{t('Campaign')}</MenuItem>
           ) : (
             ''
           )}
         {targetStixDomainObjectTypes === undefined
-        || targetStixDomainObjectTypes.includes('X-OpenCTI-Incident') ? (
+        || targetStixDomainObjectTypes.some(
+          (r) => ['Stix-Domain-Object', 'X-OpenCTI-Incident'].indexOf(r) >= 0,
+        ) ? (
           <MenuItem value="X-OpenCTI-Incident">{t('Incident')}</MenuItem>
           ) : (
             ''
           )}
         {targetStixDomainObjectTypes === undefined
-        || targetStixDomainObjectTypes.includes('Malware') ? (
+        || targetStixDomainObjectTypes.some(
+          (r) => ['Stix-Domain-Object', 'Malware'].indexOf(r) >= 0,
+        ) ? (
           <MenuItem value="Malware">{t('Malware')}</MenuItem>
           ) : (
             ''
           )}
         {targetStixDomainObjectTypes === undefined
-        || targetStixDomainObjectTypes.includes('Tool') ? (
+        || targetStixDomainObjectTypes.some(
+          (r) => ['Stix-Domain-Object', 'Tool'].indexOf(r) >= 0,
+        ) ? (
           <MenuItem value="Tool">{t('Tool')}</MenuItem>
           ) : (
             ''
           )}
         {targetStixDomainObjectTypes === undefined
-        || targetStixDomainObjectTypes.includes('Vulnerability') ? (
+        || targetStixDomainObjectTypes.some(
+          (r) => ['Stix-Domain-Object', 'Vulnerability'].indexOf(r) >= 0,
+        ) ? (
           <MenuItem value="Vulnerability">{t('Vulnerability')}</MenuItem>
           ) : (
             ''
           )}
         {targetStixDomainObjectTypes === undefined
-        || targetStixDomainObjectTypes.includes('Attack-Pattern') ? (
+        || targetStixDomainObjectTypes.some(
+          (r) => ['Stix-Domain-Object', 'Attack-Pattern'].indexOf(r) >= 0,
+        ) ? (
           <MenuItem value="Attack-Pattern">{t('Attack pattern')}</MenuItem>
           ) : (
             ''
           )}
         {targetStixDomainObjectTypes === undefined
-        || targetStixDomainObjectTypes.includes('Indicator') ? (
+        || targetStixDomainObjectTypes.some(
+          (r) => ['Stix-Domain-Object', 'Indicator'].indexOf(r) >= 0,
+        ) ? (
           <MenuItem value="Indicator">{t('Indicator')}</MenuItem>
           ) : (
             ''
           )}
         {targetStixDomainObjectTypes === undefined
-        || targetStixDomainObjectTypes.includes('City')
-        || targetStixDomainObjectTypes.includes('Identity') ? (
+        || targetStixDomainObjectTypes.some(
+          (r) => ['Stix-Domain-Object', 'Location', 'City'].indexOf(r) >= 0,
+        ) ? (
           <MenuItem value="City">{t('City')}</MenuItem>
           ) : (
             ''
           )}
         {targetStixDomainObjectTypes === undefined
-        || targetStixDomainObjectTypes.includes('Country')
-        || targetStixDomainObjectTypes.includes('Identity') ? (
+        || targetStixDomainObjectTypes.some(
+          (r) => ['Stix-Domain-Object', 'Location', 'Country'].indexOf(r) >= 0,
+        ) ? (
           <MenuItem value="Country">{t('Country')}</MenuItem>
           ) : (
             ''
           )}
         {targetStixDomainObjectTypes === undefined
-        || targetStixDomainObjectTypes.includes('Region')
-        || targetStixDomainObjectTypes.includes('Identity') ? (
+        || targetStixDomainObjectTypes.some(
+          (r) => ['Stix-Domain-Object', 'Location', 'Region'].indexOf(r) >= 0,
+        ) ? (
           <MenuItem value="Region">{t('Region')}</MenuItem>
           ) : (
             ''
