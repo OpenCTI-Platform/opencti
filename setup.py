60943c5db80c90d8d25362af9a4ebcd255cb7136
Samuel Hassine <samuel@hassine.fr>
2021-01-07 11:54:13 +0100

    [client] Fix upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 from setuptools import setup
 from setuptools.command.install import install
 
-VERSION = "4.0.4"
+VERSION = "4.0.5"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
