f3875225c3c7ac804aa6f0e7e259256e80a38724
Samuel Hassine <samuel@hassine.fr>
2021-01-14 08:56:22 +0100

    [client] Prepare release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 from setuptools import setup
 from setuptools.command.install import install
 
-VERSION = "4.0.6"
+VERSION = "4.0.7"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
