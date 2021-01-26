61c22c9c58a8d9d728b1199e798769d4a133e3fb
Julien Richard <richard.julien@gmail.com>
2021-01-26 22:24:10 +0100

    [api] Fix opencti version in test

--- opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/settings-test.js
+++ opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/settings-test.js
@@ -49,7 +49,7 @@ describe('Settings resolver standard behavior', () => {
     expect(queryResult).not.toBeNull();
     const { about } = queryResult.data;
     expect(about).not.toBeNull();
-    expect(about.version).toContain('4.0');
+    expect(about.version).toContain('4.1');
     expect(about.dependencies.length).toEqual(4);
   });
   it('should settings information', async () => {
