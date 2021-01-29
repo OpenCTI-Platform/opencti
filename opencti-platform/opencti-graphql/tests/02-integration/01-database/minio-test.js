ac318a0a872c1777f449cc8dce711d1a4cdcab27
Samuel Hassine <samuel@hassine.fr>
2021-01-29 07:54:35 +0100

    [api] Fix tests with new distribution

--- opencti-platform/opencti-graphql/tests/02-integration/01-database/minio-test.js
+++ opencti-platform/opencti-graphql/tests/02-integration/01-database/minio-test.js
@@ -45,7 +45,7 @@ describe('Minio file listing', () => {
     let file = head(list.edges).node;
     expect(file.id).toEqual(exportFileId);
     expect(file.name).toEqual(exportFileName);
-    expect(file.size).toEqual(10513);
+    expect(file.size).toEqual(10489);
     expect(file.metaData).not.toBeNull();
     expect(file.metaData['content-type']).toEqual('application/octet-stream');
     expect(file.metaData.encoding).toEqual('7bit');
@@ -56,7 +56,7 @@ describe('Minio file listing', () => {
     expect(list.edges.length).toEqual(1);
     file = head(list.edges).node;
     expect(file.id).toEqual(importFileId);
-    expect(file.size).toEqual(10513);
+    expect(file.size).toEqual(10489);
     expect(file.name).toEqual(exportFileName);
   });
   it('should file download', async () => {
@@ -75,7 +75,7 @@ describe('Minio file listing', () => {
     expect(file).not.toBeNull();
     expect(file.id).toEqual(exportFileId);
     expect(file.name).toEqual(exportFileName);
-    expect(file.size).toEqual(10513);
+    expect(file.size).toEqual(10489);
   });
   it('should delete file', async () => {
     let deleted = await deleteFile({ user_email: 'test@opencti.io' }, exportFileId);
