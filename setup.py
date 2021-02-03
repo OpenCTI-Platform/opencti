f485949ebcc2018dab8039880321d681fbf25cd0
Samuel Hassine <samuel@hassine.fr>
2021-02-03 09:20:13 +0100

    [client] Fix creation of locations

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 from setuptools import setup
 from setuptools.command.install import install
 
-VERSION = "4.1.1"
+VERSION = "4.2.0"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
