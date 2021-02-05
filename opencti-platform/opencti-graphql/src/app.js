9ed37e8dde60d38d3584130cb6a5051606fca3c6
Samuel Hassine <samuel@hassine.fr>
2021-02-05 12:37:12 +0100

    [api] Fix policies for image loading (#1055)

--- opencti-platform/opencti-graphql/src/app.js
+++ opencti-platform/opencti-graphql/src/app.js
@@ -54,7 +54,7 @@ const createApp = async (apolloServer, broadcaster) => {
           'https://fonts.googleapis.com/',
         ],
         fontSrc: ["'self'", 'https://fonts.gstatic.com/'],
-        imgSrc: ["'self'", 'data', 'http://cdn.jsdelivr.net/npm/@apollographql/', 'https://map.opencti.io/'],
+        imgSrc: ["'self'", 'data:', 'https://*', 'http://*'],
         connectSrc: ["'self'", 'wss://*', 'ws://*'],
         objectSrc: ["'none'"],
       },
