de98c93a27d8378ca4324137564b53385f36c68e
Samuel Hassine <samuel@hassine.fr>
2021-02-17 14:34:40 +0100

    [client] Prepare new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 from setuptools import setup
 from setuptools.command.install import install
 
-VERSION = "4.2.2"
+VERSION = "4.2.3"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
