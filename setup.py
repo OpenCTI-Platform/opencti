6af45de9874a5263fb660cfb247796305e4f10d0
Samuel Hassine <samuel@hassine.fr>
2021-03-30 12:05:50 +0200

    [client] Prepare new release
    (original commit: OpenCTI-Platform/client-python@61273d7)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 from setuptools import setup
 from setuptools.command.install import install
 
-VERSION = "4.3.3"
+VERSION = "4.3.4"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
