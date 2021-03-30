0448fa8f782750c345d9e4603004d2801333c459
Samuel Hassine <samuel@hassine.fr>
2021-03-30 20:03:17 +0200

    [client] Fix listing of incidents (OpenCTI-Platform/client-python#157)
    (original commit: OpenCTI-Platform/client-python@7d6ab58)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 from setuptools import setup
 from setuptools.command.install import install
 
-VERSION = "4.3.4"
+VERSION = "4.3.5"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
