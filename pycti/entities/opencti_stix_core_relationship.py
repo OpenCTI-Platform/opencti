42b15eebeb81f15cdb3d43d4307559d1b862320d
Samuel Hassine <samuel@hassine.fr>
2021-04-01 20:31:39 +0200

    [client] Introduce custom properties on Stix Core Relationships list

--- pycti/entities/opencti_stix_core_relationship.py
+++ pycti/entities/opencti_stix_core_relationship.py
@@ -289,6 +289,7 @@ class StixCoreRelationship:
         after = kwargs.get("after", None)
         order_by = kwargs.get("orderBy", None)
         order_mode = kwargs.get("orderMode", None)
+        custom_attributes = kwargs.get("customAttributes", None)
         get_all = kwargs.get("getAll", False)
         with_pagination = kwargs.get("withPagination", False)
         if get_all:
@@ -311,7 +312,7 @@ class StixCoreRelationship:
                         edges {
                             node {
                                 """
-            + self.properties
+            + (custom_attributes if custom_attributes is not None else self.properties)
             + """
                         }
                     }
