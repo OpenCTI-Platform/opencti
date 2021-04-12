2286ba7d42496997e45d9600e97feff687c89234
Julien Richard <richard.julien@gmail.com>
2021-04-12 15:12:05 +0200

    [api] Migration to webpack 5 (#1249)

--- opencti-platform/opencti-graphql/webpack.config.js
+++ opencti-platform/opencti-graphql/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const { DefinePlugin, HotModuleReplacementPlugin } = require('webpack');
 const nodeExternals = require('webpack-node-externals');
+const TerserPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const StartServerPlugin = require('start-server-webpack-plugin');
-const ora = require('ora');
+const { RunScriptWebpackPlugin } = require('run-script-webpack-plugin');
 
 module.exports = (env, argv) => {
   const resolvePath = (dir) => path.resolve(__dirname, dir);
@@ -21,15 +21,24 @@ module.exports = (env, argv) => {
     resolve: {
       extensions: ['.wasm', '.mjs', '.js', '.json', '.graphql'],
     },
-    externals: [nodeExternals({ allowlist: [/^webpack/] })],
+    externals: [
+      nodeExternals({
+        additionalModuleDirs: [path.resolve(__dirname, '../node_modules')],
+        allowlist: [/^webpack/],
+      }),
+    ],
     watch: isDev,
     target: 'node',
     output: {
       path: resolvePath('build'),
       libraryTarget: 'commonjs2',
     },
-    devtool: isDev ? 'eval-source-map' : '',
-    optimization: { noEmitOnErrors: true },
+    devtool: isDev ? 'eval-source-map' : 'nosources-source-map',
+    optimization: {
+      emitOnErrors: false,
+      minimize: true,
+      minimizer: [new TerserPlugin({ extractComments: false })],
+    },
     stats: !isDev && {
       children: false,
       entrypoints: false,
@@ -59,46 +68,9 @@ module.exports = (env, argv) => {
       new CleanWebpackPlugin(),
       ...addIf(isDev, [
         new HotModuleReplacementPlugin(),
-        new StartServerPlugin(),
-        {
-          apply: (compiler) => {
-            const interactive = process.stdout.isTTY;
-            const spinner = ora();
-            const spinStart = (msg) => (interactive ? spinner.start(msg) : console.log(msg));
-            const spinSucceed = (msg) => (interactive ? spinner.succeed(msg) : console.log(msg));
-            const spinWarn = (msg) => (interactive ? spinner.warn(msg) : console.log(msg));
-            const spinFail = (msg) => (interactive ? spinner.fail(msg) : console.log(msg));
-
-            compiler.hooks.invalid.tap('CompilationReport', () => {
-              spinStart('Compiling...');
-            });
-
-            compiler.hooks.done.tap('CompilationReport', (stats) => {
-              // remove compiler stack traces
-              stats.compilation.errors.forEach((e) => {
-                e.message = e.error.message;
-              });
-              const statsData = stats.toJson({ all: false, warnings: true, errors: true });
-
-              const hasWarnings = statsData.warnings.length > 0;
-              const hasErrors = statsData.errors.length > 0;
-
-              if (hasErrors) {
-                // Compilation failed
-                spinFail('Failed to compile.');
-                console.log([...statsData.errors, ...statsData.warnings].join('\n\n'));
-                console.log();
-              } else if (hasWarnings) {
-                // Compilation succeed with warning
-                spinWarn('Compiled with warnings.');
-                console.log(statsData.warnings.join('\n\n'));
-                console.log();
-              } else {
-                spinSucceed('Compiled successfully!');
-              }
-            });
-          },
-        },
+        new RunScriptWebpackPlugin({
+          nodeArgs: ['--inspect'],
+        }),
       ]),
     ],
   };
