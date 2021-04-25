7ae7eed0c393712071be1da26dc3d26218a2a2cb
Samuel Hassine <samuel@hassine.fr>
2021-04-25 19:27:20 +0200

    [client] Format with black

--- pycti/entities/opencti_report.py
+++ pycti/entities/opencti_report.py
@@ -489,7 +489,8 @@ class Report:
             return True
         else:
             self.opencti.log(
-                "error", "[opencti_report] Missing parameters: id and stixObjectOrStixRelationshipId"
+                "error",
+                "[opencti_report] Missing parameters: id and stixObjectOrStixRelationshipId",
             )
             return False
 
@@ -535,7 +536,8 @@ class Report:
             return True
         else:
             self.opencti.log(
-                "error", "[opencti_report] Missing parameters: id and stixObjectOrStixRelationshipId"
+                "error",
+                "[opencti_report] Missing parameters: id and stixObjectOrStixRelationshipId",
             )
             return False
 
