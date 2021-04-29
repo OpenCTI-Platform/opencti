8107b12ff35677e4af8890f3764ebf5d2cb7788c
Samuel Hassine <samuel@hassine.fr>
2021-04-30 00:01:07 +0200

    [integration] Add new test with PoisonIvy file

--- opencti-platform/opencti-graphql/tests/02-integration/03-import/oasis-test.js
+++ opencti-platform/opencti-graphql/tests/02-integration/03-import/oasis-test.js
@@ -5,7 +5,7 @@ import { FIVE_MINUTES, PYTHON_PATH, API_TOKEN, API_URI } from '../../utils/testQ
 import { execPython3 } from '../../../src/python/pythonBridge';
 
 describe('Database provision', () => {
-  const importOpts = [API_URI, API_TOKEN, '/tests/data/DATA-TEST-STIX2_v2.json'];
+  const importOpts = [API_URI, API_TOKEN, '/tests/data/poisonivy.json'];
   it(
     'should platform init',
     () => {
@@ -15,18 +15,6 @@ describe('Database provision', () => {
   );
   it(
     'Should import creation succeed',
-    async () => {
-      const httpServer = await listenServer();
-      const execution = await execPython3(PYTHON_PATH, 'local_importer.py', importOpts);
-      expect(execution).not.toBeNull();
-      expect(execution.status).toEqual('success');
-      await stopServer(httpServer);
-    },
-    FIVE_MINUTES
-  );
-  // Python lib is fixed but we need to wait for a new release
-  it(
-    'Should import update succeed',
     async () => {
       const httpServer = await listenServer();
       const execution = await execPython3(PYTHON_PATH, 'local_importer.py', importOpts);
