f8e271b2bea6085eaa444505d088ab4b9137dcec
Samuel Hassine <samuel@hassine.fr>
2021-05-16 22:35:26 +0200

    [examples] Fix hashes example (OpenCTI-Platform/client-python#173)
    (original commit: OpenCTI-Platform/client-python@4228691)

--- examples/create_file_with_hashes.py
+++ examples/create_file_with_hashes.py
@@ -4,7 +4,7 @@ from pycti import OpenCTIApiClient
 
 # Variables
 api_url = "https://demo.opencti.io"
-api_token = "YOUR_TOKEN"
+api_token = "98481988-5aac-42e3-9be1-e1328ef86419"
 
 # OpenCTI initialization
 opencti_api_client = OpenCTIApiClient(api_url, api_token)
@@ -14,9 +14,9 @@ observable = opencti_api_client.stix_cyber_observable.create(
     observableData={
         "type": "file",
         "hashes": {
-            "md5": "16b3f663d0f0371a4706642c6ac04e42",
-            "sha1": "3a1f908941311fc357051b5c35fd2a4e0c834e37",
-            "sha256": "bcc70a49fab005b4cdbe0cbd87863ec622c6b2c656987d201adbb0e05ec03e56",
+            "md5": "fcd76de79819813b631d949b18b1e996",
+            "sha-1": "e08b42d92fa579c095834909b893d49259b158be",
+            "sha-256": "7cca822e0fdfeca033762213bf16a3f04d7cac8c345f84a0d740324d97f671c0",
         },
     }
 )
