c6714a8ae93a1143067fbaa24262920e330557d9
nor3th <73965465+nor3th@users.noreply.github.com>
2021-07-14 17:55:48 +0200

    [tests] Fix CircleCI pytests (#196)

--- tests/01-unit/utils/test_opencti_stix2_splitter.py
+++ tests/01-unit/utils/test_opencti_stix2_splitter.py
@@ -1,5 +1,4 @@
-import json
-from stix.report import Report
+from stix2 import Report
 from pycti.utils.opencti_stix2_splitter import OpenCTIStix2Splitter
 
 
@@ -18,18 +17,25 @@ def test_split_bundle():
 
 def test_crate_bundle():
     stix_splitter = OpenCTIStix2Splitter()
-    report = Report().to_json()
+    report = Report(
+        report_types=["campaign"],
+        name="Bad Cybercrime",
+        published="2016-04-06T20:03:00.000Z",
+        object_refs=["indicator--a740531e-63ff-4e49-a9e1-a0a3eed0e3e7"],
+    ).serialize()
     observables = [report]
-    bundle = stix_splitter.stix2_create_bundle(
-        observables, use_json=True, event_version=None
-    )
-    for key in ["type", "id", "spec_version", "objects"]:
-        assert key in bundle
-    assert len(json.loads(bundle).keys()) == 4
 
     bundle = stix_splitter.stix2_create_bundle(
-        observables, use_json=True, event_version=1
+        observables, use_json=False, event_version=None
+    )
+
+    for key in ["type", "id", "spec_version", "objects"]:
+        assert key in bundle
+    assert len(bundle.keys()) == 4
+
+    bundle = stix_splitter.stix2_create_bundle(
+        observables, use_json=False, event_version=1
     )
     for key in ["type", "id", "spec_version", "objects", "x_opencti_event_version"]:
         assert key in bundle
-    assert len(json.loads(bundle).keys()) == 5
+    assert len(bundle.keys()) == 5
