5533b136f6fadbdfedb3fb320bf9ace6e2863af4
Samuel Hassine <samuel@hassine.fr>
2021-08-11 13:51:55 +0200

    [frontend] Take into account entity type in container entities export (#1443)

--- opencti-platform/opencti-front/src/private/components/common/containers/ContainerStixDomainObjects.js
+++ opencti-platform/opencti-front/src/private/components/common/containers/ContainerStixDomainObjects.js
@@ -168,8 +168,12 @@ class ContainerStixDomainObjectsComponent extends Component {
       orderBy: sortBy,
       orderMode: orderAsc ? 'asc' : 'desc',
     };
+    const filters = [{ key: 'containedBy', values: [container.id] }];
+    if (types.length > 0) {
+      filters.push({ key: 'entity_type', values: types });
+    }
     const exportPaginationOptions = {
-      filters: [{ key: 'containedBy', values: [container.id] }],
+      filters,
       orderBy: sortBy,
       orderMode: orderAsc ? 'asc' : 'desc',
       search: searchTerm,
