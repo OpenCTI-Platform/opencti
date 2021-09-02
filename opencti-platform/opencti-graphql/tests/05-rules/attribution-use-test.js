a79d07c1fdd474bbd6db07b3eaee829c5ad77514
Julien Richard <richard.julien@gmail.com>
2021-09-02 14:20:30 +0200

    [api] Improve attribute use rule testing

--- opencti-platform/opencti-graphql/tests/05-rules/attribution-use-test.js
+++ opencti-platform/opencti-graphql/tests/05-rules/attribution-use-test.js
@@ -6,11 +6,12 @@ import { RELATION_ATTRIBUTED_TO, RELATION_USES } from '../../src/schema/stixCore
 import { RULE_PREFIX } from '../../src/schema/general';
 import AttributionUseRule from '../../src/rules/attribution-use/AttributionUseRule';
 import { activateRule, disableRule, getInferences, inferenceLookup } from './rule-utils';
-import { FIVE_MINUTES } from '../utils/testQuery';
+import { FIVE_MINUTES, FIVE_SECS, sleep } from '../utils/testQuery';
 
 const RULE = RULE_PREFIX + AttributionUseRule.id;
 const APT41 = 'intrusion-set--d12c5319-f308-5fef-9336-20484af42084';
 const PARADISE_RANSOMWARE = 'malware--21c45dbe-54ec-5bb7-b8cd-9f27cc518714';
+const SPELEVO = 'malware--8a4b5aef-e4a7-524c-92f9-a61c08d1cd85';
 const TLP_WHITE_ID = 'marking-definition--613f2e26-407d-48c7-9eca-b8e91df99dc9';
 
 describe('Attribute use rule', () => {
@@ -51,13 +52,33 @@ describe('Attribute use rule', () => {
       expect(myThreatToParadise.stop_time).toBe('2020-02-29T14:00:00.000Z');
       // Create new element to trigger a live event
       // ---- base
-      // TODO
+      // APT41 -> uses -> Spelevo (start: 2020-01-10T20:30:00.000Z, stop: 2020-02-19T14:00:00.000Z, confidence: 30)
+      const aptUseSpelevo = await createRelation(SYSTEM_USER, {
+        fromId: APT41,
+        toId: SPELEVO,
+        start_time: '2020-01-10T20:30:00.000Z',
+        stop_time: '2020-02-28T14:00:00.000Z',
+        confidence: 90,
+        relationship_type: RELATION_USES,
+        objectMarking: [TLP_WHITE_ID],
+      });
+      await sleep(FIVE_SECS); // let some time to rule manager to create the elements
+      // Check the inferences
+      const afterLiveRelations = await getInferences(RELATION_USES);
+      expect(afterLiveRelations.length).toBe(2);
+      const myThreatToSpelevo = await inferenceLookup(afterLiveRelations, MY_THREAT, SPELEVO, RELATION_USES);
+      expect(myThreatToSpelevo).not.toBeNull();
+      expect(myThreatToSpelevo[RULE].length).toBe(1);
+      expect(myThreatToSpelevo.confidence).toBe(50); // AVG 2 relations (90 + 10) = 50
+      expect(myThreatToSpelevo.start_time).toBe('2020-01-20T20:30:00.000Z');
+      expect(myThreatToSpelevo.stop_time).toBe('2020-02-28T14:00:00.000Z');
       // Disable the rule
       await disableRule(AttributionUseRule.id);
       // Check the number of inferences
       const afterDisableRelations = await getInferences(RELATION_USES);
       expect(afterDisableRelations.length).toBe(0);
       // Clean
+      await deleteElement(SYSTEM_USER, aptUseSpelevo);
       await deleteElement(SYSTEM_USER, threat);
       // Stop
       await shutdownModules();
