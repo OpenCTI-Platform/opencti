1cf29d7ddf402f054f31c1ab460fa8a1dbc2b400
Julien Richard <richard.julien@gmail.com>
2021-09-13 03:19:43 +0200

    [api] Change observed rule resolution

--- opencti-platform/opencti-graphql/src/rules/observed-sighting/ObserveSightingRule.js
+++ opencti-platform/opencti-graphql/src/rules/observed-sighting/ObserveSightingRule.js
@@ -4,17 +4,16 @@ import {
   deleteInferredRuleElement,
   internalLoadById,
   listAllRelations,
+  stixDataById,
 } from '../../database/middleware';
 import { RELATION_BASED_ON } from '../../schema/stixCoreRelationship';
 import def from './ObserveSightingDefinition';
-
 import { ENTITY_TYPE_CONTAINER_OBSERVED_DATA, ENTITY_TYPE_INDICATOR } from '../../schema/stixDomainObject';
 import { RELATION_OBJECT } from '../../schema/stixMetaRelationship';
 import { createRuleContent, RULE_MANAGER_USER, RULES_ATTRIBUTES_BEHAVIOR, RULES_DECLARATION } from '../rules';
 import { STIX_SIGHTING_RELATIONSHIP } from '../../schema/stixSightingRelationship';
 import { ABSTRACT_STIX_CYBER_OBSERVABLE } from '../../schema/general';
 import { generateInternalType } from '../../schema/schemaUtils';
-import { buildStixData } from '../../database/stix';
 
 // 'If **observed-data A (created-by Organization X)** have `object` **observable B** and **indicator C** ' +
 // 'is `based-on` **observable B**, and `revoked` = **false** and `x_opencti_detection` = **false**' +
@@ -140,13 +139,13 @@ const ruleObserveSightingBuilder = () => {
   };
   const handleObservedDataRelationUpsert = async (objectRelation) => {
     const { x_opencti_source_ref: observedDataId } = objectRelation;
-    const observedData = await internalLoadById(RULE_MANAGER_USER, observedDataId);
-    return handleObservedDataUpsert(buildStixData(observedData));
+    const observedData = await stixDataById(RULE_MANAGER_USER, observedDataId);
+    return handleObservedDataUpsert(observedData);
   };
   const handleObservableRelationUpsert = async (baseOnRelation) => {
     const { x_opencti_source_ref: indicatorId } = baseOnRelation;
-    const baseOnIndicator = await internalLoadById(RULE_MANAGER_USER, indicatorId);
-    return handleIndicatorUpsert(buildStixData(baseOnIndicator));
+    const baseOnIndicator = await stixDataById(RULE_MANAGER_USER, indicatorId);
+    return handleIndicatorUpsert(baseOnIndicator);
   };
   const applyUpsert = async (data) => {
     const events = [];
