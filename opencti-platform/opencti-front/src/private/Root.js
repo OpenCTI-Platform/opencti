7318d96ca555d2ec92ec40c5e20f713ba82a0ada
Dallas Phillips <dallasphillips24@gmail.com>
2021-10-18 09:57:41 -0700

    Setting client_id and token to localstorage

--- opencti-platform/opencti-front/src/private/Root.js
+++ opencti-platform/opencti-front/src/private/Root.js
@@ -7,6 +7,7 @@ import { ConnectedThemeProvider } from '../components/AppThemeProvider';
 import Index from './Index';
 import { UserContext } from '../utils/Security';
 import AuthBoundaryComponent from './components/AuthBoundary';
+import { getAccount } from '../services/account.service';
 
 const rootPrivateQuery = graphql`
   query RootPrivateQuery {
@@ -32,13 +33,36 @@ const rootPrivateQuery = graphql`
   }
 `;
 
+const clearToken = () => {
+  localStorage.removeItem('token');
+};
+
+const clearClientId = () => {
+  localStorage.removeItem('client_id');
+};
+
 const Root = () => (
   <AuthBoundaryComponent>
     <QueryRenderer
       query={rootPrivateQuery}
       variables={{}}
       render={({ props }) => {
+        clearToken();
+        clearClientId();
         if (props) {
+          if (props.me && props.me.access_token) {
+            const token = props.me.access_token;
+            localStorage.setItem('token', token);
+            getAccount().then((res) => {
+              const account = res.data;
+              if (account) {
+                const clientId = account.clients?.[0].client_id;
+                localStorage.setItem('client_id', clientId);
+              } else {
+                clearToken();
+              }
+            });
+          }
           return (
             <UserContext.Provider
               value={{ me: props.me, settings: props.settings }}
