5e2eec130ce7d77938840694952e4f77ebdde8b3
Axel Fahy <axf@open.ch>
2021-10-27 10:47:08 +0200

    [api] fix `resolves-to` relationship between two `domain-names`

--- opencti-platform/opencti-front/src/utils/Relation.js
+++ opencti-platform/opencti-front/src/utils/Relation.js
@@ -212,6 +212,7 @@ const stixCyberObservableRelationshipTypesMapping = {
   'Email-Mime-Part-Type_Artifact': ['body-raw'],
   StixFile_Directory: ['parent-directory', 'contains'],
   StixFile_Artifact: ['obs_content'],
+  'Domain-Name_Domain-Name': ['resolves-to'],
   'Domain-Name_IPv4-Addr': ['resolves-to'],
   'Domain-Name_IPv6-Addr': ['resolves-to'],
   'IPv4-Addr_Mac-Addr': ['resolves-to'],
