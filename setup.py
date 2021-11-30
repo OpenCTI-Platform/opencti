e20c2380b7a194a2a1749d0a8f95f4d3b9843458
Samuel Hassine <samuel@hassine.fr>
2021-11-30 11:01:32 +0100

    [client] Bump version
    (original commit: OpenCTI-Platform/client-python@5b9b3ca)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 from setuptools import setup
 from setuptools.command.install import install
 
-VERSION = "5.1.0"
+VERSION = "5.1.1"
 
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
