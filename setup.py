95833c3910572697536ec7f6e85728facbe6c10b
nor3th <73965465+nor3th@users.noreply.github.com>
2021-12-19 22:21:51 +0100

    [client] Bump version for fix
    (original commit: OpenCTI-Platform/client-python@40419eb)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 from setuptools import setup
 from setuptools.command.install import install
 
-VERSION = "5.1.2"
+VERSION = "5.1.2a"
 
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
