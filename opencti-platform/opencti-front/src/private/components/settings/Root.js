8ebcfcc4aa581b4cebb1bf9313e7a4086b511b5c
Samuel Hassine <samuel@hassine.fr>
2021-12-23 18:57:44 +0100

    [frontend] Protect settings pages even if API queries are allowed (#1777)

--- opencti-platform/opencti-front/src/private/components/settings/Root.js
+++ opencti-platform/opencti-front/src/private/components/settings/Root.js
@@ -14,75 +14,78 @@ import Labels from './Labels';
 import Workflow from './Workflow';
 import Retention from './Retention';
 import { BoundaryRoute } from '../Error';
+import Security, { SETTINGS } from '../../../utils/Security';
 
 const Root = () => (
   <Switch>
-    <BoundaryRoute exact path="/dashboard/settings" component={Settings} />
-    <BoundaryRoute
-      exact
-      path="/dashboard/settings/accesses"
-      render={() => <Redirect to="/dashboard/settings/accesses/roles" />}
-    />
-    <BoundaryRoute
-      exact
-      path="/dashboard/settings/accesses/users"
-      component={Users}
-    />
-    <BoundaryRoute
-      path="/dashboard/settings/accesses/users/:userId"
-      render={(routeProps) => <RootUser {...routeProps} />}
-    />
-    <BoundaryRoute
-      exact
-      path="/dashboard/settings/accesses/roles"
-      component={Roles}
-    />
-    <BoundaryRoute
-      exact
-      path="/dashboard/settings/accesses/groups"
-      component={Groups}
-    />
-    <BoundaryRoute
-      exact
-      path="/dashboard/settings/accesses/sessions"
-      component={Sessions}
-    />
-    <BoundaryRoute
-      exact
-      path="/dashboard/settings/accesses/marking"
-      component={MarkingDefinitions}
-    />
-    <BoundaryRoute
-      exact
-      path="/dashboard/settings/workflow"
-      component={Workflow}
-    />
-    <BoundaryRoute
-      exact
-      path="/dashboard/settings/retention"
-      component={Retention}
-    />
-    <BoundaryRoute exact path="/dashboard/settings/rules" component={Rules} />
-    <BoundaryRoute
-      exact
-      path="/dashboard/settings/attributes"
-      render={() => <Redirect to="/dashboard/settings/attributes/labels" />}
-    />
-    <BoundaryRoute
-      exact
-      path="/dashboard/settings/attributes/labels"
-      component={Labels}
-    />
-    <BoundaryRoute
-      exact
-      path="/dashboard/settings/attributes/kill_chain_phases"
-      component={KillChainPhases}
-    />
-    <BoundaryRoute
-      exact
-      path="/dashboard/settings/attributes/:attributeKey"
-      component={Attributes}
-    />
+    <Security needs={[SETTINGS]} placeholder={<Redirect to="/dashboard" />}>
+      <BoundaryRoute exact path="/dashboard/settings" component={Settings} />
+      <BoundaryRoute
+        exact
+        path="/dashboard/settings/accesses"
+        render={() => <Redirect to="/dashboard/settings/accesses/roles" />}
+      />
+      <BoundaryRoute
+        exact
+        path="/dashboard/settings/accesses/users"
+        component={Users}
+      />
+      <BoundaryRoute
+        path="/dashboard/settings/accesses/users/:userId"
+        render={(routeProps) => <RootUser {...routeProps} />}
+      />
+      <BoundaryRoute
+        exact
+        path="/dashboard/settings/accesses/roles"
+        component={Roles}
+      />
+      <BoundaryRoute
+        exact
+        path="/dashboard/settings/accesses/groups"
+        component={Groups}
+      />
+      <BoundaryRoute
+        exact
+        path="/dashboard/settings/accesses/sessions"
+        component={Sessions}
+      />
+      <BoundaryRoute
+        exact
+        path="/dashboard/settings/accesses/marking"
+        component={MarkingDefinitions}
+      />
+      <BoundaryRoute
+        exact
+        path="/dashboard/settings/workflow"
+        component={Workflow}
+      />
+      <BoundaryRoute
+        exact
+        path="/dashboard/settings/retention"
+        component={Retention}
+      />
+      <BoundaryRoute exact path="/dashboard/settings/rules" component={Rules} />
+      <BoundaryRoute
+        exact
+        path="/dashboard/settings/attributes"
+        render={() => <Redirect to="/dashboard/settings/attributes/labels" />}
+      />
+      <BoundaryRoute
+        exact
+        path="/dashboard/settings/attributes/labels"
+        component={Labels}
+      />
+      <BoundaryRoute
+        exact
+        path="/dashboard/settings/attributes/kill_chain_phases"
+        component={KillChainPhases}
+      />
+      <BoundaryRoute
+        exact
+        path="/dashboard/settings/attributes/:attributeKey"
+        component={Attributes}
+      />
+    </Security>
   </Switch>
 );
 
