5144f415f0feee92a41243c2eb690353bb1b06f2
Julien Richard <richard.julien@gmail.com>
2021-12-24 13:51:04 +0100

    [api] Add capability to support multiple OpenID Connect strategy providers (#1754)

--- opencti-platform/opencti-graphql/src/config/providers.js
+++ opencti-platform/opencti-graphql/src/config/providers.js
@@ -127,7 +127,7 @@ const providerKeys = Object.keys(confProviders);
 for (let i = 0; i < providerKeys.length; i += 1) {
   const providerIdent = providerKeys[i];
   const provider = confProviders[providerIdent];
-  const { strategy, config } = provider;
+  const { identifier, strategy, config } = provider;
   let mappedConfig = configRemapping(config);
   if (config === undefined || !config.disabled) {
     const providerName = config?.label || providerIdent;
@@ -147,7 +147,7 @@ for (let i = 0; i < providerKeys.length; i += 1) {
       providers.push({ name: providerName, type: AUTH_FORM, strategy, provider: 'local' });
     }
     if (strategy === STRATEGY_LDAP) {
-      // eslint-disable-next-line
+      const providerRef = identifier || 'ldapauth';
       const allowSelfSigned = mappedConfig.allow_self_signed || mappedConfig.allow_self_signed === 'true';
       mappedConfig = R.assoc('tlsOptions', { rejectUnauthorized: !allowSelfSigned }, mappedConfig);
       const ldapOptions = { server: mappedConfig };
@@ -197,11 +197,12 @@ for (let i = 0; i < providerKeys.length; i += 1) {
           done({ message: 'Restricted access, ask your administrator' });
         }
       });
-      passport.use('ldapauth', ldapStrategy);
-      providers.push({ name: providerName, type: AUTH_FORM, strategy, provider: 'ldapauth' });
+      passport.use(providerRef, ldapStrategy);
+      providers.push({ name: providerName, type: AUTH_FORM, strategy, provider: providerRef });
     }
     // SSO Strategies
     if (strategy === STRATEGY_SAML) {
+      const providerRef = identifier || 'saml';
       const samlOptions = { ...mappedConfig };
       const samlStrategy = new SamlStrategy(samlOptions, (profile, done) => {
         const roleAttributes = mappedConfig.roles_management?.role_attributes || ['Role'];
@@ -225,10 +226,11 @@ for (let i = 0; i < providerKeys.length; i += 1) {
           done({ message: 'Restricted access, ask your administrator' });
         }
       });
-      passport.use('saml', samlStrategy);
-      providers.push({ name: providerName, type: AUTH_SSO, strategy, provider: 'saml' });
+      passport.use(providerRef, samlStrategy);
+      providers.push({ name: providerName, type: AUTH_SSO, strategy, provider: providerRef });
     }
     if (strategy === STRATEGY_OPENID) {
+      const providerRef = identifier || 'oic';
       // Here we use directly the config and not the mapped one.
       // All config of openid lib use snake case.
       OpenIDIssuer.discover(config.issuer).then((issuer) => {
@@ -277,11 +279,12 @@ for (let i = 0; i < providerKeys.length; i += 1) {
             done({ message: 'Restricted access, ask your administrator' });
           }
         });
-        passport.use('oic', openIDStrategy);
-        providers.push({ name: providerName, type: AUTH_SSO, strategy, provider: 'oic' });
+        passport.use(providerRef, openIDStrategy);
+        providers.push({ name: providerName, type: AUTH_SSO, strategy, provider: providerRef });
       });
     }
     if (strategy === STRATEGY_FACEBOOK) {
+      const providerRef = identifier || 'facebook';
       const specificConfig = { profileFields: ['id', 'emails', 'name'], scope: 'email' };
       const facebookOptions = { passReqToCallback: true, ...mappedConfig, ...specificConfig };
       const facebookStrategy = new FacebookStrategy(
@@ -293,10 +296,11 @@ for (let i = 0; i < providerKeys.length; i += 1) {
           providerLoginHandler({ email, name: data.first_name }, [], [], done);
         }
       );
-      passport.use('facebook', facebookStrategy);
-      providers.push({ name: providerName, type: AUTH_SSO, strategy, provider: 'facebook' });
+      passport.use(providerRef, facebookStrategy);
+      providers.push({ name: providerName, type: AUTH_SSO, strategy, provider: providerRef });
     }
     if (strategy === STRATEGY_GOOGLE) {
+      const providerRef = identifier || 'google';
       const domains = mappedConfig.domains || [];
       const specificConfig = { scope: ['email', 'profile'] };
       const googleOptions = { passReqToCallback: true, ...mappedConfig, ...specificConfig };
@@ -315,10 +319,11 @@ for (let i = 0; i < providerKeys.length; i += 1) {
           done({ message: 'Restricted access, ask your administrator' });
         }
       });
-      passport.use('google', googleStrategy);
-      providers.push({ name: providerName, type: AUTH_SSO, strategy, provider: 'google' });
+      passport.use(providerRef, googleStrategy);
+      providers.push({ name: providerName, type: AUTH_SSO, strategy, provider: providerRef });
     }
     if (strategy === STRATEGY_GITHUB) {
+      const providerRef = identifier || 'github';
       const organizations = mappedConfig.organizations || [];
       const scope = organizations.length > 0 ? 'user:email,read:org' : 'user:email';
       const githubOptions = { passReqToCallback: true, ...mappedConfig, scope };
@@ -344,10 +349,11 @@ for (let i = 0; i < providerKeys.length; i += 1) {
           done({ message: 'Restricted access, ask your administrator' });
         }
       });
-      passport.use('github', githubStrategy);
-      providers.push({ name: providerName, type: AUTH_SSO, strategy, provider: 'github' });
+      passport.use(providerRef, githubStrategy);
+      providers.push({ name: providerName, type: AUTH_SSO, strategy, provider: providerRef });
     }
     if (strategy === STRATEGY_AUTH0) {
+      const providerRef = identifier || 'auth0';
       const auth0Options = { passReqToCallback: true, ...mappedConfig };
       const auth0Strategy = new Auth0Strategy(
         auth0Options,
@@ -358,13 +364,14 @@ for (let i = 0; i < providerKeys.length; i += 1) {
           providerLoginHandler({ email, name }, [], [], done);
         }
       );
-      passport.use('auth0', auth0Strategy);
-      providers.push({ name: providerName, type: AUTH_SSO, strategy, provider: 'auth0' });
+      passport.use(providerRef, auth0Strategy);
+      providers.push({ name: providerName, type: AUTH_SSO, strategy, provider: providerRef });
     }
     // CERT Strategies
     if (strategy === STRATEGY_CERT) {
+      const providerRef = identifier || 'cert';
       // This strategy is directly handled by express
-      providers.push({ name: providerName, type: AUTH_SSO, strategy, provider: 'cert' });
+      providers.push({ name: providerName, type: AUTH_SSO, strategy, provider: providerRef });
     }
   }
 }
