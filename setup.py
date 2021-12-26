48c9350d7b45fea819350fd095ea02e1e4e199f5
Samuel Hassine <samuel@hassine.fr>
2021-12-26 19:08:52 +0100

    [client] Prepare next release
    (original commit: OpenCTI-Platform/client-python@7579c64)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 from setuptools import setup
 from setuptools.command.install import install
 
-VERSION = "5.1.2a"
+VERSION = "5.1.3"
 
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
