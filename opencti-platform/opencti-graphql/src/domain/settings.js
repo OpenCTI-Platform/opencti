4ee8989aa62e2d82f5a5dbc280af4c2238600c9f
Samuel Hassine <samuel@hassine.fr>
2021-12-27 17:40:39 +0100

    [api] Restore platform_url parameter

--- opencti-platform/opencti-graphql/src/domain/settings.js
+++ opencti-platform/opencti-graphql/src/domain/settings.js
@@ -50,6 +50,7 @@ export const getSettings = async () => {
   ];
   return {
     ...platformSettings,
+    platform_url: conf.get('app:base_url'),
     platform_enable_reference: conf.get('app:enforce_references'),
     platform_reference_attachment: conf.get('app:reference_attachment'),
     platform_feature_flags: featureFlags,
