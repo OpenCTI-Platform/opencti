c9508326108709cdaf8c7d10355212491e5fade2
nor3th <73965465+nor3th@users.noreply.github.com>
2022-01-24 22:33:11 +0100

    [api] Remove faulty Indicator -indicates-> SRO relationship tests

--- opencti-platform/opencti-graphql/tests/03-streams/00-Raw/raw-test.js
+++ opencti-platform/opencti-graphql/tests/03-streams/00-Raw/raw-test.js
@@ -45,7 +45,7 @@ describe('Raw streams tests', () => {
       expect(createEventsByTypes.label.length).toBe(15);
       expect(createEventsByTypes.identity.length).toBe(13);
       expect(createEventsByTypes.relationship.length).toBe(115);
-      expect(createEventsByTypes.indicator.length).toBe(30);
+      expect(createEventsByTypes.indicator.length).toBe(29);
       expect(createEventsByTypes['attack-pattern'].length).toBe(6);
       expect(createEventsByTypes.report.length).toBe(3);
       expect(createEventsByTypes.tool.length).toBe(2);
