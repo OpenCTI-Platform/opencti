9d0119e98384f2a49d9433336c3d4e46205a9093
dphillips9124 <dallasphillips24@gmail.com>
2022-01-25 19:01:39 -0800

    fix(explore results): Adding back the clear filters button. Working on the correct state changes to get to a clean result.

--- opencti-platform/opencti-front/src/private/components/vsac/ExploreResults.js
+++ opencti-platform/opencti-front/src/private/components/vsac/ExploreResults.js
@@ -2,52 +2,18 @@
 import React, { Component } from 'react';
 import * as PropTypes from 'prop-types';
 import ReactHtmlParser from 'react-html-parser';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import * as R from 'ramda';
-import { QueryRenderer } from '../../../relay/environment';
-import {
-  buildViewParamsFromUrlAndStorage,
-  convertFilters,
-  saveViewParameters,
-} from '../../../utils/ListParameters';
-import ListLines from '../../../components/list_lines/ListLines';
 import inject18n from '../../../components/i18n';
-import ToolBar from '../data/ToolBar';
-import { isUniqFilter } from '../common/lists/Filters';
-import Security, { KNOWLEDGE_KNUPDATE } from '../../../utils/Security';
-import DescriptionIcon from '@material-ui/icons/Description';
-import AddIcon from '@material-ui/icons/Add';
-import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
-import ExploreIcon from '@material-ui/icons/Explore';
-import ShowChartIcon from '@material-ui/icons/ShowChart';
-import DeleteIcon from '@material-ui/icons/Delete';
-import IconButton from '@material-ui/core/IconButton';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import ImportExportIcon from '@material-ui/icons/ImportExport';
-import CompareIcon from '@material-ui/icons/Compare';
-import ScannerIcon from '@material-ui/icons/Scanner';
-import PublishIcon from '@material-ui/icons/Publish';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardActions from '@material-ui/core/CardActions';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import Typography from '@material-ui/core/Typography';
-import Tooltip from '@material-ui/core/Tooltip';
 import CardContent from '@material-ui/core/CardContent';
-import { DescriptionOutlined } from '@material-ui/icons';
 import { withStyles } from '@material-ui/core/styles';
-import { fetchAllScans } from '../../../services/scan.service';
 import {
-  fetchAllAnalysis,
   getAnalysisHosts,
   getAnalysisSoftware,
   getAnalysisWeaknesses,
@@ -57,11 +23,7 @@ import {
   getAnalysisFilteredResultsDetails,
   getAnalysisFilteredResultsVulnerability,
 } from '../../../services/analysis.service';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 import moment from 'moment';
-import Dialog from '@material-ui/core/Dialog';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -132,7 +94,9 @@ class ExploreResults extends Component {
       scan: this.props.location.state.scan,
       hosts: null,
       software: null,
+      weakness: null,
       filteredResultsData: null,
+      filteredResultsDataDetails: null,
       currentResult: null,
       tabValue: 0,
       weaknessAccordion: false,
@@ -300,8 +264,7 @@ class ExploreResults extends Component {
       handleFilterResults(params, name);
     };
 
-    const handleVulnerabilitiesAccordion =
-      (panel, params) => (event, isExpanded) => {
+    const handleVulnerabilitiesAccordion = (panel, params) => (event, isExpanded) => {
         this.setState({ vulnerabilitiesAccordion: isExpanded ? panel : false });
         this.setState({ vulnerabilitiesDetails: '' });
         getAnalysisFilteredResultsVulnerability(
@@ -317,15 +280,27 @@ class ExploreResults extends Component {
           });
       };
 
+    const handleReset = () => {
+
+      this.setState({weaknessDetails: null});
+      this.setState({weaknessAccordion: false});
+      this.setState({selectedRow: null});
+      this.setState({currentResult: null});
+      this.setState({filteredResultData: null});
+      this.setState({filteredResultsDataDetails: null});
+      this.setState({vulnerabilitiesDetails: null});
+      this.setState({vulnerabilitiesAccordion: false});
+      handleFilterResults({}, null)
+    }
+
     return (
       <div>
         <Grid container={true} spacing={3}>
           <Grid item={true} xs={12}>
             <Typography variant="h1" component="h2" gutterBottom>
-              {analysis.scan.scan_name} :{' '}
-              {moment(analysis.completed_date).fromNow()}
+              {`${analysis.scan.scan_name}: ${moment(analysis.completed_date).fromNow()}`}
             </Typography>
-            <div>
+            <div style={{display: "flex", alignItems: "center"}}>
               <Chip size="small" style={{ margin: 3 }} label="Top 4" />
               <Chip
                 size="small"
@@ -360,6 +335,15 @@ class ExploreResults extends Component {
                   label={analysis.vignette_name}
                 />
               )}
+              <Button
+                  size="medium"
+                  variant="contained"
+                  style={{margin: 3, marginLeft: "auto"}}
+                  disabled={selectedRow == null}
+                  onClick={handleReset}
+              >
+                Reset
+              </Button>
             </div>
           </Grid>
           <Grid item={true} xs={4}>
