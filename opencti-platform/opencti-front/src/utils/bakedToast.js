32de2deba8c65697486a90774cb6fd15fb8f3b94
dphillips9124 <dallasphillips24@gmail.com>
2022-02-03 16:25:39 -0800

    chore: added more baked toast for other types of messages. added toasts to scan upload

--- opencti-platform/opencti-front/src/utils/bakedToast.js
+++ opencti-platform/opencti-front/src/utils/bakedToast.js
@@ -1,12 +1,42 @@
+/* eslint-disable no-console */
 import { toast } from 'react-toastify';
 
 const baseCloseTime = 4000;
+
+export const toastInfo = (message) => {
+  if (message === undefined) {
+    console.debug('can\'t toast with no message');
+    return;
+  }
+  toast.info(message, {
+    autoClose: baseCloseTime,
+  });
+};
+
+export const toastSuccess = (message) => {
+  if (message === undefined) {
+    console.debug('can\'t toast with no message');
+    return;
+  }
+  toast.success(message, {
+    autoClose: baseCloseTime,
+  });
+};
+
+export const toastWarn = (message) => {
+  if (message === undefined) {
+    console.debug('can\'t toast with no message');
+    return;
+  }
+  toast.warn(message, {
+    autoClose: baseCloseTime,
+  });
+};
 
 export const toastGenericError = (message) => {
   toast.error(
     message || 'An error has occurred',
     {
-      theme: 'dark',
       autoClose: baseCloseTime,
     },
   );
@@ -16,7 +46,6 @@ export const toastAxiosError = (message) => {
   toast.error(
     message || 'Data Fetch Error',
     {
-      theme: 'dark',
       autoClose: baseCloseTime,
     },
   );
