737e5428b6bb172de8179ff219190ab9fc3116bb
Julien Richard <richard.julien@gmail.com>
2022-02-09 19:42:45 +0100

    [api] Upgrade LRU Cache and eslint

--- opencti-platform/opencti-graphql/src/graphql/sseMiddleware.js
+++ opencti-platform/opencti-graphql/src/graphql/sseMiddleware.js
@@ -321,7 +321,7 @@ const createSeeMiddleware = () => {
       // Create channel.
       const { channel, client } = createSseChannel(req, res);
       // If stream is starting after, we need to use the main database to catchup
-      const cache = new LRU({ max: MAX_CACHE_SIZE, maxAge: MAX_CACHE_TIME });
+      const cache = new LRU({ max: MAX_CACHE_SIZE, ttl: MAX_CACHE_TIME });
       // If empty start date, stream all results corresponding to the filters
       // We need to fetch from this start date until the stream existence
       let after = isNotEmptyField(startFrom) ? startFrom : FROM_START_STR;
