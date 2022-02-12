bb657d794081d24f52fefbef89648078dac9dcc9
Samuel Hassine <samuel@hassine.fr>
2022-02-12 11:32:14 +0100

    [api] Update version number in the test, prepare 5.2.0

--- opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/settings-test.js
+++ opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/settings-test.js
@@ -47,7 +47,7 @@ describe('Settings resolver standard behavior', () => {
     expect(queryResult).not.toBeNull();
     const { about } = queryResult.data;
     expect(about).not.toBeNull();
-    expect(about.version).toContain('5.1.4');
+    expect(about.version).toContain('5.2.0');
     expect(about.dependencies.length).toEqual(3);
   });
   it('should settings information', async () => {
