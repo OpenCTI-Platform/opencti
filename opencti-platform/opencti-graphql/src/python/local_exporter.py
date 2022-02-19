017987812b1e409438252cb07f7aa2bfac6cbdec
Davis Schirmer <djds@bghost.xyz>
2022-02-19 15:18:45 -0500

    [graphql] Lint local_importer, local_exporter
    lstrip leading os.path.sep from config file in local_importer
    
    Per https://docs.python.org/3/library/os.path.html#os.path.join:
    
    >  If a component is an absolute path, all previous components are thrown away
    >  and joining continues from the absolute path component.

--- opencti-platform/opencti-graphql/src/python/local_exporter.py
+++ opencti-platform/opencti-graphql/src/python/local_exporter.py
@@ -41,12 +41,13 @@ class TestLocalExporter:
 
 if __name__ == "__main__":
     try:
-        api_url = sys.argv[1]
-        api_token = sys.argv[2]
-        entity_id = sys.argv[3]
-        file_name = sys.argv[4]
-        testLocalExporter = TestLocalExporter(api_url, api_token, entity_id, file_name)
+        testLocalExporter = TestLocalExporter(
+            api_url=sys.argv[1],
+            api_token=sys.argv[2],
+            entity_id=sys.argv[3],
+            file_name=sys.argv[4],
+        )
         testLocalExporter.upload()
-    except Exception as e:
+    except Exception as e:  # pylint: disable=broad-except
         logging.exception(str(e))
         sys.exit(1)
