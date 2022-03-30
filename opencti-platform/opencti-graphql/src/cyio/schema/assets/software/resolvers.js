8210994edccddd5b329174cb260198341173f405
CyberDaedalus00 <Paul.Patrick@daedafusion.com>
2022-03-29 22:33:10 -0400

    return empty array when no labels, notes, or external references

--- opencti-platform/opencti-graphql/src/cyio/schema/assets/software/resolvers.js
+++ opencti-platform/opencti-graphql/src/cyio/schema/assets/software/resolvers.js
@@ -216,7 +216,7 @@ const softwareResolvers = {
   // field-level resolvers
   SoftwareAsset: {
     labels: async (parent, args, {dbName, dataSources, selectMap}) => {
-      if (parent.labels_iri === undefined) return null;
+      if (parent.labels_iri === undefined) return [];
       let iriArray = parent.labels_iri;
       const results = [];
       if (Array.isArray(iriArray) && iriArray.length > 0) {
@@ -256,7 +256,7 @@ const softwareResolvers = {
       }
     },
     external_references: async (parent, args, {dbName, dataSources, selectMap}) => {
-      if (parent.ext_ref_iri === undefined) return null;
+      if (parent.ext_ref_iri === undefined) return [];
       let iriArray = parent.ext_ref_iri;
       const results = [];
       if (Array.isArray(iriArray) && iriArray.length > 0) {
@@ -296,7 +296,7 @@ const softwareResolvers = {
       }
     },
     notes: async (parent, args, {dbName, dataSources, selectMap}) => {
-      if (parent.notes_iri === undefined) return null;
+      if (parent.notes_iri === undefined) return [];
       let iriArray = parent.notes_iri;
       const results = [];
       if (Array.isArray(iriArray) && iriArray.length > 0) {
