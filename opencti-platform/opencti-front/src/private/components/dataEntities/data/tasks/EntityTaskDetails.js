c17bd0343ffc3572d87dd1d92e9c53331d1f864b
aman <aman82591@gmail.com>
2022-05-02 11:51:48 +0530

    Added UI design for Task in Data Entities.

---
+++ opencti-platform/opencti-front/src/private/components/dataEntities/data/tasks/EntityTaskDetails.js
@@ -0,0 +1,285 @@
+/* eslint-disable */
+/* refactor */
+import React, { Component } from 'react';
+import * as PropTypes from 'prop-types';
+import { compose } from 'ramda';
+import { createFragmentContainer } from 'react-relay';
+import graphql from 'babel-plugin-relay/macro';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import { Grid, Switch, Tooltip } from '@material-ui/core';
+import Chip from '@material-ui/core/Chip';
+import Link from '@material-ui/core/Link';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Launch from '@material-ui/icons/Launch';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import { BullseyeArrow, ArmFlexOutline, Information } from 'mdi-material-ui';
+import ListItemText from '@material-ui/core/ListItemText';
+import ExpandableMarkdown from '../../../../../components/ExpandableMarkdown';
+import inject18n from '../../../../../components/i18n';
+import CyioCoreObjectLabelsView from '../../../common/stix_core_objects/CyioCoreObjectLabelsView';
+const styles = (theme) => ({
+  paper: {
+    height: '100%',
+    minHeight: '100%',
+    margin: '10px 0 0 0',
+    padding: '24px 24px 32px 24px',
+    borderRadius: 6,
+  },
+  link: {
+    textAlign: 'left',
+    fontSize: '16px',
+    font: 'DIN Next LT Pro',
+  },
+  chip: {
+    color: theme.palette.header.text,
+    height: 25,
+    fontSize: 12,
+    padding: '14px 12px',
+    margin: '0 7px 7px 0',
+    backgroundColor: theme.palette.header.background,
+  },
+  scrollBg: {
+    background: theme.palette.header.background,
+    width: '100%',
+    color: 'white',
+    padding: '10px 5px 10px 15px',
+    borderRadius: '5px',
+    lineHeight: '20px',
+  },
+  scrollDiv: {
+    width: '100%',
+    background: theme.palette.header.background,
+    height: '223px',
+    overflow: 'hidden',
+    overflowY: 'scroll',
+  },
+  scrollObj: {
+    color: theme.palette.header.text,
+    fontFamily: 'sans-serif',
+    padding: '0px',
+    textAlign: 'left',
+  },
+  markingText: {
+    background: theme.palette.header.text,
+    color: 'black',
+    width: '100px',
+    textAlign: 'center',
+    padding: '3px 0',
+  }
+});
+
+class EntityTaskDetailsComponent extends Component {
+  render() {
+    const {
+      t,
+      classes,
+      refreshQuery,
+      task,
+      fd,
+      history,
+    } = this.props;
+    return (
+      <div style={{ height: '100%' }}>
+        <Typography variant="h4" gutterBottom={true}>
+          {t('Basic Information')}
+        </Typography>
+        <Paper classes={{ root: classes.paper }} elevation={2}>
+          <Grid container={true} spacing={3}>
+            <Grid item={true} xs={3}>
+              <div>
+                <Typography
+                  variant="h3"
+                  color="textSecondary"
+                  gutterBottom={true}
+                >
+                  {t('Name')}
+                </Typography>
+                <div className="clearfix" />
+                {t('Lorem Ipsum')}
+              </div>
+              <div style={{ marginTop: '20px' }}>
+                <Typography
+                  variant="h3"
+                  color="textSecondary"
+                  gutterBottom={true}
+                >
+                  {t('Milestone')}
+                </Typography>
+                <div className="clearfix" />
+                {t('Lorem Ipsum')}
+              </div>
+              <div style={{ marginTop: '20px' }}>
+                <Typography
+                  variant="h3"
+                  color="textSecondary"
+                  gutterBottom={true}
+                >
+                  {t('Associated Activities')}
+                </Typography>
+                <div className="clearfix" />
+                {t('Activity Name')}
+              </div>
+              <div style={{ marginTop: '20px' }}>
+                <Typography
+                  variant="h3"
+                  color="textSecondary"
+                  gutterBottom={true}
+                >
+                  {t('Resource Type')}
+                </Typography>
+                <div className="clearfix" />
+                {t('Screen Shot')}
+              </div>
+              <div style={{ marginTop: '20px' }}>
+                <Typography
+                  variant="h3"
+                  color="textSecondary"
+                  gutterBottom={true}
+                >
+                  {t('Related Tasks')}
+                </Typography>
+                <div className="clearfix" />
+                {t('Task Name 1')}
+              </div>
+            </Grid>
+            <Grid item={true} xs={4}>
+              <div>
+                <Typography
+                  variant="h3"
+                  color="textSecondary"
+                  gutterBottom={true}
+                >
+                  {t('ID')}
+                </Typography>
+                <div className="clearfix" />
+                {t('Lorem Ipsum')}
+              </div>
+              <div style={{ marginTop: '20px' }}>
+                <Typography
+                  variant="h3"
+                  color="textSecondary"
+                  gutterBottom={true}
+                >
+                  {t('End Date')}
+                </Typography>
+                <div className="clearfix" />
+                {t('Lorem Ipsum')}
+              </div>
+              <div style={{ marginTop: '20px' }}>
+                <Typography
+                  variant="h3"
+                  color="textSecondary"
+                  gutterBottom={true}
+                >
+                  {t('Dependencies')}
+                </Typography>
+                <div className="clearfix" />
+                {t('Dependency Name')}
+              </div>
+              <div style={{ marginTop: '20px' }}>
+                <Typography
+                  variant="h3"
+                  color="textSecondary"
+                  gutterBottom={true}
+                >
+                  {t('Responsible Parties')}
+                </Typography>
+                <div className="clearfix" />
+                {t('Responsible Party 1')}
+              </div>
+              <div style={{ marginTop: '20px' }}>
+                <Typography
+                  variant="h3"
+                  color="textSecondary"
+                  gutterBottom={true}
+                >
+                  {t('Resource Name')}
+                </Typography>
+                <div className="clearfix" />
+                {t('Screenshot Name')}
+              </div>
+            </Grid>
+            <Grid item={true} xs={4}>
+              <Typography
+                variant="h3"
+                color="textSecondary"
+                gutterBottom={true}
+              >
+                {t('Description')}
+              </Typography>
+              <div className="clearfix" />
+              <div className={classes.scrollBg}>
+                <div className={classes.scrollDiv}>
+                  <div className={classes.scrollObj}>
+                    {t('Lorem Ipsum')}
+                  </div>
+                </div>
+              </div>
+            </Grid>
+          </Grid>
+          <Grid container={true} spacing={3}>
+            <Grid item={true} xs={3}>
+              <CyioCoreObjectLabelsView
+                labels={task.labels}
+                marginTop={0}
+                refreshQuery={refreshQuery}
+                id={task.id}
+                typename={task.__typename}
+              />
+            </Grid>
+            <Grid item={true} xs={4}>
+              <Typography
+                variant="h3"
+                color="textSecondary"
+                gutterBottom={true}
+              >
+                {t('Markings')}
+              </Typography>
+              <div className="clearfix" />
+              <p className={classes.markingText}>
+                {t('IEP: WHITE')}
+              </p>
+            </Grid>
+          </Grid>
+        </Paper>
+      </div>
+    );
+  }
+}
+
+EntityTaskDetailsComponent.propTypes = {
+  task: PropTypes.object,
+  classes: PropTypes.object,
+  refreshQuery: PropTypes.func,
+  t: PropTypes.func,
+  fd: PropTypes.func,
+};
+
+const EntityTaskDetails = createFragmentContainer(
+  EntityTaskDetailsComponent,
+  {
+    task: graphql`
+      fragment EntityTaskDetails_task on OscalTask {
+        __typename
+        id
+        created
+        modified
+        name
+        description
+        labels {
+          __typename
+          id
+          name
+          color
+          entity_type
+          description
+        }
+      }
+    `,
+  },
+);
+
+export default compose(inject18n, withStyles(styles))(EntityTaskDetails);
