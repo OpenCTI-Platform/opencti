7ba6aebdd1192f8004f6d4c04d49eb40358a7b49
Sachin <sachin755@gmail.com>
2022-05-03 15:42:12 +0530

    Fixed bugs in responsible parties

--- opencti-platform/opencti-front/src/private/components/nav/TopBarMenu.js
+++ opencti-platform/opencti-front/src/private/components/nav/TopBarMenu.js
@@ -48,6 +48,7 @@ import TopMenuAssets from './TopMenuAssets';
 import TopMenuDataEntities from './TopMenuDataEntities';
 import TopMenuDataRolesEntities from './TopMenuDataRolesEntities';
 import TopMenuDataTasksEntities from './TopMenuDataTasksEntities';
+import TopMenuDataResponsiblePartiesEntities from './TopMenuDataResponsiblePartiesEntities';
 import TopMenuRiskAssessment from './TopMenuRiskAssessment';
 import TopMenuThreatActor from './TopMenuThreatActor';
 import TopMenuDevice from './TopMenuDevice';
@@ -256,6 +257,10 @@ const TopBarMenu = ({
           {(location.pathname === '/data/entities/tasks'
           || location.pathname === '/data/data source/tasks') && (
               <TopMenuDataTasksEntities />
+            )}
+          {(location.pathname === '/data/entities/responsible_parties'
+          || location.pathname === '/data/data source/responsible_parties') && (
+              <TopMenuDataResponsiblePartiesEntities />
             )}
           {location.pathname.includes('/defender HQ/assets/devices/') && (
             <TopMenuDevice />
