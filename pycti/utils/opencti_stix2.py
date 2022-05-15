37e71c3e21e53df496aa500ace8233fb6f42eb41
Samuel Hassine <samuel.hassine@filigran.io>
2022-05-15 17:29:16 +0200

    [client] Change ordering

--- pycti/utils/opencti_stix2.py
+++ pycti/utils/opencti_stix2.py
@@ -1730,11 +1730,11 @@ class OpenCTIStix2:
                                         item, observed_data_ref, to_id, update
                                     )
                 elif item["type"] == "label":
-                    self.opencti.label.create(update=update, **item)
+                    self.opencti.label.create(**item, update=update)
                 elif item["type"] == "external-reference":
-                    self.opencti.external_reference.create(update=update, **item)
+                    self.opencti.external_reference.create(**item, update=update)
                 elif item["type"] == "kill-chain-phase":
-                    self.opencti.kill_chain_phase.create(update=update, **item)
+                    self.opencti.kill_chain_phase.create(**item, update=update)
                 elif StixCyberObservableTypes.has_value(item["type"]):
                     if types is None or len(types) == 0:
                         self.import_observable(item, update, types)
