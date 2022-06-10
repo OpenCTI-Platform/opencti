0ead280fbc4e316e78cf438d678364d9e2127732
Samuel Hassine <samuel.hassine@filigran.io>
2022-06-10 19:55:37 +0200

    [api/worker] Bump versions and Python for hotfix

--- opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/settings-test.js
+++ opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/settings-test.js
@@ -47,7 +47,7 @@ describe('Settings resolver standard behavior', () => {
     expect(queryResult).not.toBeNull();
     const { about } = queryResult.data;
     expect(about).not.toBeNull();
-    expect(about.version).toContain('5.3.4');
+    expect(about.version).toContain('5.3.5');
     expect(about.dependencies.length).toEqual(3);
   });
   it('should settings information', async () => {
