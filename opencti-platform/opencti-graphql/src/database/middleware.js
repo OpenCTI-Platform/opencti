8674a2363c4fbf4a5da8fa2e986e10adf1c4029b
Samuel Hassine <samuel.hassine@filigran.io>
2022-06-14 00:43:45 +0200

    [api] Fix confidence level check at entities creation

--- opencti-platform/opencti-graphql/src/database/middleware.js
+++ opencti-platform/opencti-graphql/src/database/middleware.js
@@ -2112,8 +2112,7 @@ const buildRelationTimeFilter = (input) => {
 
 const upsertElementRaw = async (user, element, type, updatePatch) => {
   // Upsert relation
-  // const forceUpdate = updatePatch.update === true;
-  const forceUpdate = updatePatch.update === true || (typeof updatePatch.confidence === 'number' && typeof element.confidence === 'number' && updatePatch.confidence > element.confidence);
+  const forceUpdate = updatePatch.update === true || updatePatch.confidence > element.confidence;
   const patchInputs = []; // Sourced inputs for event stream
   const impactedInputs = []; // All inputs impacted by modifications (+inner)
   const rawRelations = [];
@@ -2874,8 +2873,7 @@ export const createEntityRaw = async (user, input, type, opts = {}) => {
         // If creation is not by a reference
         // We can in best effort try to merge a common stix_id
         const existingByStandard = R.find((e) => e.standard_id === standardId, filteredEntities);
-        // if (resolvedInput.update === true) {
-        if (resolvedInput.update === true || (typeof resolvedInput.confidence === 'number' && typeof existingByStandard.confidence === 'number' && resolvedInput.confidence > existingByStandard.confidence)) {
+        if (resolvedInput.update === true) {
           // The new one is new reference, merge all found entities
           // Target entity is existingByStandard by default or any other
           const target = R.find((e) => e.standard_id === standardId, filteredEntities) || R.head(filteredEntities);
