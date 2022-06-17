22ceace5c25ba6e90ff9d8de4cddb118bc6dce64
Jake Walker <hi@jakew.me>
2022-06-17 15:35:38 +0100

    Support for IAM role usage when interacting with S3 (#2168)
    Supporting IAM role access to interact with S3 instead of requiring IAM users when a user wishes to use AWS S3 instead of MinIO server.
    
    Co-authored-by: ReadyElbow <53292976+ReadyElbow@users.noreply.github.com>

--- opencti-platform/opencti-graphql/src/database/minio.js
+++ opencti-platform/opencti-graphql/src/database/minio.js
@@ -2,6 +2,7 @@ import * as Minio from 'minio';
 import * as He from 'he';
 import * as R from 'ramda';
 import querystring from 'querystring';
+import { fromNodeProviderChain } from '@aws-sdk/credential-providers';
 import conf, { booleanConf, configureCA, logApp, logAudit } from '../config/conf';
 import { buildPagination } from './utils';
 import { loadExportWorksAsProgressFiles, deleteWorkForFile } from '../domain/work';
@@ -13,19 +14,51 @@ const bucketName = conf.get('minio:bucket_name') || 'opencti-bucket';
 const bucketRegion = conf.get('minio:bucket_region') || 'us-east-1';
 const excludedFiles = conf.get('minio:excluded_files') || ['.DS_Store'];
 
-const minioClient = new Minio.Client({
-  endPoint: conf.get('minio:endpoint'),
-  port: conf.get('minio:port') || 9000,
-  useSSL: booleanConf('minio:use_ssl', false),
+const minioCredentials = {
   accessKey: String(conf.get('minio:access_key')),
   secretKey: String(conf.get('minio:secret_key')),
-  reqOptions: {
-    ...configureCA(conf.get('minio:ca')),
-    servername: conf.get('minio:endpoint'),
-  },
-});
+  sessionToken: undefined,
+  expiration: -1
+};
+
+const getMinioClient = async () => {
+  // Attempt to fetch AWS role for authentication if enabled
+  if (booleanConf('minio:use_aws_role', false)) {
+    // Add 5 minutes to the current time so that new credentials are fetched
+    const expireDate = new Date((new Date()).getTime() + 300000);
+
+    if (minioCredentials.expiration === -1 || (minioCredentials.expiration && minioCredentials.expiration < expireDate)) {
+      try {
+        const credentialProvider = fromNodeProviderChain();
+        const awsCredentials = await credentialProvider();
+
+        minioCredentials.accessKey = awsCredentials.accessKeyId;
+        minioCredentials.secretKey = awsCredentials.secretAccessKey;
+        minioCredentials.sessionToken = awsCredentials.sessionToken;
+        minioCredentials.expiration = awsCredentials.expiration;
+      } catch (e) {
+        logApp.error('[MINIO] Failed to fetch AWS role credentials', { error: e });
+      }
+    }
+  }
+
+  return new Minio.Client({
+    endPoint: conf.get('minio:endpoint'),
+    port: conf.get('minio:port') || 9000,
+    useSSL: booleanConf('minio:use_ssl', false),
+    accessKey: minioCredentials.accessKey,
+    secretKey: minioCredentials.secretKey,
+    sessionToken: minioCredentials.sessionToken,
+    reqOptions: {
+      ...configureCA(conf.get('minio:ca')),
+      servername: conf.get('minio:endpoint'),
+    },
+  });
+};
 
 export const initializeMinioBucket = async () => {
+  const minioClient = await getMinioClient();
+
   return new Promise((resolve, reject) => {
     try {
       minioClient.bucketExists(bucketName, (existErr, exists) => {
@@ -52,6 +85,8 @@ export const isStorageAlive = () => {
 };
 
 export const deleteFile = async (user, id) => {
+  const minioClient = await getMinioClient();
+
   logApp.debug(`[MINIO] delete file ${id} by ${user.user_email}`);
   await minioClient.removeObject(bucketName, id);
   await deleteWorkForFile(user, id);
@@ -67,7 +102,9 @@ export const deleteFiles = async (user, ids) => {
   return true;
 };
 
-export const downloadFile = (id) => {
+export const downloadFile = async (id) => {
+  const minioClient = await getMinioClient();
+
   try {
     return minioClient.getObject(bucketName, id);
   } catch (err) {
@@ -78,17 +115,22 @@ export const downloadFile = (id) => {
 
 export const getFileContent = (id) => {
   return new Promise((resolve, reject) => {
-    let str = '';
-    minioClient.getObject(bucketName, id, (err, stream) => {
-      stream.on('data', (data) => {
-        str += data.toString('utf-8');
+    getMinioClient().then((minioClient) => {
+      let str = '';
+
+      minioClient.getObject(bucketName, id, (err, stream) => {
+        stream.on('data', (data) => {
+          str += data.toString('utf-8');
+        });
+        stream.on('end', () => {
+          resolve(str);
+        });
+        stream.on('error', (error) => {
+          reject(error);
+        });
       });
-      stream.on('end', () => {
-        resolve(str);
-      });
-      stream.on('error', (error) => {
-        reject(error);
-      });
+    }).catch((err) => {
+      reject(err);
     });
   });
 };
@@ -103,6 +145,8 @@ export const storeFileConverter = (user, file) => {
 };
 
 export const loadFile = async (user, filename) => {
+  const minioClient = await getMinioClient();
+
   try {
     const stat = await minioClient.statObject(bucketName, filename);
     return {
@@ -126,19 +170,23 @@ const isFileObjectExcluded = (id) => {
 };
 export const rawFilesListing = (user, directory, recursive = false) => {
   return new Promise((resolve, reject) => {
-    const files = [];
-    const stream = minioClient.listObjectsV2(bucketName, directory, recursive);
-    stream.on('data', async (obj) => {
-      if (obj.name && !isFileObjectExcluded(obj.name)) {
-        files.push(R.assoc('id', obj.name, obj));
-      }
+    getMinioClient().then((minioClient) => {
+      const files = [];
+      const stream = minioClient.listObjectsV2(bucketName, directory, recursive);
+      stream.on('data', async (obj) => {
+        if (obj.name && !isFileObjectExcluded(obj.name)) {
+          files.push(R.assoc('id', obj.name, obj));
+        }
+      });
+      /* istanbul ignore next */
+      stream.on('error', (e) => {
+        logApp.error('[MINIO] Error listing files', { error: e });
+        reject(e);
+      });
+      stream.on('end', () => resolve(files));
+    }).catch((err) => {
+      reject(err);
     });
-    /* istanbul ignore next */
-    stream.on('error', (e) => {
-      logApp.error('[MINIO] Error listing files', { error: e });
-      reject(e);
-    });
-    stream.on('end', () => resolve(files));
   }).then((files) => {
     return Promise.all(
       R.map((elem) => {
@@ -150,6 +198,8 @@ export const rawFilesListing = (user, directory, recursive = false) => {
 };
 
 export const upload = async (user, path, fileUpload, metadata = {}) => {
+  const minioClient = await getMinioClient();
+
   const { createReadStream, filename, mimetype, encoding = '', version = now() } = await fileUpload;
   logAudit.info(user, UPLOAD_ACTION, { path, filename, metadata });
   const escapeName = querystring.escape(filename);
