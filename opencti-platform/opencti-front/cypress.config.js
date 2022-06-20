471a01487b7ef37308c6983dcb1efc68c16d81d2
Ryan Joyce <r-joyce@users.noreply.github.com>
2022-06-20 11:29:42 -0700

    Upgrading Cypress to version 10

---
+++ opencti-platform/opencti-front/cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  chromeWebSecurity: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'https://cyio-localhost.darklight.ai:3000',
+  },
+})
