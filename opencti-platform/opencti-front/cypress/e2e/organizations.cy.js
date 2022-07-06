0551a1cc089f6f3666d2eb53a2f70bf04382c001
Ryan Joyce <r-joyce@users.noreply.github.com>
2022-07-06 15:08:19 -0700

    Updating organization tests

--- opencti-platform/opencti-front/cypress/e2e/organizations.cy.js
+++ opencti-platform/opencti-front/cypress/e2e/organizations.cy.js
@@ -1,14 +1,25 @@
 describe('Organizations', () => {
-  beforeEach(() => {
+  before(function () {
     cy.fixture('users.json').then((user) => {
+      this.user = user;
       cy.visit('/');
       cy.login(user.email, user.password);
-      cy.url().should('eq', `${Cypress.config().baseUrl}/dashboard`);
     });
+  });
+
+  beforeEach(() => {
+    Cypress.Cookies.preserveOnce('token', 'client_id');
   });
 
   it('Has a current organization', () => {
     cy.expect('[data-cy="organization"]')
       .to.have.length.of.at.least(1);
+  });
+
+  it('Can view the list of organizations to switch to', () => {
+    cy.get('[data-cy="organization"]').click();
+    cy.get('[data-cy="org selection"]').should('be.visible');
+    cy.get('[data-cy="org selection"]').click();
+    cy.get('[data-cy="an org"]').should('be.visible');
   });
 });
