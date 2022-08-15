3167614e484cae39ec39ec7c5b9d10bb599c83c4
Dallas Phillips <dallasphillips24@gmail.com>
2022-08-15 15:12:13 -0700

    feat: expose ability to update a scan name

---
+++ opencti-platform/opencti-front/src/private/components/vsac/modals/UpdateScan.js
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Card, CardActions, CardContent, CardHeader, FormControl, FormGroup, Paper, TextField, Typography,
+} from '@material-ui/core';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import { updateScan } from '../../../../services/scan.service';
+
+const UpdateScan = (props) => {
+  const [newName, setNewName] = useState(null);
+  const [handling, setHandling] = useState(false);
+  const [errors, setErrors] = useState({ name: 'value is required' });
+  const [submitError, setSubmitError] = useState(null);
+  const [validForm, setValidForm] = useState(false);
+
+  function handleOnClose(success) {
+    props.onClose?.(success);
+  }
+
+  function handleNameChange(value) {
+    setNewName(value);
+    if (value === undefined) {
+      errors.name = 'value is required';
+    } else if (value.length < 3) {
+      errors.name = 'value must be at least 3 characters';
+    } else {
+      errors.name = null;
+    }
+    setErrors(errors);
+  }
+
+  function submitForm() {
+    setHandling(true);
+    updateScan(props.scan.id, props.clientId, { scan_name: newName })
+      .then(() => {
+        setHandling(false);
+        handleOnClose(true);
+      })
+      .catch((err) => {
+        console.error(err);
+        setHandling(false);
+        setSubmitError('failed to submit changes');
+      });
+  }
+
+  useEffect(() => {
+    if (Object.values(errors).filter((v) => v != null).length === 0) {
+      setValidForm(true);
+    } else {
+      setValidForm(false);
+    }
+  }, [errors, newName]);
+
+  return (
+    <Paper elevation={2} style={{ width: 400 }}>
+      <Card>
+        <CardHeader title={`Edit ${props.scan.scan_name}`}/>
+        <CardContent>
+          <FormGroup fullwidth>
+            <FormControl>
+              <TextField variant={'filled'} label={'Name'} onChange={(e) => handleNameChange(e.target.value)}/>
+            </FormControl>
+          </FormGroup>
+          {
+            submitError && <Typography variant={'caption'} sx={{ color: 'crimson' }}> submitError</Typography>
+          }
+        </CardContent>
+        <CardActions style={{ justifyContent: 'right' }}>
+          <Button
+            size="small"
+            color="secondary"
+            disabled={handling}
+            onClick={() => handleOnClose(false)}
+          >
+            Cancel
+          </Button>
+          <Button
+            size="small"
+            color="primary"
+            disabled={handling || !validForm}
+            onClick={() => submitForm()}
+          >
+            Submit
+          </Button>
+        </CardActions>
+      </Card>
+    </Paper>
+  );
+};
+
+UpdateScan.propTypes = {
+  onClose: PropTypes.func,
+  clientId: PropTypes.string,
+  scan: PropTypes.object,
+};
+
+export default UpdateScan;
