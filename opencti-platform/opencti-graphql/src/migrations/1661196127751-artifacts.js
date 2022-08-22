6868cfec0a3fa453d12eede00127b0f97a87d5d5
Julien Richard <julien.richard@filigran.io>
2022-08-22 22:22:13 +0200

    [api] Add artifact files migration (#2247)

---
+++ opencti-platform/opencti-graphql/src/migrations/1661196127751-artifacts.js
@@ -0,0 +1,42 @@
+import * as R from 'ramda';
+import { Promise } from 'bluebird';
+import { deleteFiles, rawFilesListing, storeFileConverter } from '../database/file-storage';
+import { SYSTEM_USER } from '../utils/access';
+import { elUpdate, ES_MAX_CONCURRENCY } from '../database/engine';
+import { internalLoadById } from '../database/middleware';
+import { logApp } from '../config/conf';
+
+export const up = async (next) => {
+  logApp.info('[MIGRATION] Starting files artifacts migration');
+  const imports = await rawFilesListing(SYSTEM_USER, '/import/Artifact', true);
+  const importGroups = R.groupBy((i) => i.metaData.entity_id, imports);
+  const groupSize = Object.keys(importGroups).length;
+  logApp.info(`[MIGRATION] Migrating ${groupSize} artifacts references`);
+  const importEntries = Object.entries(importGroups);
+  let migratedCount = 0;
+  const concurrentChange = (entry) => {
+    const [id, groupFiles] = entry;
+    return internalLoadById(SYSTEM_USER, id).then((element) => {
+      if (element) {
+        const eventFiles = groupFiles.map((f) => storeFileConverter(SYSTEM_USER, f));
+        const source = 'ctx._source.x_opencti_files = params.files;';
+        return elUpdate(element._index, element.internal_id, {
+          script: { source, lang: 'painless', params: { files: eventFiles } },
+        });
+      }
+      return deleteFiles(SYSTEM_USER, groupFiles.map((g) => g.id));
+    }).then(() => {
+      migratedCount += 1;
+      if (migratedCount % 100 === 0) {
+        logApp.info(`[MIGRATION] Migrating artifacts: ${migratedCount}/${groupSize}`);
+      }
+    });
+  };
+  await Promise.map(importEntries, concurrentChange, { concurrency: ES_MAX_CONCURRENCY });
+  logApp.info('[MIGRATION] files artifacts migration finished');
+  next();
+};
+
+export const down = async (next) => {
+  next();
+};
