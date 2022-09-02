844fa2605743457ae99d00aef0e1f4174acba065
Samuel Hassine <samuel.hassine@filigran.io>
2022-09-02 21:47:20 +0200

    [api] Fix objectContains filter on observed data (#2305)

--- opencti-platform/opencti-graphql/src/resolvers/observedData.js
+++ opencti-platform/opencti-graphql/src/resolvers/observedData.js
@@ -19,7 +19,7 @@ import {
   stixDomainObjectEditContext,
   stixDomainObjectEditField,
 } from '../domain/stixDomainObject';
-import { RELATION_CREATED_BY, RELATION_OBJECT_LABEL, RELATION_OBJECT_MARKING } from '../schema/stixMetaRelationship';
+import { RELATION_CREATED_BY, RELATION_OBJECT, RELATION_OBJECT_LABEL, RELATION_OBJECT_MARKING } from '../schema/stixMetaRelationship';
 import { buildRefRelationKey } from '../schema/general';
 
 const observedDataResolvers = {
@@ -58,6 +58,7 @@ const observedDataResolvers = {
     createdBy: buildRefRelationKey(RELATION_CREATED_BY),
     markedBy: buildRefRelationKey(RELATION_OBJECT_MARKING),
     labelledBy: buildRefRelationKey(RELATION_OBJECT_LABEL),
+    objectContains: buildRefRelationKey(RELATION_OBJECT),
   },
   Mutation: {
     observedDataEdit: (_, { id }, { user }) => ({
