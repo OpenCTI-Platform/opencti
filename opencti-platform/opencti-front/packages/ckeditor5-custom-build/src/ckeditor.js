3f27022b52509aaef5dad4b94456fd08fd4c2900
Samuel Hassine <samuel.hassine@filigran.io>
2022-09-08 09:20:44 +0200

    [frontend] Fix HTML rich editor (#2326)

--- opencti-platform/opencti-front/packages/ckeditor5-custom-build/src/ckeditor.js
+++ opencti-platform/opencti-front/packages/ckeditor5-custom-build/src/ckeditor.js
@@ -4,12 +4,16 @@
  */
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
+import AutoImage from '@ckeditor/ckeditor5-image/src/autoimage.js';
 import AutoLink from '@ckeditor/ckeditor5-link/src/autolink.js';
+import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import Code from '@ckeditor/ckeditor5-basic-styles/src/code.js';
 import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
 import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
 import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily.js';
 import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
@@ -19,23 +23,24 @@ import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalli
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
 import ImageInsert from '@ckeditor/ckeditor5-image/src/imageinsert.js';
 import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize.js';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
 import LinkImage from '@ckeditor/ckeditor5-link/src/linkimage.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
-import Mention from '@ckeditor/ckeditor5-mention/src/mention.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
-import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting.js';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
 import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters.js';
 import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js';
 import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js';
 import Table from '@ckeditor/ckeditor5-table/src/table.js';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
+import TableColumnResize from '@ckeditor/ckeditor5-table/src/tablecolumnresize.js';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
 
 class Editor extends ClassicEditor {}
@@ -43,12 +48,16 @@ class Editor extends ClassicEditor {}
 // Plugins to include in the build.
 Editor.builtinPlugins = [
 	Alignment,
+	Autoformat,
+	AutoImage,
 	AutoLink,
+	Base64UploadAdapter,
 	BlockQuote,
 	Bold,
 	Code,
 	CodeBlock,
 	Essentials,
+	FontBackgroundColor,
 	FontColor,
 	FontFamily,
 	FontSize,
@@ -58,23 +67,24 @@ Editor.builtinPlugins = [
 	Image,
 	ImageInsert,
 	ImageResize,
+	ImageStyle,
+	ImageToolbar,
 	ImageUpload,
 	Indent,
+	IndentBlock,
 	Italic,
 	Link,
 	LinkImage,
 	List,
 	MediaEmbed,
-	Mention,
 	Paragraph,
 	PasteFromOffice,
-	SourceEditing,
+	RemoveFormat,
 	SpecialCharacters,
 	Subscript,
 	Superscript,
 	Table,
-	TableToolbar,
-	TextTransformation,
+	TableColumnResize,
 	Underline
 ];
 
@@ -82,45 +92,49 @@ Editor.builtinPlugins = [
 Editor.defaultConfig = {
 	toolbar: {
 		items: [
-			'fontFamily',
 			'heading',
-			'|',
-			'fontColor',
+			'fontFamily',
 			'fontSize',
-			'highlight',
+			'|',
 			'bold',
 			'italic',
 			'underline',
+			'link',
+			'fontColor',
+			'highlight',
+			'fontBackgroundColor',
 			'|',
 			'alignment',
-			'outdent',
-			'indent',
 			'bulletedList',
 			'numberedList',
+			'outdent',
+			'indent',
 			'|',
-			'link',
-			'imageInsert',
+			'imageUpload',
+			'blockQuote',
 			'code',
 			'codeBlock',
-			'blockQuote',
 			'insertTable',
 			'mediaEmbed',
-			'specialCharacters',
-			'subscript',
+			'imageInsert',
 			'superscript',
+			'subscript',
+			'specialCharacters',
 			'horizontalLine',
 			'|',
-			'undo',
+			'removeFormat',
 			'redo',
-			'sourceEditing'
+			'undo'
 		]
 	},
 	language: 'en',
-	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
+	image: {
+		toolbar: [
+			'imageTextAlternative',
+			'imageStyle:inline',
+			'imageStyle:block',
+			'imageStyle:side',
+			'linkImage'
 		]
 	}
 };
