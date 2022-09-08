b519695ccc92f251c86d93295f9db1e329591cf5
Samuel Hassine <samuel.hassine@filigran.io>
2022-09-08 09:54:46 +0200

    [frontend] Enrich editor not ignore build

---
+++ opencti-platform/opencti-front/packages/ckeditor5-custom-build/build/translations/oc.js
@@ -0,0 +1,1 @@
+(function(o){const r=o["oc"]=o["oc"]||{};r.dictionary=Object.assign(r.dictionary||{},{"%0 of %1":"",Bold:"Gras",Cancel:"Anullar",Code:"",Italic:"Italica","Remove color":"",Save:"Enregistrar","Show more items":"",Subscript:"",Superscript:"",Underline:""});r.getPluralForm=function(o){return o>1}})(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
