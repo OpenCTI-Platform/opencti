1ae8f50076b6bb43aeb74066ce0db0d1cfb3f728
Samuel Hassine <samuel.hassine@filigran.io>
2022-09-21 16:38:23 +0200

    [frontend] Remove scrollbars in enriched editor tables (#2372)

--- opencti-platform/opencti-graphql/src/domain/work.js
+++ opencti-platform/opencti-graphql/src/domain/work.js
@@ -176,7 +176,7 @@ export const reportExpectation = async (user, workId, errorData) => {
       ctx._source['completed_number'] = params.completed_number;
       ctx._source['completed_time'] = params.now;`;
     }
-    // To avoid maximum string in Elastic (536870888), arbitrary limit the number of errors possible in a work to 5000 (50000 * 50000 < 536870888)
+    // To avoid maximum string in Elastic (536870888), arbitrary limit the number of possible errors in a work to 5000 (5000*50000 < 536870888)
     if (errorData) {
       const { error, source } = errorData;
       sourceScript += 'if (ctx._source.errors.length < 5000) { ctx._source.errors.add(["timestamp": params.now, "message": params.error, "source": params.source]); }';
