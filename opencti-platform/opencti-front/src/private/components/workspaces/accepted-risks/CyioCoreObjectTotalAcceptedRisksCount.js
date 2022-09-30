7cdedcbdc946046eef6f745d38951c83aeef7c8a
sachin <sachin755@gmail.com>
2022-09-30 17:18:37 +0530

    Fixed styling in card widget

--- opencti-platform/opencti-front/src/private/components/workspaces/accepted-risks/CyioCoreObjectTotalAcceptedRisksCount.js
+++ opencti-platform/opencti-front/src/private/components/workspaces/accepted-risks/CyioCoreObjectTotalAcceptedRisksCount.js
@@ -6,7 +6,9 @@ import { withStyles, withTheme } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import { Card, CardContent, ListItemIcon } from '@material-ui/core';
+import {
+  Card, CardContent, ListItemIcon, SvgIcon,
+} from '@material-ui/core';
 import { QueryRenderer } from '../../../../relay/environment';
 import inject18n from '../../../../components/i18n';
 import { monthsAgo, now, numberOfDays } from '../../../../utils/Time';
@@ -27,24 +29,28 @@ const styles = (theme) => ({
     width: '100%',
     height: 'inherit',
     borderRadius: 6,
+    padding: '0 0 1rem 0',
   },
   number: {
-    marginTop: 10,
+    marginTop: '1rem',
     float: 'left',
-    fontSize: 30,
+    fontSize: '1.5rem',
+    height: 'inherit',
   },
   title: {
     marginTop: 5,
     textTransform: 'uppercase',
-    fontSize: 12,
+    fontSize: '0.8rem',
     fontWeight: 500,
     color: theme.palette.text.secondary,
+    height: 'inherit',
   },
   icon: {
     color: theme.palette.primary.main,
     width: 'auto',
     display: 'flex',
     justifyContent: 'flex-end',
+    height: 'inherit',
   },
 });
 class CyioCoreObjectTotalAcceptedRisksCount extends Component {
@@ -67,7 +73,8 @@ class CyioCoreObjectTotalAcceptedRisksCount extends Component {
             />
             <div className={classes.icon}>
               <ListItemIcon style={{ minWidth: 35 }}>
-                <svg
+              <SvgIcon style={{ fontSize: '2rem' }}>
+              <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
                   height="24"
@@ -78,6 +85,7 @@ class CyioCoreObjectTotalAcceptedRisksCount extends Component {
                     d="M18.905 14c-2.029 2.401-4.862 5.005-7.905 8-5.893-5.8-11-10.134-11-14.371 0-6.154 8.114-7.587 11-2.676 2.865-4.875 11-3.499 11 2.676 0 .784-.175 1.572-.497 2.371h-6.278c-.253 0-.486.137-.61.358l-.813 1.45-2.27-4.437c-.112-.219-.331-.364-.576-.38-.246-.016-.482.097-.622.299l-1.88 2.71h-1.227c-.346-.598-.992-1-1.732-1-1.103 0-2 .896-2 2s.897 2 2 2c.74 0 1.386-.402 1.732-1h1.956c.228 0 .441-.111.573-.297l.989-1.406 2.256 4.559c.114.229.343.379.598.389.256.011.496-.118.629-.337l1.759-2.908h8.013v2h-5.095z"
                   />
                 </svg>
+              </SvgIcon>
               </ListItemIcon>
             </div>
           </CardContent>
