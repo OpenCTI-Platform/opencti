fc2f6a1ae8d3836a92a40940a3c0226149f79372
aman <aman82591@gmail.com>
2022-10-03 13:29:06 +0530

    Added styling in inventory item charts.

--- opencti-platform/opencti-front/src/private/components/workspaces/inventory-items/CyioCoreObjectTotalInventoryItemsCount.js
+++ opencti-platform/opencti-front/src/private/components/workspaces/inventory-items/CyioCoreObjectTotalInventoryItemsCount.js
@@ -65,17 +65,13 @@ class CyioCoreObjectTotalInventoryItemsCount extends Component {
           <CardContent>
             <div className={classes.title}>{t('Total Inventory Items')}</div>
             <div className={classes.content}>
-            <div className={classes.number}>{n('10')}</div>
-            <ItemNumberDifference
-            difference={500}
-            description={t('24 hours')}
-            />
+              <div className={classes.number}>{n('10')}</div>
             </div>
             <div className={classes.icon}>
               <ListItemIcon style={{ minWidth: 35 }}>
-              <SvgIcon style={{ fontSize: '2rem' }}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#ffffff" d="M12 4.942c1.827 1.105 3.474 1.6 5 1.833v7.76c0 1.606-.415 1.935-5 4.76v-14.353zm9-1.942v11.535c0 4.603-3.203 5.804-9 9.465-5.797-3.661-9-4.862-9-9.465v-11.535c3.516 0 5.629-.134 9-3 3.371 2.866 5.484 3 9 3zm-2 1.96c-2.446-.124-4.5-.611-7-2.416-2.5 1.805-4.554 2.292-7 2.416v9.575c0 3.042 1.69 3.83 7 7.107 5.313-3.281 7-4.065 7-7.107v-9.575z" /></svg>
-              </SvgIcon>
+                <SvgIcon style={{ fontSize: '2rem' }}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#ffffff" d="M12 4.942c1.827 1.105 3.474 1.6 5 1.833v7.76c0 1.606-.415 1.935-5 4.76v-14.353zm9-1.942v11.535c0 4.603-3.203 5.804-9 9.465-5.797-3.661-9-4.862-9-9.465v-11.535c3.516 0 5.629-.134 9-3 3.371 2.866 5.484 3 9 3zm-2 1.96c-2.446-.124-4.5-.611-7-2.416-2.5 1.805-4.554 2.292-7 2.416v9.575c0 3.042 1.69 3.83 7 7.107 5.313-3.281 7-4.065 7-7.107v-9.575z" /></svg>
+                </SvgIcon>
               </ListItemIcon>
             </div>
           </CardContent>
