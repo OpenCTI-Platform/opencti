c3bfd1c695114d135e6f1c1d508532b6dd58376c
John Justice <johnjustice@Darklight.local>
2022-10-06 15:26:00 -0400

    AB#5854 Changed references to User Component to Uses Component

--- opencti-platform/opencti-front/src/private/components/dataEntities/data/assessment_platform/AssessmentPlatformEntityEditionContainer.js
+++ opencti-platform/opencti-front/src/private/components/dataEntities/data/assessment_platform/AssessmentPlatformEntityEditionContainer.js
@@ -340,10 +340,10 @@ class AssessmentPlatformEntityEditionContainer extends Component {
                         gutterBottom={true}
                         style={{ float: 'left' }}
                       >
-                        {t('User Component(s)')}
+                        {t('Uses Component(s)')}
                       </Typography>
                       <div style={{ float: 'left', margin: '1px 0 0 5px' }}>
-                        <Tooltip title={t('User Component(s)')} >
+                        <Tooltip title={t('Uses Component(s)')} >
                           <Information fontSize="inherit" color="disabled" />
                         </Tooltip>
                       </div>
@@ -360,7 +360,7 @@ class AssessmentPlatformEntityEditionContainer extends Component {
                         <Field
                           component={SelectField}
                           variant='outlined'
-                          name="user_component"
+                          name="uses_component"
                           fullWidth={true}
                           style={{ height: '38.09px' }}
                           containerstyle={{ width: '100%' }}
