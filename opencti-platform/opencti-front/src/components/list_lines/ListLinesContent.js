d14b4ce30b0ac9a7b0f5e1ced6850295e4e06eb5
aman <aman82591@gmail.com>
2022-10-08 00:22:48 +0530

    Fixed the scrolling issue in Risk/Asset.

--- opencti-platform/opencti-front/src/components/list_lines/ListLinesContent.js
+++ opencti-platform/opencti-front/src/components/list_lines/ListLinesContent.js
@@ -68,7 +68,7 @@ class ListLinesContent extends Component {
       });
     }
     if (window.pageYOffset < 40 && this.state.newDataList.length > 50
-      && this.props.offset > 0) {
+      && this.props.offset >= 0) {
       window.scrollTo(0, 2500);
       this.props.handleDecrementedOffsetChange();
       this.setState({ newDataList: this.state.newDataList.slice(-this.props.dataList.length) });
