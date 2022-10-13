6a65093534474cb3062ed02da98951f6b8b1e040
Julien Richard <julien.richard@filigran.io>
2022-10-13 09:49:54 +0200

    [tool] Change relay config to use json

--- opencti-platform/opencti-front/relay.config.js
+++
@@ -1,7 +0,0 @@
-// relay.config.js
-module.exports = {
-  src: './src',
-  schema: './src/schema/relay.schema.graphql',
-  language: "typescript",
-  excludes: ['/node_modules/', '/__mocks__/', '/__generated__/'],
-};
