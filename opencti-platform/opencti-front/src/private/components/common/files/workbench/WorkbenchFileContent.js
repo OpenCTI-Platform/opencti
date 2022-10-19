9f32aeef1a13cd199e2b23aed9abbd24acdcba23
Samuel Hassine <samuel.hassine@filigran.io>
2022-10-19 23:16:50 +0200

    [frontend] Be able to modify relationships

--- opencti-platform/opencti-front/src/private/components/common/files/workbench/WorkbenchFileContent.js
+++ opencti-platform/opencti-front/src/private/components/common/files/workbench/WorkbenchFileContent.js
@@ -285,6 +285,7 @@ class WorkbenchFileContentComponent extends Component {
       displayObservable: false,
       observableType: null,
       observableId: null,
+      relationshipId: null,
       containerStep: null,
       containerType: null,
       containerId: null,
@@ -1457,6 +1458,256 @@ class WorkbenchFileContentComponent extends Component {
 
   onResetEntity() {
     this.handleCloseEntity();
+  }
+  // endregion
+
+  // region relationship
+  handleOpenRelationship(relationshipId) {
+    this.setState({
+      relationshipId,
+    });
+  }
+
+  handleCloseRelationship() {
+    this.setState({
+      relationshipId: null,
+    });
+  }
+
+  renderRelationshipForm() {
+    const { relationshipId, stixCoreRelationships } = this.state;
+    const { classes, t } = this.props;
+    const relationship = R.head(stixCoreRelationships.filter((n) => n.id === relationshipId))
+      || {};
+    return (
+      <QueryRenderer
+        query={workbenchFileContentAttributesQuery}
+        variables={{ elementType: 'stix-core-relationship' }}
+        render={({ props }) => {
+          if (props && props.schemaAttributes) {
+            const initialValues = {
+              createdBy: this.convertCreatedByRef(relationship),
+              objectMarking: this.convertMarkings(relationship),
+              objectLabel: this.convertLabels(relationship),
+              externalReferences: this.convertExternalReferences(relationship),
+            };
+            const attributes = R.filter(
+              (n) => R.includes(
+                n,
+                R.map((o) => o.node.value, props.schemaAttributes.edges),
+              ),
+              workbenchAttributes,
+            );
+            for (const attribute of attributes) {
+              if (R.includes(attribute, dateAttributes)) {
+                initialValues[attribute] = relationship[attribute]
+                  ? buildDate(relationship[attribute])
+                  : now();
+              } else if (R.includes(attribute, booleanAttributes)) {
+                initialValues[attribute] = relationship[attribute] || false;
+              } else {
+                initialValues[attribute] = relationship[attribute] || '';
+              }
+            }
+            return (
+              <Formik
+                initialValues={initialValues}
+                onSubmit={this.onSubmitRelationship.bind(this)}
+                onReset={this.onResetRelationship.bind(this)}
+              >
+                {({
+                  submitForm,
+                  handleReset,
+                  isSubmitting,
+                  setFieldValue,
+                  values,
+                }) => (
+                  <Form
+                    style={{ margin: '0 0 20px 0', padding: '0 15px 0 15px' }}
+                  >
+                    <div>
+                      {attributes.map((attribute) => {
+                        if (R.includes(attribute, dateAttributes)) {
+                          return (
+                            <Field
+                              component={DateTimePickerField}
+                              key={attribute}
+                              name={attribute}
+                              withSeconds={true}
+                              TextFieldProps={{
+                                label: attribute,
+                                variant: 'standard',
+                                fullWidth: true,
+                                style: { marginTop: 20 },
+                              }}
+                            />
+                          );
+                        }
+                        if (R.includes(attribute, numberAttributes)) {
+                          return (
+                            <Field
+                              component={TextField}
+                              variant="standard"
+                              key={attribute}
+                              name={attribute}
+                              label={attribute}
+                              fullWidth={true}
+                              type="number"
+                              style={{ marginTop: 20 }}
+                            />
+                          );
+                        }
+                        if (R.includes(attribute, booleanAttributes)) {
+                          return (
+                            <Field
+                              component={SwitchField}
+                              type="checkbox"
+                              key={attribute}
+                              name={attribute}
+                              label={attribute}
+                              containerstyle={{ marginTop: 20 }}
+                            />
+                          );
+                        }
+                        if (R.includes(attribute, markdownAttributes)) {
+                          return (
+                            <Field
+                              component={MarkDownField}
+                              key={attribute}
+                              name={attribute}
+                              label={attribute}
+                              fullWidth={true}
+                              multiline={true}
+                              rows="4"
+                              style={{ marginTop: 20 }}
+                            />
+                          );
+                        }
+                        return (
+                          <Field
+                            component={TextField}
+                            variant="standard"
+                            key={attribute}
+                            name={attribute}
+                            label={attribute}
+                            fullWidth={true}
+                            style={{ marginTop: 20 }}
+                          />
+                        );
+                      })}
+                    </div>
+                    <CreatedByField
+                      name="createdBy"
+                      style={{ marginTop: 20, width: '100%' }}
+                      setFieldValue={setFieldValue}
+                    />
+                    <ObjectLabelField
+                      name="objectLabel"
+                      style={{ marginTop: 20, width: '100%' }}
+                      setFieldValue={setFieldValue}
+                      values={values.objectLabel}
+                    />
+                    <ObjectMarkingField
+                      name="objectMarking"
+                      style={{ marginTop: 20, width: '100%' }}
+                    />
+                    <ExternalReferencesField
+                      name="externalReferences"
+                      style={{ marginTop: 20, width: '100%' }}
+                      setFieldValue={setFieldValue}
+                      values={values.externalReferences}
+                    />
+                    <div className={classes.buttons}>
+                      <Button
+                        variant="contained"
+                        onClick={handleReset}
+                        disabled={isSubmitting}
+                        classes={{ root: classes.button }}
+                      >
+                        {t('Cancel')}
+                      </Button>
+                      <Button
+                        variant="contained"
+                        color="secondary"
+                        onClick={submitForm}
+                        disabled={isSubmitting}
+                        classes={{ root: classes.button }}
+                      >
+                        {t('Update')}
+                      </Button>
+                    </div>
+                  </Form>
+                )}
+              </Formik>
+            );
+          }
+          return <div />;
+        }}
+      />
+    );
+  }
+
+  onSubmitRelationship(values) {
+    const { relationshipId, stixCoreRelationships, stixDomainObjects } = this.state;
+    const relationship = R.head(stixCoreRelationships.filter((n) => n.id === relationshipId))
+      || {};
+    const markingDefinitions = R.pluck('entity', values.objectMarking).map(
+      (n) => ({
+        ...n,
+        id: n.standard_id || n.id,
+        type: 'marking-definition',
+      }),
+    );
+    const identity = values.createdBy?.entity
+      ? {
+        ...values.createdBy.entity,
+        id: values.createdBy.entity.standard_id || values.createdBy.entity.id,
+        type: 'identity',
+      }
+      : null;
+    const finalValues = {
+      ...R.omit(
+        ['objectLabel', 'objectMarking', 'createdBy', 'externalReferences'],
+        values,
+      ),
+      labels: R.pluck('label', values.objectLabel),
+      object_marking_refs: R.pluck('entity', values.objectMarking).map(
+        (n) => n.standard_id || n.id,
+      ),
+      created_by_ref:
+        values.createdBy?.entity?.standard_id || values.createdBy?.entity?.id,
+      external_references: R.pluck('entity', values.externalReferences),
+    };
+    const updatedRelationship = {
+      ...relationship,
+      ...finalValues,
+      id: relationship.id ? relationship.id : `relationship--${uuid()}`,
+    };
+    this.setState({
+      stixCoreRelationships: uniqWithByFields(
+        ['source_ref', 'target_ref', 'relationship_type'],
+        R.uniqBy(R.prop('id'), [
+          ...stixCoreRelationships.filter(
+            (n) => n.id !== updatedRelationship.id,
+          ),
+          updatedRelationship,
+        ]),
+      ),
+      stixDomainObjects: uniqWithByFields(
+        ['name', 'type'],
+        R.uniqBy(R.prop('id'), [
+          ...stixDomainObjects,
+          ...markingDefinitions,
+          ...(identity ? [identity] : []),
+        ]),
+      ),
+    });
+    this.handleCloseRelationship();
+    this.saveFile();
+  }
+
+  onResetRelationship() {
+    this.handleCloseRelationship();
   }
   // endregion
 
@@ -2971,6 +3222,7 @@ class WorkbenchFileContentComponent extends Component {
       selectAll,
       deSelectedElements,
       selectedElements,
+      relationshipId,
     } = this.state;
     const indexedStixObjects = {
       ...R.indexBy(R.prop('id'), stixDomainObjects),
@@ -3038,7 +3290,7 @@ class WorkbenchFileContentComponent extends Component {
                 classes={{ root: classes.item }}
                 divider={true}
                 button={true}
-                onClick={null}
+                onClick={this.handleOpenRelationship.bind(this, object.id)}
               >
                 <ListItemIcon
                   classes={{ root: classes.itemIcon }}
@@ -3117,6 +3369,30 @@ class WorkbenchFileContentComponent extends Component {
             );
           })}
         </List>
+        <Drawer
+          open={relationshipId !== null}
+          anchor="right"
+          sx={{ zIndex: 1202 }}
+          elevation={1}
+          classes={{ paper: classes.drawerPaper }}
+          onClose={this.handleCloseRelationship.bind(this)}
+        >
+          <div className={classes.header}>
+            <IconButton
+              aria-label="Close"
+              className={classes.closeButton}
+              onClick={this.handleCloseRelationship.bind(this)}
+              size="large"
+              color="primary"
+            >
+              <Close fontSize="small" color="primary" />
+            </IconButton>
+            <Typography variant="h6">{t('Manage a relationship')}</Typography>
+          </div>
+          <div className={classes.container}>
+            {relationshipId && this.renderRelationshipForm()}
+          </div>
+        </Drawer>
       </div>
     );
   }
@@ -3192,15 +3468,11 @@ class WorkbenchFileContentComponent extends Component {
                 classes={{ root: classes.item }}
                 divider={true}
                 button={object.type !== 'marking-definition'}
-                onClick={
-                  object.type === 'marking-definition'
-                    ? null
-                    : this.handleOpenContainer.bind(
-                      this,
-                      object.type,
-                      object.id,
-                    )
-                }
+                onClick={this.handleOpenContainer.bind(
+                  this,
+                  object.type,
+                  object.id,
+                )}
               >
                 <ListItemIcon
                   classes={{ root: classes.itemIcon }}
