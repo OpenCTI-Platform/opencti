55f837ba524056b3e3b70bb94392067378256862
Paul Patrick <Paul.Patrick.Sr@gmail.com>
2022-10-20 21:21:11 -0400

    AB#5812 - implementation of assetsTimeSeries and risksTimeSeries GraphQL APIs

--- opencti-platform/opencti-graphql/src/cyio/schema/global/resolvers/dashboard-sparqlQuery.js
+++ opencti-platform/opencti-graphql/src/cyio/schema/global/resolvers/dashboard-sparqlQuery.js
@@ -87,8 +87,8 @@ export const entitiesCountQuery = (args) => {
     endDate = new Date().toISOString();
   }
   filterClause = `
-    ?iri <http://darklight.ai/ns/common#modified> ?modified .
-    FILTER (?modified > "${endDate}"^^xsd:dateTime)
+    ?iri <http://darklight.ai/ns/common#modified> ?created .
+    FILTER (?created > "${endDate}"^^xsd:dateTime)
     `;
 
   if (args.field !== 'entity_type') {
@@ -113,7 +113,7 @@ export const entitiesCountQuery = (args) => {
     ?iri a ${classIri} .
     OPTIONAL {
       {
-        SELECT (COUNT(?modified) AS ?count)
+        SELECT (COUNT(?created) AS ?count)
         WHERE {
           ?iri a ${classIri} .
           ${matchPredicates.join("\n")}
@@ -124,8 +124,102 @@ export const entitiesCountQuery = (args) => {
   } GROUP BY ?count
   `;
 }
+
+export const entitiesTimeSeriesQuery = (args) => {
+  let classIri, predicate, type, field, startDate, endDate;
+  const matchPredicates = [];
+  if ('type' in args) {
+    type = args.type.toLowerCase();
+    field = 'object_type';
+  } else if (args.field === 'entity_type') {
+    field = 'object_type';
+    for (let match of args.match) {
+      match = match.toLowerCase();
+      type = match;
+      if (!objectMap.hasOwnProperty(match)) {
+        let found = false;
+        for (let [key, value] of Object.entries(objectMap)) {
+          // check for alternate key
+          if (value.alternateKey != undefined && match == value.alternateKey) {
+            type = key;
+            found = true;
+            break;
+          }
+          // check if the GraphQL type name was supplied
+          if (match == value.graphQLType) {
+            type = key;
+            found = true;
+            break;
+          }
+        }
+        if (!found) throw new CyioError(`Unknown field '${args.match}'`);
+      }
+    }
+  }
+  if (type === undefined) throw new CyioError (`Unable to determine object type`);
+
+  // Validate field is defined 
+  const predicateMap = objectMap[type].predicateMap;
+  if ('field' in args && args.field !== 'entity_type') {
+    field = args.field;
+    if (!predicateMap.hasOwnProperty(field)) throw new CyioError(`Field '${field}' is not defined for the entity.`);
+    predicate = predicateMap[args.field].predicate;
+  }
+
+  while (objectMap[type].parent !== undefined) {
+    type = objectMap[type].parent;
+  }
+
+  // construct the IRI
+  if ('objectId' in args) {
+    classIri = `<${objectMap[type].iriTemplate}-${args.objectId}>`;
+  } else {
+    classIri = `<${objectMap[type].iriTemplate}>`;
+  }
+
+  if (('startDate' in args) && (args.startDate instanceof Date)) {
+    // convert start date to string, if specified 
+    startDate = args.startDate.toISOString();
+  } else {
+    // use Epoch time
+    startDate = '1970-01-01-01T00:00:00Z';
+  }
+  if (('endDate' in args) && (args.endDate instanceof Date)) {
+    // convert end date to string, if specified 
+    endDate = args.endDate.toISOString();
+  } else{
+    // uses the current date and time
+    endDate = new Date().toISOString();
+  }
+
+  if (args.field !== 'entity_type') {
+    if ('match' in args && args.match.length > 0) {
+      let values = "";
+      for (let match of args.match) {
+        values = values + ` "${match}"`;
+      }
+      if (values.length > 0) {
+        values = values.trim();
+        matchPredicates.push(`  Values ?o {${values}} .`);
+        matchPredicates.push(`  ?iri ${predicate} ?o .`);    
+      }
+    }
+  }
   
-  // Predicate Map
+  return `
+  PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+  SELECT ?iri ?created
+  FROM <tag:stardog:api:context:local>
+  WHERE {
+    ?iri a ${classIri} .
+    ${matchPredicates.join("\n")}
+    ?iri <http://darklight.ai/ns/common#created> ?created .
+    FILTER (?created > "${startDate}"^^xsd:dateTime && ?created < "${endDate}"^^xsd:dateTime)
+  } GROUP BY ?iri ?created
+  `;
+}
+
+// Predicate Map
 export const entityPredicateMap = {
   id: {
     predicate: "<http://docs.oasis-open.org/ns/cti#id>",
@@ -149,6 +243,7 @@ export const entityPredicateMap = {
   },
 }
 
+// Singularization Schema
 export const dashboardSingularizeSchema = { singularizeVariables: {
     "": false, // so there is an object as the root instead of an array
     "id": true,
@@ -156,5 +251,8 @@ export const dashboardSingularizeSchema = { singularizeVariables: {
     "object_type": true,
     "total": true,
     "count": true,
+    "created": true,
+    "modified": true,
   }
 };
+
