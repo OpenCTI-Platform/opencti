f201a6af2a95ceadf6b5937b69cafa5beb1dbc22
Samuel Hassine <samuel.hassine@filigran.io>
2022-10-29 14:53:50 +0200

    [client] Update version number
    (original commit: OpenCTI-Platform/client-python@03ce552)

--- pycti/__init__.py
+++ pycti/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "5.3.17"
+__version__ = "5.4.0"
 
 from .api.opencti_api_client import OpenCTIApiClient
 from .api.opencti_api_connector import OpenCTIApiConnector
