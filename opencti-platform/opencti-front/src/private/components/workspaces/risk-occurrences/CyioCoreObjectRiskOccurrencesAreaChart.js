ac5d99c2d3f1facc0ca219450ac440ede2652854
aman <aman82591@gmail.com>
2022-10-31 12:14:15 +0530

    Added query in Risk Occurrences chart.

--- opencti-platform/opencti-front/src/private/components/workspaces/risk-occurrences/CyioCoreObjectRiskOccurrencesAreaChart.js
+++ opencti-platform/opencti-front/src/private/components/workspaces/risk-occurrences/CyioCoreObjectRiskOccurrencesAreaChart.js
@@ -24,7 +24,7 @@ const styles = () => ({
     minHeight: 280,
     height: '100%',
     margin: '4px 0 0 0',
-    padding: '1rem',
+    padding: '0 0 10px 0',
     borderRadius: 6,
   },
   chip: {
@@ -34,107 +34,31 @@ const styles = () => ({
   },
 });
 
-// const cyioCoreObjectVulnerabilitiesAreaChartQuery = graphql`
-//   query CyioCoreObjectVulnerabilitiesAreaChartQuery(
-//     $objectId: String
-//     $authorId: String
-//     $reportClass: String
-//     $field: String!
-//     $operation: StatsOperation!
-//     $startDate: DateTime!
-//     $endDate: DateTime!
-//     $interval: String!
-//   ) {
-//     reportsTimeSeries(
-//       objectId: $objectId
-//       authorId: $authorId
-//       reportType: $reportClass
-//       field: $field
-//       operation: $operation
-//       startDate: $startDate
-//       endDate: $endDate
-//       interval: $interval
-//     ) {
-//       date
-//       value
-//     }
-//   }
-// `;
-
-const data = [
-  {
-    name: 'Jan A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Jan B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Jan C',
-    uv: 2000,
-    pv: 3800,
-    amt: 2290,
-  },
-  {
-    name: 'Jan D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Jan E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Jan F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Jan G',
-    uv: 3770,
-    pv: 4300,
-    amt: 2100,
-  },
-  {
-    name: 'Jan H',
-    uv: 3490,
-    pv: 3300,
-    amt: 2200,
-  },
-  {
-    name: 'Jan I',
-    uv: 3490,
-    pv: 3500,
-    amt: 2150,
-  },
-  {
-    name: 'Jan J',
-    uv: 4790,
-    pv: 5300,
-    amt: 2850,
-  },
-  {
-    name: 'Jan K',
-    uv: 3790,
-    pv: 4600,
-    amt: 2960,
-  },
-  {
-    name: 'Jan L',
-    uv: 3490,
-    pv: 4700,
-    amt: 2770,
-  },
-];
+const cyioCoreObjectRiskOccurrencesAreaChartQuery = graphql`
+  query CyioCoreObjectRiskOccurrencesAreaChartQuery(
+    $type: String
+    $field: String!
+    $match: [String]
+    $operation: StatsOperation!
+    $startDate: DateTime!
+    $endDate: DateTime!
+    $interval: Interval!
+  ) {
+    risksTimeSeries(
+      type: $type
+      match: $match
+      field: $field
+      operation: $operation
+      startDate: $startDate
+      endDate: $endDate
+      interval: $interval
+    ) {
+      date
+      label
+      value
+    }
+  }
+`;
 
 class CyioCoreObjectRiskOccurrencesAreaChart extends Component {
   renderContent() {
@@ -142,150 +66,115 @@ class CyioCoreObjectRiskOccurrencesAreaChart extends Component {
       t,
       md,
       nsd,
-      reportType,
       startDate,
       endDate,
-      stixCoreObjectId,
-      authorId,
       theme,
     } = this.props;
-    const interval = 'day';
+    const interval = 'month';
     const finalStartDate = startDate || monthsAgo(12);
     const finalEndDate = endDate || now();
     const days = numberOfDays(finalStartDate, finalEndDate);
-    let tickFormatter = md;
+    // let tickFormatter = md;
     if (days <= 30) {
       tickFormatter = nsd;
     }
-    let reportsTimeSeriesVariables;
-    if (authorId) {
-      reportsTimeSeriesVariables = {
-        authorId,
-        objectId: null,
-        reportType: reportType || null,
-        field: 'created_at',
-        operation: 'count',
-        startDate: finalStartDate,
-        endDate: finalEndDate,
-        interval,
-      };
-    } else {
-      reportsTimeSeriesVariables = {
-        authorId: null,
-        objectId: stixCoreObjectId,
-        reportType: reportType || null,
-        field: 'created_at',
-        operation: 'count',
-        startDate: finalStartDate,
-        endDate: finalEndDate,
-        interval,
-      };
-    }
+    const riskTimeSeriesVariables = {
+      type: 'Risk',
+      field: 'occurrences',
+      match: ['open', 'investigating', 'remediating', 'deviation_requested', 'deviation_approved'],
+      operation: 'count',
+      startDate: finalStartDate,
+      endDate: finalEndDate,
+      interval,
+    };
     return (
-      // <QueryRenderer
-      //   query={cyioCoreObjectVulnerabilitiesAreaChartQuery}
-      //   variables={reportsTimeSeriesVariables}
-      //   render={({ props }) => {
-      //     if (props && props.reportsTimeSeries) {
-      //       return (
-      <ResponsiveContainer height="100%" width="100%">
-        <AreaChart
-          // data={props.reportsTimeSeries}
-          data={data}
-          margin={{
-            top: 20,
-            right: 0,
-            bottom: 20,
-            left: -10,
-          }}
-        >
-          <CartesianGrid
-            strokeDasharray="2 2"
-            // stroke={theme.palette.primary.main}
-            stroke='rgba(241, 241, 242, 0.35)'
-            vertical={false}
-          />
-          <XAxis
-            dataKey="name"
-            stroke={theme.palette.text.primary}
-            // interval={interval}
-            textAnchor="end"
-          // angle={-30}
-          // tickFormatter={tickFormatter}
-          />
-          <YAxis stroke={theme.palette.text.primary} />
-          <Tooltip
-            cursor={{
-              fill: 'rgba(0, 0, 0, 0.2)',
-              stroke: 'rgba(0, 0, 0, 0.2)',
-              strokeWidth: 2,
-            }}
-            contentStyle={{
-              backgroundColor: '#1F2842',
-              fontSize: 12,
-              border: '1px solid #06102D',
-              borderRadius: 10,
-            }}
-          // labelFormatter={tickFormatter}
-          />
-          <Area
-            dataKey="amt"
-            stroke={theme.palette.primary.main}
-            strokeWidth={2}
-            // fill={theme.palette.primary.main}
-            fill='#075AD3'
-            fillOpacity={0.4}
-          />
-          <Area
-            dataKey="uv"
-            stroke={theme.palette.primary.main}
-            strokeWidth={2}
-            // fill={theme.palette.primary.main}
-            fill='#49B8FC'
-            fillOpacity={0.3}
-          />
-          <Area
-            dataKey="pv"
-            stroke={theme.palette.primary.main}
-            strokeWidth={2}
-            // fill={theme.palette.primary.main}
-            fill='#49B8FC'
-            fillOpacity={0.4}
-          />
-        </AreaChart>
-      </ResponsiveContainer>
-      //       );
-      //     }
-      //     if (props) {
-      //       return (
-      //         <div style={{ display: 'table', height: '100%', width: '100%' }}>
-      //           <span
-      //             style={{
-      //               display: 'table-cell',
-      //               verticalAlign: 'middle',
-      //               textAlign: 'center',
-      //             }}
-      //           >
-      //             {t('No entities of this type has been found.')}
-      //           </span>
-      //         </div>
-      //       );
-      //     }
-      //     return (
-      //       <div style={{ display: 'table', height: '100%', width: '100%' }}>
-      //         <span
-      //           style={{
-      //             display: 'table-cell',
-      //             verticalAlign: 'middle',
-      //             textAlign: 'center',
-      //           }}
-      //         >
-      //           <CircularProgress size={40} thickness={2} />
-      //         </span>
-      //       </div>
-      //     );
-      //   }}
-      // />
+      <QueryRenderer
+        query={cyioCoreObjectRiskOccurrencesAreaChartQuery}
+        variables={riskTimeSeriesVariables}
+        render={({ props }) => {
+          if (props && props.risksTimeSeries) {
+            return (
+              <ResponsiveContainer height="100%" width="100%">
+                <AreaChart
+                  data={props.risksTimeSeries}
+                  margin={{
+                    top: 20,
+                    right: 0,
+                    bottom: 20,
+                    left: -10,
+                  }}
+                >
+                  <CartesianGrid
+                    strokeDasharray="2 2"
+                    // stroke={theme.palette.primary.main}
+                    stroke='rgba(241, 241, 242, 0.35)'
+                    vertical={false}
+                  />
+                  <XAxis
+                    dataKey="label"
+                    stroke={theme.palette.text.primary}
+                    interval={interval}
+                    textAnchor="end"
+                  // angle={-30}
+                  // tickFormatter={tickFormatter}
+                  />
+                  <YAxis stroke={theme.palette.text.primary} />
+                  <Tooltip
+                    cursor={{
+                      fill: 'rgba(0, 0, 0, 0.2)',
+                      stroke: 'rgba(0, 0, 0, 0.2)',
+                      strokeWidth: 2,
+                    }}
+                    contentStyle={{
+                      backgroundColor: '#1F2842',
+                      fontSize: 12,
+                      border: '1px solid #06102D',
+                      borderRadius: 10,
+                    }}
+                  // labelFormatter={tickFormatter}
+                  />
+                  <Area
+                    dataKey='value'
+                    stroke={theme.palette.primary.main}
+                    strokeWidth={2}
+                    // fill={theme.palette.primary.main}
+                    fill='#49B8FC'
+                    fillOpacity={0.3}
+                  />
+                </AreaChart>
+              </ResponsiveContainer>
+            );
+          }
+          if (props) {
+            return (
+              <div style={{ display: 'table', height: '100%', width: '100%' }}>
+                <span
+                  style={{
+                    display: 'table-cell',
+                    verticalAlign: 'middle',
+                    textAlign: 'center',
+                  }}
+                >
+                  {t('No entities of this type has been found.')}
+                </span>
+              </div>
+            );
+          }
+          return (
+            <div style={{ display: 'table', height: '100%', width: '100%' }}>
+              <span
+                style={{
+                  display: 'table-cell',
+                  verticalAlign: 'middle',
+                  textAlign: 'center',
+                }}
+              >
+                <CircularProgress size={40} thickness={2} />
+              </span>
+            </div>
+          );
+        }}
+      />
     );
   }
 
@@ -296,7 +185,7 @@ class CyioCoreObjectRiskOccurrencesAreaChart extends Component {
     return (
       <div style={{ height: height || '100%' }}>
         <Typography variant="h4" gutterBottom={true}>
-          {title || t('Risk Activities')}
+          {title || t('Total Active Risks')}
         </Typography>
         {variant !== 'inLine' ? (
           <Paper classes={{ root: classes.paper }} elevation={2}>
@@ -313,8 +202,6 @@ class CyioCoreObjectRiskOccurrencesAreaChart extends Component {
 CyioCoreObjectRiskOccurrencesAreaChart.propTypes = {
   classes: PropTypes.object,
   theme: PropTypes.object,
-  stixCoreObjectId: PropTypes.string,
-  authorId: PropTypes.string,
   t: PropTypes.func,
   md: PropTypes.func,
   nsd: PropTypes.func,
--- opencti-platform/opencti-front/src/private/components/workspaces/risk-occurrences/CyioCoreObjectRiskOccurrencesVerticalBars.js
+++ opencti-platform/opencti-front/src/private/components/workspaces/risk-occurrences/CyioCoreObjectRiskOccurrencesVerticalBars.js
@@ -34,237 +34,146 @@ const styles = () => ({
   },
 });
 
-// const stixCoreObjectReporstVerticalBarsTimeSeriesQuery = graphql`
-//   query StixCoreObjectReportsVerticalBarsTimeSeriesQuery(
-//     $objectId: String
-//     $authorId: String
-//     $reportClass: String
-//     $field: String!
-//     $operation: StatsOperation!
-//     $startDate: DateTime!
-//     $endDate: DateTime!
-//     $interval: String!
-//   ) {
-//     reportsTimeSeries(
-//       objectId: $objectId
-//       authorId: $authorId
-//       reportType: $reportClass
-//       field: $field
-//       operation: $operation
-//       startDate: $startDate
-//       endDate: $endDate
-//       interval: $interval
-//     ) {
-//       date
-//       value
-//     }
-//   }
-// `;
-
-const data = [
-  {
-    name: 'Jan A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Jan B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Jan C',
-    uv: 2000,
-    pv: 3800,
-    amt: 2290,
-  },
-  {
-    name: 'Jan D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Jan E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Jan F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Jan G',
-    uv: 3770,
-    pv: 4300,
-    amt: 2100,
-  },
-  {
-    name: 'Jan H',
-    uv: 3490,
-    pv: 3300,
-    amt: 2200,
-  },
-  {
-    name: 'Jan I',
-    uv: 3490,
-    pv: 3500,
-    amt: 2150,
-  },
-  {
-    name: 'Jan J',
-    uv: 4790,
-    pv: 5300,
-    amt: 2850,
-  },
-  {
-    name: 'Jan K',
-    uv: 3790,
-    pv: 4600,
-    amt: 2960,
-  },
-  {
-    name: 'Jan L',
-    uv: 3490,
-    pv: 4700,
-    amt: 2770,
-  },
-];
+const cyioCoreObjectRiskOccurrencesVerticalBarsQuery = graphql`
+  query CyioCoreObjectRiskOccurrencesVerticalBarsQuery(
+    $type: String
+    $field: String!
+    $operation: StatsOperation!
+    $startDate: DateTime!
+    $endDate: DateTime!
+  ) {
+    risksDistribution(
+      type: $type
+      field: $field
+      operation: $operation
+      startDate: $startDate
+      endDate: $endDate
+    ) {
+      label
+      value
+      entity {
+        ... on Risk {
+          id
+          created
+          name
+          first_seen
+          last_seen
+          risk_level
+          occurrences
+          deadline
+        }
+      }
+    }
+  }
+`;
 
 class CyioCoreObjectRiskOccurrencesVerticalBars extends Component {
   renderContent() {
     const {
       t,
       md,
-      reportType,
       startDate,
       endDate,
-      stixCoreObjectId,
-      authorId,
       theme,
     } = this.props;
-    const interval = 'day';
     const finalStartDate = startDate || monthsAgo(12);
     const finalEndDate = endDate || now();
-    let reportsTimeSeriesVariables;
-    if (authorId) {
-      reportsTimeSeriesVariables = {
-        authorId,
-        objectId: null,
-        reportType: reportType || null,
-        field: 'created_at',
-        operation: 'count',
-        startDate: finalStartDate,
-        endDate: finalEndDate,
-        interval,
-      };
-    } else {
-      reportsTimeSeriesVariables = {
-        authorId: null,
-        objectId: stixCoreObjectId,
-        reportType: reportType || null,
-        field: 'created_at',
-        operation: 'count',
-        startDate: finalStartDate,
-        endDate: finalEndDate,
-        interval,
-      };
-    }
+    const riskDistributionVariables = {
+      type: 'Risk',
+      field: 'occurrences',
+      operation: 'count',
+      startDate: finalStartDate,
+      endDate: finalEndDate,
+    };
+
     return (
-      // <QueryRenderer
-      //   query={stixCoreObjectReporstVerticalBarsTimeSeriesQuery}
-      //   variables={reportsTimeSeriesVariables}
-      //   render={({ props }) => {
-      //     if (props && props.reportsTimeSeries) {
-      //       return (
-      <ResponsiveContainer height="100%" width="100%">
-        <BarChart
-          layout='vertical'
-          // data={props.reportsTimeSeries}
-          data={data}
-          margin={{
-            top: 20,
-            right: 20,
-            bottom: 0,
-            left: 0,
-          }}
-          barGap={0}
-        >
-          <CartesianGrid
-            strokeDasharray="2 2"
-            stroke='rgba(241, 241, 242, 0.35)'
-            // stroke={theme.palette.action.grid}
-            vertical={false}
-          />
-          <XAxis
-            // dataKey="name"
-            type='number'
-            stroke={theme.palette.text.primary}
-          // interval={interval}
-          // angle={-45}
-          // textAnchor="end"
-          // tickFormatter={md}
-          />
-          <YAxis type='category' dataKey='name' stroke={theme.palette.text.primary} />
-          <Tooltip
-            cursor={{
-              fill: 'rgba(0, 0, 0, 0.2)',
-              stroke: 'rgba(0, 0, 0, 0.2)',
-              strokeWidth: 2,
-            }}
-            contentStyle={{
-              backgroundColor: '#1F2842',
-              fontSize: 12,
-              border: '1px solid #06102D',
-              borderRadius: 10,
-            }}
-            labelFormatter={md}
-          />
-          <Bar
-            // fill={theme.palette.primary.main}
-            fill="#075AD3"
-            dataKey="amt"
-            barSize={20}
-          />
-        </BarChart>
-      </ResponsiveContainer>
-      //       );
-      //     }
-      //     if (props) {
-      //       return (
-      //         <div style={{ display: 'table', height: '100%', width: '100%' }}>
-      //           <span
-      //             style={{
-      //               display: 'table-cell',
-      //               verticalAlign: 'middle',
-      //               textAlign: 'center',
-      //             }}
-      //           >
-      //             {t('No entities of this type has been found.')}
-      //           </span>
-      //         </div>
-      //       );
-      //     }
-      //     return (
-      //       <div style={{ display: 'table', height: '100%', width: '100%' }}>
-      //         <span
-      //           style={{
-      //             display: 'table-cell',
-      //             verticalAlign: 'middle',
-      //             textAlign: 'center',
-      //           }}
-      //         >
-      //           <CircularProgress size={40} thickness={2} />
-      //         </span>
-      //       </div>
-      //     );
-      //   }}
-      // />
+      <QueryRenderer
+        query={cyioCoreObjectRiskOccurrencesVerticalBarsQuery}
+        variables={riskDistributionVariables}
+        render={({ props }) => {
+          if (props && props.risksDistribution) {
+            return (
+              <ResponsiveContainer height="100%" width="100%">
+                <BarChart
+                  layout='vertical'
+                  data={props.risksDistribution}
+                  margin={{
+                    top: 20,
+                    right: 20,
+                    bottom: 0,
+                    left: 0,
+                  }}
+                  barGap={0}
+                >
+                  <CartesianGrid
+                    strokeDasharray="2 2"
+                    stroke='rgba(241, 241, 242, 0.35)'
+                    // stroke={theme.palette.action.grid}
+                    vertical={false}
+                  />
+                  <XAxis
+                    dataKey='value'
+                    stroke={theme.palette.text.primary}
+                  // interval={interval}
+                  // angle={-45}
+                  // textAnchor="end"
+                  // tickFormatter={md}
+                  />
+                  <YAxis type='category' dataKey='label' stroke={theme.palette.text.primary} />
+                  <Tooltip
+                    cursor={{
+                      fill: 'rgba(0, 0, 0, 0.2)',
+                      stroke: 'rgba(0, 0, 0, 0.2)',
+                      strokeWidth: 2,
+                    }}
+                    contentStyle={{
+                      backgroundColor: '#1F2842',
+                      fontSize: 12,
+                      border: '1px solid #06102D',
+                      borderRadius: 10,
+                    }}
+                    labelFormatter={md}
+                  />
+                  <Bar
+                    // fill={theme.palette.primary.main}
+                    fill="#075AD3"
+                    dataKey="amt"
+                    barSize={20}
+                  />
+                </BarChart>
+              </ResponsiveContainer>
+            );
+          }
+          if (props) {
+            return (
+              <div style={{ display: 'table', height: '100%', width: '100%' }}>
+                <span
+                  style={{
+                    display: 'table-cell',
+                    verticalAlign: 'middle',
+                    textAlign: 'center',
+                  }}
+                >
+                  {t('No entities of this type has been found.')}
+                </span>
+              </div>
+            );
+          }
+          return (
+            <div style={{ display: 'table', height: '100%', width: '100%' }}>
+              <span
+                style={{
+                  display: 'table-cell',
+                  verticalAlign: 'middle',
+                  textAlign: 'center',
+                }}
+              >
+                <CircularProgress size={40} thickness={2} />
+              </span>
+            </div>
+          );
+        }}
+      />
     );
   }
 
@@ -275,7 +184,7 @@ class CyioCoreObjectRiskOccurrencesVerticalBars extends Component {
     return (
       <div style={{ height: height || '100%' }}>
         <Typography variant="h4" gutterBottom={true}>
-          {title || t('Risk Activities')}
+          {title || t('Top N Risks by Severity')}
         </Typography>
         {variant !== 'inLine' ? (
           <Paper classes={{ root: classes.paper }} elevation={2}>
@@ -292,8 +201,6 @@ class CyioCoreObjectRiskOccurrencesVerticalBars extends Component {
 CyioCoreObjectRiskOccurrencesVerticalBars.propTypes = {
   classes: PropTypes.object,
   theme: PropTypes.object,
-  stixCoreObjectId: PropTypes.string,
-  authorId: PropTypes.string,
   t: PropTypes.func,
   md: PropTypes.func,
 };
