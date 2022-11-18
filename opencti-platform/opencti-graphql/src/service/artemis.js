282a1a604e5a48e34814cab682f82bd68bb39c90
Dallas Phillips <dallasphillips24@gmail.com>
2022-11-18 08:56:16 -0800

    Adding using app:https_cert:key|crt values for artemis comm.

--- opencti-platform/opencti-graphql/src/service/artemis.js
+++ opencti-platform/opencti-graphql/src/service/artemis.js
@@ -1,10 +1,15 @@
-import conf from '../config/conf.js';
+import conf, {logApp} from '../config/conf.js';
 import fetch from 'node-fetch';
+import https from "node:https";
+import {readFileSync} from "fs";
 
 const host = conf.get('artemis:rest:hostname');
 const port = conf.get('artemis:rest:port');
 const username = conf.get('artemis:rest:username');
 const password = conf.get('artemis:rest:passcode');
+
+const key = conf.get('app:https_cert:key')
+const crt = conf.get('app:https_cert:crt')
 
 export const artemisAlive = async () => {
   let credentials = Buffer.from(username + ":" + password).toString('base64');
@@ -12,13 +17,27 @@ export const artemisAlive = async () => {
 
   try {
     let url = server_url + 'queues/cyio.tasks.export'
+    let httpsAgent = null
+
+    if (key || crt) {
+      httpsAgent = new https.Agent({
+        key: key ? readFileSync(key): null,
+        cert: crt ? readFileSync(crt) : null
+      })
+    }
+
     let topicResponse = await fetch(url, {
+      agent: httpsAgent,
       method: 'get',
       headers: {
         'Authorization': 'Basic ' + credentials
       },
     });
-    if (!topicResponse.ok) throw new HTTPResponseError(topicResponse);
+
+    if (!topicResponse.ok) {
+      logApp.error("[INIT] Failed to communicate with Artemis")
+      return false
+    }
 
     // get the msg-create URL from the header so that the post can be performed
     if (topicResponse.headers.get('msg-create') != null ) return true;
