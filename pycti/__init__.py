dfb499eefc0261133585b86f234155413eda8273
Sarah Bocognano <98532359+SarahBocognano@users.noreply.github.com>
2022-11-23 11:27:12 +0100

    [client] Release 5.4.1
    (original commit: OpenCTI-Platform/client-python@819d49b)

--- pycti/__init__.py
+++ pycti/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "5.4.0"
+__version__ = "5.4.1"
 
 from .api.opencti_api_client import OpenCTIApiClient
 from .api.opencti_api_connector import OpenCTIApiConnector
