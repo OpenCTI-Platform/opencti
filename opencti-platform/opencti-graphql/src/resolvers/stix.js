0b549da4e37258f8e5c537fc9851b4203ed43baf
Julien Richard <julien.richard@filigran.io>
2022-12-12 23:33:44 +0100

    [front] Add resolve for StixObject type

--- opencti-platform/opencti-graphql/src/resolvers/stix.js
+++ opencti-platform/opencti-graphql/src/resolvers/stix.js
@@ -13,6 +13,16 @@ const stixResolvers = {
       merge: ({ stixObjectsIds }) => stixObjectMerge(context, context.user, id, stixObjectsIds),
     }),
   },
+  StixObject: {
+    // eslint-disable-next-line
+    __resolveType(obj) {
+      if (obj.entity_type) {
+        return obj.entity_type.replace(/(?:^|-|_)(\w)/g, (matches, letter) => letter.toUpperCase());
+      }
+      /* istanbul ignore next */
+      return 'Unknown';
+    },
+  }
 };
 
 export default stixResolvers;
