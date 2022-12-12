f8ee0d3e5dca3d9923a4089cab309ec4e4e18bcc
Julien Richard <julien.richard@filigran.io>
2022-12-12 23:09:59 +0100

    [client] Move merge function from stixCore to stix

--- pycti/entities/opencti_stix_core_object.py
+++ pycti/entities/opencti_stix_core_object.py
@@ -660,55 +660,6 @@ class StixCoreObject:
                 result["data"]["stixCoreObjects"], with_pagination
             )
 
-    """
-        Update a Stix-Domain-Object object field
-
-        :param id: the Stix-Domain-Object id
-        :param key: the key of the field
-        :param value: the value of the field
-        :return The updated Stix-Domain-Object object
-    """
-
-    def merge(self, **kwargs):
-        id = kwargs.get("id", None)
-        stix_core_objects_ids = kwargs.get("object_ids", None)
-        if id is not None and stix_core_objects_ids is not None:
-            self.opencti.log(
-                "info",
-                "Merging Core object {"
-                + id
-                + "} with {"
-                + ",".join(stix_core_objects_ids)
-                + "}.",
-            )
-            query = """
-                    mutation StixCoreObjectEdit($id: ID!, $stixCoreObjectsIds: [String]!) {
-                        stixCoreObjectEdit(id: $id) {
-                            merge(stixCoreObjectsIds: $stixCoreObjectsIds) {
-                                id
-                                standard_id
-                                entity_type
-                            }
-                        }
-                    }
-                """
-            result = self.opencti.query(
-                query,
-                {
-                    "id": id,
-                    "stixCoreObjectsIds": stix_core_objects_ids,
-                },
-            )
-            return self.opencti.process_multiple_fields(
-                result["data"]["stixCoreObjectEdit"]["merge"]
-            )
-        else:
-            self.opencti.log(
-                "error",
-                "[opencti_stix_core_object] Missing parameters: id and object_ids",
-            )
-            return None
-
     def list_files(self, **kwargs):
         id = kwargs.get("id", None)
         self.opencti.log(
