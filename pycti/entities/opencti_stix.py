5165a53e479cd50273bb7f6e2772002a461c1aad
Julien Richard <julien.richard@filigran.io>
2022-12-12 23:09:59 +0100

    [client] Move merge function from stixCore to stix
    (original commit: OpenCTI-Platform/client-python@f8ee0d3)

--- pycti/entities/opencti_stix.py
+++ pycti/entities/opencti_stix.py
@@ -27,3 +27,52 @@ class Stix:
         else:
             self.opencti.log("error", "[opencti_stix] Missing parameters: id")
             return None
+
+    """
+            Merge a Stix-Object object field
+    
+            :param id: the Stix-Object id
+            :param key: the key of the field
+            :param value: the value of the field
+            :return The updated Stix-Object object
+        """
+
+    def merge(self, **kwargs):
+        id = kwargs.get("id", None)
+        stix_objects_ids = kwargs.get("object_ids", None)
+        if id is not None and stix_objects_ids is not None:
+            self.opencti.log(
+                "info",
+                "Merging Stix object {"
+                + id
+                + "} with {"
+                + ",".join(stix_objects_ids)
+                + "}.",
+            )
+            query = """
+                        mutation StixEdit($id: ID!, $stixObjectsIds: [String]!) {
+                            stixEdit(id: $id) {
+                                merge(stixObjectsIds: $stixObjectsIds) {
+                                    id
+                                    standard_id
+                                    entity_type
+                                }
+                            }
+                        }
+                    """
+            result = self.opencti.query(
+                query,
+                {
+                    "id": id,
+                    "stixObjectsIds": stix_objects_ids,
+                },
+            )
+            return self.opencti.process_multiple_fields(
+                result["data"]["stixEdit"]["merge"]
+            )
+        else:
+            self.opencti.log(
+                "error",
+                "[opencti_stix] Missing parameters: id and object_ids",
+            )
+            return None
