ca215dfe3ca6939dde4589c94a6801e55b6d984b
Samuel Hassine <samuel.hassine@filigran.io>
2022-12-14 01:29:14 +0100

    [client] Fix missing new SDOs queries
    (original commit: OpenCTI-Platform/client-python@987e5f5)

--- pycti/__init__.py
+++ pycti/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "5.4.1"
+__version__ = "5.5.0"
 
 from .api.opencti_api_client import OpenCTIApiClient
 from .api.opencti_api_connector import OpenCTIApiConnector
