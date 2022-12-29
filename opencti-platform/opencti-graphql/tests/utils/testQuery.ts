8a678cd5ac93812c73ff0b3465fca736da9c01a0
Samuel Hassine <samuel.hassine@filigran.io>
2022-12-29 17:28:29 +0100

    Fix tests

--- opencti-platform/opencti-graphql/tests/utils/testQuery.ts
+++ opencti-platform/opencti-graphql/tests/utils/testQuery.ts
@@ -15,7 +15,7 @@ export const SYNC_DIRECT_START_REMOTE_URI = conf.get('app:sync_direct_start_remo
 export const SYNC_RESTORE_START_REMOTE_URI = conf.get('app:sync_restore_start_remote_uri');
 export const SYNC_TEST_REMOTE_URI = `http://api-tests:${PORT}`;
 export const RAW_EVENTS_SIZE = 711;
-export const SYNC_LIVE_EVENTS_SIZE = 510;
+export const SYNC_LIVE_EVENTS_SIZE = 511;
 
 export const PYTHON_PATH = './src/python/testing';
 export const API_URI = `http://localhost:${conf.get('app:port')}`;
