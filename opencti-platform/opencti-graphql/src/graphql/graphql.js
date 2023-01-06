5c39f89c6b7f39ce4e0d07169329dd27289f4cab
Paul Patrick <Paul.Patrick.Sr@gmail.com>
2023-01-06 18:27:04 -0500

    add support for DataSources, DataMarkings, ConnectionInformation,WorkActivity, and DynamoDB as a data source in GraphQL

--- opencti-platform/opencti-graphql/src/graphql/graphql.js
+++ opencti-platform/opencti-graphql/src/graphql/graphql.js
@@ -14,6 +14,7 @@ import {getSettings} from "../domain/settings";
 import { applicationSession } from '../database/session';
 import StardogKB from '../datasources/stardog.js';
 import Artemis from '../datasources/artemis.js';
+import DynamoDB from '../datasources/dynamoDB.js';
 import nconf from "nconf";
 import querySelectMap from "../cyio/schema/querySelectMap";
 import {
@@ -86,6 +87,7 @@ const createApolloServer = (app) => {
     dataSources: () => ({
       Stardog: new StardogKB( ),
       Artemis: new Artemis( ),
+      DynamoDB: new DynamoDB( ),
     }),
     playground: {
       cdnUrl,
