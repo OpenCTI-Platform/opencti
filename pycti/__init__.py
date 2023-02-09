3da40c924dbc6e320b22eb651ca7587886fdef5e
Samuel Hassine <samuel.hassine@filigran.io>
2023-02-09 08:53:35 +0100

    [client] Change version number

--- pycti/__init__.py
+++ pycti/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "5.5.5"
+__version__ = "5.5.6"
 
 from .api.opencti_api_client import OpenCTIApiClient
 from .api.opencti_api_connector import OpenCTIApiConnector
