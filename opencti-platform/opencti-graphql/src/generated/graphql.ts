c8d48d3c956adc3cb34da808884e4285e0eff1eb
Cathia Archidoit <cathia.archidoit@filigran.io>
2023-02-09 09:27:03 +0100

    [Back] generated files (#2735)

--- opencti-platform/opencti-graphql/src/generated/graphql.ts
+++ opencti-platform/opencti-graphql/src/generated/graphql.ts
@@ -19921,8 +19921,6 @@ export type StixDomainObjectsFiltering = {
 
 export enum StixDomainObjectsOrdering {
   Score = '_score',
-  AttributeCount = 'attribute_count',
-  Confidence = 'confidence',
   AttributeAbstract = 'attribute_abstract',
   AttributeCount = 'attribute_count',
   ChannelTypes = 'channel_types',
