orgTestCodacy11KRepos110MB/repo-5178-opencti

[Front] Refacto Incident component into pure function

opencti-platform/opencti-front/src/private/components/events/incidents/IncidentPopover.tsx
@@ -1,0 +1,177 @@
+import React, { useState } from 'react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Drawer from '@mui/material/Drawer';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import MoreVert from '@mui/icons-material/MoreVert';
+import { graphql, useMutation } from 'react-relay';
+import makeStyles from '@mui/styles/makeStyles';
+import { useNavigate } from 'react-router-dom-v5-compat';
+import { PopoverProps } from '@mui/material/Popover';
+import DialogContentText from '@mui/material/DialogContentText';
+import { useFormatter } from '../../../../components/i18n';
+import { IncidentEditionQuery } from './IncidentEdition';
+import IncidentEditionContainer from './IncidentEditionContainer';
+import Loader, { LoaderVariant } from '../../../../components/Loader';
+import Security from '../../../../utils/Security';
+import Transition from '../../../../components/Transition';
+import { KNOWLEDGE_KNUPDATE_KNDELETE } from '../../../../utils/hooks/useGranted';
+import { Theme } from '../../../../components/Theme';
+import useQueryLoading from '../../../../utils/hooks/useQueryLoading';
+import useDeletion from '../../../../utils/hooks/useDeletion';
+import { IncidentEditionContainerQuery } from './__generated__/IncidentEditionContainerQuery.graphql';
+
+const useStyles = makeStyles<Theme>((theme) => ({
+  container: {
+    margin: 0,
+  },
+  drawerPaper: {
+    minHeight: '100vh',
+    width: '50%',
+    position: 'fixed',
+    overflow: 'auto',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    padding: 0,
+  },
+}));
+
+const IncidentPopoverDeletionMutation = graphql`
+  mutation IncidentPopoverDeletionMutation($id: ID!) {
+    incidentEdit(id: $id) {
+      delete
+    }
+  }
+`;
+
+const IncidentPopover = ({ id }: { id: string }) => {
+  const classes = useStyles();
+  const { t } = useFormatter();
+
+  const navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = useState<PopoverProps['anchorEl']>();
+  const [displayEdit, setDisplayEdit] = useState<boolean>(false);
+
+  const [commit] = useMutation(IncidentPopoverDeletionMutation);
+  const queryRef = useQueryLoading<IncidentEditionContainerQuery>(IncidentEditionQuery, { id });
+
+  const handleOpen = (event: React.SyntheticEvent) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(undefined);
+  };
+
+  const handleOpenEdit = () => {
+    setDisplayEdit(true);
+    handleClose();
+  };
+
+  const handleCloseEdit = () => {
+    setDisplayEdit(false);
+  };
+
+  const {
+    deleting,
+    handleOpenDelete,
+    displayDelete,
+    handleCloseDelete,
+    setDeleting,
+  } = useDeletion({ handleClose });
+
+  const submitDelete = () => {
+    setDeleting(true);
+    commit({
+      variables: {
+        id,
+      },
+      onCompleted: () => {
+        setDeleting(false);
+        handleClose();
+        navigate('/dashboard/events/incidents');
+      },
+    });
+  };
+
+  return (
+      <div className={classes.container}>
+        <IconButton
+          onClick={handleOpen}
+          aria-haspopup="true"
+          style={{ marginTop: 3 }}
+          size="large"
+        >
+          <MoreVert />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleOpenEdit}>
+            {t('Update')}
+          </MenuItem>
+          <Security needs={[KNOWLEDGE_KNUPDATE_KNDELETE]}>
+            <MenuItem onClick={handleOpenDelete}>
+              {t('Delete')}
+            </MenuItem>
+          </Security>
+        </Menu>
+        <Dialog
+          PaperProps={{ elevation: 1 }}
+          open={displayDelete}
+          keepMounted={true}
+          TransitionComponent={Transition}
+          onClose={handleCloseDelete}
+        >
+          <DialogContent>
+            <DialogContentText>
+              {t('Do you want to delete this incident?')}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={handleCloseDelete}
+              disabled={deleting}
+            >
+              {t('Cancel')}
+            </Button>
+            <Button
+              color="secondary"
+              onClick={submitDelete}
+              disabled={deleting}
+            >
+              {t('Delete')}
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <Drawer
+          open={displayEdit}
+          anchor="right"
+          elevation={1}
+          sx={{ zIndex: 1202 }}
+          classes={{ paper: classes.drawerPaper }}
+          onClose={handleCloseEdit}
+        >
+          {queryRef && (
+            <React.Suspense fallback={<Loader variant={LoaderVariant.inElement} />}>
+              <IncidentEditionContainer
+                queryRef={queryRef}
+                handleClose={handleClose}
+              />
+            </React.Suspense>
+          )}
+        </Drawer>
+      </div>
+  );
+};
+//
+export default IncidentPopover;

opencti-platform/opencti-front/src/private/components/events/incidents/Root.tsx
@@ -1,23 +1,26 @@
-import React, { Component } from 'react';
-import * as PropTypes from 'prop-types';
-import { Route, Redirect, withRouter, Switch } from 'react-router-dom';
-import { graphql } from 'react-relay';
-import {
-  QueryRenderer,
-  requestSubscription,
-} from '../../../../relay/environment';
+// TODO Remove this when V6
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import React, { useMemo } from 'react';
+import { Route, Redirect, Switch, useParams } from 'react-router-dom';
+import { graphql, usePreloadedQuery, useSubscription } from 'react-relay';
+import { GraphQLSubscriptionConfig } from 'relay-runtime';
 import TopBar from '../../nav/TopBar';
 import Incident from './Incident';
 import IncidentKnowledge from './IncidentKnowledge';
 import StixDomainObjectHeader from '../../common/stix_domain_objects/StixDomainObjectHeader';
 import FileManager from '../../common/files/FileManager';
 import IncidentPopover from './IncidentPopover';
-import Loader from '../../../../components/Loader';
+import Loader, { LoaderVariant } from '../../../../components/Loader';
 import StixCoreObjectHistory from '../../common/stix_core_objects/StixCoreObjectHistory';
 import StixCoreObjectOrStixCoreRelationshipContainers from '../../common/containers/StixCoreObjectOrStixCoreRelationshipContainers';
 import ErrorNotFound from '../../../../components/ErrorNotFound';
 import StixCoreObjectKnowledgeBar from '../../common/stix_core_objects/StixCoreObjectKnowledgeBar';
 import StixDomainObjectContent from '../../common/stix_domain_objects/StixDomainObjectContent';
+import useQueryLoading from '../../../../utils/hooks/useQueryLoading';
+
+import { RootIncidentQuery } from './__generated__/RootIncidentQuery.graphql';
+import { RootIncidentSubscription } from './__generated__/RootIncidentSubscription.graphql';
 
 const subscription = graphql`
   subscription RootIncidentSubscription($id: ID!) {
@@ -34,7 +37,7 @@ const subscription = graphql`
   }
 `;
 
-const IncidentQuery = graphql`
+const incidentQuery = graphql`
   query RootIncidentQuery($id: String!) {
     incident(id: $id) {
       id
@@ -59,32 +62,20 @@ const IncidentQuery = graphql`
   }
 `;
 
-class RootIncident extends Component {
-  constructor(props) {
-    super(props);
-    const {
-      match: {
-        params: { incidentId },
-      },
-    } = props;
-    this.sub = requestSubscription({
+const RootIncidentComponent = ({ queryRef }) => {
+  const { incidentId } = useParams() as { incidentId: string };
+  const link = `/dashboard/events/incidents/${incidentId}/knowledge`;
+  const subConfig = useMemo<GraphQLSubscriptionConfig<RootIncidentSubscription>>(
+    () => ({
       subscription,
       variables: { id: incidentId },
-    });
-  }
-
-  componentWillUnmount() {
-    this.sub.dispose();
-  }
-
-  render() {
-    const {
-      match: {
-        params: { incidentId },
-      },
-    } = this.props;
-    const link = `/dashboard/events/incidents/${incidentId}/knowledge`;
-    return (
+    }),
+    [incidentId],
+  );
+  useSubscription(subConfig);
+  const data = usePreloadedQuery(incidentQuery, queryRef);
+  const { incident, connectorsForImport, connectorsForExport } = data;
+  return (
       <div>
         <TopBar />
         <Route path="/dashboard/events/incidents/:incidentId/knowledge">
@@ -101,20 +92,13 @@ class RootIncident extends Component {
             ]}
           />
         </Route>
-        <QueryRenderer
-          query={IncidentQuery}
-          variables={{ id: incidentId }}
-          render={({ props }) => {
-            if (props) {
-              if (props.incident) {
-                return (
+        <>
+          {incident ? (
                   <Switch>
                     <Route
                       exact
                       path="/dashboard/events/incidents/:incidentId"
-                      render={(routeProps) => (
-                        <Incident {...routeProps} incident={props.incident} />
-                      )}
+                      render={() => <Incident incidentData={incident} />}
                     />
                     <Route
                       exact
@@ -130,7 +114,7 @@ class RootIncident extends Component {
                       render={(routeProps) => (
                         <IncidentKnowledge
                           {...routeProps}
-                          incident={props.incident}
+                          incident={incident}
                         />
                       )}
                     />
@@ -141,13 +125,13 @@ class RootIncident extends Component {
                         <React.Fragment>
                           <StixDomainObjectHeader
                             entityType={'Incident'}
-                            stixDomainObject={props.incident}
-                            PopoverComponent={<IncidentPopover />}
+                            stixDomainObject={incident}
+                            PopoverComponent={IncidentPopover}
                             disableSharing={true}
                           />
                           <StixDomainObjectContent
                             {...routeProps}
-                            stixDomainObject={props.incident}
+                            stixDomainObject={incident}
                           />
                         </React.Fragment>
                       )}
@@ -159,13 +143,13 @@ class RootIncident extends Component {
                         <React.Fragment>
                           <StixDomainObjectHeader
                             entityType={'Incident'}
-                            stixDomainObject={props.incident}
-                            PopoverComponent={<IncidentPopover />}
+                            stixDomainObject={incident}
+                            PopoverComponent={IncidentPopover}
                           />
                           <StixCoreObjectOrStixCoreRelationshipContainers
                             {...routeProps}
                             stixDomainObjectOrStixCoreRelationship={
-                              props.incident
+                              incident
                             }
                           />
                         </React.Fragment>
@@ -178,15 +162,15 @@ class RootIncident extends Component {
                         <React.Fragment>
                           <StixDomainObjectHeader
                             entityType={'Incident'}
-                            stixDomainObject={props.incident}
-                            PopoverComponent={<IncidentPopover />}
+                            stixDomainObject={incident}
+                            PopoverComponent={IncidentPopover}
                           />
                           <FileManager
                             {...routeProps}
                             id={incidentId}
-                            connectorsImport={props.connectorsForImport}
-                            connectorsExport={props.connectorsForExport}
-                            entity={props.incident}
+                            connectorsImport={connectorsForImport}
+                            connectorsExport={connectorsForExport}
+                            entity={incident}
                           />
                         </React.Fragment>
                       )}
@@ -198,8 +182,8 @@ class RootIncident extends Component {
                         <React.Fragment>
                           <StixDomainObjectHeader
                             entityType={'Incident'}
-                            stixDomainObject={props.incident}
-                            PopoverComponent={<IncidentPopover />}
+                            stixDomainObject={incident}
+                            PopoverComponent={IncidentPopover}
                           />
                           <StixCoreObjectHistory
                             {...routeProps}
@@ -209,21 +193,23 @@ class RootIncident extends Component {
                       )}
                     />
                   </Switch>
-                );
-              }
-              return <ErrorNotFound />;
-            }
-            return <Loader />;
-          }}
-        />
+          ) : (
+            <ErrorNotFound />
+          )}
+        </>
       </div>
-    );
-  }
-}
-
-RootIncident.propTypes = {
-  children: PropTypes.node,
-  match: PropTypes.object,
+  );
 };
 
-export default withRouter(RootIncident);
+const RootIncident = () => {
+  const { incidentId } = useParams() as { incidentId: string };
+  const queryRef = useQueryLoading < RootIncidentQuery >(incidentQuery, { id: incidentId });
+  return queryRef ? (
+    <React.Suspense fallback={<Loader variant={LoaderVariant.inElement} />}>
+      <RootIncidentComponent queryRef={queryRef} />
+    </React.Suspense>
+  ) : (
+    <Loader variant={LoaderVariant.inElement} />
+  );
+};
+export default RootIncident;

OpenCTI-Platform/opencti

opencti-platform/opencti-front/src/private/components/events/incidents/IncidentPopover.tsx
@@ -1,0 +1,175 @@
+import React, { useState } from 'react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Drawer from '@mui/material/Drawer';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import MoreVert from '@mui/icons-material/MoreVert';
+import { graphql, useMutation } from 'react-relay';
+import makeStyles from '@mui/styles/makeStyles';
+import { useNavigate } from 'react-router-dom-v5-compat';
+import { PopoverProps } from '@mui/material/Popover';
+import DialogContentText from '@mui/material/DialogContentText';
+import { useFormatter } from '../../../../components/i18n';
+import IncidentEditionContainer, { IncidentEditionQuery } from './IncidentEditionContainer';
+import Loader, { LoaderVariant } from '../../../../components/Loader';
+import Security from '../../../../utils/Security';
+import Transition from '../../../../components/Transition';
+import { KNOWLEDGE_KNUPDATE_KNDELETE } from '../../../../utils/hooks/useGranted';
+import { Theme } from '../../../../components/Theme';
+import useQueryLoading from '../../../../utils/hooks/useQueryLoading';
+import useDeletion from '../../../../utils/hooks/useDeletion';
+import { IncidentEditionContainerQuery } from './__generated__/IncidentEditionContainerQuery.graphql';
+
+const useStyles = makeStyles<Theme>((theme) => ({
+  container: {
+    margin: 0,
+  },
+  drawerPaper: {
+    minHeight: '100vh',
+    width: '50%',
+    position: 'fixed',
+    overflow: 'auto',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    padding: 0,
+  },
+}));
+
+const IncidentPopoverDeletionMutation = graphql`
+  mutation IncidentPopoverDeletionMutation($id: ID!) {
+    incidentEdit(id: $id) {
+      delete
+    }
+  }
+`;
+
+const IncidentPopover = ({ id }: { id: string }) => {
+  const classes = useStyles();
+  const { t } = useFormatter();
+
+  const navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = useState<PopoverProps['anchorEl']>();
+  const [displayEdit, setDisplayEdit] = useState<boolean>(false);
+
+  const [commit] = useMutation(IncidentPopoverDeletionMutation);
+  const queryRef = useQueryLoading<IncidentEditionContainerQuery>(IncidentEditionQuery, { id });
+  const handleOpen = (event: React.SyntheticEvent) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(undefined);
+  };
+
+  const handleOpenEdit = () => {
+    setDisplayEdit(true);
+    handleClose();
+  };
+
+  const handleCloseEdit = () => {
+    setDisplayEdit(false);
+  };
+
+  const {
+    deleting,
+    handleOpenDelete,
+    displayDelete,
+    handleCloseDelete,
+    setDeleting,
+  } = useDeletion({ handleClose });
+
+  const submitDelete = () => {
+    setDeleting(true);
+    commit({
+      variables: {
+        id,
+      },
+      onCompleted: () => {
+        setDeleting(false);
+        handleClose();
+        navigate('/dashboard/events/incidents');
+      },
+    });
+  };
+
+  return (
+      <div className={classes.container}>
+        <IconButton
+          onClick={handleOpen}
+          aria-haspopup="true"
+          style={{ marginTop: 3 }}
+          size="large"
+        >
+          <MoreVert />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleOpenEdit}>
+            {t('Update')}
+          </MenuItem>
+          <Security needs={[KNOWLEDGE_KNUPDATE_KNDELETE]}>
+            <MenuItem onClick={handleOpenDelete}>
+              {t('Delete')}
+            </MenuItem>
+          </Security>
+        </Menu>
+        <Dialog
+          PaperProps={{ elevation: 1 }}
+          open={displayDelete}
+          keepMounted={true}
+          TransitionComponent={Transition}
+          onClose={handleCloseDelete}
+        >
+          <DialogContent>
+            <DialogContentText>
+              {t('Do you want to delete this incident?')}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={handleCloseDelete}
+              disabled={deleting}
+            >
+              {t('Cancel')}
+            </Button>
+            <Button
+              color="secondary"
+              onClick={submitDelete}
+              disabled={deleting}
+            >
+              {t('Delete')}
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <Drawer
+          open={displayEdit}
+          anchor="right"
+          elevation={1}
+          sx={{ zIndex: 1202 }}
+          classes={{ paper: classes.drawerPaper }}
+          onClose={handleCloseEdit}
+        >
+          {queryRef && (
+            <React.Suspense fallback={<Loader variant={LoaderVariant.inElement} />}>
+              <IncidentEditionContainer
+                queryRef={queryRef}
+                handleClose={handleCloseEdit}
+              />
+            </React.Suspense>
+          )}
+        </Drawer>
+      </div>
+  );
+};
+//
+export default IncidentPopover;

opencti-platform/opencti-front/src/private/components/events/incidents/Root.tsx
@@ -1,23 +1,26 @@
-import React, { Component } from 'react';
-import * as PropTypes from 'prop-types';
-import { Route, Redirect, withRouter, Switch } from 'react-router-dom';
-import { graphql } from 'react-relay';
-import {
-  QueryRenderer,
-  requestSubscription,
-} from '../../../../relay/environment';
+// TODO Remove this when V6
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import React, { useMemo } from 'react';
+import { Route, Redirect, Switch, useParams } from 'react-router-dom';
+import { graphql, usePreloadedQuery, useSubscription } from 'react-relay';
+import { GraphQLSubscriptionConfig } from 'relay-runtime';
 import TopBar from '../../nav/TopBar';
 import Incident from './Incident';
 import IncidentKnowledge from './IncidentKnowledge';
 import StixDomainObjectHeader from '../../common/stix_domain_objects/StixDomainObjectHeader';
 import FileManager from '../../common/files/FileManager';
 import IncidentPopover from './IncidentPopover';
-import Loader from '../../../../components/Loader';
+import Loader, { LoaderVariant } from '../../../../components/Loader';
 import StixCoreObjectHistory from '../../common/stix_core_objects/StixCoreObjectHistory';
 import StixCoreObjectOrStixCoreRelationshipContainers from '../../common/containers/StixCoreObjectOrStixCoreRelationshipContainers';
 import ErrorNotFound from '../../../../components/ErrorNotFound';
 import StixCoreObjectKnowledgeBar from '../../common/stix_core_objects/StixCoreObjectKnowledgeBar';
 import StixDomainObjectContent from '../../common/stix_domain_objects/StixDomainObjectContent';
+import useQueryLoading from '../../../../utils/hooks/useQueryLoading';
+
+import { RootIncidentQuery } from './__generated__/RootIncidentQuery.graphql';
+import { RootIncidentSubscription } from './__generated__/RootIncidentSubscription.graphql';
 
 const subscription = graphql`
   subscription RootIncidentSubscription($id: ID!) {
@@ -34,7 +37,7 @@ const subscription = graphql`
   }
 `;
 
-const IncidentQuery = graphql`
+const incidentQuery = graphql`
   query RootIncidentQuery($id: String!) {
     incident(id: $id) {
       id
@@ -59,32 +62,20 @@ const IncidentQuery = graphql`
   }
 `;
 
-class RootIncident extends Component {
-  constructor(props) {
-    super(props);
-    const {
-      match: {
-        params: { incidentId },
-      },
-    } = props;
-    this.sub = requestSubscription({
+const RootIncidentComponent = ({ queryRef }) => {
+  const { incidentId } = useParams() as { incidentId: string };
+  const link = `/dashboard/events/incidents/${incidentId}/knowledge`;
+  const subConfig = useMemo<GraphQLSubscriptionConfig<RootIncidentSubscription>>(
+    () => ({
       subscription,
       variables: { id: incidentId },
-    });
-  }
-
-  componentWillUnmount() {
-    this.sub.dispose();
-  }
-
-  render() {
-    const {
-      match: {
-        params: { incidentId },
-      },
-    } = this.props;
-    const link = `/dashboard/events/incidents/${incidentId}/knowledge`;
-    return (
+    }),
+    [incidentId],
+  );
+  useSubscription(subConfig);
+  const data = usePreloadedQuery(incidentQuery, queryRef);
+  const { incident, connectorsForImport, connectorsForExport } = data;
+  return (
       <div>
         <TopBar />
         <Route path="/dashboard/events/incidents/:incidentId/knowledge">
@@ -103,20 +94,13 @@ class RootIncident extends Component {
             ]}
           />
         </Route>
-        <QueryRenderer
-          query={IncidentQuery}
-          variables={{ id: incidentId }}
-          render={({ props }) => {
-            if (props) {
-              if (props.incident) {
-                return (
+        <>
+          {incident ? (
                   <Switch>
                     <Route
                       exact
                       path="/dashboard/events/incidents/:incidentId"
-                      render={(routeProps) => (
-                        <Incident {...routeProps} incident={props.incident} />
-                      )}
+                      render={() => <Incident incidentData={incident} />}
                     />
                     <Route
                       exact
@@ -129,10 +113,9 @@ class RootIncident extends Component {
                     />
                     <Route
                       path="/dashboard/events/incidents/:incidentId/knowledge"
-                      render={(routeProps) => (
+                      render={() => (
                         <IncidentKnowledge
-                          {...routeProps}
-                          incident={props.incident}
+                          incidentData={incident}
                         />
                       )}
                     />
@@ -143,13 +126,13 @@ class RootIncident extends Component {
                         <React.Fragment>
                           <StixDomainObjectHeader
                             entityType={'Incident'}
-                            stixDomainObject={props.incident}
-                            PopoverComponent={<IncidentPopover />}
+                            stixDomainObject={incident}
+                            PopoverComponent={IncidentPopover}
                             disableSharing={true}
                           />
                           <StixDomainObjectContent
                             {...routeProps}
-                            stixDomainObject={props.incident}
+                            stixDomainObject={incident}
                           />
                         </React.Fragment>
                       )}
@@ -161,13 +144,13 @@ class RootIncident extends Component {
                         <React.Fragment>
                           <StixDomainObjectHeader
                             entityType={'Incident'}
-                            stixDomainObject={props.incident}
-                            PopoverComponent={<IncidentPopover />}
+                            stixDomainObject={incident}
+                            PopoverComponent={IncidentPopover}
                           />
                           <StixCoreObjectOrStixCoreRelationshipContainers
                             {...routeProps}
                             stixDomainObjectOrStixCoreRelationship={
-                              props.incident
+                              incident
                             }
                           />
                         </React.Fragment>
@@ -180,15 +163,15 @@ class RootIncident extends Component {
                         <React.Fragment>
                           <StixDomainObjectHeader
                             entityType={'Incident'}
-                            stixDomainObject={props.incident}
-                            PopoverComponent={<IncidentPopover />}
+                            stixDomainObject={incident}
+                            PopoverComponent={IncidentPopover}
                           />
                           <FileManager
                             {...routeProps}
                             id={incidentId}
-                            connectorsImport={props.connectorsForImport}
-                            connectorsExport={props.connectorsForExport}
-                            entity={props.incident}
+                            connectorsImport={connectorsForImport}
+                            connectorsExport={connectorsForExport}
+                            entity={incident}
                           />
                         </React.Fragment>
                       )}
@@ -200,8 +183,8 @@ class RootIncident extends Component {
                         <React.Fragment>
                           <StixDomainObjectHeader
                             entityType={'Incident'}
-                            stixDomainObject={props.incident}
-                            PopoverComponent={<IncidentPopover />}
+                            stixDomainObject={incident}
+                            PopoverComponent={IncidentPopover}
                           />
                           <StixCoreObjectHistory
                             {...routeProps}
@@ -211,21 +194,23 @@ class RootIncident extends Component {
                       )}
                     />
                   </Switch>
-                );
-              }
-              return <ErrorNotFound />;
-            }
-            return <Loader />;
-          }}
-        />
+          ) : (
+            <ErrorNotFound />
+          )}
+        </>
       </div>
-    );
-  }
-}
-
-RootIncident.propTypes = {
-  children: PropTypes.node,
-  match: PropTypes.object,
+  );
 };
 
-export default withRouter(RootIncident);
+const RootIncident = () => {
+  const { incidentId } = useParams() as { incidentId: string };
+  const queryRef = useQueryLoading < RootIncidentQuery >(incidentQuery, { id: incidentId });
+  return queryRef ? (
+    <React.Suspense fallback={<Loader variant={LoaderVariant.inElement} />}>
+      <RootIncidentComponent queryRef={queryRef} />
+    </React.Suspense>
+  ) : (
+    <Loader variant={LoaderVariant.inElement} />
+  );
+};
+export default RootIncident;