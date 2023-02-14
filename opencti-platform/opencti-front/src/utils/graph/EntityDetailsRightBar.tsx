d8b80ef02475e2ad2c6094aaf3685c4d4707fb24
marie flores <marie.flores@filigran.io>
2023-02-14 11:48:49 +0100

    [Front] Add selectedNode type and link to entity

--- opencti-platform/opencti-front/src/utils/graph/EntityDetailsRightBar.tsx
+++ opencti-platform/opencti-front/src/utils/graph/EntityDetailsRightBar.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import makeStyles from '@mui/styles/makeStyles';
 import Drawer from '@mui/material/Drawer';
 import { Theme } from '@mui/material/styles/createTheme';
@@ -6,10 +6,16 @@ import List from '@mui/material/List';
 import ListSubheader from '@mui/material/ListSubheader';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import { Link } from 'react-router-dom';
+import { InfoOutlined } from '@mui/icons-material';
 import { useFormatter } from '../../components/i18n';
+import { resolveLink } from '../Entity';
 
 const useStyles = makeStyles < Theme >((theme) => ({
   drawerPaper: {
+    variant: 'persistent',
     minHeight: '100vh',
     width: 250,
     padding: '0 20px 20px 20px',
@@ -22,23 +28,53 @@ const useStyles = makeStyles < Theme >((theme) => ({
   toolbar: theme.mixins.toolbar,
 }));
 
+interface selectedNode {
+  id: string
+  name: string
+  description: string
+  parent_types: string
+  relationship_type: string
+  fromType: string
+  fromId: string
+  entity_type: string
+
+}
 interface EntityDetailsRightsBarProps {
-  selectedNodes: { id: string, value:string }[];
+  selectedNodes: selectedNode[];
   open: boolean
-  handleClose: () => void
+  handleClose?: () => void
 }
 const EntityDetailsRightsBar: FunctionComponent<EntityDetailsRightsBarProps> = ({ selectedNodes, open, handleClose }) => {
   const classes = useStyles();
   const { t } = useFormatter();
-  const entity = selectedNodes[0];
-  console.log('entity:   ', entity);
+
+  const [controlOpen, setControlOpen] = useState<boolean>(open ?? false);
+  const handleControlClose = () => setControlOpen(false);
+  console.log('entity:   ', selectedNodes[0]);
+
+  const viewLink = () => {
+    if (
+      !selectedNodes[0].parent_types.includes(
+        'stix-cyber-observable-relationship',
+      )
+      && selectedNodes[0].relationship_type
+    ) {
+      return `${resolveLink(selectedNodes[0].fromType)}/${
+        selectedNodes[0].fromId
+      }/knowledge/relations/${selectedNodes[0].id}`;
+    }
+    return `${resolveLink(selectedNodes[0].entity_type)}/${
+      selectedNodes[0].id
+    }`;
+  };
+
   return (
     <Drawer
-      open={open}
+      open={handleClose ? open : controlOpen}
       variant="permanent"
       anchor="right"
       classes={{ paper: classes.drawerPaper }}
-      onClose={handleClose}
+      onClose={handleClose ?? handleControlClose }
     >
       <List classes={{ root: classes.title }}
         subheader={
@@ -52,6 +88,20 @@ const EntityDetailsRightsBar: FunctionComponent<EntityDetailsRightsBarProps> = (
             classes={{ root: classes.item }}
           >
             <ListItemText primary={node.id} />
+            <Tooltip title={t('View the item')}>
+                  <span>
+                    <IconButton
+                      color="primary"
+                      component={Link}
+                      target="_blank"
+                      to={viewLink}
+                      disabled={!viewLink}
+                      size="large"
+                    >
+                      <InfoOutlined />
+                    </IconButton>
+                  </span>
+            </Tooltip>
           </ListItem>
         ))}
       </List>
