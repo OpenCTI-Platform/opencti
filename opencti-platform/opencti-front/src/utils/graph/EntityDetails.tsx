09779c758d337dcec83c8cb3eff452cf29f387bb
marie flores <marie.flores@filigran.io>
2023-03-03 10:17:51 +0100

    [Front] Add external references

--- opencti-platform/opencti-front/src/utils/graph/EntityDetails.tsx
+++ opencti-platform/opencti-front/src/utils/graph/EntityDetails.tsx
@@ -6,6 +6,10 @@ import { Link } from 'react-router-dom';
 import { InfoOutlined } from '@mui/icons-material';
 import Typography from '@mui/material/Typography';
 import { graphql, PreloadedQuery, usePreloadedQuery } from 'react-relay';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
 import { useFormatter } from '../../components/i18n';
 import { resolveLink } from '../Entity';
 import ItemAuthor from '../../components/ItemAuthor';
@@ -17,6 +21,7 @@ import ItemMarkings from '../../components/ItemMarkings';
 import { truncate } from '../String';
 import type { SelectedEntity } from './EntitiesDetailsRightBar';
 import ErrorNotFound from '../../components/ErrorNotFound';
+import ItemIcon from '../../components/ItemIcon';
 
 const useStyles = makeStyles(() => ({
   entity: {
@@ -56,139 +61,150 @@ const entityDetailsQuery = graphql`
           }
         }
       }
-      ... on StixDomainObject {
-        created
-      }
-      ... on AttackPattern {
-        name
-        x_mitre_id
-      }
-      ... on Campaign {
-        name
-        first_seen
-        last_seen
-      }
-      ... on CourseOfAction {
-        name
-      }
-      ... on Note {
-        attribute_abstract
-        content
-      }
-      ... on ObservedData {
-        name
-      }
-      ... on Opinion {
-        opinion
-      }
-      ... on Report {
-        name
-        published
-      }
-      ... on Grouping {
-        name
-        description
-      }
-      ... on Individual {
-        name
-        description
-      }
-      ... on Organization {
-        name
-        description
-      }
-      ... on Sector {
-        name
-        description
-      }
-      ... on System {
-        name
-        description
-      }
-      ... on Indicator {
-        name
-        description
-      }
-      ... on Infrastructure {
-        name
-        description
-      }
-      ... on IntrusionSet {
-        name
-        first_seen
-        last_seen
-        description
-      }
-      ... on Position {
-        name
-        description
-      }
-      ... on City {
-        name
-        description
-      }
-      ... on AdministrativeArea {
-        name
-        description
-      }
-      ... on Country {
-        name
-        description
-      }
-      ... on Region {
-        name
-        description
-      }
-      ... on Malware {
-        name
-        first_seen
-        last_seen
-        description
-      }
-      ... on ThreatActor {
-        name
-        first_seen
-        last_seen
-        description
-      }
-      ... on Tool {
-        name
-      }
-      ... on Vulnerability {
-        name
-      }
-      ... on Incident {
-        name
-        first_seen
-        last_seen
-        description
-      }
-      ... on StixCyberObservable {
-        observable_value
-      }
-      ... on StixFile {
-        observableName: name
-      }
-      ... on Event {
-        name
-      }
-      ... on Case {
-        name
-      }
-      ... on Narrative {
-        name
-      }
-      ... on DataComponent {
-        name
-      }
-      ... on DataSource {
-        name
-      }
-      ... on Language {
-        name
-      }
+      externalReferences  {
+                edges {
+                    node {
+                        id
+                        source_name
+                        url
+                        external_id
+                        description
+                    }
+                }
+            }
+            ... on StixDomainObject {
+                created
+            }
+            ... on AttackPattern {
+                name
+                x_mitre_id
+            }
+            ... on Campaign {
+                name
+                first_seen
+                last_seen
+            }
+            ... on CourseOfAction {
+                name
+            }
+            ... on Note {
+                attribute_abstract
+                content
+            }
+            ... on ObservedData {
+                name
+            }
+            ... on Opinion {
+                opinion
+            }
+            ... on Report {
+                name
+                published
+            }
+            ... on Grouping {
+                name
+                description
+            }
+            ... on Individual {
+                name
+                description
+            }
+            ... on Organization {
+                name
+                description
+            }
+            ... on Sector {
+                name
+                description
+            }
+            ... on System {
+                name
+                description
+            }
+            ... on Indicator {
+                name
+                description
+            }
+            ... on Infrastructure {
+                name
+                description
+            }
+            ... on IntrusionSet {
+                name
+                first_seen
+                last_seen
+                description
+            }
+            ... on Position {
+                name
+                description
+            }
+            ... on City {
+                name
+                description
+            }
+            ... on AdministrativeArea {
+                name
+                description
+            }
+            ... on Country {
+                name
+                description
+            }
+            ... on Region {
+                name
+                description
+            }
+            ... on Malware {
+                name
+                first_seen
+                last_seen
+                description
+            }
+            ... on ThreatActor {
+                name
+                first_seen
+                last_seen
+                description
+            }
+            ... on Tool {
+                name
+            }
+            ... on Vulnerability {
+                name
+            }
+            ... on Incident {
+                name
+                first_seen
+                last_seen
+                description
+            }
+            ... on StixCyberObservable {
+                observable_value
+            }
+            ... on StixFile {
+                observableName: name
+            }
+            ... on Event {
+                name
+            }
+            ... on Case {
+                name
+            }
+            ... on Narrative {
+                name
+            }
+            ... on DataComponent {
+                name
+            }
+            ... on DataSource {
+                name
+            }
+            ... on Language {
+                name
+            }
+        }
     }
-  }
 `;
 
 interface EntityDetailsComponentProps {
@@ -206,6 +222,10 @@ EntityDetailsComponentProps
     queryRef,
   );
   const { stixCoreObject } = entity;
+
+  const externalReferencesEdges = stixCoreObject?.externalReferences?.edges;
+
+  console.log('externalReferencesEdges   :', externalReferencesEdges);
 
   if (!stixCoreObject) {
     return <ErrorNotFound />;
@@ -242,6 +262,13 @@ EntityDetailsComponentProps
         {t('Type')}
       </Typography>
       {stixCoreObject.entity_type}
+      <Typography variant="h3"
+                  gutterBottom={true}
+                  className={classes.label}
+      >
+        {t('Creation date')}
+      </Typography>
+      {fldt(stixCoreObject.created_at)}
       <Typography
         variant="h3"
         gutterBottom={true}
@@ -258,13 +285,6 @@ EntityDetailsComponentProps
         '-'
       )
       }
-      <Typography variant="h3"
-                  gutterBottom={true}
-                  className={classes.label}
-      >
-        {t('Creation date')}
-      </Typography>
-      {fldt(stixCoreObject.created_at)}
       <Typography variant="h3"
                   gutterBottom={true}
                   className={classes.label}
@@ -287,8 +307,55 @@ EntityDetailsComponentProps
         {t('Author')}
       </Typography>
       <ItemAuthor
-          createdBy={stixCoreObject.createdBy}
-        />
+        createdBy={stixCoreObject.createdBy}
+      />
+      <Typography
+        variant="h3"
+        gutterBottom={true}
+        className={classes.label}
+      >
+        {t('External References')}
+      </Typography>
+      <List style={{ marginBottom: 0 }}>
+        {externalReferencesEdges?.map((externalReference) => {
+          const externalReferenceId = externalReference.node.external_id
+            ? `(${externalReference.node.external_id})`
+            : '';
+          let externalReferenceSecondary = '';
+          if (externalReference.node.url && externalReference.node.url.length > 0) {
+            externalReferenceSecondary = externalReference.node.url;
+          } else if (
+            externalReference.node.description
+            && externalReference.node.description.length > 0
+          ) {
+            externalReferenceSecondary = externalReference.node.description;
+          } else {
+            externalReferenceSecondary = t('No description');
+          }
+          return (
+            <div key={externalReference.node.id}>
+              <ListItem
+                component={Link}
+                to={`/dashboard/analysis/external_references/${externalReference.node.id}`}
+                dense={true}
+                divider={true}
+                button={true}
+              >
+                <ListItemIcon>
+                  <ItemIcon type="External-Reference" />
+                </ListItemIcon>
+                <ListItemText
+                  primary={truncate(
+                    `${externalReference.node.source_name} ${externalReferenceId}`,
+                    70,
+                  )}
+                  secondary={truncate(externalReferenceSecondary, 70)}
+                />
+              </ListItem>
+            </div>
+          );
+        })}
+      </List>
     </div>
   );
 };
