005680421e2aece69fe4926f9f513dc700348519
Cathia Archidoit <cathia.archidoit@filigran.io>
2023-05-02 09:42:53 +0200

    [frontend] filters reset after the creation of a live trigger (#3258)

--- opencti-platform/opencti-front/src/private/components/profile/triggers/TriggerLiveCreation.tsx
+++ opencti-platform/opencti-front/src/private/components/profile/triggers/TriggerLiveCreation.tsx
@@ -149,7 +149,10 @@ const TriggerLiveCreation: FunctionComponent<TriggerLiveCreationProps> = ({
   const [filters, setFilters] = useState<
   Record<string, { id: string; value: string }[]>
   >({});
-  const onReset = () => handleClose && handleClose();
+  const onReset = () => {
+    handleClose?.();
+    setFilters({});
+  };
   const handleAddFilter = (
     key: string,
     id: string,
