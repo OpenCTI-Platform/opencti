11b4d64f96b39b547c40fabb1272efce0f66c676
Yassine Ouaamou <yassine.ouaamou@filigran.io>
2023-05-16 18:15:44 +0200

    [backend] lazy-loading spec_version when fetching stix Refs

--- opencti-platform/opencti-graphql/src/resolvers/stixRefRelationship.js
+++ opencti-platform/opencti-graphql/src/resolvers/stixRefRelationship.js
@@ -20,6 +20,7 @@ import { batchLoader, distributionRelations } from '../database/middleware';
 import { elBatchIds } from '../database/engine';
 import { batchNotes, batchOpinions, batchReports } from '../domain/stixCoreRelationship';
 import { batchCreators } from '../domain/user';
+import { STIX_SPEC_VERSION } from '../database/stix';
 
 const loadByIdLoader = batchLoader(elBatchIds);
 const notesLoader = batchLoader(batchNotes);
@@ -44,7 +45,8 @@ const stixRefRelationshipResolvers = {
     opinions: (rel, _, context) => opinionsLoader.load(rel.id, context, context.user),
     creators: (rel, _, context) => creatorsLoader.load(rel.creator_id, context, context.user),
     editContext: (rel) => fetchEditContext(rel.id),
-    datable: (rel) => isDatable(rel.fromType, rel.relationship_type)
+    datable: (rel) => isDatable(rel.fromType, rel.relationship_type),
+    spec_version: (rel) => { return rel.spec_version ?? STIX_SPEC_VERSION; }
   },
   Mutation: {
     stixRefRelationshipEdit: (_, { id }, context) => ({
