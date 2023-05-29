2d4b54533042057a6334ba23a69efdc33ce26088
sarahBocognano <sarah.bocognano@filigran.io>
2023-05-29 14:17:41 +0200

    [backend/frontend] Error when trying to add an external references with same source_name or url AND a file (#issue/3386)

--- opencti-platform/opencti-graphql/src/schema/schema-validator.ts
+++ opencti-platform/opencti-graphql/src/schema/schema-validator.ts
@@ -135,7 +135,7 @@ const validateUpdatableAttribute = (instanceType: string, input: Record<string, 
   Object.entries(input).forEach(([key]) => {
     const attribute = schemaAttributesDefinition.getAttribute(instanceType, key);
     if (attribute?.update === false) {
-      throw ValidationError(attribute, { message: `You cannot update ${attribute} attribute` });
+      throw ValidationError(attribute.name, { message: `You cannot update ${attribute.name} attribute` });
     }
   });
 };
