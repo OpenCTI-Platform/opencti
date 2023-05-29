a042a9c32ea46a02cf61a50702be33d421e3a6b8
sarahBocognano <sarah.bocognano@filigran.io>
2023-05-29 10:12:52 +0200

    [bugfix] fix test (#issue/3386)

--- opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/attribute-test.js
+++ opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/attribute-test.js
@@ -68,7 +68,7 @@ describe('Attribute resolver standard behavior', () => {
       variables: { elementType: ENTITY_TYPE_VULNERABILITY }
     });
     attributes = queryResult.data.schemaAttributes.edges.map((edgeNode) => edgeNode.node);
-    expect(attributes.length).toEqual(25);
+    expect(attributes.length).toEqual(26);
     expect(attributes.map((node) => node.value).includes('x_opencti_stix_ids')).toBeTruthy(); // Inherit attribute
     expect(attributes.map((node) => node.value).includes('revoked')).toBeTruthy(); // Inherit attribute
     expect(attributes.map((node) => node.value).includes('description')).toBeTruthy(); // Direct attribute
