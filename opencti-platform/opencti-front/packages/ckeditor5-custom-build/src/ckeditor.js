f0e499c6892440116b9c2161d8354b4e4eb35bc6
Cathia Archidoit <cathia.archidoit@filigran.io>
2023-05-31 10:30:20 +0200

    [frontend] disable autolinks in CKEditor (#3403)

--- opencti-platform/opencti-front/packages/ckeditor5-custom-build/src/ckeditor.js
+++ opencti-platform/opencti-front/packages/ckeditor5-custom-build/src/ckeditor.js
@@ -6,7 +6,6 @@ import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
 import AutoImage from '@ckeditor/ckeditor5-image/src/autoimage.js';
-import AutoLink from '@ckeditor/ckeditor5-link/src/autolink.js';
 import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
@@ -50,7 +49,6 @@ Editor.builtinPlugins = [
 	Alignment,
 	Autoformat,
 	AutoImage,
-	AutoLink,
 	Base64UploadAdapter,
 	BlockQuote,
 	Bold,
