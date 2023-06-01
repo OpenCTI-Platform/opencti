35b6b5bd436671306bd2da02a98ca48f275f4c1d
Param Constructor <132086448+ParamConstructor@users.noreply.github.com>
2023-06-01 04:09:19 -0400

    [frontend/backend] Pre-commit configuration updates

---
+++ opencti-platform/opencti-graphql/.eslintrc.js
@@ -0,0 +1,90 @@
+module.exports = {
+  plugins: [
+    '@typescript-eslint/eslint-plugin'
+  ],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
+  },
+  env: {
+    jest: true
+  },
+  ignorePatterns: [
+    '**/build/**',
+    '**/builder/**',
+    '**/coverage/**', 
+    '**/node_module/**', 
+    '**/packages/**', 
+    '**/public/**', 
+    '**/static/**',
+    '**/src/generated/**',
+    '**/src/stixpattern/**',
+    'jest.config.js', 
+    'jest.setup.js',
+    'jest.file.transform.js',
+    'jest.relay.transform.js'
+  ],
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        'js': 'never',
+        'ts': 'never'
+      }
+    ],
+    'max-len': [
+      'error',
+      180,
+      2,
+      {
+        'ignoreUrls': true,
+        'ignoreComments': false,
+        'ignoreRegExpLiterals': true,
+        'ignoreStrings': true,
+        'ignoreTemplateLiterals': true
+      }
+    ],
+    'camelcase': 'off',
+    'no-underscore-dangle': 'off',
+    'no-await-in-loop': 'off',
+    'import/no-import-module-exports': 'off',
+    'import/prefer-default-export': 'off',
+    'arrow-body-style': 'off',
+    'object-curly-newline': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/comma-dangle': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        'argsIgnorePattern': '^_',
+        'varsIgnorePattern': '^_',
+        'caughtErrorsIgnorePattern': '^_'
+      }
+    ]
+  },
+  overrides: [
+    {
+      'files': [
+        '*.js'
+      ],
+      'rules': {
+        '@typescript-eslint/no-this-alias': 'off',
+        '@typescript-eslint/return-await': 'off',
+        '@typescript-eslint/no-use-before-define': 'off'
+      }
+    }
+  ]
+}
