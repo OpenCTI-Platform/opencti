e3b37b459e31a1e22e4648f1181f5c404e7eebc9
Julien Richard <julien.richard@filigran.io>
2023-06-06 19:24:35 +0200

    [backend] List of available streams should take care of groups restrictions (#3020)

--- opencti-platform/opencti-graphql/src/domain/stream.js
+++ opencti-platform/opencti-graphql/src/domain/stream.js
@@ -15,10 +15,10 @@ import {
 import { listEntities, storeLoadById } from '../database/middleware-loader';
 import { delEditContext, notify, setEditContext } from '../database/redis';
 import { BUS_TOPICS } from '../config/conf';
-import { ABSTRACT_INTERNAL_RELATIONSHIP, BASE_TYPE_ENTITY } from '../schema/general';
+import { ABSTRACT_INTERNAL_RELATIONSHIP, BASE_TYPE_ENTITY, buildRefRelationKey } from '../schema/general';
 import { getParentTypes } from '../schema/schemaUtils';
 import { RELATION_ACCESSES_TO } from '../schema/internalRelationship';
-import { SYSTEM_USER } from '../utils/access';
+import { isUserHasCapability, SYSTEM_USER, TAXIIAPI_SETCOLLECTIONS } from '../utils/access';
 
 // Stream graphQL handlers
 export const createStreamCollection = async (context, user, input) => {
@@ -55,21 +55,22 @@ export const createGroupRelation = async (context, user, collectionId, groupId) 
   return findById(context, user, collectionId);
 };
 export const findAll = (context, user, args) => {
+  // If user is logged, list all streams where the user have access.
   if (user) {
-    return listEntities(context, user, [ENTITY_TYPE_STREAM_COLLECTION], args);
-  }
-  return listEntities(
-    context,
-    SYSTEM_USER,
-    [ENTITY_TYPE_STREAM_COLLECTION],
-    {
-      ...(args ?? {}),
-      filters: [
-        ...(args?.filters ?? []),
-        { key: ['stream_public'], values: ['true'] },
-      ],
+    // If user can manage the feeds, list everything
+    if (isUserHasCapability(user, TAXIIAPI_SETCOLLECTIONS)) {
+      return listEntities(context, user, [ENTITY_TYPE_STREAM_COLLECTION], args);
     }
-  );
+    // If user has no right to manage streams, only list the stream without groups or with correct groups
+    const userGroupIds = (user.groups ?? []).map((g) => g.id);
+    const accessFilter = { key: [buildRefRelationKey(RELATION_ACCESSES_TO)], values: [...userGroupIds, null] };
+    const userArgs = { ...(args ?? {}), filters: [...(args?.filters ?? []), accessFilter] };
+    return listEntities(context, user, [ENTITY_TYPE_STREAM_COLLECTION], userArgs);
+  }
+  // No user specify, listing only public streams
+  const publicFilter = { key: ['stream_public'], values: ['true'] };
+  const publicArgs = { ...(args ?? {}), filters: [...(args?.filters ?? []), publicFilter] };
+  return listEntities(context, SYSTEM_USER, [ENTITY_TYPE_STREAM_COLLECTION], publicArgs);
 };
 export const streamCollectionEditField = async (context, user, collectionId, input) => {
   const { element } = await updateAttribute(context, user, collectionId, ENTITY_TYPE_STREAM_COLLECTION, input);
