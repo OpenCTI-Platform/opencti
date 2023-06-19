6159d660cc1e0d11fda55bd7c47324b994a3ae45
tommychat <thomas.bennett@caminu.fr>
2023-06-19 14:50:14 +0200

    [backend/frontend] ignore generated files such as types definitions and relay schema to f

--- opencti-platform/opencti-graphql/script/script-generate-schema.js
+++ opencti-platform/opencti-graphql/script/script-generate-schema.js
@@ -1,11 +1,20 @@
 /* eslint-disable */
 // noinspection ES6UnusedImports,ES6CheckImport
 
-import fs from 'node:fs';
-import { printSchema } from 'graphql/utilities';
+import fs, {mkdir} from 'node:fs';
+import {printSchema} from 'graphql/utilities';
 import createSchema from '../src/graphql/schema';
 import _ from '../src/modules/index';
 
 const schema = createSchema();
 const printedSchema = printSchema(schema);
+
+try {
+  fs.mkdirSync('../opencti-front/src/schema/');
+} catch (error) {
+  if (!(error.message.startsWith("EEXIST"))) {
+    throw error;
+  }
+}
+
 fs.writeFileSync('../opencti-front/src/schema/relay.schema.graphql', printedSchema);
