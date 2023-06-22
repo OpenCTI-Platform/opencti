da40009931b5dcab22e5dc52e2f097606ace2e32
sarahBocognano <sarah.bocognano@filigran.io>
2023-06-22 15:04:32 +0200

    [frontend/backend] migration for renaming threat-actor(#issue/3166)

---
+++ opencti-platform/opencti-graphql/src/migrations/1687438290000-threat-actor-rename.js
@@ -0,0 +1,34 @@
+import { elUpdateByQueryForMigration } from '../database/engine';
+import { READ_INDEX_INTERNAL_OBJECTS } from '../database/utils';
+import { DatabaseError } from '../config/errors';
+import { convertTypeToStixType } from '../database/stix-converter';
+
+const entityTypeChange = (fromType, toType, indices) => {
+  const updateQuery = {
+    script: {
+      params: { toType, prefix: convertTypeToStixType(toType) },
+      source: "ctx._source.entity_type = params.toType; ctx._source.standard_id = (params.prefix + '--' + ctx._source.standard_id.splitOnToken('--')[1]);",
+    },
+    query: {
+      bool: {
+        must: [
+          { term: { 'entity_type.keyword': { value: fromType } } },
+        ],
+      },
+    },
+  };
+  const message = `[MIGRATION] Rewriting entity type from ${fromType} to ${toType}`;
+  return elUpdateByQueryForMigration(message, indices, updateQuery).catch((err) => {
+    throw DatabaseError('Error updating elastic', { error: err });
+  });
+};
+
+export const up = async (next) => {
+  // Change Threat Actor type to Threat Actor Group
+  await entityTypeChange('Threat-Actor', 'Threat-Actor-Group', READ_INDEX_INTERNAL_OBJECTS);
+  next();
+};
+
+export const down = async (next) => {
+  next();
+};
