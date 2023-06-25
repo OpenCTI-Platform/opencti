00343631023fac3da1ca3604cd25d214664ab131
Samuel Hassine <samuel.hassine@filigran.io>
2023-06-25 23:42:57 +0200

    [backend] Fix incidents date generation

--- opencti-platform/opencti-graphql/src/domain/incident.js
+++ opencti-platform/opencti-graphql/src/domain/incident.js
@@ -1,11 +1,11 @@
-import { assoc, pipe } from 'ramda';
+import { assoc, isNil, pipe } from 'ramda';
 import { createEntity, timeSeriesEntities } from '../database/middleware';
 import { listEntities, storeLoadById } from '../database/middleware-loader';
 import { BUS_TOPICS } from '../config/conf';
 import { notify } from '../database/redis';
 import { ENTITY_TYPE_INCIDENT } from '../schema/stixDomainObject';
 import { ABSTRACT_STIX_DOMAIN_OBJECT, buildRefRelationKey } from '../schema/general';
-import { now } from '../utils/format';
+import { FROM_START, UNTIL_END } from '../utils/format';
 
 export const findById = (context, user, incidentId) => {
   return storeLoadById(context, user, incidentId, ENTITY_TYPE_INCIDENT);
@@ -28,10 +28,9 @@ export const incidentsTimeSeries = (context, user, args) => {
 // endregion
 
 export const addIncident = async (context, user, incident) => {
-  const currentDate = now();
   const incidentToCreate = pipe(
-    assoc('first_seen', incident.first_seen ? incident.first_seen : currentDate),
-    assoc('last_seen', incident.last_seen ? incident.last_seen : currentDate)
+    assoc('first_seen', isNil(incident.first_seen) ? new Date(FROM_START) : incident.first_seen),
+    assoc('last_seen', isNil(incident.last_seen) ? new Date(UNTIL_END) : incident.last_seen)
   )(incident);
   const created = await createEntity(context, user, incidentToCreate, ENTITY_TYPE_INCIDENT);
   return notify(BUS_TOPICS[ABSTRACT_STIX_DOMAIN_OBJECT].ADDED_TOPIC, created, user);
--- opencti-platform/opencti-graphql/src/domain/threatActor.js
+++ opencti-platform/opencti-graphql/src/domain/threatActor.js
@@ -1,3 +1,4 @@
+import { assoc, isNil, pipe } from 'ramda';
 import { createEntity, batchListThroughGetFrom } from '../database/middleware';
 import { listEntities, storeLoadById } from '../database/middleware-loader';
 import { BUS_TOPICS } from '../config/conf';
@@ -5,6 +6,7 @@ import { notify } from '../database/redis';
 import { ENTITY_TYPE_LOCATION_COUNTRY, ENTITY_TYPE_THREAT_ACTOR } from '../schema/stixDomainObject';
 import { ABSTRACT_STIX_DOMAIN_OBJECT, ENTITY_TYPE_LOCATION } from '../schema/general';
 import { RELATION_ORIGINATES_FROM } from '../schema/stixCoreRelationship';
+import { FROM_START, UNTIL_END } from '../utils/format';
 
 export const findById = (context, user, threatActorId) => {
   return storeLoadById(context, user, threatActorId, ENTITY_TYPE_THREAT_ACTOR);
@@ -15,7 +17,11 @@ export const findAll = (context, user, args) => {
 };
 
 export const addThreatActor = async (context, user, threatActor) => {
-  const created = await createEntity(context, user, threatActor, ENTITY_TYPE_THREAT_ACTOR);
+  const threatActorToCreate = pipe(
+    assoc('first_seen', isNil(threatActor.first_seen) ? new Date(FROM_START) : threatActor.first_seen),
+    assoc('last_seen', isNil(threatActor.last_seen) ? new Date(UNTIL_END) : threatActor.last_seen)
+  )(threatActor);
+  const created = await createEntity(context, user, threatActorToCreate, ENTITY_TYPE_THREAT_ACTOR);
   return notify(BUS_TOPICS[ABSTRACT_STIX_DOMAIN_OBJECT].ADDED_TOPIC, created, user);
 };
 
