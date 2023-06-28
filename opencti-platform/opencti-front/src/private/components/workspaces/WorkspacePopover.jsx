59817cd3adc67f438b117e716ff26215d672211e
Samuel Hassine <samuel.hassine@filigran.io>
2023-06-28 12:54:44 +0200

    [frontend] Fix deepscan

--- opencti-platform/opencti-front/src/private/components/workspaces/WorkspacePopover.jsx
+++ opencti-platform/opencti-front/src/private/components/workspaces/WorkspacePopover.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Button from '@mui/material/Button';
