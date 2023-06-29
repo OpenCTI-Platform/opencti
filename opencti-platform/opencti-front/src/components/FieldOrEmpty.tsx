c67ce3a42583148db6edbebdbd7e59ea9f15621e
Yassine Ouaamou <yassine.ouaamou@filigran.io>
2023-06-29 14:32:20 +0200

    [frontend] apply eslint-disable only on line

--- opencti-platform/opencti-front/src/components/FieldOrEmpty.tsx
+++ opencti-platform/opencti-front/src/components/FieldOrEmpty.tsx
@@ -1,9 +1,10 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-// Had to use any to allow the component to accept arrays of any type as source
 import React, { FunctionComponent } from 'react';
 import { isNotEmptyField } from '../utils/utils';
 
 interface FieldOrEmptyProps {
+  // Had to use 'any' to allow the component to accept arrays of any type as a source
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+
   source: string | null | ReadonlyArray<any>;
   children: React.ReactNode;
 }
