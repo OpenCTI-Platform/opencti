7251d9dffb6256b23d82d16abf0fd4e4b6a569b0
sarahBocognano <sarah.bocognano@filigran.io>
2023-06-30 12:16:14 +0200

    [backend] fix migration for relation and entity_type wip(#issue/3166)

--- opencti-platform/opencti-graphql/src/migrations/1688117986361-threat-actor-rename.js
+++ opencti-platform/opencti-graphql/src/migrations/1688117986361-threat-actor-rename.js
@@ -1,12 +1,17 @@
 import { elUpdateByQueryForMigration } from '../database/engine';
-import { READ_INDEX_STIX_DOMAIN_OBJECTS, READ_INDEX_STIX_META_OBJECTS } from '../database/utils';
+import {
+  READ_INDEX_INTERNAL_OBJECTS, READ_INDEX_INTERNAL_RELATIONSHIPS,
+  READ_INDEX_STIX_CORE_RELATIONSHIPS, READ_INDEX_STIX_CYBER_OBSERVABLE_RELATIONSHIPS,
+  READ_INDEX_STIX_DOMAIN_OBJECTS, READ_INDEX_STIX_META_RELATIONSHIPS,
+  READ_INDEX_STIX_SIGHTING_RELATIONSHIPS
+} from '../database/utils';
 import { DatabaseError } from '../config/errors';
 
 const entityTypeChange = (fromType, toType, indices) => {
   const updateQuery = {
     script: {
       params: { toType },
-      source: 'ctx._source.entity_type = params.toType;',
+      source: 'ctx._source.entity_type = params.toType; ctx._source.fromType = params.toType; ctx_source.target_type = params.toType',
     },
     query: {
       bool: {
@@ -24,7 +29,7 @@ const entityTypeChange = (fromType, toType, indices) => {
 
 export const up = async (next) => {
   // Change Threat Actor type to Threat Actor Group
-  await entityTypeChange('Threat-Actor', 'Threat-Actor-Group', READ_INDEX_STIX_META_OBJECTS, READ_INDEX_STIX_DOMAIN_OBJECTS);
+  await entityTypeChange('Threat-Actor', 'Threat-Actor-Group', READ_INDEX_INTERNAL_OBJECTS, READ_INDEX_STIX_DOMAIN_OBJECTS, READ_INDEX_STIX_CORE_RELATIONSHIPS, READ_INDEX_INTERNAL_RELATIONSHIPS, READ_INDEX_STIX_SIGHTING_RELATIONSHIPS, READ_INDEX_STIX_CYBER_OBSERVABLE_RELATIONSHIPS, READ_INDEX_STIX_META_RELATIONSHIPS);
   next();
 };
 
