c573619a5a99237353f452d9072e78ef804ace71
Yassine Ouaamou <yassine.ouaamou@filigran.io>
2023-06-30 09:17:11 +0200

    [frontend] rebase

--- opencti-platform/opencti-front/src/components/FieldOrEmpty.tsx
+++ opencti-platform/opencti-front/src/components/FieldOrEmpty.tsx
@@ -4,7 +4,6 @@ import { isNotEmptyField } from '../utils/utils';
 interface FieldOrEmptyProps {
   // Had to use 'any' to allow the component to accept arrays of any type as a source
   /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-
   source: string | null | ReadonlyArray<any>;
   children: React.ReactNode;
 }
