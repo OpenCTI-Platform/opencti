315dddd5f4bc7a2d6147b3cf8124f3fa4e7a2e6b
sarahBocognano <sarah.bocognano@filigran.io>
2023-07-03 17:23:52 +0200

    [backend] migration to divide threat actors (#issue/3166bb)

--- opencti-platform/opencti-graphql/src/migrations/1688404932000-threat-actors-divider.js
+++ opencti-platform/opencti-graphql/src/migrations/1688404932000-threat-actors-divider.js
@@ -2,7 +2,7 @@ import { elUpdateByQueryForMigration } from '../database/engine';
 import { READ_INDEX_STIX_DOMAIN_OBJECTS } from '../database/utils';
 import { DatabaseError } from '../config/errors';
 
-const splitThreatActorsByCategory = async (indices) => {
+const splitThreatActorsByCategory = async (toType, fromType, indices) => {
   const updateIndividualQuery = {
     script: {
       params: { toType: 'Threat-Actor-Individual' },
@@ -13,20 +13,23 @@ const splitThreatActorsByCategory = async (indices) => {
       `,
     },
     query: {
-      term: { 'entity_type.keyword': { value: 'Threat-Actor-Group' } },
+      bool: {
+        should: [
+          { term: { 'resource_level.keyword': { value: 'individual' } } },
+        ],
+        minimum_should_match: 1
+      },
     },
   };
 
   const message = '[MIGRATION] Splitting Threat-Actor into Threat-Actor-Group and Threat-Actor-Individual';
-  try {
-    await elUpdateByQueryForMigration(message, indices, updateIndividualQuery);
-  } catch (err) {
-    throw new DatabaseError('Error updating elastic', { error: err });
-  }
+  return elUpdateByQueryForMigration(message, indices, updateIndividualQuery).catch((err) => {
+    throw DatabaseError('Error updating elastic', { error: err });
+  });
 };
 
 export const up = async (next) => {
-  await splitThreatActorsByCategory(READ_INDEX_STIX_DOMAIN_OBJECTS);
+  await splitThreatActorsByCategory('Threat-Actor-Individual', 'Threat-Actor-Group', READ_INDEX_STIX_DOMAIN_OBJECTS);
   next();
 };
 
