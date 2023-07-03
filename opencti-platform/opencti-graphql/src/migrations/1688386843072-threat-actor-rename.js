8a347880e3d36f86d103bde63f110f7a0cf4e184
sarahBocognano <sarah.bocognano@filigran.io>
2023-07-03 14:35:36 +0200

    [backend] migration fix (#issue/3166)

---
+++ opencti-platform/opencti-graphql/src/migrations/1688386843072-threat-actor-rename.js
@@ -0,0 +1,87 @@
+import { elUpdateByQueryForMigration } from '../database/engine';
+import {
+  READ_INDEX_INTERNAL_OBJECTS, READ_INDEX_INTERNAL_RELATIONSHIPS,
+  READ_INDEX_STIX_CORE_RELATIONSHIPS, READ_INDEX_STIX_CYBER_OBSERVABLE_RELATIONSHIPS,
+  READ_INDEX_STIX_DOMAIN_OBJECTS, READ_INDEX_STIX_META_RELATIONSHIPS,
+  READ_INDEX_STIX_SIGHTING_RELATIONSHIPS
+} from '../database/utils';
+import { DatabaseError } from '../config/errors';
+
+const entityTypeChange = (fromType, toType, indices) => {
+  const updateQuery = {
+    script: {
+      params: { toType },
+      source: 'ctx._source.entity_type = params.toType;'
+    },
+    query: {
+      bool: {
+        should: [
+          { term: { 'entity_type.keyword': { value: fromType } } },
+        ],
+        minimum_should_match: 1
+      },
+    },
+  };
+  const message = `[MIGRATION] Rewriting entity type from ${fromType} to ${toType}`;
+  return elUpdateByQueryForMigration(message, indices, updateQuery).catch((err) => {
+    throw DatabaseError('Error updating elastic', { error: err });
+  });
+};
+
+const targetTypeChange = (fromType, toType, indices) => {
+  const updateQuery = {
+    script: {
+      params: { toType },
+      source: 'ctx._source.target_type = params.toType;'
+    },
+    query: {
+      bool: {
+        should: [
+          { term: { 'target_type.keyword': { value: fromType } } }
+        ],
+        minimum_should_match: 1
+      },
+    },
+  };
+  const message = `[MIGRATION] Rewriting target type from ${fromType} to ${toType}`;
+  return elUpdateByQueryForMigration(message, indices, updateQuery).catch((err) => {
+    throw DatabaseError('Error updating elastic', { error: err });
+  });
+};
+
+const relationshipTypeChange = (fromType, toType, indices) => {
+  const updateQuery = {
+    script: {
+      params: { toType },
+      source: 'for(def connection : ctx._source.connections) {'
+        + 'if (connection.types.contains("Threat-Actor")) { '
+        + 'connection.types = params.toType; '
+        + '} } '
+        + 'ctx._source.fromType = params.toType'
+    },
+    query: {
+      bool: {
+        should: [
+          { term: { 'fromType.keyword': { value: fromType } } }
+        ],
+        minimum_should_match: 1
+      },
+    },
+  };
+  const message = `[MIGRATION] Rewriting relationship types from ${fromType} to ${toType}`;
+  return elUpdateByQueryForMigration(message, indices, updateQuery).catch((err) => {
+    throw DatabaseError('Error updating elastic', { error: err });
+  });
+};
+
+export const up = async (next) => {
+  // Change Threat Actor type to Threat Actor Group
+  await entityTypeChange('Threat-Actor', 'Threat-Actor-Group', READ_INDEX_STIX_DOMAIN_OBJECTS);
+  await targetTypeChange('Threat-Actor', 'Threat-Actor-Group', READ_INDEX_INTERNAL_OBJECTS, READ_INDEX_INTERNAL_RELATIONSHIPS);
+  await relationshipTypeChange('Threat-Actor', 'Threat-Actor-Group', READ_INDEX_STIX_CORE_RELATIONSHIPS, READ_INDEX_STIX_SIGHTING_RELATIONSHIPS, READ_INDEX_STIX_CYBER_OBSERVABLE_RELATIONSHIPS, READ_INDEX_STIX_META_RELATIONSHIPS);
+  next();
+};
+
+export const down = async (next) => {
+  next();
+};
