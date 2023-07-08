159698219c017300796a9b380e4cf7988911c6a6
Samuel Hassine <samuel.hassine@filigran.io>
2023-07-08 15:12:42 +0200

    Rebasing

--- opencti-platform/opencti-front/src/private/components/settings/users/MembersListContainerForGroup.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/users/MembersListContainerForGroup.tsx
@@ -7,7 +7,9 @@ import { PreloadedQuery } from 'react-relay';
 import { Theme } from '../../../../components/Theme';
 import { useFormatter } from '../../../../components/i18n';
 import useQueryLoading from '../../../../utils/hooks/useQueryLoading';
-import MembersListForGroup, { membersListForGroupQuery } from './MembersListForGroup';
+import MembersListForGroup, {
+  membersListForGroupQuery,
+} from './MembersListForGroup';
 import SearchInput from '../../../../components/SearchInput';
 import { usePaginationLocalStorage } from '../../../../utils/hooks/useLocalStorage';
 import Loader, { LoaderVariant } from '../../../../components/Loader';
@@ -31,12 +33,17 @@ interface MembersListContainerProps {
   groupId: string;
 }
 
-const MembersListContainerForGroup: FunctionComponent<MembersListContainerProps> = ({ groupId }) => {
+const MembersListContainerForGroup: FunctionComponent<
+MembersListContainerProps
+> = ({ groupId }) => {
   const classes = useStyles();
   const { t } = useFormatter();
-
-  const { viewStorage, helpers, paginationOptions: paginationOptionsFromStorage } = usePaginationLocalStorage<MembersListForGroupQuery$variables>(
-    `view-${groupId}-members`,
+  const {
+    viewStorage,
+    helpers,
+    paginationOptions: paginationOptionsFromStorage,
+  } = usePaginationLocalStorage<MembersListForGroupQuery$variables>(
+    `view-group-${groupId}-members`,
     {
       id: groupId,
       searchTerm: '',
@@ -49,7 +56,7 @@ const MembersListContainerForGroup: FunctionComponent<MembersListContainerProps>
       },
     },
     undefined,
-    false,
+    true,
   );
   const { searchTerm, sortBy, orderAsc } = viewStorage;
   const paginationOptions = {
@@ -110,10 +117,7 @@ const MembersListContainerForGroup: FunctionComponent<MembersListContainerProps>
           keyword={searchTerm}
         />
       </div>
-      <Paper
-        classes={{ root: classes.paper }}
-        variant="outlined"
-      >
+      <Paper classes={{ root: classes.paper }} variant="outlined">
         <ColumnsLinesTitles
           dataColumns={userColumns}
           sortBy={sortBy}
@@ -126,7 +130,9 @@ const MembersListContainerForGroup: FunctionComponent<MembersListContainerProps>
           >
             <MembersListForGroup
               userColumns={userColumns}
-              queryRef={membersQueryRef as PreloadedQuery<MembersListForGroupQuery>}
+              queryRef={
+                membersQueryRef as PreloadedQuery<MembersListForGroupQuery>
+              }
               paginationOptions={paginationOptions}
             />
           </React.Suspense>
--- opencti-platform/opencti-front/src/private/components/settings/users/MembersListContainerForOrganization.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/users/MembersListContainerForOrganization.tsx
@@ -15,7 +15,9 @@ import {
   MembersListForOrganizationQuery,
   MembersListForOrganizationQuery$variables,
 } from './__generated__/MembersListForOrganizationQuery.graphql';
-import MembersListForOrganization, { membersListForOrganizationQuery } from './MembersListForOrganization';
+import MembersListForOrganization, {
+  membersListForOrganizationQuery,
+} from './MembersListForOrganization';
 
 const useStyles = makeStyles<Theme>(() => ({
   paper: {
@@ -31,12 +33,17 @@ interface MembersListContainerProps {
   organizationId: string;
 }
 
-const MembersListContainerForOrganization: FunctionComponent<MembersListContainerProps> = ({ organizationId }) => {
+const MembersListContainerForOrganization: FunctionComponent<
+MembersListContainerProps
+> = ({ organizationId }) => {
   const classes = useStyles();
   const { t } = useFormatter();
-
-  const { viewStorage, helpers, paginationOptions: paginationOptionsFromStorage } = usePaginationLocalStorage<MembersListForOrganizationQuery$variables>(
-    `view-${organizationId}-members`,
+  const {
+    viewStorage,
+    helpers,
+    paginationOptions: paginationOptionsFromStorage,
+  } = usePaginationLocalStorage<MembersListForOrganizationQuery$variables>(
+    `view-organization-${organizationId}-members`,
     {
       id: organizationId,
       searchTerm: '',
@@ -49,7 +56,7 @@ const MembersListContainerForOrganization: FunctionComponent<MembersListContaine
       },
     },
     undefined,
-    false,
+    true,
   );
   const { searchTerm, sortBy, orderAsc } = viewStorage;
   const paginationOptions = {
@@ -110,10 +117,7 @@ const MembersListContainerForOrganization: FunctionComponent<MembersListContaine
           keyword={searchTerm}
         />
       </div>
-      <Paper
-        classes={{ root: classes.paper }}
-        variant="outlined"
-      >
+      <Paper classes={{ root: classes.paper }} variant="outlined">
         <ColumnsLinesTitles
           dataColumns={userColumns}
           sortBy={sortBy}
@@ -126,7 +130,9 @@ const MembersListContainerForOrganization: FunctionComponent<MembersListContaine
           >
             <MembersListForOrganization
               userColumns={userColumns}
-              queryRef={membersQueryRef as PreloadedQuery<MembersListForOrganizationQuery>}
+              queryRef={
+                membersQueryRef as PreloadedQuery<MembersListForOrganizationQuery>
+              }
               paginationOptions={paginationOptions}
             />
           </React.Suspense>
