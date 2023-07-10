5f84512a984bef74715c7b5d1c4a3e031520824c
Filigran Automation <automation@filigran.io>
2023-07-10 16:02:32 +0000

    [client] Release 5.9.0
    (original commit: OpenCTI-Platform/client-python@8add394)

--- pycti/__init__.py
+++ pycti/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "5.8.7"
+__version__ = "5.9.0"
 
 from .api.opencti_api_client import OpenCTIApiClient
 from .api.opencti_api_connector import OpenCTIApiConnector
