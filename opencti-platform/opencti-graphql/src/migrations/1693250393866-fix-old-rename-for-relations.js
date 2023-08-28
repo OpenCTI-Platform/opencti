ea96c415215b8105f8eed101b928fe1d07ff8c69
Souad Hadjiat <souad.hadjiat@filigran.io>
2023-08-28 22:25:47 +0200

    [backend] add missing migration for renamed entity types (#4156)

---
+++ opencti-platform/opencti-graphql/src/migrations/1693250393866-fix-old-rename-for-relations.js
@@ -0,0 +1,60 @@
+import { logApp } from '../config/conf';
+import { elUpdateByQueryForMigration } from '../database/engine';
+import { READ_RELATIONSHIPS_INDICES } from '../database/utils';
+
+const message = '[MIGRATION] Fix old rename for relations';
+
+export const up = async (next) => {
+  logApp.info(`${message} > started`);
+  const types = [
+    { source: 'X-OpenCTI-Cryptographic-Key', destination: 'Cryptographic-Key' },
+    { source: 'X-OpenCTI-Cryptocurrency-Wallet', destination: 'Cryptocurrency-Wallet' },
+    { source: 'X-OpenCTI-Hostname', destination: 'Hostname' },
+    { source: 'X-OpenCTI-Text', destination: 'Text' },
+    { source: 'X-OpenCTI-User-Agent', destination: 'User-Agent' },
+  ];
+  const scriptSource = `
+    if (ctx._source.fromType == params.type) {
+      ctx._source.fromType = params.target;
+    }
+    if (ctx._source.toType == params.type) {
+      ctx._source.toType = params.target;
+    }
+    for(connection in ctx._source.connections) {
+      if (connection.types.contains(params.type)) {
+        connection.types.remove(params.type);
+        connection.types.add(params.target)
+      }
+    }
+  `;
+  for (let index = 0; index < types.length; index += 1) {
+    const { source, destination } = types[index];
+    const updateQuery = {
+      script: {
+        params: { type: source, target: destination },
+        source: scriptSource,
+      },
+      query: {
+        nested: {
+          path: 'connections',
+          query: {
+            term: {
+              'connections.types.keyword': source,
+            },
+          },
+        },
+      },
+    };
+    await elUpdateByQueryForMigration(
+      `Rename entity type ${source} to ${destination}`,
+      READ_RELATIONSHIPS_INDICES,
+      updateQuery
+    );
+  }
+  logApp.info(`${message} > done`);
+  next();
+};
+
+export const down = async (next) => {
+  next();
+};
