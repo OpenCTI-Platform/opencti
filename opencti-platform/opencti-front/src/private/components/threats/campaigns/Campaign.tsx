ef7f10b38754261bb26125a6ab6daa98973ef9a8
Samuel Hassine <samuel.hassine@filigran.io>
2023-09-23 12:29:32 +0200

    [frontend] Fix linting on campaigns

--- opencti-platform/opencti-front/src/private/components/threats/campaigns/Campaign.tsx
+++ opencti-platform/opencti-front/src/private/components/threats/campaigns/Campaign.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { graphql, useFragment } from "react-relay";
-import Grid from "@mui/material/Grid";
-import { makeStyles } from "@mui/styles";
-import CampaignDetails from "./CampaignDetails";
-import CampaignEdition from "./CampaignEdition";
-import CampaignPopover from "./CampaignPopover";
-import StixDomainObjectHeader from "../../common/stix_domain_objects/StixDomainObjectHeader";
-import Security from "../../../../utils/Security";
-import { KNOWLEDGE_KNUPDATE } from "../../../../utils/hooks/useGranted";
-import StixCoreObjectOrStixCoreRelationshipNotes from "../../analyses/notes/StixCoreObjectOrStixCoreRelationshipNotes";
-import StixDomainObjectOverview from "../../common/stix_domain_objects/StixDomainObjectOverview";
-import StixCoreObjectExternalReferences from "../../analyses/external_references/StixCoreObjectExternalReferences";
-import StixCoreObjectLatestHistory from "../../common/stix_core_objects/StixCoreObjectLatestHistory";
-import SimpleStixObjectOrStixRelationshipStixCoreRelationships from "../../common/stix_core_relationships/SimpleStixObjectOrStixRelationshipStixCoreRelationships";
-import { Campaign_campaign$key } from "./__generated__/Campaign_campaign.graphql";
-import StixCoreObjectOrStixRelationshipLastContainers from "../../common/containers/StixCoreObjectOrStixRelationshipLastContainers";
+import React from 'react';
+import { graphql, useFragment } from 'react-relay';
+import Grid from '@mui/material/Grid';
+import { makeStyles } from '@mui/styles';
+import CampaignDetails from './CampaignDetails';
+import CampaignEdition from './CampaignEdition';
+import CampaignPopover from './CampaignPopover';
+import StixDomainObjectHeader from '../../common/stix_domain_objects/StixDomainObjectHeader';
+import Security from '../../../../utils/Security';
+import { KNOWLEDGE_KNUPDATE } from '../../../../utils/hooks/useGranted';
+import StixCoreObjectOrStixCoreRelationshipNotes from '../../analyses/notes/StixCoreObjectOrStixCoreRelationshipNotes';
+import StixDomainObjectOverview from '../../common/stix_domain_objects/StixDomainObjectOverview';
+import StixCoreObjectExternalReferences from '../../analyses/external_references/StixCoreObjectExternalReferences';
+import StixCoreObjectLatestHistory from '../../common/stix_core_objects/StixCoreObjectLatestHistory';
+import SimpleStixObjectOrStixRelationshipStixCoreRelationships from '../../common/stix_core_relationships/SimpleStixObjectOrStixRelationshipStixCoreRelationships';
+import { Campaign_campaign$key } from './__generated__/Campaign_campaign.graphql';
+import StixCoreObjectOrStixRelationshipLastContainers from '../../common/containers/StixCoreObjectOrStixRelationshipLastContainers';
 
 const useStyles = makeStyles(() => ({
   container: {
@@ -96,7 +96,7 @@ const CampaignComponent = ({
   return (
     <div className={classes.container}>
       <StixDomainObjectHeader
-        entityType={"Campaign"}
+        entityType={'Campaign'}
         stixDomainObject={campaignData}
         PopoverComponent={<CampaignPopover />}
         enableQuickSubscription
