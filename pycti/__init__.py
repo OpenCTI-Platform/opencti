6e4844358c0b20b93771e8818cec10b562229b7e
Filigran Automation <automation@filigran.io>
2023-09-25 01:13:38 +0000

    [client] Release 5.10.3

--- pycti/__init__.py
+++ pycti/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "5.10.2"
+__version__ = "5.10.3"
 
 from .api.opencti_api_client import OpenCTIApiClient
 from .api.opencti_api_connector import OpenCTIApiConnector
