77e6ce3f7051fb3a5b5ee5d23b7d370d52686177
Yassine Ouaamou <yassine.ouaamou@filigran.io>
2023-10-04 16:49:40 +0200

    [backend/frontend] give access to edit user to orga Admin

--- opencti-platform/opencti-front/src/private/components/settings/users/UserEditionOverview.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/users/UserEditionOverview.tsx
@@ -16,6 +16,7 @@ import { UserEditionOverview_user$data } from './__generated__/UserEditionOvervi
 import DateTimePickerField from '../../../../components/DateTimePickerField';
 import { fieldSpacingContainerStyle } from '../../../../utils/field';
 import useAuth from '../../../../utils/hooks/useAuth';
+import { BYPASS, ORGA_ADMIN, SETTINGS_SETACCESSES } from '../../../../utils/hooks/useGranted';
 
 const userMutationFieldPatch = graphql`
   mutation UserEditionOverviewFieldPatchMutation(
@@ -85,17 +86,22 @@ interface UserEditionOverviewComponentProps {
   } | null)[]
   | null;
 }
+const isOnlyOrganizationAdmin = (user) => {
+  const userCapabilities = user.capabilities.map((n) => n.name);
+  return userCapabilities.includes(ORGA_ADMIN) && !userCapabilities.includes(BYPASS) && !userCapabilities.includes(SETTINGS_SETACCESSES);
+};
 
 const UserEditionOverviewComponent: FunctionComponent<
 UserEditionOverviewComponentProps
 > = ({ user, context }) => {
   const { t } = useFormatter();
-  const { settings } = useAuth();
+  const { settings, me } = useAuth();
   const [commitFocus] = useMutation(userEditionOverviewFocus);
   const [commitFieldPatch] = useMutation(userMutationFieldPatch);
   const [commitGroupAdd] = useMutation(userMutationGroupAdd);
   const [commitGroupDelete] = useMutation(userMutationGroupDelete);
 
+  const userIsOnlyOrganizationAdmin = isOnlyOrganizationAdmin(me)
   const external = user.external === true;
   const objectOrganization = convertOrganizations(user);
 
@@ -269,6 +275,7 @@ UserEditionOverviewComponentProps
           <ObjectOrganizationField
             name="objectOrganization"
             label="Organizations"
+            disabled={userIsOnlyOrganizationAdmin}
             onChange={handleChangeObjectOrganization}
             style={fieldSpacingContainerStyle}
             outlined={false}
