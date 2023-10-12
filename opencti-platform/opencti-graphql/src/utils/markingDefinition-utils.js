4515a53408933ee6ea20cd014adfdb1dfcc23b1e
Helene Nguyen <helene.nguyen@filigran.io>
2023-10-12 17:27:45 +0200

    [backend] fix PR review change name in clean markings

--- opencti-platform/opencti-graphql/src/utils/markingDefinition-utils.js
+++ opencti-platform/opencti-graphql/src/utils/markingDefinition-utils.js
@@ -8,9 +8,9 @@ export const cleanMarkings = async (context, values) => {
   const markingsMap = await getEntitiesMapFromCache(context, SYSTEM_USER, ENTITY_TYPE_MARKING_DEFINITION);
   const defaultMarkingValues = values?.map((d) => markingsMap.get(d) ?? d) ?? [];
   const defaultGroupedMarkings = R.groupBy((m) => m.definition_type, defaultMarkingValues);
-  return Object.entries(defaultGroupedMarkings).map(([_, key]) => {
-    const max = Math.max(...key.map((m) => m.x_opencti_order));
-    const results = key.filter((m) => m.x_opencti_order === max);
+  return Object.entries(defaultGroupedMarkings).map(([_, markingValues]) => {
+    const max = Math.max(...markingValues.map((m) => m.x_opencti_order));
+    const results = markingValues.filter((m) => m.x_opencti_order === max);
     return R.uniqWith((a, b) => a.id === b.id, results);
   }).flat();
 };
