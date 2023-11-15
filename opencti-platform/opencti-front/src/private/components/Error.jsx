c6f228e9dc1d0a0883c126ef0624544fa3a20c37
Landry Trebon <landry.trebon@filigran.io>
2023-11-15 14:21:23 +0100

    fix type

--- opencti-platform/opencti-front/src/private/components/Error.jsx
+++ opencti-platform/opencti-front/src/private/components/Error.jsx
@@ -86,7 +86,7 @@ export const BoundaryRoute = (props) => {
 };
 
 BoundaryRoute.propTypes = {
-  component: PropTypes.object,
+  component: PropTypes.func,
   display: PropTypes.object,
   exact: PropTypes.bool,
   path: PropTypes.string,
