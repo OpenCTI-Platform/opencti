599f5e91283d02fca6a28cba1c0ef3c55062480d
Cathia Archidoit <cathia.archidoit@filigran.io>
2023-11-22 14:10:54 +0100

    [backend] deepscan (#2686)

--- opencti-platform/opencti-graphql/src/utils/filtering/filtering-activity-event/activity-event-testers.ts
+++ opencti-platform/opencti-graphql/src/utils/filtering/filtering-activity-event/activity-event-testers.ts
@@ -50,7 +50,6 @@ export const testMembersOrganization = (event: any, filter: Filter) => {
  */
 export const testActivityStatuses = (event: any, filter: Filter) => {
   const value = event.status;
-  console.log('value', value);
   return testStringFilter(filter, [value]);
 };
 
