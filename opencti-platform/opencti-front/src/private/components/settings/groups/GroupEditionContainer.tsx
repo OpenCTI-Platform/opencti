0399892524d8b7d11a3d9ccf91b48e88c3e37ee5
Adrien Servel <adrien.servel@filigran.io>
2023-11-28 16:55:31 +0100

    [frontend] Fix group member edition

--- opencti-platform/opencti-front/src/private/components/settings/groups/GroupEditionContainer.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/groups/GroupEditionContainer.tsx
@@ -4,6 +4,7 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import { graphql, PreloadedQuery, useFragment, usePreloadedQuery } from 'react-relay';
 import Drawer, { DrawerVariant } from '@components/common/drawer/Drawer';
+import { GroupUsersLinesQuery$variables } from '@components/settings/users/__generated__/GroupUsersLinesQuery.graphql';
 import GroupEditionOverview from './GroupEditionOverview';
 import Loader, { LoaderVariant } from '../../../../components/Loader';
 import GroupEditionRoles, { groupEditionRolesLinesSearchQuery } from './GroupEditionRoles';
@@ -13,8 +14,9 @@ import useQueryLoading from '../../../../utils/hooks/useQueryLoading';
 import { GroupEditionRolesLinesSearchQuery } from './__generated__/GroupEditionRolesLinesSearchQuery.graphql';
 import { GroupEditionContainerQuery } from './__generated__/GroupEditionContainerQuery.graphql';
 import { GroupEditionContainer_group$key } from './__generated__/GroupEditionContainer_group.graphql';
-import ErrorNotFound from '../../../../components/ErrorNotFound';
 import GroupEditionMarkings from './GroupEditionMarkings';
+import { usePaginationLocalStorage } from '../../../../utils/hooks/useLocalStorage';
+import ErrorNotFound from '../../../../components/ErrorNotFound';
 
 export const groupEditionContainerQuery = graphql`
   query GroupEditionContainerQuery($id: String!) {
@@ -31,9 +33,16 @@ const GroupEditionContainerFragment = graphql`
     rolesOrderMode: { type: "OrderingMode", defaultValue: asc }
   ) {
     id
+    members {
+      edges {
+        node {
+          id
+          name
+        }
+      }
+    }
     ...GroupEditionOverview_group
     ...GroupEditionMarkings_group
-    ...GroupEditionUsers_group
     ...GroupEditionRoles_group
     @arguments(
       orderBy: $rolesOrderBy
@@ -52,51 +61,60 @@ interface GroupEditionContainerProps {
   open?: boolean
 }
 
-const GroupEditionContainer: FunctionComponent<GroupEditionContainerProps> = ({ groupQueryRef, handleClose = () => {}, open }) => {
+const GroupEditionContainer: FunctionComponent<GroupEditionContainerProps> = ({
+  groupQueryRef, handleClose = () => {
+  }, open,
+}) => {
   const { t } = useFormatter();
+
   const [currentTab, setTab] = useState(0);
+
   const groupData = usePreloadedQuery<GroupEditionContainerQuery>(groupEditionContainerQuery, groupQueryRef);
   const roleQueryRef = useQueryLoading<GroupEditionRolesLinesSearchQuery>(groupEditionRolesLinesSearchQuery);
-  if (groupData.group) {
-    const group = useFragment<GroupEditionContainer_group$key>(
-      GroupEditionContainerFragment,
-      groupData.group,
-    );
-    const { editContext } = group;
-    return (
-      <Drawer
-        title={t('Update a group')}
-        variant={open == null ? DrawerVariant.updateWithPanel : undefined}
-        context={editContext}
-        onClose={handleClose}
-        open={open}
-      >
-        <>
-          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <Tabs value={currentTab} onChange={(event, value) => setTab(value)}>
-              <Tab label={t('Overview')} />
-              <Tab label={t('Roles')} />
-              <Tab label={t('Markings')} />
-              <Tab label={t('Members')} />
-            </Tabs>
-          </Box>
-          {currentTab === 0 && (
-            <GroupEditionOverview group={group} context={editContext} />
-          )}
-          {currentTab === 1 && roleQueryRef && (
-            <React.Suspense
-              fallback={<Loader variant={LoaderVariant.inElement} />}
-            >
-              <GroupEditionRoles group={group} queryRef={roleQueryRef} />
-            </React.Suspense>
-          )}
-          {currentTab === 2 && <GroupEditionMarkings group={group} />}
-          {currentTab === 3 && <GroupEditionUsers group={group} />}
-        </>
-      </Drawer>
-    );
+  const group = useFragment<GroupEditionContainer_group$key>(
+    GroupEditionContainerFragment,
+    groupData.group,
+  );
+
+  const { paginationOptions } = usePaginationLocalStorage<GroupUsersLinesQuery$variables>(`group-${group?.id}-users`, {});
+
+  if (!group) {
+    return <ErrorNotFound />;
   }
-  return <ErrorNotFound />;
+
+  const { editContext } = group;
+  return (
+    <Drawer
+      title={t('Update a group')}
+      variant={open == null ? DrawerVariant.updateWithPanel : undefined}
+      context={editContext}
+      onClose={handleClose}
+      open={open}
+    >
+      <>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={currentTab} onChange={(event, value) => setTab(value)}>
+            <Tab label={t('Overview')} />
+            <Tab label={t('Roles')} />
+            <Tab label={t('Markings')} />
+            <Tab label={t('Members')} />
+          </Tabs>
+        </Box>
+        {currentTab === 0 && (
+          <GroupEditionOverview group={group} context={editContext} />
+        )}
+        {currentTab === 1 && roleQueryRef && (
+          <React.Suspense
+            fallback={<Loader variant={LoaderVariant.inElement} />}
+          >
+            <GroupEditionRoles group={group} queryRef={roleQueryRef} />
+          </React.Suspense>
+        )}
+        {currentTab === 2 && <GroupEditionMarkings group={group} />}
+        {currentTab === 3 && <GroupEditionUsers group={group} paginationOptions={paginationOptions} />}
+      </>
+    </Drawer>
+  );
 };
 
 export default GroupEditionContainer;
