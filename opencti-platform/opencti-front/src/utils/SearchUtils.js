8f6f9cf200f91b11b1226447f3d3ac4c1ac28142
Adrien Servel <50013774+Kedae@users.noreply.github.com>
2023-12-02 18:08:46 +0100

    [frontend] Fix search on empty string (#5039)

--- opencti-platform/opencti-front/src/utils/SearchUtils.js
+++ opencti-platform/opencti-front/src/utils/SearchUtils.js
@@ -1,10 +1,8 @@
 export const handleSearchByKeyword = (searchKeyword, searchScope, history) => {
-  if (searchKeyword.length > 0) {
-    // With need to double encode because of react router.
-    // Waiting for history 5.0 integrated to react router.
-    const encodeKey = encodeURIComponent(encodeURIComponent(searchKeyword));
-    history.push(`/dashboard/search/${searchScope}/${encodeKey}`);
-  }
+  // With need to double encode because of react router.
+  // Waiting for history 5.0 integrated to react router.
+  const encodeKey = encodeURIComponent(encodeURIComponent(searchKeyword));
+  history.push(`/dashboard/search/${searchScope}/${encodeKey}`);
 };
 
 export const decodeSearchKeyword = (searchKeyword) => {
