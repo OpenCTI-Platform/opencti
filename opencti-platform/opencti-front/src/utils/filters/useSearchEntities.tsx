25acd7231e139342d4d9a49bc092ecdf14fc3e9e
Laurent Bonnet <laurent.bonnet@filigran.io>
2023-12-12 17:38:02 +0100

    [frontend] fix spacing

--- opencti-platform/opencti-front/src/utils/filters/useSearchEntities.tsx
+++ opencti-platform/opencti-front/src/utils/filters/useSearchEntities.tsx
@@ -40,212 +40,212 @@ import { Theme } from '../../components/Theme';
 import { useIsAdmin } from '../hooks/useGranted';
 
 const filtersStixCoreObjectsContainersSearchQuery = graphql`
-    query useSearchEntitiesStixCoreObjectsContainersSearchQuery(
-        $search: String
-        $filters: FilterGroup
-    ) {
-        containers(search: $search, filters: $filters) {
-            edges {
-                node {
-                    id
-                    entity_type
-                    parent_types
-                    representative {
-                        main
-                    }
-                }
-            }
+  query useSearchEntitiesStixCoreObjectsContainersSearchQuery(
+    $search: String
+    $filters: FilterGroup
+  ) {
+    containers(search: $search, filters: $filters) {
+      edges {
+        node {
+          id
+          entity_type
+          parent_types
+          representative {
+            main
+          }
         }
+      }
     }
+  }
 `;
 
 const filtersStixCoreObjectsSearchQuery = graphql`
-    query useSearchEntitiesStixCoreObjectsSearchQuery(
-        $search: String
-        $types: [String]
-        $count: Int
-        $filters: FilterGroup
+  query useSearchEntitiesStixCoreObjectsSearchQuery(
+    $search: String
+    $types: [String]
+    $count: Int
+    $filters: FilterGroup
+  ) {
+    stixCoreObjects(
+      search: $search
+      types: $types
+      first: $count
+      filters: $filters
     ) {
-        stixCoreObjects(
-            search: $search
-            types: $types
-            first: $count
-            filters: $filters
-        ) {
-            edges {
-                node {
-                    id
-                    entity_type
-                    parent_types
-                    ... on AttackPattern {
-                        name
-                        description
-                        x_mitre_id
-                    }
-                    ... on Note {
-                        attribute_abstract
-                        content
-                    }
-                    ... on ObservedData {
-                        first_observed
-                        last_observed
-                    }
-                    ... on Opinion {
-                        opinion
-                    }
-                    ... on Report {
-                        name
-                    }
-                    ... on Grouping {
-                        name
-                    }
-                    ... on Campaign {
-                        name
-                        description
-                    }
-                    ... on CourseOfAction {
-                        name
-                        description
-                    }
-                    ... on Individual {
-                        name
-                        description
-                    }
-                    ... on Organization {
-                        name
-                        description
-                    }
-                    ... on Sector {
-                        name
-                        description
-                    }
-                    ... on System {
-                        name
-                        description
-                    }
-                    ... on Indicator {
-                        name
-                        description
-                    }
-                    ... on Infrastructure {
-                        name
-                        description
-                    }
-                    ... on IntrusionSet {
-                        name
-                        description
-                    }
-                    ... on Position {
-                        name
-                        description
-                    }
-                    ... on City {
-                        name
-                        description
-                    }
-                    ... on AdministrativeArea {
-                        name
-                        description
-                    }
-                    ... on Country {
-                        name
-                        description
-                    }
-                    ... on Region {
-                        name
-                        description
-                    }
-                    ... on Malware {
-                        name
-                        description
-                    }
-                    ... on MalwareAnalysis {
-                        product
-                        operatingSystem {
-                            name
-                        }
-                    }
-                    ... on ThreatActor {
-                        name
-                        description
-                    }
-                    ... on Tool {
-                        name
-                        description
-                    }
-                    ... on Vulnerability {
-                        name
-                        description
-                    }
-                    ... on Incident {
-                        name
-                        description
-                    }
-                    ... on Event {
-                        name
-                        description
-                    }
-                    ... on Channel {
-                        name
-                        description
-                    }
-                    ... on Narrative {
-                        name
-                        description
-                    }
-                    ... on DataComponent {
-                        name
-                    }
-                    ... on DataSource {
-                        name
-                    }
-                    ... on Case {
-                        name
-                    }
-                    ... on Task {
-                        name
-                    }
-                    ... on Language {
-                        name
-                    }
-                    ... on StixCyberObservable {
-                        observable_value
-                    }
-                    createdBy {
-                        ... on Identity {
-                            id
-                            name
-                            entity_type
-                        }
-                    }
-                    objectMarking {
-                        edges {
-                            node {
-                                id
-                                definition_type
-                                definition
-                                x_opencti_order
-                                x_opencti_color
-                            }
-                        }
-                    }
-                }
+      edges {
+        node {
+          id
+          entity_type
+          parent_types
+          ... on AttackPattern {
+            name
+            description
+            x_mitre_id
+          }
+          ... on Note {
+            attribute_abstract
+            content
+          }
+          ... on ObservedData {
+            first_observed
+            last_observed
+          }
+          ... on Opinion {
+            opinion
+          }
+          ... on Report {
+            name
+          }
+          ... on Grouping {
+            name
+          }
+          ... on Campaign {
+            name
+            description
+          }
+          ... on CourseOfAction {
+            name
+            description
+          }
+          ... on Individual {
+            name
+            description
+          }
+          ... on Organization {
+            name
+            description
+          }
+          ... on Sector {
+            name
+            description
+          }
+          ... on System {
+            name
+            description
+          }
+          ... on Indicator {
+            name
+            description
+          }
+          ... on Infrastructure {
+            name
+            description
+          }
+          ... on IntrusionSet {
+            name
+            description
+          }
+          ... on Position {
+            name
+            description
+          }
+          ... on City {
+            name
+            description
+          }
+          ... on AdministrativeArea {
+            name
+            description
+          }
+          ... on Country {
+            name
+            description
+          }
+          ... on Region {
+            name
+            description
+          }
+          ... on Malware {
+            name
+            description
+          }
+          ... on MalwareAnalysis {
+            product
+            operatingSystem {
+              name
             }
+          }
+          ... on ThreatActor {
+            name
+            description
+          }
+          ... on Tool {
+            name
+            description
+          }
+          ... on Vulnerability {
+            name
+            description
+          }
+          ... on Incident {
+            name
+            description
+          }
+          ... on Event {
+            name
+            description
+          }
+          ... on Channel {
+            name
+            description
+          }
+          ... on Narrative {
+            name
+            description
+          }
+          ... on DataComponent {
+            name
+          }
+          ... on DataSource {
+            name
+          }
+          ... on Case {
+            name
+          }
+          ... on Task {
+            name
+          }
+          ... on Language {
+            name
+          }
+          ... on StixCyberObservable {
+            observable_value
+          }
+          createdBy {
+            ... on Identity {
+              id
+              name
+              entity_type
+            }
+          }
+          objectMarking {
+            edges {
+              node {
+                id
+                definition_type
+                definition
+                x_opencti_order
+                x_opencti_color
+              }
+            }
+          }
         }
+      }
     }
+  }
 `;
 
 const filtersSchemaSCOSearchQuery = graphql`
-    query useSearchEntitiesSchemaSCOSearchQuery {
-        schemaSCOs: subTypes(type: "Stix-Cyber-Observable") {
-            edges {
-                node {
-                    id
-                    label
-                }
-            }
+  query useSearchEntitiesSchemaSCOSearchQuery {
+    schemaSCOs: subTypes(type: "Stix-Cyber-Observable") {
+      edges {
+        node {
+          id
+          label
         }
+      }
     }
+  }
 `;
 
 export interface EntityValue {
