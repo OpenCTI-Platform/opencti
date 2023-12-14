23b3b60e0b047258c998c0a17fbe7dff9c0a1549
Angelique <angelique.jard@filigran.io>
2023-12-14 08:54:55 +0100

    [frontend] slightly better UI again (#2859)

--- opencti-platform/opencti-front/src/private/components/settings/decay/Decay.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/decay/Decay.tsx
@@ -61,6 +61,11 @@ export const handleSubmit = (values: FormikValues) => {
     if (newCutOff) {
         graphData.decayModel.decay_revoked_cutoff = newCutOff;
     }
+
+    const newFactor: number = parseInt(values.factor);
+    if (newFactor) {
+        graphData.decayModel.decay_factor = newFactor;
+    }
 }
 const Decay = () => {
 
@@ -96,7 +101,8 @@ const Decay = () => {
                                 pound: '1',
                                 lifetime: '30',
                                 reactionPoints:'80;40;20',
-                                cutoff: 20
+                                cutoff: 20,
+                                factor: 3
                             }}
                             onSubmit={async (values) => {
                                 handleSubmit(values);
@@ -104,11 +110,13 @@ const Decay = () => {
                             }}
                         >
                             <Form>
+                                <h3>Données de l'indicateur</h3>
                                 <div>
                                     <label htmlFor="startScore">Start score (100-0):</label>
                                     <Field id="startScore" name="startScore"/>
                                 </div>
 
+                                <h3>Configurable dans les settings</h3>
                                 <div>
                                     <label htmlFor="pound">Pound (ex: 0.8):</label>
                                     <Field id="pound" name="pound" />
@@ -129,6 +137,13 @@ const Decay = () => {
                                     <Field id="cutoff" name="cutoff"/>
                                 </div>
 
+                                <h3>En "dur" dans l'appli</h3>
+                                <div>
+                                    <label htmlFor="factor">Decay factor (ex: 3):</label>
+                                    <Field id="factor" name="factor"/>
+                                </div>
+
+                                <h3></h3>
                                 <div>
                                     <button type="submit">Submit</button>
                                 </div>
@@ -136,7 +151,7 @@ const Decay = () => {
                         </Formik>
                     </div>
                     <div>
-                        <div>Model is:- </div>
+                        <h3>Model is:- </h3>
                         <div>decay_factor: {graphData.decayModel.decay_factor}</div>
                         <div>decay_lifetime: {graphData.decayModel.decay_lifetime} day(s)</div>
                         <div>decay_revoked_cutoff: {graphData.decayModel.decay_revoked_cutoff} day(s)</div>
