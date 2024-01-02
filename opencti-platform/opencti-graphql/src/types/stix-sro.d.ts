6cadc537f79c92c4574d000b3740f27b27ab0d07
BocognanoSarah <sarah.bocognano@filigran.io>
2024-01-02 16:33:35 +0100

    [renovate] fix linter

--- opencti-platform/opencti-graphql/src/types/stix-sro.d.ts
+++ opencti-platform/opencti-graphql/src/types/stix-sro.d.ts
@@ -1,7 +1,6 @@
 import type { StixId } from './stix';
-import type { StixRelationshipObject, StixOpenctiExtension, StixDate } from './stix-common';
+import type { StixRelationshipObject, StixOpenctiExtension, StixDate, StixKillChainPhase } from './stix-common';
 import { STIX_EXT_OCTI } from './stix-extensions';
-import type { StixKillChainPhase } from './stix-smo';
 
 // Relationship Specific Properties
 // relationship_type, description, source_ref, target_ref, start_time, stop_time
