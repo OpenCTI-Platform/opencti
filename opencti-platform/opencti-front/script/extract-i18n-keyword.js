a670af36ad82d87a9308f9d76aafeda17324f76e
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-01-02 07:58:02 +0100

    [frontend] rework the script and modify Localization structure

--- opencti-platform/opencti-front/script/extract-i18n-keyword.js
+++ opencti-platform/opencti-front/script/extract-i18n-keyword.js
@@ -1,72 +1,63 @@
-// read-files-script.js
 const fs = require('fs');
+const util = require('util');
 const path = require('path');
 
-// Specify the path to the source directory
+const readdir = util.promisify(fs.readdir);
+const stat = util.promisify(fs.stat);
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
 const srcDirectory = 'src';
-//Specify the name of the file
 const englishTranslationFiles = 'lang/en.json';
-
-// Define the pattern to search for in files
 const searchPattern = /\{t\('[^']+'\)}/g;
-
-// Object to store extracted key-value pairs
 const extractedValues = {};
+const jsxTsxFileExtensions = ['.jsx', '.tsx'];
 
 function extractValueFromPattern(pattern) {
   const match = /\{t\('([^']+)'\)\}/.exec(pattern);
   return match ? match[1] : null;
 }
 
-// Function to read files in a directory and its subdirectories
-function extractI18nValues(directory) {
-  fs.readdir(directory, (err, files) => {
-    if (err) {
-      console.error(`Error reading directory ${directory}: ${err.message}`);
-    } else {
-      files.forEach(file => {
-        const filePath = path.join(directory, file);
-        fs.stat(filePath, (statErr, stats) => {
-          if (statErr) {
-            console.error(`Error checking file ${filePath}: ${statErr.message}`);
-          } else {
-            if (stats.isDirectory()) {
-              // If it's a directory, recursively call the function for that directory
-              extractI18nValues(filePath);
-            } else if (stats.isFile()) {
-              // If it's a file, read and search for the pattern
-              fs.readFile(filePath, 'utf8', (readErr, data) => {
-                if (readErr) {
-                  console.error(`Error reading file ${filePath}: ${readErr.message}`);
-                } else {
-                  // Use a regular expression to find all occurrences of the pattern {t('[...')}
-                  const matches = data.match(searchPattern);
-                  
-                  if (matches) {
-                    // Log the matches in the console
-                    // console.log(`Matches in ${filePath}:`, matches);
-                    matches.forEach(match => {
-                      const value = extractValueFromPattern(match);
-                      if (value) {
-                        extractedValues[value] = value;
-                      }
-                    })
-                  }
-                }
-              });
+async function extractI18nValues(directory) {
+  try {
+    const files = await readdir(directory);
+    for (const file of files) {
+      const filePath = path.join(directory, file);
+      const stats = await stat(filePath);
+      
+      if (stats.isDirectory()) {
+        await extractI18nValues(filePath); // Recursively call the function for directories
+      } else if (stats.isFile() && jsxTsxFileExtensions.includes(path.extname(filePath))) {
+        const data = await readFile(filePath, 'utf8');
+        const matches = data.match(searchPattern);
+        
+        console.log(filePath);
+        console.log(matches);
+        
+        if (matches) {
+          matches.forEach(match => {
+            const value = extractValueFromPattern(match);
+            if (value) {
+              extractedValues[value] = value;
             }
-          }
-        });
-      });
+          });
+        }
+      }
     }
-  });
-  
-  fs.writeFile(englishTranslationFiles, JSON.stringify(extractedValues, null, 2), err => {
-    if (err) {
-      console.error(`Error writing to en.json: ${err.message}`);
-    }
-  });
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
 }
 
-// Call the function with the main source directory
-extractI18nValues(srcDirectory);
+async function main() {
+  await extractI18nValues(srcDirectory);
+  
+  try {
+    await writeFile(englishTranslationFiles, JSON.stringify(extractedValues, null, 2));
+    console.log('File written successfully');
+  } catch (error) {
+    console.error(`Error writing to en.json: ${error.message}`);
+  }
+}
+
+main();
