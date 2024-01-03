d3bd590b832f3d36af11bce4636af696f642b120
Laurent Bonnet <laurent.bonnet@filigran.io>
2024-01-03 16:16:59 +0100

    [backend] improve coverage of case-domain

--- opencti-platform/opencti-graphql/src/modules/case/case-domain.ts
+++ opencti-platform/opencti-graphql/src/modules/case/case-domain.ts
@@ -10,11 +10,11 @@ import { RELATION_OBJECT_MARKING } from '../../schema/stixRefRelationship';
 import { taskAdd } from '../task/task-domain';
 import { FilterMode } from '../../generated/graphql';
 
-export const findById = (context: AuthContext, user: AuthUser, caseId: string): BasicStoreEntityCase => {
-  return storeLoadById(context, user, caseId, ENTITY_TYPE_CONTAINER_CASE) as unknown as BasicStoreEntityCase;
+export const findById = async (context: AuthContext, user: AuthUser, caseId: string) => {
+  return storeLoadById<BasicStoreEntityCase>(context, user, caseId, ENTITY_TYPE_CONTAINER_CASE);
 };
 
-export const findAll = (context: AuthContext, user: AuthUser, opts: EntityOptions<BasicStoreEntityCase>) => {
+export const findAll = async (context: AuthContext, user: AuthUser, opts: EntityOptions<BasicStoreEntityCase>) => {
   return listEntitiesPaginated<BasicStoreEntityCase>(context, user, [ENTITY_TYPE_CONTAINER_CASE], opts);
 };
 
