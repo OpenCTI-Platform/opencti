27550e4a376f319678deac4d1ca5e3db73995b35
Laurent Bonnet <laurent.bonnet@filigran.io>
2024-01-09 10:54:36 +0100

    [frontend] view user's confidence level (#4304)

---
+++ opencti-platform/opencti-front/src/private/components/settings/users/UserConfidenceLevel.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { User_user$data } from '@components/settings/users/__generated__/User_user.graphql';
+import { InfoOutlined } from '@mui/icons-material';
+import Tooltip from '@mui/material/Tooltip';
+import { useFormatter } from '../../../../components/i18n';
+
+type UserConfidenceLevelFieldProps = {
+  userConfidenceLevel: User_user$data['user_confidence_level']
+};
+
+const UserConfidenceLevel: React.FC<UserConfidenceLevelFieldProps> = ({ userConfidenceLevel }) => {
+  const { t } = useFormatter();
+
+  const overrides = userConfidenceLevel.overrides
+    .map(({ entity_type, max_confidence }) => `${t(`entity_${entity_type}`)}: ${max_confidence}`)
+    .join('\n');
+
+  return (
+    <>
+      <div style={{ float: 'left', marginRight: 5 }}>
+        {`${userConfidenceLevel.max_confidence ?? '-'}`}
+      </div>
+      <div>
+        {
+        userConfidenceLevel.overrides.length > 0 && (
+        <Tooltip title={
+          <div style={{ whiteSpace: 'pre-line' }}>
+            {`${t('This value is overridden for the following entity types')}\n\n${overrides}`}
+          </div>
+        }
+        >
+          <InfoOutlined
+            fontSize="small"
+            color="warning"
+          />
+        </Tooltip>
+        )
+      }
+      </div>
+    </>
+  );
+};
+
+export default UserConfidenceLevel;
