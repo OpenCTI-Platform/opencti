1d5974d69efc485d433ba4b683ae65bc0cc673d2
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-01-15 14:47:41 +0100

    [frontend] fix filters not back to initial filter after closing dialog without saving

--- opencti-platform/opencti-front/src/private/components/workspaces/dashboards/WidgetConfig.jsx
+++ opencti-platform/opencti-front/src/private/components/workspaces/dashboards/WidgetConfig.jsx
@@ -383,6 +383,7 @@ const WidgetConfig = ({ workspace, widget, onComplete, closeMenu }) => {
       setStepIndex(2);
     }
     setOpen(false);
+    setDataSelection(widget?.dataSelection ?? [initialSelection]);
   };
   const completeSetup = () => {
     onComplete({
@@ -815,8 +816,7 @@ const WidgetConfig = ({ workspace, widget, onComplete, closeMenu }) => {
                 remarkGfmPlugin={true}
                 commonmark={true}
               />,
-            )
-                            }
+            )}
             l18n={{
               write: t('Write'),
               preview: t('Preview'),
