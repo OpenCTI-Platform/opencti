1d071bdcbfd1980b6eb0319075c38125ca879ae6
Landry Trebon <landry.trebon@filigran.io>
2024-01-22 14:54:57 +0100

    [frontend] new widgets components + refacto widget number

--- opencti-platform/opencti-front/src/public/components/PublicDashboard.tsx
+++ opencti-platform/opencti-front/src/public/components/PublicDashboard.tsx
@@ -1,15 +1,15 @@
-import React, { FunctionComponent, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import { graphql, PreloadedQuery, usePreloadedQuery } from 'react-relay';
 import { useParams } from 'react-router-dom';
 import RGL, { WidthProvider } from 'react-grid-layout';
 import { ErrorBoundary, SimpleError } from '@components/Error';
 import Paper from '@mui/material/Paper';
 import Loader, { LoaderVariant } from '../../components/Loader';
-import { daysAgo, dayStartDate, monthsAgo, yearsAgo } from '../../utils/Time';
 import { PublicDashboardQuery } from './__generated__/PublicDashboardQuery.graphql';
 import useQueryLoading from '../../utils/hooks/useQueryLoading';
 import { fromB64 } from '../../utils/String';
-import StixCoreObjectsMultiHeatMapPublic from './StixCoreObjectsMultiHeatMapPublic';
+import type { PublicManifest } from './dashboard/PublicManifest';
+import usePublicDashboardWidgets from './dashboard/usePublicDashboardWidgets';
 
 const publicDashboardQuery = graphql`
   query PublicDashboardQuery(
@@ -19,7 +19,7 @@ const publicDashboardQuery = graphql`
       uri_key: $uri_key
     ) {
       name
-      public_manifest  
+      public_manifest
     }
   }
 `;
@@ -29,40 +29,22 @@ interface PublicDashboardComponentProps {
   uriKey: string;
 }
 
-const PublicDashboardComponent: FunctionComponent<PublicDashboardComponentProps> = ({ queryRef, uriKey }) => {
+const PublicDashboardComponent = ({
+  queryRef,
+  uriKey,
+}: PublicDashboardComponentProps) => {
   const ReactGridLayout = useMemo(() => WidthProvider(RGL), []);
 
-  const publicDashboard = usePreloadedQuery<PublicDashboardQuery>(publicDashboardQuery, queryRef);
+  const publicDashboard = usePreloadedQuery(publicDashboardQuery, queryRef);
   const manifest = publicDashboard.publicDashboardPublic?.public_manifest;
-  const parsedManifest = JSON.parse(fromB64(manifest ?? '{}'));
-  const { config, widgets } = parsedManifest;
+  const parsedManifest: PublicManifest = JSON.parse(fromB64(manifest ?? '{}'));
+  const { widgets, config } = parsedManifest;
 
-  const widget = widgets['687702dc-9237-4eb4-a16a-0c38044209e7']; // HARDCODED FOR NOW TO GET HEAT MAP
-  const { relativeDate } = config;
-
-  const getDayStartDate = () => {
-    return dayStartDate(null, false);
-  };
-
-  const computerRelativeDate = () => {
-    if (relativeDate) {
-      if (relativeDate.includes('days')) {
-        return daysAgo(relativeDate.split('-')[1], null, false);
-      }
-      if (relativeDate.includes('months')) {
-        return monthsAgo(relativeDate.split('-')[1]);
-      }
-      if (relativeDate.includes('years')) {
-        return yearsAgo(relativeDate.split('-')[1]);
-      }
-    }
-    return null;
-  };
-
-  const startDate = relativeDate
-    ? computerRelativeDate()
-    : config.startDate;
-  const endDate = relativeDate ? getDayStartDate() : config.endDate;
+  const {
+    entityWidget,
+    relationshipWidget,
+    rawWidget,
+  } = usePublicDashboardWidgets(uriKey, config);
 
   return (
     <>
@@ -74,10 +56,10 @@ const PublicDashboardComponent: FunctionComponent<PublicDashboardComponentProps>
         isDraggable={false}
         isResizable={false}
       >
-        {[widget].map((w) => (
+        {Object.values(widgets).map((widget) => (
           <Paper
-            key={w.id}
-            data-grid={w.layout}
+            key={widget.id}
+            data-grid={widget.layout}
             variant="outlined"
             sx={{
               height: '100%',
@@ -95,12 +77,9 @@ const PublicDashboardComponent: FunctionComponent<PublicDashboardComponentProps>
                 </div>
               }
             >
-              <StixCoreObjectsMultiHeatMapPublic
-                startDate={startDate}
-                endDate={endDate}
-                uriKey={uriKey}
-                widgetId={w.id}
-              />
+              {widget.perspective === 'entities' && entityWidget(widget)}
+              {widget.perspective === 'relationships' && relationshipWidget(widget)}
+              {widget.perspective === null && rawWidget(widget)}
             </ErrorBoundary>
           </Paper>
         ))}
