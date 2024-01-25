a0b745a64646541b9dd7e41a0b39dd7e70cc144d
Samuel Hassine <samuel.hassine@filigran.io>
2024-01-25 13:21:13 +0100

    [backend] Add workflow ID at report creation (#5674)

--- opencti-platform/opencti-graphql/src/generated/graphql.ts
+++ opencti-platform/opencti-graphql/src/generated/graphql.ts
@@ -20081,6 +20081,7 @@ export type ReportAddInput = {
   update?: InputMaybe<Scalars['Boolean']['input']>;
   x_opencti_reliability?: InputMaybe<Scalars['String']['input']>;
   x_opencti_stix_ids?: InputMaybe<Array<InputMaybe<Scalars['StixId']['input']>>>;
+  x_opencti_workflow_id?: InputMaybe<Scalars['String']['input']>;
 };
 
 export type ReportConnection = {
