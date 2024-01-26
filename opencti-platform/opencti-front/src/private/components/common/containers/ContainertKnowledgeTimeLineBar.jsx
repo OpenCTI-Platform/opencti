ecd4e4d5b27aef2d44186ad67a7f344b8b3e5ac7
Celine Sebe <celine.sebe@filigran.io>
2024-01-26 11:55:26 +0100

    [frontend] ArrowDownIcon works (#5636)

--- opencti-platform/opencti-front/src/private/components/common/containers/ContainertKnowledgeTimeLineBar.jsx
+++ opencti-platform/opencti-front/src/private/components/common/containers/ContainertKnowledgeTimeLineBar.jsx
@@ -5,6 +5,7 @@ import Tooltip from '@mui/material/Tooltip';
 import Drawer from '@mui/material/Drawer';
 import Divider from '@mui/material/Divider';
 import { makeStyles } from '@mui/styles';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import SearchInput from '../../../../components/SearchInput';
 import { useFormatter } from '../../../../components/i18n';
 import { MESSAGING$ } from '../../../../relay/environment';
@@ -19,10 +20,10 @@ const useStyles = makeStyles(() => ({
     overflow: 'hidden',
   },
   divider: {
-    display: 'inline-block',
-    verticalAlign: 'middle',
-    height: '100%',
+    height: '51px',
     margin: '0 5px 0 5px',
+    paddingTop: 3,
+    paddingBottom: 3,
   },
 }));
 
@@ -45,6 +46,9 @@ const ContentKnowledgeTimeLineBar = ({
   const [navOpen, setNavOpen] = useState(
     localStorage.getItem('navOpen') === 'true',
   );
+
+  const [open, setOpen] = useState(false);
+
   useEffect(() => {
     const sub = MESSAGING$.toggleNav.subscribe({
       next: () => setNavOpen(localStorage.getItem('navOpen') === 'true'),
@@ -71,96 +75,88 @@ const ContentKnowledgeTimeLineBar = ({
         >
           <div
             style={{
-              height: 54,
-              verticalAlign: 'top',
+              height: open ? 108 : 54,
+              minHeight: open ? 108 : 54,
               transition: 'height 0.2s linear',
             }}
           >
             <div
               style={{
-                verticalAlign: 'top',
-                width: '100%',
-                height: 54,
-                paddingTop: 3,
+                display: 'flex',
+
               }}
             >
-              <div
-                style={{
-                  float: 'left',
-
-                  height: '100%',
-                  display: 'flex',
-                }}
+              <Tooltip
+                title={
+                  timeLineDisplayRelationships
+                    ? t_i18n('Do not display relationships')
+                    : t_i18n('Display relationships')
+              }
               >
-                <Tooltip
-                  title={
-                                        timeLineDisplayRelationships
-                                          ? t_i18n('Do not display relationships')
-                                          : t_i18n('Display relationships')
-                                    }
-                >
-                  <span>
-                    <IconButton
-                      color={timeLineDisplayRelationships ? 'secondary' : 'primary'}
-                      size="large"
-                      onClick={() => handleToggleTimeLineDisplayRelationships()}
-                    >
-                      <RelationManyToMany/>
-                    </IconButton>
-                  </span>
-                </Tooltip>
-                <Tooltip
-                  title={
+                <span>
+                  <IconButton
+                    color={timeLineDisplayRelationships ? 'secondary' : 'primary'}
+                    size="large"
+                    onClick={() => handleToggleTimeLineDisplayRelationships()}
+                  >
+                    <RelationManyToMany/>
+                  </IconButton>
+                </span>
+              </Tooltip>
+              <Tooltip
+                title={
                                         timeLineFunctionalDate
                                           ? t_i18n('Use technical dates')
                                           : t_i18n('Use functional dates')
                                     }
-                >
-                  <span>
-                    <IconButton
-                      color={timeLineFunctionalDate ? 'secondary' : 'primary'}
-                      size="large"
-                      onClick={() => handleToggleTimeLineFunctionalDate()}
-                    >
-                      <CalendarMultiselectOutline/>
-                    </IconButton>
-                  </span>
-                </Tooltip>
-                <Divider className={classes.divider} orientation="vertical" />
-                <div style={{ margin: '9px 10px 0 10px' }}>
-                  <SearchInput
-                    variant="thin"
-                    onSubmit={handleTimeLineSearch}
-                    keyword={timeLineSearchTerm}
-                  />
-                </div>
-                <Divider className={classes.divider} orientation="vertical" />
-                <div style={{ paddingTop: 4 }}>
-                  <Filters
-                    availableFilterKeys={[
-                      'entity_type',
-                      'objectMarking',
-                      'objectLabel',
-                      'createdBy',
-                      'relationship_type',
-                    ]}
-                    availableEntityTypes={[
-                      'Stix-Domain-Object',
-                      'Stix-Cyber-Observable',
-                    ]}
-                    handleAddFilter={handleAddTimeLineFilter}
-                  />
-                </div>
-                <div style={{ paddingTop: 3 }}>
-                  <FilterIconButton
-                    filters={timeLineFilters}
-                    handleRemoveFilter={handleRemoveTimeLineFilter}
-                    handleSwitchLocalMode={handleSwitchFilterLocalMode}
-                    handleSwitchGlobalMode={handleSwitchFilterGlobalMode}
-                    redirection
-                  />
-                </div>
+              >
+                <span>
+                  <IconButton
+                    color={timeLineFunctionalDate ? 'secondary' : 'primary'}
+                    size="large"
+                    onClick={() => handleToggleTimeLineFunctionalDate()}
+                  >
+                    <CalendarMultiselectOutline/>
+                  </IconButton>
+                </span>
+              </Tooltip>
+              <Divider className={classes.divider} orientation="vertical" />
+              <div style={{ flexGrow: 0 }}>
+                <SearchInput
+                  variant="thin"
+                  onSubmit={handleTimeLineSearch}
+                  keyword={timeLineSearchTerm}
+                />
               </div>
+              <Divider className={classes.divider} orientation="vertical" />
+              <div>
+                <Filters
+                  availableFilterKeys={[
+                    'entity_type',
+                    'objectMarking',
+                    'objectLabel',
+                    'createdBy',
+                    'relationship_type',
+                  ]}
+                  availableEntityTypes={[
+                    'Stix-Domain-Object',
+                    'Stix-Cyber-Observable',
+                  ]}
+                  handleAddFilter={handleAddTimeLineFilter}
+                />
+              </div>
+              <div style={{ flexGrow: 1 }}>
+                <FilterIconButton
+                  filters={timeLineFilters}
+                  handleRemoveFilter={handleRemoveTimeLineFilter}
+                  handleSwitchLocalMode={handleSwitchFilterLocalMode}
+                  handleSwitchGlobalMode={handleSwitchFilterGlobalMode}
+                  redirection
+                />
+              </div>
+              <IconButton style={{ height: 'fit-content' }} onClick={() => setOpen(!open)}>
+                <KeyboardArrowDownIcon />
+              </IconButton>
             </div>
           </div>
         </Drawer>)}
