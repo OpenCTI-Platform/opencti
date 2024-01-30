560c35b70fdc566cfa4428aecbb53316f6848a3b
Filigran Automation <automation@filigran.io>
2024-01-30 14:39:37 +0000

    [client] Release 5.12.26
    (original commit: OpenCTI-Platform/client-python@65ca592)

--- pycti/__init__.py
+++ pycti/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "5.12.25"
+__version__ = "5.12.26"
 
 from .api.opencti_api_client import OpenCTIApiClient
 from .api.opencti_api_connector import OpenCTIApiConnector
