8405c5bc2bfb93d53fc2e0c6b3e31ba757daa00c
mahmut-the-guy <158579334+mahmut-the-guy@users.noreply.github.com>
2024-02-02 14:16:55 +0000

    [client] Remove mutable default argument from `OpenCTIApiClient.query` (#546)

--- pycti/api/opencti_api_client.py
+++ pycti/api/opencti_api_client.py
@@ -215,7 +215,7 @@ class OpenCTIApiClient:
             "" if retry_number is None else str(retry_number)
         )
 
-    def query(self, query, variables={}):
+    def query(self, query, variables=None):
         """submit a query to the OpenCTI GraphQL API
 
         :param query: GraphQL query string
@@ -225,7 +225,7 @@ class OpenCTIApiClient:
         :return: returns the response json content
         :rtype: Any
         """
-
+        variables = variables or {}
         query_var = {}
         files_vars = []
         # Implementation of spec https://github.com/jaydenseric/graphql-multipart-request-spec
