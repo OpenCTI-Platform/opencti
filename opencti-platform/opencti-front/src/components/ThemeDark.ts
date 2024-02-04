f2412c2b01d4c9e2b80b5a0b2c57cdc8fc6f0022
Samuel Hassine <samuel.hassine@filigran.io>
2024-02-04 13:03:32 +0100

    [worker] Log the headers

--- opencti-platform/opencti-front/src/components/ThemeDark.ts
+++ opencti-platform/opencti-front/src/components/ThemeDark.ts
@@ -9,7 +9,7 @@ const EE_COLOR = '#00f1bd';
 export const THEME_DARK_DEFAULT_BACKGROUND = '#00020c'; // 0a1929
 const THEME_DARK_DEFAULT_PRIMARY = '#0fbcff'; // 1c8eb6 // for road:
 const THEME_DARK_DEFAULT_SECONDARY = '#00f1bd'; // d81b60
-const THEME_DARK_DEFAULT_ACCENT = '#04092a'; // 01478d // for building: 030721
+const THEME_DARK_DEFAULT_ACCENT = '#001e4e'; // 01478d // for building: 030721
 const THEME_DARK_DEFAULT_PAPER = '#00020c'; // 001e3c
 const THEME_DARK_DEFAULT_NAV = '#00020c'; // 071a2e
 
