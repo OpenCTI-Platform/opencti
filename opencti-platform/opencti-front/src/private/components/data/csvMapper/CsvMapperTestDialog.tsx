f24e44219f7b2f75a7f67afb5afc49aa0c06f8df
Jeremy Cloarec <jeremy.cloarec@filigran.io>
2024-02-08 14:44:43 +0100

    [frontend] Added explanation in comments to answer for possible future questions

--- opencti-platform/opencti-front/src/private/components/data/csvMapper/CsvMapperTestDialog.tsx
+++ opencti-platform/opencti-front/src/private/components/data/csvMapper/CsvMapperTestDialog.tsx
@@ -98,6 +98,7 @@ const CsvMapperTestDialog: FunctionComponent<CsvMapperTestDialogProps> = ({
           <CustomFileUploader
             setFieldValue={(field, v) => onChange(field, v)}
             label={'Your testing file limited to 100 lines (csv only, max 1MB)'}
+            // we also accept application/vnd.ms-excel type because that's how csv's seem to be seen as under WindowsOS + Firefox browser
             acceptMimeTypes={'text/csv,application/vnd.ms-excel'}
             // we limit the file size so the upload does not take too long for a simple test
             sizeLimit={1000000}
