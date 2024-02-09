9fefe42d396e57b525c7e0f57c5205c762fd85f8
Marie Flores <73853856+marieflorescontact@users.noreply.github.com>
2024-02-09 15:51:44 +0100

    [backend] Rename data sharing capability description  (#5831)

---
+++ opencti-platform/opencti-graphql/src/migrations/1707487253148-rename-data-sharing-capability-description.js
@@ -0,0 +1,19 @@
+import { executionContext, SYSTEM_USER } from '../utils/access';
+import { elLoadById, elReplace } from '../database/engine';
+
+export const up = async (next) => {
+  const context = executionContext('migration');
+  // ------ Access data sharing
+  const accessTaxii = await elLoadById(context, SYSTEM_USER, 'capability--d258afde-7a8a-5917-8b4b-83119d3f8e52');
+  const accessPatch = { description: 'Access data sharing & ingestions' };
+  await elReplace(accessTaxii._index, accessTaxii.internal_id, { doc: accessPatch });
+  // ------ Manage data sharing
+  const manageTaxii = await elLoadById(context, SYSTEM_USER, 'capability--24f9401c-8a77-59d5-8a8f-4ea21a1a733b');
+  const managePatch = { description: 'Manage data sharing & ingestions' };
+  await elReplace(manageTaxii._index, manageTaxii.internal_id, { doc: managePatch });
+  next();
+};
+
+export const down = async (next) => {
+  next();
+};
