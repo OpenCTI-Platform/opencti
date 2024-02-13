ab8b8afeb1794f4d35997fee6b26c64ffab38b18
Marie Flores <73853856+marieflorescontact@users.noreply.github.com>
2024-02-13 14:20:31 +0100

    [backend] fix typo in data sharing capabilities migration (#5831)

--- opencti-platform/opencti-graphql/src/migrations/1707808059963-rename-data-sharing-capability-description.js
+++ opencti-platform/opencti-graphql/src/migrations/1707808059963-rename-data-sharing-capability-description.js
@@ -5,11 +5,11 @@ export const up = async (next) => {
   const context = executionContext('migration');
   // ------ Access data sharing
   const accessTaxii = await elLoadById(context, SYSTEM_USER, 'capability--d258afde-7a8a-5917-8b4b-83119d3f8e52');
-  const accessPatch = { description: 'Access data sharing & ingestions' };
+  const accessPatch = { description: 'Access data sharing & ingestion' };
   await elReplace(accessTaxii._index, accessTaxii.internal_id, { doc: accessPatch });
   // ------ Manage data sharing
   const manageTaxii = await elLoadById(context, SYSTEM_USER, 'capability--24f9401c-8a77-59d5-8a8f-4ea21a1a733b');
-  const managePatch = { description: 'Manage data sharing & ingestions' };
+  const managePatch = { description: 'Manage data sharing & ingestion' };
   await elReplace(manageTaxii._index, manageTaxii.internal_id, { doc: managePatch });
   next();
 };
