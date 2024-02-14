3415f9069dff29a778f329f176f3e87af7924953
Laurent Bonnet <laurent.bonnet@filigran.io>
2024-02-14 16:47:27 +0100

    [backend] update test counters

--- opencti-platform/opencti-graphql/tests/03-streams/00-Raw/raw-test.js
+++ opencti-platform/opencti-graphql/tests/03-streams/00-Raw/raw-test.js
@@ -42,7 +42,7 @@ describe('Raw streams tests', () => {
       expect(updateEventsByTypes['campaign'].length).toBe(7);
       expect(updateEventsByTypes['relationship'].length).toBe(8);
       expect(updateEventsByTypes['identity'].length).toBe(11);
-      expect(updateEventsByTypes['malware'].length).toBe(13);
+      expect(updateEventsByTypes['malware'].length).toBe(15);
       expect(updateEventsByTypes['intrusion-set'].length).toBe(4);
       expect(updateEventsByTypes['data-component'].length).toBe(2);
       expect(updateEventsByTypes['location'].length).toBe(12);
@@ -65,7 +65,7 @@ describe('Raw streams tests', () => {
       expect(updateEventsByTypes['threat-actor'].length).toBe(17);
       expect(updateEventsByTypes['vocabulary'].length).toBe(3);
       expect(updateEventsByTypes['vulnerability'].length).toBe(3);
-      expect(updateEvents.length).toBe(132);
+      expect(updateEvents.length).toBe(134);
       for (let updateIndex = 0; updateIndex < updateEvents.length; updateIndex += 1) {
         const event = updateEvents[updateIndex];
         const { data: insideData, origin, type } = event;
