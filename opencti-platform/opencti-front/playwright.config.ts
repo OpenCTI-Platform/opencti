cdbb20276f5ffb1a52bbbff05f2cc17a615dfde6
Landry Trebon <landry.trebon@filigran.io>
2024-02-19 17:44:47 +0100

    add link to doc to install browsers

--- opencti-platform/opencti-front/playwright.config.ts
+++ opencti-platform/opencti-front/playwright.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
+// https://playwright.dev/docs/browsers
+
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
