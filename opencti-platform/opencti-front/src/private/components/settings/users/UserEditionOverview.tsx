97328a23fba53e91352d3c7077eb31985a94c074
Laurent Bonnet <laurent.bonnet@filigran.io>
2024-02-19 15:04:47 +0100

    [backend] fix deepsca

--- opencti-platform/opencti-front/src/private/components/settings/users/UserEditionOverview.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/users/UserEditionOverview.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent } from 'react';
 import { createFragmentContainer, graphql, useMutation } from 'react-relay';
 import { Field, Form, Formik } from 'formik';
 import * as R from 'ramda';
