494a70d850fb12dbd9805906bf4150ff14667d01
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-02-23 11:22:27 +0100

    [frontend] remove start:e2e

--- opencti-platform/opencti-front/playwright.config.ts
+++ opencti-platform/opencti-front/playwright.config.ts
@@ -31,7 +31,6 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     ignoreHTTPSErrors: true,
-    screenshot: 'only-on-failure'
   },
   expect: { timeout: 200000 },
   timeout: 200000,
@@ -83,7 +82,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
    webServer: {
-     command: 'yarn start',
+     command: 'yarn start:e2e',
      url: 'http://localhost:3000',
      reuseExistingServer: true,
    },
