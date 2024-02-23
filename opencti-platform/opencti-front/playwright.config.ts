42fac2a7942cfe9c2663a9e1e72936e9ce65906a
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-02-23 11:42:10 +0100

    [frontend] remove start:e2e

--- opencti-platform/opencti-front/playwright.config.ts
+++ opencti-platform/opencti-front/playwright.config.ts
@@ -82,7 +82,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
    webServer: {
-     command: 'yarn start:e2e',
+     command: 'yarn start',
      url: 'http://localhost:3000',
      reuseExistingServer: true,
    },
