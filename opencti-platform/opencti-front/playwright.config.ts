e5e3f1d775dcd15d15bcaeb3ecb0ecc4dcc400c5
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-02-26 09:48:37 +0100

    [frontend] Put on using only 25% of CPU on launching Playwright test

--- opencti-platform/opencti-front/playwright.config.ts
+++ opencti-platform/opencti-front/playwright.config.ts
@@ -17,10 +17,9 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : '25%',
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
