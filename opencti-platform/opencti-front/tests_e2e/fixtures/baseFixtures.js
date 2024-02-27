c27bbb4a00e5047022d9f1e5356d58745536518f
Landry Trebon <landry.trebon@filigran.io>
2024-02-27 14:21:32 +0100

    [frontend] fix rebase

--- opencti-platform/opencti-front/tests_e2e/fixtures/baseFixtures.js
+++ opencti-platform/opencti-front/tests_e2e/fixtures/baseFixtures.js
@@ -1,42 +1,42 @@
 // fixtures.js for v8 coverage
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { test as testBase, expect } from '@playwright/test';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { addCoverageReport } from 'monocart-reporter';
 
 const test = testBase.extend({
   autoTestFixture: [async ({ page }, use) => {
-    
     // NOTE: it depends on your project name
     const activateCoverage = process.env.E2E_COVERAGE;
-    
+
     // console.log('autoTestFixture setup...');
     // coverage API is chromium only
     if (activateCoverage) {
       await Promise.all([
         page.coverage.startJSCoverage({
-          resetOnNavigation: false
+          resetOnNavigation: false,
         }),
         page.coverage.startCSSCoverage({
-          resetOnNavigation: false
-        })
+          resetOnNavigation: false,
+        }),
       ]);
     }
-    
+
     await use('autoTestFixture');
-    
+
     // console.log('autoTestFixture teardown...');
     if (activateCoverage) {
       const [jsCoverage, cssCoverage] = await Promise.all([
         page.coverage.stopJSCoverage(),
-        page.coverage.stopCSSCoverage()
+        page.coverage.stopCSSCoverage(),
       ]);
-      const coverageList = [... jsCoverage, ... cssCoverage];
+      const coverageList = [...jsCoverage, ...cssCoverage];
       // console.log(coverageList.map((item) => item.url));
       await addCoverageReport(coverageList, test.info());
     }
-    
   }, {
     scope: 'test',
-    auto: true
-  }]
+    auto: true,
+  }],
 });
 export { test, expect };
