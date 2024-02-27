08195b91780839ce628b2c2c9c2f91f8f4eacd70
Celine Sebe <celine.sebe@filigran.io>
2024-02-27 09:18:16 +0100

    [frontend]Fix indentation issues(#6052)

--- opencti-platform/opencti-front/tests_e2e/artifact/createArtifact.spec.ts
+++ opencti-platform/opencti-front/tests_e2e/artifact/createArtifact.spec.ts
@@ -3,12 +3,12 @@ import { ArtifactImportPage} from "../model/ArtifactImport.pageModel";
 import { expect, test } from "../fixtures/baseFixtures";
 
 
-test('Artifact error message in the absence of a file.', async ({ page }) => {
-        const artifactPage = new ArtifactPage(page);
-        const artifactImport = new ArtifactImportPage(page);
-        await page.goto('/dashboard/observations/artifacts')
-        await artifactPage.addNewArtifactImport().click();
-        artifactImport.getFileInput();
-        await artifactImport.getCreateArtifactImportButton().click();
-        await expect (artifactImport.getErrorMessage()).toBeVisible();
+  test('Artifact error message in the absence of a file.', async ({ page }) => {
+    const artifactPage = new ArtifactPage(page);
+    const artifactImport = new ArtifactImportPage(page);
+    await page.goto('/dashboard/observations/artifacts')
+    await artifactPage.addNewArtifactImport().click();
+    artifactImport.getFileInput();
+    await artifactImport.getCreateArtifactImportButton().click();
+    await expect (artifactImport.getErrorMessage()).toBeVisible();
 })
