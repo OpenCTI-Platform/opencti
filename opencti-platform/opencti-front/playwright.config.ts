c2d958882ff68c980c15c41fd955a355e8bf7348
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-03-15 15:36:09 +0100

    [frontend] Launch playwright on testing platform

--- opencti-platform/opencti-front/playwright.config.ts
+++ opencti-platform/opencti-front/playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.BASE_URL ?? 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -92,8 +92,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
    webServer: {
      command: 'yarn start',
-     url: 'http://localhost:3000',
-     reuseExistingServer: false,
+     url: process.env.BASE_URL ?? 'http://localhost:3000',
+     reuseExistingServer: !!process.env.BASE_URL,
    },
 
 });
