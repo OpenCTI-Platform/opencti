84ba5f17f95e61516192eed86b89ec417526d73d
Souad Hadjiat <souad.hadjiat@filigran.io>
2024-03-19 17:43:09 +0100

    [e2e] add role (wip)

--- opencti-platform/opencti-front/tests_e2e/auth.setup.ts
+++ opencti-platform/opencti-front/tests_e2e/auth.setup.ts
@@ -5,13 +5,35 @@ import LoginPage from './model/login.pageModel';
 const authFile = 'tests_e2e/.setup/.auth/user.json';
 const prepLogoutFile = 'tests_e2e/.setup/.auth/logout-user.json';
 
-setup('authenticate', async ({ page }) => {
+const analystCapabilities = ['Delete knowledge', 'Delete exploration', 'Generate knowledge export', 'Import knowledge', 'Upload knowledge files', 'Manage authorized members', 'Restrict organization access', 'Access to collaborative creation', 'Ask for knowledge enrichment'];
+
+setup('authenticate', async ({ page, request }) => {
   const dashboardPage = new DashboardPage(page);
   const loginPage = new LoginPage(page);
   await loginPage.login();
   await expect(dashboardPage.getPage()).toBeVisible();
   // End of authentication steps.
   await page.context().storageState({ path: authFile });
+
+  const response = await request.post('http://localhost:3000/graphql', {
+    data: {
+      query: `mutation {
+        roleAdd(input:{
+            name:"Analyst"
+        }){
+            id
+            name
+        }
+    }`,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer d434ce02-e58e-4cac-8b4c-42bf16748e84',
+    },
+  });
+  expect(response.ok()).toBeTruthy();
+  expect(response.status()).toBe(200);
+  expect((await response.json()).data.roleAdd.name).toBe('Analyst');
 });
 
 setup('authenticate for logout user', async ({ page }) => {
