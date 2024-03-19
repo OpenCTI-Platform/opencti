67bac4d96c7ea8d97cfd36fd276ec3428e817b27
Cathia Archidoit <cathia.archidoit@filigran.io>
2024-03-19 17:02:54 +0100

    [frontend] filters e2e : correct added filters in background tasks

--- opencti-platform/opencti-front/tests_e2e/filters/addFilter.spec.ts
+++ opencti-platform/opencti-front/tests_e2e/filters/addFilter.spec.ts
@@ -5,11 +5,23 @@ import ReportPage from '../model/report.pageModel';
 test('Add a new filter in the observables list and check the filter is still present when we come back to the page', async ({ page }) => {
   await page.goto('/dashboard/observations/observables');
   const filterUtils = new FiltersPageModel(page);
-  await filterUtils.addFilter('Entity type', 'Artifact');
+  await filterUtils.addFilterFromList('Entity type', 'Artifact');
   await expect(page.getByRole('button', { name: 'Entity type = Artifact' })).toBeVisible();
   await page.goto('/dashboard/');
   await page.goto('/dashboard/observations/observables');
   await expect(page.getByRole('button', { name: 'Entity type = Artifact' })).toBeVisible();
+});
+
+test('Add different filters with different operators', async ({ page }) => {
+  await page.goto('/dashboard/observations/observables');
+  const filterUtils = new FiltersUtils(page);
+  await filterUtils.addFilterFromDate('Original creation date', '12', 'Lower than');
+  await filterUtils.addFilterFromTextAndOperator('Value', 'Starts with', 'Empty');
+  await filterUtils.addFilterFromTextAndOperator('Value', 'Starts with', 'Contains', 'content to test');
+  await page.goto('/dashboard/observations/observables');
+  await expect(page.getByRole('button', { name: 'Modification date < 12' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Value is empty' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Value contains : content to test' })).toBeVisible();
 });
 
 test('Check entity type background tasks filter is correct', async ({ page }) => {
@@ -18,5 +30,18 @@ test('Check entity type background tasks filter is correct', async ({ page }) =>
   await reportPage.selectAllReports(); // check all the reports
   await page.getByRole('button', { name: 'delete' }).click(); // delete them via the toolbar
   await expect(page.getByText('Entity type: Report')).toBeVisible(); // the 'Entity type: Report' filter should be present
+  await page.getByRole('button', { name: 'Cancel' }).click(); // don't launch the background task
+});
+
+test('Check added filter is present in background task', async ({ page }) => {
+  const reportPage = new ReportPage(page);
+  await page.goto('/dashboard/analyses/reports');
+  const filterUtils = new FiltersUtils(page);
+  await filterUtils.addFilterFromText('Modification date', '01/01/2020');
+  await reportPage.selectAllReports(); // check all the reports
+  await page.getByRole('button', { name: 'delete' }).click(); // delete them via the toolbar
+  await page.getByRole('button', { name: 'Filters are not fully displayed' }).click(); // click in the 'filters are not fully displayed' box
+  await expect(page.getByText('updated_at')).toBeVisible(); // the added filter should be present
+  await page.getByRole('button', { name: 'Close' }).click(); // close the 'filters are not fully displayed panel'
   await page.getByRole('button', { name: 'Cancel' }).click(); // don't launch the background task
 });
--- opencti-platform/opencti-front/tests_e2e/model/filters.pageModel.ts
+++ opencti-platform/opencti-front/tests_e2e/model/filters.pageModel.ts
@@ -3,7 +3,7 @@ import { Page } from '@playwright/test';
 export default class FiltersPageModel {
   constructor(private page: Page) {}
 
-  async addFilter(filterKey: string, filterLabel: string, autoOpen = true) {
+  async addFilterFromList(filterKey: string, filterLabel: string, autoOpen = true) {
     await this.page.getByLabel('Add filter').click();
     await this.page.getByRole('option', { name: filterKey }).click();
     if (!autoOpen) {
@@ -15,5 +15,33 @@ export default class FiltersPageModel {
   }
   async removeLastFilter() {
     await this.page.getByTestId('CancelIcon').last().click();
+  }
+
+  async addFilterFromText(filterKey: string, entry: string) {
+    await this.page.getByLabel('Add filter').click();
+    await this.page.getByRole('option', { name: filterKey, exact: true }).click();
+    await this.page.getByLabel(filterKey).fill(entry);
+    await this.page.locator('.MuiPopover-root > .MuiBackdrop-root').click();
+  }
+
+  async addFilterFromTextAndOperator(filterKey: string, defaultOperator: string, newOperator: string, entry?: string) {
+    await this.page.getByLabel('Add filter').click();
+    await this.page.getByRole('option', { name: filterKey, exact: true }).click();
+    await this.page.getByLabel(defaultOperator).click();
+    await this.page.getByRole('option', { name: newOperator, exact: true }).click();
+    if (entry) await this.page.getByLabel(filterKey).fill(entry);
+    await this.page.locator('.MuiPopover-root > .MuiBackdrop-root').click();
+  }
+
+  async addFilterFromDate(filterKey: string, dateNumber: string, newOperator?: string) {
+    await this.page.getByLabel('Add filter').click();
+    await this.page.getByRole('option', { name: filterKey, exact: true }).click();
+    if (newOperator) {
+      await this.page.getByLabel('Greater than/ Equals').click();
+      await this.page.getByRole('option', { name: newOperator, exact: true }).click();
+    }
+    await this.page.getByLabel('Choose date').click();
+    await this.page.getByRole('gridcell', { name: dateNumber }).click();
+    // await this.page.locator('.MuiPopover-root > .MuiBackdrop-root').click();
   }
 }
