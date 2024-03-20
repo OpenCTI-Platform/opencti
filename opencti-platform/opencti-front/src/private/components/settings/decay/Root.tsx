760d4974b637ff0cc48f2cc408264b276376ee55
Celine Sebe <celine.sebe@filigran.io>
2024-03-20 17:09:42 +0100

    [frontend] Fix decayRule settings(#5575)

--- opencti-platform/opencti-front/src/private/components/settings/decay/Root.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/decay/Root.tsx
@@ -2,17 +2,16 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import React from 'react';
-import { Route, Routes, useRouteMatch } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { boundaryWrapper } from '@components/Error';
 import DecayRules from './DecayRules';
 import DecayRule from './DecayRule';
 
 const RootDecayRule = () => {
-  const match = useRouteMatch();
-
   return (
     <Routes>
-      <Route path={`${match.path}/:decayRuleId`} Component={DecayRule} />
-      <Route path="/" Component={DecayRules} />
+      <Route path="/" Component={boundaryWrapper(DecayRules)} element={ <Navigate to={'/dashboard/settings/customization/decay'} /> }></Route>
+      <Route path="decay/:decayRuleId/*" Component={boundaryWrapper(DecayRule)} />
     </Routes>
   );
 };
