5a87835a59a1f8cbce40fff3a5a5843e3122fc3c
Adrien Servel <adrien.servel@filigran.io>
2024-03-21 10:05:01 +0100

    [frontend] Fix e2e testing

--- opencti-platform/opencti-front/tests_e2e/auth.setup.ts
+++ opencti-platform/opencti-front/tests_e2e/auth.setup.ts
@@ -1,11 +1,19 @@
 import { expect, test as setup } from './fixtures/baseFixtures';
 import DashboardPage from './model/dashboard.pageModel';
 import LoginPage from './model/login.pageModel';
+import * as fs from 'fs';
 
 const authFile = 'tests_e2e/.setup/.auth/user.json';
 const prepLogoutFile = 'tests_e2e/.setup/.auth/logout-user.json';
 
+// For quicker local testing, don't redo the auth if the seed is still valid
+const authSessionStorage = JSON.parse(fs.readFileSync(authFile, 'utf-8'));
+const logoutSessionStorage = JSON.parse(fs.readFileSync(prepLogoutFile, 'utf-8'));
+
 setup('authenticate', async ({ page }) => {
+  if (authSessionStorage.cookies[0].expires > (Date.now() / 1000)) {
+    return;
+  }
   const dashboardPage = new DashboardPage(page);
   const loginPage = new LoginPage(page);
   await loginPage.login();
@@ -15,6 +23,9 @@ setup('authenticate', async ({ page }) => {
 });
 
 setup('authenticate for logout user', async ({ page }) => {
+  if (logoutSessionStorage.cookies[0].expires > (Date.now() / 1000)) {
+    return;
+  }
   const dashboardPage = new DashboardPage(page);
   const loginPage = new LoginPage(page);
   await loginPage.login();
