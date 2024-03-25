76d5c063b4065680519b6ba68f2313b2ede6345c
Angelique <angelique.jard@filigran.io>
2024-03-25 17:53:29 +0100

    Module support on backend ok

--- opencti-platform/opencti-graphql/src/modules/support/support-domain.ts
+++ opencti-platform/opencti-graphql/src/modules/support/support-domain.ts
@@ -2,10 +2,15 @@ import fs from 'node:fs';
 import { logApp } from '../../config/conf';
 import type { AuthContext, AuthUser } from '../../types/user';
 import { createInternalObject } from '../../domain/internalObject';
-import { ENTITY_TYPE_SUPPORT_PACKAGE, type StoreEntitySupportPackage } from './support-types';
+import { type BasicStoreEntitySupportPackage, ENTITY_TYPE_SUPPORT_PACKAGE, type StoreEntitySupportPackage } from './support-types';
+import { storeLoadById } from '../../database/middleware-loader';
 
 export const addSupportPackage = async (context: AuthContext, user: AuthUser, input: any) => {
   return createInternalObject<StoreEntitySupportPackage>(context, user, input, ENTITY_TYPE_SUPPORT_PACKAGE);
+};
+
+export const findById = (context: AuthContext, user: AuthUser, id: string) => {
+  return storeLoadById<BasicStoreEntitySupportPackage>(context, user, id, ENTITY_TYPE_SUPPORT_PACKAGE);
 };
 
 export const findInLogs = () => {
