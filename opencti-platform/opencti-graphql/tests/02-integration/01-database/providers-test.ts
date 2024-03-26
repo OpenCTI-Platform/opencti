5251576fc8e8ba3e25ddf33f324bb27ec500997f
Angelique <angelique.jard@filigran.io>
2024-03-26 17:46:02 +0100

    Remove too much tests

--- opencti-platform/opencti-graphql/tests/02-integration/01-database/providers-test.ts
+++ opencti-platform/opencti-graphql/tests/02-integration/01-database/providers-test.ts
@@ -16,15 +16,15 @@ describe('initializeAdminUser configurations verifications', () => {
     initialToken = conf.get('app:admin:token');
   });
 
-  afterAll(async () => {
+  afterAll(() => {
     // reset to value that were set before running this test.
     conf.set('app:admin:email', initialEmail);
     conf.set('app:admin:password', initialPassword);
     conf.set('app:admin:token', initialToken);
-    await initializeAdminUser({});
+    initializeAdminUser({});
   });
 
-  it('should admin be initialized', async () => {
+  it('should well configured admin be initialized', async () => {
     // GIVEN configuration
     conf.set('app:admin:email', 'cecilia.payne@filigran.io');
     conf.set('app:admin:password', 'IDiscoveredUniverseMatter');
@@ -34,74 +34,6 @@ describe('initializeAdminUser configurations verifications', () => {
 
     const existingAdmin = await findById({}, SYSTEM_USER, OPENCTI_ADMIN_UUID);
     expect(existingAdmin.user_email).toBe('cecilia.payne@filigran.io');
-  });
-
-  it('should missing admin email raise exception', async () => {
-    // GIVEN configuration
-    conf.set('app:admin:email', '');
-    conf.set('app:admin:password', initialPassword);
-    conf.set('app:admin:token', initialToken);
-
-    let exceptionNotRaised = false;
-    try {
-      await initializeAdminUser({});
-      exceptionNotRaised = true;
-    } catch (error) {
-      expect(error).toBeDefined();
-    } finally {
-      expect(exceptionNotRaised, 'Having not set an email in env or yml should raise and exception').toBeFalsy();
-    }
-  });
-
-  it('should missing admin password raise exception', async () => {
-    // GIVEN configuration
-    conf.set('app:admin:email', initialEmail);
-    conf.set('app:admin:password', null);
-    conf.set('app:admin:token', initialToken);
-
-    let exceptionNotRaised = false;
-    try {
-      await initializeAdminUser({});
-      exceptionNotRaised = true;
-    } catch (error) {
-      expect(error).toBeDefined();
-    } finally {
-      expect(exceptionNotRaised, 'Having not set a password in env or yml should raise and exception').toBeFalsy();
-    }
-  });
-
-  it('should missing Password raise exception', async () => {
-    // GIVEN configuration
-    conf.set('app:admin:email', initialEmail);
-    conf.set('app:admin:password', initialPassword);
-    conf.set('app:admin:token', null);
-
-    let exceptionNotRaised = false;
-    try {
-      await initializeAdminUser({});
-      exceptionNotRaised = true;
-    } catch (error) {
-      expect(error).toBeDefined();
-    } finally {
-      expect(exceptionNotRaised, 'Having not set a Password in env or yml should raise and exception').toBeFalsy();
-    }
-  });
-
-  it('should missing token raise exception', async () => {
-    // GIVEN configuration
-    conf.set('app:admin:email', initialEmail);
-    conf.set('app:admin:password', initialPassword);
-    conf.set('app:admin:token', null);
-
-    let exceptionNotRaised = false;
-    try {
-      await initializeAdminUser({});
-      exceptionNotRaised = true;
-    } catch (error) {
-      expect(error).toBeDefined();
-    } finally {
-      expect(exceptionNotRaised, 'Having not set a Token in env or yml should raise and exception').toBeFalsy();
-    }
   });
 
   it('should password env with digit only works', async () => {
