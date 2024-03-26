891f4897df51b7988cf131db93778aea8b52400e
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-03-26 11:38:04 +0100

    [frontend] fix check-ts

--- opencti-platform/opencti-front/src/private/components/common/stix_core_relationships/EntityStixCoreRelationships.tsx
+++ opencti-platform/opencti-front/src/private/components/common/stix_core_relationships/EntityStixCoreRelationships.tsx
@@ -19,11 +19,11 @@ interface EntityStixCoreRelationshipsProps {
   defaultStartTime: string;
   defaultStopTime: string;
   relationshipTypes: string[];
-  stixCoreObjectTypes?: string[];
-  currentView?: string;
+  stixCoreObjectTypes: string[];
+  currentView: string;
   enableNestedView?: boolean;
-  enableContextualView?: boolean;
-  isRelationReversed?: boolean;
+  enableContextualView: boolean;
+  isRelationReversed: boolean;
   allDirections?: boolean;
   role?: string;
   paddingRightButtonAdd?: number;
