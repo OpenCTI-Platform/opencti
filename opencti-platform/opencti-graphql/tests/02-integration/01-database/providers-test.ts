5e7492df08b602393731c214859d3a9302076520
A. Jard <angelique.jard@filigran.io>
2024-03-28 03:34:56 +0100

    [backend] Allow passwords with digits only for initial admin (#6382)

---
+++ opencti-platform/opencti-graphql/tests/02-integration/01-database/providers-test.ts
@@ -0,0 +1,48 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import { initializeAdminUser } from '../../../src/config/providers';
+import conf from '../../../src/config/conf';
+import { SYSTEM_USER } from '../../../src/utils/access';
+import { OPENCTI_ADMIN_UUID } from '../../../src/schema/general';
+import { findById } from '../../../src/domain/user';
+
+describe('initializeAdminUser configurations verifications', () => {
+  let initialEmail: string;
+  let initialPassword: string;
+  let initialToken: string;
+
+  beforeAll(() => {
+    initialEmail = conf.get('app:admin:email');
+    initialPassword = conf.get('app:admin:password');
+    initialToken = conf.get('app:admin:token');
+  });
+
+  afterAll(() => {
+    // reset to value that were set before running this test.
+    conf.set('app:admin:email', initialEmail);
+    conf.set('app:admin:password', initialPassword);
+    conf.set('app:admin:token', initialToken);
+    initializeAdminUser({});
+  });
+
+  it('should well configured admin be initialized', async () => {
+    // GIVEN configuration
+    conf.set('app:admin:email', 'cecilia.payne@filigran.io');
+    conf.set('app:admin:password', 'IDiscoveredUniverseMatter');
+    conf.set('app:admin:token', 'aaaaaaaa-1111-2222-3333-999999999999');
+
+    await initializeAdminUser({});
+
+    const existingAdmin = await findById({}, SYSTEM_USER, OPENCTI_ADMIN_UUID);
+    expect(existingAdmin.user_email).toBe('cecilia.payne@filigran.io');
+  });
+
+  it('should password env with digit only works', async () => {
+    // GIVEN configuration
+    conf.set('app:admin:email', initialEmail);
+    conf.set('app:admin:password', 1111); // ENV var with digit only is interpreted as number by node.
+    conf.set('app:admin:token', initialToken);
+
+    await initializeAdminUser({});
+    // expect no exception, exception are failing tests so nothing to check more.
+  });
+});
