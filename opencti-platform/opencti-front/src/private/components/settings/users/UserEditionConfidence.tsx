6b3bbf538cd0d33e49ffe493100441b31f4e0949
BocognanoSarah <sarah.bocognano@filigran.io>
2024-04-15 11:58:40 +0200

    [frontend] Add mutation and submit (#issue/4900-front)

--- opencti-platform/opencti-front/src/private/components/settings/users/UserEditionConfidence.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/users/UserEditionConfidence.tsx
@@ -6,8 +6,11 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import { Add } from '@mui/icons-material';
 import makeStyles from '@mui/styles/makeStyles';
-import { useFormatter } from '../../../../components/i18n';
+import * as Yup from 'yup';
+import { useMutation } from 'react-relay';
+import { userEditionOverviewFocus, userMutationFieldPatch } from '@components/settings/users/UserEditionOverview';
 import { fieldSpacingContainerStyle } from '../../../../utils/field';
+import { useFormatter } from '../../../../components/i18n';
 
 const useStyles = makeStyles(() => ({
   createButton: {
@@ -25,13 +28,17 @@ interface UserEditionConfidenceProps {
   | null | undefined;
 }
 
-const onSubmit = () => {
-  console.log('onSubmit');
-};
-
-const onFocus = () => {
-  console.log('onFocus');
-};
+const userConfidenceValidation = (t: (value: string) => string) => Yup.object().shape({
+  user_confidence_level_enabled: Yup.boolean(),
+  user_confidence_level: Yup.number()
+    .min(0, t('The value must be greater than or equal to 0'))
+    .max(100, t('The value must be less than or equal to 100'))
+    .when('user_confidence_level_enabled', {
+      is: true,
+      then: (schema) => schema.required(t('This field is required')).nullable(),
+      otherwise: (schema) => schema.nullable(),
+    }),
+});
 
 const UserEditionConfidence: FunctionComponent<UserEditionConfidenceProps> = ({ user, context }) => {
   const { t_i18n } = useFormatter();
@@ -40,6 +47,77 @@ const UserEditionConfidence: FunctionComponent<UserEditionConfidenceProps> = ({ 
   const initialValues = {
     user_confidence_level_enabled: !!user.user_confidence_level,
     user_confidence_level: user.user_confidence_level?.max_confidence,
+  };
+
+  const [commitFocus] = useMutation(userEditionOverviewFocus);
+  const [commitFieldPatch] = useMutation(userMutationFieldPatch);
+
+  const handleChangeFocus = (name: string) => {
+    commitFocus({
+      variables: {
+        id: user.id,
+        input: {
+          focusOn: name,
+        },
+      },
+    });
+  };
+
+  const handleSubmitField = (name: string, value: string | null) => {
+    userConfidenceValidation(t_i18n)
+      .validateAt(name, { [name]: value })
+      .then(() => {
+        // specific case for user confidence level: to update an object we have several use-cases
+        if (name === 'user_confidence_level') {
+          if (user.user_confidence_level && value) {
+            // We edit an existing value inside the object: use object_path
+            commitFieldPatch({
+              variables: {
+                id: user.id,
+                input: {
+                  key: 'user_confidence_level',
+                  object_path: '/user_confidence_level/max_confidence',
+                  value: parseInt(value, 10),
+                },
+              },
+            });
+          } else if (!user.user_confidence_level && value) {
+            // We have no user_confidence_level and we add one: push a complete object
+            commitFieldPatch({
+              variables: {
+                id: user.id,
+                input: {
+                  key: 'user_confidence_level',
+                  value: {
+                    max_confidence: parseInt(value, 10),
+                    overrides: [],
+                  },
+                },
+              },
+            });
+          } else if (user.user_confidence_level && !value) {
+            // we have an existing value but we want to remove it: push [null] (and not null!)
+            commitFieldPatch({
+              variables: {
+                id: user.id,
+                input: {
+                  key: 'user_confidence_level',
+                  value: [null],
+                },
+              },
+            });
+          }
+        } else {
+          // simple case for all flat attributes
+          commitFieldPatch({
+            variables: {
+              id: user.id,
+              input: { key: name, value: value || '' },
+            },
+          });
+        }
+      })
+      .catch(() => false);
   };
 
   return (
@@ -53,8 +131,8 @@ const UserEditionConfidence: FunctionComponent<UserEditionConfidenceProps> = ({ 
           <UserConfidenceLevelField
             name="user_confidence_level"
             label={t_i18n('Max Confidence Level')}
-            onFocus={onFocus}
-            onSubmit={onSubmit}
+            onFocus={handleChangeFocus}
+            onSubmit={handleSubmitField}
             containerStyle={fieldSpacingContainerStyle}
             editContext={context}
             user={user}
