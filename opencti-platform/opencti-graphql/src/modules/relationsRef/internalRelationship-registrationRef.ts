80759382b2104d9e8e26297080f8175912977bc3
Valentin Bouzin <valentin.bouzin@filigran.io>
2024-04-22 10:37:56 +0200

    [backend] internal relationship ref rollback (#5797)

---
+++ opencti-platform/opencti-graphql/src/modules/relationsRef/internalRelationship-registrationRef.ts
@@ -0,0 +1,4 @@
+import { schemaRelationsRefDefinition } from '../../schema/schema-relationsRef';
+import { ABSTRACT_INTERNAL_RELATIONSHIP } from '../../schema/general';
+
+schemaRelationsRefDefinition.registerRelationsRef(ABSTRACT_INTERNAL_RELATIONSHIP, []);
