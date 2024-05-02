8d1c4dde8f7a1f9f5ffb1f41e0a3786f09c0aa1a
Souad Hadjiat <souad.hadjiat@filigran.io>
2024-05-02 15:36:34 +0200

    [backend] fix imports after rebase

--- opencti-platform/opencti-graphql/src/modules/deleteOperation/deleteOperation-domain.ts
+++ opencti-platform/opencti-graphql/src/modules/deleteOperation/deleteOperation-domain.ts
@@ -1,10 +1,7 @@
 import { type BasicStoreEntityDeleteOperation, ENTITY_TYPE_DELETE_OPERATION } from './deleteOperation-types';
-import { FunctionalError } from '../../config/errors';
-import { elDeleteInstances, elFindByIds } from '../../database/engine';
-import { deleteAllObjectFiles } from '../../database/file-storage-helper';
 import { FunctionalError, LockTimeoutError, TYPE_LOCK_ERROR } from '../../config/errors';
 import { elDeleteElements, elDeleteInstances, elFindByIds } from '../../database/engine';
-import { deleteAllObjectFiles } from '../../database/file-storage';
+import { deleteAllObjectFiles } from '../../database/file-storage-helper';
 import { listAllEntities, listEntitiesPaginated, storeLoadById } from '../../database/middleware-loader';
 import { INDEX_DELETED_OBJECTS, isNotEmptyField, READ_INDEX_DELETED_OBJECTS } from '../../database/utils';
 import { FilterMode, FilterOperator, OrderingMode, type QueryDeleteOperationsArgs } from '../../generated/graphql';
