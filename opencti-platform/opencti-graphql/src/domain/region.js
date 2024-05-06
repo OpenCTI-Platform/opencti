03427f9a4a79d40a2d81c1003feefc17beb36abb
Marie Flores <73853856+marieflorescontact@users.noreply.github.com>
2024-05-06 14:50:57 +0200

    [backend] Fix no country flag next to IP addresses  in observables list (#6888)

--- opencti-platform/opencti-graphql/src/domain/region.js
+++ opencti-platform/opencti-graphql/src/domain/region.js
@@ -24,7 +24,11 @@ export const childRegionsPaginated = async (context, user, regionId, args) => {
 };
 
 export const countriesPaginated = async (context, user, elementId, args) => {
-  return listEntitiesThroughRelationsPaginated(context, user, elementId, RELATION_LOCATED_AT, ENTITY_TYPE_LOCATION_COUNTRY, true, args);
+  const element = await findById(context, user, elementId);
+  if (element) {
+    return listEntitiesThroughRelationsPaginated(context, user, elementId, RELATION_LOCATED_AT, ENTITY_TYPE_LOCATION_COUNTRY, true, args);
+  }
+  return listEntitiesThroughRelationsPaginated(context, user, elementId, RELATION_LOCATED_AT, ENTITY_TYPE_LOCATION_COUNTRY, false, args);
 };
 
 export const addRegion = async (context, user, region) => {
