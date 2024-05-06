0ae995fc5ce0614f1eb5e2bc725a42f5fb27cc8d
Angelique <angelique.jard@filigran.io>
2024-05-06 20:00:39 +0200

    Forgot to remove an import

--- opencti-platform/opencti-graphql/src/modules/support/support-types.ts
+++ opencti-platform/opencti-graphql/src/modules/support/support-types.ts
@@ -1,7 +1,6 @@
 import type { BasicStoreEntity, StoreEntity } from '../../types/store';
 import type { StixObject, StixOpenctiExtensionSDO } from '../../types/stix-common';
 import { STIX_EXT_OCTI } from '../../types/stix-extensions';
-import type { SupportNodeStatus } from '../../generated/graphql';
 
 export const ENTITY_TYPE_SUPPORT_PACKAGE = 'Support-Package';
 export const SUPPORT_BUS = 'SupportBus';
