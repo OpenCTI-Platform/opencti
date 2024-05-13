19384c805c65c87e33ebd09827e391aba76a6394
Adrien Servel <adrien.servel@filigran.io>
2024-05-13 09:23:09 +0200

    [frontend] Fix double encoding on search

---
+++ opencti-platform/opencti-front/src/utils/SearchUtils.ts
@@ -0,0 +1,16 @@
+import { NavigateFunction } from 'react-router/dist/lib/hooks';
+
+export const handleSearchByKeyword = (searchKeyword: string, searchScope: string, navigate: NavigateFunction) => {
+  const encodeKey = encodeURIComponent(searchKeyword);
+  navigate(`/dashboard/search/${searchScope}/${encodeKey}`);
+};
+
+export const decodeSearchKeyword = (searchKeyword: string) => {
+  let searchTerm = '';
+  try {
+    searchTerm = decodeURIComponent(searchKeyword || '');
+  } catch (e) {
+    // Do nothing
+  }
+  return searchTerm;
+};
