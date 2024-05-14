679f1d8589102a018a8829b81ce3ada36c13d8be
FrancoisGrunert <francois.grunert@filigran.io>
2024-05-14 09:40:11 +0200

    [frontend] use complete path (#6383)

--- opencti-platform/opencti-front/src/private/components/events/incidents/Root.tsx
+++ opencti-platform/opencti-front/src/private/components/events/incidents/Root.tsx
@@ -84,8 +84,8 @@ const RootIncidentComponent = ({ queryRef }) => {
   const { incident, connectorsForImport, connectorsForExport } = data;
   const isOverview = location.pathname === `/dashboard/events/incidents/${incident.id}`;
   const paddingRightValue = () => {
-    if (location.pathname.includes(`/knowledge`)) return 200;
-    if (location.pathname.includes(`/content`)) return 350;
+    if (location.pathname.includes(`/dashboard/events/incidents/${incident.id}/knowledge`)) return 200;
+    if (location.pathname.includes(`/dashboard/events/incidents/${incident.id}/content`)) return 350;
     return 0;
   };
   return (
