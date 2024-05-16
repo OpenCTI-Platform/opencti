c57feb255ff6774cf27265e639bbd6252217ef4e
Cathia Archidoit <cathia.archidoit@filigran.io>
2024-05-16 16:31:59 +0200

    [frontend] fixes

--- opencti-platform/opencti-front/tests_e2e/filters/addFilter.spec.ts
+++ opencti-platform/opencti-front/tests_e2e/filters/addFilter.spec.ts
@@ -2,19 +2,18 @@ import { expect, test } from '../fixtures/baseFixtures';
 import FiltersPageModel from '../model/filters.pageModel';
 import ReportPage from '../model/report.pageModel';
 
-test('Add a new filter in the observables list and check the filter is still present when we come back to the page', async ({ page }) => {
+test('Add filters with different operators', async ({ page }) => {
+  // Add a new filter in the observables list and check the filter is still present when we come back to the page
   await page.goto('/dashboard/observations/observables');
-  const filterUtils = new FiltersPageModel(page);
+  let filterUtils = new FiltersPageModel(page);
   await filterUtils.addFilterFromList('Entity type', 'Artifact');
   await expect(page.getByRole('button', { name: 'Entity type = Artifact' })).toBeVisible();
   await page.goto('/dashboard/');
   await page.goto('/dashboard/observations/observables');
   await expect(page.getByRole('button', { name: 'Entity type = Artifact' })).toBeVisible();
-});
-
-test('Add different filters with different operators', async ({ page }) => {
+  // Add different filters with different operators
   await page.goto('/dashboard/observations/observables');
-  const filterUtils = new FiltersUtils(page);
+  filterUtils = new FiltersUtils(page);
   await filterUtils.addFilterFromDate('Original creation date', '12', 'Lower than');
   await filterUtils.addFilterFromTextAndOperator('Value', 'Starts with', 'Empty');
   await filterUtils.addFilterFromTextAndOperator('Value', 'Starts with', 'Contains', 'content to test');
@@ -22,21 +21,17 @@ test('Add different filters with different operators', async ({ page }) => {
   await expect(page.getByRole('button', { name: 'Modification date < 12' })).toBeVisible();
   await expect(page.getByRole('button', { name: 'Value is empty' })).toBeVisible();
   await expect(page.getByRole('button', { name: 'Value contains : content to test' })).toBeVisible();
-});
-
-test('Check entity type background tasks filter is correct', async ({ page }) => {
-  const reportPage = new ReportPage(page);
+  // Check entity type background tasks filter is correct
+  let reportPage = new ReportPage(page);
   await page.goto('/dashboard/analyses/reports');
   await reportPage.selectAllReports(); // check all the reports
   await page.getByRole('button', { name: 'delete' }).click(); // delete them via the toolbar
   await expect(page.getByText('Entity type: Report')).toBeVisible(); // the 'Entity type: Report' filter should be present
   await page.getByRole('button', { name: 'Cancel' }).click(); // don't launch the background task
-});
-
-test('Check added filter is present in background task', async ({ page }) => {
-  const reportPage = new ReportPage(page);
+  // Check added filter is present in background task
+  reportPage = new ReportPage(page);
   await page.goto('/dashboard/analyses/reports');
-  const filterUtils = new FiltersUtils(page);
+  filterUtils = new FiltersUtils(page);
   await filterUtils.addFilterFromText('Modification date', '01/01/2020');
   await reportPage.selectAllReports(); // check all the reports
   await page.getByRole('button', { name: 'delete' }).click(); // delete them via the toolbar
