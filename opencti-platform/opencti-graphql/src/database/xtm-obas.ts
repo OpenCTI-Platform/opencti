b2024b6c411b96a4bc568962add2b5f8d3b03c33
Romuald Lemesle <romu.lemesle@gmail.com>
2024-05-29 16:17:22 +0200

    [backend] Fix error in OpenBas scenario generation (#7169)

--- opencti-platform/opencti-graphql/src/database/xtm-obas.ts
+++ opencti-platform/opencti-graphql/src/database/xtm-obas.ts
@@ -140,7 +140,7 @@ export const createInjectInScenario = async (
       {
         inject_injector_contract: contractId,
         inject_type: injectorType,
-        inject_title: title,
+        inject_title: title.length > 255 ? `${title.substring(0, 250)}...` : title,
         inject_depends_duration: dependsDuration,
         inject_content: content,
         inject_tags: obasTagsIds,
