753adeab8b3db43829123e27e7ce854cba048133
FrancoisGrunert <francois.grunert@filigran.io>
2024-06-03 14:19:04 +0200

    [frontend/backend] add 4 new settings capabilities (#5550)

--- opencti-platform/opencti-graphql/src/database/data-initialization.js
+++ opencti-platform/opencti-graphql/src/database/data-initialization.js
@@ -64,10 +64,9 @@ export const SETTINGS_CAPABILITIES = {
     { name: 'SETMARKINGS', description: 'Manage marking definitions', attribute_order: 3300 },
     { name: 'SETLABELS', description: 'Manage labels & Attributes', attribute_order: 3400 },
     { name: 'SECURITYACTIVITY', description: 'Access Security Activity', attribute_order: 3500 },
-    { name: 'KNOWLEDGEACTIVITY', description: 'Knowledge Activity', attribute_order: 3600 },
-    { name: 'SETTAXONOMIES', description: 'Manage taxonomies', attribute_order: 3700 },
-    { name: 'FILEINDEXING', description: 'Access to file indexing', attribute_order: 3800 },
-    { name: 'SUPPORT', description: 'Access to support data', attribute_order: 3900 },
+    { name: 'SETTAXONOMIES', description: 'Manage taxonomies', attribute_order: 3600 },
+    { name: 'FILEINDEXING', description: 'Access to file indexing', attribute_order: 3700 },
+    { name: 'SUPPORT', description: 'Access to support data', attribute_order: 3800 },
   ],
 };
 export const CAPABILITIES = [
