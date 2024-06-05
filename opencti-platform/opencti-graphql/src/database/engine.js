4da1e98dc9eb8eed9a2701ed0278d630c7022a62
Souad Hadjiat <souad.hadjiat@filigran.io>
2024-06-05 15:23:28 +0200

    [backend] Fix reindexing fail error (#7031)

--- opencti-platform/opencti-graphql/src/database/engine.js
+++ opencti-platform/opencti-graphql/src/database/engine.js
@@ -3258,7 +3258,7 @@ export const elReindexElements = async (context, user, ids, sourceIndex, destInd
         index: destIndex
       },
       script: { // remove old fields that are not mapped anymore but can be present in DB
-        source: "ctx._source.remove('fromType'); ctx._source.remove('toType'); ctx._source.remove('spec_version');"
+        source: "ctx._source.remove('fromType'); ctx._source.remove('toType'); ctx._source.remove('spec_version'); ctx._source.remove('representative');"
       },
     }
   };
