abce5a314018cebb8a3b35100e1801213c2ad0e9
Laurent Bonnet <laurent.bonnet@filigran.io>
2024-06-19 10:13:35 +0200

    [client] remove deprecated API from examples (now in tests)
    (original commit: OpenCTI-Platform/client-python@e3827ba)

--- examples/promote_observable_to_indicator.py
+++ examples/promote_observable_to_indicator.py
@@ -9,20 +9,10 @@ api_token = "bfa014e0-e02e-4aa6-a42b-603b19dcf159"
 # OpenCTI initialization
 opencti_api_client = OpenCTIApiClient(api_url, api_token)
 
-# DEPRECATED >=6.2 & <6.5. Use `promote_to_indicator_v2` instead
-obs1 = opencti_api_client.stix_cyber_observable.create(
-    simple_observable_key="IPv4-Addr.value", simple_observable_value="55.55.55.55"
-)
-observable = opencti_api_client.stix_cyber_observable.promote_to_indicator(
-    id=obs1.get("id")
-)
-print("promoted observable, original observable is", observable)
-
-# Please use promote_to_indicator_v2
-obs2 = opencti_api_client.stix_cyber_observable.create(
+obs = opencti_api_client.stix_cyber_observable.create(
     simple_observable_key="IPv4-Addr.value", simple_observable_value="55.55.55.99"
 )
 indicator = opencti_api_client.stix_cyber_observable.promote_to_indicator_v2(
-    id=obs2.get("id")
+    id=obs.get("id")
 )
-print("promoted observable, indicator is", indicator)
+print("promoted observable, new indicator is", indicator)
