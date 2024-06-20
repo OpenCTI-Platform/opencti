6a234ba59174945349985704bec4fe5c9078db35
Bonsai8863 <131906254+Bonsai8863@users.noreply.github.com>
2024-06-20 12:43:07 -0400

    [frontend] Add page titles to Settings pages
    Revert WIP conversion of Sectors
    
    test commit
    
    Fix paginationOptions issue for Groups, KillChainPhases, Roles
    
    Fix MarkingDefinitions, update KillChainPhases to reflect master
    
    Remove changes established in PR #7753
    
    Add Malwares.tsx to removed changes
    
    Post-rebase cleanup
    
    Remove changes from PR 7753
    
    Remove outdated changes to KillChainPhases
    
    Fix translations
    
    Align GroupLine design with master
    
    Fix typing on GroupLines;
    
    Remove variant from breadcrumbs

--- opencti-platform/opencti-front/src/private/components/settings/Sessions.jsx
+++ opencti-platform/opencti-front/src/private/components/settings/Sessions.jsx
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
-import * as PropTypes from 'prop-types';
-import { compose, propOr } from 'ramda';
-import withStyles from '@mui/styles/withStyles';
+import React from 'react';
+import makeStyles from '@mui/styles/makeStyles';
 import AccessesMenu from './AccessesMenu';
-import inject18n from '../../../components/i18n';
 import { QueryRenderer } from '../../../relay/environment';
 import SessionsList, { sessionsListQuery } from './SessionsList';
 import SearchInput from '../../../components/SearchInput';
-import { buildViewParamsFromUrlAndStorage, saveViewParameters } from '../../../utils/ListParameters';
-import withRouter from '../../../utils/compat_router/withRouter';
 import Breadcrumbs from '../../../components/Breadcrumbs';
+import { useFormatter } from '../../../components/i18n';
+import { usePaginationLocalStorage } from '../../../utils/hooks/useLocalStorage';
+import useConnectedDocumentModifier from '../../../utils/hooks/useConnectedDocumentModifier';
 
-const styles = () => ({
+const useStyles = makeStyles(() => ({
   container: {
     margin: 0,
     padding: '0 200px 50px 0',
@@ -20,73 +18,44 @@ const styles = () => ({
     float: 'left',
     marginTop: -10,
   },
-});
+}));
 
 const LOCAL_STORAGE_KEY = 'sessions';
 
-class Sessions extends Component {
-  constructor(props) {
-    super(props);
-    const params = buildViewParamsFromUrlAndStorage(
-      props.navigate,
-      props.location,
-      LOCAL_STORAGE_KEY,
-    );
-    this.state = {
-      searchTerm: propOr('', 'searchTerm', params),
-      openExports: false,
-    };
-  }
-
-  saveView() {
-    saveViewParameters(
-      this.props.navigate,
-      this.props.location,
-      LOCAL_STORAGE_KEY,
-      this.state,
-    );
-  }
-
-  handleSearch(value) {
-    this.setState({ searchTerm: value }, () => this.saveView());
-  }
-
-  render() {
-    const { searchTerm } = this.state;
-    const { t, classes } = this.props;
-    return (
-      <div className={classes.container}>
-        <AccessesMenu />
-        <Breadcrumbs elements={[{ label: t('Settings') }, { label: t('Security') }, { label: t('Sessions'), current: true }]} />
-        <div className={classes.parameters}>
-          <div style={{ float: 'left', marginRight: 20 }}>
-            <SearchInput
-              variant="small"
-              onSubmit={this.handleSearch.bind(this)}
-              keyword={searchTerm}
-            />
-          </div>
+const Sessions = () => {
+  const classes = useStyles();
+  const { t_i18n } = useFormatter();
+  const { setTitle } = useConnectedDocumentModifier();
+  setTitle(t_i18n('Security: Sessions | Settings'));
+  const { viewStorage, helpers } = usePaginationLocalStorage(
+    LOCAL_STORAGE_KEY,
+    {},
+  );
+  return (
+    <div className={classes.container}>
+      <AccessesMenu />
+      <Breadcrumbs elements={[{ label: t_i18n('Settings') }, { label: t_i18n('Security') }, { label: t_i18n('Sessions'), current: true }]} />
+      <div className={classes.parameters}>
+        <div style={{ float: 'left', marginRight: 20 }}>
+          <SearchInput
+            variant="small"
+            onSubmit={helpers.handleSearch}
+            keyword={viewStorage.searchTerm ?? ''}
+          />
         </div>
-        <div className="clearfix" />
-        <QueryRenderer
-          query={sessionsListQuery}
-          render={({ props }) => {
-            if (props) {
-              return <SessionsList data={props} keyword={searchTerm} />;
-            }
-            return <div />;
-          }}
-        />
       </div>
-    );
-  }
-}
-
-Sessions.propTypes = {
-  t: PropTypes.func,
-  classes: PropTypes.object,
-  navigate: PropTypes.func,
-  location: PropTypes.object,
+      <div className="clearfix" />
+      <QueryRenderer
+        query={sessionsListQuery}
+        render={({ props }) => {
+          if (props) {
+            return <SessionsList data={props} keyword={viewStorage.searchTerm ?? ''} />;
+          }
+          return <div />;
+        }}
+      />
+    </div>
+  );
 };
 
-export default compose(inject18n, withRouter, withStyles(styles))(Sessions);
+export default Sessions;
--- opencti-platform/opencti-front/src/private/components/settings/SessionsList.jsx
+++ opencti-platform/opencti-front/src/private/components/settings/SessionsList.jsx
@@ -1,8 +1,6 @@
-import React, { Component } from 'react';
-import * as PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
 import * as R from 'ramda';
 import { Link } from 'react-router-dom';
-import withStyles from '@mui/styles/withStyles';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
@@ -17,17 +15,17 @@ import Button from '@mui/material/Button';
 import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction';
 import IconButton from '@mui/material/IconButton';
 import { graphql, createRefetchContainer } from 'react-relay';
-import inject18n from '../../../components/i18n';
+import makeStyles from '@mui/styles/makeStyles';
 import { commitMutation } from '../../../relay/environment';
 import { FIVE_SECONDS, timestamp } from '../../../utils/Time';
 import { userSessionKillMutation } from './users/User';
 import ItemIcon from '../../../components/ItemIcon';
 import Transition from '../../../components/Transition';
-import withRouter from '../../../utils/compat_router/withRouter';
+import { useFormatter } from '../../../components/i18n';
 
 const interval$ = interval(FIVE_SECONDS);
 
-const styles = (theme) => ({
+const useStyles = makeStyles((theme) => ({
   item: {},
   itemNested: {
     paddingLeft: theme.spacing(4),
@@ -76,187 +74,167 @@ const styles = (theme) => ({
     position: 'absolute',
     right: -10,
   },
-});
+}));
 
-class SessionsListComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayUpdate: false,
-      displayKillSession: false,
-      killing: false,
-      sessionToKill: null,
+const SessionsListComponent = ({ relay, data, keyword }) => {
+  const classes = useStyles();
+  const { t_i18n, nsdt } = useFormatter();
+  const [displayKillSession, setDisplayKillSession] = useState(false);
+  const [killing, setKilling] = useState(false);
+  const [sessionToKill, setSessionToKill] = useState(null);
+
+  useEffect(() => {
+    const subscription = interval$.subscribe(() => relay.refetch());
+    return () => {
+      subscription.unsubscribe();
     };
-  }
+  }, []);
 
-  componentDidMount() {
-    this.subscription = interval$.subscribe(() => {
-      this.props.relay.refetch();
-    });
-  }
+  const handleOpenKillSession = (session) => {
+    setDisplayKillSession(true);
+    setSessionToKill(session);
+  };
 
-  componentWillUnmount() {
-    this.subscription.unsubscribe();
-  }
+  const handleCloseKillSession = () => {
+    setDisplayKillSession(false);
+    setSessionToKill(null);
+  };
 
-  handleOpenKillSession(session) {
-    this.setState({ displayKillSession: true, sessionToKill: session });
-  }
-
-  handleCloseKillSession() {
-    this.setState({ displayKillSession: false, sessionToKill: null });
-  }
-
-  submitKillSession() {
-    this.setState({ killing: true });
+  const submitKillSession = () => {
+    setKilling(true);
     commitMutation({
       mutation: userSessionKillMutation,
       variables: {
-        id: this.state.sessionToKill,
+        id: sessionToKill,
       },
       onCompleted: () => {
-        this.setState({ killing: false });
-        this.handleCloseKillSession();
+        setKilling(false);
+        handleCloseKillSession();
       },
       onError: () => {
-        this.setState({ killing: false });
+        setKilling(false);
       },
     });
-  }
+  };
 
-  render() {
-    const { classes, nsdt, t, data, keyword } = this.props;
-    const sortByNameCaseInsensitive = R.sortBy(
-      R.compose(R.toLower, R.path(['user', 'name'])),
-    );
-    const filterByKeyword = (n) => keyword === ''
-      || n.user.name.toLowerCase().indexOf(keyword.toLowerCase()) !== -1;
-    const sessions = R.pipe(
-      R.propOr([], 'sessions'),
-      R.filter(filterByKeyword),
-      sortByNameCaseInsensitive,
-    )(data);
-    return (
-      <>
-        <List
-          component="nav"
-          aria-labelledby="nested-list-subheader"
-          className={classes.root}
-        >
-          {sessions.map((session) => {
-            const { user, sessions: userSessions } = session;
-            const orderedSessions = R.sort(
-              (a, b) => timestamp(a.created) - timestamp(b.created),
-              userSessions,
-            );
-            return (
-              <div key={session.user.id}>
-                <ListItem
-                  classes={{ root: classes.item }}
-                  divider={true}
-                  button={true}
-                  component={Link}
-                  to={`/dashboard/settings/accesses/users/${user.id}`}
-                >
-                  <ListItemIcon>
-                    <ItemIcon type="User" />
-                  </ListItemIcon>
-                  <ListItemText
-                    primary={
-                      <div>
-                        <div className={classes.name}>{user.name}</div>
-                        <div className={classes.email}>{user.email}</div>
-                      </div>
-                    }
-                  />
-                  <ListItemIcon classes={{ root: classes.goIcon }}>
-                    &nbsp;
-                  </ListItemIcon>
-                </ListItem>
-                <List style={{ margin: 0, padding: 0 }}>
-                  {orderedSessions.map((userSession) => (
-                    <ListItem
-                      key={userSession.id}
-                      classes={{ root: classes.itemNested }}
-                      divider={true}
-                    >
-                      <ListItemIcon>
-                        <ItemIcon type="Session" />
-                      </ListItemIcon>
-                      <ListItemText
-                        primary={
-                          <div>
-                            <div className={classes.created}>
-                              {nsdt(userSession.created)}
-                            </div>
-                            <div className={classes.ttl}>
-                              {Math.round(userSession.ttl / 60)}{' '}
-                              {t('minutes left')} /{' '}
-                              {Math.round(userSession.originalMaxAge / 60)}
-                            </div>
+  const sortByNameCaseInsensitive = R.sortBy(
+    R.compose(R.toLower, R.path(['user', 'name'])),
+  );
+  const filterByKeyword = (n) => keyword === ''
+    || n.user.name.toLowerCase().indexOf(keyword.toLowerCase()) !== -1;
+  const sessions = R.pipe(
+    R.propOr([], 'sessions'),
+    R.filter(filterByKeyword),
+    sortByNameCaseInsensitive,
+  )(data);
+  return (
+    <>
+      <List
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        className={classes.root}
+      >
+        {sessions.map((session) => {
+          const { user, sessions: userSessions } = session;
+          const orderedSessions = R.sort(
+            (a, b) => timestamp(a.created) - timestamp(b.created),
+            userSessions,
+          );
+          return (
+            <div key={session.user.id}>
+              <ListItem
+                classes={{ root: classes.item }}
+                divider={true}
+                button={true}
+                component={Link}
+                to={`/dashboard/settings/accesses/users/${user.id}`}
+              >
+                <ListItemIcon>
+                  <ItemIcon type="User" />
+                </ListItemIcon>
+                <ListItemText
+                  primary={
+                    <div>
+                      <div className={classes.name}>{user.name}</div>
+                      <div className={classes.email}>{user.email}</div>
+                    </div>
+                  }
+                />
+                <ListItemIcon classes={{ root: classes.goIcon }}>
+                  &nbsp;
+                </ListItemIcon>
+              </ListItem>
+              <List style={{ margin: 0, padding: 0 }}>
+                {orderedSessions.map((userSession) => (
+                  <ListItem
+                    key={userSession.id}
+                    classes={{ root: classes.itemNested }}
+                    divider={true}
+                  >
+                    <ListItemIcon>
+                      <ItemIcon type="Session" />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={
+                        <div>
+                          <div className={classes.created}>
+                            {nsdt(userSession.created)}
                           </div>
-                        }
-                      />
-                      <ListItemSecondaryAction>
-                        <IconButton
-                          aria-label="Kill"
-                          onClick={this.handleOpenKillSession.bind(
-                            this,
-                            userSession.id,
-                          )}
-                          size="large"
-                          color="primary"
-                        >
-                          <DeleteOutlined />
-                        </IconButton>
-                      </ListItemSecondaryAction>
-                    </ListItem>
-                  ))}
-                </List>
-              </div>
-            );
-          })}
-        </List>
-        <Dialog
-          open={this.state.displayKillSession}
-          PaperProps={{ elevation: 1 }}
-          keepMounted={true}
-          TransitionComponent={Transition}
-          onClose={this.handleCloseKillSession.bind(this)}
-        >
-          <DialogContent>
-            <DialogContentText>
-              {t('Do you want to kill this session?')}
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button
-              onClick={this.handleCloseKillSession.bind(this)}
-              disabled={this.state.killing}
-            >
-              {t('Cancel')}
-            </Button>
-            <Button
-              onClick={this.submitKillSession.bind(this)}
-              color="primary"
-              disabled={this.state.killing}
-            >
-              {t('Delete')}
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </>
-    );
-  }
-}
-
-SessionsListComponent.propTypes = {
-  t: PropTypes.func,
-  classes: PropTypes.object,
-  navigate: PropTypes.func,
-  location: PropTypes.object,
-  nsdt: PropTypes.func,
-  data: PropTypes.object,
+                          <div className={classes.ttl}>
+                            {Math.round(userSession.ttl / 60)}{' '}
+                            {t_i18n('minutes left')} /{' '}
+                            {Math.round(userSession.originalMaxAge / 60)}
+                          </div>
+                        </div>
+                      }
+                    />
+                    <ListItemSecondaryAction>
+                      <IconButton
+                        aria-label="Kill"
+                        onClick={() => handleOpenKillSession(userSession.id)}
+                        size="large"
+                        color="primary"
+                      >
+                        <DeleteOutlined />
+                      </IconButton>
+                    </ListItemSecondaryAction>
+                  </ListItem>
+                ))}
+              </List>
+            </div>
+          );
+        })}
+      </List>
+      <Dialog
+        open={displayKillSession}
+        PaperProps={{ elevation: 1 }}
+        keepMounted={true}
+        TransitionComponent={Transition}
+        onClose={handleCloseKillSession}
+      >
+        <DialogContent>
+          <DialogContentText>
+            {t_i18n('Do you want to kill this session?')}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={handleCloseKillSession}
+            disabled={killing}
+          >
+            {t_i18n('Cancel')}
+          </Button>
+          <Button
+            onClick={submitKillSession}
+            color="primary"
+            disabled={killing}
+          >
+            {t_i18n('Delete')}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 };
 
 export const sessionsListQuery = graphql`
@@ -265,7 +243,7 @@ export const sessionsListQuery = graphql`
   }
 `;
 
-const SessionsList = createRefetchContainer(
+export default createRefetchContainer(
   SessionsListComponent,
   {
     data: graphql`
@@ -287,9 +265,3 @@ const SessionsList = createRefetchContainer(
   },
   sessionsListQuery,
 );
-
-export default R.compose(
-  inject18n,
-  withRouter,
-  withStyles(styles),
-)(SessionsList);
