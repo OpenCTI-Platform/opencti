92cdff5502cb50624c304d5833a90d1845772699
Jeremy Cloarec <159018898+JeremyCloarec@users.noreply.github.com>
2024-06-20 11:38:03 +0200

    [backend] Fix handling of platform_entity_files_ref config (#7406)

--- opencti-platform/opencti-graphql/src/modules/attributes/stixMetaObject-registrationAttributes.ts
+++ opencti-platform/opencti-graphql/src/modules/attributes/stixMetaObject-registrationAttributes.ts
@@ -23,6 +23,7 @@ const stixMetaObjectsAttributes: { [k: string]: Array<AttributeDefinition> } = {
     { name: 'description', label: 'Description', type: 'string', format: 'text', mandatoryType: 'no', editDefault: false, multiple: false, upsert: true, isFilterable: true },
     { name: 'url', label: 'URL', type: 'string', format: 'short', mandatoryType: 'no', editDefault: false, multiple: false, upsert: false, isFilterable: true },
     { name: 'hash', label: 'Hash', type: 'string', format: 'short', mandatoryType: 'no', editDefault: false, multiple: false, upsert: false, isFilterable: false },
+    { name: 'fileId', label: 'File ID', type: 'string', format: 'short', mandatoryType: 'no', editDefault: false, multiple: false, upsert: false, isFilterable: false },
     { name: 'external_id', label: 'External id', type: 'string', format: 'short', mandatoryType: 'no', editDefault: false, multiple: false, upsert: false, isFilterable: false },
   ],
   [ENTITY_TYPE_KILL_CHAIN_PHASE]: [
