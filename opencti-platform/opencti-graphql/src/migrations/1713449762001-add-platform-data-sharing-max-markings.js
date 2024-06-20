53b88d9341b1087d88f578245644fbe463fab8de
Archidoit <75783086+Archidoit@users.noreply.github.com>
2024-06-20 09:39:33 +0200

    [backend] fix max shareable markings migration (#7392)

--- opencti-platform/opencti-graphql/src/migrations/1713449762001-add-platform-data-sharing-max-markings.js
+++ opencti-platform/opencti-graphql/src/migrations/1713449762001-add-platform-data-sharing-max-markings.js
@@ -1,15 +1,21 @@
-import { executionContext, SYSTEM_USER } from '../utils/access';
-import { getSettings, settingsEditField } from '../domain/settings';
-import { logApp } from '../config/conf';
+// import { executionContext, SYSTEM_USER } from '../utils/access';
+// import { getSettings, settingsEditField } from '../domain/settings';
+// import { logApp } from '../config/conf';
 
 export const up = async (next) => {
-  logApp.info('[MIGRATION] Add platform data sharing max markings');
-  const context = executionContext('migration');
+  // ----- Explanations ----------
+  // This migration has been removed
+  // because platform_data_sharing_max_markings does not exist anymore since version 6.1.11
+  // -----------------------------
+
+  // -------- Old code -----------
+  // logApp.info('[MIGRATION] Add platform data sharing max markings');
+  // const context = executionContext('migration');
   // ------ Add platform_data_sharing_max_markings
-  const settings = await getSettings(context);
-  const patch = [{ key: 'platform_data_sharing_max_markings', value: [] }];
-  await settingsEditField(context, SYSTEM_USER, settings.id, patch);
-  logApp.info('[MIGRATION] Add platform data sharing max markings done.');
+  // const settings = await getSettings(context);
+  // const patch = [{ key: 'platform_data_sharing_max_markings', value: [] }];
+  // await settingsEditField(context, SYSTEM_USER, settings.id, patch);
+  // logApp.info('[MIGRATION] Add platform data sharing max markings done.');
   next();
 };
 
