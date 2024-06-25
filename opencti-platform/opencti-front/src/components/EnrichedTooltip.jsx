c7466ad13696423205b76dd7ee3a11037122359c
Bonsai8863 <131906254+Bonsai8863@users.noreply.github.com>
2024-06-25 10:08:52 -0400

    [frontend] Modified EnrichedTooltip

--- opencti-platform/opencti-front/src/components/EnrichedTooltip.jsx
+++ opencti-platform/opencti-front/src/components/EnrichedTooltip.jsx
@@ -4,10 +4,14 @@ import { styled } from '@mui/material/styles';
 
 const EnrichedTooltip = styled(({ className, ...props }) => (
   <Tooltip {...props} classes={{ popper: className }} />
-))(() => ({
+))(({ theme }) => ({
   [`& .${tooltipClasses.tooltip}`]: {
     width: 400,
     padding: '20px 20px 10px 20px',
+    backgroundColor: theme.palette.mode === 'light'
+      ? theme.palette.common.white
+      : theme.palette.common.black,
+    boxShadow: theme.shadows[1],
   },
   [`& .${tooltipClasses.popper}`]: {},
 }));
