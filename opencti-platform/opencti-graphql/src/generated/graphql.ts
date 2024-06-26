64d72aa4b20e6fbb710dae26e9d899aa3d1f32ff
Helene Nguyen <helene.nguyen@filigran.io>
2024-06-26 09:23:07 +0200

    [frontend/backend] Fix generated schema

--- opencti-platform/opencti-graphql/src/generated/graphql.ts
+++ opencti-platform/opencti-graphql/src/generated/graphql.ts
@@ -3465,8 +3465,8 @@ export type ConnectorMetadata = {
 
 export type ConnectorQueueDetails = {
   __typename?: 'ConnectorQueueDetails';
-  messages_number: Scalars['Int']['output'];
-  messages_size: Scalars['Int']['output'];
+  messages_number: Scalars['Float']['output'];
+  messages_size: Scalars['Float']['output'];
 };
 
 export enum ConnectorType {
@@ -31420,8 +31420,8 @@ export type ConnectorMetadataResolvers<ContextType = any, ParentType extends Res
 }>;
 
 export type ConnectorQueueDetailsResolvers<ContextType = any, ParentType extends ResolversParentTypes['ConnectorQueueDetails'] = ResolversParentTypes['ConnectorQueueDetails']> = ResolversObject<{
-  messages_number?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
-  messages_size?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  messages_number?: Resolver<ResolversTypes['Float'], ParentType, ContextType>;
+  messages_size?: Resolver<ResolversTypes['Float'], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 }>;
 
