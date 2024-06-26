d9d64c4d301f53f67e27fbd00480518cab5d9802
Gwendoline Favre-Felix <gwendoline.favre-felix@filigran.io>
2024-06-26 09:28:09 +0200

    [frontend] Make nullable first seen and last seen (#7268)

--- opencti-platform/opencti-front/src/private/components/events/stix_sighting_relationships/StixSightingRelationshipEditionOverview.tsx
+++ opencti-platform/opencti-front/src/private/components/events/stix_sighting_relationships/StixSightingRelationshipEditionOverview.tsx
@@ -224,10 +224,10 @@ const StixSightingRelationshipEditionOverviewComponent: FunctionComponent<Omit<S
       .required(t_i18n('This field is required')),
     first_seen: Yup.date()
       .typeError(t_i18n('The value must be a datetime (yyyy-MM-dd hh:mm (a|p)m)'))
-      .required(t_i18n('This field is required')),
+      .nullable(),
     last_seen: Yup.date()
       .typeError(t_i18n('The value must be a datetime (yyyy-MM-dd hh:mm (a|p)m)'))
-      .required(t_i18n('This field is required')),
+      .nullable(),
     description: Yup.string().nullable(),
     x_opencti_negative: Yup.boolean(),
     x_opencti_workflow_id: Yup.object(),
@@ -250,8 +250,8 @@ const StixSightingRelationshipEditionOverviewComponent: FunctionComponent<Omit<S
     const inputValues = Object.entries({
       ...otherValues,
       confidence: parseInt(String(values.confidence), 10),
-      first_seen: formatDate(values.first_seen),
-      last_seen: formatDate(values.last_seen),
+      first_seen: values.first_seen ? formatDate(values.first_seen) : null,
+      last_seen: values.last_seen ? formatDate(values.last_seen) : null,
       x_opencti_workflow_id: values.x_opencti_workflow_id?.value,
       createdBy: values.createdBy?.value,
       objectMarking: (values.objectMarking ?? []).map(({ value }) => value),
