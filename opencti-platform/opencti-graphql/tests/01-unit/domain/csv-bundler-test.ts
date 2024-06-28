41cd957efd562ae3c5eb3ea29fdcbf67f3139855
Romy Alula <romy.alula@filigran.io>
2024-06-28 12:41:29 +0200

    [backend] Test kill chain phases import (#7036)

--- opencti-platform/opencti-graphql/tests/01-unit/domain/csv-bundler-test.ts
+++ opencti-platform/opencti-graphql/tests/01-unit/domain/csv-bundler-test.ts
@@ -1,13 +1,17 @@
-import { describe, it, expect } from 'vitest';
-import { bundleProcess } from '../../../src/parser/csv-bundler';
-import { ADMIN_USER, testContext } from '../../utils/testQuery';
-import type { StixBundle, StixDomainObject } from '../../../src/types/stix-common';
+import {describe, it, expect} from 'vitest';
+import {bundleProcess} from '../../../src/parser/csv-bundler';
+import {ADMIN_USER, testContext} from '../../utils/testQuery';
+import type {StixBundle, StixDomainObject} from '../../../src/types/stix-common';
 import {
   indicatorsWithExternalReferencesCsvContent,
   indicatorsWithExternalReferencesCsvMapper
 } from '../../data/csv-bundler/external-references-constants';
-import type { CsvMapperParsed } from '../../../src/modules/internal/csvMapper/csvMapper-types';
-import { indicatorsWithLabelsCsvContent, indicatorsWithLabelsCsvMapper } from '../../data/csv-bundler/labels-constants';
+import type {CsvMapperParsed} from '../../../src/modules/internal/csvMapper/csvMapper-types';
+import {indicatorsWithLabelsCsvContent, indicatorsWithLabelsCsvMapper} from '../../data/csv-bundler/labels-constants';
+import {
+  indicatorsWithKillChainPhasesCsvContent,
+  indicatorsWithKillChainPhasesCsvMapper
+} from "../../data/csv-bundler/kill-chains-constants";
 
 describe('CSV bundler', () => {
   describe('Embedded properties', () => {
@@ -171,6 +175,95 @@ describe('CSV bundler', () => {
         indicatorsWithLabelsActualBundleWithoutId
       ).toStrictEqual(
         indicatorsWithLabelsExpectedBundleWithoutId
+      )
+    })
+    it('Should list kill chain phases', async () => {
+      const indicatorsWithKillChainPhasesExpectedBundle: StixBundle = {
+        id: "bundle--a58ed5eb-8881-475f-933a-098221a2052f",
+        spec_version: "2.1",
+        type: "bundle",
+        objects: [
+          {
+            extensions: {
+              "extension-definition--ea279b3e-5c71-4632-ac08-831c66a786ba": {
+                converter_csv: "[ipv4-addr:value = '198.168.8.3'],IPv4-Addr,kill_chain_name_1,kill_chain_phase_name_1,1",
+                extension_type: "new-sdo",
+                type: "Kill-Chain-Phase",
+              },
+            },
+            id: "kill-chain-phase--e0cf81cd-fad2-5e07-bed5-06d4556e8ac1",
+            kill_chain_name: "kill_chain_name_1",
+            order: 1,
+            phase_name: "kill_chain_phase_name_1",
+            spec_version: "2.1",
+            type: "kill-chain-phase",
+          },
+          {
+            extensions: {
+              "extension-definition--322b8f77-262a-4cb8-a915-1e441e00329b": {
+                extension_type: "property-extension",
+              },
+              "extension-definition--ea279b3e-5c71-4632-ac08-831c66a786ba": {
+                converter_csv: "[ipv4-addr:value = '198.168.8.3'],IPv4-Addr,kill_chain_name_1,kill_chain_phase_name_1,1",
+                extension_type: "property-extension",
+                main_observable_type: "IPv4-Addr",
+                type: "Indicator",
+              },
+            },
+            id: "indicator--71e02a09-dea3-5336-ade3-9f397e6f1184",
+            kill_chain_phases: [
+              {
+                kill_chain_name: "kill_chain_name_1",
+                phase_name: "kill_chain_phase_name_1",
+              },
+            ],
+            name: "[ipv4-addr:value = '198.168.8.3']",
+            pattern: "[ipv4-addr:value = '198.168.8.3']",
+            pattern_type: "stix",
+            spec_version: "2.1",
+            type: "indicator",
+          },
+          {
+            extensions: {
+              "extension-definition--322b8f77-262a-4cb8-a915-1e441e00329b": {
+                extension_type: "property-extension",
+              },
+              "extension-definition--ea279b3e-5c71-4632-ac08-831c66a786ba": {
+                converter_csv: "[ipv4-addr:value = '198.168.8.4'],IPv4-Addr,kill_chain_name_1,kill_chain_phase_name_1,1",
+                extension_type: "property-extension",
+                main_observable_type: "IPv4-Addr",
+                type: "Indicator",
+              },
+            },
+            id: "indicator--43f6ef9e-ea3c-5e64-9898-f635ead37622",
+            kill_chain_phases: [
+              {
+                kill_chain_name: "kill_chain_name_1",
+                phase_name: "kill_chain_phase_name_1",
+              },
+            ],
+            name: "[ipv4-addr:value = '198.168.8.4']",
+            pattern: "[ipv4-addr:value = '198.168.8.4']",
+            pattern_type: "stix",
+            spec_version: "2.1",
+            type: "indicator",
+          },
+        ] as unknown as StixDomainObject[],
+      }
+      const indicatorsWithKillChainPhasesActualBundle = await bundleProcess(
+        testContext,
+        ADMIN_USER,
+        Buffer.from(indicatorsWithKillChainPhasesCsvContent),
+        indicatorsWithKillChainPhasesCsvMapper as CsvMapperParsed
+      )
+      const {id: _expectedId, ...expectedRest} = indicatorsWithKillChainPhasesExpectedBundle
+      const indicatorsWithKillChainPhasesExpectedBundleWithoutId = {...expectedRest}
+      const {id: _actualId, ...actualRest} = indicatorsWithKillChainPhasesActualBundle
+      const indicatorsWithKillChainPhasesActualBundleWithoutId = {...actualRest}
+      expect(
+        indicatorsWithKillChainPhasesActualBundleWithoutId
+      ).toStrictEqual(
+        indicatorsWithKillChainPhasesExpectedBundleWithoutId
       )
     })
   })
