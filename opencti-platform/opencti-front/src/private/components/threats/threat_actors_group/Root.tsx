dae4cd465664e3422cdc1129b57e7c3bec0f7c17
Valentin Bouzin <valentin.bouzin@filigran.io>
2024-07-02 08:40:05 +0200

    threat actors group root updated with ts and bulk relation dialog container

---
+++ opencti-platform/opencti-front/src/private/components/threats/threat_actors_group/Root.tsx
@@ -0,0 +1,284 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// TODO Remove this when V6
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import React, { Suspense, useMemo } from 'react';
+import { Route, Routes, Link, Navigate, useLocation, useParams } from 'react-router-dom';
+import { graphql, useSubscription, usePreloadedQuery, PreloadedQuery } from 'react-relay';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import useQueryLoading from 'src/utils/hooks/useQueryLoading';
+import { RootThreatActorGroupQuery } from '@components/threats/threat_actors_group/__generated__/RootThreatActorGroupQuery.graphql';
+import useForceUpdate from '@components/common/bulk/useForceUpdate';
+import BulkRelationDialogContainer from '@components/common/bulk/dialog/BulkRelationDialogContainer';
+import StixCoreObjectContentRoot from '../../common/stix_core_objects/StixCoreObjectContentRoot';
+import StixCoreObjectSimulationResult from '../../common/stix_core_objects/StixCoreObjectSimulationResult';
+import ThreatActorGroup from './ThreatActorGroup';
+import ThreatActorGroupKnowledge from './ThreatActorGroupKnowledge';
+import Loader, { LoaderVariant } from '../../../../components/Loader';
+import FileManager from '../../common/files/FileManager';
+import StixDomainObjectHeader from '../../common/stix_domain_objects/StixDomainObjectHeader';
+import ThreatActorGroupPopover from './ThreatActorGroupPopover';
+import StixCoreObjectHistory from '../../common/stix_core_objects/StixCoreObjectHistory';
+import StixCoreObjectOrStixCoreRelationshipContainers from '../../common/containers/StixCoreObjectOrStixCoreRelationshipContainers';
+import ErrorNotFound from '../../../../components/ErrorNotFound';
+import StixCoreObjectKnowledgeBar from '../../common/stix_core_objects/StixCoreObjectKnowledgeBar';
+import { useFormatter } from '../../../../components/i18n';
+import Breadcrumbs from '../../../../components/Breadcrumbs';
+import { getCurrentTab, getPaddingRight } from '../../../../utils/utils';
+
+const subscription = graphql`
+  subscription RootThreatActorsGroupSubscription($id: ID!) {
+    stixDomainObject(id: $id) {
+      ... on ThreatActor {
+        ...ThreatActorGroup_ThreatActorGroup
+        ...ThreatActorGroupEditionContainer_ThreatActorGroup
+      }
+      ...FileImportViewer_entity
+      ...FileExportViewer_entity
+      ...FileExternalReferencesViewer_entity
+      ...WorkbenchFileViewer_entity
+      ...PictureManagementViewer_entity
+    }
+  }
+`;
+
+const ThreatActorGroupQuery = graphql`
+  query RootThreatActorGroupQuery($id: String!) {
+    threatActorGroup(id: $id) {
+      id
+      standard_id
+      entity_type
+      name
+      aliases
+      x_opencti_graph_data
+      stixCoreObjectsDistribution(field: "entity_type", operation: count) {
+        label
+        value
+      }  
+      ...ThreatActorGroup_ThreatActorGroup
+      ...ThreatActorGroupKnowledge_ThreatActorGroup
+      ...FileImportViewer_entity
+      ...FileExportViewer_entity
+      ...FileExternalReferencesViewer_entity
+      ...WorkbenchFileViewer_entity
+      ...PictureManagementViewer_entity
+      ...StixCoreObjectContent_stixCoreObject
+    }
+    connectorsForImport {
+      ...FileManager_connectorsImport
+    }
+    connectorsForExport {
+      ...FileManager_connectorsExport
+    }
+  }
+`;
+
+type RootThreatActorGroupProps = {
+  threatActorGroupId: string;
+  queryRef: PreloadedQuery<RootThreatActorGroupQuery>
+};
+
+const RootThreatActorGroup = ({ queryRef, threatActorGroupId }: RootThreatActorGroupProps) => {
+  const subConfig = useMemo(() => ({
+    subscription,
+    variables: { id: threatActorGroupId },
+  }), [threatActorGroupId]);
+
+  const location = useLocation();
+  const { t_i18n } = useFormatter();
+  useSubscription(subConfig);
+
+  const {
+    threatActorGroup,
+    connectorsForExport,
+    connectorsForImport,
+  } = usePreloadedQuery<RootThreatActorGroupQuery>(ThreatActorGroupQuery, queryRef);
+
+  const { forceUpdate, handleForceUpdate } = useForceUpdate();
+
+  const isOverview = location.pathname === `/dashboard/threats/threat_actors_group/${threatActorGroupId}`;
+  const isKnowledge = location.pathname.startsWith(`/dashboard/threats/threat_actors_group/${threatActorGroupId}/knowledge`);
+  const paddingRight = getPaddingRight(location.pathname, threatActorGroupId, '/dashboard/threats/threat_actors_group');
+  const link = `/dashboard/threats/threat_actors_group/${threatActorGroupId}/knowledge`;
+  return threatActorGroup ? (
+    <>
+      <Routes>
+        <Route
+          path="/knowledge/*"
+          element={
+            <StixCoreObjectKnowledgeBar
+              stixCoreObjectLink={link}
+              availableSections={[
+                'victimology',
+                'threat_actors',
+                'intrusion_sets',
+                'campaigns',
+                'incidents',
+                'malwares',
+                'attack_patterns',
+                'channels',
+                'narratives',
+                'tools',
+                'vulnerabilities',
+                'indicators',
+                'observables',
+                'infrastructures',
+                'sightings',
+              ]}
+              stixCoreObjectsDistribution={threatActorGroup.stixCoreObjectsDistribution}
+            />
+              }
+        />
+      </Routes>
+      <div style={{ paddingRight }}>
+        <Breadcrumbs variant="object" elements={[
+          { label: t_i18n('Threats') },
+          { label: t_i18n('Threat actors (group)'), link: '/dashboard/threats/threat_actors_group' },
+          { label: threatActorGroup.name, current: true },
+        ]}
+        />
+        <StixDomainObjectHeader
+          entityType="Threat-Actor-Group"
+          stixDomainObject={threatActorGroup}
+          PopoverComponent={<ThreatActorGroupPopover />}
+          enableQuickSubscription={true}
+        />
+        <Box
+          sx={{
+            borderBottom: 1,
+            borderColor: 'divider',
+            marginBottom: 4,
+          }}
+        >
+          <Tabs
+            value={getCurrentTab(location.pathname, threatActorGroup.id, '/dashboard/threats/threat_actors_group')}
+          >
+            <Tab
+              component={Link}
+              to={`/dashboard/threats/threat_actors_group/${threatActorGroup.id}`}
+              value={`/dashboard/threats/threat_actors_group/${threatActorGroup.id}`}
+              label={t_i18n('Overview')}
+            />
+            <Tab
+              component={Link}
+              to={`/dashboard/threats/threat_actors_group/${threatActorGroup.id}/knowledge/overview`}
+              value={`/dashboard/threats/threat_actors_group/${threatActorGroup.id}/knowledge`}
+              label={t_i18n('Knowledge')}
+            />
+            <Tab
+              component={Link}
+              to={`/dashboard/threats/threat_actors_group/${threatActorGroup.id}/content`}
+              value={`/dashboard/threats/threat_actors_group/${threatActorGroup.id}/content`}
+              label={t_i18n('Content')}
+            />
+            <Tab
+              component={Link}
+              to={`/dashboard/threats/threat_actors_group/${threatActorGroup.id}/analyses`}
+              value={`/dashboard/threats/threat_actors_group/${threatActorGroup.id}/analyses`}
+              label={t_i18n('Analyses')}
+            />
+            <Tab
+              component={Link}
+              to={`/dashboard/threats/threat_actors_group/${threatActorGroup.id}/files`}
+              value={`/dashboard/threats/threat_actors_group/${threatActorGroup.id}/files`}
+              label={t_i18n('Data')}
+            />
+            <Tab
+              component={Link}
+              to={`/dashboard/threats/threat_actors_group/${threatActorGroup.id}/history`}
+              value={`/dashboard/threats/threat_actors_group/${threatActorGroup.id}/history`}
+              label={t_i18n('History')}
+            />
+          </Tabs>
+          {isKnowledge && (
+            <BulkRelationDialogContainer
+              stixDomainObjectId={threatActorGroup.id}
+              stixDomainObjectName={threatActorGroup.name}
+              stixDomainObjectType="Threat-Actor-Group"
+              handleRefetch={handleForceUpdate}
+            />
+          )}
+          {isOverview && (
+            <StixCoreObjectSimulationResult id={threatActorGroup.id} type="threat" />
+          )}
+        </Box>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ThreatActorGroup threatActorGroup={threatActorGroup} />
+            }
+          />
+          <Route
+            path="/knowledge"
+            element={
+              <Navigate to={`/dashboard/threats/threat_actors_group/${threatActorGroupId}/knowledge/overview`} replace={true} />
+            }
+          />
+          <Route
+            path="/knowledge/*"
+            element={
+              <div key={forceUpdate}>
+                <ThreatActorGroupKnowledge threatActorGroup={threatActorGroup} />
+              </div>
+            }
+          />
+          <Route
+            path="/content/*"
+            element={
+              <StixCoreObjectContentRoot
+                stixCoreObject={threatActorGroup}
+              />
+            }
+          />
+          <Route
+            path="/analyses"
+            element={
+              <StixCoreObjectOrStixCoreRelationshipContainers stixDomainObjectOrStixCoreRelationship={threatActorGroup} />
+            }
+          />
+          <Route
+            path="/files"
+            element={
+              <FileManager
+                id={threatActorGroupId}
+                connectorsImport={connectorsForImport}
+                connectorsExport={connectorsForExport}
+                entity={threatActorGroup}
+              />
+                }
+          />
+          <Route
+            path="/history"
+            element={
+              <StixCoreObjectHistory stixCoreObjectId={threatActorGroupId} />
+            }
+          />
+        </Routes>
+      </div>
+    </>
+  ) : (
+    <ErrorNotFound />
+  );
+};
+
+const Root = () => {
+  const { threatActorGroupId } = useParams();
+  const queryRef = useQueryLoading<RootThreatActorGroupQuery>(ThreatActorGroupQuery, {
+    id: threatActorGroupId ?? '',
+  });
+
+  return (
+    <>
+      {queryRef && (
+        <Suspense fallback={<Loader variant={LoaderVariant.container} />}>
+          <RootThreatActorGroup queryRef={queryRef} threatActorGroupId={threatActorGroupId} />
+        </Suspense>
+      )}
+    </>
+  );
+};
+
+export default Root;
