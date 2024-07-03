5a0a6c53c385c15191602de2df796e9f7f6ac480
Archidoit <75783086+Archidoit@users.noreply.github.com>
2024-07-03 13:42:10 +0200

    [frontend/backend] source and target filters for sightings (#7182)

--- opencti-platform/opencti-graphql/src/modules/attributes/stixSightingRelationship-registrationAttributes.ts
+++ opencti-platform/opencti-graphql/src/modules/attributes/stixSightingRelationship-registrationAttributes.ts
@@ -1,16 +1,43 @@
-import * as R from 'ramda';
-import type { AttributeDefinition } from '../../schema/attribute-definition';
+import type { AttributeDefinition, IdAttribute, NestedObjectAttribute } from '../../schema/attribute-definition';
+import { entityType, internalId } from '../../schema/attribute-definition';
 import { schemaAttributesDefinition } from '../../schema/schema-attributes';
 import { STIX_SIGHTING_RELATIONSHIP } from '../../schema/stixSightingRelationship';
+import { connections } from './basicRelationship-registrationAttributes';
+import { ABSTRACT_STIX_CORE_OBJECT } from '../../schema/general';
+import {
+  INSTANCE_RELATION_FILTER,
+  INSTANCE_RELATION_TYPES_FILTER,
+  RELATION_FROM_FILTER,
+  RELATION_FROM_TYPES_FILTER,
+  RELATION_TO_FILTER,
+  RELATION_TO_TYPES_FILTER
+} from '../../utils/filtering/filtering-constants';
 
-export const stixSightingRelationshipsAttributes: { [k: string]: Array<AttributeDefinition> } = {
-  [STIX_SIGHTING_RELATIONSHIP]: [
-    { name: 'attribute_count', label: 'Count', type: 'numeric', precision: 'integer', mandatoryType: 'external', editDefault: true, multiple: false, upsert: true, isFilterable: true },
-    { name: 'first_seen', label: 'First seen', type: 'date', mandatoryType: 'customizable', editDefault: true, multiple: false, upsert: true, isFilterable: true },
-    { name: 'last_seen', label: 'Last seen', type: 'date', mandatoryType: 'customizable', editDefault: true, multiple: false, upsert: true, isFilterable: true },
-    { name: 'description', label: 'Description', type: 'string', format: 'text', mandatoryType: 'customizable', editDefault: true, multiple: false, upsert: true, isFilterable: true },
-    { name: 'x_opencti_negative', label: 'False positive', type: 'boolean', mandatoryType: 'customizable', editDefault: true, multiple: false, upsert: false, isFilterable: true },
-    { name: 'x_opencti_workflow_id', label: 'Workflow status', type: 'string', format: 'short', mandatoryType: 'no', editDefault: false, multiple: false, upsert: true, isFilterable: false },
-  ],
-};
-R.forEachObjIndexed((value, key) => schemaAttributesDefinition.registerAttributes(key as string, value), stixSightingRelationshipsAttributes);
+export const stixSightingRelationshipsAttributes: Array<AttributeDefinition> = [
+  { ...entityType, isFilterable: false },
+  { name: 'attribute_count', label: 'Count', type: 'numeric', precision: 'integer', mandatoryType: 'external', editDefault: true, multiple: false, upsert: true, isFilterable: true },
+  { name: 'first_seen', label: 'First seen', type: 'date', mandatoryType: 'customizable', editDefault: true, multiple: false, upsert: true, isFilterable: true },
+  { name: 'last_seen', label: 'Last seen', type: 'date', mandatoryType: 'customizable', editDefault: true, multiple: false, upsert: true, isFilterable: true },
+  { name: 'description', label: 'Description', type: 'string', format: 'text', mandatoryType: 'customizable', editDefault: true, multiple: false, upsert: true, isFilterable: true },
+  { name: 'x_opencti_negative', label: 'False positive', type: 'boolean', mandatoryType: 'customizable', editDefault: true, multiple: false, upsert: false, isFilterable: true },
+  { name: 'x_opencti_workflow_id', label: 'Workflow status', type: 'string', format: 'short', mandatoryType: 'no', editDefault: false, multiple: false, upsert: true, isFilterable: false },
+  { ...connections as NestedObjectAttribute,
+    isFilterable: true,
+    mappings: [
+      { ...internalId as IdAttribute,
+        isFilterable: true,
+        entityTypes: [ABSTRACT_STIX_CORE_OBJECT],
+        associatedFilterKeys: [
+          { key: RELATION_FROM_FILTER, label: 'Source entity' },
+          { key: RELATION_TO_FILTER, label: 'Target entity' },
+          { key: INSTANCE_RELATION_FILTER, label: 'Related entity' }
+        ]
+      },
+      { name: 'name', label: 'Name', type: 'string', format: 'short', editDefault: false, mandatoryType: 'no', multiple: true, upsert: true, isFilterable: false },
+      { name: 'role', label: 'Role', type: 'string', format: 'short', editDefault: false, mandatoryType: 'no', multiple: true, upsert: true, isFilterable: false },
+      { name: 'types', label: 'Types', type: 'string', format: 'short', editDefault: false, mandatoryType: 'no', multiple: true, upsert: true, isFilterable: true, associatedFilterKeys: [{ key: RELATION_FROM_TYPES_FILTER, label: 'Source type' }, { key: RELATION_TO_TYPES_FILTER, label: 'Target type' }, { key: INSTANCE_RELATION_TYPES_FILTER, label: 'Related type' }] },
+    ],
+  },
+];
+
+schemaAttributesDefinition.registerAttributes(STIX_SIGHTING_RELATIONSHIP, stixSightingRelationshipsAttributes);
