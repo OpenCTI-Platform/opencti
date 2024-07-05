a37a9c5c222b8db78f137103de061d3284d5712d
Celine Sebe <celine.sebe@filigran.io>
2024-07-05 12:18:06 +0200

    [backend]get relationsships ref(#7565)

--- opencti-platform/opencti-graphql/src/database/stix-ref.ts
+++ opencti-platform/opencti-graphql/src/database/stix-ref.ts
@@ -1,7 +1,16 @@
 import { schemaRelationsRefDefinition } from '../schema/schema-relationsRef';
+import { STIX_CORE_RELATIONSHIPS } from '../schema/stixCoreRelationship';
+import type { RefAttribute } from '../schema/attribute-definition';
 
 export const schemaRelationsRefTypesMapping = () => {
-  return Array.from(schemaRelationsRefDefinition.relationsRefCacheArray.entries()).map(([key, refs]) => {
+  const relationshipsRefs = new Map<string, RefAttribute[]>();
+  STIX_CORE_RELATIONSHIPS.forEach((relation) => {
+    relationshipsRefs.set(relation, schemaRelationsRefDefinition.getRelationsRef(relation));
+  });
+  return [
+    ...Array.from(relationshipsRefs.entries()),
+    ...Array.from(schemaRelationsRefDefinition.relationsRefCacheArray.entries())
+  ].map(([key, refs]) => {
     return {
       key,
       values: refs.map((ref) => ({
