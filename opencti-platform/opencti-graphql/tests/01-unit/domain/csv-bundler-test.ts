97985bcb3ff6121b3cc20e9193145f67736e5c55
Romy Alula <romy.alula@filigran.io>
2024-07-09 14:48:43 +0200

    [backend] Lint (#7036)

--- opencti-platform/opencti-graphql/tests/01-unit/domain/csv-bundler-test.ts
+++ opencti-platform/opencti-graphql/tests/01-unit/domain/csv-bundler-test.ts
@@ -7,11 +7,7 @@ import {
   indicatorsWithExternalReferencesExpectedBundle
 } from '../../data/csv-bundler/external-references-constants';
 import type { CsvMapperParsed } from '../../../src/modules/internal/csvMapper/csvMapper-types';
-import {
-  indicatorsWithLabelsCsvContent,
-  indicatorsWithLabelsCsvMapper,
-  indicatorsWithLabelsExpectedBundle
-} from '../../data/csv-bundler/labels-constants';
+import { indicatorsWithLabelsCsvContent, indicatorsWithLabelsCsvMapper, indicatorsWithLabelsExpectedBundle } from '../../data/csv-bundler/labels-constants';
 import {
   indicatorsWithKillChainPhasesCsvContent,
   indicatorsWithKillChainPhasesCsvMapper,
@@ -27,49 +23,49 @@ describe('CSV bundler', () => {
         Buffer.from(indicatorsWithExternalReferencesCsvContent),
         indicatorsWithExternalReferencesCsvMapper as CsvMapperParsed
       );
-      const {id: _expectedId, ...expectedRest} = indicatorsWithExternalReferencesExpectedBundle
-      const indicatorsWithExternalReferencesExpectedBundleWithoutId = {...expectedRest}
-      const {id: _actualId, ...actualRest} = indicatorsWithExternalReferencesActualBundle
-      const indicatorsWithExternalReferencesActualBundleWithoutId = {...actualRest}
+      const { id: _expectedId, ...expectedRest } = indicatorsWithExternalReferencesExpectedBundle;
+      const indicatorsWithExternalReferencesExpectedBundleWithoutId = { ...expectedRest };
+      const { id: _actualId, ...actualRest } = indicatorsWithExternalReferencesActualBundle;
+      const indicatorsWithExternalReferencesActualBundleWithoutId = { ...actualRest };
       expect(
         indicatorsWithExternalReferencesActualBundleWithoutId
       ).toStrictEqual(
         indicatorsWithExternalReferencesExpectedBundleWithoutId
-      )
-    })
+      );
+    });
     it('Should list labels', async () => {
       const indicatorsWithLabelsActualBundle = await bundleProcess(
         testContext,
         ADMIN_USER,
         Buffer.from(indicatorsWithLabelsCsvContent),
         indicatorsWithLabelsCsvMapper as CsvMapperParsed
-      )
-      const {id: _expectedId, ...expectedRest} = indicatorsWithLabelsExpectedBundle
-      const indicatorsWithLabelsExpectedBundleWithoutId = {...expectedRest}
-      const {id: _actualId, ...actualRest} = indicatorsWithLabelsActualBundle
-      const indicatorsWithLabelsActualBundleWithoutId = {...actualRest}
+      );
+      const { id: _expectedId, ...expectedRest } = indicatorsWithLabelsExpectedBundle;
+      const indicatorsWithLabelsExpectedBundleWithoutId = { ...expectedRest };
+      const { id: _actualId, ...actualRest } = indicatorsWithLabelsActualBundle;
+      const indicatorsWithLabelsActualBundleWithoutId = { ...actualRest };
       expect(
         indicatorsWithLabelsActualBundleWithoutId
       ).toStrictEqual(
         indicatorsWithLabelsExpectedBundleWithoutId
-      )
-    })
+      );
+    });
     it('Should list kill chain phases', async () => {
       const indicatorsWithKillChainPhasesActualBundle = await bundleProcess(
         testContext,
         ADMIN_USER,
         Buffer.from(indicatorsWithKillChainPhasesCsvContent),
         indicatorsWithKillChainPhasesCsvMapper as CsvMapperParsed
-      )
-      const {id: _expectedId, ...expectedRest} = indicatorsWithKillChainPhasesExpectedBundle
-      const indicatorsWithKillChainPhasesExpectedBundleWithoutId = {...expectedRest}
-      const {id: _actualId, ...actualRest} = indicatorsWithKillChainPhasesActualBundle
-      const indicatorsWithKillChainPhasesActualBundleWithoutId = {...actualRest}
+      );
+      const { id: _expectedId, ...expectedRest } = indicatorsWithKillChainPhasesExpectedBundle;
+      const indicatorsWithKillChainPhasesExpectedBundleWithoutId = { ...expectedRest };
+      const { id: _actualId, ...actualRest } = indicatorsWithKillChainPhasesActualBundle;
+      const indicatorsWithKillChainPhasesActualBundleWithoutId = { ...actualRest };
       expect(
         indicatorsWithKillChainPhasesActualBundleWithoutId
       ).toStrictEqual(
         indicatorsWithKillChainPhasesExpectedBundleWithoutId
-      )
-    })
-  })
-})
+      );
+    });
+  });
+});
