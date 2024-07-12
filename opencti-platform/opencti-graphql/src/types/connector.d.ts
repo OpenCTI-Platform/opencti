34aa9d50208fd7abb2316a596fbb75ad04e7f525
Angelique <angelique.jard@filigran.io>
2024-07-12 09:28:33 +0200

    Not needed

--- opencti-platform/opencti-graphql/src/types/connector.d.ts
+++ opencti-platform/opencti-graphql/src/types/connector.d.ts
@@ -1,5 +1,4 @@
 import type { StoreEntity } from './store';
-import { BasicStoreEntity } from './store';
 
 export interface ConnectorInfo {
   run_and_terminate: boolean,
@@ -7,11 +6,6 @@ export interface ConnectorInfo {
   queue_threshold: number,
   queue_messages_size: number,
   next_run_datetime: string, // TODO datetime instead of string
-}
-
-export interface BasicStoreConnector extends BasicStoreEntity {
-  active: boolean,
-  connector_info: ConnectorInfo,
 }
 
 export interface StoreConnector extends StoreEntity {
