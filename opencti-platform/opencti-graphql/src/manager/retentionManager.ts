879d21c5c5b38190e4c0ea06b163d4641aeeb680
Cathia Archidoit <cathia.archidoit@filigran.io>
2024-07-15 15:12:14 +0200

    [backend] migrate retention manager (#7482)

--- opencti-platform/opencti-graphql/src/manager/retentionManager.ts
+++ opencti-platform/opencti-graphql/src/manager/retentionManager.ts
@@ -1,9 +1,6 @@
-import { clearIntervalAsync, setIntervalAsync } from 'set-interval-async/dynamic';
 import moment from 'moment';
-import { lockResource } from '../database/redis';
 import { findAll as findRetentionRulesToExecute } from '../domain/retentionRule';
 import conf, { booleanConf, logApp } from '../config/conf';
-import { TYPE_LOCK_ERROR } from '../config/errors';
 import { deleteElementById, patchAttribute } from '../database/middleware';
 import { executionContext, RETENTION_MANAGER_USER } from '../utils/access';
 import { ENTITY_TYPE_RETENTION_RULE } from '../schema/internalObject';
@@ -11,16 +8,20 @@ import { now, utcDate } from '../utils/format';
 import { READ_STIX_INDICES } from '../database/utils';
 import { elPaginate } from '../database/engine';
 import { convertFiltersToQueryOptions } from '../utils/filtering/filtering-resolution';
+import type { ManagerDefinition } from './managerModule';
+import { registerManager } from './managerModule';
+import type { AuthContext } from '../types/user';
+import type { RetentionRule } from '../generated/graphql';
 
+const RETENTION_MANAGER_ENABLED = booleanConf('retention_manager:enabled', false);
 // Retention manager responsible to cleanup old data
 // Each API will start is retention manager.
 // If the lock is free, every API as the right to take it.
 const SCHEDULE_TIME = conf.get('retention_manager:interval') || 60000;
 const RETENTION_MANAGER_KEY = conf.get('retention_manager:lock_key') || 'retention_manager_lock';
 const RETENTION_BATCH_SIZE = conf.get('retention_manager:batch_size') || 100;
-let running = false;
 
-const executeProcessing = async (context, retentionRule) => {
+const executeProcessing = async (context: AuthContext, retentionRule: RetentionRule) => {
   const { id, name, max_retention: maxDays, filters } = retentionRule;
   logApp.debug(`[OPENCTI] Executing retention manager rule ${name}`);
   const jsonFilters = filters ? JSON.parse(filters) : null;
@@ -51,61 +52,37 @@ const executeProcessing = async (context, retentionRule) => {
   await patchAttribute(context, RETENTION_MANAGER_USER, id, ENTITY_TYPE_RETENTION_RULE, patch);
 };
 
-const retentionHandler = async () => {
-  logApp.debug('[OPENCTI-MODULE] Running retention manager');
-  let lock;
-  try {
-    // Lock the manager
-    lock = await lockResource([RETENTION_MANAGER_KEY], { retryCount: 0 });
-    running = true;
-    const context = executionContext('retention_manager');
-    const retentionRules = await findRetentionRulesToExecute(context, RETENTION_MANAGER_USER, { connectionFormat: false });
-    logApp.debug(`[OPENCTI] Retention manager execution for ${retentionRules.length} rules`);
-    // Execution of retention rules
-    if (retentionRules.length > 0) {
-      for (let index = 0; index < retentionRules.length; index += 1) {
-        lock.signal.throwIfAborted();
-        const retentionRule = retentionRules[index];
-        await executeProcessing(context, retentionRule);
-      }
+const retentionHandler = async (lock: { signal: AbortSignal, extend: () => Promise<void>, unlock: () => Promise<void> }) => {
+  const context = executionContext('retention_manager');
+  const retentionRules = await findRetentionRulesToExecute(context, RETENTION_MANAGER_USER, { connectionFormat: false });
+  logApp.debug(`[OPENCTI] Retention manager execution for ${retentionRules.length} rules`);
+  // Execution of retention rules
+  if (retentionRules.length > 0) {
+    for (let index = 0; index < retentionRules.length; index += 1) {
+      lock.signal.throwIfAborted();
+      const retentionRule = retentionRules[index];
+      await executeProcessing(context, retentionRule as unknown as RetentionRule);
     }
-  } catch (e) {
-    // We dont care about failing to get the lock.
-    if (e.name === TYPE_LOCK_ERROR) {
-      logApp.debug('[OPENCTI-MODULE] Retention manager already in progress by another API');
-    } else {
-      logApp.error(e, { manager: 'RETENTION_MANAGER' });
-    }
-  } finally {
-    running = false;
-    if (lock) await lock.unlock();
   }
 };
-const initRetentionManager = () => {
-  let scheduler;
-  return {
-    start: async () => {
-      logApp.info('[OPENCTI-MODULE] Running retention manager');
-      scheduler = setIntervalAsync(async () => {
-        await retentionHandler();
-      }, SCHEDULE_TIME);
-    },
-    status: () => {
-      return {
-        id: 'RETENTION_MANAGER',
-        enable: booleanConf('retention_manager:enabled', false),
-        running,
-      };
-    },
-    shutdown: async () => {
-      logApp.info('[OPENCTI-MODULE] Stopping retention manager');
-      if (scheduler) {
-        return clearIntervalAsync(scheduler);
-      }
-      return true;
-    },
-  };
-};
-const retentionManager = initRetentionManager();
 
-export default retentionManager;
+const RETENTION_MANAGER_DEFINITION: ManagerDefinition = {
+  id: 'RETENTION_MANAGER',
+  label: 'Retention manager',
+  executionContext: 'retention_manager',
+  cronSchedulerHandler: {
+    handler: retentionHandler,
+    interval: SCHEDULE_TIME,
+    lockKey: RETENTION_MANAGER_KEY,
+    lockInHandlerParams: true,
+  },
+  enabledByConfig: RETENTION_MANAGER_ENABLED,
+  enabledToStart(): boolean {
+    return this.enabledByConfig;
+  },
+  enabled(): boolean {
+    return this.enabledByConfig;
+  }
+};
+
+registerManager(RETENTION_MANAGER_DEFINITION);
