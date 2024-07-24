cb304b72b3440dcb8f49bacc321d1cd657f39fdc
Cathia Archidoit <cathia.archidoit@filigran.io>
2024-07-24 15:26:53 +0200

    [backend] tests for retention rules on files (#7482)

--- opencti-platform/opencti-graphql/tests/02-integration/04-manager/retentionManager-test.ts
+++ opencti-platform/opencti-graphql/tests/02-integration/04-manager/retentionManager-test.ts
@@ -1,9 +1,15 @@
 import { describe, expect, it } from 'vitest';
 import gql from 'graphql-tag';
+import { Readable } from 'stream';
 import { ADMIN_USER, queryAsAdmin, testContext } from '../../utils/testQuery';
 import { utcDate } from '../../../src/utils/format';
 import { elementsToDelete } from '../../../src/manager/retentionManager';
 import { allFilesForPaths } from '../../../src/modules/internal/document/document-domain';
+import { uploadToStorage } from '../../../src/database/file-storage-helper';
+import { elRawUpdateByQuery } from '../../../src/database/engine';
+import { READ_INDEX_INTERNAL_OBJECTS } from '../../../src/database/utils';
+import { DatabaseError } from '../../../src/config/errors';
+import { deleteFile, loadFile } from '../../../src/database/file-storage';
 
 describe('Retention Manager tests ', () => {
   const context = testContext;
@@ -79,22 +85,57 @@ describe('Retention Manager tests ', () => {
     expect(fileRuleDeletion.data?.retentionRuleEdit.delete).toEqual(fileRule.id);
   });
   it('should fetch the correct files to be deleted by a retention rule on files', async () => {
-    // check the number of files imported in Data/import
     const globalPath = 'import/global';
+    // create a file not modified since '2023-01-01T00:00:00.000Z'
+    const fileName = 'fileToTestRetentionRule';
+    const fileId = `${globalPath}/${fileName}`;
+    const fileToUpload = {
+      createReadStream: () => Readable.from('This is a file content.'),
+      filename: fileName,
+    };
+    await uploadToStorage(context, ADMIN_USER, globalPath, fileToUpload, {});
+    const lastModified = '2023-01-01T00:00:00.000Z';
+    const updateQuery = {
+      script: {
+        params: { lastModified },
+        source: 'ctx._source.lastModified = params.lastModified;',
+      },
+      query: {
+        bool: {
+          must: [
+            { term: { 'internal_id.keyword': { value: fileId } } },
+          ],
+        },
+      },
+    };
+    await elRawUpdateByQuery({
+      index: [READ_INDEX_INTERNAL_OBJECTS],
+      refresh: true,
+      wait_for_completion: true,
+      body: updateQuery
+    }).catch((err: Error) => {
+      throw DatabaseError('Error updating elastic', { cause: err });
+    });
+    const file = await loadFile(context, ADMIN_USER, fileId);
+    expect(file?.lastModified).toEqual(lastModified);
+    // check the number of files imported in Data/import
     const files = await allFilesForPaths(testContext, ADMIN_USER, [globalPath]);
-    expect(files.length).toEqual(7); // 7 files from index-file-test
-    // retention rule on files not modified since 2023-01-01
-    const before = utcDate('2023-01-01T00:00:00.000Z');
+    expect(files.length).toEqual(8); // 7 files from index-file-test + the created file
+    // retention rule on files not modified since 2023-07-01
+    const before = utcDate('2023-07-01T00:00:00.000Z');
     const filesToDelete = await elementsToDelete(context, 'file', before);
-    expect(filesToDelete.edges.length).toEqual(1); // 'TEST_FILE_7' has not been modified since 'before'
-    expect(filesToDelete.edges[0].node.id).toEqual('test');
+    expect(filesToDelete.edges.length).toEqual(1); // fileToTestRetentionRule is the only file that has not been modified since 'before'
+    expect(filesToDelete.edges[0].node.id).toEqual(fileId);
     // retention rule on all the files
     const filesToDelete2 = await elementsToDelete(context, 'file', utcDate());
-    expect(filesToDelete2.edges.length).toEqual(7); // all the files has not been modified since now
+    expect(filesToDelete2.edges.length).toEqual(8); // all the files has not been modified since now
+    // delete the created file
+    const deleted = await deleteFile(context, ADMIN_USER, fileId);
+    expect(deleted?.id).toEqual(fileId);
   });
   it('should fetch the correct elements to be deleted by a retention rule on knowledge', async () => {
-    const before = utcDate('2021-01-01T00:00:00.000Z');
+    const before = utcDate('2024-01-01T00:00:00.000Z');
     const elements = await elementsToDelete(context, 'knowledge', before);
-    expect(elements.pageInfo.globalCount).toEqual(0);
+    expect(elements.pageInfo.globalCount).toEqual(0); // no entity have been modified since before
   });
 });
