25479cad7fab5ca9841a34f853b77ad3e3bcb1c7
Samuel Hassine <samuel.hassine@filigran.io>
2024-07-28 17:06:03 +0200

    [client] Enhance logging for playbooks

--- pycti/api/opencti_api_playbook.py
+++ pycti/api/opencti_api_playbook.py
@@ -6,7 +6,12 @@ class OpenCTIApiPlaybook:
 
     def playbook_step_execution(self, playbook: dict, bundle: str):
         self.api.app_logger.info(
-            "Executing playbook step", {"playbook_id": playbook["playbook_id"]}
+            "Executing playbook step",
+            {
+                "playbook_id": playbook["playbook_id"],
+                "step_id": playbook["step_id"],
+                "data_instance_id": playbook["data_instance_id"],
+            },
         )
         query = """
             mutation PlaybookStepExecution($execution_id: ID!, $event_id: ID!, $execution_start: DateTime!, $data_instance_id: ID!, $playbook_id: ID!, $previous_step_id: ID!, $step_id: ID!, $previous_bundle: String!, $bundle: String!) {
