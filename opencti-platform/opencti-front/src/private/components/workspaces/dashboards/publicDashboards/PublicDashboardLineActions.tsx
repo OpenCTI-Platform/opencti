6be532c5e7263f7be66e4ad792a6c9a0803f551e
Celine Sebe <celine.sebe@filigran.io>
2024-08-01 15:25:40 +0200

    [frontend] WIP Deletion

--- opencti-platform/opencti-front/src/private/components/workspaces/dashboards/publicDashboards/PublicDashboardLineActions.tsx
+++ opencti-platform/opencti-front/src/private/components/workspaces/dashboards/publicDashboards/PublicDashboardLineActions.tsx
@@ -1,19 +1,26 @@
 import { PublicDashboards_PublicDashboard$data } from '@components/workspaces/dashboards/publicDashboards/__generated__/PublicDashboards_PublicDashboard.graphql';
 import MoreVert from '@mui/icons-material/MoreVert';
-import { Menu, MenuItem, IconButton, MenuProps } from '@mui/material';
+import { IconButton, Menu, MenuItem, MenuProps } from '@mui/material';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { graphql } from 'react-relay';
 import { useFormatter } from '../../../../../components/i18n';
 import { copyToClipboard } from '../../../../../utils/utils';
 import useApiMutation from '../../../../../utils/hooks/useApiMutation';
+import useDeletion from '../../../../../utils/hooks/useDeletion';
+import DeleteDialog from '../../../../../components/DeleteDialog';
 
 interface PublicDashboardLineActionsProps {
   publicDashboard: PublicDashboards_PublicDashboard$data
 }
 
-const publicDashboardMutation = graphql`
-  mutation PublicDashboardLineActionsMutation($id: ID!, $input: [EditInput!]!) {
+const publicDashboardLineActionsDeleteMutation = graphql`
+  mutation PublicDashboardLineActionsDeleteMutation($id: ID!){
+    publicDashboardDelete(id: $id)
+  }`;
+
+const publicDashboardLineActionsEditMutation = graphql`
+  mutation PublicDashboardLineActionsEditMutation($id: ID!, $input: [EditInput!]!) {
     publicDashboardFieldPatch(id: $id, input: $input) {
       id
       uri_key
@@ -26,8 +33,11 @@ const PublicDashboardLineActions = ({ publicDashboard }: PublicDashboardLineActi
   const { t_i18n } = useFormatter();
   const navigate = useNavigate();
   const [anchor, setAnchor] = useState<MenuProps['anchorEl']>();
-  const [commitEditMutation] = useApiMutation(publicDashboardMutation);
-
+  const [commitEditMutation] = useApiMutation(publicDashboardLineActionsEditMutation);
+  const [commitDeleteMutation] = useApiMutation(publicDashboardLineActionsDeleteMutation);
+  const deletion = useDeletion({});
+  const { handleOpenDelete } = deletion;
+  console.log('DASHBOARD', publicDashboard);
   const copyLinkUrl = () => {
     copyToClipboard(
       t_i18n,
@@ -39,6 +49,20 @@ const PublicDashboardLineActions = ({ publicDashboard }: PublicDashboardLineActi
     navigate(`/dashboard/workspaces/dashboards/${publicDashboard.dashboard?.id}`);
   };
 
+  const onDelete = () => {
+    if (publicDashboard.id) {
+      deletion.setDeleting(true);
+      commitDeleteMutation({
+        variables: {
+          id: publicDashboard.id,
+        },
+        onCompleted: () => {
+          deletion.setDeleting(false);
+          deletion.handleCloseDelete();
+        },
+      });
+    }
+  };
   const onToggleEnabled = () => {
     commitEditMutation({
       variables: {
@@ -83,7 +107,19 @@ const PublicDashboardLineActions = ({ publicDashboard }: PublicDashboardLineActi
             ? t_i18n('Disable public link')
             : t_i18n('Enable public dashboard')}
         </MenuItem>
+        <MenuItem
+          onClick={() => handleOpenDelete()}
+          aria-label="Delete"
+        >
+          {t_i18n('Delete')}
+        </MenuItem>
+
       </Menu>
+      <DeleteDialog
+        title={t_i18n('Are you sure you want to delete this public dashboard?')}
+        deletion={deletion}
+        submitDelete={onDelete}
+      />
     </>
   );
 };
