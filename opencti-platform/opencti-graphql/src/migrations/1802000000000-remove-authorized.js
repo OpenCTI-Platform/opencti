ee01850a466e466753cd47529dd702204fb55c8e
Celine Sebe <celine.sebe@filigran.io>
2024-08-02 16:53:52 +0200

    [back] remove authorized migration ok

--- opencti-platform/opencti-graphql/src/migrations/1802000000000-remove-authorized.js
+++ opencti-platform/opencti-graphql/src/migrations/1802000000000-remove-authorized.js
@@ -1,26 +1,42 @@
-import { ENTITY_TYPE_PUBLIC_DASHBOARD } from '../modules/publicDashboard/publicDashboard-types';
+import { READ_DATA_INDICES } from '../database/utils';
 import { logApp } from '../config/conf';
-import { executionContext, SYSTEM_USER } from '../utils/access';
-import { listAllEntities } from '../database/middleware-loader';
-import { patchAttribute } from '../database/middleware';
+import { elUpdateByQueryForMigration } from '../database/engine';
 
 const message = '[MIGRATION] Remove authorized members in Public dashboards';
 
 export const up = async (next) => {
   logApp.info(`${message} > started`);
-  const context = executionContext('migration', SYSTEM_USER);
+  // const publicDashboards = await listAllEntities(
+  //   context,
+  //   context.user,
+  //   [ENTITY_TYPE_PUBLIC_DASHBOARD],
+  // );
+  //
+  // for (let index = 0; index < publicDashboards.length; index += 1) {
+  //   const publicDashboard = publicDashboards[index];
+  //   const patch = { authorized_members: undefined };
+  //   await patchAttribute(context, SYSTEM_USER, publicDashboard.id, ENTITY_TYPE_PUBLIC_DASHBOARD, patch);
+  // }
 
-  const publicDashboards = await listAllEntities(
-    context,
-    context.user,
-    [ENTITY_TYPE_PUBLIC_DASHBOARD],
+  const updateQuery = {
+    script: {
+      params: { fieldToRemove: 'authorized_members' },
+      source: 'ctx._source.remove(params.fieldToRemove)',
+    },
+    query: {
+      bool: {
+        must: [
+          { term: { 'entity_type.keyword': { value: 'PublicDashboard' } } },
+        ],
+      },
+    },
+  };
+  await elUpdateByQueryForMigration(
+    message,
+    READ_DATA_INDICES,
+    updateQuery
   );
 
-  for (let index = 0; index < publicDashboards.length; index += 1) {
-    const publicDashboard = publicDashboards[index];
-    const patch = { authorized_members: undefined };
-    await patchAttribute(context, SYSTEM_USER, publicDashboard.id, ENTITY_TYPE_PUBLIC_DASHBOARD, patch);
-  }
   logApp.info(`${message} > done`);
   next();
 };
