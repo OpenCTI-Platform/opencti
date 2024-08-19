74ab30a90fd1412218b45c9e24111892abcfa75d
Adrien Servel <adrien.servel@filigran.io>
2024-08-19 15:16:40 +0200

    [frontend] Fix toolbar for merge on entities Release 6.2.13

--- opencti-platform/opencti-front/src/private/components/data/DataTableToolBar.jsx
+++ opencti-platform/opencti-front/src/private/components/data/DataTableToolBar.jsx
@@ -1301,7 +1301,9 @@ class DataTableToolBar extends Component {
     const { actions, keptEntityId, mergingElement, actionsInputs, promoteToContainer } = this.state;
     const { entityTypeFilterValues, selectedElementsList, selectedTypes } = this.getSelectedTypes();
 
-    const typesAreDifferent = selectedTypes.length > 1;
+    // Some filter types are high level, we do not want to check them as "Different"
+    // We might need to add some other types here before refactoring the toolbar
+    const typesAreDifferent = (selectedTypes.filter((type) => !['Stix-Domain-Object', 'stix-core-relationship'].includes(type))).length > 1;
     const preventMerge = selectedTypes.at(0) === 'Vocabulary'
       && Object.values(selectedElements).some(({ builtIn }) => Boolean(builtIn));
     // region update
