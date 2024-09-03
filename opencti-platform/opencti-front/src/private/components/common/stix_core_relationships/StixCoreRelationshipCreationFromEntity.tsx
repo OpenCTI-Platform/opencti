ce40ca07aed63130af07575199dd71bc30000451
Valentin Bouzin <valentin.bouzin@filigran.io>
2024-09-03 09:22:41 +0200

    deepscan fix

--- opencti-platform/opencti-front/src/private/components/common/stix_core_relationships/StixCoreRelationshipCreationFromEntity.tsx
+++ opencti-platform/opencti-front/src/private/components/common/stix_core_relationships/StixCoreRelationshipCreationFromEntity.tsx
@@ -729,7 +729,7 @@ const StixCoreRelationshipCreationFromEntity: FunctionComponent<StixCoreRelation
     const newTargetEntities: TargetEntity[] = Object.values(selectedElements).map((item) => ({
       id: item.id,
       entity_type: item.entity_type ?? '',
-      name: item.name ?? String(item.observable_value) ?? '',
+      name: String(item.name ?? item.observable_value ?? ''),
     }));
     setTargetEntities(newTargetEntities);
   }, [selectedElements]);
