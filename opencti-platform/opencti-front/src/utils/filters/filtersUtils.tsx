703d7cf6317a169687f43b4c4ff7d8e0fdc2e287
Cathia Archidoit <cathia.archidoit@filigran.io>
2024-09-03 14:21:12 +0200

    [frontend] fix (#3016)

--- opencti-platform/opencti-front/src/utils/filters/filtersUtils.tsx
+++ opencti-platform/opencti-front/src/utils/filters/filtersUtils.tsx
@@ -461,21 +461,6 @@ export const addFilter = (
   };
 };
 
-// create a new filter: filterGroup1 AND filterGroup2
-export const addFilterGroup = (
-  filters1: FilterGroup | undefined,
-  filters2: FilterGroup,
-  mode = 'and',
-): FilterGroup | undefined => {
-  return (filters1
-    ? {
-      mode,
-      filters: [],
-      filterGroups: [filters1, filters2],
-    }
-    : filters2) as FilterGroup | undefined;
-};
-
 //----------------------------------------------------------------------------------------------------------------------
 
 // add a filter (k, id, op) in a filterGroup smartly, for usage in forms
@@ -659,7 +644,7 @@ export const getAvailableOperatorForFilter = (
   filterDefinition: FilterDefinition | undefined,
   subKey?: string,
 ): string[] => {
-  if (filterDefinition && subKey) return getAvailableOperatorForFilterSubKey(filterDefinition?.filterKey, subKey);
+  if (filterDefinition && subKey) return getAvailableOperatorForFilterSubKey(filterDefinition.filterKey, subKey);
   return getAvailableOperatorForFilterKey(filterDefinition);
 };
 
