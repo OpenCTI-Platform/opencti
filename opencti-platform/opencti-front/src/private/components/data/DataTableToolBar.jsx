c8bad9448f5769eb746c98fdad366fce32154f69
Gwendoline Favre-Felix <gwendoline.favre-felix@filigran.io>
2024-09-04 14:05:31 +0200

    [frontend] Removing the Stix-Cyber-Observable type from selectedTypes (#8172)

--- opencti-platform/opencti-front/src/private/components/data/DataTableToolBar.jsx
+++ opencti-platform/opencti-front/src/private/components/data/DataTableToolBar.jsx
@@ -1270,7 +1270,7 @@ class DataTableToolBar extends Component {
     const selectedElementsList = Object.values(this.props.selectedElements || {});
 
     const selectedTypes = R.uniq([...selectedElementsList.map((o) => o.entity_type), ...entityTypeFilterValues]
-      .filter((entity_type) => entity_type !== undefined));
+      .filter((entity_type) => entity_type !== undefined && entity_type !== 'Stix-Cyber-Observable'));
     return { entityTypeFilterValues, selectedElementsList, selectedTypes };
   }
 
