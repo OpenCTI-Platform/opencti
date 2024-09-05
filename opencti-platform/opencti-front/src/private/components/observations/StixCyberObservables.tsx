ebd041423276fc795b294eeb1436bcf213f2825b
Laurent Bonnet <laurent.bonnet@filigran.io>
2024-09-05 22:06:08 +0200

    [frontend] use DataTable in observables view

--- opencti-platform/opencti-front/src/private/components/observations/StixCyberObservables.tsx
+++ opencti-platform/opencti-front/src/private/components/observations/StixCyberObservables.tsx
@@ -1,24 +1,25 @@
 import { FunctionComponent } from 'react';
 import { React } from 'mdi-material-ui';
 import useHelper from 'src/utils/hooks/useHelper';
+import { StixCyberObservablesLines_data$data } from '@components/observations/stix_cyber_observables/__generated__/StixCyberObservablesLines_data.graphql';
+import { stixCyberObservableLineFragment } from '@components/observations/stix_cyber_observables/StixCyberObservableLine';
 import StixCyberObservableCreation from './stix_cyber_observables/StixCyberObservableCreation';
 import Security from '../../../utils/Security';
 import { usePaginationLocalStorage } from '../../../utils/hooks/useLocalStorage';
-import ListLines from '../../../components/list_lines/ListLines';
-import StixCyberObservablesLines, { stixCyberObservablesLinesQuery, stixCyberObservablesLinesSearchQuery } from './stix_cyber_observables/StixCyberObservablesLines';
-import ToolBar from '../data/ToolBar';
-import { StixCyberObservablesLinesPaginationQuery$data } from './stix_cyber_observables/__generated__/StixCyberObservablesLinesPaginationQuery.graphql';
-import { QueryRenderer } from '../../../relay/environment';
-import useCopy from '../../../utils/hooks/useCopy';
-import { StixCyberObservablesLinesSearchQuery$data } from './stix_cyber_observables/__generated__/StixCyberObservablesLinesSearchQuery.graphql';
+import { stixCyberObservablesLinesFragment, stixCyberObservablesLinesQuery } from './stix_cyber_observables/StixCyberObservablesLines';
+import {
+  StixCyberObservablesLinesPaginationQuery,
+  StixCyberObservablesLinesPaginationQuery$variables,
+} from './stix_cyber_observables/__generated__/StixCyberObservablesLinesPaginationQuery.graphql';
 import useAuth from '../../../utils/hooks/useAuth';
 import { KNOWLEDGE_KNUPDATE } from '../../../utils/hooks/useGranted';
 import ExportContextProvider from '../../../utils/ExportContextProvider';
-import useEntityToggle from '../../../utils/hooks/useEntityToggle';
-import { StixCyberObservableLine_node$data } from './stix_cyber_observables/__generated__/StixCyberObservableLine_node.graphql';
 import { useBuildEntityTypeBasedFilterContext, emptyFilterGroup, useGetDefaultFilterObject } from '../../../utils/filters/filtersUtils';
 import { useFormatter } from '../../../components/i18n';
 import Breadcrumbs from '../../../components/Breadcrumbs';
+import useQueryLoading from '../../../utils/hooks/useQueryLoading';
+import { UsePreloadedPaginationFragment } from '../../../utils/hooks/usePreloadedPaginationFragment';
+import DataTable from '../../../components/dataGrid/DataTable';
 
 const LOCAL_STORAGE_KEY = 'stixCyberObservables';
 
@@ -28,205 +29,107 @@ const StixCyberObservables: FunctionComponent = () => {
   const {
     platformModuleHelpers: { isRuntimeFieldEnable },
   } = useAuth();
+
   const isRuntimeSort = isRuntimeFieldEnable() ?? false;
-
-  const { viewStorage, paginationOptions, helpers } = usePaginationLocalStorage(
-    LOCAL_STORAGE_KEY,
-    {
-      filters: {
-        ...emptyFilterGroup,
-        filters: useGetDefaultFilterObject(['entity_type', 'sightedBy'], ['Stix-Cyber-Observable']),
-      },
-      searchTerm: '',
-      sortBy: 'created_at',
-      orderAsc: false,
-      openExports: false,
-    },
-  );
-  const {
-    numberOfElements,
-    filters,
-    searchTerm,
-    sortBy,
-    orderAsc,
-    openExports,
-  } = viewStorage;
-
-  const contextFilters = useBuildEntityTypeBasedFilterContext('Stix-Cyber-Observable', filters);
-
-  const {
-    onToggleEntity,
-    numberOfSelectedElements,
-    handleClearSelectedElements,
-    selectedElements,
-    deSelectedElements,
-    handleToggleSelectAll,
-    selectAll,
-  } = useEntityToggle<StixCyberObservableLine_node$data>(LOCAL_STORAGE_KEY);
-
-  const getValuesForCopy = (
-    data: StixCyberObservablesLinesSearchQuery$data,
-  ) => {
-    return (data.stixCyberObservables?.edges ?? []).map((o) => (o
-      ? { id: o.node.id, value: o.node.observable_value }
-      : { id: '', value: '' }));
-  };
-
-  const handleCopy = useCopy<StixCyberObservablesLinesSearchQuery$data>(
-    {
-      filters: contextFilters,
-      searchTerm: searchTerm ?? '',
-      query: stixCyberObservablesLinesSearchQuery,
-      selectedValues: Object.values(selectedElements).map(
-        ({ observable_value }) => observable_value,
-      ),
-      deselectedIds: Object.values(deSelectedElements).map((o) => o.id),
-      getValuesForCopy,
-    },
-    selectAll,
-  );
-
-  const buildColumns = () => {
-    return {
-      entity_type: {
-        label: 'Type',
-        width: '12%',
-        isSortable: true,
-      },
-      observable_value: {
-        label: 'Representation',
-        width: '25%',
-        isSortable: isRuntimeSort,
-      },
-      createdBy: {
-        label: 'Author',
-        width: '12%',
-        isSortable: isRuntimeSort,
-      },
-      creator: {
-        label: 'Creators',
-        width: '12%',
-        isSortable: isRuntimeSort,
-      },
-      objectLabel: {
-        label: 'Labels',
-        width: '15%',
-        isSortable: false,
-      },
-      created_at: {
-        label: 'Platform creation date',
-        width: '10%',
-        isSortable: true,
-      },
-      objectMarking: {
-        label: 'Marking',
-        width: '10%',
-        isSortable: isRuntimeSort,
-      },
-    };
-  };
-
   const isFABReplaced = isFeatureEnable('FAB_REPLACEMENT');
 
-  const renderLines = () => {
-    return (
-      <>
-        <ListLines
-          helpers={helpers}
-          sortBy={sortBy}
-          orderAsc={orderAsc}
-          dataColumns={buildColumns()}
-          handleSort={helpers.handleSort}
-          handleSearch={helpers.handleSearch}
-          handleAddFilter={helpers.handleAddFilter}
-          handleRemoveFilter={helpers.handleRemoveFilter}
-          handleSwitchGlobalMode={helpers.handleSwitchGlobalMode}
-          handleSwitchLocalMode={helpers.handleSwitchLocalMode}
-          handleToggleExports={helpers.handleToggleExports}
-          openExports={openExports}
-          handleToggleSelectAll={handleToggleSelectAll}
-          selectAll={selectAll}
-          exportContext={{ entity_type: 'Stix-Cyber-Observable' }}
-          availableEntityTypes={['Stix-Cyber-Observable']}
-          keyword={searchTerm}
-          filters={filters}
-          iconExtension={true}
-          paginationOptions={paginationOptions}
-          numberOfElements={numberOfElements}
-          createButton={isFABReplaced && <Security needs={[KNOWLEDGE_KNUPDATE]}>
-            <StixCyberObservableCreation
-              paginationKey="Pagination_stixCyberObservables"
-              paginationOptions={paginationOptions}
-              contextual={false}
-              open={false}
-              handleClose={undefined}
-              onCompleted={undefined}
-              isFromBulkRelation={undefined}
-              type={undefined}
-              display={undefined}
-              speeddial={false}
-              inputValue={undefined}
-            />
-          </Security>}
-        >
-          <QueryRenderer
-            query={stixCyberObservablesLinesQuery}
-            variables={paginationOptions}
-            render={({
-              props,
-            }: {
-              props: StixCyberObservablesLinesPaginationQuery$data;
-            }) => (
-              <StixCyberObservablesLines
-                data={props}
-                paginationOptions={paginationOptions}
-                dataColumns={buildColumns()}
-                initialLoading={props === null}
-                onLabelClick={helpers.handleAddFilter}
-                selectedElements={selectedElements}
-                deSelectedElements={deSelectedElements}
-                onToggleEntity={onToggleEntity}
-                selectAll={selectAll}
-                setNumberOfElements={helpers.handleSetNumberOfElements}
-              />
-            )}
-          />
-        </ListLines>
-        <ToolBar
-          selectedElements={selectedElements}
-          deSelectedElements={deSelectedElements}
-          numberOfSelectedElements={numberOfSelectedElements}
-          selectAll={selectAll}
-          filters={contextFilters}
-          search={searchTerm}
-          handleClearSelectedElements={handleClearSelectedElements}
-          handleCopy={handleCopy}
-        />
-      </>
-    );
+  const initialValues = {
+    filters: {
+      ...emptyFilterGroup,
+      filters: useGetDefaultFilterObject(['entity_type', 'sightedBy'], ['Stix-Cyber-Observable']),
+    },
+    searchTerm: '',
+    sortBy: 'created_at',
+    orderAsc: false,
+    openExports: false,
+  };
+
+  const { viewStorage, paginationOptions, helpers: storageHelpers } = usePaginationLocalStorage(
+    LOCAL_STORAGE_KEY,
+    initialValues,
+  );
+
+  const contextFilters = useBuildEntityTypeBasedFilterContext('Stix-Cyber-Observable', viewStorage.filters);
+  const queryPaginationOptions = {
+    ...paginationOptions,
+    filters: contextFilters,
+  } as unknown as StixCyberObservablesLinesPaginationQuery$variables;
+  const queryRef = useQueryLoading<StixCyberObservablesLinesPaginationQuery>(
+    stixCyberObservablesLinesQuery,
+    queryPaginationOptions,
+  );
+
+  const preloadedPaginationProps = {
+    linesQuery: stixCyberObservablesLinesQuery,
+    linesFragment: stixCyberObservablesLinesFragment,
+    queryRef,
+    nodePath: ['stixCyberObservables', 'pageInfo', 'globalCount'],
+    setNumberOfElements: storageHelpers.handleSetNumberOfElements,
+  } as UsePreloadedPaginationFragment<StixCyberObservablesLinesPaginationQuery>;
+
+  const dataColumns = {
+    entity_type: {
+      percentWidth: 12,
+    },
+    observable_value: {
+      label: 'Representation',
+      percentWidth: 29,
+      isSortable: isRuntimeSort,
+    },
+    createdBy: {
+      percentWidth: 12,
+      isSortable: isRuntimeSort,
+    },
+    creator: {
+      percentWidth: 12,
+      isSortable: isRuntimeSort,
+    },
+    objectLabel: {
+      percentWidth: 15,
+    },
+    created_at: {
+      percentWidth: 10,
+    },
+    objectMarking: {
+      percentWidth: 10,
+      isSortable: isRuntimeSort,
+    },
   };
 
   return (
     <ExportContextProvider>
       <Breadcrumbs variant="list" elements={[{ label: t_i18n('Observations') }, { label: t_i18n('Observables'), current: true }]} />
-      {renderLines()}
-      {!isFABReplaced
-        && <Security needs={[KNOWLEDGE_KNUPDATE]}>
-          <StixCyberObservableCreation
-            paginationKey="Pagination_stixCyberObservables"
-            paginationOptions={paginationOptions}
-            contextual={false}
-            open={false}
-            handleClose={undefined}
-            onCompleted={undefined}
-            isFromBulkRelation={undefined}
-            type={undefined}
-            display={undefined}
-            speeddial={false}
-            inputValue={undefined}
-          />
-        </Security>
-      }
+      {queryRef && (
+        <DataTable
+          storageKey={LOCAL_STORAGE_KEY}
+          initialValues={initialValues}
+          preloadedPaginationProps={preloadedPaginationProps}
+          resolvePath={(data: StixCyberObservablesLines_data$data) => data.stixCyberObservables?.edges?.map?.((n) => n?.node)}
+          dataColumns={dataColumns}
+          lineFragment={stixCyberObservableLineFragment}
+          toolbarFilters={contextFilters}
+          exportContext={{ entity_type: 'Stix-Cyber-Observable' }}
+          availableEntityTypes={['Stix-Cyber-Observable']}
+          searchContextFinal={{ entityTypes: ['Stix-Cyber-Observable'] }} // ???? for entity_type fileter
+          createButton={!isFABReplaced
+            && <Security needs={[KNOWLEDGE_KNUPDATE]}>
+              <StixCyberObservableCreation
+                paginationKey="Pagination_stixCyberObservables"
+                paginationOptions={queryPaginationOptions}
+                contextual={false}
+                open={false}
+                handleClose={undefined}
+                onCompleted={undefined}
+                isFromBulkRelation={undefined}
+                type={undefined}
+                display={undefined}
+                speeddial={false}
+                inputValue={undefined}
+              />
+            </Security>
+          }
+        />
+      )}
     </ExportContextProvider>
   );
 };
