092d35187a630194fb6376beff4688d8f8a0a41e
Souad Hadjiat <souad.hadjiat@filigran.io>
2024-09-13 16:35:17 +0200

    [backend] fix & test migration

--- opencti-platform/opencti-graphql/src/migrations/1726041032381-delete-files-duplicates.js
+++ opencti-platform/opencti-graphql/src/migrations/1726041032381-delete-files-duplicates.js
@@ -1,8 +1,8 @@
-import { groupBy, uniq } from 'ramda';
+import { groupBy } from 'ramda';
 import { logApp } from '../config/conf';
 import { listAllEntities } from '../database/middleware-loader';
 import { ENTITY_TYPE_INTERNAL_FILE } from '../schema/internalObject';
-import {elDeleteInstances, elIndexGetAlias} from '../database/engine';
+import { elDeleteInstances, elIndexGetAlias } from '../database/engine';
 import { executionContext, SYSTEM_USER } from '../utils/access';
 import { READ_INDEX_INTERNAL_OBJECTS } from '../database/utils';
 
@@ -23,11 +23,11 @@ export const up = async (next) => {
       if (filesList.length > 1) { // if a duplicate exists
         const maximumLastModified = filesList.map((h) => h.lastModified).sort((a, b) => b.localeCompare(a))[0];
         const olderFiles = filesList.filter((f) => f.lastModified !== maximumLastModified);
-        filesToDelete.push(olderFiles);
+        filesToDelete.push(...olderFiles);
       }
     });
     // keep uniq couples (id, index) of files to delete
-    const finalFilesToDelete = uniq(filesToDelete.map((h) => [h._index, h.internal_id]));
+    const finalFilesToDelete = filesToDelete.map((h) => ({ _index: h._index, internal_id: h.internal_id }));
     logApp.info(`Deleting ${finalFilesToDelete.length} files that have duplicates.`);
     // delete the files
     await elDeleteInstances(finalFilesToDelete);
