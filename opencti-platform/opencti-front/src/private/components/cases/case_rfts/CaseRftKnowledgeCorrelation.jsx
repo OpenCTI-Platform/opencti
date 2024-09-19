cfc1f6f7ab9534e10e164dda6928ed5be989ce06
Archidoit <75783086+Archidoit@users.noreply.github.com>
2024-09-19 09:22:28 +0200

    [frontend] Fix search in Containers Correlation view (#8162)

--- opencti-platform/opencti-front/src/private/components/cases/case_rfts/CaseRftKnowledgeCorrelation.jsx
+++ opencti-platform/opencti-front/src/private/components/cases/case_rfts/CaseRftKnowledgeCorrelation.jsx
@@ -30,6 +30,8 @@ import { caseRftMutationFieldPatch } from './CaseRftEditionOverview';
 import { UserContext } from '../../../../utils/hooks/useAuth';
 import CaseRftKnowledgeGraphBar from './CaseRftKnowledgeGraphBar';
 import EntitiesDetailsRightsBar from '../../../../utils/graph/EntitiesDetailsRightBar';
+import { isNotEmptyField } from '../../../../utils/utils';
+import { getMainRepresentative, getSecondaryRepresentative } from '../../../../utils/defaultRepresentatives';
 
 const PARAMETERS$ = new Subject().pipe(debounce(() => timer(2000)));
 const POSITIONS$ = new Subject().pipe(debounce(() => timer(2000)));
@@ -811,25 +813,20 @@ class CaseRftKnowledgeCorrelationComponent extends Component {
   }
 
   handleSearch(keyword) {
-    const filterAdjust = {
-      selectedTimeRangeInterval: this.state.selectedTimeRangeInterval,
-      markedBy: this.state.markedBy,
-      createdBy: this.state.createdBy,
-      stixCoreObjectsTypes: this.state.stixCoreObjectsTypes,
-      keyword,
-    };
-    this.setState(
-      {
-        selectedTimeRangeInterval: filterAdjust.selectedTimeRangeInterval,
-        graphData: buildCorrelationData(
-          this.graphObjects,
-          decodeGraphData(this.props.grouping.x_opencti_graph_data),
-          this.props.t,
-          filterAdjust,
-        ),
-      },
-      () => this.saveParameters(false),
-    );
+    this.selectedLinks.clear();
+    this.selectedNodes.clear();
+    if (isNotEmptyField(keyword)) {
+      const filterByKeyword = (n) => keyword === ''
+          || (getMainRepresentative(n) || '').toLowerCase().indexOf(keyword.toLowerCase())
+          !== -1
+          || (getSecondaryRepresentative(n) || '')
+            .toLowerCase()
+            .indexOf(keyword.toLowerCase()) !== -1
+          || (n.entity_type || '').toLowerCase().indexOf(keyword.toLowerCase())
+          !== -1;
+      this.state.graphData.nodes.map((n) => filterByKeyword(n) && this.selectedNodes.add(n));
+      this.setState({ numberOfSelectedNodes: this.selectedNodes.size });
+    }
   }
 
   render() {
