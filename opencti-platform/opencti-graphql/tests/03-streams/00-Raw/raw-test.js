686d100678ffd2ef86a8e210d17bf19bfe266d94
Julien Richard <julien.richard@filigran.io>
2024-09-29 12:04:10 +0200

    [backend] Fix test count

--- opencti-platform/opencti-graphql/tests/03-streams/00-Raw/raw-test.js
+++ opencti-platform/opencti-graphql/tests/03-streams/00-Raw/raw-test.js
@@ -62,7 +62,7 @@ describe('Raw streams tests', () => {
       expect(updateEventsByTypes['malware'].length).toBe(16);
       expect(updateEventsByTypes['intrusion-set'].length).toBe(4);
       expect(updateEventsByTypes['data-component'].length).toBe(4);
-      expect(updateEventsByTypes['location'].length).toBe(13);
+      expect(updateEventsByTypes['location'].length).toBe(14);
       expect(updateEventsByTypes['attack-pattern'].length).toBe(3);
       expect(updateEventsByTypes['feedback'].length).toBe(1);
       expect(updateEventsByTypes['course-of-action'].length).toBe(3);
@@ -82,7 +82,7 @@ describe('Raw streams tests', () => {
       expect(updateEventsByTypes['threat-actor'].length).toBe(17);
       expect(updateEventsByTypes['vocabulary'].length).toBe(3);
       expect(updateEventsByTypes['vulnerability'].length).toBe(3);
-      expect(updateEvents.length).toBe(162);
+      expect(updateEvents.length).toBe(163);
       for (let updateIndex = 0; updateIndex < updateEvents.length; updateIndex += 1) {
         const event = updateEvents[updateIndex];
         const { data: insideData, origin, type } = event;
