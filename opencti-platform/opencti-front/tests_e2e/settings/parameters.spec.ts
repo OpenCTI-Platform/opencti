9b5556cd302a8c20aa6e6fba02defd66a5cfc154
Bonsai8863 <131906254+Bonsai8863@users.noreply.github.com>
2024-10-01 07:39:21 -0400

    [frontend] Custom Theme Support
    [backend] Added Theme internal object + resolvers
    
    [frontend] Theme creation and edition supported in UI
    
    [frontend] Require every color in theme creation
    
    [frontend/backend] Custom theme logo used
    
    [backend] Added migration file to add default light/dark themes
    
    [frontend] User profile theme customization option
    
    [frontend] Import/Export theme support
    
    [frontend] Finished base custom theme support
    
    [frontend] Added translations
    
    [frontend] Updating migration file timestamp
    
    [backend] Modified theme fetch function to create themes if none exist
    
    [frontend] Updated logo replacement test
    
    [frontend] Updated Settings.tsx to use new theme settings
    
    [frontend] Custom theme logo replacement
    
    [backend/frontend] Addressing review comments
    
    [frontend] Resolved type import
    
    [frontend] Various PR comments addressed
    
    [frontend] Modified custom themes workflow
    
    [frontend] Fixed generated graphql imports after moving files to subfolder
    
    [frontend/backend] WIP: Updated import/export functionality
    
    [frontend/backend] Translated system themes
    
    [frontend/backend] Custom theme support fully functional
    
    [frontend] Fixing e2e tests and adding translations
    
    [frontend] Changed test to expect light mode
    
    [frontend/backend] Migration script to set default theme
    
    [backend] Setting initializeData's platform_theme to "Dark"
    
    [frontend] Settings test sets theme for consistency

--- opencti-platform/opencti-front/tests_e2e/settings/parameters.spec.ts
+++ opencti-platform/opencti-front/tests_e2e/settings/parameters.spec.ts
@@ -1,5 +1,15 @@
+import { Page } from '@playwright/test';
 import { expect, test } from '../fixtures/baseFixtures';
 import LeftBarPage from '../model/menu/leftBar.pageModel';
+
+const openThemeEditMenu = async (themeName: string, page: Page) => {
+  await page
+    .getByTestId(`${themeName}-popover`)
+    .click();
+  await page
+    .getByRole('menuitem', { name: 'Update' })
+    .click();
+};
 
 test('Check Logo replacement', async ({ page }) => {
   const leftBarPage = new LeftBarPage(page);
@@ -8,23 +18,38 @@ test('Check Logo replacement', async ({ page }) => {
   await leftBarPage.open();
   await leftBarPage.clickOnMenu('Settings', 'Parameters');
 
+  // Set platform theme to be Dark
+  await page.locator("#mui-component-select-platform_theme").click();
+  await page.locator('li[data-value="Dark"]').click();
+
   let logoSrc = await page.getByRole('link', { name: 'logo' }).locator('img').getAttribute('src');
   expect(logoSrc).toContain('static/images/logo');
+
+  // Set Dark theme logo to the Google logo
+  openThemeEditMenu('Dark', page);
   await page
-    .locator('input[name="platform_theme_dark_logo"]')
+    .locator('input[name="theme_logo"]')
     .fill('https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png');
   await page
-    .locator('input[name="platform_theme_dark_logo"]')
-    .press('Tab');
+    .getByLabel('Close')
+    .click();
   await page.waitForTimeout(1000);
   logoSrc = await page.getByRole('link', { name: 'logo' }).locator('img').getAttribute('src');
   expect(logoSrc).not.toContain('static/images/logo');
+
+  // Close toast
   await page
-    .locator('input[name="platform_theme_dark_logo"]')
+    .getByLabel('Close')
+    .click();
+  
+  // Reset logo
+  openThemeEditMenu('Dark', page);
+  await page
+    .locator('input[name="theme_logo"]')
     .fill('');
   await page
-    .locator('input[name="platform_theme_dark_logo"]')
-    .press('Tab');
+    .getByLabel('Close')
+    .click();
   await page.waitForTimeout(1000);
   logoSrc = await page.getByRole('link', { name: 'logo' }).locator('img').getAttribute('src');
   expect(logoSrc).toContain('static/images/logo');
