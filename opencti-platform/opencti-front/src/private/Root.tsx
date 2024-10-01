86e20dd95c9efd91eaf38f51418e88e4c3fec849
Bonsai8863 <131906254+Bonsai8863@users.noreply.github.com>
2024-10-01 07:39:21 -0400

    [frontend] Custom Theme Support
    [backend] Added Theme internal object + resolvers
    
    [frontend] Theme creation and edition supported in UI
    
    [frontend] Require every color in theme creation
    
    [frontend/backend] Custom theme logo used
    
    [backend] Added migration file to add default light/dark themes
    
    [frontend] User profile theme customization option
    
    [frontend] Import/Export theme support
    
    [frontend] Finished base custom theme support
    
    [frontend] Added translations
    
    [frontend] Updating migration file timestamp
    
    [backend] Modified theme fetch function to create themes if none exist
    
    [frontend] Updated logo replacement test
    
    [frontend] Updated Settings.tsx to use new theme settings
    
    [frontend] Custom theme logo replacement

--- opencti-platform/opencti-front/src/private/Root.tsx
+++ opencti-platform/opencti-front/src/private/Root.tsx
@@ -228,6 +228,22 @@ const rootPrivateQuery = graphql`
     settings {
       ...RootSettings
     }
+    themes {
+      edges {
+        node {
+          name
+          theme_background
+          theme_paper
+          theme_nav
+          theme_primary
+          theme_secondary
+          theme_accent
+          theme_logo
+          theme_logo_collapsed
+          theme_logo_login
+        }
+      }
+    }
     about {
       version
     }
@@ -337,6 +353,7 @@ const RootComponent: FunctionComponent<RootComponentProps> = ({ queryRef }) => {
   const {
     me: meFragment,
     settings: settingsFragment,
+    themes,
     entitySettings,
     schemaSCOs,
     schemaSDOs,
@@ -388,7 +405,7 @@ const RootComponent: FunctionComponent<RootComponentProps> = ({ queryRef }) => {
       }}
     >
       <StyledEngineProvider injectFirst={true}>
-        <ConnectedThemeProvider settings={settings}>
+        <ConnectedThemeProvider settings={settings} themes={themes}>
           <ConnectedIntlProvider settings={settings}>
             <AnalyticsProvider instance={Analytics(platformAnalyticsConfiguration)}>
               <Index settings={settings} />
