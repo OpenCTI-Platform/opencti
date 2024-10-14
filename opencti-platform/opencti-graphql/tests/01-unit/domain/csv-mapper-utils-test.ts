b2a15c04cb0c1c1df4d99766aef548bb2309879e
Celine Sebe <celine.sebe@filigran.io>
2024-10-14 15:38:07 +0200

    [backend]clean up(#7400)

--- opencti-platform/opencti-graphql/tests/01-unit/domain/csv-mapper-utils-test.ts
+++ opencti-platform/opencti-graphql/tests/01-unit/domain/csv-mapper-utils-test.ts
@@ -1,5 +1,5 @@
 import { assert, describe, expect, it } from 'vitest';
-import { csvMapperMockSimpleDifferentEntities, csvMapperMockWithDynamicColumn } from '../../02-integration/05-parser/dynamic-simple-test/csv-mapper-mock-simple-different-entities';
+import { csvMapperMockSimpleDifferentEntities } from '../../02-integration/05-parser/dynamic-simple-test/csv-mapper-mock-simple-different-entities';
 import { validateCsvMapper } from '../../../src/modules/internal/csvMapper/csvMapper-utils';
 import { ADMIN_USER, testContext } from '../../utils/testQuery';
 import type { CsvMapperParsed } from '../../../src/modules/internal/csvMapper/csvMapper-types';
@@ -31,15 +31,5 @@ describe('CSV Mapper - without dynamic entity', () => {
         mapper.representations[1],
       ]
     })).rejects.toThrowError('Missing values for required attribute');
-  });
-});
-
-describe('CSV Mapper with dynamic entity - parsing of mapper configuration', () => {
-  it('should a valid csv mapper with dynamic entity be valid', async () => {
-    await validateCsvMapper(testContext, ADMIN_USER, {
-      ...csvMapperMockWithDynamicColumn as CsvMapperParsed,
-      name: 'Valid dynamic Mapper'
-    });
-    assert(true);
   });
 });
