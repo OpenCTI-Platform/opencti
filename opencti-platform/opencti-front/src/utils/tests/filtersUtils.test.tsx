07d0ba0f8c8a8477218ce15c48bc383602a23cab
Landry Trebon <landry.trebon@filigran.io>
2024-10-17 14:11:57 +0200

    fix test

--- opencti-platform/opencti-front/src/utils/tests/filtersUtils.test.tsx
+++ opencti-platform/opencti-front/src/utils/tests/filtersUtils.test.tsx
@@ -1,9 +1,6 @@
 import { describe, it, expect } from 'vitest';
-import { renderHook } from '@testing-library/react';
-import React from 'react';
 import { getEntityTypeTwoFirstLevelsFilterValues, useBuildFilterKeysMapFromEntityType } from '../filters/filtersUtils';
-import { createMockUserContext, ProvidersWrapper, ProvidersWrapperProps } from './test-render';
-import { BYPASS } from '../hooks/useGranted';
+import { createMockUserContext, testRenderHook } from './test-render';
 import filterKeysSchema from './FilterUtilsConstants';
 
 describe('Filters utils', () => {
@@ -11,37 +8,23 @@ describe('Filters utils', () => {
     it('should list filter definitions by given entity types attributes', () => {
       const stixCoreObjectKey = 'Stix-Core-Object';
       const entityTypes = [stixCoreObjectKey];
-      const wrapper = ({ children }: ProvidersWrapperProps) => {
-        return (
-          <ProvidersWrapper
-            userContext={ createMockUserContext({
-              me: {
-                name: 'admin',
-                user_email: 'admin@opencti.io',
-                capabilities: [{ name: BYPASS }],
-              },
-              settings: undefined,
-              bannerSettings: undefined,
-              entitySettings: undefined,
-              platformModuleHelpers: undefined,
-              schema: {
-                scos: [{ id: '', label: '' }],
-                sdos: [{ id: '', label: '' }],
-                smos: [{ id: '', label: '' }],
-                scrs: [{ id: '', label: '' }],
-                schemaRelationsTypesMapping: new Map<string, readonly string[]>(),
-                schemaRelationsRefTypesMapping: new Map<string, readonly { name: string, toTypes: string[] }[]>(),
-                filterKeysSchema,
-              },
-            })
-            }
-          >
-            {children}
-          </ProvidersWrapper>
-        );
-      };
-      const { result } = renderHook(() => useBuildFilterKeysMapFromEntityType(entityTypes), { wrapper });
-      expect(result.current).toStrictEqual(filterKeysSchema.get(stixCoreObjectKey));
+      const { hook } = testRenderHook(
+        () => useBuildFilterKeysMapFromEntityType(entityTypes),
+        {
+          userContext: createMockUserContext({
+            schema: {
+              scos: [{ id: '', label: '' }],
+              sdos: [{ id: '', label: '' }],
+              smos: [{ id: '', label: '' }],
+              scrs: [{ id: '', label: '' }],
+              schemaRelationsTypesMapping: new Map<string, readonly string[]>(),
+              schemaRelationsRefTypesMapping: new Map<string, readonly { name: string, toTypes: string[] }[]>(),
+              filterKeysSchema,
+            },
+          }),
+        },
+      );
+      expect(hook.result.current).toStrictEqual(filterKeysSchema.get(stixCoreObjectKey));
     });
   });
 
