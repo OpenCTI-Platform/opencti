d339b9c95047772dfe037f5f34b5b7d20db65b09
Celine Sebe <celine.sebe@filigran.io>
2024-10-17 16:46:24 +0200

    [front/back]remove dynamic_mapping_name(#7400)

--- opencti-platform/opencti-graphql/src/modules/internal/csvMapper/csvMapper-types.ts
+++ opencti-platform/opencti-graphql/src/modules/internal/csvMapper/csvMapper-types.ts
@@ -53,7 +53,6 @@ export interface CsvMapperRepresentation {
   attributes: CsvMapperRepresentationAttribute[]
   from?: string
   to?: string
-  dynamic_mapping_name?: string
 }
 
 export interface CsvMapperRepresentationAttributeResolved {
@@ -75,7 +74,6 @@ export interface CsvMapperRepresentationResolved {
 export type CsvMapperParsed = Omit<BasicStoreEntityCsvMapper, 'representations'> & {
   representations: CsvMapperRepresentation[]
   user_chosen_markings?: string[]
-  has_entity_dynamic_mapping?: boolean
 };
 
 export type CsvMapperResolved = Omit<BasicStoreEntityCsvMapper, 'representations'> & {
