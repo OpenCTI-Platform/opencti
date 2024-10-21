6d54d59e3316e4a62879c0b3cb06caec9cad0102
Gwendoline-FAVRE-FELIX <gwendoline.favre-felix@filigran.io>
2024-10-21 14:24:34 +0200

    [frontend] Enable enrichment with the selectAll of observables and stix-domain-objects of the same type (#5582)

--- opencti-platform/opencti-front/src/utils/filters/filtersUtils.tsx
+++ opencti-platform/opencti-front/src/utils/filters/filtersUtils.tsx
@@ -198,6 +198,36 @@ export const removeEntityTypeAllFromFilterGroup = (inputFilters?: FilterGroup) =
   return inputFilters;
 };
 
+// fetch the entity type filters possible values of first and second levels
+// and remove Observable if the filters target only some sub observable types
+// exemple: Observable AND (Domain-Name) --> [Domain-Name]
+// exemple: Domain-Name OR Observable --> [Domain-Name, Observable]
+// exemple: Stix-Domain-Object AND (Malware OR (Country AND City)) --> [Stix-Domain-Object, Malware]
+export const getEntityTypeTwoFirstLevelsFilterValues = (filters?: FilterGroup, observableTypes?: string[], domainObjectTypes?: string []) => {
+  if (!filters) {
+    return [];
+  }
+  let firstLevelValues = findFilterFromKey(filters.filters, 'entity_type', 'eq')?.values ?? [];
+  const subFiltersSeparatedWithAnd = filters.filterGroups
+    .filter((fg) => fg.mode === 'and' || (fg.mode === 'or' && fg.filters.length === 1))
+    .map((fg) => fg.filters)
+    .flat();
+  if (subFiltersSeparatedWithAnd.length > 0) {
+    const secondLevelValues = findFilterFromKey(subFiltersSeparatedWithAnd, 'entity_type', 'eq')?.values ?? [];
+    if (filters.mode === 'and') {
+      // if all second values are observables sub types : remove observable from firstLevelValue
+      if (secondLevelValues.every((type) => observableTypes?.includes(type))) {
+        firstLevelValues = firstLevelValues.filter((type) => type !== 'Stix-Cyber-Observable');
+      }
+      if (secondLevelValues.every((type) => domainObjectTypes?.includes(type))) {
+        firstLevelValues = firstLevelValues.filter((type) => type !== 'Stix-Domain-Object');
+      }
+    }
+    return [...firstLevelValues, ...secondLevelValues];
+  }
+  return firstLevelValues;
+};
+
 // construct filters and options for widgets
 export const buildFiltersAndOptionsForWidgets = (
   inputFilters: FilterGroup | undefined,
