43c28a1f53deba2119efd7cfc60ee4c7562b3b35
Landry Trebon <landry.trebon@filigran.io>
2024-10-24 14:13:39 +0200

    fix

--- opencti-platform/opencti-front/src/utils/object.ts
+++ opencti-platform/opencti-front/src/utils/object.ts
@@ -1,5 +1,6 @@
 /**
  * Get the value of a property of an object.
+ * Examples of path : objectLabel.value, representative.main, created_at...
  *
  * @param object The object to manipulate.
  * @param path The path to access the property.
@@ -11,9 +12,9 @@ function getObjectProperty<T extends object>(object: T, path = ''): unknown {
 
   // If at the end of the path, stop recursion.
   if (splitPath.length === 0) return property;
-  // If not at the end of the path but the value is not an object, error.
-  if (typeof property !== 'object' || property === null) throw Error('Ínvalid path, a subpart is not an object');
-  // Continue deeper, if array, need to continue for each element of the array.
+  // If not at the end of the path but the value is not an object, throw error.
+  if (typeof property !== 'object' || property === null) throw Error('Invalid path, a subpart is not an object');
+  // Continue deeper, if it's array then need to continue for each element of the array.
   return Array.isArray(property)
     ? property.map((el) => getObjectProperty(el, splitPath.join('.')))
     : getObjectProperty(property, splitPath.join('.'));
