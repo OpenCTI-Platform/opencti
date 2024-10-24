96473b1cfdf8b61f52c7d5b4a1fca3096962f3e5
daimyo007 <165801009+daimyo007@users.noreply.github.com>
2024-10-24 04:01:01 -0400

    [frontend] adding header and enrichment file

--- opencti-platform/opencti-front/src/private/components/common/stix_core_objects/StixCoreObjectEnrichment.jsx
+++ opencti-platform/opencti-front/src/private/components/common/stix_core_objects/StixCoreObjectEnrichment.jsx
@@ -14,7 +14,7 @@ const StixCoreObjectEnrichment = (props) => {
   const { t, stixCoreObjectId, handleClose, open } = props;
   // otherwise, a button + internal state allow to open and close
   const [openDrawer, setOpenDrawer] = useState(false);
-
+  const [search, setSearch] = useState('');
   const { isFeatureEnable } = useHelper();
   const isFABReplaced = isFeatureEnable('FAB_REPLACEMENT');
 
@@ -23,12 +23,49 @@ const StixCoreObjectEnrichment = (props) => {
   };
   const handleCloseEnrichment = () => {
     setOpenDrawer(false);
+    setSearch('');
   };
 
   return (
     <>
-      {(isFABReplaced || !handleClose) && (
-        <Tooltip title={t('Enrichment')}>
+      {isFABReplaced && <Tooltip title={t('Enrichment')}>
+        <ToggleButton
+          onClick={handleOpenEnrichment}
+          value="enrich"
+          size="small"
+          style={{ marginRight: 3 }}
+        >
+          <CloudRefreshOutline fontSize="small" color="primary" />
+        </ToggleButton>
+      </Tooltip>}
+      {isFABReplaced && <Drawer
+        open={openDrawer}
+        onClose={() => { setOpenDrawer(false); setSearch(''); }}
+        title={t('Enrichment connectors')}
+      >
+        <QueryRenderer
+          query={stixCoreObjectEnrichmentLinesQuery}
+          variables={{ id: stixCoreObjectId }}
+          render={({ props: queryProps }) => {
+            if (
+              queryProps
+              && queryProps.stixCoreObject
+              && queryProps.connectorsForImport
+            ) {
+              return (
+                <StixCoreObjectEnrichmentLines
+                  stixCoreObject={queryProps.stixCoreObject}
+                  connectorsForImport={queryProps.connectorsForImport}
+                  search={search}
+                />
+              );
+            }
+            return <div />;
+          }}
+        />
+      </Drawer>}
+      {!isFABReplaced && !handleClose
+        && <Tooltip title={t('Enrichment')}>
           <ToggleButton
             onClick={handleOpenEnrichment}
             value="enrich"
@@ -38,9 +75,9 @@ const StixCoreObjectEnrichment = (props) => {
             <CloudRefreshOutline fontSize="small" color="primary" />
           </ToggleButton>
         </Tooltip>
-      )}
-      <Drawer
-        open={open || openDrawer}
+      }
+      {!isFABReplaced && <Drawer
+        open={open || handleOpenEnrichment}
         onClose={handleClose || handleCloseEnrichment}
         title={t('Enrichment connectors')}
       >
@@ -63,7 +100,7 @@ const StixCoreObjectEnrichment = (props) => {
             return <div />;
           }}
         />
-      </Drawer>
+      </Drawer>}
     </>
   );
 };
