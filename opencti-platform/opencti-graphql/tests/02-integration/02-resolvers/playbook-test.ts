b4757be2bce2fe52867be3daba2b8002cbdcaafe
Celine Sebe <celine.sebe@filigran.io>
2024-10-25 16:10:20 +0200

    [backend]create/delete playbook

--- opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/playbook-test.ts
+++ opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/playbook-test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import gql from 'graphql-tag';
 import { queryAsAdmin } from '../../utils/testQuery';
 
@@ -29,9 +29,46 @@ const LIST_PLAYBOOKS = gql`
   }
 `;
 
+const CREATE_PLAYBOOK = gql`
+  mutation playbookAdd($input: PlaybookAddInput!)
+  {
+    playbookAdd(input: $input){
+      id
+      name
+    }
+  }
+`;
+const DELETE_PLAYBOOK = gql`
+  mutation playbookDelete($id: ID!){
+    playbookDelete(id:$id)
+  }`;
+
 describe('Playbook resolver standard behavior', () => {
+  let playbookId = '';
   it('should list playbooks', async () => {
     const queryResult = await queryAsAdmin({ query: LIST_PLAYBOOKS, variables: { first: 10 } });
     expect(queryResult.data?.playbooks.edges.length).toEqual(0);
+  });
+  it('should add playbook', async () => {
+    const input = {
+      input: {
+        name: 'Playbook1',
+      }
+    };
+    const queryResult = await queryAsAdmin({
+      query: CREATE_PLAYBOOK,
+      variables: input,
+    });
+    expect(queryResult).not.toBeNull();
+    expect(queryResult.data?.playbookAdd.name).toEqual('Playbook1');
+    playbookId = queryResult.data?.playbookAdd.id;
+  });
+  it('should remove playbook', async () => {
+    const queryResult = await queryAsAdmin({
+      query: DELETE_PLAYBOOK,
+      variables: { id: playbookId },
+    });
+    expect(queryResult).not.toBeNull();
+    expect(queryResult.data?.playbookDelete).toEqual(playbookId);
   });
 });
