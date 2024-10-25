8c525adaa06bb07f0bc18f20467530a8adcfb0b7
marie flores <marie.flores@filigran.io>
2024-10-25 16:35:17 +0200

    [backend] Add list and read test for playbook resolver

--- opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/playbook-test.ts
+++ opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/playbook-test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 import gql from 'graphql-tag';
-import { queryAsAdmin } from '../../utils/testQuery';
+import { adminQueryWithSuccess } from '../../utils/testQueryHelper';
 
 const LIST_PLAYBOOKS = gql`
   query playbooks(
@@ -30,45 +30,66 @@ const LIST_PLAYBOOKS = gql`
 `;
 
 const CREATE_PLAYBOOK = gql`
-  mutation playbookAdd($input: PlaybookAddInput!)
-  {
+  mutation playbookAdd($input: PlaybookAddInput!) {
     playbookAdd(input: $input){
       id
       name
     }
   }
 `;
+
+const READ_PLAYBOOK = gql`
+  query playbook($id: String!) {
+    playbook(id: $id) {
+      id
+      name
+      description
+      playbook_running
+      playbook_definition
+    }
+  }
+`;
+
 const DELETE_PLAYBOOK = gql`
-  mutation playbookDelete($id: ID!){
+  mutation playbookDelete($id: ID!) {
     playbookDelete(id:$id)
-  }`;
+  }
+`;
 
 describe('Playbook resolver standard behavior', () => {
   let playbookId = '';
+  const playbookName = 'Playbook1';
   it('should list playbooks', async () => {
-    const queryResult = await queryAsAdmin({ query: LIST_PLAYBOOKS, variables: { first: 10 } });
+    const queryResult = await adminQueryWithSuccess({ query: LIST_PLAYBOOKS, variables: { first: 10 } });
     expect(queryResult.data?.playbooks.edges.length).toEqual(0);
   });
   it('should add playbook', async () => {
     const input = {
       input: {
-        name: 'Playbook1',
+        name: playbookName,
       }
     };
-    const queryResult = await queryAsAdmin({
+    const queryResult = await adminQueryWithSuccess({
       query: CREATE_PLAYBOOK,
       variables: input,
     });
-    expect(queryResult).not.toBeNull();
-    expect(queryResult.data?.playbookAdd.name).toEqual('Playbook1');
+    expect(queryResult.data?.playbookAdd.name).toEqual(playbookName);
     playbookId = queryResult.data?.playbookAdd.id;
   });
+  it('should list playbooks', async () => {
+    const queryResult = await adminQueryWithSuccess({ query: LIST_PLAYBOOKS, variables: { first: 10 } });
+    expect(queryResult.data?.playbooks.edges.length).toEqual(1);
+  });
+  it('should read playbook', async () => {
+    const queryResult = await adminQueryWithSuccess({ query: READ_PLAYBOOK, variables: { id: playbookId } });
+    expect(queryResult.data?.playbook.name).toEqual(playbookName);
+    expect(queryResult.data?.playbook.playbook_running).toEqual(false);
+  });
   it('should remove playbook', async () => {
-    const queryResult = await queryAsAdmin({
+    const queryResult = await adminQueryWithSuccess({
       query: DELETE_PLAYBOOK,
       variables: { id: playbookId },
     });
-    expect(queryResult).not.toBeNull();
     expect(queryResult.data?.playbookDelete).toEqual(playbookId);
   });
 });
