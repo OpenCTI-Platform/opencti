1864c66f04b47dbd835c8aab3f9442e024c59270
Francois GRUNERT <francois.grunert@filigran.io>
2024-10-30 15:07:16 +0100

    [frontend] connector errors UI - resolve entities - improve design (#8614)
    Co-authored-by: Laurent Bonnet <laurent.bonnet@filigran.io>

--- opencti-platform/opencti-front/src/private/components/data/connectors/ConnectorWorksErrorLine.tsx
+++ opencti-platform/opencti-front/src/private/components/data/connectors/ConnectorWorksErrorLine.tsx
@@ -10,7 +10,9 @@ import Dialog from '@mui/material/Dialog';
 import IconButton from '@mui/material/IconButton';
 import { InfoOutlined } from '@mui/icons-material';
 import Tooltip from '@mui/material/Tooltip';
-import { ParsedWorkMessage } from '@components/data/connectors/parseWorkErrors';
+import { ParsedWorkMessage, ResolvedEntity } from '@components/data/connectors/parseWorkErrors';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
 import ItemCopy from '../../../../components/ItemCopy';
 import Transition from '../../../../components/Transition';
 import { useFormatter } from '../../../../components/i18n';
@@ -23,6 +25,7 @@ interface ConnectorWorksErrorLineProps {
 const ConnectorWorksErrorLine: FunctionComponent<ConnectorWorksErrorLineProps> = ({ error }) => {
   const { t_i18n, nsdt } = useFormatter();
   const [openModalErrorDetails, setOpenModalErrorDetails] = useState<boolean>(false);
+  const truncateLimit = 80;
 
   const handleToggleModalError = () => {
     setOpenModalErrorDetails(!openModalErrorDetails);
@@ -31,6 +34,24 @@ const ConnectorWorksErrorLine: FunctionComponent<ConnectorWorksErrorLineProps> =
   if (!error.rawError) {
     return null;
   }
+
+  const displayEntityOrId = (entity: ResolvedEntity, isCopyable = false) => {
+    const name = entity.representative?.main;
+
+    const displayStandardId = (isCopyable ? (
+      <pre><ItemCopy content={entity.standard_id ?? ''} variant={'wrap'} /></pre>
+    ) : (
+      <div>{entity.standard_id}</div>
+    ));
+
+    return entity.entity_type ? (
+      <Tooltip title={name}>
+        <a href={`/dashboard/id/${entity.id}`} target="_blank" rel="noreferrer">
+          [{entity.entity_type}] {truncate(name, truncateLimit)}
+        </a>
+      </Tooltip>
+    ) : displayStandardId;
+  };
 
   return (
     <>
@@ -46,9 +67,11 @@ const ConnectorWorksErrorLine: FunctionComponent<ConnectorWorksErrorLineProps> =
         <TableCell>{error.isParsed ? error.parsedError.message : error.rawError.message}</TableCell>
         <TableCell>
           {error.isParsed ? (
-            <a href={`/dashboard/id/${error.parsedError.entity.id}`} target="_blank" rel="noreferrer">[{error.parsedError.entity.type}] {error.parsedError.entity.name}</a>
+            displayEntityOrId(error.parsedError.entity)
           ) : (
-            truncate(error.rawError.source, 50)
+            <Tooltip title={t_i18n('Click on details to see more information')}>
+              {truncate(error.rawError.source, truncateLimit)}
+            </Tooltip>
           )}
         </TableCell>
         <TableCell>
@@ -70,12 +93,57 @@ const ConnectorWorksErrorLine: FunctionComponent<ConnectorWorksErrorLineProps> =
         TransitionComponent={Transition}
         onClose={handleToggleModalError}
       >
-        <DialogTitle>{t_i18n('Error')}</DialogTitle>
-        <DialogContent>
+        <DialogTitle>{t_i18n('Details')}</DialogTitle>
+        <DialogContent sx={{ minWidth: '500px' }}>
           <DialogContentText>
-            <pre><ItemCopy content={error.rawError.timestamp ?? '-'} /></pre>
-            <pre><ItemCopy content={error.rawError.message ?? '-'} variant={'wrap'} /></pre>
-            <pre><ItemCopy content={error.rawError.source ?? '-'} variant={'wrap'} /></pre>
+            {error.isParsed && (
+              <>
+                <Typography variant="h4" gutterBottom={true}>{t_i18n('Source')}</Typography>
+                <Paper
+                  style={{ padding: '15px', borderRadius: 4, marginBottom: '15px' }}
+                  variant="outlined"
+                >
+                  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'stretch', gap: 15 }}>
+                    <div>
+                      <Typography variant="h3" gutterBottom={true}>{t_i18n('Entity')}</Typography>
+                      {displayEntityOrId(error.parsedError.entity, true)}
+                    </div>
+                    {error.parsedError.entity.from && (
+                      <div>
+                        <Typography variant="h3" gutterBottom={true}>{t_i18n('From')}</Typography>
+                        {displayEntityOrId(error.parsedError.entity.from, true)}
+                      </div>
+                    )}
+                    {error.parsedError.entity.to && (
+                      <div>
+                        <Typography variant="h3" gutterBottom={true}>{t_i18n('To')}</Typography>
+                        {displayEntityOrId(error.parsedError.entity.to, true)}
+                      </div>
+                    )}
+                  </div>
+                </Paper>
+              </>
+            )}
+            <Typography variant="h4" gutterBottom={true}>{t_i18n('Error')}</Typography>
+            <Paper
+              style={{ padding: '15px', borderRadius: 4 }}
+              variant="outlined"
+            >
+              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'stretch', gap: 15 }}>
+                <div>
+                  <Typography variant="h3" gutterBottom={true}>{t_i18n('Timestamp')}</Typography>
+                  <pre><ItemCopy content={error.rawError.timestamp ?? '-'} /></pre>
+                </div>
+                <div>
+                  <Typography variant="h3" gutterBottom={true}>{t_i18n('Message')}</Typography>
+                  <pre><ItemCopy content={error.rawError.message ?? '-'} variant={'wrap'} /></pre>
+                </div>
+                <div>
+                  <Typography variant="h3" gutterBottom={true}>{t_i18n('Source')}</Typography>
+                  <pre><ItemCopy content={error.rawError.source ?? '-'} variant={'wrap'} /></pre>
+                </div>
+              </div>
+            </Paper>
           </DialogContentText>
         </DialogContent>
         <DialogActions>
--- opencti-platform/opencti-front/src/private/components/data/connectors/parseWorkErrors.ts
+++ opencti-platform/opencti-front/src/private/components/data/connectors/parseWorkErrors.ts
@@ -1,17 +1,96 @@
+import { fetchQuery, graphql } from 'react-relay';
+import { parseWorkErrorsQuery$data } from '@components/data/connectors/__generated__/parseWorkErrorsQuery.graphql';
+import JSON5 from 'json5';
 import { WorkMessages } from './ConnectorWorks';
 import { getMainRepresentative } from '../../../../utils/defaultRepresentatives';
+import { environment } from '../../../../relay/environment';
+
+const parseWorkErrorsQuery = graphql`
+  query parseWorkErrorsQuery($ids: [Any!]!) {
+    stixObjectOrStixRelationships(
+      filters: {
+        mode: or
+        filterGroups: []
+        filters: [
+          {
+            key: "standard_id"
+            values: $ids
+            mode: or
+          }
+        ]
+      }
+    ) {
+      edges {
+        node {
+          ... on StixCoreObject {
+            id
+            standard_id
+            entity_type
+            representative {
+              main
+            }
+          }
+          ... on StixRelationship {
+            id
+            standard_id
+            entity_type
+            representative {
+              main
+            }
+            from {
+              ... on StixCoreObject {
+                id
+                standard_id
+                entity_type
+                representative {
+                  main
+                }
+              }
+              ... on StixRelationship {
+                id
+                standard_id
+                entity_type
+                representative {
+                  main
+                }
+              }
+            }
+            to {
+              ... on StixCoreObject {
+                id
+                standard_id
+                entity_type
+                representative {
+                  main
+                }
+              }
+              ... on StixRelationship {
+                id
+                standard_id
+                entity_type
+                representative {
+                  main
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export type ResolvedEntity = NonNullable<NonNullable<NonNullable<parseWorkErrorsQuery$data['stixObjectOrStixRelationships']>['edges']>[number]>['node'];
+
+type ErrorLevel = 'Critical' | 'Warning' | 'Unclassified';
 
 export interface FullParsedWorkMessage {
   isParsed: true,
-  level: 'Critical' | 'Warning' | 'Unclassified',
+  level: ErrorLevel,
   parsedError: {
     category: string,
     message: string,
-    entity: {
-      id: string,
-      name: string,
-      type: string,
-    }
+    entity: ResolvedEntity,
   }
   rawError: NonNullable<WorkMessages>[number],
 }
@@ -38,33 +117,50 @@ const warningErrorTypes = [
   'MISSING_REFERENCE_ERROR',
 ];
 
-// Create custom error object from error because errors are in JSON
-const parseWorkErrors = (errorsList: WorkMessages): ParsedWorkMessage[] => {
-  // sort error by critical level
-  const getLevel = (type: string) => {
+// Create custom error object from stringified error
+const parseWorkErrors = async (errorsList: WorkMessages): Promise<ParsedWorkMessage[]> => {
+  const ids: string[] = [];
+
+  const getLevel = (type: string): ErrorLevel => {
     if (criticalErrorTypes.includes(type)) return 'Critical';
     if (warningErrorTypes.includes(type)) return 'Warning';
     return 'Unclassified';
   };
-  return (errorsList ?? []).flatMap((error) => {
+
+  const parsedList: ParsedWorkMessage[] = (errorsList ?? []).flatMap((error) => {
     if (!error) return [];
     // Try/Catch to prevent JSON.parse Exception
     try {
-      const source = JSON.parse(error.source ?? '');
-      const message = JSON.parse((error.message ?? '').replace(/'/g, '"'));
-      const entityId = source.name || source.id;
+      const parsedSource = JSON5.parse(error.source ?? '');
+      const source = parsedSource.type === 'bundle' ? parsedSource.objects[0] : parsedSource;
+      const message = JSON5.parse((error.message ?? ''));
+      const entityId = source.id;
+      const fromId = source.source_ref;
+      const toId = source.target_ref;
+
+      ids.push(entityId);
+      if (fromId) ids.push(fromId);
+      if (toId) ids.push(toId);
+
+      const parsedError = {
+        category: message.name,
+        message: message.error_message,
+        entity: {
+          standard_id: entityId,
+          representative: { main: getMainRepresentative(source, entityId) },
+          from: fromId ? {
+            standard_id: fromId,
+          } : undefined,
+          to: toId ? {
+            standard_id: toId,
+          } : undefined,
+        },
+      };
+
       return {
         isParsed: true,
         level: getLevel(message.name ?? ''),
-        parsedError: {
-          category: message.name,
-          message: message.error_message,
-          entity: {
-            id: entityId,
-            name: getMainRepresentative(source, entityId),
-            type: source.type,
-          },
-        },
+        parsedError,
         rawError: error,
       };
     } catch (_) {
@@ -75,6 +171,36 @@ const parseWorkErrors = (errorsList: WorkMessages): ParsedWorkMessage[] => {
       };
     }
   });
+
+  const entities = await fetchQuery(
+    environment,
+    parseWorkErrorsQuery,
+    { ids },
+  )
+    .toPromise()
+    .then((data) => {
+      return ((data as parseWorkErrorsQuery$data)?.stixObjectOrStixRelationships?.edges ?? []).map((n) => n?.node);
+    });
+
+  parsedList.map((error) => {
+    if (error.isParsed) {
+      const err = error;
+      entities.forEach((entity) => {
+        if (!entity) return;
+        if (entity.standard_id === error.parsedError.entity.standard_id) {
+          err.parsedError.entity = entity;
+        } else if (entity.standard_id === error.parsedError.entity.from?.standard_id) {
+          err.parsedError.entity = { ...error.parsedError.entity, from: entity };
+        } else if (entity.standard_id === error.parsedError.entity.to?.standard_id) {
+          err.parsedError.entity = { ...error.parsedError.entity, to: entity };
+        }
+      });
+      return err;
+    }
+    return error;
+  });
+
+  return parsedList;
 };
 
 export default parseWorkErrors;
