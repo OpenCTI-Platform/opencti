b95bfed303821347154acc13dca919481f1b934d
Adrien Servel <50013774+Kedae@users.noreply.github.com>
2024-10-30 10:02:03 +0100

    [frontend] Fix enrichment button (#8825)

--- opencti-platform/opencti-front/src/private/components/common/stix_core_objects/StixCoreObjectEnrichment.jsx
+++ opencti-platform/opencti-front/src/private/components/common/stix_core_objects/StixCoreObjectEnrichment.jsx
@@ -3,7 +3,6 @@ import * as R from 'ramda';
 import { CloudRefreshOutline } from 'mdi-material-ui';
 import Tooltip from '@mui/material/Tooltip';
 import ToggleButton from '@mui/material/ToggleButton';
-import useHelper from '../../../../utils/hooks/useHelper';
 import Drawer from '../drawer/Drawer';
 import { QueryRenderer } from '../../../../relay/environment';
 import inject18n from '../../../../components/i18n';
@@ -12,9 +11,6 @@ import StixCoreObjectEnrichmentLines, { stixCoreObjectEnrichmentLinesQuery } fro
 const StixCoreObjectEnrichment = (props) => {
   const { t, stixCoreObjectId, handleClose, open } = props;
   const [openDrawer, setOpenDrawer] = useState(false);
-  const [search, setSearch] = useState('');
-  const { isFeatureEnable } = useHelper();
-  const isFABReplaced = isFeatureEnable('FAB_REPLACEMENT');
 
   const handleOpenEnrichment = () => {
     if (props.closeMenu) {
@@ -25,12 +21,11 @@ const StixCoreObjectEnrichment = (props) => {
 
   const handleCloseEnrichment = () => {
     setOpenDrawer(false);
-    setSearch('');
   };
 
   return (
     <>
-      {isFABReplaced && <Tooltip title={t('Enrichment')}>
+      <Tooltip title={t('Enrichment')}>
         <ToggleButton
           onClick={handleOpenEnrichment}
           value="enrich"
@@ -39,50 +34,12 @@ const StixCoreObjectEnrichment = (props) => {
         >
           <CloudRefreshOutline fontSize="small" color="primary" />
         </ToggleButton>
-      </Tooltip>}
-      {isFABReplaced && <Drawer
-        open={openDrawer}
-        onClose={() => { setOpenDrawer(false); setSearch(''); }}
-        title={t('Enrichment connectors')}
-                        >
-        <QueryRenderer
-          query={stixCoreObjectEnrichmentLinesQuery}
-          variables={{ id: stixCoreObjectId }}
-          render={({ props: queryProps }) => {
-            if (
-              queryProps
-              && queryProps.stixCoreObject
-              && queryProps.connectorsForImport
-            ) {
-              return (
-                <StixCoreObjectEnrichmentLines
-                  stixCoreObject={queryProps.stixCoreObject}
-                  connectorsForImport={queryProps.connectorsForImport}
-                  search={search}
-                />
-              );
-            }
-            return <div />;
-          }}
-        />
-      </Drawer>}
-      {!isFABReplaced && !handleClose
-        && <Tooltip title={t('Enrichment')}>
-          <ToggleButton
-            onClick={handleOpenEnrichment}
-            value="enrich"
-            size="small"
-            style={{ marginRight: 3 }}
-          >
-            <CloudRefreshOutline fontSize="small" color="primary" />
-          </ToggleButton>
-        </Tooltip>
-      }
-      {!isFABReplaced && <Drawer
-        open={open || handleOpenEnrichment}
+      </Tooltip>
+      <Drawer
+        open={open || openDrawer}
         onClose={handleClose || handleCloseEnrichment}
         title={t('Enrichment connectors')}
-                         >
+      >
         <QueryRenderer
           query={stixCoreObjectEnrichmentLinesQuery}
           variables={{ id: stixCoreObjectId }}
@@ -102,7 +59,7 @@ const StixCoreObjectEnrichment = (props) => {
             return <div />;
           }}
         />
-      </Drawer>}
+      </Drawer>
     </>
   );
 };
