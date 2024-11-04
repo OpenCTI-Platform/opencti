e11ff58ce387761f2d9c8f7f4ef789efa2eb9c00
Céline Sèbe <87119259+CelineSebe@users.noreply.github.com>
2024-11-04 14:12:48 +0100

    [frontend] Remove "update" or "delete" button on attack pattern within an incident kill chain view(#8330)

--- opencti-platform/opencti-front/src/private/components/common/stix_domain_objects/StixDomainObjectAttackPatternsKillChainLines.tsx
+++ opencti-platform/opencti-front/src/private/components/common/stix_domain_objects/StixDomainObjectAttackPatternsKillChainLines.tsx
@@ -18,7 +18,6 @@ import {
 import { StixDomainObjectAttackPatternsKillChainQuery$variables } from '@components/common/stix_domain_objects/__generated__/StixDomainObjectAttackPatternsKillChainQuery.graphql';
 import { Theme } from '@mui/material/styles/createTheme';
 import { ListItemButton } from '@mui/material';
-import StixCoreRelationshipPopover from '../stix_core_relationships/StixCoreRelationshipPopover';
 import ItemMarkings from '../../../../components/ItemMarkings';
 import MarkdownDisplay from '../../../../components/MarkdownDisplay';
 import { useFormatter } from '../../../../components/i18n';
@@ -45,8 +44,6 @@ interface StixDomainObjectAttackPatternsKillChainLinesProps {
 
 const StixDomainObjectAttackPatternsKillChainLines: FunctionComponent<StixDomainObjectAttackPatternsKillChainLinesProps> = ({
   data,
-  paginationOptions,
-  onDelete,
   searchTerm,
   coursesOfAction,
 }) => {
@@ -191,7 +188,7 @@ const StixDomainObjectAttackPatternsKillChainLines: FunctionComponent<StixDomain
                             />
                             <div className={classes.nested} >
                               <ListItemSecondaryAction>
-                                {coursesOfAction ? (
+                                {coursesOfAction && (
                                   <IconButton
                                     onClick={() => handleToggleLine(attackPattern.id)}
                                     aria-haspopup="true"
@@ -203,12 +200,6 @@ const StixDomainObjectAttackPatternsKillChainLines: FunctionComponent<StixDomain
                                       <ExpandLess />
                                     )}
                                   </IconButton>
-                                ) : (
-                                  <StixCoreRelationshipPopover
-                                    stixCoreRelationshipId={attackPattern.id}
-                                    paginationOptions={paginationOptions}
-                                    onDelete={onDelete}
-                                  />
                                 )}
                               </ListItemSecondaryAction>
                             </div>
