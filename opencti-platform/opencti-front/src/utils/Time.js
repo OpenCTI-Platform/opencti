bd44f359a4352ecbee1406f7aa34fdee823f90f2
Landry Trebon <landry.trebon@filigran.io>
2024-11-06 10:16:29 +0100

    remove useless function

--- opencti-platform/opencti-front/src/utils/Time.js
+++ opencti-platform/opencti-front/src/utils/Time.js
@@ -8,10 +8,6 @@ export const ONE_SECOND = 1000;
 export const FIVE_SECONDS = 5000;
 export const TEN_SECONDS = FIVE_SECONDS * 2;
 
-export const isDate = (value) => {
-  return !Number.isNaN(Date.parse(value));
-};
-
 export const buildDate = (date) => {
   if (isNone(date)) {
     return null;
