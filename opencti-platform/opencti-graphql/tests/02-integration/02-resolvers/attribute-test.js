f689344782e29fc994524584128fc874d729e36f
Samuel Hassine <samuel.hassine@filigran.io>
2024-11-07 15:31:17 +0100

    [backend/frontend] Implement opinions statistics and dashboard widget ordering customization (#7277)

--- opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/attribute-test.js
+++ opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/attribute-test.js
@@ -98,7 +98,7 @@ describe('Attribute resolver standard behavior', () => {
       variables: { elementType: ENTITY_TYPE_IDENTITY_INDIVIDUAL }
     });
     attributes = queryResult.data.schemaAttributeNames.edges.map((edgeNode) => edgeNode.node);
-    expect(attributes.length).toEqual(31);
+    expect(attributes.length).toEqual(32);
     expect(attributes.map((node) => node.value).includes('lang')).toBeTruthy(); // Inherit attribute
     expect(attributes.map((node) => node.value).includes('contact_information')).toBeTruthy(); // Inherit attribute
     expect(attributes.map((node) => node.value).includes('x_opencti_firstname')).toBeTruthy(); // Direct attribute
@@ -109,7 +109,7 @@ describe('Attribute resolver standard behavior', () => {
       variables: { elementType: ENTITY_TYPE_LOCATION_POSITION }
     });
     attributes = queryResult.data.schemaAttributeNames.edges.map((edgeNode) => edgeNode.node);
-    expect(attributes.length).toEqual(31);
+    expect(attributes.length).toEqual(32);
     expect(attributes.map((node) => node.value).includes('lang')).toBeTruthy(); // Inherit attribute
     expect(attributes.map((node) => node.value).includes('precision')).toBeTruthy(); // Inherit attribute
     expect(attributes.map((node) => node.value).includes('postal_code')).toBeTruthy(); // Direct attribute
