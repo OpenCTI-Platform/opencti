840c3b2dd71062e993fb584d6242847e8a322b2c
Adrien Servel <50013774+Kedae@users.noreply.github.com>
2024-11-07 11:14:40 +0100

    [frontend] Add types filtering for related entities in cases (#8911)

--- opencti-platform/opencti-front/src/private/components/cases/case_rfts/CaseRft.tsx
+++ opencti-platform/opencti-front/src/private/components/cases/case_rfts/CaseRft.tsx
@@ -185,6 +185,17 @@ const CaseRft: React.FC<CaseRftProps> = ({ caseRftData, enableReferences }) => {
                     <ContainerStixObjectsOrStixRelationships
                       isSupportParticipation={false}
                       container={caseRft}
+                      types={[
+                        'Threat-Actor',
+                        'Intrusion-Set',
+                        'Campaign',
+                        'Malware',
+                        'Tool',
+                        'Attack-Pattern',
+                        'Identity',
+                        'Location',
+                        'Vulnerability',
+                      ]}
                       enableReferences={enableReferences}
                     />
                   </Grid>
