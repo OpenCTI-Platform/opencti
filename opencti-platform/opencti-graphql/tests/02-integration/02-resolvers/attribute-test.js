96a28b365c038957c5d276e54e03935b9f43b5d9
Samuel Hassine <samuel.hassine@filigran.io>
2024-11-07 14:06:37 +0100

    [backend/frontend] Implement opinions statistics and dashboard widget ordering customization (#7277)

--- opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/attribute-test.js
+++ opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/attribute-test.js
@@ -76,7 +76,7 @@ describe('Attribute resolver standard behavior', () => {
     // Stix Cyber Observable Object
     queryResult = await queryAsAdmin({ query: SCHEMA_ATTRIBUTES_QUERY, variables: { elementType: ENTITY_EMAIL_ADDR } });
     attributes = queryResult.data.schemaAttributeNames.edges.map((edgeNode) => edgeNode.node);
-    expect(attributes.length).toEqual(18);
+    expect(attributes.length).toEqual(19);
     expect(attributes.map((node) => node.value).includes('x_opencti_description')).toBeTruthy(); // Inherit attribute
     expect(attributes.map((node) => node.value).includes('display_name')).toBeTruthy(); // Direct attribute
 
