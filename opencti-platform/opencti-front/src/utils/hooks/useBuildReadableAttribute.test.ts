6ffef781fb0324eb7c2b5291a22742561f5f7eeb
Cathia Archidoit <cathia.archidoit@filigran.io>
2024-11-07 16:25:47 +0100

    [frontend] test for useBuildReadableAttribute (#3402)

---
+++ opencti-platform/opencti-front/src/utils/hooks/useBuildReadableAttribute.test.ts
@@ -0,0 +1,42 @@
+import { afterAll, beforeAll, describe, it, vi, expect } from 'vitest';
+import * as filterUtils from '../filters/filtersUtils';
+import { testRenderHook } from '../tests/test-render';
+import useBuildReadableAttribute from './useBuildReadableAttribute';
+import { FilterDefinition } from './useAuth';
+
+describe('Hook: useBuildReadableAttribute', () => {
+  beforeAll(() => {
+    vi.spyOn(filterUtils, 'useBuildFilterKeysMapFromEntityType').mockImplementation(() => new Map([
+      ['entity_type', { type: 'string' } as FilterDefinition],
+      ['published', { type: 'date' } as FilterDefinition],
+      ['revoked', { type: 'boolean' } as FilterDefinition],
+    ]));
+  });
+  afterAll(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('should display readable attribute', () => {
+    const { hook } = testRenderHook(() => useBuildReadableAttribute());
+    const { buildReadableAttribute } = hook.result.current;
+
+    const stringAttribute = buildReadableAttribute('Report', { attribute: 'entity_type' });
+    expect(stringAttribute).toEqual('Report');
+    const dateAttribute = buildReadableAttribute('2024-11-07T14:42:41.000Z', { attribute: 'published' });
+    expect(dateAttribute).toEqual('2024-11-07');
+    const listAttribute = buildReadableAttribute(['label1', 'label2'], { attribute: 'objectLabel.value' });
+    expect(listAttribute).toEqual('label1, label2');
+    const listAttribute2 = buildReadableAttribute(['label1', 'label2'], { attribute: 'objectLabel.value', displayStyle: 'text' });
+    expect(listAttribute2).toEqual('label1, label2');
+    const emptyListAttribute = buildReadableAttribute([], { attribute: 'objectLabel.value', displayStyle: 'text' });
+    expect(emptyListAttribute).toEqual('');
+    const listAttributeWithChips = buildReadableAttribute(['label1', 'label2'], { attribute: 'objectLabel.value', displayStyle: 'list' });
+    expect(listAttributeWithChips).toEqual('<ul><li>label1</li><li>label2</li></ul>');
+    const emptyListAttributeWithChips = buildReadableAttribute([], { attribute: 'objectLabel.value', displayStyle: 'chip' });
+    expect(emptyListAttributeWithChips).toEqual('');
+    const nullAttribute = buildReadableAttribute(null, { attribute: 'objectLabel.value' });
+    expect(nullAttribute).toEqual('null');
+    const booleanAttribute = buildReadableAttribute(true, { attribute: 'revoked' });
+    expect(booleanAttribute).toEqual('true');
+  });
+});
