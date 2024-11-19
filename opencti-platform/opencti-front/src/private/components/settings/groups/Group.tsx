ef11d2ac2a70d8984e29448e0aecde67f8bc2bed
Cathia Archidoit <cathia.archidoit@filigran.io>
2024-11-19 08:46:43 +0100

    [frontend] Fix UI in Group overview (#7944)

--- opencti-platform/opencti-front/src/private/components/settings/groups/Group.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/groups/Group.tsx
@@ -392,7 +392,7 @@ const Group = ({ groupData }: { groupData: Group_group$key }) => {
                             divider={true}
                             button={false}
                           >
-                            <Typography variant="h3" gutterBottom={true} width={150}>
+                            <Typography variant="h3" gutterBottom={true} width={100}>
                               {truncate(type, 40)}
                             </Typography>
                             {isMarkingAllowed
