ab37bc0540c8ee6b96b9fc4eaadb1edcba2e1c38
Cathia Archidoit <cathia.archidoit@filigran.io>
2024-11-25 17:47:13 +0100

    [frontend] improvements

--- opencti-platform/opencti-front/src/utils/hooks/useBuildReadableAttribute.tsx
+++ opencti-platform/opencti-front/src/utils/hooks/useBuildReadableAttribute.tsx
@@ -5,7 +5,7 @@ import { useBuildFilterKeysMapFromEntityType } from '../filters/filtersUtils';
 import type { WidgetColumn } from '../widget/widget';
 import MarkdownDisplay from '../../components/MarkdownDisplay';
 
-const buildStringAttribute = (inputValue: unknown, attributeType?: string, inTab?: boolean) => {
+const buildStringAttribute = (inputValue: unknown, attributeType?: string, inTable?: boolean) => {
   let value: string | ReactElement = typeof inputValue === 'string' ? inputValue : JSON.stringify(inputValue);
   if (attributeType) {
     if (attributeType === 'date') value = dateFormat(new Date(value)) ?? '';
@@ -17,7 +17,7 @@ const buildStringAttribute = (inputValue: unknown, attributeType?: string, inTab
         disableWarningAtLinkClick
         emptyStringIfUndefined
                                />);
-      value = inTab ? valueInMarkdown : renderToString(valueInMarkdown);
+      value = inTable ? valueInMarkdown : renderToString(valueInMarkdown);
     }
   }
   return value;
@@ -29,9 +29,6 @@ const useBuildReadableAttribute = () => {
   const buildReadableAttribute = (attributeData: unknown, displayInfo: WidgetColumn, inTab = false) => {
     const { attribute, displayStyle } = displayInfo;
     const attributeType = attribute ? stixCoreObjectsAttributesMap.get(attribute)?.type : undefined;
-
-    const isElement = inTab && attributeType === 'text'; // whether the returned value readableAttribute is a React Element or not (if not, it's a string)
-    // readableAttribute is a React Element only for text (like description) contained in a tab
 
     let readableAttribute;
     if (Array.isArray(attributeData)) {
@@ -49,7 +46,7 @@ const useBuildReadableAttribute = () => {
     } else {
       readableAttribute = buildStringAttribute(attributeData, attributeType, inTab);
     }
-    return { readableAttribute, isElement };
+    return readableAttribute;
   };
 
   return { buildReadableAttribute };
