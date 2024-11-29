1872acf1ebe20d80bd53f070e9b190c2a6629c10
FrancoisGrunert <francois.grunert@filigran.io>
2024-11-29 08:58:57 +0100

    [frontend] edition form (#8941)

--- opencti-platform/opencti-front/src/private/components/settings/exclusion_lists/ExclusionListEdition.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/exclusion_lists/ExclusionListEdition.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from 'react';
 import { useFormatter } from '../../../../components/i18n';
 import { graphql, PreloadedQuery, useFragment, usePreloadedQuery } from 'react-relay';
 import TextField from '../../../../components/TextField';
-import { Field, Form, Formik, FormikConfig } from 'formik';
+import { Field, Form, Formik } from 'formik';
 import MarkdownField from '../../../../components/fields/MarkdownField';
 import useApiMutation from '../../../../utils/hooks/useApiMutation';
 import * as Yup from 'yup';
@@ -12,8 +12,6 @@ import AutocompleteField from '../../../../components/AutocompleteField';
 import { fieldSpacingContainerStyle } from '../../../../utils/field';
 import { Option } from '@components/common/form/ReferenceField';
 import { ExclusionListEntityTypes } from '@components/settings/exclusion_lists/__generated__/ExclusionListsCreationFileAddMutation.graphql';
-import Button from '@mui/material/Button';
-
 
 const exclusionListMutationFieldPatch = graphql`
   mutation ExclusionListEditionFieldPatchMutation($id: ID!, $input: [EditInput!]!) {
@@ -54,7 +52,7 @@ interface ExclusionListEditionComponentProps {
 interface ExclusionListEditionValues {
   name: string;
   description?: string | null;
-  exclusion_list_entity_types: ReadonlyArray<string>;
+  exclusion_list_entity_types: Option[];
 }
 
 const ExclusionListEdition: FunctionComponent<ExclusionListEditionComponentProps> = ({
@@ -62,25 +60,15 @@ const ExclusionListEdition: FunctionComponent<ExclusionListEditionComponentProps
   onClose,
 }) => {
   const { t_i18n } = useFormatter();
-
   const { exclusionList } = usePreloadedQuery<ExclusionListEditionQuery>(exclusionListEditionQuery, queryRef);
   const data = useFragment<ExclusionListEdition_edition$key>(exclusionListEditionFragment, exclusionList);
   const [commitFieldPatch] = useApiMutation(exclusionListMutationFieldPatch);
-  const onSubmit: FormikConfig<ExclusionListEditionValues>['onSubmit'] = (
-    values,
-    { setSubmitting, resetForm },
-  ) => {
-    const input = {
-      
-      name: values.name ?? '',
-      description: values.description,
-      exclusion_list_entity_types: values.exclusion_list_entity_types,
-    };
+  const onSubmit = (name: string, value: string[]) => {
+    // TODO : Use useFormEditor ?
     commitFieldPatch({
-      variables: { id: data?.id, input: input },
-      onCompleted: () => {
-        setSubmitting(false);
-        resetForm();
+      variables: {
+        id: data?.id,
+        input: [{ key: name, value: value }],
       },
     });
   };
@@ -88,9 +76,11 @@ const ExclusionListEdition: FunctionComponent<ExclusionListEditionComponentProps
   const initialValues: ExclusionListEditionValues = {
     name: data?.name ?? '',
     description: data?.description,
-    exclusion_list_entity_types: data?.exclusion_list_entity_types ?? [],
+    exclusion_list_entity_types: (data?.exclusion_list_entity_types ?? []).map((type) => ({
+      value: type,
+      label: type,
+    })),
   };
-
 
   const entityTypes: ExclusionListEntityTypes[] = ['IPV4_ADDR', 'IPV6_ADDR', 'DOMAIN_NAME', 'URL'];
   const entityTypesOptions = (entityTypes ?? []).map((type) => ({
@@ -98,69 +88,54 @@ const ExclusionListEdition: FunctionComponent<ExclusionListEditionComponentProps
     label: type,
   }));
 
-
   return (
-    <div>
-      <Formik
-        enableReinitialize={true}
-        initialValues={initialValues}
-        validationSchema={exclusionListValidation(t_i18n)}
-        onSubmit={onSubmit}
-        onClose={onClose}
-      >
-        {({ submitForm, isSubmitting }) => (
-          <Form>
-            <Field
-              component={TextField}
-              name="name"
-              label={t_i18n('Name')}
-              fullWidth={true}
-            />
-            <Field
-              component={MarkdownField}
-              name="description"
-              label={t_i18n('Description')}
-              fullWidth={true}
-              multiline={true}
-              rows={2}
-              style={{ marginTop: 20 }}
-            />
-            <Field
-              component={AutocompleteField}
-              name="exclusion_list_entity_types"
-              fullWidth={true}
-              multiple
-              style={fieldSpacingContainerStyle}
-              options={entityTypesOptions}
-              renderOption={(
-                props: React.HTMLAttributes<HTMLLIElement>,
-                option: Option,
-              ) => <li {...props}>{option.label}</li>}
-              textfieldprops={{ label: t_i18n('Entity Types') }}
-            />
-            <div style={{ marginTop: 20, textAlign: 'right' }}>
-              <Button
-                variant="contained"
-                onClick={onClose}
-                disabled={isSubmitting}
-                style={{ marginLeft: 16 }}
-              >
-                {t_i18n('Cancel')}
-              </Button>
-              <Button
-                variant="contained"
-                color="secondary"
-                onClick={submitForm}
-                disabled={isSubmitting}
-                style={{ marginLeft: 16 }}
-              >
-                {t_i18n('Update')}
-              </Button>
-            </div>
-          </Form>
-        )}
-      </Formik>
-    </div>
+    <Formik
+      enableReinitialize={true}
+      initialValues={initialValues}
+      validationSchema={exclusionListValidation(t_i18n)}
+      onSubmit={() => {
+      }}
+      onClose={onClose}
+    >
+      <Form>
+        <Field
+          component={TextField}
+          name="name"
+          label={t_i18n('Name')}
+          fullWidth={true}
+          onSubmit={onSubmit}
+        />
+        <Field
+          component={MarkdownField}
+          name="description"
+          label={t_i18n('Description')}
+          fullWidth={true}
+          multiline={true}
+          rows={2}
+          style={{ marginTop: 20 }}
+          onSubmit={onSubmit}
+        />
+        <Field
+          component={AutocompleteField}
+          name="exclusion_list_entity_types"
+          fullWidth={true}
+          multiple
+          style={fieldSpacingContainerStyle}
+          options={entityTypesOptions}
+          renderOption={(
+            props: React.HTMLAttributes<HTMLLIElement>,
+            option: Option,
+          ) => <li key={option.value} {...props}>{option.label}</li>}
+          textfieldprops={{ label: t_i18n('Entity Types') }}
+          onChange={(name: string, value: { value: string; label: string }[]) => {
+            onSubmit(
+              name,
+              value.map((n) => n.label),
+            );
+          }}
+        />
+      </Form>
+    </Formik>
   );
 };
 
