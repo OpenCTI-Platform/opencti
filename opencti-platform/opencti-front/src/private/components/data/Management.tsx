3673f079a789b47d6a9c8d3c7c95b2f1c0ba0b3c
Celine Sebe <celine.sebe@filigran.io>
2024-12-04 15:36:55 +0100

    [frontend] FF rooting right menu

--- opencti-platform/opencti-front/src/private/components/data/Management.tsx
+++ opencti-platform/opencti-front/src/private/components/data/Management.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import ManagementMenu from '@components/data/ManagementMenu';
 import Breadcrumbs from '../../../components/Breadcrumbs';
 import { useFormatter } from '../../../components/i18n';
+import useHelper from '../../../utils/hooks/useHelper';
 
 const Management = () => {
   const { t_i18n } = useFormatter();
+  const { isFeatureEnable } = useHelper();
+  const isNewManagementScreensEnables = isFeatureEnable('NEW_MANAGEMENT_SCREENS');
 
   return (
     <div data-testid='data-management-page'>
       <Breadcrumbs elements={[{ label: t_i18n('Data') }, { label: t_i18n('Management') }, { label: t_i18n('Restricted entities'), current: true }]}/>
+      {isNewManagementScreensEnables && (
       <ManagementMenu />
+      )}
     </div>
   );
 };
