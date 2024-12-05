0513f37628858f27858b1def9bc72c5558442177
Angelique <angelique.jard@filigran.io>
2024-12-05 18:15:28 +0100

    Fix standard_id organization in tests

--- opencti-platform/opencti-graphql/tests/01-unit/utils/access-test.ts
+++ opencti-platform/opencti-graphql/tests/01-unit/utils/access-test.ts
@@ -1,4 +1,5 @@
 import { describe, it, expect } from 'vitest';
+import { v4 as uuid } from 'uuid';
 import { isMarkingAllowed, isOrganizationAllowed } from '../../../src/utils/access';
 import type { BasicStoreCommon } from '../../../src/types/store';
 import { MARKING_TLP_AMBER, MARKING_TLP_CLEAR, MARKING_TLP_GREEN, MARKING_TLP_RED } from '../../../src/schema/identifier';
@@ -53,9 +54,12 @@ describe('Check markings test coverage', () => {
 describe('Check organization access for element.', () => {
   it('should element when no platform organization setup be allowed', async () => {
     const element: Partial<BasicStoreCommon> = {
+      internal_id: uuid()
     };
 
     const user: Partial<AuthUser> = {
+      allowed_organizations: [],
+      internal_id: uuid()
     };
 
     const settings: Partial<BasicStoreSettings> = {
@@ -66,10 +70,20 @@ describe('Check organization access for element.', () => {
   });
 
   it('should element not shared be allowed to user in platform organization', async () => {
-    const element: Partial<BasicStoreCommon> = {};
+    const element: Partial<BasicStoreCommon> = {
+      internal_id: uuid()
+    };
+
+    const org : Partial<BasicStoreEntityOrganization> = {
+      internal_id: PLATFORM_ORGANIZATION.id,
+    };
+    const allOrgs: BasicStoreEntityOrganization[] = [];
+    allOrgs.push(org as BasicStoreEntityOrganization);
 
     const user: Partial<AuthUser> = {
       inside_platform_organization: true,
+      allowed_organizations: allOrgs,
+      internal_id: uuid()
     };
 
     const settings: Partial<BasicStoreSettings> = {
@@ -79,11 +93,21 @@ describe('Check organization access for element.', () => {
     expect(isOrganizationAllowed(element as BasicStoreCommon, user as AuthUser, settings as BasicStoreSettings)).toBeTruthy();
   });
 
-  it('should element not shared be allowed to user in platform organization', async () => {
-    const element: Partial<BasicStoreCommon> = {};
+  it('should element not shared not be allowed to user in another organization', async () => {
+    const element: Partial<BasicStoreCommon> = {
+      internal_id: uuid()
+    };
+
+    const org : Partial<BasicStoreEntityOrganization> = {
+      internal_id: TEST_ORGANIZATION.id,
+    };
+    const allOrgs: BasicStoreEntityOrganization[] = [];
+    allOrgs.push(org as BasicStoreEntityOrganization);
 
     const user: Partial<AuthUser> = {
       inside_platform_organization: false,
+      allowed_organizations: allOrgs,
+      internal_id: uuid()
     };
 
     const settings: Partial<BasicStoreSettings> = {
@@ -94,21 +118,28 @@ describe('Check organization access for element.', () => {
   });
 
   it('should element shared to user organization be allowed', async () => {
-    const element: Partial<BasicStoreCommon> = {};
-    element[RELATION_GRANTED_TO] = [TEST_ORGANIZATION.name];
+    const element: Partial<BasicStoreCommon> = {
+      internal_id: uuid()
+    };
+    element[RELATION_GRANTED_TO] = [TEST_ORGANIZATION.id];
 
     const org : Partial<BasicStoreEntityOrganization> = {
       internal_id: TEST_ORGANIZATION.id,
+      id: TEST_ORGANIZATION.id,
     };
+    const allOrgs: BasicStoreEntityOrganization[] = [];
+    allOrgs.push(org as BasicStoreEntityOrganization);
+
     const user: Partial<AuthUser> = {
       inside_platform_organization: false,
-      allowed_organizations: [org as BasicStoreEntityOrganization]
+      allowed_organizations: allOrgs,
+      internal_id: uuid()
     };
 
     const settings: Partial<BasicStoreSettings> = {
       platform_organization: PLATFORM_ORGANIZATION.name,
     };
 
-    expect(isOrganizationAllowed(element as BasicStoreCommon, user as AuthUser, settings as BasicStoreSettings)).toBeFalsy();
+    expect(isOrganizationAllowed(element as BasicStoreCommon, user as AuthUser, settings as BasicStoreSettings)).toBeTruthy();
   });
 });
--- opencti-platform/opencti-graphql/tests/02-integration/01-database/middleware-organization-test.ts
+++ opencti-platform/opencti-graphql/tests/02-integration/01-database/middleware-organization-test.ts
@@ -2,8 +2,8 @@ import { describe, expect, it } from 'vitest';
 import { now } from 'moment';
 import { GraphQLError } from 'graphql/index';
 import { enableCEAndUnSetOrganization, enableEEAndSetOrganization } from '../../utils/testQueryHelper';
-import { ADMIN_USER, GREEN_DISINFORMATION_ANALYST_GROUP, PLATFORM_ORGANIZATION, testContext } from '../../utils/testQuery';
-import { addOrganization } from '../../../src/modules/organization/organization-domain';
+import { ADMIN_USER, GREEN_DISINFORMATION_ANALYST_GROUP, PLATFORM_ORGANIZATION, TEST_ORGANIZATION, testContext } from '../../utils/testQuery';
+import { addOrganization, findAll as findAllOrganization } from '../../../src/modules/organization/organization-domain';
 import type { InternalRelationshipAddInput, OrganizationAddInput, ThreatActorIndividualAddInput } from '../../../src/generated/graphql';
 import { addUser, assignOrganizationToUser, findById as findUserById, userAddRelation, userDelete } from '../../../src/domain/user';
 import { type BasicStoreEntityOrganization } from '../../../src/modules/organization/organization-types';
@@ -14,11 +14,14 @@ import { resetCacheForEntity } from '../../../src/database/cache';
 import { ENTITY_TYPE_ENTITY_SETTING } from '../../../src/modules/entitySetting/entitySetting-types';
 import { MARKING_TLP_RED } from '../../../src/schema/identifier';
 import { isFeatureEnabled, ORGA_SHARING_REQUEST_FF } from '../../../src/config/conf';
+import type { StoreEntityConnection } from '../../../src/types/store';
 
 describe('Middleware test coverage on organization sharing verification', () => {
   let externalOrg: BasicStoreEntityOrganization;
   let userInPlatformOrg: AuthUser;
   let userInExternalOrg: AuthUser;
+  let testOrganizationEntity: BasicStoreEntityOrganization | undefined;
+  let platformOrganizationEntity: BasicStoreEntityOrganization | undefined;
 
   describe('Trying to create an existing entity that is not shared to user should raise a dedicated exception.', () => {
     it('INIT - Should set platform organization and create one user in organization, one in another organization', async () => {
@@ -26,8 +29,14 @@ describe('Middleware test coverage on organization sharing verification', () => 
       const org: OrganizationAddInput = {
         name: 'ITWomen'
       };
-      externalOrg = await addOrganization(testContext, ADMIN_USER, org);
 
+      const allOrgs: StoreEntityConnection<BasicStoreEntityOrganization> = await findAllOrganization(testContext, ADMIN_USER, {});
+      platformOrganizationEntity = allOrgs.edges.find((currentOrg) => currentOrg.node.name === PLATFORM_ORGANIZATION.name)?.node;
+      testOrganizationEntity = allOrgs.edges.find((currentOrg) => currentOrg.node.name === TEST_ORGANIZATION.name)?.node;
+      expect(platformOrganizationEntity?.id).toBeDefined();
+      expect(testOrganizationEntity?.id).toBeDefined();
+
+      externalOrg = await addOrganization(testContext, ADMIN_USER, org);
       resetCacheForEntity(ENTITY_TYPE_ENTITY_SETTING);
 
       const userInExternalOrgInput = {
@@ -38,16 +47,17 @@ describe('Middleware test coverage on organization sharing verification', () => 
         lastname: 'Hopper'
       };
       const userInExternalOrgEntity = await addUser(testContext, ADMIN_USER, userInExternalOrgInput);
-      await assignOrganizationToUser(testContext, ADMIN_USER, userInExternalOrgEntity.internal_id, externalOrg.id);
+      await assignOrganizationToUser(testContext, ADMIN_USER, userInExternalOrgEntity.id, externalOrg.id);
 
       // Marking: TLP:GREEN
       const userToGroupInput: InternalRelationshipAddInput = {
         relationship_type: 'member-of',
         toId: GREEN_DISINFORMATION_ANALYST_GROUP.id
       };
-      await userAddRelation(testContext, ADMIN_USER, userInExternalOrgEntity.internal_id, userToGroupInput);
+      await userAddRelation(testContext, ADMIN_USER, userInExternalOrgEntity.id, userToGroupInput);
       userInExternalOrg = await findUserById(testContext, ADMIN_USER, userInExternalOrgEntity.id);
       expect(userInExternalOrg.inside_platform_organization).toBeFalsy();
+      expect(userInExternalOrg.effective_confidence_level?.max_confidence).toBe(100);
 
       const userInPlatformOrgInput = {
         password: 'changeme',
@@ -57,8 +67,9 @@ describe('Middleware test coverage on organization sharing verification', () => 
         lastname: 'Turing'
       };
       const userInPlatformOrgEntity = await addUser(testContext, ADMIN_USER, userInPlatformOrgInput);
-      await assignOrganizationToUser(testContext, ADMIN_USER, userInPlatformOrgEntity.internal_id, PLATFORM_ORGANIZATION.id);
-      await userAddRelation(testContext, ADMIN_USER, userInPlatformOrgEntity.internal_id, userToGroupInput);
+
+      await assignOrganizationToUser(testContext, ADMIN_USER, userInPlatformOrgEntity.id, platformOrganizationEntity?.id);
+      await userAddRelation(testContext, ADMIN_USER, userInPlatformOrgEntity.id, userToGroupInput);
       userInPlatformOrg = await findUserById(testContext, ADMIN_USER, userInPlatformOrgEntity.id);
       expect(userInPlatformOrg.inside_platform_organization).toBeTruthy();
     });
@@ -70,7 +81,7 @@ describe('Middleware test coverage on organization sharing verification', () => 
         description: 'Created by user in org platform'
       };
       const threatActor = await addThreatActorIndividual(testContext, userInPlatformOrg, inputOne);
-      await addOrganizationRestriction(testContext, ADMIN_USER, threatActor.id, PLATFORM_ORGANIZATION.id);
+      await addOrganizationRestriction(testContext, ADMIN_USER, threatActor.id, testOrganizationEntity?.id);
 
       const inputNext: ThreatActorIndividualAddInput = {
         name: threatActorIndividualName,
