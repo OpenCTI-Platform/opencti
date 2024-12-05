f760ff24248fef8af1987ac4003b7146356128a7
FlorianDelemarre <florian.delemarre@filigran.io>
2024-12-05 18:33:24 +0100

    [frontend] refactor with DataTable component (#3227)

--- opencti-platform/opencti-front/src/private/components/common/containers/RelatedContainers.tsx
+++ opencti-platform/opencti-front/src/private/components/common/containers/RelatedContainers.tsx
@@ -1,21 +1,16 @@
 import React, { useState } from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Button from '@mui/material/Button';
-import { ExpandLessOutlined, ExpandMoreOutlined, OpenInNewOutlined } from '@mui/icons-material';
-import { Link, useNavigate } from 'react-router-dom';
+import { OpenInNewOutlined } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { useTheme } from '@mui/styles';
 import { graphql, useFragment } from 'react-relay';
 import { resolveLink } from '../../../../utils/Entity';
-import ItemIcon from '../../../../components/ItemIcon';
-import ItemMarkings from '../../../../components/ItemMarkings';
 import { useFormatter } from '../../../../components/i18n';
-import type { Theme } from '../../../../components/Theme';
-import { RelatedContainersFragment_container_connection$key } from './__generated__/RelatedContainersFragment_container_connection.graphql';
+import {
+  RelatedContainersFragment_container_connection$key,
+  RelatedContainersFragment_container_connection$data,
+} from './__generated__/RelatedContainersFragment_container_connection.graphql';
+import DataTableWithoutFragment from '../../../../components/dataGrid/DataTableWithoutFragment';
 
 export const RelatedContainersFragment = graphql`
   fragment RelatedContainersFragment_container_connection on ContainerConnection {
@@ -59,8 +54,13 @@ export const RelatedContainersFragment = graphql`
         }
       }
     }
+    pageInfo {
+      globalCount
+    }
   }
 `;
+
+type RelatedContainerNode = NonNullable<NonNullable<RelatedContainersFragment_container_connection$data['edges']>[number]>['node'];
 
 interface RelatedContainersProps {
   relatedContainers: RelatedContainersFragment_container_connection$key | null | undefined;
@@ -73,27 +73,21 @@ const RelatedContainers: React.FC<RelatedContainersProps> = ({
   containerId,
   entityType,
 }) => {
-  const { t_i18n, fsd } = useFormatter();
-  const theme = useTheme<Theme>();
+  const { t_i18n } = useFormatter();
   const navigate = useNavigate();
-  const [expanded, setExpanded] = useState(false);
   const relatedContainers = useFragment(
     RelatedContainersFragment,
     relatedContainersKey,
   );
+  const [ref, setRef] = useState<HTMLDivElement | undefined>();
 
-  const containersEdges = relatedContainers?.edges ?? [];
-  const expandable = containersEdges.length > 5;
-  const containers = containersEdges
-    .filter((edge) => edge?.node.id !== containerId)
-    .slice(0, expanded ? 200 : 5);
+  const containers = (relatedContainers?.edges ?? []).filter((edge) => edge?.node.id !== containerId).map((edge) => edge?.node);
+  const containersGlobalCount = relatedContainers?.pageInfo?.globalCount ?? 0;
 
   return (
-    <div>
-      <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
-        <Typography variant="h3" gutterBottom={true}>
-          {t_i18n('Correlated containers')}
-        </Typography>
+    <div style={{ marginTop: 20, height: 400 }} ref={(r) => setRef(r ?? undefined)}>
+      <Typography variant="h3" gutterBottom={true} style={{ }}>
+        {t_i18n('Correlated containers')}
         <IconButton
           color="primary"
           aria-label="Go to correlation graph view"
@@ -103,108 +97,24 @@ const RelatedContainers: React.FC<RelatedContainersProps> = ({
         >
           <OpenInNewOutlined fontSize="small"/>
         </IconButton>
-      </div>
-      <List sx={{ marginBottom: 1 }}>
-        {containers.length > 0 ? (
-          containers.map((edge) => {
-            const relatedContainer = edge?.node;
-            return (
-              <ListItem
-                key={relatedContainer?.id}
-                button
-                divider
-                component={Link}
-                to={`${resolveLink(relatedContainer?.entity_type)}/${relatedContainer?.id}`}
-                sx={{
-                  '&': {
-                    height: 50,
-                    minHeight: 50,
-                    maxHeight: 50,
-                    paddingRight: 0,
-                  },
-                }}
-              >
-                <ListItemIcon>
-                  <ItemIcon type={relatedContainer?.entity_type}/>
-                </ListItemIcon>
-                <ListItemText primary={
-                  <div style={{
-                    whiteSpace: 'nowrap',
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                    paddingRight: 10,
-                  }}
-                  >
-                    {relatedContainer?.name}
-                  </div>
-                 }
-                />
-                <div style={{
-                  width: 100,
-                  minWidth: 100,
-                  maxWidth: 100,
-                  marginRight: 24,
-                  marginLeft: 24,
-                  whiteSpace: 'nowrap',
-                  overflow: 'hidden',
-                  textOverflow: 'ellipsis',
-                }}
-                >{relatedContainer?.createdBy?.name ?? '-'}</div>
-                <div style={{
-                  width: 100,
-                  minWidth: 100,
-                  maxWidth: 100,
-                  marginRight: 24,
-                  whiteSpace: 'nowrap',
-                  overflow: 'hidden',
-                  textOverflow: 'ellipsis',
-                }}
-                >{fsd(relatedContainer?.modified)}</div>
-                <div style={{
-                  width: 110,
-                  paddingRight: 20,
-                }}
-                >
-                  <ItemMarkings
-                    variant="inList"
-                    markingDefinitions={relatedContainer?.objectMarking ?? []}
-                    limit={1}
-                  />
-                </div>
-              </ListItem>
-            );
-          })
-        )
-          : '-'}
-      </List>
-      {expandable && (
-        <Button
-          onClick={() => setExpanded(!expanded)}
-          sx={{
-            position: 'absolute',
-            left: 0,
-            bottom: 0,
-            width: '100%',
-            height: 25,
-            color: theme.palette.primary.main,
-            backgroundColor:
-              theme.palette.mode === 'dark'
-                ? 'rgba(255, 255, 255, .1)'
-                : 'rgba(0, 0, 0, .1)',
-            borderTopLeftRadius: 0,
-            borderTopRightRadius: 0,
-            '&:hover': {
-              backgroundColor:
-                theme.palette.mode === 'dark'
-                  ? 'rgba(255, 255, 255, .2)'
-                  : 'rgba(0, 0, 0, .2)',
-            },
-          }}
-          variant="contained"
-        >
-          {expanded ? <ExpandLessOutlined/> : <ExpandMoreOutlined/>}
-        </Button>
-      )}
+      </Typography>
+      <div className="clearfix"/>
+      <DataTableWithoutFragment
+        dataColumns={{
+          entity_type: { percentWidth: 15 },
+          name: { percentWidth: 40 },
+          createdBy: { percentWidth: 15 },
+          modified: { percentWidth: 15 },
+          objectMarking: { percentWidth: 15 },
+        }}
+        data={containers}
+        globalCount={containersGlobalCount}
+        rootRef={ref}
+        disableNavigation={true}
+        storageKey={`related-containers-${entityType}-${containerId}`}
+        hideHeaders={true}
+        onLineClick={(row: RelatedContainerNode) => navigate(`${resolveLink(row?.entity_type)}/${row?.id}`)}
+      />
     </div>
   );
 };
