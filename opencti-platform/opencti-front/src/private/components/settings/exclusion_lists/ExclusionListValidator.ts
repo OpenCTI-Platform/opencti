b77ba96d717d96d3b505616985fd844d47993966
FrancoisGrunert <francois.grunert@filigran.io>
2024-12-05 09:32:51 +0100

    [frontend] validator + logo (#8941)

--- opencti-platform/opencti-front/src/private/components/settings/exclusion_lists/ExclusionListValidator.ts
+++ opencti-platform/opencti-front/src/private/components/settings/exclusion_lists/ExclusionListValidator.ts
@@ -1,11 +1,21 @@
 import * as Yup from 'yup';
 
-const exclusionListValidator = (t: (value: string) => string) => Yup.object().shape({
-  name: Yup.string().trim().min(2).required(t('This field is required')),
-  description: Yup.string().nullable(),
-  exclusion_list_entity_types: Yup.array().min(1, t('Minimum one entity type')).required(t('This field is required')),
-  // file: Yup.mixed().required(t('This field is required')),
-  // content: Yup.string().required(t('This field is required')),
-});
+const exclusionListValidator = (t: (value: string) => string, isCreatedWithFile: boolean) => {
+  if (isCreatedWithFile) {
+    return Yup.object().shape({
+      name: Yup.string().trim().min(2).required(t('This field is required')),
+      description: Yup.string().nullable(),
+      exclusion_list_entity_types: Yup.array().min(1, t('Minimum one entity type')).required(t('This field is required')),
+      file: Yup.mixed().required(t('This field is required')),
+    });
+  } else {
+    return Yup.object().shape({
+      name: Yup.string().trim().min(2).required(t('This field is required')),
+      description: Yup.string().nullable(),
+      exclusion_list_entity_types: Yup.array().min(1, t('Minimum one entity type')).required(t('This field is required')),
+      content: Yup.string().required(t('This field is required')),
+    });
+  }
+};
 
 export default exclusionListValidator;
