b88e9b4cd7c7c0c227ad7e80f2b2bfe678ba0680
FrancoisGrunert <francois.grunert@filigran.io>
2024-12-09 12:19:11 +0100

    [frontend] fix lint (#8941)

--- opencti-platform/opencti-front/src/private/components/settings/exclusion_lists/ExclusionListValidator.ts
+++ opencti-platform/opencti-front/src/private/components/settings/exclusion_lists/ExclusionListValidator.ts
@@ -8,14 +8,13 @@ const exclusionListValidator = (t: (value: string) => string, isCreatedWithFile:
       exclusion_list_entity_types: Yup.array().min(1, t('Minimum one entity type')).required(t('This field is required')),
       file: Yup.mixed().required(t('This field is required')),
     });
-  } else {
-    return Yup.object().shape({
-      name: Yup.string().trim().min(2).required(t('This field is required')),
-      description: Yup.string().nullable(),
-      exclusion_list_entity_types: Yup.array().min(1, t('Minimum one entity type')).required(t('This field is required')),
-      content: Yup.string().required(t('This field is required')),
-    });
   }
+  return Yup.object().shape({
+    name: Yup.string().trim().min(2).required(t('This field is required')),
+    description: Yup.string().nullable(),
+    exclusion_list_entity_types: Yup.array().min(1, t('Minimum one entity type')).required(t('This field is required')),
+    content: Yup.string().required(t('This field is required')),
+  });
 };
 
 export default exclusionListValidator;
