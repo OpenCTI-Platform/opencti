684140637a7671565dc216d79eee4b0eb44f3cc4
Stephanya Casanova <stephanya.casanova@filigran.io>
2024-12-09 12:13:31 +0100

    [frontend/backend] wip

--- opencti-platform/opencti-front/src/private/components/common/stix_core_objects/StixCoreObjectSimulationResult.jsx
+++ opencti-platform/opencti-front/src/private/components/common/stix_core_objects/StixCoreObjectSimulationResult.jsx
@@ -597,15 +597,15 @@ const StixCoreObjectSimulationResult = ({ id, type }) => {
       <>
         <Alert icon={<CheckOutlined fontSize="inherit" />} severity="success">
           {t_i18n('The scenario has been correctly generated in your OpenBAS platform.')}
-          {result.attackPatternsWithoutInjectorContracts.length > 0 && (
+          {result.attackPatternWithoutInjectorContracts.length > 0 && (
             <>
               {t_i18n(' The next attack patterns don\'t have any contract linked: ')}
-              {result.attackPatternsWithoutInjectorContracts.join(', ')}
+              {result.attackPatternWithoutInjectorContracts.join(', ')}
             </>
           )}
         </Alert>
         <Box textAlign='center' style={{ marginTop: 20 }}>
-          <Button component={Link} to={result.url_response} target="_blank" variant="outlined" endIcon={<OpenInNewOutlined />}>
+          <Button component={Link} to={result.urlResponse} target="_blank" variant="outlined" endIcon={<OpenInNewOutlined />}>
             {t_i18n('Access to the scenario')}
           </Button>
         </Box>
