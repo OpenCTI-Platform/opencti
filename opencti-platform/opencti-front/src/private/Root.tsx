699bc837daa8a14b11fbcdd88a55733189c00359
Bonsai8863 <131906254+Bonsai8863@users.noreply.github.com>
2024-12-11 11:34:21 -0500

    [frontend/backend] Custom theme logo used

--- opencti-platform/opencti-front/src/private/Root.tsx
+++ opencti-platform/opencti-front/src/private/Root.tsx
@@ -220,6 +220,22 @@ const rootPrivateQuery = graphql`
     settings {
       ...RootSettings
     }
+    themes {
+      edges {
+        node {
+          name
+          theme_background
+          theme_paper
+          theme_nav
+          theme_primary
+          theme_secondary
+          theme_accent
+          theme_logo
+          theme_logo_collapsed
+          theme_logo_login
+        }
+      }
+    }
     about {
       version
     }
@@ -329,6 +345,7 @@ const RootComponent: FunctionComponent<RootComponentProps> = ({ queryRef }) => {
   const {
     me: meFragment,
     settings: settingsFragment,
+    themes,
     entitySettings,
     schemaSCOs,
     schemaSDOs,
@@ -380,7 +397,7 @@ const RootComponent: FunctionComponent<RootComponentProps> = ({ queryRef }) => {
       }}
     >
       <StyledEngineProvider injectFirst={true}>
-        <ConnectedThemeProvider settings={settings}>
+        <ConnectedThemeProvider settings={settings} themes={themes}>
           <ConnectedIntlProvider settings={settings}>
             <AnalyticsProvider instance={Analytics(platformAnalyticsConfiguration)}>
               <Index settings={settings} />
