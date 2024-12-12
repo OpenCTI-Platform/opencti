aa9414c463a7b5bc522c8fe38a2e4ad1c3963c8e
Landry Trebon <landry.trebon@filigran.io>
2024-12-12 18:12:28 +0100

    wip page fintel template

--- opencti-platform/opencti-front/src/private/components/settings/sub_types/Root.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/sub_types/Root.tsx
@@ -1,35 +1,28 @@
 import React, { Suspense } from 'react';
 import { Route, Routes, useParams } from 'react-router-dom';
-import { useLazyLoadQuery } from 'react-relay';
 import EEGuard from '@components/common/entreprise_edition/EEGuard';
-import { SubTypeQuery } from '@components/settings/sub_types/__generated__/SubTypeQuery.graphql';
 import Loader from '../../../../components/Loader';
 import ErrorNotFound from '../../../../components/ErrorNotFound';
-import SubType, { subTypeQuery } from './SubType';
+import SubType from './SubType';
 import FintelTemplate from './fintel_templates/FintelTemplate';
-import useQueryLoading from '../../../../utils/hooks/useQueryLoading';
 
 const RootSubType = () => {
   const { subTypeId } = useParams<{ subTypeId?: string }>();
   if (!subTypeId) return <ErrorNotFound/>;
 
-  const subTypeRef = useQueryLoading<SubTypeQuery>(subTypeQuery, { id: subTypeId });
-
   return (
     <Suspense fallback={<Loader />}>
-      {subTypeRef && (
-        <Routes>
-          <Route path="/" element={<SubType queryRef={subTypeRef} />} />
-          <Route
-            path="/templates/:templateId"
-            element={(
-              <EEGuard redirect={`/dashboard/settings/customization/entity_types/${subTypeId}`}>
-                <FintelTemplate />
-              </EEGuard>
-            )}
-          />
-        </Routes>
-      )}
+      <Routes>
+        <Route path="/" element={<SubType />} />
+        <Route
+          path="/templates/:templateId"
+          element={(
+            <EEGuard redirect={`/dashboard/settings/customization/entity_types/${subTypeId}`}>
+              <FintelTemplate />
+            </EEGuard>
+          )}
+        />
+      </Routes>
     </Suspense>
   );
 };
