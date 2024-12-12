6f71b1c936e955b6667455cc8f6c8f50500f417d
marie flores <marie.flores@filigran.io>
2024-12-12 08:56:42 +0100

    [backend] fix counters

--- opencti-platform/opencti-graphql/tests/03-streams/00-Raw/raw-test.js
+++ opencti-platform/opencti-graphql/tests/03-streams/00-Raw/raw-test.js
@@ -46,7 +46,7 @@ describe('Raw streams tests', () => {
       expect(createEventsByTypes.tool.length).toBe(2);
       expect(createEventsByTypes.vocabulary.length).toBe(342); // 328 created at init + 2 created in tests + 5 vocabulary organizations types + 7 persona
       expect(createEventsByTypes.vulnerability.length).toBe(7);
-      expect(createEvents.length).toBe(802);
+      expect(createEvents.length).toBe(803);
       for (let createIndex = 0; createIndex < createEvents.length; createIndex += 1) {
         const { data: insideData, origin, type } = createEvents[createIndex];
         expect(origin).toBeDefined();
