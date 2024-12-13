2666d190588bdf3d52b9c7f172eb43110e144268
FlorianDelemarre <florian.delemarre@filigran.io>
2024-12-13 14:50:48 +0100

    [frontend] fix some css (#3227)

--- opencti-platform/opencti-front/src/private/components/common/containers/related_containers/RelatedContainers.tsx
+++ opencti-platform/opencti-front/src/private/components/common/containers/related_containers/RelatedContainers.tsx
@@ -150,22 +150,33 @@ const RelatedContainers: React.FC<RelatedContainersProps> = ({
         </IconButton>
       </Typography>
       <div className="clearfix"/>
-      <DataTableWithoutFragment
-        dataColumns={{
-          entity_type: { percentWidth: 15 },
-          name: { percentWidth: 40 },
-          createdBy: { percentWidth: 15 },
-          modified: { percentWidth: 15 },
-          objectMarking: { percentWidth: 15 },
-        }}
-        data={containers}
-        globalCount={containersGlobalCount}
-        rootRef={ref}
-        disableNavigation={true}
-        storageKey={`related-containers-${entityType}-${containerId}`}
-        hideHeaders={true}
-        onLineClick={(row: RelatedContainerNode) => handleOpenDetails(row)}
-      />
+      {containersGlobalCount > 0
+        ? <DataTableWithoutFragment
+            dataColumns={{
+              entity_type: { percentWidth: 15 },
+              name: { percentWidth: 40 },
+              createdBy: { percentWidth: 15 },
+              modified: { percentWidth: 15 },
+              objectMarking: { percentWidth: 15 },
+            }}
+            data={containers}
+            globalCount={containersGlobalCount}
+            rootRef={ref}
+            disableNavigation={true}
+            storageKey={`related-containers-${entityType}-${containerId}`}
+            hideHeaders={true}
+            onLineClick={(row: RelatedContainerNode) => handleOpenDetails(row)}
+          /> : <div style={{ display: 'table', height: '100%', width: '100%' }}>
+            <span
+              style={{
+                display: 'table-cell',
+                verticalAlign: 'middle',
+                textAlign: 'center',
+              }}
+            >
+              {t_i18n('No correlated containers has been found.')}
+            </span>
+          </div>}
       <Drawer
         title={selectedContainer?.name ?? '-'}
         open={!!selectedContainer}
