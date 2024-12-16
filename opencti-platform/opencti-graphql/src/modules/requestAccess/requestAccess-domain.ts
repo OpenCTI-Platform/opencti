1e65b9dd213a180c8e33b9bff18643ca4eb5adf6
BocognanoSarah <sarah.bocognano@filigran.io>
2024-12-16 17:03:33 +0100

    [backend] Request Access API + addRequestAccess mutation

--- opencti-platform/opencti-graphql/src/modules/requestAccess/requestAccess-domain.ts
+++ opencti-platform/opencti-graphql/src/modules/requestAccess/requestAccess-domain.ts
@@ -1,12 +1,19 @@
 import type { AuthContext, AuthUser } from '../../types/user';
-import type { RequestAccessAddInput } from '../../generated/graphql';
+import type { CaseRfiAddInput, RequestAccessAddInput } from '../../generated/graphql';
+import { addCaseRfi } from '../case/case-rfi/case-rfi-domain';
 
-export const addRequestAccess = (context: AuthContext, user: AuthUser, input: RequestAccessAddInput) => {
-  // TODO: create an RFI with the request access
-  return input.name;
+export const addRequestAccess = async (context: AuthContext, user: AuthUser, input: RequestAccessAddInput) => {
+  // TODO: find assignee
+  const rfiInput: CaseRfiAddInput = {
+    name: `Request Access for entity ${input.request_access_entities.join(', ')}`,
+    objectParticipant: [user.id],
+    objects: input.request_access_entities,
+  };
+  const requestForInformation = await addCaseRfi(context, user, rfiInput);
+  return requestForInformation.id;
 };
 
 export const validateRequestAccess = (context: AuthContext, user: AuthUser, id: string) => {
   // TODO: return the validation of request
-  return id;
+  return true;
 };
