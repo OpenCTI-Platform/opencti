b3a9e8b7629cb7b36841677046f46ccfae9e6aec
Landry Trebon <landry.trebon@filigran.io>
2024-12-17 18:17:28 +0100

    wip new save

---
+++ opencti-platform/opencti-front/src/private/components/settings/sub_types/fintel_templates/FintelTemplateTabs.tsx
@@ -0,0 +1,85 @@
+import { Box, Tab, Tabs, Button, Tooltip } from '@mui/material';
+import React, { Dispatch, ReactNode, SetStateAction, useState } from 'react';
+import { graphql, useFragment } from 'react-relay';
+import { useTheme } from '@mui/styles';
+import { Save } from '@mui/icons-material';
+import useFintelTemplateEdit from './useFintelTemplateEdit';
+import { useFormatter } from '../../../../../components/i18n';
+import { FintelTemplateTabs_template$key } from './__generated__/FintelTemplateTabs_template.graphql';
+import type { Theme } from '../../../../../components/Theme';
+
+const tabsFragment = graphql`
+  fragment FintelTemplateTabs_template on FintelTemplate {
+    id
+    content
+  }
+`;
+
+interface ChildrenProps {
+  index: number
+  setEditorValue: Dispatch<SetStateAction<string>>
+}
+
+interface FintelTemplateTabsProps {
+  data: FintelTemplateTabs_template$key
+  children: (props: ChildrenProps) => ReactNode
+}
+
+const FintelTemplateTabs = ({ children, data }: FintelTemplateTabsProps) => {
+  const theme = useTheme<Theme>();
+  const { t_i18n } = useFormatter();
+  const [index, setIndex] = useState(0);
+
+  const { content, id } = useFragment(tabsFragment, data);
+  const [editorValue, setEditorValue] = useState(content);
+
+  const commitEditMutation = useFintelTemplateEdit();
+
+  const onSubmit = () => {
+    const input = { key: 'content', value: [editorValue] };
+    commitEditMutation({ id, input: [input] });
+  };
+
+  return (
+    <>
+      <Box sx={{
+        borderBottom: 1,
+        borderColor: 'divider',
+        marginBottom: 3,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}
+      >
+        <Tabs value={index} onChange={(_, i) => setIndex(i)}>
+          <Tab label={t_i18n('Content Editor')} />
+          <Tab label={t_i18n('Content Preview')} />
+        </Tabs>
+
+        <div style={{ display: 'flex', alignItems: 'center', gap: theme.spacing(1) }}>
+          {editorValue !== content && (
+            <span style={{ color: theme.palette.dangerZone.main }}>
+              {t_i18n('You have unsaved changes')}
+            </span>
+          )}
+          <Tooltip title={t_i18n('Save changes')}>
+            <div>
+              <Button
+                variant="outlined"
+                className="icon-outlined"
+                onClick={onSubmit}
+                disabled={editorValue === content}
+              >
+                <Save fontSize="small" />
+              </Button>
+            </div>
+          </Tooltip>
+        </div>
+      </Box>
+
+      {children({ index, setEditorValue })}
+    </>
+  );
+};
+
+export default FintelTemplateTabs;
