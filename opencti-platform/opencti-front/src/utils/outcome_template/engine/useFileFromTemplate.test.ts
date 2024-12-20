4110860ced7e18d2f1e3925f04f603742c1e475a
Landry Trebon <landry.trebon@filigran.io>
2024-12-20 17:31:56 +0100

    fix tests

--- opencti-platform/opencti-front/src/utils/outcome_template/engine/useFileFromTemplate.test.ts
+++ opencti-platform/opencti-front/src/utils/outcome_template/engine/useFileFromTemplate.test.ts
@@ -44,19 +44,17 @@ describe('Hook: useFileFromTemplate', () => {
       return MockPayloadGenerator.generate(op, {
         FintelTemplate() {
           return {
-            fintelTemplate: {
-              id: 'testTemplate',
-              name: 'Test template',
-              fintel_template_widgets: [{
-                id: 'XXXX',
-                variable_name: 'myAttributes',
-                widget: {
-                  type: 'attribute',
-                  dataSelection: [{}],
-                },
-              }],
-              content: 'Hello, I am container $containerName of type $containerType',
-            },
+            id: 'testTemplate',
+            name: 'Test template',
+            fintel_template_widgets: [{
+              id: 'XXXX',
+              variable_name: 'myAttributes',
+              widget: {
+                type: 'attribute',
+                dataSelection: [{}],
+              },
+            }],
+            content: 'Hello, I am container $containerName of type $containerType',
           };
         },
       });
@@ -77,21 +75,19 @@ describe('Hook: useFileFromTemplate', () => {
       return MockPayloadGenerator.generate(op, {
         FintelTemplate() {
           return {
-            fintelTemplate: {
-              id: 'testTemplate',
-              name: 'Test template',
-              fintel_template_widgets: [{
-                id: 'YYY',
-                variable_name: 'containerList',
-                widget: {
-                  type: 'list',
-                  dataSelection: [{
-                    filters: null,
-                  }],
-                },
-              }],
-              content: 'Hello, I have: $containerList',
-            },
+            id: 'testTemplate',
+            name: 'Test template',
+            fintel_template_widgets: [{
+              id: 'YYY',
+              variable_name: 'containerList',
+              widget: {
+                type: 'list',
+                dataSelection: [{
+                  filters: null,
+                }],
+              },
+            }],
+            content: 'Hello, I have: $containerList',
           };
         },
       });
