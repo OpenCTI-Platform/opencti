b4d02dd41af1d456729fc048adffd42d6dedb9fd
BocognanoSarah <sarah.bocognano@filigran.io>
2024-12-23 16:42:28 +0100

    [frontend] Add create entities in draftEntities WIP

--- opencti-platform/opencti-front/src/private/components/drafts/DraftEntities.tsx
+++ opencti-platform/opencti-front/src/private/components/drafts/DraftEntities.tsx
@@ -1,8 +1,12 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { DraftEntitiesLinesPaginationQuery, DraftEntitiesLinesPaginationQuery$variables } from '@components/drafts/__generated__/DraftEntitiesLinesPaginationQuery.graphql';
 import { useParams } from 'react-router-dom';
 import { graphql } from 'react-relay';
 import { DraftEntitiesLines_data$data } from '@components/drafts/__generated__/DraftEntitiesLines_data.graphql';
+import StixDomainObjectCreation from '@components/common/stix_domain_objects/StixDomainObjectCreation';
+import StixCyberObservableCreation from '@components/observations/stix_cyber_observables/StixCyberObservableCreation';
+import { Add } from '@mui/icons-material';
+import Fab from '@mui/material/Fab';
 import useAuth from '../../../utils/hooks/useAuth';
 import { usePaginationLocalStorage } from '../../../utils/hooks/useLocalStorage';
 import useQueryLoading from '../../../utils/hooks/useQueryLoading';
@@ -10,6 +14,7 @@ import { useBuildEntityTypeBasedFilterContext, emptyFilterGroup } from '../../..
 import { UsePreloadedPaginationFragment } from '../../../utils/hooks/usePreloadedPaginationFragment';
 import DataTable from '../../../components/dataGrid/DataTable';
 import { DataTableProps } from '../../../components/dataGrid/dataTableTypes';
+import useHelper from '../../../utils/hooks/useHelper';
 
 const draftEntitiesLineFragment = graphql`
     fragment DraftEntities_node on StixCoreObject {
@@ -107,7 +112,21 @@ const DraftEntities : FunctionComponent<DraftEntitiesProps> = ({
   entitiesType = 'Stix-Core-Object',
 }) => {
   const { draftId } = useParams() as { draftId: string };
+  const { isFeatureEnable } = useHelper();
+  const isFABReplaced = isFeatureEnable('FAB_REPLACEMENT');
+  const [open, setOpen] = useState(false);
+  const [openCreateEntity, setOpenCreateEntity] = useState(false);
+  const [openCreateObservable, setOpenCreateObservable] = useState(false);
   const { platformModuleHelpers: { isRuntimeFieldEnable } } = useAuth();
+  const handleCloseCreateEntity = () => {
+    setOpenCreateEntity(false);
+    setOpen(false);
+  };
+  const handleCloseCreateObservable = () => {
+    setOpenCreateObservable(false);
+    setOpen(false);
+  };
+
   const initialValues = {
     filters: emptyFilterGroup,
     searchTerm: '',
@@ -125,6 +144,7 @@ const DraftEntities : FunctionComponent<DraftEntitiesProps> = ({
   } = usePaginationLocalStorage<DraftEntitiesLinesPaginationQuery$variables>(LOCAL_STORAGE_KEY, initialValues);
   const {
     filters,
+    searchTerm,
   } = viewStorage;
 
   const contextFilters = useBuildEntityTypeBasedFilterContext(entitiesType, filters);
@@ -197,6 +217,68 @@ const DraftEntities : FunctionComponent<DraftEntitiesProps> = ({
           exportContext={{ entity_type: 'Stix-Domain-Object' }}
           redirectionModeEnabled
           disableSelectAll // TODO: To handle selectAll
+          createButton={
+            isFABReplaced && (
+              entitiesType === 'Stix-Cyber-Observable' ? (
+                <>
+                  <Fab
+                    onClick={() => setOpenCreateObservable(true)}
+                    color="primary"
+                    aria-label="Add"
+                    style={{
+                      position: 'fixed',
+                      bottom: 30,
+                      right: 30,
+                      zIndex: 2000,
+                    }}
+                  >
+                    <Add/>
+                  </Fab>
+                  <StixCyberObservableCreation
+                    display={open}
+                    contextual={true}
+                    inputValue={searchTerm}
+                    paginationKey="Pagination_draftWorkspaceEntities"
+                    paginationOptions={queryPaginationOptions}
+                    speeddial={true}
+                    open={openCreateObservable}
+                    handleClose={handleCloseCreateObservable}
+                  />
+                </>
+              ) : (
+                <>
+                  <Fab
+                    onClick={() => setOpenCreateEntity(true)}
+                    color="primary"
+                    aria-label="Add"
+                    style={{
+                      position: 'fixed',
+                      bottom: 30,
+                      right: 30,
+                      zIndex: 2000,
+                    }}
+                  >
+                    <Add/>
+                  </Fab>
+                  <StixDomainObjectCreation
+                    display={open}
+                    inputValue={searchTerm}
+                    paginationKey="Pagination_stixCoreObjects"
+                    paginationOptions={queryPaginationOptions}
+                    speeddial={true}
+                    open={openCreateEntity}
+                    handleClose={handleCloseCreateEntity}
+                    onCompleted={() => setOpenCreateEntity(false)}
+                    creationCallback={undefined}
+                    confidence={undefined}
+                    defaultCreatedBy={undefined}
+                    isFromBulkRelation={undefined}
+                    defaultMarkingDefinitions={undefined}
+                    stixDomainObjectTypes={undefined}
+                  />
+                </>
+              ))
+          }
         />
       )}
     </span>
