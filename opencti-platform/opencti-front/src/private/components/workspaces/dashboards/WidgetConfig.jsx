ed18e558058e1e1eb4ed52fd1b2c7f616009707a
Laurent Bonnet <laurent.bonnet@filigran.io>
2024-12-26 11:34:21 +0100

    [frontend] fix styling and tab title

--- opencti-platform/opencti-front/src/private/components/workspaces/dashboards/WidgetConfig.jsx
+++ opencti-platform/opencti-front/src/private/components/workspaces/dashboards/WidgetConfig.jsx
@@ -1452,7 +1452,7 @@ const WidgetConfig = ({ workspace, widget, onComplete, closeMenu }) => {
               <Button
                 variant='outlined'
                 disableElevation
-                style={{ marginTop: '2.5px', marginLeft: '4px' }}
+                sx={{ marginLeft: 1 }}
                 onClick={() => inputRef.current?.click()}
               >
                 {t_i18n('Import Widget')}
@@ -1460,7 +1460,7 @@ const WidgetConfig = ({ workspace, widget, onComplete, closeMenu }) => {
               <Button
                 variant='contained'
                 disableElevation
-                style={{ marginTop: '2.5px', marginLeft: '2px' }}
+                sx={{ marginLeft: 1 }}
                 onClick={() => setOpen(true)}
               >
                 {t_i18n('Create Widget')}
