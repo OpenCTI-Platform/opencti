6f907be9e1cde982b54b113b03929508866bde85
Julien Richard <julien.richard@filigran.io>
2024-12-27 12:19:22 +0100

    [backend] Auto copy prebuilt binaries from datadog/pprof at application building

--- opencti-platform/opencti-graphql/builder/dev/dev.js
+++ opencti-platform/opencti-graphql/builder/dev/dev.js
@@ -2,11 +2,22 @@ const esbuild = require('esbuild');
 const {default: importGlobPlugin} = require('esbuild-plugin-import-glob');
 const {default: graphqlLoaderPlugin} = require('@luckycatfactory/esbuild-graphql-loader');
 const nativeNodePlugin = require("../plugin/native.node.plugin");
+const {copy} = require('esbuild-plugin-copy');
 
 esbuild.build({
     logLevel: 'info',
     define: {'process.env.NODE_ENV': '\"development\"'},
-    plugins: [importGlobPlugin(), graphqlLoaderPlugin(), nativeNodePlugin()],
+    plugins: [
+        importGlobPlugin(),
+        graphqlLoaderPlugin(),
+        nativeNodePlugin(),
+        copy({
+            assets: {
+                from: ['./node_modules/@datadog/pprof/prebuilds/**/*'],
+                to: ['./prebuilds'],
+            }
+        }),
+    ],
     entryPoints: [
         'src/back.js',
         'script/script-clean-relations.js',
