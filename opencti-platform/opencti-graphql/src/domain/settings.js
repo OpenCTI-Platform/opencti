28a4f78d99d35667970d7d1262e35d067bb10417
Cathia Archidoit <cathia.archidoit@filigran.io>
2024-12-30 11:24:33 +0100

    [frontend/backend] playground enabled in settings

--- opencti-platform/opencti-graphql/src/domain/settings.js
+++ opencti-platform/opencti-graphql/src/domain/settings.js
@@ -2,7 +2,16 @@ import { getHeapStatistics } from 'node:v8';
 import nconf from 'nconf';
 import * as R from 'ramda';
 import { createEntity, listAllThings, loadEntity, patchAttribute, updateAttribute } from '../database/middleware';
-import conf, { ACCOUNT_STATUSES, booleanConf, BUS_TOPICS, ENABLED_DEMO_MODE, ENABLED_FEATURE_FLAGS, getBaseUrl, PLATFORM_VERSION } from '../config/conf';
+import conf, {
+  ACCOUNT_STATUSES,
+  booleanConf,
+  BUS_TOPICS,
+  ENABLED_DEMO_MODE,
+  ENABLED_FEATURE_FLAGS,
+  getBaseUrl,
+  PLATFORM_VERSION,
+  PLAYGROUND_ENABLED
+} from '../config/conf';
 import { delEditContext, getClusterInstances, getRedisVersion, notify, setEditContext } from '../database/redis';
 import { isRuntimeSortEnable, searchEngineVersion } from '../database/engine';
 import { getRabbitMQVersion } from '../database/rabbitmq';
@@ -234,6 +243,10 @@ export const getMessagesFilteredByRecipients = (user, settings) => {
     return isEmptyField(recipients) || recipients.some((recipientId) => [user.id, ...user.groups.map(({ id }) => id), ...user.organizations.map(({ id }) => id)].includes(recipientId));
   });
 };
+
+export const isPlaygroundEnabled = () => {
+  return PLAYGROUND_ENABLED;
+}
 
 export const settingEditMessage = async (context, user, settingsId, message) => {
   const messageToStore = {
