8a66e8bd983bd5c73d8c787abcb7ad3f1dcc9c12
A. Jard <angelique.jard@filigran.io>
2025-01-03 08:40:18 +0100

    [CI] Add a test-result folder in backend and upload to JFrog to help debug stream count issues.

--- opencti-platform/opencti-graphql/tests/03-streams/00-Raw/raw-test.js
+++ opencti-platform/opencti-graphql/tests/03-streams/00-Raw/raw-test.js
@@ -5,6 +5,7 @@ import { FIVE_MINUTES, RAW_EVENTS_SIZE } from '../../utils/testQuery';
 import { checkStreamData, checkStreamGenericContent, fetchStreamEvents, } from '../../utils/testStream';
 import { PORT } from '../../../src/config/conf';
 import { EVENT_TYPE_CREATE, EVENT_TYPE_DELETE, EVENT_TYPE_MERGE, EVENT_TYPE_UPDATE } from '../../../src/database/utils';
+import { writeTestDataToFile } from '../../utils/testOutput';
 
 describe('Raw streams tests', () => {
   // We need to check the event format to be sure that everything is setup correctly
@@ -13,6 +14,7 @@ describe('Raw streams tests', () => {
     async () => {
       // Read all events from the beginning.
       const events = await fetchStreamEvents(`http://localhost:${PORT}/stream`, { from: '0' });
+      writeTestDataToFile(JSON.stringify(events), 'raw-test-all-event.json');
       // Check the number of events
       // 01 - CHECK CREATE EVENTS.
       const createEvents = events.filter((e) => e.type === EVENT_TYPE_CREATE);
