c9b36c2b88342751e8562bbb60765dba199afcdf
Angelique <angelique.jard@filigran.io>
2025-01-06 16:54:52 +0100

    Testing without new test

--- opencti-platform/opencti-graphql/tests/01-unit/angie-test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it } from 'vitest';
-import { writeTestDataToFile } from '../utils/testOutput';
-
-describe('Filter Boolean logic engine ', () => {
-  it('tests OR mode', () => {
-    writeTestDataToFile('Contenu du ficher', 'test.log');
-  });
-});
