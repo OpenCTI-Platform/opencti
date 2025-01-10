eb67bb38f99eeb629869695b38c12c5d44531621
Jeremy Cloarec <jeremy.cloarec@filigran.io>
2025-01-10 17:40:59 +0100

    [frontend] add confirmation dialog to draft approve

--- opencti-platform/opencti-front/src/private/components/drafts/DraftContextBanner.tsx
+++ opencti-platform/opencti-front/src/private/components/drafts/DraftContextBanner.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql } from 'react-relay';
 import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import DraftBlock from '@components/common/draft/DraftBlock';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogActions from '@mui/material/DialogActions';
+import Dialog from '@mui/material/Dialog';
 import { useFormatter } from '../../../components/i18n';
 import useApiMutation from '../../../utils/hooks/useApiMutation';
 import useAuth from '../../../utils/hooks/useAuth';
 import { truncate } from '../../../utils/String';
 import { MESSAGING$ } from '../../../relay/environment';
+import Transition from '../../../components/Transition';
 
 export const draftContextBannerMutation = graphql`
   mutation DraftContextBannerMutation(
@@ -37,6 +42,8 @@ const DraftContextBanner = () => {
   const { t_i18n } = useFormatter();
   const [commitExitDraft] = useApiMutation(draftContextBannerMutation);
   const [commitValidateDraft] = useApiMutation(draftContextBannerValidateDraftMutation);
+  const [displayApprove, setDisplayApprove] = useState(false);
+  const [approving, setApproving] = useState(false);
   const { me } = useAuth();
   const navigate = useNavigate();
   const currentDraftContextName = me.draftContext ? me.draftContext.name : '';
@@ -53,12 +60,14 @@ const DraftContextBanner = () => {
   };
 
   const handleValidateDraft = () => {
+    setApproving(true);
     if (me.draftContext) {
       commitValidateDraft({
         variables: {
           id: me.draftContext.id,
         },
         onCompleted: () => {
+          setApproving(false);
           MESSAGING$.notifySuccess('Draft validation in progress');
           navigate('/');
         },
@@ -77,10 +86,31 @@ const DraftContextBanner = () => {
             variant="contained"
             color="primary"
             style={{ width: '100%' }}
-            onClick={handleValidateDraft}
+            onClick={() => setDisplayApprove(true)}
           >
             {t_i18n('Approve draft')}
           </Button>
+          <Dialog
+            open={displayApprove}
+            PaperProps={{ elevation: 1 }}
+            keepMounted={true}
+            TransitionComponent={Transition}
+            onClose={() => setDisplayApprove(false)}
+          >
+            <DialogContent>
+              <DialogContentText>
+                {t_i18n('Do you want to approve this draft and send it to ingestion?')}
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={() => setDisplayApprove(false)} disabled={approving}>
+                {t_i18n('Cancel')}
+              </Button>
+              <Button color="secondary" onClick={handleValidateDraft} disabled={approving}>
+                {t_i18n('Approve')}
+              </Button>
+            </DialogActions>
+          </Dialog>
         </div>
         <div style={{ padding: '0 12px' }}>
           <Button
