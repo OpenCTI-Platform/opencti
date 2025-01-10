2dac69229ba4a79baf96a4d98df645826fd441af
Bonsai8863 <131906254+Bonsai8863@users.noreply.github.com>
2025-01-10 10:29:09 -0500

    [backend] Modified theme fetch function to create themes if none exist

--- opencti-platform/opencti-graphql/src/modules/theme/theme-domain.ts
+++ opencti-platform/opencti-graphql/src/modules/theme/theme-domain.ts
@@ -9,6 +9,32 @@ import type { AuthContext, AuthUser } from '../../types/user';
 import { SYSTEM_USER } from '../../utils/access';
 import { type BasicStoreEntityTheme } from './theme-types';
 
+const defaultLightTheme: ThemeAddInput = {
+  name: 'light',
+  theme_accent: '#eeeeee',
+  theme_background: '#f8f8f8',
+  theme_logo: '',
+  theme_logo_collapsed: '',
+  theme_logo_login: '',
+  theme_nav: '#ffffff',
+  theme_paper: '#ffffff',
+  theme_primary: '#001bda',
+  theme_secondary: '#0c7e69',
+};
+
+const defaultDarkTheme: ThemeAddInput = {
+  name: 'dark',
+  theme_accent: '#0f1e38',
+  theme_background: '#070d19',
+  theme_logo: '',
+  theme_logo_collapsed: '',
+  theme_logo_login: '',
+  theme_nav: '#070d19',
+  theme_paper: '#09101e',
+  theme_primary: '#0fbcff',
+  theme_secondary: '#00f1bd',
+};
+
 export const findById = (
   context: AuthContext,
   id: string,
@@ -17,16 +43,6 @@ export const findById = (
   SYSTEM_USER,
   id,
   ENTITY_TYPE_THEME,
-);
-
-export const findAll = async (
-  context: AuthContext,
-  opts: EntityOptions<BasicStoreEntityTheme>,
-) => listEntitiesPaginated<BasicStoreEntityTheme>(
-  context,
-  SYSTEM_USER,
-  [ENTITY_TYPE_THEME],
-  opts,
 );
 
 export const addTheme = async (
@@ -51,6 +67,29 @@ export const addTheme = async (
 
   return notify(BUS_TOPICS[ENTITY_TYPE_THEME].ADDED_TOPIC, created, user);
 };
+
+export const findAll = async (
+  context: AuthContext,
+  opts: EntityOptions<BasicStoreEntityTheme>,
+) => listEntitiesPaginated<BasicStoreEntityTheme>(
+  context,
+  SYSTEM_USER,
+  [ENTITY_TYPE_THEME],
+  opts,
+).then(async (storeEntityConnection) => {
+  if (storeEntityConnection.edges.length > 0) return storeEntityConnection;
+
+  // If there are no themes present, add the default light and dark themes.
+  await addTheme(context, SYSTEM_USER, defaultLightTheme);
+  await addTheme(context, SYSTEM_USER, defaultDarkTheme);
+
+  return listEntitiesPaginated<BasicStoreEntityTheme>(
+    context,
+    SYSTEM_USER,
+    [ENTITY_TYPE_THEME],
+    opts,
+  );
+});
 
 export const deleteTheme = async (
   context: AuthContext,
