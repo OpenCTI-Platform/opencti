a11b7238f7d719b4994fa67d3dd3097f46218b5d
Valentin Bouzin <valentin.bouzin@filigran.io>
2025-01-13 09:10:56 +0100

    fix types

--- opencti-platform/opencti-front/src/private/components/settings/exclusion_lists/ExclusionListUtils.ts
+++ opencti-platform/opencti-front/src/private/components/settings/exclusion_lists/ExclusionListUtils.ts
@@ -5,7 +5,8 @@ const ipAddrList = ['IPv4-Addr', 'IPv6-Addr'];
 const observableTypeList = ['Artifact', 'Domain-Name', 'Hostname', 'Url', 'StixFile', 'Email-Addr'];
 export const availableEntityTypes = [...observableTypeList, ...ipAddrList];
 
-const entityTypeListValidator = (entityTypeList: Option[]) => {
+const entityTypeListValidator = (entityTypeList?: Option[]) => {
+  if (!entityTypeList) return false;
   let containIpAddr = false;
   let containObservableType = false;
   for (let i = 0; i < entityTypeList.length; i += 1) {
@@ -26,7 +27,7 @@ export const exclusionListUpdateValidator = (t: (n: string) => string) => Yup.ob
   exclusion_list_entity_types: Yup.array().min(1, t('Minimum one entity type')).test(
     'entityTypeListValidator',
     'Incompatible types, can\'t mix IP types with other types',
-    (value: Option[]) => entityTypeListValidator(value),
+    (value?: Option[]) => entityTypeListValidator(value),
   ).required(t('This field is required')),
 });
 
@@ -37,7 +38,7 @@ export const exclusionListCreationValidator = (t: (value: string) => string, isC
     exclusion_list_entity_types: Yup.array().min(1, t('Minimum one entity type')).test(
       'entityTypeListValidator',
       'Incompatible types, can\'t mix IP types with other types',
-      (value: Option[]) => entityTypeListValidator(value),
+      (value?: Option[]) => entityTypeListValidator(value),
     ).required(t('This field is required')),
     file: isCreatedWithFile ? Yup.mixed().required(t('This field is required')) : Yup.mixed().nullable(),
     content: isCreatedWithFile ? Yup.string().nullable() : Yup.string().required(t('This field is required')),
