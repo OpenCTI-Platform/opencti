941f72073496883871ae3f9cbc3a584033826f4f
Bonsai8863 <131906254+Bonsai8863@users.noreply.github.com>
2025-01-15 16:20:26 -0500

    [frontend] Role edition FAB replacement

--- opencti-platform/opencti-front/src/private/components/settings/roles/RoleEdition.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/roles/RoleEdition.tsx
@@ -1,53 +1,125 @@
 import React, { FunctionComponent, useState } from 'react';
-import { createFragmentContainer, graphql } from 'react-relay';
+import { graphql, useFragment } from 'react-relay';
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Drawer, { DrawerVariant } from '@components/common/drawer/Drawer';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import RoleEditionOverview from './RoleEditionOverview';
 import RoleEditionCapabilities, { roleEditionCapabilitiesLinesSearch } from './RoleEditionCapabilities';
 import useQueryLoading from '../../../../utils/hooks/useQueryLoading';
 import Loader, { LoaderVariant } from '../../../../components/Loader';
 import { useFormatter } from '../../../../components/i18n';
 import { RoleEditionCapabilitiesLinesSearchQuery } from './__generated__/RoleEditionCapabilitiesLinesSearchQuery.graphql';
-import { RoleEdition_role$data } from './__generated__/RoleEdition_role.graphql';
+import { RoleEdition_role$key } from './__generated__/RoleEdition_role.graphql';
+import { RolePopoverEditionQuery$data } from './__generated__/RolePopoverEditionQuery.graphql';
+import useHelper from '../../../../utils/hooks/useHelper';
+import Transition from '../../../../components/Transition';
+import useApiMutation from '../../../../utils/hooks/useApiMutation';
 
-interface RoleEditionProps {
-  role: RoleEdition_role$data
+export const roleDeletionMutation = graphql`
+  mutation RoleEditionDeletionMutation($id: ID!) {
+    roleEdit(id: $id) {
+      delete
+    }
+  }
+`;
+
+const RoleEditionFragment = graphql`
+  fragment RoleEdition_role on Role {
+    id
+    ...RoleEditionOverview_role
+    ...RoleEditionCapabilities_role
+    editContext {
+      name
+      focusOn
+    }
+  }
+`;
+
+const UpdateRoleControlledDial = ({ onOpen }: { onOpen: () => void }) => {
+  const { t_i18n } = useFormatter();
+  return (
+    <Button
+      variant='outlined'
+      onClick={onOpen}
+      style={{ float: 'right' }}
+    >
+      {t_i18n('Update')}
+    </Button>
+  );
+};
+
+interface RoleEditionDrawerProps {
+  roleRef: RolePopoverEditionQuery$data['role']
   handleClose?: () => void
   open?: boolean
   disabled?: boolean
 }
 
-const RoleEdition: FunctionComponent<RoleEditionProps> = ({
+const RoleEditionDrawer: FunctionComponent<RoleEditionDrawerProps> = ({
   handleClose = () => {},
-  role,
+  roleRef,
   open,
   disabled = false,
 }) => {
   const { t_i18n } = useFormatter();
-  const [currentTab, setTab] = useState(0);
-  const { editContext } = role;
-
+  const navigate = useNavigate();
+  const [currentTab, setCurrentTab] = useState(0);
+  const [deleting, setDeleting] = useState(false);
+  const [displayDelete, setDisplayDelete] = useState(false);
+  const { isFeatureEnable } = useHelper();
+  const isFABReplaced = isFeatureEnable('FAB_REPLACEMENT');
   const queryRef = useQueryLoading<RoleEditionCapabilitiesLinesSearchQuery>(roleEditionCapabilitiesLinesSearch);
+  const role = useFragment<RoleEdition_role$key>(RoleEditionFragment, roleRef);
+  const deleteSuccessMessage = t_i18n('', {
+    id: '... successfully deleted',
+    values: { entity_type: t_i18n('Role') },
+  });
+  const [commit] = useApiMutation(
+    roleDeletionMutation,
+    undefined,
+    { successMessage: deleteSuccessMessage },
+  );
+
+  const handleOpenDelete = () => setDisplayDelete(true);
+  const handleCloseDelete = () => setDisplayDelete(false);
+  const submitDelete = (roleId: string) => {
+    setDeleting(true);
+    commit({
+      variables: { id: roleId },
+      onCompleted: () => {
+        setDeleting(false);
+        handleClose();
+        navigate('/dashboard/settings/accesses/roles');
+      },
+    });
+  };
 
   return (
     <Drawer
       title={t_i18n('Update a role')}
-      variant={open == null ? DrawerVariant.updateWithPanel : undefined}
+      variant={open == null && !isFABReplaced
+        ? DrawerVariant.updateWithPanel
+        : undefined}
       open={open}
       onClose={handleClose}
-      context={editContext}
+      context={role?.editContext}
       disabled={disabled}
+      controlledDial={isFABReplaced
+        ? UpdateRoleControlledDial
+        : undefined
+      }
     >
-      <>
+      {role ? (<>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={currentTab} onChange={(event, value) => setTab(value)}>
+          <Tabs value={currentTab} onChange={(_, value) => setCurrentTab(value)}>
             <Tab label={t_i18n('Overview')} />
             <Tab label={t_i18n('Capabilities')} />
           </Tabs>
         </Box>
-        {currentTab === 0 && <RoleEditionOverview role={role} context={editContext} />}
+        {currentTab === 0 && <RoleEditionOverview role={role} context={role.editContext} />}
         {currentTab === 1 && queryRef && (
           <React.Suspense
             fallback={<Loader variant={LoaderVariant.inline} />}
@@ -55,23 +127,71 @@ const RoleEdition: FunctionComponent<RoleEditionProps> = ({
             <RoleEditionCapabilities role={role} queryRef={queryRef} />
           </React.Suspense>
         )}
-      </>
+        <Button
+          onClick={handleOpenDelete}
+          variant='contained'
+          color='error'
+          disabled={deleting}
+          sx={{ marginTop: 2 }}
+        >
+          {t_i18n('Delete')}
+        </Button>
+        <Dialog
+          open={displayDelete}
+          PaperProps={{ elevation: 1 }}
+          keepMounted={true}
+          TransitionComponent={Transition}
+          onClose={handleCloseDelete}
+        >
+          <DialogContent>
+            <DialogContentText>
+              {t_i18n('Do you want to delete this role?')}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={handleCloseDelete}
+              disabled={deleting}
+            >
+              {t_i18n('Cancel')}
+            </Button>
+            <Button
+              color="secondary"
+              onClick={() => submitDelete(role.id)}
+              disabled={deleting}
+            >
+              {t_i18n('Delete')}
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </>)
+        : (<Loader />)}
     </Drawer>
   );
 };
 
-const RoleEditionFragment = createFragmentContainer(RoleEdition, {
-  role: graphql`
-    fragment RoleEdition_role on Role {
-      id
-      ...RoleEditionOverview_role
-      ...RoleEditionCapabilities_role
-      editContext {
-        name
-        focusOn
-      }
-    }
-  `,
-});
+interface RoleEditionProps {
+  roleEditionData?: RolePopoverEditionQuery$data
+  handleClose?: () => void
+  open?: boolean
+  disabled?: boolean
+}
 
-export default RoleEditionFragment;
+const RoleEdition: FunctionComponent<RoleEditionProps> = ({
+  roleEditionData,
+  handleClose = () => {},
+  open,
+  disabled = false,
+}) => {
+  if (!roleEditionData) return <Loader />;
+  return (
+    <RoleEditionDrawer
+      roleRef={roleEditionData.role}
+      handleClose={handleClose}
+      open={open}
+      disabled={disabled}
+    />
+  );
+};
+
+export default RoleEdition;
