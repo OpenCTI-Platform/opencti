e173d0a460ba15f74ce1eaa6a9091f64c7b3f87c
Celine Sebe <celine.sebe@filigran.io>
2025-01-20 11:56:04 +0100

    [frontend] reverse background summary accordion

--- opencti-platform/opencti-front/src/components/Accordion.jsx
+++ opencti-platform/opencti-front/src/components/Accordion.jsx
@@ -16,8 +16,7 @@ export const Accordion = styled((props) => (<MuiAccordion disableGutters elevati
 export const AccordionSummary = styled((props) => (
   <MuiAccordionSummary expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />} {...props}/>
 ))(({ theme }) => ({
-  // backgroundColor: theme.palette.mode === 'dark' ? 'rgba(255, 255, 255, .05)' : 'rgba(0, 0, 0, .03)',
-  backgroundColor: 'rgba(255, 255, 255, .05)',
+  backgroundColor: theme.palette.mode === 'dark' ? 'rgba(255, 255, 255, .05)' : 'rgba(0, 0, 0, .03)',
   flexDirection: 'row-reverse',
   '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
     transform: 'rotate(90deg)',
