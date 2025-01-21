4a19a1f35ef57e90ace1abfa05300813398cea3f
Julien Richard <julien.richard@filigran.io>
2025-01-21 12:22:32 +0100

    [backend] Try init fork for tests

--- opencti-platform/opencti-graphql/tests/utils/testSetup.js
+++ opencti-platform/opencti-graphql/tests/utils/testSetup.js
@@ -4,9 +4,11 @@ import { initializeRedisClients } from '../../src/database/redis';
 import { searchEngineInit } from '../../src/database/engine';
 import { initializeFileStorageClient } from '../../src/database/file-storage';
 import { initExclusionListCache } from '../../src/database/exclusionListCache';
+import { initLockFork } from '../../src/lock/master-lock';
 
 await initializeRedisClients();
 await searchEngineInit();
 await initializeFileStorageClient();
 cacheManager.init();
 await initExclusionListCache();
+initLockFork();
