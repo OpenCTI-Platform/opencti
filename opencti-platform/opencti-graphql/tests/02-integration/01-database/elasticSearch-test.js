1267a63cdec7742e29351057fd207fdd82dd7b20
FrancoisGrunert <francois.grunert@filigran.io>
2025-01-21 08:35:43 +0100

    [backend] fix tests (#5551)

--- opencti-platform/opencti-graphql/tests/02-integration/01-database/elasticSearch-test.js
+++ opencti-platform/opencti-graphql/tests/02-integration/01-database/elasticSearch-test.js
@@ -736,7 +736,7 @@ describe('Elasticsearch pagination', () => {
     expect(entityTypeMap.get('Label')).toBe(13);
     expect(entityTypeMap.get('Kill-Chain-Phase')).toBe(2);
     expect(entityTypeMap.get('External-Reference')).toBe(7);
-    expect(data.edges.length).toEqual(539);
+    expect(data.edges.length).toEqual(541);
     const createdDates = R.map((e) => e.node.created, data.edges);
     let previousCreatedDate = null;
     for (let index = 0; index < createdDates.length; index += 1) {
