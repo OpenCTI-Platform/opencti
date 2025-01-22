7c317196ea5df28f9671e5b1bb2cf7c62e9a9f01
Cathia Archidoit <cathia.archidoit@filigran.io>
2025-01-22 15:22:09 +0100

    [backend] fix types

--- opencti-platform/opencti-graphql/src/modules/fintelTemplate/fintel-template-resolvers.ts
+++ opencti-platform/opencti-graphql/src/modules/fintelTemplate/fintel-template-resolvers.ts
@@ -6,7 +6,7 @@ const fintelTemplateResolvers: Resolvers = {
     fintelTemplate: (_, { id }, context) => findById(context, context.user, id),
   },
   FintelTemplate: {
-    configuration_export: (fintelTemplate, _, context) => fintelTemplateExport(context, context.user, fintelTemplate),
+    configuration_export: (fintelTemplate, _, context) => fintelTemplateExport(context, context.user, fintelTemplate), // TODO rename toConfigurationExport
   },
   Mutation: {
     fintelTemplateAdd: (_, { input }, context) => {
