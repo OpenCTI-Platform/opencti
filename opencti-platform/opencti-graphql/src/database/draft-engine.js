fef04d4039e5e19926f75b780c7300bd3bdfc20a
Jeremy Cloarec <jeremy.cloarec@filigran.io>
2025-01-23 09:04:50 +0100

    [backend] remove from draft WIP

--- opencti-platform/opencti-graphql/src/database/draft-engine.js
+++ opencti-platform/opencti-graphql/src/database/draft-engine.js
@@ -1,6 +1,25 @@
-import { READ_INDEX_DRAFT_OBJECTS, READ_INDEX_HISTORY, READ_INDEX_INTERNAL_OBJECTS } from './utils';
-import { DatabaseError } from '../config/errors';
-import { elRawDeleteByQuery, elRawUpdateByQuery } from './engine';
+import { INDEX_DRAFT_OBJECTS, READ_INDEX_DRAFT_OBJECTS, READ_INDEX_HISTORY, READ_INDEX_INTERNAL_OBJECTS } from './utils';
+import { DatabaseError, UnsupportedError } from '../config/errors';
+import {elDeleteElements, elRawDeleteByQuery, elRawUpdateByQuery, getRelationsToRemove} from './engine';
+import { DRAFT_OPERATION_CREATE, DRAFT_OPERATION_DELETE_LINKED, DRAFT_OPERATION_UPDATE_LINKED } from '../modules/draftWorkspace/draftOperations';
+import { SYSTEM_USER } from '../utils/access';
+
+export const elRemoveElementFromDraft = async (context, user, element) => {
+  if (!element._index.includes(INDEX_DRAFT_OBJECTS)) {
+    return element;
+  }
+
+  if (element.draft_change?.draft_operation === DRAFT_OPERATION_UPDATE_LINKED || element.draft_change?.draft_operation === DRAFT_OPERATION_DELETE_LINKED) {
+    throw UnsupportedError('Cannot remove linked elements from draft', { id: element.id });
+  }
+
+  if (element.draft_change?.draft_operation === DRAFT_OPERATION_CREATE) {
+    await elDeleteElements()
+    const { relations } = await getRelationsToRemove(context, SYSTEM_USER, [element], { includeDeletedInDraft: true });
+    const draftRelations = relations.filter((f) => f._index.includes(INDEX_DRAFT_OBJECTS));
+  }
+  // TODO denormalized relations potentially need to be cleaned up + UPDATE_LINKED can be removed if no longer necessary
+};
 
 export const elDeleteDraftElements = async (context, user, draftId) => {
   return elRawDeleteByQuery({
