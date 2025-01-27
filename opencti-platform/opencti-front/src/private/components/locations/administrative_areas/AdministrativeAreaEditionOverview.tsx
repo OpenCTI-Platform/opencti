ed7830fe8a2fa9e64dcab37b7da2034889ca08d5
daimyo007 <165801009+daimyo007@users.noreply.github.com>
2025-01-27 15:25:32 -0500

    [frontend] added marking as mandatory on update

--- opencti-platform/opencti-front/src/private/components/locations/administrative_areas/AdministrativeAreaEditionOverview.tsx
+++ opencti-platform/opencti-front/src/private/components/locations/administrative_areas/AdministrativeAreaEditionOverview.tsx
@@ -161,6 +161,7 @@ AdministrativeAreaEditionOverviewProps
       .nullable(),
     references: Yup.array(),
     x_opencti_workflow_id: Yup.object(),
+    objectMarking: Yup.array().nullable(),
   }, mandatoryAttributes);
   const administrativeAreaValidator = useDynamicSchemaEditionValidation(mandatoryAttributes, basicShape);
 
--- opencti-platform/opencti-front/src/private/components/locations/cities/CityEditionOverview.tsx
+++ opencti-platform/opencti-front/src/private/components/locations/cities/CityEditionOverview.tsx
@@ -154,6 +154,7 @@ const CityEditionOverview: FunctionComponent<CityEditionOverviewProps> = ({
       .nullable(),
     references: Yup.array(),
     x_opencti_workflow_id: Yup.object(),
+    objectMarking: Yup.array().nullable(),
   }, mandatoryAttributes);
   const cityValidator = useDynamicSchemaEditionValidation(mandatoryAttributes, basicShape);
   const queries = {
