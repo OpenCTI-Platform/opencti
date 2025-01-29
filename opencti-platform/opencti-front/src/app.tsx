d27e61bcd38e27d63717554dd4ee6b33b4db2d09
Adrien Servel <50013774+Kedae@users.noreply.github.com>
2025-01-29 15:11:53 +0100

    [frontend] Fix the high level error for OTP

--- opencti-platform/opencti-front/src/app.tsx
+++ opencti-platform/opencti-front/src/app.tsx
@@ -1,6 +1,5 @@
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import React, { Suspense, lazy } from 'react';
-import { HighLevelError, ErrorBoundary } from '@components/Error';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import React, { lazy, Suspense } from 'react';
 import { APP_BASE_PATH } from './relay/environment';
 import { RedirectManager } from './components/RedirectManager';
 import AuthBoundaryComponent from './private/components/AuthBoundary';
@@ -14,17 +13,15 @@ const App = () => (
     <AuthBoundaryComponent>
       <RedirectManager>
         <Suspense fallback={<Loader />}>
-          <ErrorBoundary display={HighLevelError}>
-            <Routes>
-              <Route path="/dashboard/*" Component={PrivateRoot} />
-              <Route path="/public/*" Component={PublicRoot} />
-              {/* By default, redirect to dashboard */}
-              <Route
-                path="/*"
-                element={<Navigate to="/dashboard" replace={true} />}
-              />
-            </Routes>
-          </ErrorBoundary>
+          <Routes>
+            <Route path="/dashboard/*" Component={PrivateRoot} />
+            <Route path="/public/*" Component={PublicRoot} />
+            {/* By default, redirect to dashboard */}
+            <Route
+              path="/*"
+              element={<Navigate to="/dashboard" replace={true} />}
+            />
+          </Routes>
         </Suspense>
       </RedirectManager>
     </AuthBoundaryComponent>
