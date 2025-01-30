e2e791b402a911e8139e9232d0388905eb008cf9
Laurent Bonnet <146674147+labo-flg@users.noreply.github.com>
2025-01-30 08:58:53 +0100

    [backend] save column selection when creating public dashboards (#7295)
    Co-authored-by: Valentin Bouzin <valentin.bouzin@filigran.io>
    Co-authored-by: FlorianDelemarre <florian.delemarre@filigran.io>

--- opencti-platform/opencti-front/src/public/components/dashboard/stix_core_objects/PublicStixCoreObjectsList.tsx
+++ opencti-platform/opencti-front/src/public/components/dashboard/stix_core_objects/PublicStixCoreObjectsList.tsx
@@ -30,6 +30,20 @@ const publicStixCoreObjectsListQuery = graphql`
           id
           entity_type
           created_at
+          representative {
+            main
+            secondary
+          }
+          opinions_metrics {
+            mean
+            min
+            max
+            total
+          }
+          creators {
+            id
+            name
+          }
           ... on StixDomainObject {
             created
             modified
@@ -37,136 +51,326 @@ const publicStixCoreObjectsListQuery = graphql`
           ... on AttackPattern {
             name
             description
+            modified
+            x_mitre_id
+            aliases
+          }
+          ... on Note {
+            note_types
+            modified
           }
           ... on Campaign {
             name
             description
+            modified
+            aliases
           }
           ... on Note {
             attribute_abstract
+            modified
           }
           ... on ObservedData {
             name
+            modified
             first_observed
             last_observed
           }
           ... on Opinion {
             opinion
+            modified
           }
           ... on Report {
             name
             description
+            modified
             published
+            report_types
+            objectAssignee {
+              entity_type
+              id
+              name
+            }
+            objectParticipant {
+              entity_type
+              id
+              name
+            }
           }
           ... on Grouping {
             name
             description
+            modified
+            x_opencti_aliases
+            context
           }
           ... on CourseOfAction {
             name
             description
+            modified
+            x_opencti_aliases
+            x_mitre_id
           }
           ... on Individual {
             name
             description
+            modified
+            x_opencti_aliases
           }
           ... on Organization {
             name
             description
+            modified
+            x_opencti_aliases
+            x_opencti_organization_type
           }
           ... on Sector {
             name
             description
+            modified
+            x_opencti_aliases
           }
           ... on System {
             name
             description
+            modified
+            x_opencti_aliases
           }
           ... on Indicator {
             name
             description
+            modified
+            indicator_types
+            pattern
+            pattern_type
+            valid_from
+            valid_until
+            x_opencti_score
           }
           ... on Infrastructure {
             name
             description
+            modified
+            aliases
           }
           ... on IntrusionSet {
             name
             description
+            modified
+            aliases
+            resource_level
           }
           ... on Position {
             name
             description
+            modified
+            x_opencti_aliases
           }
           ... on City {
             name
             description
+            modified
+            x_opencti_aliases
           }
           ... on AdministrativeArea {
             name
             description
+            modified
+            x_opencti_aliases
           }
           ... on Country {
             name
             description
+            modified
+            x_opencti_aliases
           }
           ... on Region {
             name
             description
+            modified
+            x_opencti_aliases
           }
           ... on Malware {
             name
             description
+            modified
+            malware_types
+            aliases
           }
           ... on MalwareAnalysis {
             result_name
+            product
+            modified
+            objectAssignee {
+              entity_type
+              id
+              name
+            }
           }
           ... on ThreatActor {
             name
             description
+            modified
+            aliases
+            threat_actor_types
+          }
+          ... on ThreatActorGroup {
+            threat_actor_types
+            modified
+            aliases
+          }
+          ... on ThreatActorIndividual {
+            threat_actor_types
+            modified
+            aliases
           }
           ... on Tool {
             name
             description
+            modified
+            tool_types
+            aliases
           }
           ... on Vulnerability {
             name
             description
+            modified
+            x_opencti_aliases
+            x_opencti_cvss_base_score
+            x_opencti_cvss_base_severity
+            x_opencti_cisa_kev
+            x_opencti_epss_score
+            x_opencti_epss_percentile
           }
           ... on Incident {
             name
             description
+            modified
+            incident_type
+            severity
+            aliases
           }
           ... on Event {
             name
             description
+            modified
+            event_types
+            aliases
           }
           ... on Channel {
             name
             description
+            modified
+            channel_types
+            aliases
           }
           ... on Narrative {
             name
             description
+            modified
+            aliases
           }
           ... on Language {
             name
+            modified
+            aliases
           }
           ... on DataComponent {
             name
+            modified
+            aliases
           }
           ... on DataSource {
             name
-          }
-          ... on Case {
-            name
+            modified
+            aliases
           }
           ... on Task {
             name
             description
+            modified
+            due_date
+            objectAssignee {
+              id
+              name
+              entity_type
+            }
+            objectParticipant {
+              id
+              name
+              entity_type
+            }
+          }
+          ... on Case {
+            name
+            modified
+            objectAssignee {
+              id
+              name
+              entity_type
+            }
+            objectParticipant {
+              id
+              name
+              entity_type
+            }
+          }
+          ... on CaseIncident {
+            modified
+            priority
+            severity
+            response_types
+            objectAssignee {
+              id
+              name
+              entity_type
+            }
+            objectParticipant {
+              id
+              name
+              entity_type
+            }
+          }
+          ... on CaseRfi {
+            modified
+            priority
+            severity
+            information_types
+            objectAssignee {
+              id
+              name
+              entity_type
+            }
+            objectParticipant {
+              id
+              name
+              entity_type
+            }
+          }
+          ... on CaseRft {
+            modified
+            priority
+            severity
+            takedown_types
+            objectAssignee {
+              id
+              name
+              entity_type
+            }
+            objectParticipant {
+              id
+              name
+              entity_type
+            }
+          }
+          ... on Task {
+            name
+            description
+            modified
+            due_date
           }
           ... on StixCyberObservable {
             observable_value
+            x_opencti_description
           }
           createdBy {
             ... on Identity {
@@ -188,6 +392,7 @@ const publicStixCoreObjectsListQuery = graphql`
             x_opencti_color
           }
           ... on StixDomainObject {
+            modified
             status {
               id
               order
