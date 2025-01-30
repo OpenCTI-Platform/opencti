673fe077f0a2ed0c827a029380b6ab80f3b8b7cc
daimyo007 <165801009+daimyo007@users.noreply.github.com>
2025-01-30 03:56:37 -0500

    [frontend] adding the marking to the update

--- opencti-platform/opencti-front/src/private/components/entities/events/EventEditionOverview.jsx
+++ opencti-platform/opencti-front/src/private/components/entities/events/EventEditionOverview.jsx
@@ -93,6 +93,7 @@ const EventEditionOverviewComponent = (props) => {
       .nullable(),
     references: Yup.array(),
     x_opencti_workflow_id: Yup.object(),
+    objectMarking: Yup.array().nullable(),
   }, mandatoryAttributes);
   const eventValidator = useDynamicSchemaEditionValidation(mandatoryAttributes, basicShape);
 
