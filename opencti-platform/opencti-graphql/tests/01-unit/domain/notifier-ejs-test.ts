86ed688a0872588a365f83e2a2a93a05cbd04779
Angelique <angelique.jard@filigran.io>
2025-02-04 13:48:32 +0100

    Adding some allowed function in webhook

--- opencti-platform/opencti-graphql/tests/01-unit/domain/notifier-ejs-test.ts
+++ opencti-platform/opencti-graphql/tests/01-unit/domain/notifier-ejs-test.ts
@@ -9,6 +9,28 @@ describe('EJS pre verifications test', () => {
     checkAllowedEjsFunctions(DEFAULT_TEAM_MESSAGE.notifier_configuration, true);
   });
 
+  it('should some custom templates be allowed', async () => {
+    // expect to not throw exception.
+    checkAllowedEjsFunctions(`<% data.forEach(function(item, index) { %>
+    {
+      <% Object.keys(item).forEach(function(key, keyIndex, keys) { %>
+        "<%= key %>":
+        <% if (Array.isArray(item[key])) { %>
+          [
+            <% item[key].forEach(function(subItem, subIndex) { %>
+              {
+                <% Object.keys(subItem).forEach(function(subKey, subKeyIndex, subKeys) { %>
+                  "<%= subKey %>": "<%= JSON.stringify(subItem[subKey], null, 2) %>"
+                  <% if (subKeyIndex < subKeys.length - 1) { %>, <% } %>
+                  <% }); %>
+              }
+              <% if (subIndex < item[key].length - 1) { %>, <% } %>
+              <% }); %>
+        ]
+          <% } else if (typeof item[key] === 'object' && item[key] !== null) { %>
+          {`);
+  });
+
   it('should be forbidden to use functions not in allowed list', async () => {
     expect(() => checkAllowedEjsFunctions('{"random": "<%=myFunction1(content)%>"}')).toThrowError();
     expect(() => checkAllowedEjsFunctions('{"random": "<%=if(anotherForbiddenInside())%>"}')).toThrowError();
