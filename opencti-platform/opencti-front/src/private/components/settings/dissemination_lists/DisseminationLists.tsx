589d8f184e83fcd20b8250413e064989015d84b1
BocognanoSarah <sarah.bocognano@filigran.io>
2025-02-10 11:50:00 +0100

    [frontend] Spacing is not correct in dissemination list table between search and headers

--- opencti-platform/opencti-front/src/private/components/settings/dissemination_lists/DisseminationLists.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/dissemination_lists/DisseminationLists.tsx
@@ -20,7 +20,6 @@ import ItemIcon from '../../../../components/ItemIcon';
 import { UsePreloadedPaginationFragment } from '../../../../utils/hooks/usePreloadedPaginationFragment';
 import AlertInfo from '../../../../components/AlertInfo';
 import useConnectedDocumentModifier from '../../../../utils/hooks/useConnectedDocumentModifier';
-import PageContainer from '../../../../components/PageContainer';
 import useEnterpriseEdition from '../../../../utils/hooks/useEnterpriseEdition';
 
 export const disseminationListsQuery = graphql`
@@ -163,33 +162,32 @@ const DisseminationLists = () => {
   return (
     <>
       <AccessesMenu/>
-      <PageContainer withRightMenu>
-        <Breadcrumbs noMargin elements={[{ label: t_i18n('Settings') }, { label: t_i18n('Security') }, { label: t_i18n('Dissemination lists'), current: true }]} />
-        {!isEnterpriseEdition ? (
-          <EnterpriseEdition feature="File indexing" />
-        ) : (
-          <>
-            <AlertInfo
-              content={t_i18n('Disseminations lists can be used to send files to a list of recipients that do not necessarily have an OpenCTI account.')}
-            />
-            {queryRef && (
-              <DataTable
-                dataColumns={dataColumns}
-                resolvePath={(data) => data.disseminationLists?.edges?.map(({ node }: { node: DisseminationListsLine_node$data }) => node)}
-                storageKey={LOCAL_STORAGE_KEY}
-                initialValues={initialValues}
-                toolbarFilters={contextFilters}
-                lineFragment={disseminationListsLineFragment}
-                disableLineSelection
-                disableNavigation
-                preloadedPaginationProps={preloadedPaginationProps}
-                actions={(row) => <DisseminationListPopover data={row} paginationOptions={queryPaginationOptions} />}
-              />
-            )}
-            <DisseminationListCreation paginationOptions={queryPaginationOptions} />
-          </>
-        )}
-      </PageContainer>
+      <Breadcrumbs elements={[{ label: t_i18n('Settings') }, { label: t_i18n('Security') }, { label: t_i18n('Dissemination lists'), current: true }]} />
+      {!isEnterpriseEdition ? (
+        <EnterpriseEdition feature="File indexing" />
+      ) : (
+        <>
+          <AlertInfo
+            style={{ marginBottom: '16px' }}
+            content={t_i18n('Disseminations lists can be used to send files to a list of recipients that do not necessarily have an OpenCTI account.')}
+          />
+          {queryRef && (
+          <DataTable
+            dataColumns={dataColumns}
+            resolvePath={(data) => data.disseminationLists?.edges?.map(({ node }: { node: DisseminationListsLine_node$data }) => node)}
+            storageKey={LOCAL_STORAGE_KEY}
+            initialValues={initialValues}
+            toolbarFilters={contextFilters}
+            lineFragment={disseminationListsLineFragment}
+            disableLineSelection
+            disableNavigation
+            preloadedPaginationProps={preloadedPaginationProps}
+            actions={(row) => <DisseminationListPopover data={row} paginationOptions={queryPaginationOptions} />}
+          />
+          )}
+          <DisseminationListCreation paginationOptions={queryPaginationOptions} />
+        </>
+      )}
     </>
   );
 };
