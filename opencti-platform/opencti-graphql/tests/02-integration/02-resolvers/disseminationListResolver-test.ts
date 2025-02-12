cc241151916995e350e4b5b0d700e480f3476f8d
FrancoisGrunert <francois.grunert@filigran.io>
2025-02-12 14:35:15 +0100

    [backend] tests resolvers (#8949)

--- opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/disseminationListResolver-test.ts
+++ opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/disseminationListResolver-test.ts
@@ -26,6 +26,18 @@ const READ_QUERY = gql`
   }
 `;
 
+const EDIT_QUERY = gql`
+  mutation DisseminationListEdit($id: ID!, $input: [EditInput!]!) {
+    disseminationListFieldPatch(id: $id, input: $input) {
+      id
+      standard_id
+      name
+      description
+      emails
+    }
+  }
+`;
+
 const DELETE_QUERY = gql`
   mutation disseminationListDelete($id: ID!) {
     disseminationListDelete(id: $id)
@@ -41,6 +53,7 @@ describe('Dissemination list resolver', () => {
       emails: ['email1@test.com', 'email2@test.com', 'email3@test.com'],
     }
   };
+  const NEW_DESCRIPTION = 'New description';
 
   it('should dissemination list created', async () => {
     await enableEE();
@@ -54,6 +67,28 @@ describe('Dissemination list resolver', () => {
     expect(disseminationList.data?.disseminationListAdd.description).toEqual(DISSEMINATION_LIST_TO_CREATE.input.description);
     expect(disseminationList.data?.disseminationListAdd.emails).toEqual(DISSEMINATION_LIST_TO_CREATE.input.emails);
     disseminationListInternalId = disseminationList.data?.disseminationListAdd.id;
+  });
+
+  it('should dissemination list loaded by internal id', async () => {
+    const queryResult = await queryAsAdmin({ query: READ_QUERY, variables: { id: disseminationListInternalId } });
+    expect(queryResult).not.toBeNull();
+    expect(queryResult.data?.disseminationList).not.toBeNull();
+    expect(queryResult.data?.disseminationList.id).toEqual(disseminationListInternalId);
+    expect(queryResult.data?.disseminationList.name).toEqual(DISSEMINATION_LIST_TO_CREATE.input.name);
+  });
+
+  it('should dissemination list edited', async () => {
+    const queryResult = await queryAsAdmin({
+      query: EDIT_QUERY,
+      variables: {
+        id: disseminationListInternalId,
+        input: [{ key: 'description', value: [NEW_DESCRIPTION] }],
+      }
+    });
+    const disseminationListDescription = queryResult.data?.disseminationListFieldPatch.description;
+    expect(disseminationListDescription).toEqual(NEW_DESCRIPTION);
+    const queryResult2 = await queryAsAdmin({ query: READ_QUERY, variables: { id: disseminationListInternalId } });
+    expect(queryResult2.data?.disseminationList.description).toEqual(NEW_DESCRIPTION);
   });
 
   it('should dissemination list deleted', async () => {
