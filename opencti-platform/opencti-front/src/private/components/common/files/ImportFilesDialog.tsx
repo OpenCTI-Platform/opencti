5817f7e479477d9c27d9594ea009bff40dba883f
FlorianDelemarre <florian.delemarre@filigran.io>
2025-02-12 17:20:34 +0100

    [frontend] add stepper (#9760)

--- opencti-platform/opencti-front/src/private/components/common/files/ImportFilesDialog.tsx
+++ opencti-platform/opencti-front/src/private/components/common/files/ImportFilesDialog.tsx
@@ -1,5 +1,5 @@
-import React, { FunctionComponent } from 'react';
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import React, { useState } from 'react';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Stepper, Step, StepButton } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import { useFormatter } from '../../../../components/i18n';
 import Transition from '../../../../components/Transition';
@@ -9,28 +9,63 @@ interface ImportFilesDialogProps {
   handleClose: () => void;
 }
 
-const ImportFilesDialog: FunctionComponent<ImportFilesDialogProps> = ({
-  open,
-  handleClose,
-}) => {
+const ImportFilesDialog = ({ open, handleClose } : ImportFilesDialogProps) => {
   const { t_i18n } = useFormatter();
+
+  const [activeStep, setActiveStep] = useState(0);
+  const [data, setData] = useState({});
+
+  const steps = ['Select files', 'Specific files configurations', 'Import options'];
+
+  const onCancel = () => {
+    handleClose();
+    setActiveStep(0);
+    setData({});
+  };
+
+  const handleSubmit = () => {
+    console.log('Submit:', data);
+    handleClose();
+  };
 
   return (
     <Dialog
       open={open}
-      PaperProps={{ elevation: 1 }}
       TransitionComponent={Transition}
-      fullWidth={true}
+      fullWidth
+      maxWidth={false}
+      PaperProps={{
+        elevation: 1,
+        style: {
+          height: '100vh',
+        },
+      }}
     >
-      <DialogTitle><Typography variant="h5">{t_i18n('Import files')}</Typography> </DialogTitle>
-      <DialogContent>
-
+      <DialogTitle><Typography variant="h5">{t_i18n('Import files')}</Typography></DialogTitle>
+      <DialogContent sx={{ padding: 20 }}>
+        <Stepper nonLinear activeStep={activeStep} >
+          {steps.map((label, index) => (
+            <Step key={label}>
+              <StepButton color="inherit" onClick={() => setActiveStep(index)}>
+                {label}
+              </StepButton>
+            </Step>
+          ))}
+        </Stepper>
       </DialogContent>
       <DialogActions>
-        <Button onClick={() => handleClose()}>
+        <Button onClick={() => onCancel()}>
           {t_i18n('Cancel')}
         </Button>
-        <Button color="secondary" >{t_i18n('Next')}</Button>
+        {activeStep !== steps.length - 1 ? (
+          <Button onClick={() => setActiveStep(activeStep + 1)} color="secondary">
+            {t_i18n('Next')}
+          </Button>
+        ) : (
+          <Button onClick={() => handleSubmit()} color="secondary">
+            {t_i18n('Submit')}
+          </Button>
+        )}
       </DialogActions>
     </Dialog>
   );
