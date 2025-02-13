b2d9241bd62fdf01c008e342a697fb6a730cf215
marie flores <marie.flores@filigran.io>
2025-02-13 16:45:17 +0100

    [frontend] Add files options form

--- opencti-platform/opencti-front/src/private/components/common/files/ImportFilesDialog.tsx
+++ opencti-platform/opencti-front/src/private/components/common/files/ImportFilesDialog.tsx
@@ -4,9 +4,14 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { useTheme } from '@mui/styles';
 import { CloudUploadOutlined, DeleteOutlined, UploadFileOutlined } from '@mui/icons-material';
+import { Formik } from 'formik';
+import ObjectMarkingField from '@components/common/form/ObjectMarkingField';
+import AssociatedEntityField, { AssociatedEntityOption } from '@components/common/form/AssociatedEntityField';
+import { Option } from '@components/common/form/ReferenceField';
 import { useFormatter } from '../../../../components/i18n';
 import Transition from '../../../../components/Transition';
 import type { Theme } from '../../../../components/Theme';
+import { fieldSpacingContainerStyle } from '../../../../utils/field';
 
 interface ImportFilesDialogProps {
   open: boolean;
@@ -95,8 +100,40 @@ const ImportFilesConfigurations = () => {
   return (<Box>CONFIG</Box>);
 };
 
-const ImportFilesOptions = () => {
-  return (<Box>OPTION</Box>);
+const ImportFilesOptions = ({ setFieldValue, entityId }: { setFieldValue: (name: string, values: Option[] | AssociatedEntityOption) => void; entityId?: string }) => {
+  const { t_i18n } = useFormatter();
+  return (
+    <Box sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 2,
+      width: '50%',
+    }}
+    >
+      <ObjectMarkingField
+        name="fileMarkings"
+        label={t_i18n('File marking definition levels')}
+        style={fieldSpacingContainerStyle}
+        setFieldValue={setFieldValue}
+        required={false}
+      />
+      {!entityId
+      && (
+        <div style={{ paddingTop: '10px' }}>
+          <AssociatedEntityField
+            label={t_i18n('Associated entity')}
+            name="associatedEntity"
+            onChange={setFieldValue}
+          />
+        </div>
+      )}
+    </Box>
+  );
+};
+
+type SubmittedFormValues = {
+  fileMarkings: Option[];
+  associatedEntity: AssociatedEntityOption;
 };
 
 const ImportFilesDialog = ({ open, handleClose }: ImportFilesDialogProps) => {
@@ -113,57 +150,73 @@ const ImportFilesDialog = ({ open, handleClose }: ImportFilesDialogProps) => {
     setData({});
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = (values: SubmittedFormValues) => {
+    console.log({ values });
     handleClose();
   };
 
   return (
-    <Dialog
-      open={open}
-      TransitionComponent={Transition}
-      fullWidth
-      maxWidth={false}
-      PaperProps={{
-        elevation: 1,
-        style: {
-          height: '100vh',
-        },
+    <Formik
+      enableReinitialize={true}
+      initialValues={{
+        fileMarkings: [],
+        associatedEntity: { label: '', value: '', type: '' },
       }}
+      onSubmit={handleSubmit}
     >
-      <DialogTitle>
-        <Typography variant="h5">{t_i18n('Import files')}</Typography>
-      </DialogTitle>
-      <DialogContent sx={{ paddingInline: 20, marginBlock: 10 }}>
-        <Stepper nonLinear activeStep={activeStep} sx={{ marginInline: 10 }}>
-          {steps.map((label, index) => (
-            <Step key={label}>
-              <StepButton color="inherit" onClick={() => setActiveStep(index)}>
-                {label}
-              </StepButton>
-            </Step>
-          ))}
-        </Stepper>
-        <Box sx={{ paddingBlock: 10 }}>
-          {activeStep === 0 && <ImportFilesUploader files={data.files} onChange={(files) => setData({ files })} />}
-          {activeStep === 1 && <ImportFilesConfigurations />}
-          {activeStep === 2 && <ImportFilesOptions />}
-        </Box>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={() => onCancel()}>
-          {t_i18n('Cancel')}
-        </Button>
-        {activeStep !== steps.length - 1 ? (
-          <Button onClick={() => setActiveStep(activeStep + 1)} color="secondary">
-            {t_i18n('Next')}
-          </Button>
-        ) : (
-          <Button onClick={() => handleSubmit()} color="secondary">
-            {t_i18n('Submit')}
-          </Button>
-        )}
-      </DialogActions>
-    </Dialog>
+      {({ resetForm, submitForm, setFieldValue }) => (
+        <Dialog
+          open={open}
+          TransitionComponent={Transition}
+          fullWidth
+          maxWidth={false}
+          PaperProps={{
+            elevation: 1,
+            style: {
+              height: '100vh',
+            },
+          }}
+        >
+          <DialogTitle>
+            <Typography variant="h5">{t_i18n('Import files')}</Typography>
+          </DialogTitle>
+          <DialogContent sx={{ paddingInline: 20, marginBlock: 10 }}>
+            <Stepper nonLinear activeStep={activeStep} sx={{ marginInline: 10 }}>
+              {steps.map((label, index) => (
+                <Step key={label}>
+                  <StepButton color="inherit" onClick={() => setActiveStep(index)}>
+                    {label}
+                  </StepButton>
+                </Step>
+              ))}
+            </Stepper>
+            <Box sx={{ paddingBlock: 10 }}>
+              {activeStep === 0 && <ImportFilesUploader files={data.files} onChange={(files) => setData({ files })} />}
+              {activeStep === 1 && <ImportFilesConfigurations />}
+              {activeStep === 2 && <ImportFilesOptions setFieldValue={setFieldValue}/>}
+            </Box>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => {
+              resetForm();
+              onCancel();
+            }}
+            >
+              {t_i18n('Cancel')}
+            </Button>
+            {activeStep !== steps.length - 1 ? (
+              <Button onClick={() => setActiveStep(activeStep + 1)} color="secondary">
+                {t_i18n('Next')}
+              </Button>
+            ) : (
+              <Button onClick={submitForm} color="secondary">
+                {t_i18n('Submit')}
+              </Button>
+            )}
+          </DialogActions>
+        </Dialog>
+      )}
+    </Formik>
   );
 };
 
