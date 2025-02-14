bd1ae0f7c7507969a918926878ff3534618e238f
Bonsai8863 <131906254+Bonsai8863@users.noreply.github.com>
2025-02-14 11:37:27 -0500

    [backend/frontend] Addressing review comments
    [frontend] Resolved type import
    
    [frontend] Various PR comments addressed
    
    [frontend] Modified custom themes workflow
    
    [frontend] Fixed generated graphql imports after moving files to subfolder
    
    [frontend/backend] WIP: Updated import/export functionality
    
    [frontend/backend] Translated system themes
    
    [frontend/backend] Custom theme support fully functional
    
    [frontend] Fixing e2e tests and adding translations
    
    [frontend] Changed test to expect light mode
    
    [frontend/backend] Migration script to set default theme
    
    [backend] Setting initializeData's platform_theme to "Dark"
    
    [frontend] Settings test sets theme for consistency

--- opencti-platform/opencti-graphql/src/modules/theme/theme-converter.ts
+++ opencti-platform/opencti-graphql/src/modules/theme/theme-converter.ts
@@ -7,15 +7,7 @@ const convertThemeToStix = (instance: StoreEntityTheme): StixTheme => {
   return {
     ...stixDomainObject,
     name: instance.name,
-    theme_background: instance.theme_background,
-    theme_paper: instance.theme_paper,
-    theme_nav: instance.theme_nav,
-    theme_primary: instance.theme_primary,
-    theme_secondary: instance.theme_secondary,
-    theme_accent: instance.theme_accent,
-    theme_logo: instance.theme_logo,
-    theme_logo_collapsed: instance.theme_logo_collapsed,
-    theme_logo_login: instance.theme_logo_login,
+    manifest: instance.manifest,
     extensions: {
       [STIX_EXT_OCTI]: cleanObject({
         ...stixDomainObject.extensions[STIX_EXT_OCTI],
