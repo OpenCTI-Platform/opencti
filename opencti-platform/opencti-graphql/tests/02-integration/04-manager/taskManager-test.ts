7f578fca083e6c47d38655281fc08ca97a847eaf
Julien Richard <julien.richard@filigran.io>
2025-02-15 17:44:22 +0100

    [backend] Fix test

--- opencti-platform/opencti-graphql/tests/02-integration/04-manager/taskManager-test.ts
+++ opencti-platform/opencti-graphql/tests/02-integration/04-manager/taskManager-test.ts
@@ -1,16 +1,11 @@
 import { afterAll, describe, expect, it } from 'vitest';
-import type { BasicStoreEntity, StoreEntityReport } from '../../../src/types/store';
-import { addStixCyberObservable } from '../../../src/domain/stixCyberObservable';
 import type { AuthContext } from '../../../src/types/user';
-import { ADMIN_USER, getUserIdByEmail, TEST_ORGANIZATION, testContext, USER_EDITOR } from '../../utils/testQuery';
+import { ADMIN_USER, getUserIdByEmail, USER_EDITOR } from '../../utils/testQuery';
 import { addReport, findById as findReportById } from '../../../src/domain/report';
 import { stixDomainObjectDelete } from '../../../src/domain/stixDomainObject';
-import { generateFiltersForSharingTask } from '../../../src/domain/stix';
-import { addStixCoreRelationship } from '../../../src/domain/stixCoreRelationship';
 import { editAuthorizedMembers } from '../../../src/utils/authorizedMembers';
 import { KNOWLEDGE_KNUPDATE_KNMANAGEAUTHMEMBERS } from '../../../src/utils/access';
 import { executeRemoveAuthMembers } from '../../../src/domain/stixCoreObject';
-import { taskQuery } from '../../../src/manager/taskManager';
 
 describe('TaskManager executeRemoveAuthMembers tests', () => {
   const adminContext: AuthContext = { user: ADMIN_USER, tracing: undefined, source: 'taskManager-integration-test', otp_mandatory: false };
@@ -61,78 +56,5 @@ describe('TaskManager executeRemoveAuthMembers tests', () => {
     // Verify there are no authorized
     const reportAfterRemove = await findReportById(adminContext, adminContext.user, reportId);
     expect(reportAfterRemove.authorized_members).toBeUndefined();
-  });
-});
-
-describe('TaskManager computeQueryTaskElements', () => {
-  let observable1;
-  let observable2;
-  let createdReport: StoreEntityReport;
-
-  it('Create data for test', async () => {
-    const observable1Data = {
-      type: 'Domain-Name',
-      DomainName: { value: 'observable-in-report-querytask.com' },
-    };
-
-    const observable2Data = {
-      type: 'Domain-Name',
-      DomainName: { value: 'observable-in-report-querytask.fr' },
-    };
-
-    observable1 = await addStixCyberObservable(testContext, ADMIN_USER, observable1Data);
-    observable2 = await addStixCyberObservable(testContext, ADMIN_USER, observable2Data);
-    createdReport = await addReport(testContext, ADMIN_USER, {
-      name: 'taskManager test - computeQueryTaskElements',
-      published: '2024-10-06T22:00:00.000Z',
-      description: 'report use for taskManager test purpose on orderMode',
-      objects: [observable1.id, observable2.id]
-    });
-    const relationShipAddInput = {
-      relationship_type: 'related-to',
-      confidence: 100,
-      description: '',
-      killChainPhases: [],
-      externalReferences: [],
-      objectMarking: [],
-      fromId: observable1.id,
-      toId: observable2.id
-    };
-    await addStixCoreRelationship(testContext, ADMIN_USER, relationShipAddInput);
-  });
-
-  it('When order mode is set to asc it should be taken', async () => {
-    const filters = generateFiltersForSharingTask(createdReport.internal_id);
-    const task = {
-      task_filters: JSON.stringify(filters),
-      task_excluded_ids: [],
-      task_order_mode: 'asc',
-      actions: [{ type: 'SHARE', context: { values: [TEST_ORGANIZATION.id] } }],
-      scope: 'KNOWLEDGE',
-    };
-    const elements: BasicStoreEntity[] = [];
-    await taskQuery(testContext, ADMIN_USER, task, (results: BasicStoreEntity[]) => {
-      elements.push(...results);
-    });
-    const observableOrder1: BasicStoreEntity = elements[0];
-    const observableOrder2: BasicStoreEntity = elements[1];
-    expect(observableOrder1.created_at < observableOrder2.created_at);
-  });
-
-  it('When order mode is not set, default desc should be taken', async () => {
-    const filters = generateFiltersForSharingTask(createdReport.internal_id);
-    const task = {
-      task_filters: JSON.stringify(filters),
-      task_excluded_ids: [],
-      actions: [{ type: 'SHARE', context: { values: [TEST_ORGANIZATION.id] } }],
-      scope: 'KNOWLEDGE',
-    };
-    const elements: BasicStoreEntity[] = [];
-    await taskQuery(testContext, ADMIN_USER, task, (results: BasicStoreEntity[]) => {
-      elements.push(...results);
-    });
-    const observableOrder1: BasicStoreEntity = elements[0];
-    const observableOrder2: BasicStoreEntity = elements[1];
-    expect(observableOrder1.created_at > observableOrder2.created_at);
   });
 });
