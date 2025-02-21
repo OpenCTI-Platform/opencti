e11a066ce330d143d648b0807ebc348a3255cc9e
Celine Sebe <celine.sebe@filigran.io>
2025-02-21 17:34:57 +0100

    [frontend] unit test Malware

--- opencti-platform/opencti-front/src/private/components/common/stix_domain_objects/diamond/test/getFilterFromEntity.test.tsx
+++ opencti-platform/opencti-front/src/private/components/common/stix_domain_objects/diamond/test/getFilterFromEntity.test.tsx
@@ -78,3 +78,81 @@ describe('get the filters from Threat-actor-group, containing a diamond ', () =>
     expect(results).toEqual(encodedFilters);
   });
 });
+
+describe('get the filters from Malware, containing a diamond ', () => {
+  const entityType = 'Malware' as DiamondEntityType;
+  it('should list filter for adversary', () => {
+    const nodeType = 'adversary' as DiamondNodeType;
+    const expectedFilters = {
+      mode: 'and',
+      filters: [
+        {
+          key: 'entity_type',
+          operator: 'eq',
+          values: [
+            'Intrusion-Set',
+            'Threat-Actor-Group',
+            'Threat-Actor-Individual',
+          ],
+          mode: 'or',
+        },
+        {
+          key: 'regardingOf',
+          operator: 'eq',
+          values: [
+            {
+              key: 'relationship_type',
+              values: [
+                'authored-by',
+              ],
+            },
+          ],
+          mode: 'or',
+        },
+      ],
+      filterGroups: [],
+    };
+    const encodedFilters = encodeURIComponent(JSON.stringify(expectedFilters));
+    const results = getFilterFromEntityTypeAndNodeType(entityType, nodeType);
+    expect(results).toEqual(encodedFilters);
+  });
+  it('should list filter for infrastructure', () => {
+    const nodeType = 'infrastructure' as DiamondNodeType;
+    const expectedFilters = {
+      mode: 'and',
+      filters: [
+        {
+          key: 'entity_type',
+          operator: 'eq',
+          values: [
+            'IPv4-Addr',
+            'IPv6-Addr',
+            'Infrastructure',
+            'Domain-Name',
+          ],
+          mode: 'or',
+        },
+        {
+          key: 'regardingOf',
+          operator: 'eq',
+          values: [
+            {
+              key: 'relationship_type',
+              values: [
+                'uses',
+                'exfiltrates-to',
+                'beacons-to',
+                'communicates-to',
+              ],
+            },
+          ],
+          mode: 'or',
+        },
+      ],
+      filterGroups: [],
+    };
+    const encodedFilters = encodeURIComponent(JSON.stringify(expectedFilters));
+    const results = getFilterFromEntityTypeAndNodeType(entityType, nodeType);
+    expect(results).toEqual(encodedFilters);
+  });
+});
