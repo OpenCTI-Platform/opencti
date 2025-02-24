5c12b190a63a3ac8f1e17dcd565f8a277de7c3bc
Julien Richard <julien.richard@filigran.io>
2025-02-24 18:48:48 +0100

    [backend] Fix test

--- opencti-platform/opencti-graphql/tests/03-streams/00-Raw/raw-test.js
+++ opencti-platform/opencti-graphql/tests/03-streams/00-Raw/raw-test.js
@@ -84,7 +84,7 @@ describe('Raw streams tests', () => {
       expect(updateEventsByTypes['threat-actor'].length).toBe(17);
       expect(updateEventsByTypes['vocabulary'].length).toBe(3);
       expect(updateEventsByTypes['vulnerability'].length).toBe(3);
-      expect(updateEvents.length).toBe(169);
+      expect(updateEvents.length).toBe(171);
       for (let updateIndex = 0; updateIndex < updateEvents.length; updateIndex += 1) {
         const event = updateEvents[updateIndex];
         const { data: insideData, origin, type } = event;
