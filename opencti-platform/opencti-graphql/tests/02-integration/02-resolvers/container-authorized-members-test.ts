3b44fa0b00b079691b1664893f0d41d9b407c107
Julien Richard <julien.richard@filigran.io>
2025-02-24 17:57:57 +0100

    [backend] Change test to direct sharing

--- opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/container-authorized-members-test.ts
+++ opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/container-authorized-members-test.ts
@@ -16,7 +16,6 @@ import {
 import { adminQueryWithSuccess, disableEE, enableCEAndUnSetOrganization, enableEE, enableEEAndSetOrganization, queryAsUserIsExpectedForbidden } from '../../utils/testQueryHelper';
 import { ENTITY_TYPE_CONTAINER_CASE_INCIDENT } from '../../../src/modules/case/case-incident/case-incident-types';
 import conf from '../../../src/config/conf';
-import { wait } from '../../../src/database/utils';
 
 const CREATE_QUERY = gql`
   mutation CaseIncidentAdd($input: CaseIncidentAddInput!) {
@@ -146,9 +145,10 @@ const ORGANIZATION_SHARING_QUERY = gql`
   mutation StixCoreObjectSharingGroupAddMutation(
     $id: ID!
     $organizationId: [ID!]!
+    $directContainerSharing: Boolean
   ) {
     stixCoreObjectEdit(id: $id) {
-      restrictionOrganizationAdd(organizationId: $organizationId) {
+      restrictionOrganizationAdd(organizationId: $organizationId, directContainerSharing: $directContainerSharing) {
         id
         objectOrganization {
           id
@@ -495,12 +495,10 @@ describe('Case Incident Response and organization sharing standard behavior with
     organizationId = await getOrganizationIdByName(PLATFORM_ORGANIZATION.name);
     const organizationSharingQueryResult = await adminQuery({
       query: ORGANIZATION_SHARING_QUERY,
-      variables: { id: caseIrId, organizationId }
+      variables: { id: caseIrId, organizationId, directContainerSharing: true }
     });
     expect(organizationSharingQueryResult).not.toBeNull();
     expect(organizationSharingQueryResult?.data?.stixCoreObjectEdit.restrictionOrganizationAdd).not.toBeNull();
-    // Wait background task execution
-    await wait(5000);
     // Verify Editor user has access to Case incident after sharing
     const caseIRQueryResult = await adminQuery({ query: READ_QUERY, variables: { id: caseIrId } });
     expect(caseIRQueryResult).not.toBeNull();
@@ -661,12 +659,10 @@ describe('Case Incident Response and organization sharing standard behavior with
     testOrganizationId = await getOrganizationIdByName(TEST_ORGANIZATION.name);
     const organizationSharingQueryResult = await adminQuery({
       query: ORGANIZATION_SHARING_QUERY,
-      variables: { id: caseIrId, organizationId: testOrganizationId }
+      variables: { id: caseIrId, organizationId: testOrganizationId, directContainerSharing: true }
     });
     expect(organizationSharingQueryResult).not.toBeNull();
     expect(organizationSharingQueryResult?.data?.stixCoreObjectEdit.restrictionOrganizationAdd).not.toBeNull();
-    // Wait background task execution
-    await wait(5000);
     // Verify Editor user has access to Case incident
     const caseIRQueryResult = await editorQuery({ query: READ_QUERY, variables: { id: caseIrId } });
     expect(caseIRQueryResult).not.toBeNull();
