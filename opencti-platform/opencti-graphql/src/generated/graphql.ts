71e690716274422e7c7030043ef049fdd135c8b8
Julien Richard <julien.richard@filigran.io>
2025-02-26 11:42:20 +0100

    First commit to support connectors query for manager

--- opencti-platform/opencti-graphql/src/generated/graphql.ts
+++ opencti-platform/opencti-graphql/src/generated/graphql.ts
@@ -19640,6 +19640,7 @@ export type Query = {
   connectorsForAnalysis?: Maybe<Array<Maybe<Connector>>>;
   connectorsForExport?: Maybe<Array<Maybe<Connector>>>;
   connectorsForImport?: Maybe<Array<Maybe<Connector>>>;
+  connectorsForManager?: Maybe<Array<Connector>>;
   connectorsForNotification?: Maybe<Array<Maybe<Connector>>>;
   connectorsForWorker?: Maybe<Array<Maybe<Connector>>>;
   container?: Maybe<Container>;
@@ -20226,6 +20227,11 @@ export type QueryCityArgs = {
 
 export type QueryConnectorArgs = {
   id: Scalars['String']['input'];
+};
+
+
+export type QueryConnectorsForManagerArgs = {
+  managerId: Scalars['ID']['input'];
 };
 
 
@@ -39116,6 +39122,7 @@ export type QueryResolvers<ContextType = any, ParentType extends ResolversParent
   connectorsForAnalysis?: Resolver<Maybe<Array<Maybe<ResolversTypes['Connector']>>>, ParentType, ContextType>;
   connectorsForExport?: Resolver<Maybe<Array<Maybe<ResolversTypes['Connector']>>>, ParentType, ContextType>;
   connectorsForImport?: Resolver<Maybe<Array<Maybe<ResolversTypes['Connector']>>>, ParentType, ContextType>;
+  connectorsForManager?: Resolver<Maybe<Array<ResolversTypes['Connector']>>, ParentType, ContextType, RequireFields<QueryConnectorsForManagerArgs, 'managerId'>>;
   connectorsForNotification?: Resolver<Maybe<Array<Maybe<ResolversTypes['Connector']>>>, ParentType, ContextType>;
   connectorsForWorker?: Resolver<Maybe<Array<Maybe<ResolversTypes['Connector']>>>, ParentType, ContextType>;
   container?: Resolver<Maybe<ResolversTypes['Container']>, ParentType, ContextType, Partial<QueryContainerArgs>>;
