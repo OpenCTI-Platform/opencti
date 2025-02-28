fd3b74f894a3cb4f9c0f1855b9734974a5dc55c3
marie flores <marie.flores@filigran.io>
2025-02-28 16:28:00 +0100

    [backend] wip: Malware converter

--- opencti-platform/opencti-graphql/src/database/stix-converter-2-0.ts
+++ opencti-platform/opencti-graphql/src/database/stix-converter-2-0.ts
@@ -2,5 +2,5 @@ import type { StoreEntity } from '../types/store';
 import type * as SDO from '../types/stix-2-0/stix-2-0-sdo';
 
 const convertMalwareToStix2 = (instance: StoreEntity, type: string): SDO.StixMalware2 => {
-
+  return {};
 };
