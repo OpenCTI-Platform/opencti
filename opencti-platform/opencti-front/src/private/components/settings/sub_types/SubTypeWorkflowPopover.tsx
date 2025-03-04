6a26f76b66ccf2a346312a81776318df014f5f2b
BocognanoSarah <sarah.bocognano@filigran.io>
2025-03-04 16:29:15 +0100

    Rework workflow UI

--- opencti-platform/opencti-front/src/private/components/settings/sub_types/SubTypeWorkflowPopover.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/sub_types/SubTypeWorkflowPopover.tsx
@@ -1,10 +1,13 @@
 import React, { FunctionComponent, useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import { Edit } from '@mui/icons-material';
+import { InformationOutline } from 'mdi-material-ui';
+import { Tooltip } from '@mui/material';
 import Loader, { LoaderVariant } from '../../../../components/Loader';
 import useQueryLoading from '../../../../utils/hooks/useQueryLoading';
 import SubTypeWorkflow, { subTypeWorkflowEditionQuery } from './SubTypeWorkflow';
 import { SubTypeWorkflowEditionQuery } from './__generated__/SubTypeWorkflowEditionQuery.graphql';
+import { useFormatter } from '../../../../components/i18n';
 
 interface SubTypeStatusPopoverProps {
   subTypeId: string
@@ -16,9 +19,11 @@ const SubTypeStatusPopover: FunctionComponent<SubTypeStatusPopoverProps> = ({ su
     subTypeWorkflowEditionQuery,
     { id: subTypeId },
   );
+  const { t_i18n } = useFormatter();
   const [displayUpdate, setDisplayUpdate] = useState<boolean>(false);
   const handleOpenUpdate = () => setDisplayUpdate(true);
   const handleCloseUpdate = () => setDisplayUpdate(false);
+  const requestAccessScope = scope === 'REQUEST_ACCESS';
   return (
     <>
       <IconButton
@@ -30,6 +35,18 @@ const SubTypeStatusPopover: FunctionComponent<SubTypeStatusPopoverProps> = ({ su
       >
         <Edit fontSize="small" />
       </IconButton>
+      {requestAccessScope && (
+        <Tooltip
+          title={t_i18n('RFI of type "request access" are subject to a specific workflow, that you can configure here.\n'
+            + 'Request Access cases have 2 actions, Validate and Decline, that change the status automatically according to your configuration. Only specific groups of users are authorized to validate and decline Request Access cases.')}
+        >
+          <InformationOutline
+            fontSize="small"
+            color="primary"
+            style={{ cursor: 'default', margin: '-2px 0 -6px 0' }}
+          />
+        </Tooltip>
+      )}
       {queryRef && (
         <React.Suspense
           fallback={<Loader variant={LoaderVariant.inElement} />}
