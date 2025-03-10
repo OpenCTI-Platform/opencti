71c53263f7b7f0582b52310a19808983520a13e9
BocognanoSarah <sarah.bocognano@filigran.io>
2025-03-10 16:06:03 +0100

    [frontend] Refacto Message refacto

--- opencti-platform/opencti-front/src/components/Message.tsx
+++ opencti-platform/opencti-front/src/components/Message.tsx
@@ -3,14 +3,44 @@ import Snackbar, { SnackbarCloseReason } from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
 import { AutoAwesomeOutlined } from '@mui/icons-material';
 import { useTheme } from '@mui/styles';
+import { Formik, Form } from 'formik';
+import Button from '@mui/material/Button/Button';
 import { MESSAGING$ } from '../relay/environment';
 import { useFormatter } from './i18n';
 import type { Theme } from './Theme';
+import RequestAccessDialog from './RequestAccessDialog';
+import useHelper from '../utils/hooks/useHelper';
+import useEnterpriseEdition from '../utils/hooks/useEnterpriseEdition';
+
+type FullError = {
+  message?: string;
+  extensions?: {
+    code?: string;
+    data?: {
+      http_status?: number;
+      genre?: string;
+      entityIds?: string[];
+    };
+    stacktrace?: string[];
+  };
+};
 
 const Message = () => {
   const [open, setOpen] = useState(false);
   const [type, setType] = useState('message');
   const [text, setText] = useState<string | ReactElement>('');
+  const [_, setError] = useState(false);
+  const [fullError, setFullError] = useState<FullError | null>(null);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const isEnterpriseEdition = useEnterpriseEdition();
+
+  let isRequestAccessFeatureEnabled = false;
+  try {
+    const { isRequestAccessEnabled, isFeatureEnable } = useHelper();
+    isRequestAccessFeatureEnabled = isFeatureEnable('ORGA_SHARING_REQUEST_FF') && isRequestAccessEnabled() && isEnterpriseEdition;
+  } catch (e) {
+    // When called being unauthenticated there is no useAuth()
+  }
 
   const { t_i18n } = useFormatter();
   const theme = useTheme<Theme>();
@@ -19,13 +49,16 @@ const Message = () => {
     const subscription = MESSAGING$.messages.subscribe({
       next: (messages) => {
         if (messages && messages.length > 0 && messages[0]) {
-          const firstMessage = messages[0] as { text: string | ReactElement, type: string };
+          const firstMessage = messages[0] as { text: string | ReactElement, type: string, fullError: FullError | null };
           const translatedText = firstMessage.text instanceof String
             ? t_i18n(firstMessage.text)
             : firstMessage.text;
+          const firstMessageError = firstMessage.type === 'error';
           setOpen(true);
-          setType(firstMessage.type);
+          setFullError(firstMessage.fullError || null);
+          setError(firstMessageError);
           setText(translatedText);
+          setType(firstMessage.type);
         }
       },
     });
@@ -39,7 +72,48 @@ const Message = () => {
     setOpen(false);
   };
 
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+  };
+
+  const handleDialogOpen = () => {
+    if (!isEnterpriseEdition) {
+      setText(t_i18n('You need to enable EE License to use this feature'));
+      return;
+    }
+    setDialogOpen(true);
+    setOpen(false);
+  };
+
+  const entityIds = fullError?.extensions?.data?.entityIds || [];
+
   const displayAlert = () => {
+    if (fullError?.extensions?.code === 'ACCESS_REQUIRED') {
+      return (
+        <Alert
+          severity="error"
+          style={{ display: 'flex', alignItems: 'center' }}
+          onClose={() => handleCloseMessage()}
+        >
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            {text}
+            <Formik initialValues={{}} onSubmit={() => {}}>
+              <Form>
+                <Button
+                  variant="outlined"
+                  size="small"
+                  color="error"
+                  sx={{ marginLeft: 2 }}
+                  onClick={handleDialogOpen}
+                >
+                  Request Access
+                </Button>
+              </Form>
+            </Formik>
+          </div>
+        </Alert>
+      );
+    }
     switch (type) {
       case 'error':
         return <Alert severity="error" onClose={() => handleCloseMessage()}>
@@ -65,14 +139,23 @@ const Message = () => {
   };
 
   return (
-    <Snackbar
-      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-      open={open}
-      onClose={(_, reason) => handleCloseMessage(reason)}
-      autoHideDuration={type === 'message' ? 4000 : 8000}
-    >
-      {displayAlert()}
-    </Snackbar>
+    <>
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open={open}
+        onClose={(__, reason) => handleCloseMessage(reason)}
+        autoHideDuration={type === 'message' ? 4000 : 8000}
+      >
+        {displayAlert()}
+      </Snackbar>
+      {isRequestAccessFeatureEnabled && (
+      <RequestAccessDialog
+        open={dialogOpen}
+        onClose={handleDialogClose}
+        entitiesIds={entityIds}
+      />
+      )}
+    </>
   );
 };
 
