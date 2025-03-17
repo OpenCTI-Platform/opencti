0fb8fb5c979dcab78aaaccf446ae0d87053a73c0
Valentin Bouzin <valentin.bouzin@filigran.io>
2025-03-17 16:51:51 +0100

    saved filter button and create mutation

---
+++ opencti-platform/opencti-front/src/components/saved_filters/SavedFilterDialog.tsx
@@ -0,0 +1,91 @@
+import React, { useState, ChangeEvent } from 'react';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import { useFormatter } from 'src/components/i18n';
+import TextField from '@mui/material/TextField';
+import { graphql } from 'react-relay';
+import { useDataTableContext } from 'src/components/dataGrid/components/DataTableContext';
+import useApiMutation from '../../utils/hooks/useApiMutation';
+
+const savedFilterDialogCreateMutation = graphql`
+  mutation SavedFilterDialogCreateMutation($input: SavedFilterAddInput!) {
+    savedFilterAdd(input: $input) {
+      id
+      name
+      filters
+      scope
+    }
+  }
+`;
+
+type SavedFilterDialogProps = {
+  onClose: () => void;
+  isOpen: boolean;
+};
+
+const SavedFilterDialog = ({ isOpen, onClose }: SavedFilterDialogProps) => {
+  const { t_i18n } = useFormatter();
+
+  const {
+    useDataTablePaginationLocalStorage: {
+      localStorageKey,
+      viewStorage: { filters },
+    },
+  } = useDataTableContext();
+
+  const [filterName, setFilterName] = useState<string>();
+
+  const [commit] = useApiMutation(savedFilterDialogCreateMutation);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value === '') setFilterName(undefined);
+    else setFilterName(event.target.value);
+  };
+  const handleSubmitSaveFilter = () => {
+    commit({
+      variables: {
+        input: {
+          name: filterName,
+          filters: JSON.stringify(filters),
+          scope: localStorageKey,
+        },
+      },
+      onCompleted: () => {
+        onClose();
+      },
+      onError: () => {
+        onClose();
+      },
+    });
+  };
+
+  return (
+    <Dialog
+      open={isOpen}
+      PaperProps={{ elevation: 1 }}
+      onClose={onClose}
+      fullWidth
+      maxWidth="xs"
+    >
+      <DialogTitle>{t_i18n('Save Filter')}</DialogTitle>
+      <DialogContent>
+        <TextField
+          label={t_i18n('Name')}
+          placeholder={t_i18n('my saved filter')}
+          fullWidth
+          value={filterName}
+          onChange={handleChange}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>{t_i18n('Cancel')}</Button>
+        <Button onClick={handleSubmitSaveFilter} disabled={!filterName} color="secondary">{t_i18n('Save')}</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default SavedFilterDialog;
