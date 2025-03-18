b33a463d2b085effdac4f073fc89b516dbce3baa
Celine Sebe <celine.sebe@filigran.io>
2025-03-18 17:38:20 +0100

    saved filters query

--- opencti-platform/opencti-front/src/components/saved_filters/SavedFiltersInput.tsx
+++ opencti-platform/opencti-front/src/components/saved_filters/SavedFiltersInput.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
+import { graphql } from 'react-relay';
 
+const savedFilterFragment = graphql`
+  query SavedFiltersInputQuery {
+    savedFilters {
+      edges {
+        node {
+          id
+          standard_id
+          entity_type
+          parent_types
+          name
+          filters
+          scope
+        }
+      }
+    }
+  }
+`;
+console.log('SAVED FILTER', savedFilterFragment);
 const SavedFiltersInput = () => {
   return (
     <>
