5149a00b1136b8d14e12626ed10f98c9dd500daf
marie flores <marie.flores@filigran.io>
2025-03-21 17:10:47 +0100

    [backend] WIP: implement Malware stix 2.0 converter

---
+++ opencti-platform/opencti-graphql/src/types/stix-2-0-sdo.d.ts
@@ -0,0 +1,18 @@
+import type { StixDomainObject2 } from './stix-2-0-common';
+import type { StixDate, StixId, StixKillChainPhase } from './stix-common';
+
+export interface StixMalware2 extends StixDomainObject2 {
+  name: string; // optional
+  description: string; // optional
+  malware_types: Array<string>; // optional
+  is_family: boolean;
+  aliases: Array<string>; // optional
+  kill_chain_phases: Array<StixKillChainPhase>; // optional
+  first_seen: StixDate; // optional
+  last_seen: StixDate; // optional
+  architecture_execution_envs: Array<string>; // optional
+  implementation_languages: Array<string>; // optional
+  capabilities: Array<string>; // optional
+  operating_system_refs: Array<StixId>; // optional
+  sample_refs: Array<StixId>; // optional
+}
