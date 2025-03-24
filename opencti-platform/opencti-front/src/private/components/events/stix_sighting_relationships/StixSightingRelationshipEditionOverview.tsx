0fcd03ac2b4d8586242cee31672aea78e26eeb96
BocognanoSarah <sarah.bocognano@filigran.io>
2025-03-24 10:17:45 +0100

    [frontend] Delete button bad margin in Relationship edition form (#10268)

--- opencti-platform/opencti-front/src/private/components/events/stix_sighting_relationships/StixSightingRelationshipEditionOverview.tsx
+++ opencti-platform/opencti-front/src/private/components/events/stix_sighting_relationships/StixSightingRelationshipEditionOverview.tsx
@@ -432,16 +432,6 @@ const StixSightingRelationshipEditionOverviewComponent: FunctionComponent<Omit<S
                 width: '100%',
               }}
               >
-                {typeof handleDelete === 'function' && (
-                  <Button
-                    variant="contained"
-                    onClick={() => handleDelete()}
-                    classes={{ root: classes.button }}
-                    disabled={inferred}
-                  >
-                    {t_i18n('Delete')}
-                  </Button>
-                )}
                 {enableReferences && (
                   <CommitMessage
                     submitForm={submitForm}
@@ -457,6 +447,16 @@ const StixSightingRelationshipEditionOverviewComponent: FunctionComponent<Omit<S
             </Form>
           )}
         </Formik>
+        {typeof handleDelete === 'function' && (
+          <Button
+            variant="contained"
+            onClick={() => handleDelete()}
+            classes={{ root: classes.button }}
+            disabled={inferred}
+          >
+            {t_i18n('Delete')}
+          </Button>
+        )}
       </div>
     </>
   );
