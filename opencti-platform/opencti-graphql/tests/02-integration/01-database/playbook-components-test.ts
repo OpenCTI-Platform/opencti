c0e3a639dbca145488622f558870db25a1bb832f
marie flores <marie.flores@filigran.io>
2025-03-26 16:00:06 +0100

    [backend] wip: test

--- opencti-platform/opencti-graphql/tests/02-integration/01-database/playbook-components-test.ts
+++ opencti-platform/opencti-graphql/tests/02-integration/01-database/playbook-components-test.ts
@@ -5,7 +5,42 @@ import type { BasicStoreEntityOrganization } from '../../../src/modules/organiza
 import { enableCEAndUnSetOrganization, enableEEAndSetOrganization } from '../../utils/testQueryHelper';
 import { PLATFORM_ORGANIZATION, TEST_ORGANIZATION } from '../../utils/testQuery';
 import { getOrganizationEntity } from '../../utils/domainQueryHelper';
-import { bundle } from './playbook-components/playbook-components-bundles';
+
+const bundle = {
+  id: '411628bf-745b-43f6-8194-cbe441edecfd',
+  objects: [
+    {
+      confidence: 100,
+      created: '2025-03-25T09:59:10.000Z',
+      description: 'fff',
+      extensions: {
+        'extension-definition--ea279b3e-5c71-4632-ac08-831c66a786ba': {
+          created_at: '2025-03-25T09:59:17.024Z',
+          creator_ids: [
+            '88ec0c6a-13ce-5e39-b486-354fe4a7084f'
+          ],
+          extension_type: 'property-extension',
+          id: '82e80255-9793-4283-b34b-872b30f23f57',
+          type: 'Report',
+          updated_at: '2025-03-25T09:59:44.832Z',
+          workflow_id: 'a4b90e6f-06ae-461a-8dac-666cdb4a5ae7'
+        }
+      },
+      id: 'report--b70b1781-f963-5790-9fe7-55aec16c05f4',
+      lang: 'en',
+      modified: '2025-03-25T09:59:44.832Z',
+      name: 'report 28',
+      object_refs: [
+        'campaign--fdcacc8e-de4d-5a13-8886-401d363664fd'
+      ],
+      published: '2025-03-25T09:59:10.000Z',
+      spec_version: '2.1',
+      type: 'report'
+    }
+  ],
+  spec_version: '2.1',
+  type: 'bundle'
+} as unknown as StixBundle;
 
 describe('playbook sharing component', () => {
   let externalOrganizationEntity: BasicStoreEntityOrganization;
@@ -52,9 +87,7 @@ describe('playbook sharing component', () => {
                 '88ec0c6a-13ce-5e39-b486-354fe4a7084f'
               ],
               extension_type: 'property-extension',
-              granted_refs: [
-                externalOrganizationEntity.standard_id
-              ],
+              granted_refs: [externalOrganizationEntity.standard_id],
               id: '82e80255-9793-4283-b34b-872b30f23f57',
               type: 'Report',
               updated_at: '2025-03-25T09:59:44.832Z',
@@ -71,11 +104,27 @@ describe('playbook sharing component', () => {
           published: '2025-03-25T09:59:10.000Z',
           spec_version: '2.1',
           type: 'report'
+        },
+        {
+          id: 'campaign--fdcacc8e-de4d-5a13-8886-401d363664fd',
+          spec_version: '2.1',
+          revoked: false,
+          confidence: 100,
+          created: '2015-05-15T09:12:16.432Z',
+          modified: '2025-03-26T10:00:10.363Z',
+          name: 'admin@338',
+          description: 'description',
+          first_seen: '2008-01-07T00:00:00.000Z',
+          x_opencti_id: '683e5146-1022-4f69-9fbf-f12101934573',
+          x_opencti_type: 'Campaign',
+          type: 'campaign',
+          x_opencti_granted_refs: [externalOrganizationEntity.standard_id]
         }
       ],
       spec_version: '2.1',
       type: 'bundle'
     } as unknown as StixBundle;
+
     const result = await PLAYBOOK_SHARING_COMPONENT.executor({
       dataInstanceId,
       eventId: '',
