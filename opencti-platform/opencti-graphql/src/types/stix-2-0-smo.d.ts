d2e0aded6084a8a01f4ff8b15a94b59cce930c68
marie flores <marie.flores@filigran.io>
2025-03-28 18:10:54 +0100

    [backend] TDD step 2 : test green

--- opencti-platform/opencti-graphql/src/types/stix-2-0-smo.d.ts
+++ opencti-platform/opencti-graphql/src/types/stix-2-0-smo.d.ts
@@ -3,5 +3,12 @@ export interface StixInternalExternalReference {
   source_name: string;
   description: string;
   url: string;
-  external_id: string;
+  // external_id: string; ?
+  // hashes: e.hashes, ?
+}
+
+export interface StixInternalKillChainPhase {
+  kill_chain_name: string;
+  phase_name: string;
+  x_opencti_order: number,
 }
