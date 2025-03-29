9f1d1f8423eba91f2861d77cd5513c58339ed8b2
Julien Richard <julien.richard@filigran.io>
2025-03-29 02:16:03 +0100

    [frontend] more for json feed management

--- opencti-platform/opencti-graphql/src/modules/internal/jsonMapper/jsonMapper-domain.ts
+++ opencti-platform/opencti-graphql/src/modules/internal/jsonMapper/jsonMapper-domain.ts
@@ -1,12 +1,18 @@
 import type { AuthContext, AuthUser } from '../../../types/user';
-import { listEntitiesPaginated, storeLoadById } from '../../../database/middleware-loader';
+import { storeLoadById } from '../../../database/middleware-loader';
 import { type BasicStoreEntityJsonMapper, ENTITY_TYPE_JSON_MAPPER } from './jsonMapper-types';
 import type { QueryJsonMappersArgs } from '../../../generated/graphql';
+import { testIngestion } from '../../../manager/ingestionManager';
 
 export const findById = async (context: AuthContext, user: AuthUser, csvMapperId: string) => {
   return storeLoadById<BasicStoreEntityJsonMapper>(context, user, csvMapperId, ENTITY_TYPE_JSON_MAPPER);
 };
 
-export const findAll = (context: AuthContext, user: AuthUser, opts: QueryJsonMappersArgs) => {
-  return listEntitiesPaginated<BasicStoreEntityJsonMapper>(context, user, [ENTITY_TYPE_JSON_MAPPER], opts);
+export const findAll = (_context: AuthContext, _user: AuthUser, _opts: QueryJsonMappersArgs) => {
+  // return listEntitiesPaginated<BasicStoreEntityJsonMapper>(context, user, [ENTITY_TYPE_JSON_MAPPER], opts);
+  return {
+    edges: [{
+      node: testIngestion
+    }]
+  };
 };
