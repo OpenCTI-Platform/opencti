afcd8534d50c626c2e6a4196f07051356233d9f8
Gwendoline-FAVRE-FELIX <gwendoline.favre-felix@filigran.io>
2025-04-04 16:00:22 +0200

    [frontend] In Mass Op to Indicator: Fix toggle display for detection field (#10466)
    Co-authored-by: Laurent Bonnet <laurent.bonnet@filigran.io>

--- opencti-platform/opencti-front/src/private/components/data/playbooks/PlaybookAddComponents.jsx
+++ opencti-platform/opencti-front/src/private/components/data/playbooks/PlaybookAddComponents.jsx
@@ -369,6 +369,7 @@ const PlaybookAddComponentsContent = ({
             onChange={(_, value) => handleChangeActionInput(i, 'value', [
               { label: value, value, patch_value: value },
             ])}
+            initialValue={false} // to force onChange call on mount, as the switch starts with a correct value "false"
           />
         );
       case 'severity':
