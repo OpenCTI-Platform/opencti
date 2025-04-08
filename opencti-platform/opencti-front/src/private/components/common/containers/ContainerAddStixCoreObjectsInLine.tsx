0871e2cda420e168309f6eb0eead6f9aaaca4f27
Landry Trebon <landry.trebon@filigran.io>
2025-04-08 15:18:58 +0200

    [frontend] fix infinite render

--- opencti-platform/opencti-front/src/private/components/common/containers/ContainerAddStixCoreObjectsInLine.tsx
+++ opencti-platform/opencti-front/src/private/components/common/containers/ContainerAddStixCoreObjectsInLine.tsx
@@ -1,5 +1,5 @@
 import { Button, IconButton, Tooltip, Typography } from '@mui/material';
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent, Suspense, useState } from 'react';
 import { PreloadedQuery, usePreloadedQuery } from 'react-relay';
 import { Add } from '@mui/icons-material';
 import { useTheme } from '@mui/styles';
@@ -326,19 +326,21 @@ const ContainerAddStixCoreObjectsInLine: FunctionComponent<ContainerAddStixCoreO
         entityTypes={targetStixCoreObjectTypes}
       >
         {(containerRef && queryRef) && (
-          <ContainerAddStixCreObjectsInLineLoader
-            queryRef={queryRef}
-            containerId={containerId}
-            buildColumns={buildColumns}
-            linesPaginationOptions={linesPaginationOptions}
-            knowledgeGraph={knowledgeGraph}
-            selectedElements={selectedElements}
-            handleSelect={handleSelect}
-            handleDeselect={handleDeselect}
-            helpers={helpers}
-            containerRef={containerRef}
-            enableReferences={enableReferences}
-          />
+          <Suspense>
+            <ContainerAddStixCreObjectsInLineLoader
+              queryRef={queryRef}
+              containerId={containerId}
+              buildColumns={buildColumns}
+              linesPaginationOptions={linesPaginationOptions}
+              knowledgeGraph={knowledgeGraph}
+              selectedElements={selectedElements}
+              handleSelect={handleSelect}
+              handleDeselect={handleDeselect}
+              helpers={helpers}
+              containerRef={containerRef}
+              enableReferences={enableReferences}
+            />
+          </Suspense>
         )}
       </ListLines>
     </Drawer>
