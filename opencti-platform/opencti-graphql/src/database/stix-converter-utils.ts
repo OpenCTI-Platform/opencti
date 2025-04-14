ae5d02cb1a1d8f73230d08ac7a3151481d1d1bfc
marie flores <marie.flores@filigran.io>
2025-04-14 10:21:04 +0200

    [backend] type renaming

--- opencti-platform/opencti-graphql/src/database/stix-converter-utils.ts
+++ opencti-platform/opencti-graphql/src/database/stix-converter-utils.ts
@@ -1,5 +1,6 @@
 import { UnsupportedError } from '../config/errors';
 import type * as S from '../types/stix-2-1-common';
+import type * as S2 from '../types/stix-2-0-common';
 import { FROM_START, FROM_START_STR, UNTIL_END, UNTIL_END_STR } from '../utils/format';
 import { objects } from '../schema/stixRefRelationship';
 import { isEmptyField } from './utils';
@@ -9,7 +10,7 @@ export const assertType = (type: string, instanceType: string) => {
     throw UnsupportedError('Incompatible type', { instanceType, type });
   }
 };
-export const convertToStixDate = (date: Date | string | undefined): S.StixDate => {
+export const convertToStixDate = (date: Date | string | undefined): S.StixDate | S2.StixDate => {
   if (date === undefined) {
     return undefined;
   }
