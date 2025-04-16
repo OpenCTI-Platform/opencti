bd3b376bf95459ee4aa138fec2d9b8b829d50b6f
marie flores <marie.flores@filigran.io>
2025-04-16 10:57:56 +0200

    [backend] wip: tests

---
+++ opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/notifications-test.ts
@@ -0,0 +1,164 @@
+import { beforeAll, describe, expect, it } from 'vitest';
+import gql from 'graphql-tag';
+import { ADMIN_USER, getUserIdByEmail, testContext, USER_EDITOR } from '../../utils/testQuery';
+import { addNotification } from '../../../src/modules/notification/notification-domain';
+import type { AuthUser } from '../../../src/types/user';
+import { adminQueryWithSuccess, queryAsUserWithSuccess } from '../../utils/testQueryHelper';
+
+const READ_NOTIFICATION_QUERY = gql`
+  query notification($id: String!) {
+    notification(id: $id) {
+      id
+      name
+    }
+  }
+`;
+const DELETE_NOTIFICATION_QUERY = gql`
+  mutation NotificationDelete($id: ID!) {
+    notificationDelete(id: $id)
+  }
+`;
+const MARK_READ_NOTIFICATION_QUERY = gql`
+  mutation NotificationMarkRead($id: ID!, $read: Boolean!) {
+    notificationMarkRead(id: $id, read: $read) {
+      is_read
+    }
+  }
+`;
+
+describe('Notifications resolver restriction behavior', () => {
+  let adminNotificationId: string;
+  let userNotificationId: string;
+  beforeAll(async () => {
+    // Admin creates 1 notification
+    const adminNotificationInput = {
+      is_read: false,
+      name: 'Admin notification',
+      notification_type: 'live',
+      notification_content: [
+        {
+          title: 'report same org intersec',
+          events: [
+            {
+              operation: 'update',
+              message: '[report] report same org intersec - `admin` replaces `description updated` in `Description`',
+              instance_id: 'report--b07ba95d-8a26-59c9-91a0-54a0829eae09'
+            }
+          ]
+        }
+      ],
+      user_id: ADMIN_USER.id
+    };
+
+    const adminNotification = await addNotification(testContext, ADMIN_USER, adminNotificationInput);
+    adminNotificationId = adminNotification.id;
+
+    // User creates 1 notification
+    const userID = await getUserIdByEmail(USER_EDITOR.email);
+    const userNotificationInput = {
+      is_read: false,
+      name: 'User notification',
+      notification_type: 'live',
+      notification_content: [
+        {
+          title: 'report same org intersec',
+          events: [
+            {
+              operation: 'update',
+              message: '[report] report same org intersec - `admin` replaces `description updated` in `Description`',
+              instance_id: 'report--b07ba95d-8a26-59c9-91a0-54a0829eae09'
+            }
+          ]
+        }
+      ],
+      user_id: userID
+    };
+    const userNotification = await addNotification(testContext, USER_EDITOR as unknown as AuthUser, userNotificationInput);
+    userNotificationId = userNotification.id;
+  });
+
+  it('Admin user should get its own notification', async () => {
+    const notification = await adminQueryWithSuccess({
+      query: READ_NOTIFICATION_QUERY,
+      variables: {
+        id: adminNotificationId
+      }
+    });
+    expect(notification.id).toEqual(adminNotificationId);
+  });
+  it.todo('Admin user should get user notification', async () => {
+    const notification = await adminQueryWithSuccess({
+      query: READ_NOTIFICATION_QUERY,
+      variables: {
+        id: userNotificationId
+      }
+    });
+    expect(notification.id).toEqual(userNotificationId);
+  });
+  it.todo('User should get its own notification', async () => {
+    const notification = await queryAsUserWithSuccess(USER_EDITOR.client, {
+      query: READ_NOTIFICATION_QUERY,
+      variables: {
+        id: userNotificationId
+      }
+    });
+    expect(notification.id).toEqual(userNotificationId);
+  });
+  it.todo('User should not get Admin notification', async () => {
+    const notification = await queryAsUserWithSuccess(USER_EDITOR.client, {
+      query: READ_NOTIFICATION_QUERY,
+      variables: {
+        id: adminNotificationId
+      }
+    });
+    expect(notification.id).toBeNull();
+  });
+  it.todo('User should not update Admin notification', async () => {
+    const result = await queryAsUserWithSuccess(USER_EDITOR.client, {
+      query: MARK_READ_NOTIFICATION_QUERY,
+      variables: {
+        id: adminNotificationId
+      }
+    });
+    expect(result).toBeNull();
+  });
+  it.todo('User should not delete Admin notification', async () => {
+    const result = await queryAsUserWithSuccess(USER_EDITOR.client, {
+      query: DELETE_NOTIFICATION_QUERY,
+      variables: {
+        id: adminNotificationId
+      }
+    });
+    expect(result).toBeNull();
+  });
+  it.todo('Admin should delete User notification', async () => {
+    await adminQueryWithSuccess({
+      query: DELETE_NOTIFICATION_QUERY,
+      variables: {
+        id: userNotificationId
+      }
+    });
+    const notification = await adminQueryWithSuccess({
+      query: READ_NOTIFICATION_QUERY,
+      variables: {
+        id: userNotificationId
+      }
+    });
+    expect(notification.id).toBeNull();
+  });
+  it.todo('Admin should delete its own notification', async () => {
+    await adminQueryWithSuccess({
+      query: DELETE_NOTIFICATION_QUERY,
+      variables: {
+        id: adminNotificationId
+      }
+    });
+    const notification = await adminQueryWithSuccess({
+      query: READ_NOTIFICATION_QUERY,
+      variables: {
+        id: userNotificationId
+      }
+    });
+    expect(notification.id).toBeNull();
+  });
+});
