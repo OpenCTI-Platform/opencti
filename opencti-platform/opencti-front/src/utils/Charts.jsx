6de9668efd0d4bcbad7d2e42f2996946e839c620
Sarah Bocognano <98532359+SarahBocognano@users.noreply.github.com>
2025-04-16 10:13:49 +0200

    Update opencti-platform/opencti-front/src/utils/Charts.jsx
    Co-authored-by: Landry Trebon <33682259+lndrtrbn@users.noreply.github.com>

--- opencti-platform/opencti-front/src/utils/Charts.jsx
+++ opencti-platform/opencti-front/src/utils/Charts.jsx
@@ -31,7 +31,7 @@ const isColorCloseToWhite = (hex, threshold = 0.9) => {
   const g = parseInt(c.substr(2, 2), 16);
   const b = parseInt(c.substr(4, 2), 16);
   const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
-  return luminance > 0.9; // filter on too white colors
+  return luminance >= threshold; // filter on too white colors
 };
 
 const toolbarOptions = {
