597284c495c92da4cfc1bddb2394cfc9b7fcca92
Valentin Bouzin <valentin.bouzin@filigran.io>
2025-04-17 10:03:59 +0200

    [frontend] useDatatable called on datatatble body  (#10252)

--- opencti-platform/opencti-front/src/components/dataGrid/dataTableTypes.ts
+++ opencti-platform/opencti-front/src/components/dataGrid/dataTableTypes.ts
@@ -3,6 +3,8 @@ import type { Dispatch, MutableRefObject, ReactNode, SetStateAction } from 'reac
 import React from 'react';
 import { GraphQLTaggedNode } from 'react-relay';
 import { PopoverProps } from '@mui/material/Popover/Popover';
+import { UsePreloadedPaginationFragment } from 'src/utils/hooks/usePreloadedPaginationFragment';
+import { OperationType } from 'relay-runtime';
 import type { LocalStorage } from '../../utils/hooks/useLocalStorageModel';
 import { NumberOfElements, PaginationLocalStorage, UseLocalStorageHelpers } from '../../utils/hooks/useLocalStorage';
 import { FilterGroup } from '../../utils/filters/filtersHelpers-types';
@@ -46,7 +48,7 @@ export interface DataTableContextProps {
   resolvePath: (data: any) => any
   redirectionModeEnabled?: boolean
   useLineData: DataTableProps['useLineData']
-  useDataTable: ReturnType<DataTableProps['useDataTable']>
+  dataQueryArgs: UsePreloadedPaginationFragment<OperationType>,
   useDataCellHelpers: DataTableProps['useDataCellHelpers']
   useDataTableToggle: ReturnType<DataTableProps['useDataTableToggle']>
   useComputeLink: (entity: any) => string | undefined
