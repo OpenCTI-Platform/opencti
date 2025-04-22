59a90c145e49b536685b820a7f9d67e49d45cab5
Landry Trebon <landry.trebon@filigran.io>
2025-04-22 14:51:16 +0200

    [backend] Init PIR manager listening to live stream

--- opencti-platform/opencti-graphql/src/modules/pir/pir.fake.ts
+++ opencti-platform/opencti-graphql/src/modules/pir/pir.fake.ts
@@ -1,11 +1,19 @@
-export interface PIR {
+interface PIRCriterion {
+  standard_id: string
+  // weight: number
+}
+
+interface PIR {
   id: string
   name: string
-  // TODO PIR: add definition of things to listen
+  criteria: PIRCriterion[]
 }
 
 export const FAKE_PIR: PIR = {
   id: '2b271fe3-8fdb-4df4-9b1f-bc55202dfa23',
-  name: 'PIR about Energy sector in France'
-  // TODO PIR: listen to sector Energy and country France
+  name: 'PIR about Energy sector in France',
+  criteria: [
+    { standard_id: 'location--b8d0549f-de06-5ebd-a6e9-d31a581dba5d' },
+    { standard_id: 'identity--166544e2-ba1f-5a6c-89cf-a63d0c01e91c' },
+  ]
 };
