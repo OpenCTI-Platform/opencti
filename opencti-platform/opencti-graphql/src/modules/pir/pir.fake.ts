b162c3bcb9fa41d114ae84409abe05d38cd508b8
Landry Trebon <landry.trebon@filigran.io>
2025-04-22 12:09:52 +0200

    [backend] wip init pir manager

---
+++ opencti-platform/opencti-graphql/src/modules/pir/pir.fake.ts
@@ -0,0 +1,11 @@
+export interface PIR {
+  id: string
+  name: string
+  // TODO PIR: add definition of things to listen
+}
+
+export const FAKE_PIR: PIR = {
+  id: '2b271fe3-8fdb-4df4-9b1f-bc55202dfa23',
+  name: 'PIR about Energy sector in France'
+  // TODO PIR: listen to sector Energy and country France
+};
