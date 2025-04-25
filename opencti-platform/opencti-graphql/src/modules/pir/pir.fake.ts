b3ebf35879120a5567e8fb55efc3a6418d32e75f
Landry Trebon <landry.trebon@filigran.io>
2025-04-25 11:35:42 +0200

    [backend] separate criteria

--- opencti-platform/opencti-graphql/src/modules/pir/pir.fake.ts
+++ opencti-platform/opencti-graphql/src/modules/pir/pir.fake.ts
@@ -17,9 +17,80 @@ export interface PIR {
   pirFilters: FilterGroup
 }
 
+const LANDRY_CRITERIA: PIRCriterion[] = [
+  // Targets France
+  {
+    id: '1795b286-bbbb-4516-a330-a1c525efb947',
+    weight: 2,
+    filters: {
+      mode: FilterMode.And,
+      filterGroups: [],
+      filters: [
+        { key: ['entity_type'], values: ['targets'] },
+        { key: ['toId'], values: ['7ca7cad1-2618-489a-a74c-9a8e321fd963'] },
+      ]
+    }
+  },
+  // Targets Spain
+  {
+    id: 'b03f5d74-d80a-4628-bf18-5266ed2e0291',
+    weight: 1,
+    filters: {
+      mode: FilterMode.And,
+      filterGroups: [],
+      filters: [
+        { key: ['entity_type'], values: ['targets'] },
+        { key: ['toId'], values: ['6ce17f4b-8172-41e7-961f-ab071ca6dc30'] },
+      ]
+    }
+  },
+  // Targets Germany
+  {
+    id: 'b03f5d74-d80a-4628-bf18-5266ed2e0291',
+    weight: 1,
+    filters: {
+      mode: FilterMode.And,
+      filterGroups: [],
+      filters: [
+        { key: ['entity_type'], values: ['targets'] },
+        { key: ['toId'], values: ['9d49262b-3d1f-42cf-ae47-67e72212d80b'] },
+      ]
+    }
+  }
+];
+
+const CATHIA_CRITERIA: PIRCriterion[] = [
+  // Targets France
+  {
+    id: '1795b286-bbbb-4516-a330-a1c525efb947',
+    weight: 2,
+    filters: {
+      mode: FilterMode.And,
+      filterGroups: [],
+      filters: [
+        { key: ['entity_type'], values: ['targets'] },
+        { key: ['toId'], values: ['e783195e-6445-4b45-9076-b489a7f97e59'] },
+      ]
+    }
+  },
+  // Targets Western Europe
+  {
+    id: 'b03f5d74-d80a-4628-bf18-5266ed2e0291',
+    weight: 1,
+    filters: {
+      mode: FilterMode.And,
+      filterGroups: [],
+      filters: [
+        { key: ['entity_type'], values: ['targets'] },
+        { key: ['toId'], values: ['db3ae45b-ed00-4417-8835-312693eb940f'] },
+      ]
+    }
+  }
+];
+
 // TODO PIR !!! id should be in a trigger filter to update the Resolved filters cache
 export const FAKE_PIR: PIR = {
-  id: '028ecfc0-d4d0-4f1c-a6b2-91c446eeb7c2', // existing id to be able to add meta rel involving the PIR
+  id: '23cc1789-4833-4b13-b784-332f5217dcf7', // existing id to be able to add meta rel involving the PIR
   name: 'PIR about Energy sector in France',
   pirFilters: {
     mode: FilterMode.And,
@@ -31,42 +102,5 @@ export const FAKE_PIR: PIR = {
       mode: FilterMode.Or
     }]
   },
-  pirCriteria: [
-    {
-      id: '1795b286-bbbb-4516-a330-a1c525efb947',
-      weight: 2,
-      filters: {
-        mode: FilterMode.And,
-        filterGroups: [],
-        filters: [
-          { key: ['entity_type'], values: ['targets'] },
-          { key: ['toId'], values: ['e783195e-6445-4b45-9076-b489a7f97e59'] }, // CATHIA France
-        ]
-      }
-    },
-    {
-      id: '509ba0c6-4629-4fc2-bde9-12a110ea051e',
-      weight: 1,
-      filters: {
-        mode: FilterMode.And,
-        filterGroups: [],
-        filters: [
-          { key: ['entity_type'], values: ['targets'] },
-          { key: ['toId'], values: ['6bcb82c5-e440-4589-976e-5e28a99db3b3'] },
-        ]
-      }
-    },
-    {
-      id: 'b03f5d74-d80a-4628-bf18-5266ed2e0291',
-      weight: 1,
-      filters: {
-        mode: FilterMode.And,
-        filterGroups: [],
-        filters: [
-          { key: ['entity_type'], values: ['targets'] },
-          { key: ['toId'], values: ['db3ae45b-ed00-4417-8835-312693eb940f'] }, // Cathia Western Europe
-        ]
-      }
-    }
-  ]
+  pirCriteria: [...LANDRY_CRITERIA, ...CATHIA_CRITERIA]
 };
