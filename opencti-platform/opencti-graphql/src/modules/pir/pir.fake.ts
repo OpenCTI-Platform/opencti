7a5964c6e5aa097ddc92825d0fb517213d2b01bc
Cathia Archidoit <cathia.archidoit@filigran.io>
2025-04-25 14:54:50 +0200

    [backend] define pir_explanations in meta rel

--- opencti-platform/opencti-graphql/src/modules/pir/pir.fake.ts
+++ opencti-platform/opencti-graphql/src/modules/pir/pir.fake.ts
@@ -73,7 +73,7 @@ const CATHIA_CRITERIA: PIRCriterion[] = [
       ]
     }
   },
-  // Targets Western Europe
+  // Targets Europe
   {
     id: 'b03f5d74-d80a-4628-bf18-5266ed2e0291',
     weight: 1,
@@ -90,7 +90,7 @@ const CATHIA_CRITERIA: PIRCriterion[] = [
 
 // TODO PIR !!! id should be in a trigger filter to update the Resolved filters cache
 export const FAKE_PIR: PIR = {
-  id: '23cc1789-4833-4b13-b784-332f5217dcf7', // existing id to be able to add meta rel involving the PIR
+  id: '04aae53f-9991-48cf-8f94-5b20256a7546', // existing id to be able to add meta rel involving the PIR
   name: 'PIR about Energy sector in France',
   pirFilters: {
     mode: FilterMode.And,
