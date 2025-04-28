0e9c5846573d954246e41398d0b96b2481cf666c
BocognanoSarah <sarah.bocognano@filigran.io>
2025-04-28 16:00:50 +0200

    add notes navigation

--- opencti-platform/opencti-front/tests_e2e/model/form/noteForm.pageModel.ts
+++ opencti-platform/opencti-front/tests_e2e/model/form/noteForm.pageModel.ts
@@ -4,8 +4,8 @@ import TextFieldPageModel from '../field/TextField.pageModel';
 export default class NoteFormPage {
   private readonly formLocator = this.page.getByRole('heading', { name: 'Create a note' }).locator('../..');
 
-  abstractField = new TextFieldPageModel(this.page, 'Abstract', 'text', this.formLocator);
-  contentField = new TextFieldPageModel(this.page, 'Content', 'text', this.formLocator);
+  abstractField = new TextFieldPageModel(this.page, 'Abstract', 'text-no-label', this.formLocator);
+  contentField = new TextFieldPageModel(this.page, 'Content', 'text-area', this.formLocator);
 
   constructor(private page: Page) {}
 
