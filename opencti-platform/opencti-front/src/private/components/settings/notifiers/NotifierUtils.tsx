89f5206856c7cd6bf21901ee2f74a705c3c4e4a4
Laurent Bonnet <laurent.bonnet@filigran.io>
2025-04-28 10:01:39 +0200

    [frontend] revert unrelated changes to Notifiers

--- opencti-platform/opencti-front/src/private/components/settings/notifiers/NotifierUtils.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/notifiers/NotifierUtils.tsx
@@ -1,31 +1,7 @@
-import React, { ReactNode } from 'react';
-import Grid from '@mui/material/Grid';
 import { UiSchema } from '@rjsf/utils';
-
-const ObjectFieldTemplate = ({
-  properties,
-}: { properties: { content: ReactNode }[] }) => {
-  return (
-    <>
-      <Grid container={true} spacing={2} style={{ marginTop: 0 }}>
-        {properties.map((element, index) => (
-          <Grid
-            item={true}
-            xs={12}
-            key={index}
-            style={{ marginTop: -5 }}
-          >
-            {element.content}
-          </Grid>
-        ))}
-      </Grid>
-    </>
-  );
-};
 
 // eslint-disable-next-line import/prefer-default-export
 export const uiSchema: UiSchema = {
-  'ui:ObjectFieldTemplate': ObjectFieldTemplate,
   'ui:options': {
     orderable: false,
     submitButtonOptions: {
