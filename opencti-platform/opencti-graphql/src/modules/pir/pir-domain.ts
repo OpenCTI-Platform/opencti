a25509bed3a21ee36facdfde9008c16af7380a28
Cathia Archidoit <cathia.archidoit@filigran.io>
2025-04-30 09:41:09 +0200

    [backend] past rescan at PIR creation

--- opencti-platform/opencti-graphql/src/modules/pir/pir-domain.ts
+++ opencti-platform/opencti-graphql/src/modules/pir/pir-domain.ts
@@ -1,12 +1,16 @@
 import { v4 as uuidv4 } from 'uuid';
 import type { AuthContext, AuthUser } from '../../types/user';
-import { type EntityOptions, listEntitiesPaginated, storeLoadById } from '../../database/middleware-loader';
-import { type BasicStoreEntityPIR, ENTITY_TYPE_PIR } from './pir-types';
-import type { PirAddInput } from '../../generated/graphql';
+import { type EntityOptions, listEntitiesPaginated, listRelations, storeLoadById } from '../../database/middleware-loader';
+import { type BasicStoreEntityPIR, ENTITY_TYPE_PIR, type PirDependency } from './pir-types';
+import { FilterMode, FilterOperator, type PirAddInput, type StixCoreRelationshipConnection } from '../../generated/graphql';
 import { createEntity } from '../../database/middleware';
 import { publishUserAction } from '../../listener/UserActionListener';
 import { notify } from '../../database/redis';
 import { BUS_TOPICS } from '../../config/conf';
+import { ABSTRACT_STIX_CORE_RELATIONSHIP } from '../../schema/general';
+import { SYSTEM_USER } from '../../utils/access';
+import { flagSource } from './pir-utils';
+import { addFilter } from '../../utils/filtering/filtering-utils';
 
 export const findById = (context: AuthContext, user: AuthUser, id: string) => {
   return storeLoadById<BasicStoreEntityPIR>(context, user, id, ENTITY_TYPE_PIR);
@@ -16,7 +20,10 @@ export const findAll = (context: AuthContext, user: AuthUser, opts?: EntityOptio
   return listEntitiesPaginated<BasicStoreEntityPIR>(context, user, [ENTITY_TYPE_PIR], opts);
 };
 
+const PIR_RESCAN_PERIOD = 30 * 24 * 3600 * 1000; // 1 month in milliseconds
+
 export const pirAdd = async (context: AuthContext, user: AuthUser, input: PirAddInput) => {
+  // -- create PIR --
   const finalInput = {
     ...input,
     pirCriteria: input.pirCriteria.map((c) => ({
@@ -30,13 +37,53 @@ export const pirAdd = async (context: AuthContext, user: AuthUser, input: PirAdd
     finalInput,
     ENTITY_TYPE_PIR,
   );
+  const pirId = created.id;
   await publishUserAction({
     user,
     event_type: 'mutation',
     event_scope: 'create',
     event_access: 'extended',
     message: `creates PIR \`${created.name}\``,
-    context_data: { id: created.id, entity_type: ENTITY_TYPE_PIR, input: finalInput },
+    context_data: { id: pirId, entity_type: ENTITY_TYPE_PIR, input: finalInput },
   });
+  // -- rescan past events to find pir dependencies --
+  const dependencies: Map<string, PirDependency[]> = new Map();
+  const time = new Date().getTime() - PIR_RESCAN_PERIOD;
+  const periodFilters = addFilter(undefined, 'start_time', time.toString(), FilterOperator.Gt);
+  // eslint-disable-next-line no-restricted-syntax
+  for (const criterion of finalInput.pirCriteria) {
+    const args = {
+      filters: {
+        mode: FilterMode.And,
+        filters: [],
+        filterGroups: [JSON.parse(finalInput.pirFilters), periodFilters, JSON.parse(criterion.filters)],
+      },
+    };
+    const matchingRelationships = await listRelations(context, SYSTEM_USER, ABSTRACT_STIX_CORE_RELATIONSHIP, args);
+    // eslint-disable-next-line no-restricted-syntax
+    for (const relationship of (matchingRelationships as unknown as StixCoreRelationshipConnection).edges.map((n) => n.node)) {
+      const sourceId = relationship.fromId;
+      const newDependency = {
+        relationship_id: relationship.id,
+        weight: criterion.weight,
+      };
+      const existingDependencies = dependencies.get(sourceId);
+      if (existingDependencies) {
+        dependencies.set(
+          sourceId,
+          [
+            ...existingDependencies,
+            newDependency,
+          ]
+        );
+      } else {
+        dependencies.set(sourceId, [newDependency]);
+      }
+    }
+  }
+  // -- create the meta refs between sources and the PIR --
+  await Promise.all(Array.from(dependencies.keys())
+    .map((sourceId) => flagSource(context, sourceId, pirId, dependencies.get(sourceId) as PirDependency[])));
+  // -- notify the PIR creation --
   return notify(BUS_TOPICS[ENTITY_TYPE_PIR].ADDED_TOPIC, created, user);
 };
--- opencti-platform/opencti-graphql/src/modules/pir/pir-utils.ts
+++ opencti-platform/opencti-graphql/src/modules/pir/pir-utils.ts
@@ -1,7 +1,77 @@
-import type { BasicStoreEntityPIR } from './pir-types';
+import type { BasicStoreEntityPIR, PirDependency } from './pir-types';
+import type { AuthContext } from '../../types/user';
+import { listRelationsPaginated } from '../../database/middleware-loader';
+import { SYSTEM_USER } from '../../utils/access';
+import { RELATION_IN_PIR } from '../../schema/stixRefRelationship';
+import { stixRefRelationshipEditField } from '../../domain/stixRefRelationship';
+import { FunctionalError } from '../../config/errors';
+import { ABSTRACT_STIX_CORE_OBJECT } from '../../schema/general';
+import { stixObjectOrRelationshipAddRefRelation } from '../../domain/stixObjectOrStixRelationship';
 
 export const computePirScore = (pir: BasicStoreEntityPIR, dependencies: number) => {
   const maxScore = pir.pirCriteria.reduce((acc, val) => acc + val.weight, 0);
   if (maxScore <= 0) return 0;
   return Math.round((dependencies / maxScore) * 100);
+};
+
+/**
+ * Find a meta relationship "in-pir" between an entity and a PIR and update
+ * its dependencies (matching criteria).
+ *
+ * @param context To be able to make the calls.
+ * @param sourceId ID of the source entity matching the PIR.
+ * @param pirId ID of the PIR matched by the entity.
+ * @param pirDependencies The new dependencies.
+ * @param operation The edit operation (add, replace, ...).
+ */
+export const updatePirDependencies = async (
+  context: AuthContext,
+  sourceId: string,
+  pirId: string,
+  pirDependencies: PirDependency[],
+  operation?: string, // 'add' to add a new dependency, 'replace' by default
+) => {
+  const pirMetaRels = await listRelationsPaginated(context, SYSTEM_USER, RELATION_IN_PIR, { fromId: sourceId, toId: pirId, });
+  if (pirMetaRels.edges.length !== 1) {
+    // If < 1 then the meta relationship does not exist.
+    // If > 1, well this case should not be possible at all.
+    throw FunctionalError('Find more than one relation between an entity and a PIR', { sourceId, pirId, pirMetaRels });
+  }
+  const pirMetaRel = pirMetaRels.edges[0].node;
+  const editInput = [{ key: 'pir_dependencies', value: pirDependencies, operation }];
+  const updatedRef = await stixRefRelationshipEditField(context, SYSTEM_USER, pirMetaRel.id, editInput);
+  console.log('REF', updatedRef);
+};
+
+/**
+ * Flag the source of the relationship by creating a meta relationship 'in-pir'
+ * between the source and the PIR.
+ *
+ * @param context To be able to create the relationship.
+ * @param sourceId ID of the source of the rel.
+ * @param pirId ID of the PIR.
+ * @param pirDependencies Criteria matched by the relationship.
+ */
+export const flagSource = async (
+  context: AuthContext,
+  sourceId: string,
+  pirId: string,
+  pirDependencies: PirDependency[],
+) => {
+  const addRefInput = {
+    relationship_type: RELATION_IN_PIR,
+    toId: pirId,
+    pir_dependencies: pirDependencies,
+  };
+  // First create the meta relationship.
+  await stixObjectOrRelationshipAddRefRelation(
+    context,
+    SYSTEM_USER,
+    sourceId,
+    addRefInput,
+    ABSTRACT_STIX_CORE_OBJECT,
+  );
+  // And then add the dependencies in the meta relationship.
+  // TODO PIR: improve this if possible to avoid making 2 calls.
+  await updatePirDependencies(context, sourceId, pirId, pirDependencies);
 };
