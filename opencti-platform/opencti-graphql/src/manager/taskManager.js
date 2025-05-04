a8827b348e9213e1c210f04956fbe966de16b896
Cathia Archidoit <cathia.archidoit@filigran.io>
2025-05-04 11:13:37 +0200

    [backend] PIR background task at initial meta rel creations

--- opencti-platform/opencti-graphql/src/manager/taskManager.js
+++ opencti-platform/opencti-graphql/src/manager/taskManager.js
@@ -11,6 +11,7 @@ import {
   ACTION_TYPE_ADD,
   ACTION_TYPE_ENRICHMENT,
   ACTION_TYPE_MERGE,
+  ACTION_TYPE_PIR_INITIAL_META_CREATE,
   ACTION_TYPE_PROMOTE,
   ACTION_TYPE_REMOVE,
   ACTION_TYPE_REPLACE,
@@ -71,6 +72,7 @@ import {
   ACTION_TYPE_UNSHARE,
   ACTION_TYPE_UNSHARE_MULTIPLE,
   TASK_TYPE_LIST,
+  TASK_TYPE_PIR,
   TASK_TYPE_QUERY,
   TASK_TYPE_RULE
 } from '../domain/backgroundTask-common';
@@ -90,6 +92,7 @@ import { deleteDraftWorkspace } from '../modules/draftWorkspace/draftWorkspace-d
 import { ENTITY_TYPE_DRAFT_WORKSPACE } from '../modules/draftWorkspace/draftWorkspace-types';
 import { elRemoveElementFromDraft } from '../database/draft-engine';
 import { stixObjectOrRelationshipAddRefRelations, stixObjectOrRelationshipDeleteRefRelation } from '../domain/stixObjectOrStixRelationship';
+import { flagSource } from '../modules/pir/pir-utils';
 
 // Task manager responsible to execute long manual tasks
 // Each API will start is task manager.
@@ -165,6 +168,17 @@ const computeRuleTaskElements = async (context, user, task) => {
     const element = elements[elementIndex];
     processingElements.push({ element: element.node, next: element.cursor });
   }
+  return { actions, elements: processingElements };
+};
+
+export const computePirTaskElements = async (context, user, task) => {
+  const { pir_dependencies_map, pir_id } = task;
+  const processingElements = [];
+  const pirDependenciesMap = new Map(JSON.parse(pir_dependencies_map));
+  // Apply the actions for each element
+  await Promise.all(Array.from(pirDependenciesMap.keys())
+    .map((sourceId) => flagSource(context, sourceId, pir_id, pirDependenciesMap.get(sourceId))));
+  const actions = [{ type: ACTION_TYPE_PIR_INITIAL_META_CREATE, context: { rule: task } }];
   return { actions, elements: processingElements };
 };
 
@@ -637,7 +651,8 @@ export const taskHandler = async () => {
     const isQueryTask = task.type === TASK_TYPE_QUERY;
     const isListTask = task.type === TASK_TYPE_LIST;
     const isRuleTask = task.type === TASK_TYPE_RULE;
-    if (!isQueryTask && !isListTask && !isRuleTask) {
+    const isPirTask = task.type === TASK_TYPE_PIR;
+    if (!isQueryTask && !isListTask && !isRuleTask && !isPirTask) {
       logApp.error('[OPENCTI-MODULE] Task manager unsupported type', { type: task.type });
       return;
     }
@@ -659,6 +674,9 @@ export const taskHandler = async () => {
     }
     if (isRuleTask) {
       jobToExecute = await computeRuleTaskElements(fullContext, user, task);
+    }
+    if (isPirTask) {
+      jobToExecute = await computePirTaskElements(fullContext, SYSTEM_USER, task);
     }
     // Process the elements (empty = end of execution)
     const processingElements = jobToExecute.elements;
