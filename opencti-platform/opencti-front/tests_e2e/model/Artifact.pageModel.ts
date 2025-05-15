2259509f8166408c42a9af429f86f4be341cc465
BocognanoSarah <sarah.bocognano@filigran.io>
2025-05-15 13:59:07 +0200

    add events and observations wip

--- opencti-platform/opencti-front/tests_e2e/model/Artifact.pageModel.ts
+++ opencti-platform/opencti-front/tests_e2e/model/Artifact.pageModel.ts
@@ -1,4 +1,5 @@
 import { Page } from '@playwright/test';
+import LeftBarPage from './menu/leftBar.pageModel';
 
 export default class ArtifactPage {
   constructor(private page: Page) {}
@@ -7,7 +8,17 @@ export default class ArtifactPage {
     return this.page.getByTestId('Artifact-page');
   }
 
+  async navigateFromMenu() {
+    const leftBarPage = new LeftBarPage(this.page);
+    await leftBarPage.open();
+    await leftBarPage.clickOnMenu('Observations', 'Artifacts');
+  }
+
   addNewArtifactImport() {
     return this.page.getByLabel('Create Artifact', { exact: true });
+  }
+
+  getItemFromList(name: string) {
+    return this.page.getByTestId(name).first();
   }
 }
