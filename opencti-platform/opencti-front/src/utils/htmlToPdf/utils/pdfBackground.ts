bd21966733c8677eb7d3f7378547b7129f48bb09
BocognanoSarah <sarah.bocognano@filigran.io>
2025-05-16 14:55:14 +0200

    [frontend] [Outcome template - Chunk 3] - Add preview (#10284)

--- opencti-platform/opencti-front/src/utils/htmlToPdf/utils/pdfBackground.ts
+++ opencti-platform/opencti-front/src/utils/htmlToPdf/utils/pdfBackground.ts
@@ -1,18 +1,21 @@
 import { DynamicBackground } from 'pdfmake/interfaces';
 
-const pdfBackground: DynamicBackground = (currentPage, pageSize) => {
-  return {
-    canvas: currentPage > 1
-      ? []
-      : [{
-        type: 'rect',
-        x: 0,
-        y: 0,
-        w: pageSize.width,
-        h: pageSize.height,
-        linearGradient: ['#00020C', '#001BDA'],
-      }],
+const pdfBackground = (linearGradiant: string[] | undefined) => {
+  const background: DynamicBackground = (currentPage, pageSize) => {
+    return {
+      canvas: currentPage > 1
+        ? []
+        : [{
+          type: 'rect',
+          x: 0,
+          y: 0,
+          w: pageSize.width,
+          h: pageSize.height,
+          linearGradient: linearGradiant,
+        }],
+    };
   };
+  return background;
 };
 
 export default pdfBackground;
