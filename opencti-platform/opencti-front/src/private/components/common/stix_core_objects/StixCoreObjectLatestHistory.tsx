89047ca538186e2d6bab8145e5b5f54aefb702fc
Archidoit <75783086+Archidoit@users.noreply.github.com>
2025-05-21 09:59:22 +0200

    [frontend] Stix core object history lines refacto (#10965)

--- opencti-platform/opencti-front/src/private/components/common/stix_core_objects/StixCoreObjectLatestHistory.tsx
+++ opencti-platform/opencti-front/src/private/components/common/stix_core_objects/StixCoreObjectLatestHistory.tsx
@@ -8,10 +8,10 @@ import Paper from '@mui/material/Paper';
 import ListItemText from '@mui/material/ListItemText';
 import Skeleton from '@mui/material/Skeleton';
 import { useTheme } from '@mui/material/styles';
-import { StixCoreObjectHistoryLines_data$data } from '@components/common/stix_core_objects/__generated__/StixCoreObjectHistoryLines_data.graphql';
-import { QueryRenderer } from '../../../../relay/environment';
+import { StixCoreObjectHistoryLinesQuery } from '@components/common/stix_core_objects/__generated__/StixCoreObjectHistoryLinesQuery.graphql';
 import StixCoreObjectHistoryLines, { stixCoreObjectHistoryLinesQuery } from './StixCoreObjectHistoryLines';
 import { useFormatter } from '../../../../components/i18n';
+import useQueryLoading from '../../../../utils/hooks/useQueryLoading';
 
 type StixCoreObjectLatestHistoryProps = {
   stixCoreObjectId: string;
@@ -21,85 +21,82 @@ const StixCoreObjectLatestHistory = ({ stixCoreObjectId }: StixCoreObjectLatestH
   const { t_i18n } = useFormatter();
   const theme = useTheme();
 
+  const queryRef = useQueryLoading<StixCoreObjectHistoryLinesQuery>(
+    stixCoreObjectHistoryLinesQuery,
+    {
+      filters: {
+        mode: 'and',
+        filterGroups: [],
+        filters: [
+          { key: ['context_data.id'], values: [stixCoreObjectId] },
+          {
+            key: ['event_type'],
+            values: ['mutation', 'create', 'update', 'delete', 'merge'],
+          },
+        ],
+      },
+      first: 7,
+      orderBy: 'timestamp',
+      orderMode: 'desc',
+    },
+  );
+
   return (
     <>
       <Typography variant="h4">
         {t_i18n('Most recent history')}
       </Typography>
-      <QueryRenderer
-        query={stixCoreObjectHistoryLinesQuery}
-        variables={{
-          filters: {
-            mode: 'and',
-            filterGroups: [],
-            filters: [
-              { key: 'context_data.id', values: [stixCoreObjectId] },
-              {
-                key: 'event_type',
-                values: ['mutation', 'create', 'update', 'delete', 'merge'],
-              },
-            ],
-          },
-          first: 7,
-          orderBy: 'timestamp',
-          orderMode: 'desc',
-        }}
-        render={({ props }: { props: StixCoreObjectHistoryLines_data$data }) => {
-          if (props) {
-            return (
-              <StixCoreObjectHistoryLines
-                stixCoreObjectId={stixCoreObjectId}
-                data={props}
-                isRelationLog={false}
-              />
-            );
-          }
-          return (
-            <Paper
-              sx={{
-                marginTop: theme.spacing(1),
-                padding: 0,
-                borderRadius: 4,
-              }}
-              variant="outlined"
-              className={'paper-for-grid'}
-            >
-              <List>
-                {Array.from(Array(5), (e, i) => (
-                  <ListItem
-                    key={`latest_history_skel_${i}`}
-                    dense
-                    divider
-                  >
-                    <ListItemIcon>
-                      <Avatar>{i}</Avatar>
-                    </ListItemIcon>
-                    <ListItemText
-                      primary={
-                        <Skeleton
-                          animation="wave"
-                          variant="rectangular"
-                          width="90%"
-                          height={15}
-                          style={{ marginBottom: 10 }}
-                        />
-                      }
-                      secondary={
-                        <Skeleton
-                          animation="wave"
-                          variant="rectangular"
-                          width="90%"
-                          height={15}
-                        />
-                      }
-                    />
-                  </ListItem>
-                ))}
-              </List>
-            </Paper>
-          );
-        }}
-      />
+      {queryRef
+        && <React.Suspense
+          fallback={<Paper
+            sx={{
+              marginTop: theme.spacing(1),
+              padding: 0,
+              borderRadius: 4,
+            }}
+            variant="outlined"
+            className={'paper-for-grid'}
+                    >
+            <List>
+              {Array.from(Array(5), (e, i) => (
+                <ListItem
+                  key={`latest_history_skel_${i}`}
+                  dense
+                  divider
+                >
+                  <ListItemIcon>
+                    <Avatar>{i}</Avatar>
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={
+                      <Skeleton
+                        animation="wave"
+                        variant="rectangular"
+                        width="90%"
+                        height={15}
+                        style={{ marginBottom: 10 }}
+                      />
+                    }
+                    secondary={
+                      <Skeleton
+                        animation="wave"
+                        variant="rectangular"
+                        width="90%"
+                        height={15}
+                      />
+                    }
+                  />
+                </ListItem>
+              ))}
+            </List>
+          </Paper>}
+           >
+          <StixCoreObjectHistoryLines
+            queryRef={queryRef}
+            isRelationLog={false}
+          />
+        </React.Suspense>
+      }
     </>
   );
 };
