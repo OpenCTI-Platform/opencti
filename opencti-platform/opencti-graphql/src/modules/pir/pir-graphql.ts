d7c1bc9df46de8d03f0baa54f90012e6917afd6f
Cathia Archidoit <cathia.archidoit@filigran.io>
2025-05-22 14:30:24 +0200

    [backend] fix working without FF Pir

--- opencti-platform/opencti-graphql/src/modules/pir/pir-graphql.ts
+++ opencti-platform/opencti-graphql/src/modules/pir/pir-graphql.ts
@@ -1,11 +1,8 @@
 import { registerGraphqlSchema } from '../../graphql/schema';
 import pirTypeDefs from './pir.graphql';
 import pirResolvers from './pir-resolvers';
-import { isFeatureEnabled } from '../../config/conf';
 
-if (isFeatureEnabled('Pir')) {
-  registerGraphqlSchema({
-    schema: pirTypeDefs,
-    resolver: pirResolvers,
-  });
-}
+registerGraphqlSchema({
+  schema: pirTypeDefs,
+  resolver: pirResolvers,
+});
