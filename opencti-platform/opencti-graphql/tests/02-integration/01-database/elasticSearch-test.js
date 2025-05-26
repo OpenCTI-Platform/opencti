acdaca30bf5b2577e9fcd92235aa38a2d21a8bc5
Cathia Archidoit <cathia.archidoit@filigran.io>
2025-05-26 12:10:09 +0200

    [backend] fix tests

--- opencti-platform/opencti-graphql/tests/02-integration/01-database/elasticSearch-test.js
+++ opencti-platform/opencti-graphql/tests/02-integration/01-database/elasticSearch-test.js
@@ -49,6 +49,7 @@ const elWhiteUser = async () => {
 };
 
 const VOCABULARY_COUNT = 347;
+const ENTITY_SETTINGS_COUNT = 42;
 
 describe('Elasticsearch configuration test', () => {
   it('should configuration correct', async () => {
@@ -414,7 +415,7 @@ describe('Elasticsearch pagination', () => {
     expect(entityTypeMap.get('Course-Of-Action')).toBe(1);
     expect(entityTypeMap.get('Credential')).toBe(1);
     expect(entityTypeMap.get('DecayRule')).toBe(4);
-    expect(entityTypeMap.get('EntitySetting')).toBe(44);
+    expect(entityTypeMap.get('EntitySetting')).toBe(ENTITY_SETTINGS_COUNT);
     expect(entityTypeMap.get('External-Reference')).toBe(7);
     expect(entityTypeMap.get('StixFile')).toBe(1);
     expect(entityTypeMap.get('Group')).toBe(TESTING_GROUPS.length + 3);
@@ -563,7 +564,7 @@ describe('Elasticsearch pagination', () => {
     expect(entityTypeMap.get('Course-Of-Action')).toBe(1);
     expect(entityTypeMap.get('Credential')).toBe(1);
     expect(entityTypeMap.get('DecayRule')).toBe(4);
-    expect(entityTypeMap.get('EntitySetting')).toBe(44);
+    expect(entityTypeMap.get('EntitySetting')).toBe(ENTITY_SETTINGS_COUNT);
     expect(entityTypeMap.get('External-Reference')).toBe(7);
     expect(entityTypeMap.get('StixFile')).toBe(1);
     expect(entityTypeMap.get('Group')).toBe(TESTING_GROUPS.length + 3);
@@ -696,7 +697,7 @@ describe('Elasticsearch pagination', () => {
     expect(entityTypeMap.get('Capability')).toBe(42);
     expect(entityTypeMap.get('Credential')).toBe(1);
     expect(entityTypeMap.get('DecayRule')).toBe(4);
-    expect(entityTypeMap.get('EntitySetting')).toBe(44);
+    expect(entityTypeMap.get('EntitySetting')).toBe(ENTITY_SETTINGS_COUNT);
     expect(entityTypeMap.get('StixFile')).toBe(1);
     expect(entityTypeMap.get('Group')).toBe(TESTING_GROUPS.length + 3);
     expect(entityTypeMap.get('ManagerConfiguration')).toBe(1);
