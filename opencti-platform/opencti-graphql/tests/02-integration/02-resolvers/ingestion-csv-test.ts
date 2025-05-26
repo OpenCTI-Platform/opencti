76a880b6619153a6975b83599d2093c8ba40d7f6
jpkha <jean-philippe.kha@filigran.io>
2025-05-26 14:03:00 +0200

    [Backend/Frontend] Add CSV Feed export functionality (#11085)

--- opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/ingestion-csv-test.ts
+++ opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/ingestion-csv-test.ts
@@ -1,10 +1,11 @@
 import { beforeAll, describe, expect, it } from 'vitest';
 import gql from 'graphql-tag';
 import { ADMIN_USER, queryAsAdmin, testContext, USER_DISINFORMATION_ANALYST, USER_PARTICIPATE } from '../../utils/testQuery';
-import { queryAsAdminWithSuccess, queryAsUserIsExpectedForbidden, queryAsUserWithSuccess } from '../../utils/testQueryHelper';
+import { createUploadFromTestDataFile, queryAsAdminWithSuccess, queryAsUserIsExpectedForbidden, queryAsUserWithSuccess } from '../../utils/testQueryHelper';
 import { patchCsvIngestion } from '../../../src/modules/ingestion/ingestion-csv-domain';
 import { now } from '../../../src/utils/format';
 import { SYSTEM_USER } from '../../../src/utils/access';
+import pjson from '../../../package.json';
 
 describe('CSV ingestion resolver standard behavior', () => {
   let singleColumnCsvMapperId = '';
@@ -223,6 +224,33 @@ describe('CSV ingestion resolver standard behavior', () => {
     expect(errors?.[0].message).toBe('Cannot delete this CSV Mapper: it is used by one or more IngestionCsv feed(s)');
   });
 
+  it('should generate correct export configuration', async () => {
+    const QUERY_CSV_FEED = gql(`
+      query QueryCSVFeed($id: String!) {
+        ingestionCsv(id: $id) {
+          id
+          name
+          toConfigurationExport
+        }
+      }
+    `);
+    const { data } = await queryAsAdmin({
+      query: QUERY_CSV_FEED,
+      variables: { id: singleColumnCsvFeedIngesterId }
+    });
+    expect(data?.ingestionCsv.id).toBe(singleColumnCsvFeedIngesterId);
+    expect(data?.ingestionCsv.name).toBe('Single column CSV feed ingester');
+    const csvFeedIngestion = JSON.parse(data?.ingestionCsv.toConfigurationExport);
+    expect(csvFeedIngestion.type).toBe('csvFeeds');
+    expect(csvFeedIngestion.openCTI_version).toBe(pjson.version);
+    expect(csvFeedIngestion.configuration).toBeDefined();
+    expect(csvFeedIngestion.configuration.name).toBe('Single column CSV feed ingester');
+    expect(csvFeedIngestion.configuration.uri).toBe('https://lists.blocklist.de/lists/all.txt');
+    expect(csvFeedIngestion.configuration.csv_mapper_type).toBe('inline');
+    expect(csvFeedIngestion.configuration.authentication_type).toBe('none');
+    expect(csvFeedIngestion.configuration.csv_mapper.configuration).toBeDefined();
+  });
+
   it('should delete the CSV feeds ingester', async () => {
     const CSV_FEED_INGESTER_TO_DELETE = {
       id: singleColumnCsvFeedIngesterId,
@@ -295,5 +323,30 @@ describe('CSV ingestion resolver standard behavior', () => {
       },
       'CSVMAPPERS should be required to list csv mapper.'
     );
+  });
+
+  it('should test a json file against import', async () => {
+    const upload = await createUploadFromTestDataFile('csvFeed/test-csv-feed.json', 'test-csv-feed.json', 'application/json');
+    const TEST_MUTATION = gql`
+      query CsvFeedAddInputFromImport($file: Upload!) {
+        csvFeedAddInputFromImport(file: $file){
+          authentication_type
+          name
+          csvMapper {
+            name
+          }
+        }
+      }
+    `;
+    const queryResult = await queryAsAdmin({
+      query: TEST_MUTATION,
+      variables: {
+        file: upload,
+      },
+    });
+    expect(queryResult.data?.csvFeedAddInputFromImport).toBeDefined();
+    expect(queryResult.data?.csvFeedAddInputFromImport.csvMapper).toBeDefined();
+    expect(queryResult.data?.csvFeedAddInputFromImport.csvMapper.name).toBe('Inline CSV Feed');
+    expect(queryResult.data?.csvFeedAddInputFromImport.name).toBe('Test name');
   });
 });
