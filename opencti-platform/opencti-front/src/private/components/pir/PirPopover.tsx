7991f43594dfa20845c40c47b106c65f7743b281
Landry Trebon <landry.trebon@filigran.io>
2025-05-27 17:35:41 +0200

    [frontend] update PIR

--- opencti-platform/opencti-front/src/private/components/pir/PirPopover.tsx
+++ opencti-platform/opencti-front/src/private/components/pir/PirPopover.tsx
@@ -2,37 +2,20 @@ import React, { UIEvent, useState } from 'react';
 import MoreVert from '@mui/icons-material/MoreVert';
 import Button from '@mui/material/Button';
 import { Menu, MenuItem, PopoverProps } from '@mui/material';
-import { graphql } from 'react-relay';
-import DeleteDialog from '../../../components/DeleteDialog';
 import { useFormatter } from '../../../components/i18n';
-import useDeletion from '../../../utils/hooks/useDeletion';
 import stopEvent from '../../../utils/domEvent';
-import useApiMutation from '../../../utils/hooks/useApiMutation';
-import { PirPopoverDeleteMutation } from './__generated__/PirPopoverDeleteMutation.graphql';
-
-const pirDeleteMutation = graphql`
-  mutation PirPopoverDeleteMutation($id: ID!) {
-    pirDelete(id: $id)
-  }
-`;
 
 interface PirPopoverProps {
-  pirId: string
-  onDeleteComplete?: () => void
+  handleOpenDelete: (e?: UIEvent) => void
+  deleting: boolean
 }
 
-const PirPopover = ({ pirId, onDeleteComplete }: PirPopoverProps) => {
+const PirPopover = ({
+  handleOpenDelete,
+  deleting,
+}: PirPopoverProps) => {
   const { t_i18n } = useFormatter();
   const [anchorEl, setAnchorEl] = useState<PopoverProps['anchorEl']>();
-
-  const [deleteMutation, deleting] = useApiMutation<PirPopoverDeleteMutation>(
-    pirDeleteMutation,
-    undefined,
-    { successMessage: `${t_i18n('entity_Pir')} ${t_i18n('successfully deleted')}` },
-  );
-
-  const deletion = useDeletion({ handleClose: () => setAnchorEl(undefined) });
-  const { handleOpenDelete, handleCloseDelete } = deletion;
 
   const onOpenMenu = (e: UIEvent) => {
     stopEvent(e);
@@ -42,20 +25,6 @@ const PirPopover = ({ pirId, onDeleteComplete }: PirPopoverProps) => {
   const onCloseMenu = (e: UIEvent) => {
     stopEvent(e);
     setAnchorEl(undefined);
-  };
-
-  const onDelete = (e: UIEvent) => {
-    stopEvent(e);
-    deleteMutation({
-      variables: { id: pirId },
-      onCompleted: () => {
-        handleCloseDelete();
-        onDeleteComplete?.();
-      },
-      onError: () => {
-        handleCloseDelete();
-      },
-    });
   };
 
   return (
@@ -74,12 +43,6 @@ const PirPopover = ({ pirId, onDeleteComplete }: PirPopoverProps) => {
           {t_i18n('Delete')}
         </MenuItem>
       </Menu>
-
-      <DeleteDialog
-        deletion={deletion}
-        submitDelete={onDelete}
-        message={t_i18n('Do you want to delete this PIR?')}
-      />
     </>
   );
 };
