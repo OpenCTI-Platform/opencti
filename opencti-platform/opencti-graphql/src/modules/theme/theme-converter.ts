e6a6bddebfb5a01a7edeb97694a426ec99daf749
Bonsai8863 <131906254+Bonsai8863@users.noreply.github.com>
2025-05-28 10:28:18 -0400

    Update opencti-platform/opencti-graphql/src/modules/theme/theme-converter.ts

--- opencti-platform/opencti-graphql/src/modules/theme/theme-converter.ts
+++ opencti-platform/opencti-graphql/src/modules/theme/theme-converter.ts
@@ -3,14 +3,14 @@ import { STIX_EXT_OCTI } from '../../types/stix-2-1-extensions';
 import type { StixTheme, StoreEntityTheme } from './theme-types';
 
 const convertThemeToStix = (instance: StoreEntityTheme): StixTheme => {
-  const stixDomainObject = buildStixDomain(instance);
+  const stixObject = buildStixDomain(instance);
   return {
-    ...stixDomainObject,
+    ...stixObject,
     name: instance.name,
     manifest: instance.manifest,
     extensions: {
       [STIX_EXT_OCTI]: cleanObject({
-        ...stixDomainObject.extensions[STIX_EXT_OCTI],
+        ...stixObject.extensions[STIX_EXT_OCTI],
         extension_type: 'new-sdo',
       })
     },
