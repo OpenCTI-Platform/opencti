9dca2402f6d6b5e56575b60304100c0bbc9c0cfe
Landry Trebon <landry.trebon@filigran.io>
2025-05-28 11:17:32 +0200

    [frontend] useless file

--- opencti-platform/opencti-front/src/utils/array.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export const arrayGroupBy = (input: any[], key: string) => {
-  return input.reduce((acc, currentValue) => {
-    const groupKey = currentValue[key];
-    if (!acc[groupKey]) acc[groupKey] = [];
-    acc[groupKey].push(currentValue);
-    return acc;
-  }, {});
-};
