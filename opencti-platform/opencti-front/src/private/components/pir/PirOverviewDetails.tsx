372a57a8debc618bd33e0af32a5ab96320274bc9
Landry Trebon <landry.trebon@filigran.io>
2025-05-28 14:15:16 +0200

    [frontend] display filters

--- opencti-platform/opencti-front/src/private/components/pir/PirOverviewDetails.tsx
+++ opencti-platform/opencti-front/src/private/components/pir/PirOverviewDetails.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { graphql, useFragment } from 'react-relay';
-import { Grid2 as Grid, Typography } from '@mui/material';
+import { Grid2 as Grid, Typography, Box } from '@mui/material';
 import { PirOverviewDetailsFragment$key } from './__generated__/PirOverviewDetailsFragment.graphql';
 import ExpandableMarkdown from '../../../components/ExpandableMarkdown';
 import { useFormatter } from '../../../components/i18n';
 import ItemCreators from '../../../components/ItemCreators';
+import FilterIconButton from '../../../components/FilterIconButton';
 
 const detailsFragment = graphql`
   fragment PirOverviewDetailsFragment on Pir {
@@ -43,24 +44,6 @@ const PirOverviewDetails = ({ data }: PirOverviewDetailsProps) => {
 
         <div>
           <Typography variant="h3" gutterBottom>
-            {t_i18n('Filters')}
-          </Typography>
-          TODO
-        </div>
-
-        <div>
-          <Typography variant="h3" gutterBottom>
-            {t_i18n('Criteria')}
-          </Typography>
-          TODO
-        </div>
-      </Grid>
-      <Grid
-        size={{ xs: 6 }}
-        sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}
-      >
-        <div>
-          <Typography variant="h3" gutterBottom>
             {t_i18n('Creation date')}
           </Typography>
           {fldt(pir.created_at)}
@@ -71,6 +54,38 @@ const PirOverviewDetails = ({ data }: PirOverviewDetailsProps) => {
             {t_i18n('Creators')}
           </Typography>
           <ItemCreators creators={pir.creators ?? []}/>
+        </div>
+      </Grid>
+      <Grid
+        size={{ xs: 6 }}
+        sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}
+      >
+        <div>
+          <Typography variant="h3" gutterBottom>
+            {t_i18n('Filters')}
+          </Typography>
+          <FilterIconButton
+            key={pir.pir_filters}
+            filters={JSON.parse(pir.pir_filters)}
+            entityTypes={['Stix-Core-Object']}
+            styleNumber={1}
+          />
+        </div>
+
+        <div>
+          <Typography variant="h3" gutterBottom>
+            {t_i18n('Criteria')}
+          </Typography>
+          <Box sx={{ display: 'flex', gap: 1 }}>
+            {pir.pir_criteria.map((c) => (
+              <FilterIconButton
+                key={c.filters}
+                filters={JSON.parse(c.filters)}
+                entityTypes={['Stix-Core-Object']}
+                styleNumber={1}
+              />
+            ))}
+          </Box>
         </div>
       </Grid>
     </Grid>
