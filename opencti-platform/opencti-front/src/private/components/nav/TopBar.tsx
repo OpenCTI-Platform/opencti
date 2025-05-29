4959d45d70f31b0d424acc84dd050f7913d4101b
dreadfog <quentin.fraty@gmail.com>
2025-05-29 11:42:12 +0200

    formatted topBar file, removed extra whitespaces

--- opencti-platform/opencti-front/src/private/components/nav/TopBar.tsx
+++ opencti-platform/opencti-front/src/private/components/nav/TopBar.tsx
@@ -120,9 +120,9 @@ const useStyles = makeStyles<Theme>((theme) => ({
 
 const unsanitizeSearchTerm = (term: string): string => {
   return term
-    .replace(/\[([.@/:]+)\]/g, '$1')  // remove [ ] around any combination of '.', '@', '/', ':'
-    .replace(/^hxxps:/i, 'https:')    // hxxps:// -> https://
-    .replace(/^hxxp:/i, 'http:');     // hxxp:// -> http://
+    .replace(/\[([.@/:]+)\]/g, '$1') // remove [ ] around any combination of '.', '@', '/', ':'
+    .replace(/^hxxps:/i, 'https:') // hxxps:// -> https://
+    .replace(/^hxxp:/i, 'http:'); // hxxp:// -> http://
 };
 const topBarNotificationNumberSubscription = graphql`
   subscription TopBarNotificationNumberSubscription {
@@ -235,37 +235,37 @@ const TopBarComponent: FunctionComponent<TopBarProps> = ({
   const handleCloseXtm = () => {
     setXtmOpen({ open: false, anchorEl: null });
   };
-const handleSearch = (searchKeyword: string, askAI = false) => {
-  // Unsanitize the search term before processing
-  const unsanitizedKeyword = unsanitizeSearchTerm(searchKeyword);
-  
-  if (askAI && isEnterpriseEdition) {
-    setIsNLQLoading(true);
-    commitMutationNLQ({
-      variables: {
-        search: unsanitizedKeyword, // Use unsanitized keyword
-      },
-      onCompleted: (response: TopBarAskAINLQMutation$data) => {
-        setIsNLQLoading(false);
-        const notResolvedValues = response.aiNLQ?.notResolvedValues ?? [];
-        const filters = response.aiNLQ?.filters;
-        if (notResolvedValues.length > 0) {
-          MESSAGING$.notifyNLQ(`${t_i18n('Some entities you mentioned have not been found in the platform')}: ${notResolvedValues}`);
-        } else if (!filters || !isFilterGroupNotEmpty(JSON.parse(filters))) {
-          MESSAGING$.notifyNLQ(t_i18n('The NLQ model didn\'t find filters corresponding to your question'));
-        }
-        handleSearchByFilter(unsanitizedKeyword, 'nlq', navigate, response.aiNLQ?.filters); // Use unsanitized keyword
-      },
-      onError: (error: Error) => {
-        setIsNLQLoading(false);
-        const { errors } = (error as unknown as RelayError).res;
-        MESSAGING$.notifyError(errors.at(0)?.message);
-      },
-    });
-  } else {
-    handleSearchByKeyword(unsanitizedKeyword, 'knowledge', navigate); // Use unsanitized keyword
-  }
-};
+  const handleSearch = (searchKeyword: string, askAI = false) => {
+    // Unsanitize the search term before processing
+    const unsanitizedKeyword = unsanitizeSearchTerm(searchKeyword);
+
+    if (askAI && isEnterpriseEdition) {
+      setIsNLQLoading(true);
+      commitMutationNLQ({
+        variables: {
+          search: unsanitizedKeyword, // Use unsanitized keyword
+        },
+        onCompleted: (response: TopBarAskAINLQMutation$data) => {
+          setIsNLQLoading(false);
+          const notResolvedValues = response.aiNLQ?.notResolvedValues ?? [];
+          const filters = response.aiNLQ?.filters;
+          if (notResolvedValues.length > 0) {
+            MESSAGING$.notifyNLQ(`${t_i18n('Some entities you mentioned have not been found in the platform')}: ${notResolvedValues}`);
+          } else if (!filters || !isFilterGroupNotEmpty(JSON.parse(filters))) {
+            MESSAGING$.notifyNLQ(t_i18n('The NLQ model didn\'t find filters corresponding to your question'));
+          }
+          handleSearchByFilter(unsanitizedKeyword, 'nlq', navigate, response.aiNLQ?.filters); // Use unsanitized keyword
+        },
+        onError: (error: Error) => {
+          setIsNLQLoading(false);
+          const { errors } = (error as unknown as RelayError).res;
+          MESSAGING$.notifyError(errors.at(0)?.message);
+        },
+      });
+    } else {
+      handleSearchByKeyword(unsanitizedKeyword, 'knowledge', navigate); // Use unsanitized keyword
+    }
+  };
   const handleOpenDrawer = () => {
     setOpenDrawer(true);
     handleCloseMenu();
@@ -319,51 +319,51 @@ const handleSearch = (searchKeyword: string, askAI = false) => {
         )}
         <div className={classes.barRight}>
           {!!draftContext && (
-            <DraftContextBanner/>
+            <DraftContextBanner />
           )}
           <div className={classes.barRightContainer}>
             {!draftContext && (
-            <Security needs={[KNOWLEDGE]}>
-              <>
-                { ee.license_type === 'nfr' && <ItemBoolean variant="large" label={'EE DEV LICENSE'} status={false}/> }
-                <Security needs={[KNOWLEDGE_KNASKIMPORT]}>
-                  <UploadImport
-                    variant="icon"
-                    size="medium"
-                    fontSize="medium"
-                    color="inherit"
-                  />
-                </Security>
-                <Tooltip title={t_i18n('Notifications')}>
-                  <IconButton
-                    size="medium"
-                    aria-haspopup="true"
-                    component={Link}
-                    to="/dashboard/profile/notifications"
-                    color={location.pathname === '/dashboard/profile/notifications' ? 'primary' : 'inherit'}
-                  >
-                    <Badge
-                      color="secondary"
-                      variant="dot"
-                      invisible={!isNewNotification}
+              <Security needs={[KNOWLEDGE]}>
+                <>
+                  {ee.license_type === 'nfr' && <ItemBoolean variant="large" label={'EE DEV LICENSE'} status={false} />}
+                  <Security needs={[KNOWLEDGE_KNASKIMPORT]}>
+                    <UploadImport
+                      variant="icon"
+                      size="medium"
+                      fontSize="medium"
+                      color="inherit"
+                    />
+                  </Security>
+                  <Tooltip title={t_i18n('Notifications')}>
+                    <IconButton
+                      size="medium"
+                      aria-haspopup="true"
+                      component={Link}
+                      to="/dashboard/profile/notifications"
+                      color={location.pathname === '/dashboard/profile/notifications' ? 'primary' : 'inherit'}
                     >
-                      <NotificationsOutlined fontSize="medium"/>
-                    </Badge>
-                  </IconButton>
-                </Tooltip>
-                <Tooltip title={t_i18n('Triggers')}>
-                  <IconButton
-                    size="medium"
-                    aria-haspopup="true"
-                    component={Link}
-                    to="/dashboard/profile/triggers"
-                    color={location.pathname === '/dashboard/profile/triggers' ? 'primary' : 'inherit'}
-                  >
-                    <AlarmOnOutlined fontSize="medium" />
-                  </IconButton>
-                </Tooltip>
-              </>
-            </Security>
+                      <Badge
+                        color="secondary"
+                        variant="dot"
+                        invisible={!isNewNotification}
+                      >
+                        <NotificationsOutlined fontSize="medium" />
+                      </Badge>
+                    </IconButton>
+                  </Tooltip>
+                  <Tooltip title={t_i18n('Triggers')}>
+                    <IconButton
+                      size="medium"
+                      aria-haspopup="true"
+                      component={Link}
+                      to="/dashboard/profile/triggers"
+                      color={location.pathname === '/dashboard/profile/triggers' ? 'primary' : 'inherit'}
+                    >
+                      <AlarmOnOutlined fontSize="medium" />
+                    </IconButton>
+                  </Tooltip>
+                </>
+              </Security>
             )}
             <IconButton
               color="inherit"
@@ -373,7 +373,7 @@ const handleSearch = (searchKeyword: string, askAI = false) => {
               id="xtm-menu-button"
               onClick={handleOpenXtm}
             >
-              <AppsOutlined fontSize="medium"/>
+              <AppsOutlined fontSize="medium" />
             </IconButton>
             <Popover
               anchorEl={xtmOpen.anchorEl}
@@ -437,7 +437,7 @@ const handleSearch = (searchKeyword: string, askAI = false) => {
                   : 'inherit'
               }
             >
-              <AccountCircleOutlined fontSize="medium"/>
+              <AccountCircleOutlined fontSize="medium" />
             </IconButton>
             <Menu
               id="menu-appbar"
