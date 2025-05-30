9ce0efa984e21cdaa6f1ece2ba7ba475f329ee95
Angelique <angelique.jard@filigran.io>
2025-05-30 11:19:37 +0200

    Attempt to have less counter updates on changes

--- opencti-platform/opencti-graphql/tests/03-streams/00-Raw/raw-test.js
+++ opencti-platform/opencti-graphql/tests/03-streams/00-Raw/raw-test.js
@@ -1,12 +1,22 @@
 /* eslint-disable @typescript-eslint/dot-notation */
 import { describe, expect, it } from 'vitest';
 import * as R from 'ramda';
-import { FIVE_MINUTES, RAW_EVENTS_SIZE } from '../../utils/testQuery';
+import { FIVE_MINUTES } from '../../utils/testQuery';
 import { checkStreamData, checkStreamGenericContent, fetchStreamEvents, } from '../../utils/testStream';
 import { PORT } from '../../../src/config/conf';
 import { EVENT_TYPE_CREATE, EVENT_TYPE_DELETE, EVENT_TYPE_MERGE, EVENT_TYPE_UPDATE } from '../../../src/database/utils';
 import { writeTestDataToFile } from '../../utils/testOutput';
 import { VOCABULARY_NUMBERS } from '../../04-sync/sync-utils';
+import { doTotal, RAW_EVENTS_SIZE, testCreatedCounter, testDeletedCounter, testMergedCounter, testUpdatedCounter } from '../../utils/syncCountHelper';
+
+export const dumpEventByTypeToFile = (eventTypeName, eventsByTypesRecords) => {
+  const allCreatedEventKeys = Object.keys(eventsByTypesRecords);
+  let allCreatedEventCount = '';
+  for (let i = 0; i < allCreatedEventKeys.length; i += 1) {
+    allCreatedEventCount += `counter['${JSON.stringify(allCreatedEventKeys[i])}'] = ${eventsByTypesRecords[allCreatedEventKeys[i]].length} ;\n`;
+  }
+  writeTestDataToFile(allCreatedEventCount, `raw-test-${eventTypeName}-event.txt`);
+};
 
 describe('Raw streams tests', () => {
   // We need to check the event format to be sure that everything is setup correctly
@@ -21,36 +31,48 @@ describe('Raw streams tests', () => {
       const createEvents = events.filter((e) => e.type === EVENT_TYPE_CREATE);
       // Check some events count
       const createEventsByTypes = R.groupBy((e) => e.data.data.type, createEvents);
-      expect(createEventsByTypes['marking-definition'].length).toBe(21);
-      expect(createEventsByTypes['external-reference'].length).toBe(17);
-      expect(createEventsByTypes['kill-chain-phase'].length).toBe(3);
-      expect(createEventsByTypes['course-of-action'].length).toBe(3);
-      expect(createEventsByTypes.label.length).toBe(15);
-      expect(createEventsByTypes.identity.length).toBe(38);
-      expect(createEventsByTypes.location.length).toBe(16);
-      expect(createEventsByTypes.relationship.length).toBe(138);
-      expect(createEventsByTypes.sighting.length).toBe(4);
-      expect(createEventsByTypes.indicator.length).toBe(38);
-      expect(createEventsByTypes['attack-pattern'].length).toBe(9);
-      expect(createEventsByTypes['threat-actor'].length).toBe(17);
-      expect(createEventsByTypes['observed-data'].length).toBe(1);
-      expect(createEventsByTypes['tracking-number'].length).toBe(1);
-      expect(createEventsByTypes.credential.length).toBe(1);
-      expect(createEventsByTypes['network-traffic'].length).toBe(1);
-      expect(createEventsByTypes['ipv4-addr'].length).toBe(1);
-      expect(createEventsByTypes['data-component'].length).toBe(5);
-      expect(createEventsByTypes['data-source'].length).toBe(1);
-      expect(createEventsByTypes.malware.length).toBe(49);
-      expect(createEventsByTypes.software.length).toBe(1);
-      expect(createEventsByTypes.file.length).toBe(4);
-      expect(createEventsByTypes.campaign.length).toBe(5);
-      expect(createEventsByTypes.incident.length).toBe(2);
-      expect(createEventsByTypes.report.length).toBe(42);
-      expect(createEventsByTypes.tool.length).toBe(2);
-      // 328 created at init + 1 request access + 2 created in tests + 5 vocabulary organizations types + 7 persona
+      dumpEventByTypeToFile('create', createEventsByTypes);
+      expect(createEventsByTypes.artifact.length).toBe(testCreatedCounter.artifact);
+      expect(createEventsByTypes['attack-pattern'].length).toBe(testCreatedCounter['attack-pattern']);
+      expect(createEventsByTypes['case-incident'].length).toBe(testCreatedCounter['case-incident']);
+      expect(createEventsByTypes['case-rfi'].length).toBe(testCreatedCounter['case-rfi']);
+      expect(createEventsByTypes.campaign.length).toBe(testCreatedCounter.campaign);
+      expect(createEventsByTypes['course-of-action'].length).toBe(testCreatedCounter['course-of-action']);
+      expect(createEventsByTypes.credential.length).toBe(testCreatedCounter.credential);
+      expect(createEventsByTypes['data-component'].length).toBe(testCreatedCounter['data-component']);
+      expect(createEventsByTypes['data-source'].length).toBe(testCreatedCounter['data-source']);
+      expect(createEventsByTypes['domain-name'].length).toBe(testCreatedCounter['domain-name']);
+      expect(createEventsByTypes['email-addr'].length).toBe(testCreatedCounter['email-addr']);
+      expect(createEventsByTypes['external-reference'].length).toBe(testCreatedCounter['external-reference']);
+      expect(createEventsByTypes.feedback.length).toBe(testCreatedCounter.feedback);
+      expect(createEventsByTypes.file.length).toBe(testCreatedCounter.file);
+      expect(createEventsByTypes.grouping.length).toBe(testCreatedCounter.grouping);
+      expect(createEventsByTypes.identity.length).toBe(testCreatedCounter.identity);
+      expect(createEventsByTypes.incident.length).toBe(testCreatedCounter.incident);
+      expect(createEventsByTypes.indicator.length).toBe(testCreatedCounter.indicator);
+      expect(createEventsByTypes['internal-relationship'].length).toBe(testCreatedCounter['internal-relationship']);
+      expect(createEventsByTypes['ipv4-addr'].length).toBe(testCreatedCounter['ipv4-addr']);
+      expect(createEventsByTypes['kill-chain-phase'].length).toBe(testCreatedCounter['kill-chain-phase']);
+      expect(createEventsByTypes.label.length).toBe(testCreatedCounter.label);
+      expect(createEventsByTypes.location.length).toBe(testCreatedCounter.location);
+      expect(createEventsByTypes.malware.length).toBe(testCreatedCounter.malware);
+      expect(createEventsByTypes['marking-definition'].length).toBe(testCreatedCounter['marking-definition']);
+      expect(createEventsByTypes['network-traffic'].length).toBe(testCreatedCounter['network-traffic']);
+      expect(createEventsByTypes.note.length).toBe(testCreatedCounter.note);
+      expect(createEventsByTypes['observed-data'].length).toBe(testCreatedCounter['observed-data']);
+      expect(createEventsByTypes.opinion.length).toBe(testCreatedCounter.opinion);
+      expect(createEventsByTypes.persona.length).toBe(testCreatedCounter.persona);
+      expect(createEventsByTypes.relationship.length).toBe(testCreatedCounter.relationship);
+      expect(createEventsByTypes.report.length).toBe(testCreatedCounter.report);
+      expect(createEventsByTypes.sighting.length).toBe(testCreatedCounter.sighting);
+      expect(createEventsByTypes.software.length).toBe(testCreatedCounter.software);
+      expect(createEventsByTypes['threat-actor'].length).toBe(testCreatedCounter['threat-actor']);
+      expect(createEventsByTypes.tool.length).toBe(testCreatedCounter.tool);
+      expect(createEventsByTypes['tracking-number'].length).toBe(testCreatedCounter['tracking-number']);
       expect(createEventsByTypes.vocabulary.length).toBe(VOCABULARY_NUMBERS);
-      expect(createEventsByTypes.vulnerability.length).toBe(7);
-      expect(createEvents.length).toBe(832);
+      expect(createEventsByTypes.vulnerability.length).toBe(testCreatedCounter.vulnerability);
+
+      expect(createEvents.length).toBe(doTotal(testCreatedCounter));
       for (let createIndex = 0; createIndex < createEvents.length; createIndex += 1) {
         const { data: insideData, origin, type } = createEvents[createIndex];
         expect(origin).toBeDefined();
@@ -59,6 +81,7 @@ describe('Raw streams tests', () => {
       // 02 - CHECK UPDATE EVENTS.
       const updateEvents = events.filter((e) => e.type === EVENT_TYPE_UPDATE);
       const updateEventsByTypes = R.groupBy((e) => e.data.data.type, updateEvents);
+      dumpEventByTypeToFile('update', updateEventsByTypes);
       expect(updateEventsByTypes['marking-definition'].length).toBe(2);
       expect(updateEventsByTypes['campaign'].length).toBe(7);
       expect(updateEventsByTypes['relationship'].length).toBe(8);
@@ -99,7 +122,9 @@ describe('Raw streams tests', () => {
       }
       // 03 - CHECK DELETE EVENTS
       const deleteEvents = events.filter((e) => e.type === EVENT_TYPE_DELETE);
-      expect(deleteEvents.length).toBe(175);
+      const deleteEventsByTypes = R.groupBy((e) => e.data.data.type, deleteEvents);
+      dumpEventByTypeToFile('delete', deleteEventsByTypes);
+      expect(deleteEvents.length).toBe(doTotal(testDeletedCounter));
       // const deleteEventsByTypes = R.groupBy((e) => e.data.data.type, deleteEvents);
       for (let delIndex = 0; delIndex < deleteEvents.length; delIndex += 1) {
         const { data: insideData, origin, type } = deleteEvents[delIndex];
