11140e2478ce8ea39189448d0783119fe19f59c2
Gwendoline Favre-Felix <gwendoline.favre-felix@filigran.io>
2025-05-30 14:25:37 +0200

    [frontend] WIP SecurityPlatformEdition (#10452)

---
+++ opencti-platform/opencti-front/src/private/components/entities/securityPlatforms/SecurityPlatformEditionContainer.tsx
@@ -0,0 +1,61 @@
+import React, { FunctionComponent } from 'react';
+import Drawer from '@components/common/drawer/Drawer';
+import { createFragmentContainer, graphql } from 'react-relay';
+import {
+  SecurityPlatformEditionContainer_securityPlatform$data,
+} from '@components/entities/securityPlatforms/__generated__/SecurityPlatformEditionContainer_securityPlatform.graphql';
+import SecurityPlatformEditionOverview from '@components/entities/securityPlatforms/SecurityPlatformEditionOverview';
+import { useFormatter } from '../../../../components/i18n';
+import { useIsEnforceReference } from '../../../../utils/hooks/useEntitySettings';
+
+interface securityPlatformContainerProps {
+  handleClose: () => void;
+  securityPlatform:SecurityPlatformEditionContainer_securityPlatform$data,
+  open: boolean,
+  controlledDial:() => void,
+}
+
+const SecurityPlatformEditionContainer: FunctionComponent<securityPlatformContainerProps> = ({
+  handleClose,
+  securityPlatform,
+  open,
+  controlledDial,
+}) => {
+  const { t_i18n } = useFormatter();
+  const { editContext } = securityPlatform;
+
+  return (
+    <Drawer
+      title={t_i18n('Update an security platform')}
+      open={open}
+      onClose={handleClose}
+      context={editContext}
+      controlledDial={controlledDial}
+    >
+      <SecurityPlatformEditionOverview
+        securityPlatform={securityPlatform}
+        enableReferences={useIsEnforceReference('SecurityPlatform')}
+        context={editContext}
+        handleClose={handleClose}
+      />
+    </Drawer>
+  );
+};
+
+const SecurityPlatformEditionFragment = createFragmentContainer(
+  SecurityPlatformEditionContainer,
+  {
+    securityPlatform: graphql`
+        fragment SecurityPlatformEditionContainer_securityPlatform on SecurityPlatform {
+            id
+            ...SecurityPlatformEditionOverview_securityPlatform
+            editContext {
+                name
+                focusOn
+            }
+        }
+    `,
+  },
+);
+
+export default SecurityPlatformEditionContainer;
