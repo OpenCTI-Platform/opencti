0305802a0d4db0057ddd7a693bf3e905b79ea7c0
Celine Sebe <celine.sebe@filigran.io>
2025-06-04 16:10:17 +0200

    fix creation relationships

--- opencti-platform/opencti-front/src/private/components/entities/securityPlatforms/SecurityPlatformKnowledge.tsx
+++ opencti-platform/opencti-front/src/private/components/entities/securityPlatforms/SecurityPlatformKnowledge.tsx
@@ -7,7 +7,6 @@ import StixDomainObjectAuthorKnowledge from '@components/common/stix_domain_obje
 import EntityStixCoreRelationships from '../../common/stix_core_relationships/EntityStixCoreRelationships';
 import StixCoreRelationship from '../../common/stix_core_relationships/StixCoreRelationship';
 import StixDomainObjectAttackPatterns from '../../common/stix_domain_objects/StixDomainObjectAttackPatterns';
-import StixSightingRelationship from '../../events/stix_sighting_relationships/StixSightingRelationship';
 import EntityStixCoreRelationshipsIndicators from '../../common/stix_core_relationships/views/indicators/EntityStixCoreRelationshipsIndicators';
 import { getRelationshipTypesForEntityType } from '../../../../utils/Relation';
 import useAuth from '../../../../utils/hooks/useAuth';
@@ -38,153 +37,141 @@ const SecurityPlatformKnowledgeComponent = ({
   const link = `/dashboard/entities/security_platforms/${securityPlatform.id}/knowledge`;
   const { schema } = useAuth();
   const allRelationshipsTypes = getRelationshipTypesForEntityType(securityPlatform.entity_type, schema);
-  console.log('allRelationshipsTypes', allRelationshipsTypes);
   return (
-    <Routes>
-      <Route
-        path="/relations/:relationId"
-        element={
-          <StixCoreRelationship
-            entityId={securityPlatform.id}
-            paddingRight={true}
-          />
+    <>
+      <Routes>
+        <Route
+          path="/relations/:relationId"
+          element={
+            <StixCoreRelationship
+              entityId={securityPlatform.id}
+              paddingRight={true}
+            />
         }
-      />
-      <Route
-        path="/sightings/:sightingId"
-        element={
-          <StixSightingRelationship
-            entityId={securityPlatform.id}
-            paddingRight={true}
-          />
+        />
+        <Route
+          path="/overview"
+          element={(viewAs === 'knowledge' ? (
+            <StixDomainObjectKnowledge
+              stixDomainObjectId={securityPlatform.id}
+              stixDomainObjectType="SecurityPlatform"
+            />
+          ) : (
+            <StixDomainObjectAuthorKnowledge
+              stixDomainObjectId={securityPlatform.id}
+              stixDomainObjectType="SecurityPlatform"
+            />
+          ))
         }
-      />
-      <Route
-        path="/overview"
-        element={(viewAs === 'knowledge' ? (
-          <StixDomainObjectKnowledge
-            stixDomainObjectId={securityPlatform.id}
-            stixDomainObjectType="SecurityPlatform"
-          />
-        ) : (
-          <StixDomainObjectAuthorKnowledge
-            stixDomainObjectId={securityPlatform.id}
-            stixDomainObjectType="SecurityPlatform"
-          />
-        ))
+        />
+        <Route
+          path="/all"
+          element={
+            <EntityStixCoreRelationships
+              key={location.pathname}
+              entityId={securityPlatform.id}
+              relationshipTypes={allRelationshipsTypes}
+              entityLink={link}
+              allDirections
+              currentView={''}
+              enableContextualView={true}
+              isRelationReversed={true}
+            />
         }
-      />
-      <Route
-        path="/all"
-        element={
-          <EntityStixCoreRelationships
-            key={location.pathname}
-            entityId={securityPlatform.id}
-            relationshipTypes={allRelationshipsTypes}
-            entityLink={link}
-            defaultStartTime={null}
-            defaultStopTime={null}
-            allDirections
-            stixCoreObjectTypes={['']}
-            currentView={''}
-            enableContextualView={true}
-            isRelationReversed={true}
-          />
+        />
+        <Route
+          path="/systems"
+          element={
+            <EntityStixCoreRelationships
+              key={location.pathname}
+              entityId={securityPlatform.id}
+              relationshipTypes={['part-of']}
+              stixCoreObjectTypes={['System']}
+              entityLink={link}
+              currentView={''}
+              enableContextualView={true}
+              isRelationReversed={true}
+            />
         }
-      />
-      <Route
-        path="/related"
-        element={
-          <EntityStixCoreRelationships
-            key={location.pathname}
-            entityId={securityPlatform.id}
-            relationshipTypes={['related-to']}
-            entityLink={link}
-            defaultStartTime={null}
-            defaultStopTime={null}
-            allDirections
-            stixCoreObjectTypes={['']}
-            currentView={''}
-            enableContextualView={true}
-            isRelationReversed={true}
-          />
+        />
+        <Route
+          path="/related"
+          element={
+            <EntityStixCoreRelationships
+              key={location.pathname}
+              entityId={securityPlatform.id}
+              relationshipTypes={['related-to']}
+              entityLink={link}
+              allDirections
+              stixCoreObjectTypes={['Threat-Actor',
+                'Intrusion-Set',
+                'Campaign',
+                'Incident',
+                'Malware',
+                'Tool',
+                'Vulnerability',
+                'Individual',
+                'Organization',
+                'Sector',
+                'Region',
+                'Country',
+                'City',
+                'Position']}
+              currentView={''}
+              enableContextualView={true}
+              isRelationReversed={true}
+            />
         }
-      />
-      <Route
-        path="/attack_patterns"
-        element={
-          <StixDomainObjectAttackPatterns
-            stixDomainObjectId={securityPlatform.id}
-            defaultStartTime={null}
-            defaultStopTime={null}
-            disableExport={false}
-          />
+        />
+        <Route
+          path="/attack_patterns"
+          element={
+            <StixDomainObjectAttackPatterns
+              stixDomainObjectId={securityPlatform.id}
+              disableExport={false}
+            />
         }
-      />
-      <Route
-        path="/tools"
-        element={
-          <EntityStixCoreRelationships
-            key={location.pathname}
-            entityId={securityPlatform.id}
-            relationshipTypes={['targets', 'should-cover']}
-            stixCoreObjectTypes={['Tool']}
-            entityLink={link}
-            defaultStartTime={null}
-            defaultStopTime={null}
-            currentView={''}
-            enableContextualView={true}
-            isRelationReversed={true}
-          />
+        />
+        <Route
+          path="/tools"
+          element={
+            <EntityStixCoreRelationships
+              key={location.pathname}
+              entityId={securityPlatform.id}
+              stixCoreObjectTypes={['Tool']}
+              relationshipTypes={['targets']}
+              entityLink={link}
+              currentView={''}
+              enableContextualView={true}
+              isRelationReversed={true}
+            />
         }
-      />
-      <Route
-        path="/organizations"
-        element={
-          <EntityStixCoreRelationships
-            key={location.pathname}
-            entityId={securityPlatform.id}
-            relationshipTypes={['part-of', 'derived-from']}
-            stixCoreObjectTypes={['Organization']}
-            entityLink={link}
-            defaultStartTime={null}
-            defaultStopTime={null}
-            currentView={''}
-            enableContextualView={true}
-            isRelationReversed={true}
-          />
+        />
+        <Route
+          path="/indicators"
+          element={
+            <EntityStixCoreRelationshipsIndicators
+              entityId={securityPlatform.id}
+              entityLink={link}
+            />
         }
-      />
-      <Route
-        path="/indicators"
-        element={
-          <EntityStixCoreRelationshipsIndicators
-            entityId={securityPlatform.id}
-            entityLink={link}
-            defaultStartTime={null}
-            defaultStopTime={null}
-            relationshipTypes={['should-cover']}
-          />
+        />
+        <Route
+          path="/infrastructures"
+          element={
+            <EntityStixCoreRelationships
+              key={location.pathname}
+              entityId={securityPlatform.id}
+              relationshipTypes={['uses', 'compromises']}
+              stixCoreObjectTypes={['Infrastructure']}
+              entityLink={link}
+              isRelationReversed={false}
+              currentView={''}
+            />
         }
-      />
-      <Route
-        path="/infrastructures"
-        element={
-          <EntityStixCoreRelationships
-            key={location.pathname}
-            entityId={securityPlatform.id}
-            relationshipTypes={['uses', 'compromises']}
-            stixCoreObjectTypes={['Infrastructure']}
-            entityLink={link}
-            isRelationReversed={false}
-            defaultStartTime={null}
-            defaultStopTime={null}
-            currentView={''}
-            enableContextualView
-          />
-        }
-      />
-    </Routes>
+        />
+      </Routes>
+    </>
   );
 };
 
