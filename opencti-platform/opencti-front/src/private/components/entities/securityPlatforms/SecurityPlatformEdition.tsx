a912ccb52f2fdb95064216f753a929a59301ecfb
Gwendoline Favre-Felix <gwendoline.favre-felix@filigran.io>
2025-06-04 09:25:09 +0200

    [frontend] Finish SecurityPlatformEdition (#10452)

--- opencti-platform/opencti-front/src/private/components/entities/securityPlatforms/SecurityPlatformEdition.tsx
+++ opencti-platform/opencti-front/src/private/components/entities/securityPlatforms/SecurityPlatformEdition.tsx
@@ -1,10 +1,11 @@
-import { graphql, QueryRenderer, commitMutation } from 'react-relay';
+import { graphql, commitMutation } from 'react-relay';
 import React, { FunctionComponent } from 'react';
 import SecurityPlatformEditionContainer from '@components/entities/securityPlatforms/SecurityPlatformEditionContainer';
 import { securityPlatformEditionOverviewFocus } from '@components/entities/securityPlatforms/SecurityPlatformEditionOverview';
-import { environment } from '../../../../relay/environment';
+import { SecurityPlatformEditionContainerQuery$data } from '@components/entities/securityPlatforms/__generated__/SecurityPlatformEditionContainerQuery.graphql';
+import { environment, QueryRenderer } from '../../../../relay/environment';
 import EditEntityControlledDial from '../../../../components/EditEntityControlledDial';
-import Loader from '../../../../components/Loader';
+import Loader, { LoaderVariant } from '../../../../components/Loader';
 
 export const securityPlatformEditionQuery = graphql`
 query SecurityPlatformEditionContainerQuery($id: String!) {
@@ -33,11 +34,10 @@ const SecurityPlatformEdition : FunctionComponent<SecurityPlatformEditionProps> 
 
   return (
     <QueryRenderer
-      environment={environment}
       query={securityPlatformEditionQuery}
       variables={{ id: securityPlatformId }}
-      render={({ props }) => {
-        if (props) {
+      render={({ props }: { props: SecurityPlatformEditionContainerQuery$data }) => {
+        if (props && props.securityPlatform) {
           return (
             <SecurityPlatformEditionContainer
               securityPlatform={props.securityPlatform}
@@ -46,7 +46,7 @@ const SecurityPlatformEdition : FunctionComponent<SecurityPlatformEditionProps> 
             />
           );
         }
-        return <Loader variant="inline" />;
+        return <Loader variant={LoaderVariant.inline} />;
       }}
     />
   );
