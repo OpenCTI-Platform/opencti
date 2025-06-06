03e7ef30689fec38febff95a030235e30742d2e3
Laurent Bonnet <laurent.bonnet@filigran.io>
2025-06-06 12:34:00 +0200

    [frontend] fix styling of worker status in connectors view

--- opencti-platform/opencti-front/src/private/components/data/connectors/WorkersStatus.jsx
+++ opencti-platform/opencti-front/src/private/components/data/connectors/WorkersStatus.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import * as PropTypes from 'prop-types';
 import { compose, pathOr } from 'ramda';
 import { interval } from 'rxjs';
-import withStyles from '@mui/styles/withStyles';
 import Grid from '@mui/material/Grid';
 import { createRefetchContainer, graphql } from 'react-relay';
 import Typography from '@mui/material/Typography';
@@ -12,35 +11,6 @@ import { FIVE_SECONDS } from '../../../../utils/Time';
 import inject18n from '../../../../components/i18n';
 
 const interval$ = interval(FIVE_SECONDS);
-
-const styles = (theme) => ({
-  card: {
-    borderRadius: 4,
-  },
-  metric: {
-    margin: '0 auto',
-    textAlign: 'center',
-  },
-  number: {
-    color: theme.palette.primary.main,
-    fontSize: 40,
-    lineHeight: '60px',
-    verticalAlign: 'middle',
-  },
-  date: {
-    color: theme.palette.primary.main,
-    fontSize: 20,
-    lineHeight: '60px',
-    verticalAlign: 'middle',
-  },
-  title: {
-    textTransform: 'uppercase',
-    fontSize: 12,
-  },
-  icon: {
-    color: theme.palette.primary.main,
-  },
-});
 
 class WorkersStatusComponent extends Component {
   constructor(props) {
@@ -66,7 +36,7 @@ class WorkersStatusComponent extends Component {
   }
 
   render() {
-    const { classes, t, n, data, theme } = this.props;
+    const { t, n, data, theme } = this.props;
     const { consumers, overview } = data.rabbitMQMetrics;
     const { docs, search, indexing } = data.elasticSearchMetrics;
     const currentReadOperations = Number(search.query_total);
@@ -79,66 +49,63 @@ class WorkersStatusComponent extends Component {
     if (this.lastWriteOperations !== 0) {
       writeOperations = (currentWriteOperations - this.lastWriteOperations) / 5;
     }
+
+    const paperStyle = {
+      display: 'flex',
+      padding: theme.spacing(2),
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      flexDirection: 'column',
+      height: '100%',
+    };
+    const numberStyle = {
+      color: theme.palette.primary.main,
+      fontSize: 32,
+      lineHeight: '60px',
+      verticalAlign: 'middle',
+    };
+
     return (
       <Grid
         container={true}
         spacing={3}
-        style={{ paddingBottom: 0, height: '100%' }}
       >
-        <Grid item xs={2} style={{ height: '25%' }}>
+        <Grid item xs={2}>
           <Paper
             variant="outlined"
-            style={{
-              display: 'flex',
-              padding: theme.spacing(2),
-              justifyContent: 'center',
-              alignItems: 'center',
-              flexDirection: 'column',
-            }}
+            style={paperStyle}
             className={'paper-for-grid'}
           >
             <Typography variant={'h5'}>
               {t('Connected workers')}
             </Typography>
-            <div className={classes.number}>{n(consumers)}</div>
+            <div style={numberStyle}>{n(consumers)}</div>
           </Paper>
         </Grid>
-        <Grid item xs={2} style={{ height: '25%' }}>
+        <Grid item xs={2} >
           <Paper
             variant="outlined"
-            style={{
-              display: 'flex',
-              padding: theme.spacing(2),
-              justifyContent: 'center',
-              alignItems: 'center',
-              flexDirection: 'column',
-            }}
+            style={paperStyle}
             className={'paper-for-grid'}
           >
             <Typography variant={'h5'}>
               {t('Queued bundles')}
             </Typography>
-            <div className={classes.number}>
+            <div style={numberStyle}>
               {n(pathOr(0, ['queue_totals', 'messages'], overview))}
             </div>
           </Paper>
         </Grid>
-        <Grid item xs={2} style={{ height: '25%' }}>
+        <Grid item xs={2} >
           <Paper
             variant="outlined"
-            style={{
-              display: 'flex',
-              padding: theme.spacing(2),
-              justifyContent: 'center',
-              alignItems: 'center',
-              flexDirection: 'column',
-            }}
+            style={paperStyle}
             className={'paper-for-grid'}
           >
             <Typography variant={'h5'}>
               {t('Bundles processed')}
             </Typography>
-            <div className={classes.number}>
+            <div style={numberStyle}>
               {n(
                 pathOr(
                   0,
@@ -150,59 +117,40 @@ class WorkersStatusComponent extends Component {
             </div>
           </Paper>
         </Grid>
-        <Grid item xs={2} style={{ height: '25%' }}>
+        <Grid item xs={2} >
           <Paper
             variant="outlined"
-            style={{
-              display: 'flex',
-              padding: theme.spacing(2),
-              justifyContent: 'center',
-              alignItems: 'center',
-              flexDirection: 'column',
-            }}
+            style={paperStyle}
             className={'paper-for-grid'}
           >
             <Typography variant={'h5'}>
               {t('Read operations')}
             </Typography>
-            <div className={classes.number}>{n(readOperations)}/s</div>
+            <div style={numberStyle}>{n(readOperations)}/s</div>
           </Paper>
         </Grid>
-        <Grid item xs={2} style={{ height: '25%' }}>
+        <Grid item xs={2} >
           <Paper
             variant="outlined"
-            style={{
-              display: 'flex',
-              padding: theme.spacing(2),
-              justifyContent: 'center',
-              alignItems: 'center',
-              flexDirection: 'column',
-            }}
+            style={paperStyle}
             className={'paper-for-grid'}
           >
             <Typography variant={'h5'}>
               {t('Write operations')}
             </Typography>
-            <div className={classes.number}>{n(writeOperations)}/s</div>
+            <div style={numberStyle}>{n(writeOperations)}/s</div>
           </Paper>
         </Grid>
-        <Grid item xs={2} style={{ height: '25%' }}>
+        <Grid item xs={2} >
           <Paper
             variant="outlined"
-            style={{
-              display: 'flex',
-              padding: theme.spacing(2),
-              justifyContent: 'center',
-              alignItems: 'center',
-              flexDirection: 'column',
-              overflow: 'hidden',
-            }}
+            style={paperStyle}
             className={'paper-for-grid'}
           >
-            <Typography variant={'h5'} style={{ textWrap: 'nowrap', textOverflow: 'ellipsis' }}>
+            <Typography variant={'h5'} >
               {t('Total number of documents')}
             </Typography>
-            <div className={classes.number}>{n(docs.count)}</div>
+            <div style={numberStyle}>{n(docs.count)}</div>
           </Paper>
         </Grid>
       </Grid>
@@ -267,4 +215,4 @@ const WorkersStatus = createRefetchContainer(
   workersStatusQuery,
 );
 
-export default compose(inject18n, withStyles(styles), withTheme)(WorkersStatus);
+export default compose(inject18n, withTheme)(WorkersStatus);
