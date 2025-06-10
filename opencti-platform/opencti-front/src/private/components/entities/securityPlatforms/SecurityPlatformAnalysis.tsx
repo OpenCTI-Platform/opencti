3d68867b280436f229042fcccf4b7d028f732a00
Celine Sebe <celine.sebe@filigran.io>
2025-06-10 15:49:49 +0200

    fix analysis

--- opencti-platform/opencti-front/src/private/components/entities/securityPlatforms/SecurityPlatformAnalysis.tsx
+++ opencti-platform/opencti-front/src/private/components/entities/securityPlatforms/SecurityPlatformAnalysis.tsx
@@ -1,32 +1,27 @@
 import React, { FunctionComponent } from 'react';
-import { graphql, createFragmentContainer } from 'react-relay';
+import { graphql, useFragment } from 'react-relay';
 import { SecurityPlatformAnalysis_securityPlatform$key } from '@components/entities/securityPlatforms/__generated__/SecurityPlatformAnalysis_securityPlatform.graphql';
 import StixCoreObjectOrStixCoreRelationshipContainers from '../../common/containers/StixCoreObjectOrStixCoreRelationshipContainers';
 
 interface SecurityPlatformAnalysisComponentProps {
   securityPlatform: SecurityPlatformAnalysis_securityPlatform$key;
 }
-
-const SecurityPlatformAnalysisComponent: FunctionComponent<SecurityPlatformAnalysisComponentProps> = ({ securityPlatform }) => {
-  return (
-    <StixCoreObjectOrStixCoreRelationshipContainers
-      stixDomainObjectOrStixCoreRelationship={securityPlatform}
-    />
-  );
-};
-
-const SecurityPlatformAnalysis = createFragmentContainer(
-  SecurityPlatformAnalysisComponent,
-  {
-    securityPlatform: graphql`
-      fragment SecurityPlatformAnalysis_securityPlatform on SecurityPlatform {
+const SecurityPlatformAnalysisFragment = graphql`
+    fragment SecurityPlatformAnalysis_securityPlatform on SecurityPlatform {
         id
         name
         x_opencti_aliases
         x_opencti_graph_data
-      }
-    `,
-  },
-);
+    }
+`;
+const SecurityPlatformAnalysis: FunctionComponent<SecurityPlatformAnalysisComponentProps> = ({ securityPlatform }) => {
+  const securityPlatformAnalysis = useFragment(SecurityPlatformAnalysisFragment, securityPlatform);
+
+  return (
+    <StixCoreObjectOrStixCoreRelationshipContainers
+      stixDomainObjectOrStixCoreRelationship={securityPlatformAnalysis}
+    />
+  );
+};
 
 export default SecurityPlatformAnalysis;
