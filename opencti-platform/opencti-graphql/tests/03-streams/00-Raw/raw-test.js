f991f9bd9044f1299f107f01fe62096b05b0e14a
Jeremy Cloarec <159018898+JeremyCloarec@users.noreply.github.com>
2025-06-11 11:26:09 +0200

    [backend] Fix merge counters (#11269)

--- opencti-platform/opencti-graphql/tests/03-streams/00-Raw/raw-test.js
+++ opencti-platform/opencti-graphql/tests/03-streams/00-Raw/raw-test.js
@@ -30,7 +30,7 @@ describe('Raw streams tests', () => {
       expect(createEventsByTypes.location.length).toBe(19);
       expect(createEventsByTypes.relationship.length).toBe(133);
       expect(createEventsByTypes.sighting.length).toBe(4);
-      expect(createEventsByTypes.indicator.length).toBe(49);
+      expect(createEventsByTypes.indicator.length).toBe(43);
       expect(createEventsByTypes['attack-pattern'].length).toBe(9);
       expect(createEventsByTypes['threat-actor'].length).toBe(17);
       expect(createEventsByTypes['observed-data'].length).toBe(1);
@@ -50,7 +50,7 @@ describe('Raw streams tests', () => {
       // 328 created at init + 1 request access + 2 created in tests + 5 vocabulary organizations types + 7 persona
       expect(createEventsByTypes.vocabulary.length).toBe(VOCABULARY_NUMBERS);
       expect(createEventsByTypes.vulnerability.length).toBe(7);
-      expect(createEvents.length).toBe(851);
+      expect(createEvents.length).toBe(829);
       for (let createIndex = 0; createIndex < createEvents.length; createIndex += 1) {
         const { data: insideData, origin, type } = createEvents[createIndex];
         expect(origin).toBeDefined();
@@ -86,7 +86,7 @@ describe('Raw streams tests', () => {
       expect(updateEventsByTypes['threat-actor'].length).toBe(17);
       expect(updateEventsByTypes['vocabulary'].length).toBe(3);
       expect(updateEventsByTypes['vulnerability'].length).toBe(3);
-      expect(updateEvents.length).toBe(207);
+      expect(updateEvents.length).toBe(201);
       for (let updateIndex = 0; updateIndex < updateEvents.length; updateIndex += 1) {
         const event = updateEvents[updateIndex];
         const { data: insideData, origin, type } = event;
@@ -99,7 +99,7 @@ describe('Raw streams tests', () => {
       }
       // 03 - CHECK DELETE EVENTS
       const deleteEvents = events.filter((e) => e.type === EVENT_TYPE_DELETE);
-      expect(deleteEvents.length).toBe(200);
+      expect(deleteEvents.length).toBe(166);
       // const deleteEventsByTypes = R.groupBy((e) => e.data.data.type, deleteEvents);
       for (let delIndex = 0; delIndex < deleteEvents.length; delIndex += 1) {
         const { data: insideData, origin, type } = deleteEvents[delIndex];
