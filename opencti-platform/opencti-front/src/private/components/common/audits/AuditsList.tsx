b7cebcf749a98f457675456edcbb6daead44f322
Cathia Archidoit <cathia.archidoit@filigran.io>
2025-06-12 16:11:45 +0200

    [frontend] fix imports

--- opencti-platform/opencti-front/src/private/components/common/audits/AuditsList.tsx
+++ opencti-platform/opencti-front/src/private/components/common/audits/AuditsList.tsx
@@ -15,8 +15,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
 import React, { FunctionComponent } from 'react';
 import AuditsListContent, { auditsListContentQuery } from './AuditsListContent';
-import { AuditsListContentQuery } from './__generated__/AuditsListContentQuery.graphql';
-import { LogsOrdering, OrderingMode } from './__generated__/AuditsListQuery.graphql';
+import { AuditsListContentQuery, LogsOrdering, OrderingMode } from './__generated__/AuditsListContentQuery.graphql';
 import { useFormatter } from '../../../../components/i18n';
 import useGranted, { SETTINGS_SECURITYACTIVITY, SETTINGS_SETACCESSES, VIRTUAL_ORGANIZATION_ADMIN } from '../../../../utils/hooks/useGranted';
 import useEnterpriseEdition from '../../../../utils/hooks/useEnterpriseEdition';
