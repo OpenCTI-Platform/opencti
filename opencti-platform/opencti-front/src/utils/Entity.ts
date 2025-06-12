0133dc562fb00b4c493eb135d8e4c95a57afb613
Celine Sebe <celine.sebe@filigran.io>
2025-06-12 14:51:56 +0200

    fix name in workbench

--- opencti-platform/opencti-front/src/utils/Entity.ts
+++ opencti-platform/opencti-front/src/utils/Entity.ts
@@ -190,6 +190,8 @@ export const resolveIdentityType = (identityClass: string): string => {
   }
   if (identityClass === 'system') {
     return 'System';
+  } if (identityClass === 'securityplatform') {
+    return 'SecurityPlatform';
   }
   return 'Organization';
 };
