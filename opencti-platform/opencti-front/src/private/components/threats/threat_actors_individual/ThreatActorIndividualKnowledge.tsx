366ab1e65972d70c2436b4a6fc31bb19e989cd46
Cathia Archidoit <cathia.archidoit@filigran.io>
2025-06-26 16:32:48 +0200

    [frontend] display view buttons in Attack patterns view of Malwares (#11517)

--- opencti-platform/opencti-front/src/private/components/threats/threat_actors_individual/ThreatActorIndividualKnowledge.tsx
+++ opencti-platform/opencti-front/src/private/components/threats/threat_actors_individual/ThreatActorIndividualKnowledge.tsx
@@ -172,6 +172,7 @@ const ThreatActorIndividualKnowledgeComponent = ({
             stixDomainObjectId={threatActorIndividual.id}
             defaultStartTime={threatActorIndividual.first_seen}
             defaultStopTime={threatActorIndividual.last_seen}
+            entityType={threatActorIndividual.entity_type}
           />
         }
       />
