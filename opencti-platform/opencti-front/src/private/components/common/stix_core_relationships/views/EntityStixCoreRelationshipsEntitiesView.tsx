8ab229a27a598cab9decd101f7ba9be94f82c1ae
Bonsai8863 <131906254+Bonsai8863@users.noreply.github.com>
2025-06-26 12:32:25 -0400

    [frontend] Fixed reverse relations button
    [frontend] WIP: knowledge list lines not updating properly
    
    [frontend] WIP: Using insertNode

--- opencti-platform/opencti-front/src/private/components/common/stix_core_relationships/views/EntityStixCoreRelationshipsEntitiesView.tsx
+++ opencti-platform/opencti-front/src/private/components/common/stix_core_relationships/views/EntityStixCoreRelationshipsEntitiesView.tsx
@@ -158,16 +158,19 @@ EntityStixCoreRelationshipsEntitiesViewProps
       stixCoreObjectTypes,
       relationshipTypes,
       connectionKey: 'Pagination_stixCoreObjects',
+      handleReverseRelation,
     });
   }, []);
   useEffect(() => {
     setCreateRelationshipContext({
       reversed: reversedRelation,
+      handleReverseRelation,
     });
   }, [reversedRelation]);
   useEffect(() => {
     setCreateRelationshipContext({
       paginationOptions,
+      handleReverseRelation,
     });
   }, [localStorage]);
 
