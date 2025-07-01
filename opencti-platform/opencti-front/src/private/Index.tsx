2173c2abec71251024730d1ffaee396fedf9bfdb
Jeremy Cloarec <jeremy.cloarec@filigran.io>
2025-07-01 17:24:54 +0200

    [frontend] flowise embed react poc

--- opencti-platform/opencti-front/src/private/Index.tsx
+++ opencti-platform/opencti-front/src/private/Index.tsx
@@ -5,6 +5,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { useTheme } from '@mui/styles';
 import { boundaryWrapper, NoMatch } from '@components/Error';
 import PlatformCriticalAlertDialog from '@components/settings/platform_alerts/PlatformCriticalAlertDialog';
+import { BubbleChat } from 'flowise-embed-react';
 import TopBar from './components/nav/TopBar';
 import LeftBar from './components/nav/LeftBar';
 import Message from '../components/Message';
@@ -78,6 +79,82 @@ const Index = ({ settings }: IndexProps) => {
       }
     }
   }, [theme]);
+  const chatBoxTheme = {
+    button: {
+      backgroundColor: '#001BDA',
+      right: 20,
+      bottom: 20,
+      size: 48,
+      dragAndDrop: true,
+      iconColor: 'white',
+      customIconSrc: 'https://filigran.io/app/uploads/2025/05/ai-chat.png',
+      autoWindowOpen: {
+        autoOpen: false,
+        openDelay: 2,
+        autoOpenOnMobile: false,
+      },
+    },
+    tooltip: {
+      showTooltip: false,
+    },
+    customCSS: `
+          * {
+            font-family: "IBM Plex Sans" !important;
+          }
+        `,
+    chatWindow: {
+      showTitle: true,
+      showAgentMessages: false,
+      title: 'Ariane Docs Assistant',
+      titleAvatarSrc:
+          'https://filigran.io/app/uploads/2025/05/embleme_filigran_blanc.png',
+      welcomeMessage:
+          "Hi there 👋 You're speaking with an AI Agent. I'm here to answer your questions, so what brings you here today?",
+      errorMessage: 'Sorry, an error has occurred, please try again later.',
+      backgroundColor: '#ffffff',
+      height: 700,
+      width: 400,
+      fontSize: 14,
+      starterPromptFontSize: 13,
+      clearChatOnReload: false,
+      sourceDocsTitle: 'Sources:',
+      renderHTML: true,
+      botMessage: {
+        backgroundColor: '#f7f8ff',
+        textColor: '#000000',
+        showAvatar: true,
+        avatarSrc:
+            'https://filigran.io/app/uploads/2025/05/embleme_filigran_background.png',
+      },
+      userMessage: {
+        backgroundColor: '#001BDA',
+        textColor: '#ffffff',
+        showAvatar: false,
+      },
+      textInput: {
+        placeholder: 'Ask a question...',
+        backgroundColor: '#ffffff',
+        textColor: '#303235',
+        sendButtonColor: '#001BDA',
+        maxChars: 100,
+        maxCharsWarningMessage:
+            'You exceeded the characters limit. Please input less than 50 characters.',
+        autoFocus: true,
+        sendMessageSound: false,
+        receiveMessageSound: false,
+      },
+      dateTimeToggle: {
+        date: true,
+        time: true,
+      },
+      footer: {
+        textColor: '#303235',
+        text: 'Powered by',
+        company: 'Filigran Ariane AI',
+        companyLink: 'https://filigran.io',
+      },
+    },
+  };
   return (
     <>
       <SystemBanners settings={settings} />
@@ -95,6 +172,11 @@ const Index = ({ settings }: IndexProps) => {
         <CssBaseline />
         <TopBar />
         <LeftBar />
+        <BubbleChat
+          chatflowid="46a94d00-5058-4550-bf5f-39f908919836"
+          apiHost="https://platform.filigran.ai"
+          theme={chatBoxTheme}
+        />
         <Message />
         <Box component="main" sx={boxSx}>
           <Suspense fallback={<Loader />}>
