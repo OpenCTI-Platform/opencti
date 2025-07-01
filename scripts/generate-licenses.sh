62c3e6205d9c9e5efca3f6482f96a543c5e075a3
Adrien Servel <adrien.servel@filigran.io>
2025-07-01 12:08:17 +0200

    [frontend/backend] Try update with summary generation

--- scripts/generate-licenses.sh
+++ scripts/generate-licenses.sh
@@ -3,6 +3,6 @@
 mkdir ./licenses
 pwd
 ls
-cd ./opencti-platform/opencti-front && npx license-checker-rseidelsohn --production --includePackages "$all_deps_check" --summary --out ../../licenses/front_deps.txt
-cd ./opencti-graphql && npx license-checker-rseidelsohn --production --includePackages "$all_deps_check" --summary --out ../../licenses/back_deps.txt
+cd ./opencti-platform/opencti-front && npx --yes license-checker-rseidelsohn --production --includePackages "$all_deps_check" --summary --out ../../licenses/front_deps.txt
+cd ./opencti-graphql && npx --yes license-checker-rseidelsohn --production --includePackages "$all_deps_check" --summary --out ../../licenses/back_deps.txt
 cd ../.. && npx --yes generate-license-file --config generatelicenseconfig.json --no-spinner --ci
