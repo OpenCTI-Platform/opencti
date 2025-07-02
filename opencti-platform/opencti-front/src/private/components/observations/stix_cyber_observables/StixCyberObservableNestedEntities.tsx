280b44cdf5cda8931a2564055c886c089f141d19
Florian Delemarre <florian.delemarre@filigran.io>
2025-07-02 17:43:01 +0200

    [frontend] fix Datable popover
    [frontend] WIP

--- opencti-platform/opencti-front/src/private/components/observations/stix_cyber_observables/StixCyberObservableNestedEntities.tsx
+++ opencti-platform/opencti-front/src/private/components/observations/stix_cyber_observables/StixCyberObservableNestedEntities.tsx
@@ -1,17 +1,11 @@
-import React, { CSSProperties, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import { ArrowDropDown, ArrowDropUp } from '@mui/icons-material';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import StixNestedRefRelationCreationFromEntity from '../../common/stix_nested_ref_relationships/StixNestedRefRelationshipCreationFromEntity';
 import Security from '../../../../utils/Security';
 import { KNOWLEDGE_KNUPDATE } from '../../../../utils/hooks/useGranted';
 import SearchInput from '../../../../components/SearchInput';
-import { QueryRenderer } from '../../../../relay/environment';
-import StixCyberObservableNestedEntitiesLines, { stixCyberObservableNestedEntitiesLinesQuery } from './StixCyberObservableNestedEntitiesLines';
+import StixCyberObservableNestedEntitiesTable from './StixCyberObservableNestedEntitiesTable';
 import { useFormatter } from '../../../../components/i18n';
 
 interface StixCyberObservableNestedEntitiesProps {
@@ -20,136 +14,27 @@ interface StixCyberObservableNestedEntitiesProps {
   variant?: 'inLine' | undefined;
 }
 
-const inlineStylesHeaders: Record<string, CSSProperties> = {
-  iconSort: {
-    position: 'absolute',
-    margin: '0 0 0 5px',
-    padding: 0,
-    top: '0px',
-  },
-  relationship_type: {
-    float: 'left',
-    width: '10%',
-    fontSize: 12,
-    fontWeight: '700',
-    cursor: 'pointer',
-    whiteSpace: 'nowrap',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    paddingRight: 10,
-  },
-  entity_type: {
-    float: 'left',
-    width: '10%',
-    fontSize: 12,
-    fontWeight: '700',
-    whiteSpace: 'nowrap',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    paddingRight: 10,
-  },
-  name: {
-    float: 'left',
-    width: '22%',
-    fontSize: 12,
-    fontWeight: '700',
-    whiteSpace: 'nowrap',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    paddingRight: 10,
-  },
-  creator: {
-    float: 'left',
-    width: '12%',
-    fontSize: 12,
-    fontWeight: '700',
-    whiteSpace: 'nowrap',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    paddingRight: 10,
-  },
-  start_time: {
-    float: 'left',
-    width: '15%',
-    fontSize: 12,
-    fontWeight: '700',
-    cursor: 'pointer',
-    whiteSpace: 'nowrap',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    paddingRight: 10,
-  },
-  stop_time: {
-    float: 'left',
-    width: '15%',
-    fontSize: 12,
-    fontWeight: '700',
-    cursor: 'pointer',
-    whiteSpace: 'nowrap',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    paddingRight: 10,
-  },
-};
-
 const StixCyberObservableNestedEntities: React.FC<StixCyberObservableNestedEntitiesProps> = ({
   entityId,
   entityType,
   variant,
 }) => {
   const { t_i18n } = useFormatter();
-  const [sortBy, setSortBy] = useState<string | null>(null);
-  const [orderAsc, setOrderAsc] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
-
-  const handleSort = (field: string, isAsc: boolean) => {
-    setSortBy(field);
-    setOrderAsc(isAsc);
-  };
+  const isInLine = variant === 'inLine';
+  const isNetworkTraffic = entityType === 'Network-Traffic';
+  const targetStixCoreObjectTypes = useMemo(() => (isNetworkTraffic ? ['IPv4-Addr', 'IPv6-Addr', 'Domain-Name', 'Mac-Addr'] : undefined), [isNetworkTraffic]);
 
   const handleSearch = (value: string) => {
     setSearchTerm(value);
   };
 
-  const SortHeader = (field: string, label: string, isSortable: boolean) => {
-    const sortComponent = orderAsc ? (
-      <ArrowDropDown style={inlineStylesHeaders.iconSort} />
-    ) : (
-      <ArrowDropUp style={inlineStylesHeaders.iconSort} />
-    );
-    if (isSortable) {
-      return (
-        <div
-          style={inlineStylesHeaders[field as keyof typeof inlineStylesHeaders]}
-          onClick={() => handleSort(field, !orderAsc)}
-        >
-          <span>{t_i18n(label)}</span>
-          {sortBy === field ? sortComponent : ''}
-        </div>
-      );
-    }
-    return (
-      <div style={inlineStylesHeaders[field as keyof typeof inlineStylesHeaders]}>
-        <span>{t_i18n(label)}</span>
-      </div>
-    );
-  };
-
-  const getTargetStixCoreObjectTypes = () => {
-    if (entityType === 'Network-Traffic') {
-      return ['IPv4-Addr', 'IPv6-Addr', 'Domain-Name', 'Mac-Addr'];
-    }
-    return undefined;
-  };
-
-  const isInLine = variant === 'inLine';
   const paginationOptions = {
     fromOrToId: entityId,
     search: searchTerm,
-    orderBy: sortBy,
-    orderMode: orderAsc ? 'asc' : 'desc',
+    orderBy: isInLine ? 'relationship_type' : null,
+    orderMode: 'desc',
   };
-  const targetStixCoreObjectTypes = getTargetStixCoreObjectTypes();
 
   return (
     <div
@@ -157,7 +42,7 @@ const StixCyberObservableNestedEntities: React.FC<StixCyberObservableNestedEntit
         isInLine
           ? {
             height: 'auto',
-            marginTop: 20,
+            marginTop: 13,
             paddingBlock: 10,
           }
           : {
@@ -207,52 +92,11 @@ const StixCyberObservableNestedEntities: React.FC<StixCyberObservableNestedEntit
         elevation={0}
         variant={isInLine ? undefined : 'outlined'}
       >
-        <List style={{ marginTop: isInLine ? 0 : -10 }}>
-          <ListItem
-            style={{
-              paddingLeft: 10,
-              paddingTop: 0,
-              textTransform: 'uppercase',
-            }}
-            divider={false}
-            secondaryAction={<> &nbsp; </>}
-          >
-            <ListItemIcon>
-              <span
-                style={{
-                  padding: '0 8px 0 8px',
-                  fontWeight: 700,
-                  fontSize: 12,
-                }}
-              >
-                &nbsp;
-              </span>
-            </ListItemIcon>
-            <ListItemText
-              primary={
-                <div>
-                  {SortHeader('relationship_type', 'Attribute', true)}
-                  {SortHeader('entity_type', 'Entity type', false)}
-                  {SortHeader('name', 'Name', false)}
-                  {SortHeader('creator', 'Creator', false)}
-                  {SortHeader('start_time', 'First obs.', true)}
-                  {SortHeader('stop_time', 'Last obs.', true)}
-                </div>
-              }
-            />
-          </ListItem>
-          <QueryRenderer
-            query={stixCyberObservableNestedEntitiesLinesQuery}
-            variables={{ count: 200, ...paginationOptions }}
-            render={({ props }: { props: unknown }) => (
-              <StixCyberObservableNestedEntitiesLines
-                stixCyberObservableId={entityId}
-                paginationOptions={paginationOptions}
-                data={props}
-              />
-            )}
-          />
-        </List>
+        <StixCyberObservableNestedEntitiesTable
+          stixCyberObservableId={entityId}
+          searchTerm={searchTerm}
+          isInLine={isInLine}
+        />
       </Paper>
     </div>
   );
