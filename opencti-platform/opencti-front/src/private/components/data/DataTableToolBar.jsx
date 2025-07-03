a71f5bbcc642027331cf4743d48c9ba537020d2c
Cathia Archidoit <cathia.archidoit@filigran.io>
2025-07-03 14:46:47 +0200

    [frontend] fix deepscan (#11412)

--- opencti-platform/opencti-front/src/private/components/data/DataTableToolBar.jsx
+++ opencti-platform/opencti-front/src/private/components/data/DataTableToolBar.jsx
@@ -1823,7 +1823,7 @@ class DataTableToolBar extends Component {
           const { entityTypeFilterValues, selectedElementsList, selectedTypes } = this.getSelectedTypes(stixCyberObservableSubTypes, stixDomainObjectSubTypes);
           // Some filter types are high level, we do not want to check them as "Different"
           // We might need to add some other types here before refactoring the toolbar
-          const elementsTypes = selectedElementsList && selectedElementsList.length > 0
+          const elementsTypes = selectedElementsList.length > 0
             ? R.uniq(selectedElementsList.map((e) => e.entity_type))
             : selectedTypes;
           const typesAreDifferent = elementsTypes.filter((type) => !['Stix-Core-Object', 'Stix-Domain-Object', 'stix-core-relationship', 'Stix-Cyber-Observable'].includes(type)).length > 1;
