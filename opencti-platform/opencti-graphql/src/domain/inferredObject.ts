b2c5a35ea84ce1aaf850ea3b158a88789d3dd541
Jeremy Cloarec <jeremy.cloarec@filigran.io>
2025-07-09 14:30:10 +0200

    [backend] wip inferred in bundle task manager

---
+++ opencti-platform/opencti-graphql/src/domain/inferredObject.ts
@@ -0,0 +1,13 @@
+import { createInferredEntity, createInferredRelation } from '../database/middleware';
+import type { AuthContext, AuthUser } from '../types/user';
+
+export const createInternalInferredRelation = async (context: AuthContext, user: AuthUser, jsonInput: string) => {
+  const { input, ruleContent, opts } = JSON.parse(jsonInput);
+  const createdInferredRelation = await createInferredRelation(context, input, ruleContent, opts);
+  return createdInferredRelation.id;
+};
+export const createInternalInferredEntity = async (context: AuthContext, user: AuthUser, jsonInput: string) => {
+  const { input, ruleContent, type } = JSON.parse(jsonInput);
+  const createdInferredRelation = await createInferredEntity(context, input, ruleContent, type);
+  return createdInferredRelation.id;
+};
