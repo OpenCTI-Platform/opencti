44461e50dd8cf39d29c654098cf01d8a10f593de
Gwendoline Favre-Felix <gwendoline.favre-felix@filigran.io>
2025-07-09 14:05:06 +0200

    [frontend] Finish refacto users.tsx (#3343)

--- opencti-platform/opencti-front/src/private/components/data/DataTableToolBar.jsx
+++ opencti-platform/opencti-front/src/private/components/data/DataTableToolBar.jsx
@@ -66,7 +66,7 @@ import Checkbox from '@mui/material/Checkbox';
 import { objectParticipantFieldMembersSearchQuery } from '../common/form/ObjectParticipantField';
 import { objectAssigneeFieldMembersSearchQuery } from '../common/form/ObjectAssigneeField';
 import { vocabularyQuery } from '../common/form/OpenVocabField';
-import { usersLinesSearchQuery } from '../settings/users/UsersLines';
+import usersLinesSearchQuery from '../settings/users/UsersLines';
 import PromoteDrawer from './drawers/PromoteDrawer';
 import TasksFilterValueContainer from '../../../components/TasksFilterValueContainer';
 import inject18n from '../../../components/i18n';
--- opencti-platform/opencti-front/src/private/components/settings/users/UsersLines.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/users/UsersLines.tsx
@@ -1,14 +1,6 @@
-import React from 'react';
-import { graphql, RelayPaginationProp } from 'react-relay';
-import { UsersLinesPaginationQuery$variables } from '@components/settings/users/__generated__/UsersLinesPaginationQuery.graphql';
-import { UsersLines_data$data } from '@components/settings/users/__generated__/UsersLines_data.graphql';
-import ListLinesContent from '../../../../components/list_lines/ListLinesContent';
-import { UserLine, UserLineDummy } from './UserLine';
-import { DataColumns } from '../../../../components/list_lines';
+import { graphql } from 'react-relay';
 
-const nbOfRowsToLoad = 50;
-
-export const usersLinesSearchQuery = graphql`
+const usersLinesSearchQuery = graphql`
   query UsersLinesSearchQuery(
       $first: Int, $search: String,
       $orderBy: UsersOrdering
@@ -27,29 +19,4 @@ export const usersLinesSearchQuery = graphql`
   }
 `;
 
-interface UsersLinesProps {
-  initialLoading: boolean
-  dataColumns: DataColumns
-  relay: RelayPaginationProp,
-  paginationOptions: UsersLinesPaginationQuery$variables
-  data: UsersLines_data$data
-}
-
-const UsersLines: React.FC<UsersLinesProps> = (props) => {
-  const { initialLoading, dataColumns, relay, paginationOptions, data } = props;
-  return (
-    <ListLinesContent
-      initialLoading={initialLoading}
-      loadMore={relay.loadMore.bind(this)}
-      hasMore={relay.hasMore.bind(this)}
-      isLoading={relay.isLoading.bind(this)}
-      dataList={data?.users?.edges ?? []}
-      globalCount={data?.users?.pageInfo?.globalCount ?? nbOfRowsToLoad}
-      LineComponent={UserLine}
-      DummyLineComponent={UserLineDummy}
-      dataColumns={dataColumns}
-      nbOfRowsToLoad={nbOfRowsToLoad}
-      paginationOptions={paginationOptions}
-    />
-  );
-};
+export default usersLinesSearchQuery;
