2e070a59a3ba912d26bec49a1412235aaf19ff83
Angelique <angelique.jard@filigran.io>
2025-07-10 13:58:22 +0200

    After a real logout, we need to login again for next test

--- opencti-platform/opencti-front/tests_e2e/logout.spec.ts
+++ opencti-platform/opencti-front/tests_e2e/logout.spec.ts
@@ -1,11 +1,17 @@
 import { expect, test } from './fixtures/baseFixtures';
 import TopMenuProfilePage from './model/menu/topMenuProfile.pageModel';
 import LoginFormPageModel from './model/form/loginForm.pageModel';
+import DashboardPage from './model/dashboard.pageModel';
 
 test('test logout', async ({ page }) => {
   const loginPage = new LoginFormPageModel(page);
   const topMenu = new TopMenuProfilePage(page);
+  const dashboardPage = new DashboardPage(page);
+
   await page.goto('/');
   await topMenu.logout();
   await expect(loginPage.getPage()).toBeVisible();
+
+  await loginPage.login();
+  await expect(dashboardPage.getPage()).toBeVisible();
 });
