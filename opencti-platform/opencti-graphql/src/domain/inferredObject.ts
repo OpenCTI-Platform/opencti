6f492fa10846cf83f190d1faa33fd96476a3985f
Jeremy Cloarec <jeremy.cloarec@filigran.io>
2025-07-10 10:26:37 +0200

    [backend] change capa

--- opencti-platform/opencti-graphql/src/domain/inferredObject.ts
+++ opencti-platform/opencti-graphql/src/domain/inferredObject.ts
@@ -1,12 +1,12 @@
 import { createInferredEntity, createInferredRelation } from '../database/middleware';
 import type { AuthContext, AuthUser } from '../types/user';
 
-export const createInternalInferredRelation = async (context: AuthContext, user: AuthUser, jsonInput: string) => {
+export const createInternalInferredRelation = async (context: AuthContext, _user: AuthUser, jsonInput: string) => {
   const { input, ruleContent, opts } = JSON.parse(jsonInput);
   const createdInferredRelation = await createInferredRelation(context, input, ruleContent, opts);
   return createdInferredRelation.id;
 };
-export const createInternalInferredEntity = async (context: AuthContext, user: AuthUser, jsonInput: string) => {
+export const createInternalInferredEntity = async (context: AuthContext, _user: AuthUser, jsonInput: string) => {
   const { input, ruleContent, type } = JSON.parse(jsonInput);
   const createdInferredRelation = await createInferredEntity(context, input, ruleContent, type);
   return createdInferredRelation.id;
