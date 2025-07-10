bbb515af7affebfcc32c9f4483f1bc241044975b
Florian Delemarre <florian.delemarre@filigran.io>
2025-07-10 15:24:50 +0200

    [frontend] new shared organisations drawer (#11246)

---
+++ opencti-platform/opencti-front/src/private/components/common/stix_core_objects/StixCoreObjectSharedOrganisations.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import { useFragment } from 'react-relay';
+import { StixCoreObjectSharingListFragment$key } from '@components/common/stix_core_objects/__generated__/StixCoreObjectSharingListFragment.graphql';
+import { objectOrganizationFragment } from '@components/common/stix_core_objects/StixCoreObjectSharingList';
+import { useFormatter } from '../../../../components/i18n';
+import useGranted, { SETTINGS_SETACCESSES } from '../../../../utils/hooks/useGranted';
+import StixCoreObjectSharedOrganisationsDrawer from '../containers/StixCoreObjectSharedOrganisationsDrawer';
+
+interface ContainerHeaderSharedProps {
+  data: StixCoreObjectSharingListFragment$key
+}
+
+const StixCoreObjectSharedOrganisations = ({
+  data,
+}: ContainerHeaderSharedProps) => {
+  const { t_i18n } = useFormatter();
+  const [openSharedOrganizations, setOpenSharedOrganizations] = useState(false);
+  const hasSetAccess = useGranted([SETTINGS_SETACCESSES]);
+
+  const {
+    objectOrganization,
+    id,
+  } = useFragment<StixCoreObjectSharingListFragment$key>(objectOrganizationFragment, data);
+
+  if (!objectOrganization) {
+    return (<div/>);
+  }
+  return (
+    <React.Fragment>
+      <Button
+        size="small"
+        variant="text"
+        color={hasSetAccess ? 'primary' : 'inherit'}
+        style={{
+          cursor: hasSetAccess ? 'pointer' : 'default',
+          marginRight: 10,
+          whiteSpace: 'nowrap',
+        }}
+        sx={!hasSetAccess ? {
+          '&.MuiButtonBase-root:hover': {
+            bgcolor: 'transparent',
+          },
+        } : undefined}
+        onClick={() => (hasSetAccess ? setOpenSharedOrganizations(true) : null)}
+        disableRipple={!hasSetAccess}
+      >
+        {objectOrganization.length} {t_i18n('organizations')}
+      </Button>
+      {(hasSetAccess && openSharedOrganizations) && (
+        <StixCoreObjectSharedOrganisationsDrawer
+          stixCoreObjectId={id}
+          sharedOrganisations={[...objectOrganization]}
+          open={openSharedOrganizations}
+          onClose={() => setOpenSharedOrganizations(false)}
+        />
+      )}
+    </React.Fragment>
+  );
+};
+
+export default StixCoreObjectSharedOrganisations;
