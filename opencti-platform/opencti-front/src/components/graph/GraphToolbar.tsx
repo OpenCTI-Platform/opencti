979f43d518f11a3e1fe660717a5a42dad87a339c
BocognanoSarah <sarah.bocognano@filigran.io>
2025-07-11 09:59:52 +0200

    [frontend] hide scrollbar

--- opencti-platform/opencti-front/src/components/graph/GraphToolbar.tsx
+++ opencti-platform/opencti-front/src/components/graph/GraphToolbar.tsx
@@ -64,15 +64,17 @@ const GraphToolbar = ({
           visibility: showLinearProgress || isLoadingData ? 'visible' : 'hidden',
         }}
       />
-      <div style={{
-        height: 54,
-        flex: '0 0 auto',
-        display: 'flex',
-        alignItems: 'center',
-        gap: theme.spacing(0.5),
-        padding: `0 ${theme.spacing(1)}`,
-        overflow: 'scroll',
-      }}
+      <div
+        className="hide-scrollbar"
+        style={{
+          height: 54,
+          flex: '0 0 auto',
+          display: 'flex',
+          alignItems: 'center',
+          gap: theme.spacing(0.5),
+          padding: `0 ${theme.spacing(1)}`,
+          overflow: 'scroll',
+        }}
       >
         <GraphToolbarDisplayTools onUnfixNodes={onUnfixNodes} />
         <Divider sx={{ margin: 1, height: '80%' }} orientation="vertical" />
