9aaeaeb3bf1be5f422595550425c5f87485c3e81
Jeremy Cloarec <jeremy.cloarec@filigran.io>
2025-07-18 19:11:07 +0200

    [frontend] WIP email template view

--- opencti-platform/opencti-front/src/private/components/settings/email_template/EmailTemplateCreation.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/email_template/EmailTemplateCreation.tsx
@@ -1,30 +1,8 @@
-import React, { FunctionComponent } from 'react';
-import { FormikConfig } from 'formik';
-import { RecordSourceSelectorProxy } from 'relay-runtime';
-import Drawer, { DrawerControlledDialProps } from '@components/common/drawer/Drawer';
-import { formatEmailsForApi } from '@components/settings/dissemination_lists/DisseminationListUtils';
-import { graphql } from 'react-relay';
-import DisseminationListForm from '@components/settings/dissemination_lists/DisseminationListForm';
+import React, { FunctionComponent, useState } from 'react';
+import { DrawerControlledDialProps } from '@components/common/drawer/Drawer';
 import { EmailTemplatesLinesPaginationQuery$variables } from '@components/settings/email_template/__generated__/EmailTemplatesLinesPaginationQuery.graphql';
-import { useFormatter } from '../../../../components/i18n';
-import { insertNode } from '../../../../utils/store';
-import { handleErrorInForm } from '../../../../relay/environment';
-import useApiMutation from '../../../../utils/hooks/useApiMutation';
+import EmailTemplateFormDrawer from '@components/settings/email_template/EmailTemplateFormDrawer';
 import CreateEntityControlledDial from '../../../../components/CreateEntityControlledDial';
-
-export interface DisseminationListCreationFormData {
-  name: string;
-  emails: string;
-  description: string;
-}
-
-const disseminationListCreationMutation = graphql`
-    mutation EmailTemplateCreationAddMutation($input: DisseminationListAddInput!) {
-        disseminationListAdd(input: $input) {
-            ...EmailTemplatesLine_node
-        }
-    }
-`;
 
 const CreateEmailTemplateControlledDial = (
   props: DrawerControlledDialProps,
@@ -42,57 +20,18 @@ interface EmailTemplateCreationProps {
 const EmailTemplateCreation: FunctionComponent<EmailTemplateCreationProps> = ({
   paginationOptions,
 }) => {
-  const { t_i18n } = useFormatter();
-  const updater = (store: RecordSourceSelectorProxy, rootField: string) => {
-    insertNode(
-      store,
-      'Pagination_disseminationLists',
-      paginationOptions,
-      rootField,
-    );
-  };
-
-  const [commit] = useApiMutation(disseminationListCreationMutation);
-
-  const onSubmit: FormikConfig<DisseminationListCreationFormData>['onSubmit'] = (
-    values,
-    { setSubmitting, resetForm, setErrors },
-  ) => {
-    const input = {
-      name: values.name,
-      emails: formatEmailsForApi(values.emails),
-      description: values.description,
-    };
-    commit({
-      variables: {
-        input,
-      },
-      updater: (store) => {
-        updater(store, 'disseminationListAdd');
-      },
-      onCompleted: () => {
-        setSubmitting(false);
-        resetForm();
-      },
-      onError: (error: Error) => {
-        handleErrorInForm(error, setErrors);
-        setSubmitting(false);
-      },
-    });
-  };
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
-    <Drawer
-      title={t_i18n('Create an email template')}
-      controlledDial={CreateEmailTemplateControlledDial}
-    >
-      {({ onClose }) => (
-        <DisseminationListForm
-          onSubmit={onSubmit}
-          onReset={onClose}
-        />
-      )}
-    </Drawer>
+    <>
+      <CreateEmailTemplateControlledDial
+        onOpen={() => setDrawerOpen(true)}
+      />
+      <EmailTemplateFormDrawer
+        isOpen={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      />
+    </>
   );
 };
 
--- opencti-platform/opencti-front/src/private/components/settings/email_template/useEmailTemplateEdit.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/email_template/useEmailTemplateEdit.tsx
@@ -1,18 +1,15 @@
 import { graphql } from 'react-relay';
 import { useState } from 'react';
+import { useEmailTemplateEditMutation } from '@components/settings/email_template/__generated__/useEmailTemplateEditMutation.graphql';
 import useApiMutation from '../../../../utils/hooks/useApiMutation';
 
 const emailTemplateEditMutation = graphql`
     mutation useEmailTemplateEditMutation($id: ID!, $input: [EditInput!]!) {
-        fintelTemplateFieldPatch(id: $id, input: $input) {
+        emailTemplateFieldPatch(id: $id, input: $input) {
             id
             name
             description
-            instance_filters
-            settings_types
-            start_date
-            ...FintelTemplateTabs_template
-            ...FintelTemplateWidgetsSidebar_template
+            ...EmailTemplateTabs_template
         }
     }
 `;
