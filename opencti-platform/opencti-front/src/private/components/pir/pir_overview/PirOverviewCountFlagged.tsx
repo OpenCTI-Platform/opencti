0aab2c41b7ec04cf1191abcd8a56ed5d9c9650d1
Landry Trebon <landry.trebon@filigran.io>
2025-07-21 14:19:07 +0200

    [frontend] fix overview details layout issue when lot of criterias

--- opencti-platform/opencti-front/src/private/components/pir/pir_overview/PirOverviewCountFlagged.tsx
+++ opencti-platform/opencti-front/src/private/components/pir/pir_overview/PirOverviewCountFlagged.tsx
@@ -1,5 +1,6 @@
 import { graphql, useFragment } from 'react-relay';
 import React from 'react';
+import Grid from '@mui/material/Grid2';
 import StixRelationshipsMultiAreaChart from '@components/common/stix_relationships/StixRelationshipsMultiAreaChart';
 import { PirOverviewCountFlaggedFragment$key } from './__generated__/PirOverviewCountFlaggedFragment.graphql';
 import { useFormatter } from '../../../../components/i18n';
@@ -41,19 +42,21 @@ const PirOverviewCountFlagged = ({ data }: PirOverviewCountFlaggedProps) => {
   ];
 
   return (
-    <Paper title={t_i18n('Number of flagged entities')}>
-      <StixRelationshipsMultiAreaChart
-        relationshipTypes={['in-pir']}
-        dataSelection={dataSelection}
-        parameters={{ interval: 'month' }}
-        variant="inLine"
-        height={250}
-        startDate={monthsAgo(6)}
-        endDate={null}
-        withoutTitle
-        isReadOnly
-      />
-    </Paper>
+    <Grid size={{ xs: 12 }}>
+      <Paper title={t_i18n('Number of flagged entities')}>
+        <StixRelationshipsMultiAreaChart
+          relationshipTypes={['in-pir']}
+          dataSelection={dataSelection}
+          parameters={{ interval: 'month' }}
+          variant="inLine"
+          height={250}
+          startDate={monthsAgo(6)}
+          endDate={null}
+          withoutTitle
+          isReadOnly
+        />
+      </Paper>
+    </Grid>
   );
 };
 
