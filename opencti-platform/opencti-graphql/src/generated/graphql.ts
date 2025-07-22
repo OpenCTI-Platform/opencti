b8a5b0f0be006d17c25f930e91d38444990f67bc
Gwendoline Favre-Felix <gwendoline.favre-felix@filigran.io>
2025-07-22 15:23:52 +0200

    [backend] Fix object in sendEmailToUser (#11487)

--- opencti-platform/opencti-graphql/src/generated/graphql.ts
+++ opencti-platform/opencti-graphql/src/generated/graphql.ts
@@ -25311,7 +25311,6 @@ export enum Selection {
 }
 
 export type SendUserMailInput = {
-  email_object: Scalars['String']['input'];
   email_template_id: Scalars['ID']['input'];
   target_user_id: Scalars['ID']['input'];
 };
