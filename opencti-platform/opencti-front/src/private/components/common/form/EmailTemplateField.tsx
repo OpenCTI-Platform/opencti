bfa740f1518b2da3a57b6b2452e361192be378a4
Jeremy Cloarec <jeremy.cloarec@filigran.io>
2025-07-22 14:19:45 +0200

    [frontend] add email template selection

---
+++ opencti-platform/opencti-front/src/private/components/common/form/EmailTemplateField.tsx
@@ -0,0 +1,126 @@
+import { graphql, PreloadedQuery, usePreloadedQuery } from 'react-relay';
+import React, { FunctionComponent } from 'react';
+import { Field } from 'formik';
+import { EmailTemplateFieldQuery } from '@components/common/form/__generated__/EmailTemplateFieldQuery.graphql';
+import useQueryLoading from '../../../../utils/hooks/useQueryLoading';
+import { FieldOption, fieldSpacingContainerStyle } from '../../../../utils/field';
+import { useFormatter } from '../../../../components/i18n';
+import AutocompleteField from '../../../../components/AutocompleteField';
+import ItemIcon from '../../../../components/ItemIcon';
+import Loader, { LoaderVariant } from '../../../../components/Loader';
+
+const emailTemplateFieldQuery = graphql`
+    query EmailTemplateFieldQuery(
+        $orderMode: OrderingMode,
+        $orderBy: EmailTemplateOrdering
+        $filters: FilterGroup
+    ) {
+        emailTemplates(
+            orderMode: $orderMode
+            orderBy: $orderBy
+            filters: $filters
+        ) {
+            edges {
+                node {
+                    id
+                    name
+                }
+            }
+        }
+    }
+`;
+
+export type EmailTemplate = {
+  id: string;
+  name: string
+};
+
+export type EmailTemplateFieldOption = {
+  label: string;
+  value: EmailTemplate
+};
+
+interface EmailTemplateFieldComponentProps {
+  label?: string
+  name: string;
+  style?: React.CSSProperties,
+  helperText?: string;
+  onChange?: (name: string, value: FieldOption[]) => void;
+  required?: boolean
+  queryRef: PreloadedQuery<EmailTemplateFieldQuery>
+}
+
+const EmailTemplateFieldComponent: FunctionComponent<EmailTemplateFieldComponentProps> = ({
+  label,
+  name,
+  style,
+  helperText,
+  onChange,
+  required = false,
+  queryRef,
+}) => {
+  const { t_i18n } = useFormatter();
+
+  const data = usePreloadedQuery(emailTemplateFieldQuery, queryRef);
+  const emailTemplates = data.emailTemplates?.edges?.map(({ node }) => ({ value: node, label: node?.name }));
+
+  return (
+    <div style={{ width: '100%' }}>
+      <Field
+        component={AutocompleteField}
+        name={name}
+        multiple={false}
+        disabled={false}
+        textfieldprops={{
+          variant: 'standard',
+          label: label ?? t_i18n('Email templates'),
+          helperText,
+        }}
+        required={required}
+        onChange={onChange}
+        style={fieldSpacingContainerStyle ?? style}
+        noOptionsText={t_i18n('No available options')}
+        options={emailTemplates}
+        renderOption={(
+          props: React.HTMLAttributes<HTMLLIElement>,
+          option: { label: string },
+        ) => (
+          <li {...props} style={{ display: 'flex', alignItems: 'center' }}>
+            <ItemIcon type='email-templates' />
+            <div style={{ flexGrow: 1, marginLeft: 10 }}>{option.label}</div>
+          </li>
+        )}
+        classes={{ clearIndicator: { display: 'none' } }}
+      />
+    </div>
+  );
+};
+
+type EmailTemplateFieldProps = Omit<EmailTemplateFieldComponentProps, 'queryRef'>;
+
+const EmailTemplateField = ({ ...props }: EmailTemplateFieldProps) => {
+  const queryRef = useQueryLoading<EmailTemplateFieldQuery>(emailTemplateFieldQuery);
+  const { name, label } = props;
+  return queryRef ? (
+    <React.Suspense fallback={
+      <Field
+        component={AutocompleteField}
+        name={name}
+        disabled={true}
+        fullWidth={true}
+        options={[]}
+        renderOption={() => null}
+        textfieldprops={{
+          label,
+        }}
+      />
+        }
+    >
+      <EmailTemplateFieldComponent {...props} queryRef={queryRef} />
+    </React.Suspense>
+  ) : (
+    <Loader variant={LoaderVariant.inElement} />
+  );
+};
+
+export default EmailTemplateField;
