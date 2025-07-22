002fde4aa889c80b321aee58f9257df471698177
Celine Sebe <celine.sebe@filigran.io>
2025-07-22 15:40:04 +0200

    [frontend] FF

--- opencti-platform/opencti-front/src/private/components/data/ingestionCsv/IngestionCsvEditionUserHandling.tsx
+++ opencti-platform/opencti-front/src/private/components/data/ingestionCsv/IngestionCsvEditionUserHandling.tsx
@@ -18,34 +18,35 @@ import { useFormatter } from '../../../../components/i18n';
 import { fieldSpacingContainerStyle } from '../../../../utils/field';
 import Transition from '../../../../components/Transition';
 import useApiMutation from '../../../../utils/hooks/useApiMutation';
+import useHelper from '../../../../utils/hooks/useHelper';
 
 export const ingestionCsvEditionUserHandlingQuery = graphql`
-    query IngestionCsvEditionUserHandlingQuery(
-        $name: String!
-    ) {
-        userAlreadyExists(
-            name: $name
-        )
-    }
+  query IngestionCsvEditionUserHandlingQuery(
+    $name: String!
+  ) {
+    userAlreadyExists(
+      name: $name
+    )
+  }
 `;
 
 export const ingestionCsvEditionUserHandlingFragment = graphql`
-    fragment IngestionCsvEditionUserHandlingFragment_ingestionCsv on IngestionCsv {
-        id
-        name
-        user {
-            id
-            entity_type
-            name
-        }
+  fragment IngestionCsvEditionUserHandlingFragment_ingestionCsv on IngestionCsv {
+    id
+    name
+    user {
+      id
+      entity_type
+      name
     }
+  }
 `;
 export const ingestionCsvEditionUserHandlingPatch = graphql`
-    mutation IngestionCsvEditionUserHandlingMutation($id: ID!, $input: IngestionCsvAddAutoUserInput!) {
-        ingestionCsvAddAutoUser(id: $id, input: $input) {
-            ...IngestionCsvEditionUserHandlingFragment_ingestionCsv
-        }
+  mutation IngestionCsvEditionUserHandlingMutation($id: ID!, $input: IngestionCsvAddAutoUserInput!) {
+    ingestionCsvAddAutoUser(id: $id, input: $input) {
+      ...IngestionCsvEditionUserHandlingFragment_ingestionCsv
     }
+  }
 `;
 
 export interface EditionCsvAddAutoUserInput {
@@ -63,7 +64,8 @@ const IngestionCsvEditionUserHandling: FunctionComponent<IngestionCsvEditionUser
 
   const [openDialog, setOpenDialog] = useState(false);
   const [commitUpdate] = useApiMutation(ingestionCsvEditionUserHandlingPatch);
-
+  const { isFeatureEnable } = useHelper();
+  const serviceAccountFeatureFlag = isFeatureEnable('SERVICE_ACCOUNT');
   const ingestionCsvCreationValidation = () => Yup.object().shape({
     user_name: Yup.string(),
     confidence_level: Yup.string(),
@@ -115,9 +117,12 @@ const IngestionCsvEditionUserHandling: FunctionComponent<IngestionCsvEditionUser
         sx={{ padding: '0px 10px 0px 10px', marginTop: '20px' }}
       >
         <Box>
-          {t_i18n('You have set System as a creator. Create a service account for this feed to ensure traceability of your data')}
+          {!serviceAccountFeatureFlag
+            ? t_i18n('You have set System as a creator. Create a specific user for this feed to ensure traceabilitiy of your data')
+            : t_i18n('You have set System as a creator. Create a service account for this feed to ensure traceability of your data')}
         </Box>
-        <Button onClick={() => setOpenDialog(true)}>{t_i18n('Create a service account for this feed')}</Button>
+        <Button onClick={() => setOpenDialog(true)}>{!serviceAccountFeatureFlag
+          ? t_i18n('Create a user for this feed') : t_i18n('Create a service account for this feed')}</Button>
 
       </Alert>
 
