4c7d2b8782ad3049c76ba748cec526e368b75eec
Jeremy Cloarec <jeremy.cloarec@filigran.io>
2025-07-24 18:16:51 +0200

    [frontend] improve test send message

--- opencti-platform/opencti-front/src/private/components/settings/email_template/EmailTemplateTestSend.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/email_template/EmailTemplateTestSend.tsx
@@ -6,6 +6,7 @@ import Tooltip from '@mui/material/Tooltip';
 import useApiMutation from '../../../../utils/hooks/useApiMutation';
 import { handleError, MESSAGING$ } from '../../../../relay/environment';
 import { useFormatter } from '../../../../components/i18n';
+import useAuth from '../../../../utils/hooks/useAuth';
 
 const emailTemplateTestSendMutation = graphql`
     mutation EmailTemplateTestSendMutation($id: ID!) {
@@ -19,6 +20,7 @@ interface EmailTemplateTestSendProps {
 
 const EmailTemplateTestSend = ({ templateId }: EmailTemplateTestSendProps) => {
   const { t_i18n } = useFormatter();
+  const { me } = useAuth();
   const [commitSendTestEmailMutation] = useApiMutation(emailTemplateTestSendMutation);
   const [sending, setSending] = useState(false);
 
@@ -29,7 +31,7 @@ const EmailTemplateTestSend = ({ templateId }: EmailTemplateTestSendProps) => {
         id: templateId,
       },
       onCompleted: () => {
-        MESSAGING$.notifySuccess(t_i18n('Test email sent to you'));
+        MESSAGING$.notifySuccess(`${t_i18n('Test email successfully sent to you.')} (${me.user_email})`);
         setSending(false);
       },
       onError: (error: Error) => {
