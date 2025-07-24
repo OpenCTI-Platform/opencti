58562ee117b88e444236e3e36067e16c4e30a7e7
FrancoisGrunert <francois.grunert@filigran.io>
2025-07-24 15:04:29 +0200

    [frontend] label style + use itemBoolean for verified chip (#7328)

--- opencti-platform/opencti-front/src/private/components/data/IngestionCatalog/IngestionCatalogConnectorHeader.tsx
+++ opencti-platform/opencti-front/src/private/components/data/IngestionCatalog/IngestionCatalogConnectorHeader.tsx
@@ -1,5 +1,4 @@
 import Typography from '@mui/material/Typography';
-import Chip from '@mui/material/Chip';
 import { VerifiedOutlined } from '@mui/icons-material';
 import Button from '@mui/material/Button';
 import { Launch } from 'mdi-material-ui';
@@ -11,6 +10,7 @@ import { useFormatter } from '../../../../components/i18n';
 import type { Theme } from '../../../../components/Theme';
 import { INGESTION_SETINGESTIONS } from '../../../../utils/hooks/useGranted';
 import Security from '../../../../utils/Security';
+import ItemBoolean from '../../../../components/ItemBoolean';
 
 const IngestionCatalogConnectorHeader = ({ connector }: { connector: IngestionConnector }) => {
   const { t_i18n } = useFormatter();
@@ -22,24 +22,27 @@ const IngestionCatalogConnectorHeader = ({ connector }: { connector: IngestionCo
       <div style={{ display: 'flex', gap: 20 }}>
         <img style={{ height: 70, width: 70, objectFit: 'cover', borderRadius: 4 }} src={connector.logo} alt={connector.title} />
         <div>
-          <div style={{ display: 'flex', alignItems: 'center', gap: 20 }}>
-            <Typography variant="h1" style={{ fontSize: 30, textTransform: 'uppercase', marginBottom: 0 }}>{connector.title}</Typography>
+          <div style={{ display: 'flex', gap: 20 }}>
+            <Typography variant="h1" style={{ fontSize: 30, textTransform: 'uppercase' }}>{connector.title}</Typography>
             {connector.verified && (
-              <Chip
-                variant="outlined"
-                color="success"
-                style={{ textTransform: 'uppercase', borderRadius: 4 }}
-                icon={<VerifiedOutlined color="success" />}
-                label={<Typography color="success">{t_i18n('Verified')}</Typography>}
+              <ItemBoolean
+                status={true}
+                label={
+                  <div style={{ display: 'flex', alignItems: 'center', gap: theme.spacing(1) }}>
+                    <VerifiedOutlined color="success" fontSize="small" />
+                    {t_i18n('Verified')}
+                  </div>
+                }
               />
             )}
           </div>
           <div style={{ display: 'flex' }}>
             <IngestionCatalogChip
+              isInlist
               label={t_i18n(ingestionConnectorTypeMetadata[connector.container_type].label)}
               color={ingestionConnectorTypeMetadata[connector.container_type].color}
             />
-            {connector.use_cases.map((useCase: string) => <IngestionCatalogChip key={useCase} label={useCase} />)}
+            {connector.use_cases.map((useCase: string) => <IngestionCatalogChip key={useCase} label={useCase} isInlist />)}
           </div>
         </div>
       </div>
