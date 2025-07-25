60226aeb9bb1c73dac25eb74605ed24dddc621b0
Landry Trebon <landry.trebon@filigran.io>
2025-07-25 11:16:34 +0200

    [frontend] improve overview layout

--- opencti-platform/opencti-front/src/private/components/pir/pir_overview/PirOverviewHistory.tsx
+++ opencti-platform/opencti-front/src/private/components/pir/pir_overview/PirOverviewHistory.tsx
@@ -1,12 +1,8 @@
 import Typography from '@mui/material/Typography';
-import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Tooltip from '@mui/material/Tooltip';
 import { Link } from 'react-router-dom';
 import React from 'react';
-import { deepOrange, green, indigo, pink, red, teal, yellow } from '@mui/material/colors';
-import { AddOutlined, DeleteOutlined, EditOutlined, HelpOutlined } from '@mui/icons-material';
-import { LinkVariantPlus, LinkVariantRemove, Merge } from 'mdi-material-ui';
 import { graphql, useFragment } from 'react-relay';
 import { useTheme } from '@mui/material/styles';
 import MarkdownDisplay from '../../../../components/MarkdownDisplay';
@@ -65,41 +61,6 @@ const pirHistoryFragment = graphql`
   }
 `;
 
-const HISTORY_ICON_CONFIG = {
-  create: {
-    color: pink[500],
-    icon: <AddOutlined sx={{ fontSize: 14 }} />,
-  },
-  delete: {
-    color: red[500],
-    icon: <DeleteOutlined sx={{ fontSize: 14 }} />,
-  },
-  merge: {
-    color: teal[500],
-    icon: <Merge sx={{ fontSize: 14 }} />,
-  },
-  updateReplaces: {
-    color: green[500],
-    icon: <EditOutlined sx={{ fontSize: 14 }} />,
-  },
-  updateChanges: {
-    color: green[500],
-    icon: <EditOutlined sx={{ fontSize: 14 }} />,
-  },
-  updateAdds: {
-    color: indigo[500],
-    icon: <LinkVariantPlus sx={{ fontSize: 14 }} />,
-  },
-  updateRemoves: {
-    color: deepOrange[500],
-    icon: <LinkVariantRemove sx={{ fontSize: 14 }} />,
-  },
-  default: {
-    color: yellow[500],
-    icon: <HelpOutlined sx={{ fontSize: 14 }} />,
-  },
-};
-
 interface PirOverviewHistoryProps {
   dataHistory: PirOverviewHistoryFragment$key
   dataPir: PirOverviewHistoryPirFragment$key
@@ -112,20 +73,6 @@ const PirOverviewHistory = ({ dataHistory, dataPir }: PirOverviewHistoryProps) =
   const pir = useFragment(pirFragment, dataPir);
   const { logs } = useFragment(pirHistoryFragment, dataHistory);
   const history = (logs?.edges ?? []).flatMap((e) => e?.node ?? []);
-
-  const getIconConfig = ({ event_scope, context_data }: typeof history[0]) => {
-    if (event_scope === 'create') return HISTORY_ICON_CONFIG.create;
-    if (event_scope === 'merge') return HISTORY_ICON_CONFIG.merge;
-    if (event_scope === 'delete') return HISTORY_ICON_CONFIG.delete;
-    if (event_scope === 'update') {
-      const { message } = context_data ?? {};
-      if (message?.includes('replaces')) return HISTORY_ICON_CONFIG.updateReplaces;
-      if (message?.includes('changes')) return HISTORY_ICON_CONFIG.updateChanges;
-      if (message?.includes('adds')) return HISTORY_ICON_CONFIG.updateAdds;
-      if (message?.includes('removes')) return HISTORY_ICON_CONFIG.updateRemoves;
-    }
-    return HISTORY_ICON_CONFIG.default;
-  };
 
   const getHistoryMessage = ({ context_data, entity_type, event_scope, user }: typeof history[0]) => {
     const message = context_data?.message ?? '';
@@ -171,7 +118,6 @@ const PirOverviewHistory = ({ dataHistory, dataPir }: PirOverviewHistoryProps) =
 
         {history.map((historyItem) => {
           const { id, context_data, timestamp } = historyItem;
-          const { color, icon } = getIconConfig(historyItem);
           const historyMessage = getHistoryMessage(historyItem);
 
           const isAddInPir = /adds .+ in `In PIR`/.test(context_data?.message ?? '');
@@ -213,7 +159,7 @@ const PirOverviewHistory = ({ dataHistory, dataPir }: PirOverviewHistoryProps) =
                   color: 'inherit',
                   display: 'flex',
                   gap: theme.spacing(2),
-                  alignItems: 'flex-start',
+                  alignItems: 'center',
                 }}
               >
                 <Tooltip title={t_i18n(displayEntityTypeForTranslation(context_data?.entity_type ?? ''))}>
@@ -222,33 +168,12 @@ const PirOverviewHistory = ({ dataHistory, dataPir }: PirOverviewHistoryProps) =
                   </div>
                 </Tooltip>
                 <div>
-                  <Typography
-                    sx={{ marginTop: 0.5, marginBottom: 0 }}
-                    variant="h3"
-                  >
-                    {context_data?.entity_name}
-                  </Typography>
-                  <Typography
-                    color={theme.palette.text?.secondary}
-                    sx={{ marginBottom: 1 }}
-                    variant="body2"
-                  >
+                  <Typography variant="body2" color={theme.palette.text?.secondary}>
                     {nsdt(timestamp)}
                   </Typography>
-                  <div style={{ display: 'flex', gap: theme.spacing(2) }}>
-                    <Avatar
-                      sx={{
-                        width: 24,
-                        height: 24,
-                        backgroundColor: 'transparent',
-                        border: `1px solid ${color}`,
-                        color: theme.palette.text?.primary,
-                      }}
-                    >
-                      <div>{icon}</div>
-                    </Avatar>
-                    <Tooltip title={content}>{content}</Tooltip>
-                  </div>
+                  <Typography variant="body2">
+                    {content}
+                  </Typography>
                 </div>
               </Link>
             </Box>
