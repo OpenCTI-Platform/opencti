c54d6b61444be3765e2693345b2139f64abd1f1c
marie flores <marie.flores@filigran.io>
2025-07-28 18:21:20 +0200

    [backend] STIX 2.0 converter: Containers (#8832)

--- opencti-platform/opencti-graphql/src/modules/playbook/playbook-components.ts
+++ opencti-platform/opencti-graphql/src/modules/playbook/playbook-components.ts
@@ -99,6 +99,13 @@ import { convertMembersToUsers, extractBundleBaseElement } from './playbook-util
 import { convertStoreToStix_2_1 } from '../../database/stix-2-1-converter';
 import { ENTITY_TYPE_SECURITY_COVERAGE, INPUT_COVERED, type StixSecurityCoverage, type StoreEntitySecurityCoverage } from '../securityCoverage/securityCoverage-types';
 
+const extractBundleBaseElement = (instanceId: string, bundle: StixBundle): StixObject => {
+  const baseData = bundle.objects.find((o) => o.id === instanceId);
+  if (!baseData) throw FunctionalError('Playbook base element no longer accessible');
+  return baseData;
+};
+
+
 // region built in playbook components
 interface LoggerConfiguration {
   level: string
