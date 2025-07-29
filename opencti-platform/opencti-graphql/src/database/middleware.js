2d5a4b0091a1153e75ff82057753f381aba59493
marie flores <marie.flores@filigran.io>
2025-07-29 19:36:37 +0200

    [backend] adapt toStix api to convert in 2 stix version

--- opencti-platform/opencti-graphql/src/database/middleware.js
+++ opencti-platform/opencti-graphql/src/database/middleware.js
@@ -213,7 +213,7 @@ import {
   xOpenctiStixIds
 } from '../schema/attribute-definition';
 import { ENTITY_TYPE_INDICATOR } from '../modules/indicator/indicator-types';
-import { FilterMode, FilterOperator } from '../generated/graphql';
+import { FilterMode, FilterOperator, StixVersion } from '../generated/graphql';
 import { getMandatoryAttributesForSetting } from '../modules/entitySetting/entitySetting-attributeUtils';
 import { ENTITY_TYPE_IDENTITY_ORGANIZATION } from '../modules/organization/organization-types';
 import {
@@ -237,6 +237,7 @@ import { ENTITY_TYPE_ENTITY_SETTING } from '../modules/entitySetting/entitySetti
 import { RELATION_ACCESSES_TO } from '../schema/internalRelationship';
 import { generateVulnerabilitiesUpdates } from '../utils/vulnerabilities';
 import { idLabel } from '../schema/schema-labels';
+import { convertStoreToStix2, convertStoreToStix_2_0 } from './stix-2-0-converter';
 
 // region global variables
 const MAX_BATCH_SIZE = nconf.get('elasticsearch:batch_loader_max_size') ?? 300;
@@ -515,9 +516,13 @@ export const storeLoadByIdWithRefs = async (context, user, id, opts = {}) => {
   const elements = await storeLoadByIdsWithRefs(context, user, [id], opts);
   return elements.length > 0 ? R.head(elements) : null;
 };
-export const stixLoadById = async (context, user, id, opts = {}) => {
+export const stixLoadById = async (context, user, id, opts = {}, stixVersion = StixVersion.Stix_2_1) => {
   const instance = await storeLoadByIdWithRefs(context, user, id, opts);
-  return instance ? convertStoreToStix(instance) : undefined;
+  if (stixVersion === StixVersion.Stix_2_1) {
+    return instance ? convertStoreToStix(instance) : undefined;
+  } if (stixVersion === StixVersion.Stix_2_0) {
+    return instance ? convertStoreToStix_2_0(instance) : undefined;
+  }
 };
 const convertStoreToStixWithResolvedFiles = async (instance) => {
   const instanceInStix = convertStoreToStix(instance);
@@ -549,8 +554,8 @@ export const stixLoadByIds = async (context, user, ids, opts = {}) => {
     .filter((i) => isNotEmptyField(i))
     .map((e) => (convertStoreToStix(e)));
 };
-export const stixLoadByIdStringify = async (context, user, id) => {
-  const data = await stixLoadById(context, user, id);
+export const stixLoadByIdStringify = async (context, user, id, stixVersion) => {
+  const data = await stixLoadById(context, user, id, {}, stixVersion);
   return data ? JSON.stringify(data) : '';
 };
 export const stixLoadByFilters = async (context, user, types, args) => {
