2311c9436e42c9b609ac6d6495c570cfb0f52b82
Landry Trebon <landry.trebon@filigran.io>
2025-07-30 17:21:57 +0200

    [frontend] fix useless import

--- opencti-platform/opencti-front/src/private/components/pir/pir-history-utils.ts
+++ opencti-platform/opencti-front/src/private/components/pir/pir-history-utils.ts
@@ -13,7 +13,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 */
 
-import { PirHistoryLogFragment$data } from '@components/pir/pir_history/__generated__/PirHistoryLogFragment.graphql';
 import { GqlFilterGroup, sanitizeFilterGroupKeysForFrontend } from '../../../utils/filters/filtersUtils';
 
 export const pirHistoryFilterGroup = (pirId: string): GqlFilterGroup => {
