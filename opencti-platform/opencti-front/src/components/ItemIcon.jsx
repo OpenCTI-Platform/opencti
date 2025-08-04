0890746e3c24e00867d7b6cb1934dc0ceed25448
Landry Trebon <landry.trebon@filigran.io>
2025-08-04 17:32:03 +0200

    [frontend] missing translations

--- opencti-platform/opencti-front/src/components/ItemIcon.jsx
+++ opencti-platform/opencti-front/src/components/ItemIcon.jsx
@@ -69,6 +69,7 @@ import {
   ArchitectureOutlined,
   PlaylistRemoveOutlined,
   DrawOutlined,
+  TrackChanges,
   AlternateEmailOutlined,
   EmailOutlined,
 } from '@mui/icons-material';
@@ -263,6 +264,8 @@ const iconSelector = (type, variant, fontSize, color, isReversed) => {
       return <PublicOutlined style={style} fontSize={fontSize} role="img" />;
     case 'malware':
       return <Biohazard style={style} fontSize={fontSize} role="img" />;
+    case 'in-pir':
+      return <TrackChanges style={style} fontSize={fontSize} role="img" />;
     case 'malware-analysis':
       return <BiotechOutlined style={style} fontSize={fontSize} role="img" />;
     case 'threat-actor':
