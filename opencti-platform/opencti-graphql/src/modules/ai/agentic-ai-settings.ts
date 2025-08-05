c66c25ca1b03264dd527de207e0554e89c59ac4f
Angelique <angelique.jard@filigran.io>
2025-08-05 15:17:42 +0200

    git ssh not working

--- opencti-platform/opencti-graphql/src/modules/ai/agentic-ai-settings.ts
+++ opencti-platform/opencti-graphql/src/modules/ai/agentic-ai-settings.ts
@@ -1,5 +1,5 @@
 import nconf from 'nconf';
-import { booleanConf, isFeatureEnabled, logApp } from '../../config/conf';
+import { booleanConf, isFeatureEnabled } from '../../config/conf';
 
 export const getAgenticAiEndpoint = () => {
   return nconf.get('ai:agentic:url');
@@ -8,11 +8,9 @@ export const getAgenticAiEndpoint = () => {
 export const isAgenticAiActivated = async () => {
   const isAgenticAiEnabled: boolean = booleanConf('ai:agentic:enabled', true);
   if (!isAgenticAiEnabled) {
-    logApp.info('[AI] isAgenticAiEnabled false');
     return false;
   }
   const isAgenticFeatureEnabled: boolean = isFeatureEnabled('AGENTIC_AI') || false;
   const isAgenticAiConfigured: boolean = getAgenticAiEndpoint() !== undefined;
-  logApp.info(`[AI] isAgenticFeatureEnabled:${isAgenticFeatureEnabled}, isAgenticAiConfigured:${isAgenticAiConfigured}`);
   return isAgenticFeatureEnabled && isAgenticAiConfigured;
 };
