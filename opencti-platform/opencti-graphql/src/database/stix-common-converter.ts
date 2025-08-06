3acfd449ecc863b52c0fb1f85c042d87feb903b7
marie flores <marie.flores@filigran.io>
2025-08-06 16:45:02 +0200

    [backend] rename convertToStix for 2.1

--- opencti-platform/opencti-graphql/src/database/stix-common-converter.ts
+++ opencti-platform/opencti-graphql/src/database/stix-common-converter.ts
@@ -5,14 +5,14 @@ import type * as S2 from '../types/stix-2-0-common';
 import { isEmptyField } from './utils';
 import { FunctionalError, UnsupportedError } from '../config/errors';
 import { convertToStix_2_0 } from './stix-2-0-converter';
-import { convertToStix } from './stix-2-1-converter';
+import { convertToStix_2_1 } from './stix-2-1-converter';
 import { cleanObject, isValidStix } from './stix-converter-utils';
 
 export const convertStoreToStix = (instance: StoreCommon, version = Version.Stix_2_1): S.StixObject | S2.StixObject => {
   if (isEmptyField(instance.standard_id) || isEmptyField(instance.entity_type)) {
     throw UnsupportedError('convertInstanceToStix must be used with opencti fully loaded instance');
   }
-  const converted = version === Version.Stix_2_0 ? convertToStix_2_0(instance) : convertToStix(instance);
+  const converted = version === Version.Stix_2_0 ? convertToStix_2_0(instance) : convertToStix_2_1(instance);
   const stix = cleanObject(converted);
   if (!isValidStix(stix)) {
     throw FunctionalError('Invalid stix data conversion', { id: instance.standard_id, type: instance.entity_type });
