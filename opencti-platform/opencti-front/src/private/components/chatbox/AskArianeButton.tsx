ef5dc4fb77c5ec15cb3b92ed5909b0daaac0ada5
Celine Sebe <celine.sebe@filigran.io>
2025-08-07 17:42:07 +0200

    [frontend] fix ask AI

--- opencti-platform/opencti-front/src/private/components/chatbox/AskArianeButton.tsx
+++ opencti-platform/opencti-front/src/private/components/chatbox/AskArianeButton.tsx
@@ -131,7 +131,7 @@ const AskArianeButton = () => {
         />
       ) : <></>}
 
-      {!isEnterpriseEdition ? (
+      {navOpen ? (
         <>
           Ask Ariane
           <EEChip ref={EEref} />
