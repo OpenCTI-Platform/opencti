87b858ad7d0b82edb00d59e20f617edbda564621
Celine Sebe <celine.sebe@filigran.io>
2025-08-11 11:53:53 +0200

    [frontend] fix subsectors list

--- opencti-platform/opencti-front/src/private/components/entities/sectors/SectorSubSectors.jsx
+++ opencti-platform/opencti-front/src/private/components/entities/sectors/SectorSubSectors.jsx
@@ -1,34 +1,35 @@
-import React, { Component } from 'react';
-import * as PropTypes from 'prop-types';
-import { compose, filter } from 'ramda';
+import React from 'react';
+import { filter } from 'ramda';
 import Typography from '@mui/material/Typography';
-import List from '@mui/material/List';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import { ListItemButton, ListItem } from '@mui/material';
-import { Link } from 'react-router-dom';
 import IconButton from '@mui/material/IconButton';
 import { Domain, LinkOff } from '@mui/icons-material';
 import { graphql, createFragmentContainer } from 'react-relay';
 import { AutoFix } from 'mdi-material-ui';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import { ListItemButton } from '@mui/material';
+import ListItemText from '@mui/material/ListItemText';
+import { Link } from 'react-router-dom';
 import AddSubSector from './AddSubSector';
 import { addSubSectorsMutationRelationDelete } from './AddSubSectorsLines';
 import { commitMutation } from '../../../../relay/environment';
-import inject18n from '../../../../components/i18n';
+import { useFormatter } from '../../../../components/i18n';
 import Security from '../../../../utils/Security';
 import { KNOWLEDGE_KNUPDATE } from '../../../../utils/hooks/useGranted';
 
-class SectorSubSectorsComponent extends Component {
-  removeSubSector(subSectorEdge) {
+const SectorSubSectorsComponent = ({ sector }) => {
+  const { t_i18n } = useFormatter();
+
+  const removeSubSector = (subSectorEdge) => {
     commitMutation({
       mutation: addSubSectorsMutationRelationDelete,
       variables: {
         fromId: subSectorEdge.node.id,
-        toId: this.props.sector.id,
+        toId: sector.id,
         relationship_type: 'part-of',
       },
       updater: (store) => {
-        const node = store.get(this.props.sector.id);
+        const node = store.get(sector.id);
         const subSectors = node.getLinkedRecord('subSectors');
         const edges = subSectors.getLinkedRecords('edges');
         const newEdges = filter(
@@ -38,63 +39,54 @@ class SectorSubSectorsComponent extends Component {
         subSectors.setLinkedRecords(newEdges, 'edges');
       },
     });
-  }
+  };
 
-  render() {
-    const { t, sector } = this.props;
-    return (
-      <div style={{ height: '100%' }}>
-        <Typography variant="h3" gutterBottom={true} style={{ float: 'left' }}>
-          {t('Subsectors')}
-        </Typography>
-        <Security needs={[KNOWLEDGE_KNUPDATE]}>
-          <AddSubSector
-            sector={sector}
-            sectorSubSectors={sector.subSectors.edges}
-          />
-        </Security>
-        <div className="clearfix" />
-        <List style={{ marginTop: -10 }}>
-          {sector.subSectors.edges.map((subSectorEdge) => {
-            const { types } = subSectorEdge;
-            const subSector = subSectorEdge.node;
-            return (
-              <ListItem
-                key={subSector.id}
-                dense={true}
-                divider={true}
-                secondaryAction={types.includes('manual') ? (
-                  <Security needs={[KNOWLEDGE_KNUPDATE]}>
-                    <IconButton
-                      aria-label="Remove"
-                      onClick={this.removeSubSector.bind(this, subSectorEdge)}
-                      size="large"
-                    >
-                      <LinkOff />
-                    </IconButton>
-                  </Security>
-                ) : <AutoFix fontSize="small" style={{ marginRight: 13 }}/>}
-              >
-                <ListItemButton component={Link} to={`/dashboard/entities/sectors/${subSector.id}`} >
-                  <ListItemIcon>
-                    <Domain color="primary" />
-                  </ListItemIcon>
-                  <ListItemText primary={subSector.name} />
-                </ListItemButton>
-              </ListItem>
-            );
-          })}
-        </List>
-      </div>
-    );
-  }
-}
-
-SectorSubSectorsComponent.propTypes = {
-  classes: PropTypes.object,
-  t: PropTypes.func,
-  fld: PropTypes.func,
-  sector: PropTypes.object,
+  return (
+    <div style={{ height: '100%' }}>
+      <Typography variant="h3" gutterBottom={true} style={{ float: 'left' }}>
+        {t_i18n('Subsectors')}
+      </Typography>
+      <Security needs={[KNOWLEDGE_KNUPDATE]}>
+        <AddSubSector
+          sector={sector}
+          sectorSubSectors={sector.subSectors.edges}
+        />
+      </Security>
+      <div className="clearfix" />
+      {sector.subSectors.edges.map((subSectorEdge) => {
+        const { types } = subSectorEdge;
+        const subSector = subSectorEdge.node;
+        return (
+          <ListItem
+            key={subSector.id}
+            dense={true}
+            divider={true}
+            disablePadding
+            secondaryAction={types.includes('manual') ? (
+              <Security needs={[KNOWLEDGE_KNUPDATE]}>
+                <IconButton
+                  aria-label="Remove"
+                  onClick={() => removeSubSector(subSectorEdge)}
+                  size="large"
+                >
+                  <LinkOff />
+                </IconButton>
+              </Security>
+            ) : <AutoFix fontSize="small" style={{ marginRight: 13 }}/>}
+          >
+            <ListItemButton
+              component= {Link} to={`/dashboard/entities/sectors/${subSector.id}`}
+            >
+              <ListItemIcon>
+                <Domain color="primary" />
+              </ListItemIcon>
+              <ListItemText primary={subSector.name} />
+            </ListItemButton>
+          </ListItem>
+        );
+      })}
+    </div>
+  );
 };
 
 const SectorSubSectors = createFragmentContainer(SectorSubSectorsComponent, {
@@ -119,4 +111,4 @@ const SectorSubSectors = createFragmentContainer(SectorSubSectorsComponent, {
   `,
 });
 
-export default compose(inject18n)(SectorSubSectors);
+export default SectorSubSectors;
