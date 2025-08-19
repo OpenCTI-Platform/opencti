bfd4c01eefcac70b830e3d7e67298513dc32a52f
Landry Trebon <landry.trebon@filigran.io>
2025-08-19 17:40:12 +0200

    [frontend] rescan period explanation

--- opencti-platform/opencti-front/src/private/components/pir/pir_form/PirCreationFormGeneralSettings.tsx
+++ opencti-platform/opencti-front/src/private/components/pir/pir_form/PirCreationFormGeneralSettings.tsx
@@ -50,7 +50,7 @@ const PirCreationFormGeneralSettings = () => {
         label={t_i18n('Rescan period (days)')}
         fullWidth={true}
         containerstyle={{ marginTop: 20, width: '100%' }}
-        helpertext={t_i18n('Period of events on which a rescan is done to flag elements at PIR creation')}
+        helpertext={t_i18n('Period on which a rescan is done to flag elements from the past at PIR creation')}
       >
         <MenuItem value={0}>{t_i18n('No rescan')}</MenuItem>
         <MenuItem value={1}>{t_i18n('1 day')}</MenuItem>
