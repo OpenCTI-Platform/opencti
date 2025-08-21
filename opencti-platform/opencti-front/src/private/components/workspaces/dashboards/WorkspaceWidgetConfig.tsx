1b99377f2922361ea1901f8e9e6892ebc6bd8f6f
Landry Trebon <landry.trebon@filigran.io>
2025-08-21 15:16:45 +0200

    [frontend/backend] Change dashboard widget import logic to not break existing layout

--- opencti-platform/opencti-front/src/private/components/workspaces/dashboards/WorkspaceWidgetConfig.tsx
+++ opencti-platform/opencti-front/src/private/components/workspaces/dashboards/WorkspaceWidgetConfig.tsx
@@ -1,74 +1,29 @@
 import React, { useRef, useState } from 'react';
-import { graphql, useFragment } from 'react-relay';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 import { Widget } from 'src/utils/widget/widget';
 import VisuallyHiddenInput from '../../common/VisuallyHiddenInput';
 import WidgetConfig from '../../widgets/WidgetConfig';
-import { toB64 } from '../../../../utils/String';
-import { handleError } from '../../../../relay/environment';
-import useApiMutation from '../../../../utils/hooks/useApiMutation';
 import Security from '../../../../utils/Security';
 import { EXPLORE_EXUPDATE } from '../../../../utils/hooks/useGranted';
 import { useFormatter } from '../../../../components/i18n';
-import { WorkspaceWidgetConfigFragment$key } from './__generated__/WorkspaceWidgetConfigFragment.graphql';
-
-const workspaceWidgetConfigFragment = graphql`
-  fragment WorkspaceWidgetConfigFragment on Workspace {
-    id
-    manifest
-  }
-`;
-
-const workspaceImportWidgetMutation = graphql`
-  mutation WorkspaceWidgetConfigImportMutation(
-    $id: ID!
-    $input: ImportConfigurationInput!
-  ) {
-    workspaceWidgetConfigurationImport(id: $id, input: $input) {
-      manifest
-      ...Dashboard_workspace
-    }
-  }
-`;
 
 type WorkspaceWidgetConfigProps = {
-  data: WorkspaceWidgetConfigFragment$key;
+  handleImportWidget: (widgetFile: File) => void
   widget?: Widget,
   onComplete: (value: Widget, variableName?: string) => void,
   closeMenu?: () => void;
 };
 
-const WorkspaceWidgetConfig = ({ data, widget, onComplete, closeMenu }: WorkspaceWidgetConfigProps) => {
+const WorkspaceWidgetConfig = ({ widget, onComplete, closeMenu, handleImportWidget }: WorkspaceWidgetConfigProps) => {
   const { t_i18n } = useFormatter();
-  const workspace = useFragment(workspaceWidgetConfigFragment, data);
-
   const [isWidgetConfigOpen, setIsWidgetConfigOpen] = useState<boolean>(false);
-
-  const [commitWidgetImportMutation] = useApiMutation(workspaceImportWidgetMutation);
   const inputRef: React.MutableRefObject<HTMLInputElement | null> = useRef(null);
 
   const handleWidgetImport = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const importedWidgetConfiguration = event.target.files?.[0];
-    const emptyDashboardManifest = toB64(JSON.stringify({ widgets: {}, config: {} }));
-    const dashboardManifest = workspace.manifest ?? emptyDashboardManifest;
-    commitWidgetImportMutation({
-      variables: {
-        id: workspace.id,
-        input: {
-          importType: 'widget',
-          file: importedWidgetConfiguration,
-          dashboardManifest,
-        },
-      },
-      updater: () => {
-        if (inputRef.current) inputRef.current.value = ''; // Reset the input uploader ref
-      },
-      onError: (error) => {
-        if (inputRef.current) inputRef.current.value = ''; // Reset the input uploader ref
-        handleError(error);
-      },
-    });
+    if (importedWidgetConfiguration) handleImportWidget(importedWidgetConfiguration);
+    if (inputRef.current) inputRef.current.value = ''; // Reset the input uploader ref
   };
 
   const handleOpenWidgetConfig = () => setIsWidgetConfigOpen(true);
@@ -93,24 +48,22 @@ const WorkspaceWidgetConfig = ({ data, widget, onComplete, closeMenu }: Workspac
           />
           <Security needs={[EXPLORE_EXUPDATE]}>
             <>
-              <>
-                <Button
-                  variant='outlined'
-                  disableElevation
-                  sx={{ marginLeft: 1 }}
-                  onClick={handleImportWidgetButtonClick}
-                >
-                  {t_i18n('Import Widget')}
-                </Button>
-                <Button
-                  variant='outlined'
-                  disableElevation
-                  sx={{ marginLeft: 1 }}
-                  onClick={handleOpenWidgetConfig}
-                >
-                  {t_i18n('Create Widget')}
-                </Button>
-              </>
+              <Button
+                variant='outlined'
+                disableElevation
+                sx={{ marginLeft: 1 }}
+                onClick={handleImportWidgetButtonClick}
+              >
+                {t_i18n('Import Widget')}
+              </Button>
+              <Button
+                variant='outlined'
+                disableElevation
+                sx={{ marginLeft: 1 }}
+                onClick={handleOpenWidgetConfig}
+              >
+                {t_i18n('Create Widget')}
+              </Button>
             </>
           </Security>
         </>
