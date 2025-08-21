476da5f7a4b61e08797b9d7e46fb27d89d5e4ca7
Cathia Archidoit <cathia.archidoit@filigran.io>
2025-08-21 09:39:55 +0200

    [backend] pirLastScoreDate resolver (#11149)

--- opencti-platform/opencti-graphql/src/resolvers/stixDomainObject.js
+++ opencti-platform/opencti-graphql/src/resolvers/stixDomainObject.js
@@ -14,6 +14,7 @@ import {
   stixDomainObjectExportAsk,
   stixDomainObjectFileEdit,
   stixDomainObjectPirScore,
+  stixDomainObjectLastPirScoreDate,
   stixDomainObjectsDelete,
   stixDomainObjectsDistributionByEntity,
   stixDomainObjectsExportAsk,
@@ -76,6 +77,7 @@ const stixDomainObjectResolvers = {
       return statusesType.length > 0;
     },
     pirScore: (stixDomainObject, { pirId }, context) => stixDomainObjectPirScore(context, context.user, stixDomainObject, pirId),
+    pirLastScoreDate: (stixDomainObject, { pirId }, context) => stixDomainObjectLastPirScoreDate(context, context.user, stixDomainObject, pirId),
     pirExplanations: (stixDomainObject, { pirId }, context) => stixDomainObjectsPirExplanations(context, context.user, stixDomainObject, pirId),
   },
   Mutation: {
