b38af20a5d0d048daaf8377af8ac91d3070bdcf0
Jeremy Cloarec <jeremy.cloarec@filigran.io>
2025-08-22 13:50:48 +0200

    [backend] update tests with public_key

--- opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/connector-composer-test.ts
+++ opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/connector-composer-test.ts
@@ -2,15 +2,19 @@ import { expect, it, describe, afterAll, beforeAll } from 'vitest';
 import gql from 'graphql-tag';
 import { v4 as uuidv4 } from 'uuid';
 import { queryAsAdminWithSuccess, queryAsUserIsExpectedForbidden, queryAsUserWithSuccess } from '../../utils/testQueryHelper';
-import { ADMIN_USER, USER_CONNECTOR, USER_EDITOR } from '../../utils/testQuery';
+import { USER_CONNECTOR, USER_EDITOR } from '../../utils/testQuery';
 import { wait } from '../../../src/database/utils';
 import { XTMComposerMock } from '../../utils/XTMComposerMock';
 import type { ApiConnector } from '../../utils/XTMComposerMock';
 import { catalogHelper } from '../../utils/catalogHelper';
-import { getBaseUrl } from '../../../src/config/conf';
 
 const TEST_COMPOSER_ID = uuidv4();
 const TEST_USER_CONNECTOR_ID: string = USER_CONNECTOR.id; // Initialize with default value
+const TEST_COMPOSER_PUBLIC_KEY = '-----BEGIN RSA PUBLIC KEY-----MIICCgKCAgEAk8V1xvej71cJhH+XAVwJKXkpM90fM8gf9jhq5t2SKDnMhGfl7bSNSRKEN6zUea3F4Q635SCK0klAh69J2wh1LakPb5Z/lRv5n/OA3QnIN'
+    + 'NbQwnDD2tjMoVoR/fW/oe8AG5CvgES0kLx/wfVoxUPpWyOTtWvtIjJH+Cyrj4as4lICaeci6AdjRfhR/syVaErflTEdJyps2g5DA3p8H9f+IOTxOK8dCBYWGPHLkgEXhz8Pc6mmIq7IYHeYMUXHOokrjCKiw1s59NAJvhdSmYP15Udac1QNG'
+    + 'LTIsV8IvWvYWmUmOWUOTXAgy/sX1IuH+mt3yjmPzfScaSwPkecLkaGdUIecHZGyDHy+fJsb8vmj3NWr6MiehTFLs+UKyp+VPLSzxT29Umd/ZkqoYtMH0Fj/vIptxg7E5G/YQZtwB23OXPEZ2eC6cUKXwZXext+CS7Gdp8HoQn4Z7lpSQT'
+    + 'h6lim/Olx5SpTSpXhFE8lTjT1MlbXyeEwBRFYpb/BjthsHM3d7pRdpJL613ZzMjEgJnE5dwAX7nlTdAldChLbA7U9Hw254NLZcO301j2YHcU1JiGHTtMO7nOrVFy7FIA5HS2kKRW8dWMm7C3sVmuWEjmydsCQZIvHmPTvkTE4YnEP1U'
+    + 'ZuNczinaR08oW/CBuajmNqJsCsmg6m9ThuxizJh71NzcNMCAwEAAQ==-----END RSA PUBLIC KEY-----';
 
 // Test configuration
 // The goal is to achieve a 1:1 behavior match between XTMComposerMock and XTMComposer.
@@ -132,8 +136,6 @@ const ipinfoListProperties = [
   { key: 'CONNECTOR_LISTEN_PROTOCOL_API_SSL', value: ['false'] }
 ];
 
-
-
 describe('Connector Composer and Managed Connectors', () => {
   // Track all created resources
   const createdConnectorIds = new Set<string>();
@@ -157,7 +159,8 @@ describe('Connector Composer and Managed Connectors', () => {
     it('should register a new connector composer', async () => {
       const input = {
         id: TEST_COMPOSER_ID,
-        name: 'Test Composer'
+        name: 'Test Composer',
+        public_key: TEST_COMPOSER_PUBLIC_KEY,
       };
 
       const result = await queryAsAdminWithSuccess({
@@ -169,13 +172,15 @@ describe('Connector Composer and Managed Connectors', () => {
       expect(result.data?.registerConnectorsManager).not.toBeNull();
       expect(result.data?.registerConnectorsManager.id).toEqual(TEST_COMPOSER_ID);
       expect(result.data?.registerConnectorsManager.name).toEqual('Test Composer');
+      expect(result.data?.registerConnectorsManager.public_key).toEqual(TEST_COMPOSER_PUBLIC_KEY);
       expect(result.data?.registerConnectorsManager.last_sync_execution).not.toBeNull();
     });
 
     it('should update existing connector composer', async () => {
       const input = {
         id: TEST_COMPOSER_ID,
-        name: 'Test Composer Updated'
+        name: 'Test Composer Updated',
+        public_key: TEST_COMPOSER_PUBLIC_KEY,
       };
 
       const result = await queryAsAdminWithSuccess({
@@ -185,6 +190,7 @@ describe('Connector Composer and Managed Connectors', () => {
 
       expect(result.data).toBeDefined();
       expect(result.data?.registerConnectorsManager.name).toEqual('Test Composer Updated');
+      expect(result.data?.registerConnectorsManager.public_key).toEqual(TEST_COMPOSER_PUBLIC_KEY);
     });
 
     it('should update connector composer status', async () => {
