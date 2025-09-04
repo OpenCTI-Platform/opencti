b6058384b248154a268dcec805b2aba462f8b9fc
jpkha <jean-philippe.kha@filigran.io>
2025-09-04 13:58:51 +0200

    [frontend] create custom error message on Deploy custom dashboard (#12252)

--- opencti-platform/opencti-front/src/private/components/xtm_hub/DeployCustomDashboard.tsx
+++ opencti-platform/opencti-front/src/private/components/xtm_hub/DeployCustomDashboard.tsx
@@ -8,13 +8,19 @@ import useApiMutation from '../../../utils/hooks/useApiMutation';
 import Loader from '../../../components/Loader';
 import { UserContext } from '../../../utils/hooks/useAuth';
 import useXtmHubUserPlatformToken from '../../../utils/hooks/useXtmHubUserPlatformToken';
+import { useFormatter } from '../../../components/i18n';
 
 const DeployCustomDashboard = () => {
   const navigate = useNavigate();
   const { settings } = useContext(UserContext);
   const { serviceInstanceId, fileId } = useParams();
   const { token } = useXtmHubUserPlatformToken();
-  const [commitImportMutation] = useApiMutation<WorkspaceCreationImportMutation>(importMutation);
+  const { t_i18n } = useFormatter();
+  const [commitImportMutation] = useApiMutation<WorkspaceCreationImportMutation>(importMutation, undefined, {
+    errorMessageMap: {
+      FORBIDDEN_ACCESS: t_i18n('You are not allowed to do this because you do not have the rights to create dashboards.'),
+    },
+  });
   const sendImportToBack = (importedFile: File) => {
     commitImportMutation({
       variables: { file: importedFile },
