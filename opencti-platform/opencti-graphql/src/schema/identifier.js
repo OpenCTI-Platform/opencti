6d1028997a9ceebc620d21c1c01ddc1b427d27b4
marie flores <marie.flores@filigran.io>
2025-09-05 19:59:21 +0200

    [backend/frontend] avoid duplicate url with trailing / (#12078)

--- opencti-platform/opencti-graphql/src/schema/identifier.js
+++ opencti-platform/opencti-graphql/src/schema/identifier.js
@@ -58,6 +58,9 @@ export const normalizeName = (name) => {
   return (name || '').toLowerCase().trim();
 };
 
+const normalizeUrl = (url) => {
+  return (url || '').replace(/\/+$/, '');
+};
 export const MARKING_TLP_CLEAR_ID = '613f2e26-407d-48c7-9eca-b8e91df99dc9';
 export const MARKING_TLP_CLEAR = `marking-definition--${MARKING_TLP_CLEAR_ID}`;
 const MARKING_TLP_GREEN_ID = '34098fce-860f-48ae-8e50-ebd3cc5e41da';
@@ -241,6 +244,9 @@ const stixBaseEntityContribution = {
     },
     objects(data) {
       return data.map((o) => o.standard_id).sort();
+    },
+    url(data) {
+      return normalizeUrl(data);
     },
   },
 };
