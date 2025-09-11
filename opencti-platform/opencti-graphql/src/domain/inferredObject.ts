0970f8c5c683475a528853ea170bd590d5fc92a7
Jeremy Cloarec <jeremy.cloarec@filigran.io>
2025-09-11 11:52:46 +0200

    [backend] add protection to only allow rule user manager

--- opencti-platform/opencti-graphql/src/domain/inferredObject.ts
+++ opencti-platform/opencti-graphql/src/domain/inferredObject.ts
@@ -1,12 +1,24 @@
 import { createInferredEntity, createInferredRelation } from '../database/middleware';
 import type { AuthContext, AuthUser } from '../types/user';
+import { RULE_MANAGER_USER } from '../utils/access';
+import { ForbiddenAccess } from '../config/errors';
 
-export const createInternalInferredRelation = async (context: AuthContext, _user: AuthUser, jsonInput: string) => {
+export const createInternalInferredRelation = async (context: AuthContext, user: AuthUser, jsonInput: string) => {
+  // This API should only be available to rule manager user
+  if (user.id !== RULE_MANAGER_USER.id) {
+    throw ForbiddenAccess();
+  }
+  // TODO: JSON input validation?
   const { input, ruleContent, opts } = JSON.parse(jsonInput);
   const createdInferredRelation = await createInferredRelation(context, input, ruleContent, opts);
   return createdInferredRelation.id;
 };
-export const createInternalInferredEntity = async (context: AuthContext, _user: AuthUser, jsonInput: string) => {
+export const createInternalInferredEntity = async (context: AuthContext, user: AuthUser, jsonInput: string) => {
+  // This API should only be available to rule manager user
+  if (user.id !== RULE_MANAGER_USER.id) {
+    throw ForbiddenAccess();
+  }
+  // TODO: JSON input validation?
   const { input, ruleContent, type } = JSON.parse(jsonInput);
   const createdInferredEntity = await createInferredEntity(context, input, ruleContent, type);
   return createdInferredEntity.id;
