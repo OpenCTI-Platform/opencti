210885092c62d6d221c93ae3ac9023ec994a7757
Cathia Archidoit <cathia.archidoit@filigran.io>
2025-09-11 10:51:07 +0200

    [backend] fix pir test

--- opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/pir-test.ts
+++ opencti-platform/opencti-graphql/tests/02-integration/02-resolvers/pir-test.ts
@@ -14,6 +14,7 @@ import { resetCacheForEntity } from '../../../src/database/cache';
 import { type BasicStoreRelationPir, ENTITY_TYPE_PIR } from '../../../src/modules/pir/pir-types';
 import { RELATION_IN_PIR } from '../../../src/schema/internalRelationship';
 import { connectorsForWorker } from '../../../src/database/repository';
+import { retryUntilConditionOrMaxLoop } from '../../utils/retry';
 
 const LIST_QUERY = gql`
   query pirs(
@@ -451,12 +452,22 @@ describe('PIR resolver standard behavior', () => {
     expect(queryResult.data?.pirRelationships.edges[0].node.pir_score).toEqual(100);
     expect(queryResult.data?.pirRelationships.edges[0].node.pir_explanations.length).toEqual(2);
     // Verify the pir information has been updated at the entity level for all the PIRs concerned
-    const malwareAfterFlag = await internalLoadById<BasicStoreEntity>(
+    const fetchMalwareAfterFlag = () => internalLoadById<BasicStoreEntity>(
       testContext,
       SYSTEM_USER,
       'malware--c6006dd5-31ca-45c2-8ae0-4e428e712f88',
       { type: ENTITY_TYPE_MALWARE },
     );
+    const testMalwareAfterFlag = (malware: BasicStoreEntity) => {
+      return malware.pir_information.length === 2;
+    };
+    // wait for the pir_information de-normalization to be added for the 2 PIRs
+    const malwareAfterFlag = await retryUntilConditionOrMaxLoop({
+      fnToExecute: fetchMalwareAfterFlag,
+      verify: testMalwareAfterFlag,
+      sleepTimeBetweenLoop: 2000,
+      maxRetry: 20
+    });
     expect(malwareAfterFlag.pir_information.length).toEqual(2);
     expect(malwareAfterFlag.pir_information.filter((s) => s.pir_id === pirInternalId1).length).toEqual(1);
     expect(malwareAfterFlag.pir_information.filter((s) => s.pir_id === pirInternalId1)[0].pir_score).toEqual(100);
---
+++ opencti-platform/opencti-graphql/tests/utils/retry.ts
@@ -0,0 +1,37 @@
+import { wait } from '../../src/database/utils';
+
+interface RetryUntilConditionOrMaxLoopArgs<T> {
+  fnToExecute: () => Promise<T>,
+  verify: (input: T) => boolean,
+  sleepTimeBetweenLoop?: number,
+  maxRetry?: number,
+}
+
+/**
+ * Execute a function multiple times until the result of the function
+ * makes the condition function to return true.
+ *
+ * Retry only a limited number of times, in case the condition is never
+ * verified, the result of the last execution is returned.
+ *
+ * @param fnToExecute Function to execute to check the result.
+ * @param verify Function to check the result of the last execution.
+ * @param sleepTimeBetweenLoop Time in milliseconds between 2 executions.
+ * @param maxRetry Max number of execution to do.
+ */
+export const retryUntilConditionOrMaxLoop = async <T = unknown>(args: RetryUntilConditionOrMaxLoopArgs<T>) => {
+  const {
+    fnToExecute,
+    verify,
+    sleepTimeBetweenLoop = 1000,
+    maxRetry = 10
+  } = args;
+  let result = await fnToExecute();
+  let loopCurrent = 0;
+  while (!verify(result) && loopCurrent < maxRetry) {
+    await wait(sleepTimeBetweenLoop);
+    result = await fnToExecute();
+    loopCurrent += 1;
+  }
+  return result;
+};
