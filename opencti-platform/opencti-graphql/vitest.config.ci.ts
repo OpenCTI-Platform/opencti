87679f97b5c1c91e34f57f694fa415547e32084d
Eloise Faure <eloise.faure@filigran.io>
2025-09-12 11:52:48 +0200

    Test only api test - integration

--- opencti-platform/opencti-graphql/vitest.config.ci.ts
+++ opencti-platform/opencti-graphql/vitest.config.ci.ts
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { buildTestConfig } from './vitest.config.test';
 
-export default buildTestConfig(['tests/(01)-*/**/*-test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']);
+export default buildTestConfig(['tests/(02)-*/**/*-test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']);
 // export default buildTestConfig(['tests/(02)-*/**/(loader|filterGroup|grouping|workspace)*-test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']);
