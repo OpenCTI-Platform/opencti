0cba4ce46fdad0d3f918f5305ca6d7d0870bcb92
Adrien Servel <adrien.servel@filigran.io>
2025-09-16 14:24:55 +0200

    [chatbot] Update of configuration

--- opencti-platform/opencti-graphql/src/modules/ai/chatbot-ai-settings.ts
+++ opencti-platform/opencti-graphql/src/modules/ai/chatbot-ai-settings.ts
@@ -4,15 +4,13 @@ import type { BasicStoreSettings } from '../../types/settings';
 import { CguStatus } from '../../generated/graphql';
 
 export const getFiligranChatbotAiEndpoint = () => {
-  return nconf.get('xtm:one:ai:url');
+  return `${nconf.get('xtm:xtm_one_url')}/chatbot`;
 };
 
 export const isFiligranChatbotAiActivated = async (settings: BasicStoreSettings) => {
-  const isChatbotAiEnabled: boolean = settings.filigran_chatbot_ai_cgu_status === CguStatus.Enabled;
-  if (!isChatbotAiEnabled) {
-    return false;
-  }
   const isChatbotFeatureEnabled: boolean = isFeatureEnabled('CHATBOT_AI') || false;
-  const isChatbotAiConfigured: boolean = getFiligranChatbotAiEndpoint() !== undefined;
-  return isChatbotFeatureEnabled && isChatbotAiConfigured;
+  const isChatbotCGUAccepted: boolean = settings.filigran_chatbot_ai_cgu_status === CguStatus.Enabled;
+  const isChatbotAiConfigured: boolean = nconf.get('xtm:one:ai:url') !== undefined;
+
+  return isChatbotFeatureEnabled && isChatbotCGUAccepted && isChatbotAiConfigured;
 };
