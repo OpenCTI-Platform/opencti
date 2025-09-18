e5aa76ee554879d186e2ed9079aa9fce8a0c3ba3
Cathia Archidoit <cathia.archidoit@filigran.io>
2025-09-18 15:57:29 +0200

    [frontend/backend] PIR warning if rescan before stream start date (#11149)

--- opencti-platform/opencti-front/src/private/components/pir/pir_form/PirCreationFormGeneralSettings.tsx
+++ opencti-platform/opencti-front/src/private/components/pir/pir_form/PirCreationFormGeneralSettings.tsx
@@ -13,17 +13,44 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 */
 
-import { Field } from 'formik';
+import { Field, useFormikContext } from 'formik';
 import React from 'react';
 import MenuItem from '@mui/material/MenuItem';
+import { graphql, PreloadedQuery, usePreloadedQuery } from 'react-relay';
+import { PirCreationFormData } from '@components/pir/pir_form/pir-form-utils';
+import Alert from '@mui/material/Alert';
+import { AlertTitle } from '@mui/material';
 import MarkdownField from '../../../../components/fields/MarkdownField';
 import { useFormatter } from '../../../../components/i18n';
 import { fieldSpacingContainerStyle } from '../../../../utils/field';
 import TextField from '../../../../components/TextField';
 import SelectField from '../../../../components/fields/SelectField';
+import { PirCreationFormGeneralSettingsRedisStreamQuery } from './__generated__/PirCreationFormGeneralSettingsRedisStreamQuery.graphql';
+import { daysAgo, minutesBetweenDates } from '../../../../utils/Time';
 
-const PirCreationFormGeneralSettings = () => {
-  const { t_i18n } = useFormatter();
+export const redisStreamQuery = graphql`
+  query PirCreationFormGeneralSettingsRedisStreamQuery {
+    redisStreamInfo {
+      firstEventDate
+    }
+  }
+`;
+
+interface PirCreationFormGeneralSettingsProps {
+  redisQueryRef: PreloadedQuery<PirCreationFormGeneralSettingsRedisStreamQuery>;
+}
+
+const PirCreationFormGeneralSettings = ({ redisQueryRef }: PirCreationFormGeneralSettingsProps) => {
+  const { t_i18n, fld } = useFormatter();
+  const dataRedis = usePreloadedQuery(redisStreamQuery, redisQueryRef);
+  const firstEventDate = dataRedis.redisStreamInfo?.firstEventDate;
+
+  const { values } = useFormikContext<PirCreationFormData>();
+  const { pir_rescan_days } = values;
+
+  const dateOfRescanStart = daysAgo(pir_rescan_days);
+  const diffBetweenStreamStartAndRescan = minutesBetweenDates(firstEventDate, dateOfRescanStart);
+  const showRescanAlert = diffBetweenStreamStartAndRescan < 0; // true if first stream event date is more recent than date of rescan start
 
   return (
     <>
@@ -57,6 +84,15 @@ const PirCreationFormGeneralSettings = () => {
         <MenuItem value={30}>{t_i18n('1 month')}</MenuItem>
         <MenuItem value={182}>{t_i18n('6 months')}</MenuItem>
       </Field>
+      {showRescanAlert
+        && <Alert severity="warning" variant="outlined" style={{ marginTop: 20 }}>
+          <AlertTitle>{t_i18n('Rescan begins before stream first event date')}</AlertTitle>
+          {t_i18n(
+            'Events before stream first event date ({firstEventDate}) won\'t be taken into account.',
+            { values: { firstEventDate: fld(firstEventDate) } },
+          )}
+        </Alert>
+      }
     </>
   );
 };
