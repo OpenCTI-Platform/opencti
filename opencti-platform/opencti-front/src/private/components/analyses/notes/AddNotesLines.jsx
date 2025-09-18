9312451c9866fb5c0e914d5b372f6b3d22c148b0
Cathia Archidoit <cathia.archidoit@filigran.io>
2025-09-18 09:31:18 +0200

    [frontend] add notes lines display

--- opencti-platform/opencti-front/src/private/components/analyses/notes/AddNotesLines.jsx
+++ opencti-platform/opencti-front/src/private/components/analyses/notes/AddNotesLines.jsx
@@ -152,7 +152,7 @@ class AddNotesLinesContainer extends Component {
               <div style={{ marginRight: 50 }}>
                 <ItemMarkings
                   variant="inList"
-                  markingDefinitions={data.objectMarking ?? []}
+                  markingDefinitions={note.objectMarking ?? []}
                 />
               </div>
             </ListItemButton>
@@ -204,6 +204,10 @@ const AddNotesLines = createPaginationContainer(
                 definition
                 x_opencti_order
                 x_opencti_color
+              }
+              createdBy {
+                name
+                id
               }
             }
           }
