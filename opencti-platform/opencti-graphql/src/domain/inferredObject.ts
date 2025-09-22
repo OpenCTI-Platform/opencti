cdb5251f65dd52351cfad4b7694e3789bfefed27
Jeremy Cloarec <jeremy.cloarec@filigran.io>
2025-09-22 14:48:34 +0200

    [backend] fix CI

--- opencti-platform/opencti-graphql/src/domain/inferredObject.ts
+++ opencti-platform/opencti-graphql/src/domain/inferredObject.ts
@@ -12,7 +12,7 @@ export const createInternalInferredRelation = async (context: AuthContext, user:
   // TODO: JSON input validation? Maybe we don't need it since it's only coming from task manager?
   const { input, ruleContent, opts } = JSON.parse(jsonInput);
   // Handle special case of inferred rel creation used to push event to stream
-  if (opts.isPublishStixToStream) {
+  if (opts?.isPublishStixToStream) {
     const event = opts.publishStixEvent;
     await publishStixToStream(context, RULE_MANAGER_USER, event);
     return null;
