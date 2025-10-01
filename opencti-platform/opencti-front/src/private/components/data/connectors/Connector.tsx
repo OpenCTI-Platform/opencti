46ddf42ff55a72a8de572223cb9b6d06d0268a14
esrevi <esrevi.huynh@filigran.io>
2025-10-01 23:35:44 +0200

    [frontend] connector instance name (#12572)

--- opencti-platform/opencti-front/src/private/components/data/connectors/Connector.tsx
+++ opencti-platform/opencti-front/src/private/components/data/connectors/Connector.tsx
@@ -478,6 +478,7 @@ const ConnectorComponent: FunctionComponent<ConnectorComponentProps> = ({ connec
                       <FieldOrEmpty source={connector.connector_user}>{null}</FieldOrEmpty>
                     )}
                   </Grid>
+
                   <Grid item xs={6}>
                     <Typography variant="h3" gutterBottom={true}>
                       {t_i18n("User's organizations")}
@@ -538,19 +539,25 @@ const ConnectorComponent: FunctionComponent<ConnectorComponentProps> = ({ connec
                 </Alert>
               </Grid>
               )}
+
               <Grid item={true} xs={12}>
                 <Typography variant="h3" gutterBottom={true}>
                   {t_i18n('State')}
                 </Typography>
-                <Tooltip title={connector.connector_state || ''}>
-                  <pre>
-                    <ItemCopy
-                      content={connector.connector_state || ''}
-                      limit={200}
-                    />
-                  </pre>
-                </Tooltip>
+                {
+                  connector.connector_state ? (
+                    <Tooltip title={connector.connector_state || ''}>
+                      <pre>
+                        <ItemCopy
+                          content={connector.connector_state || ''}
+                          limit={200}
+                        />
+                      </pre>
+                    </Tooltip>
+                  ) : '-'
+                }
               </Grid>
+
               <Grid item xs={6}>
                 {!connector.connector_info && (
                   connector.connector_state
@@ -634,6 +641,16 @@ const ConnectorComponent: FunctionComponent<ConnectorComponentProps> = ({ connec
                 </Typography>
                 )}
               </Grid>
+
+              {
+                connector.is_managed && (
+                  <Grid item xs={6}>
+                    <Typography variant="h3">{t_i18n('Instance name')}</Typography>
+                    <Typography component="div" variant="body1">{connector.name}</Typography>
+                  </Grid>
+                )
+              }
+
               <Grid item xs={6}>
                 <Typography variant="h3" gutterBottom={true}>
                   {t_i18n('Server capacity')}
@@ -804,6 +821,7 @@ const Connector = createRefetchContainer(
       fragment Connector_connector on Connector {
         id
         name
+        title
         active
         auto
         only_contextual
