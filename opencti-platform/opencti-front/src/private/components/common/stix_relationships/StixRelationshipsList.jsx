c1a5aeb3b919ae9fe3eec09060b17171603cf129
Cathia Archidoit <cathia.archidoit@filigran.io>
2025-10-02 10:30:19 +0200

    [frontend] Order mode selection in Timeline widget (#11689)

--- opencti-platform/opencti-front/src/private/components/common/stix_relationships/StixRelationshipsList.jsx
+++ opencti-platform/opencti-front/src/private/components/common/stix_relationships/StixRelationshipsList.jsx
@@ -4539,7 +4539,7 @@ const StixRelationshipsList = ({
           variables={{
             first: 50,
             orderBy: dateAttribute,
-            orderMode: 'desc',
+            orderMode: selection.sort_mode ?? 'asc',
             filters,
             dynamicFrom: selection.dynamicFrom,
             dynamicTo: selection.dynamicTo,
