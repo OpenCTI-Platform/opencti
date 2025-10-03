a10b8e2ceb12e0ef0ba9d9baabdba77530a1792b
Esrevi Huynh <esrevi.huynh@filigran.io>
2025-10-03 18:05:18 +0200

    [backend] add migration connector name to container_name (#12572)

--- opencti-platform/opencti-graphql/src/database/repository.js
+++ opencti-platform/opencti-graphql/src/database/repository.js
@@ -21,8 +21,8 @@ export const CONNECTOR_PRIORITY_GROUP_VALUES = Object.values(ConnectorPriorityGr
 export const completeConnector = (connector) => {
   if (connector) {
     const completed = { ...connector };
-    completed.name = connector.name;
-    completed.container_name = connector.container_name || '';
+    completed.name = connector.name || '-';
+    completed.container_name = connector.container_name || '-';
     completed.is_managed = isNotEmptyField(connector.catalog_id);
     completed.connector_scope = connector.connector_scope ? connector.connector_scope.split(',') : [];
     completed.config = connectorConfig(connector.id, connector.listen_callback_uri);
