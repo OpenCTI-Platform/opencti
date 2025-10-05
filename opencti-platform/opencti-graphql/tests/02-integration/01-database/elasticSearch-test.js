e9988af0a1b55d76c74f4a44633574cdeabcf9dc
Julien Richard <julien.richard@filigran.io>
2025-10-06 00:34:41 +0200

    [backend] Fix test count to add 2 vocabs (#11707)

--- opencti-platform/opencti-graphql/tests/02-integration/01-database/elasticSearch-test.js
+++ opencti-platform/opencti-graphql/tests/02-integration/01-database/elasticSearch-test.js
@@ -48,7 +48,7 @@ const elWhiteUser = async () => {
   return buildStandardUser([{ internal_id: TLP_WHITE.internal_id, standard_id: TLP_WHITE.standard_id }], ALL_TLP);
 };
 
-const VOCABULARY_COUNT = 347;
+const VOCABULARY_COUNT = 349;
 
 describe('Elasticsearch configuration test', () => {
   it('should configuration correct', async () => {
@@ -450,7 +450,8 @@ describe('Elasticsearch pagination', () => {
     expect(entityTypeMap.get('Tracking-Number')).toBe(1);
     expect(entityTypeMap.get('User')).toBe(TESTING_USERS.length + 1);
     expect(entityTypeMap.get('Vocabulary')).toBe(VOCABULARY_COUNT);
-    expect(data.edges.length).toEqual(542 + TESTING_ORGS.length + TESTING_USERS.length + TESTING_ROLES.length + TESTING_GROUPS.length);
+    expect(data.edges.length).toEqual(195 + VOCABULARY_COUNT + TESTING_ORGS.length + TESTING_USERS.length
+        + TESTING_ROLES.length + TESTING_GROUPS.length);
     const filterBaseTypes = R.uniq(R.map((e) => e.node.base_type, data.edges));
     expect(filterBaseTypes.length).toEqual(1);
     expect(R.head(filterBaseTypes)).toEqual('ENTITY');
