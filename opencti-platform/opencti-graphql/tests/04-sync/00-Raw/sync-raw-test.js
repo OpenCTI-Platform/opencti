f90bf79abb13d2b6390940891197faf48b2ceef3
Julien Richard <julien.richard@filigran.io>
2025-10-07 07:13:18 +0200

    [backend] Fix test (#11707)

--- opencti-platform/opencti-graphql/tests/04-sync/00-Raw/sync-raw-test.js
+++ opencti-platform/opencti-graphql/tests/04-sync/00-Raw/sync-raw-test.js
@@ -42,7 +42,7 @@ describe('Database sync raw', () => {
   it(
     'Should python raw sync succeed',
     async () => {
-      const queryResultBefore = await queryAsAdmin({ query: LIST_QUERY, variables: { first: 350 } });
+      const queryResultBefore = await queryAsAdmin({ query: LIST_QUERY, variables: { first: VOCABULARY_NUMBERS } });
 
       // Pre check
       const { objectMap, relMap, initStixReport } = await checkPreSyncContent();
@@ -55,7 +55,7 @@ describe('Database sync raw', () => {
       // to uncomment for debug if counters are failing
       // expect(execution.messages.length, `Execution messages ${JSON.stringify(execution.messages)}`).toEqual(0);
 
-      const queryResultAfter = await queryAsAdmin({ query: LIST_QUERY, variables: { first: 350 } });
+      const queryResultAfter = await queryAsAdmin({ query: LIST_QUERY, variables: { first: VOCABULARY_NUMBERS } });
 
       const vocabBefore = queryResultBefore.data.vocabularies.edges.map((e) => e.node);
       const vocabAfter = queryResultAfter.data.vocabularies.edges.map((e) => e.node);
