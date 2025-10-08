5d4438d2cb695c6b7264748157dba59d276cc6da
Esrevi Huynh <esrevi.huynh@filigran.io>
2025-10-08 12:01:21 +0200

    [backend] update theme types built_in and add tests  (#12190)

--- opencti-platform/opencti-graphql/src/modules/theme/theme-domain.ts
+++ opencti-platform/opencti-graphql/src/modules/theme/theme-domain.ts
@@ -53,8 +53,8 @@ export const addTheme = async (context: AuthContext, user: AuthUser, input: Them
   return createInternalObject<StoreEntityTheme>(context, user, themeToCreate, ENTITY_TYPE_THEME);
 };
 
-export const initDefaultTheme = async (context: AuthContext) => {
-  logApp.info('[INIT] theme defaults starts initialization');
+export const initDefaultTheme = async (context: AuthContext, user = SYSTEM_USER) => {
+  logApp.info('[INIT] Theme defaults starts initialization');
   // Create Dark theme with user customizations or defaults
   const darkThemeInput = {
     name: 'Dark',
@@ -71,7 +71,7 @@ export const initDefaultTheme = async (context: AuthContext) => {
     built_in: true
   };
 
-  const darkTheme = await addTheme(context, SYSTEM_USER, darkThemeInput);
+  const darkTheme = await addTheme(context, user, darkThemeInput);
 
   const lightThemeInput = {
     name: 'Light',
@@ -88,10 +88,10 @@ export const initDefaultTheme = async (context: AuthContext) => {
     built_in: true
   };
 
-  await addTheme(context, SYSTEM_USER, lightThemeInput);
+  await addTheme(context, user, lightThemeInput);
 
+  logApp.info('[INIT] Theme defaults initialized');
   return darkTheme;
-  logApp.info('[INIT] theme defaults initalized');
 };
 
 export const deleteTheme = async (context: AuthContext, user: AuthUser, themeId: string) => {
@@ -99,7 +99,7 @@ export const deleteTheme = async (context: AuthContext, user: AuthUser, themeId:
   if (!theme) {
     throw FunctionalError(`Theme ${themeId} cannot be found`);
   }
-  if (theme.system_default) {
+  if (theme.built_in) {
     throw FunctionalError('System default themes cannot be deleted');
   }
   return deleteInternalObject(context, user, themeId, ENTITY_TYPE_THEME);
@@ -155,210 +155,3 @@ export const themeImport = async (context: AuthContext, user: AuthUser, file: Pr
 
   return addTheme(context, user, validationResult.data);
 };
-
-// const checkExistingTheme = async (
-//   context: AuthContext,
-//   name: string,
-//   edit_id?: string,
-// ) => {
-//   const existingTheme = await elLoadBy(
-//     context,
-//     SYSTEM_USER,
-//     'name',
-//     name,
-//     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//     // @ts-expect-error elLoadBy in engine.js implicitly defines type as null
-//     ENTITY_TYPE_THEME
-//   );
-//   if (existingTheme) {
-//     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//     // @ts-expect-error TypeScript erroneously infers type as string
-//     if (edit_id !== existingTheme.internal_id) {
-//       throw FunctionalError(
-//         'Theme already exists',
-//         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//         // @ts-expect-error TypeScript erroneously infers type as string
-//         { theme_id: existingTheme.internal_id }
-//       );
-//     }
-//   }
-// };
-//
-// export const addTheme = async (
-//   context: AuthContext,
-//   user: AuthUser,
-//   input: ThemeAddInput,
-// ) => {
-//   await checkExistingTheme(context, input.name);
-//   const created = await createEntity(context, user, input, ENTITY_TYPE_THEME);
-//
-//   await publishUserAction({
-//     user,
-//     event_type: 'mutation',
-//     event_scope: 'create',
-//     event_access: 'extended',
-//     message: `creates theme \`${created.name}\``,
-//     context_data: {
-//       id: created.id,
-//       entity_type: ENTITY_TYPE_THEME,
-//       input,
-//     },
-//   });
-//
-//   return notify(BUS_TOPICS[ENTITY_TYPE_THEME].ADDED_TOPIC, created, user);
-// };
-
-// export const findAll = async (
-//   context: AuthContext,
-//   opts: EntityOptions<BasicStoreEntityTheme>,
-// ) => pageEntitiesConnection<BasicStoreEntityTheme>(
-//   context,
-//   SYSTEM_USER,
-//   [ENTITY_TYPE_THEME],
-//   opts,
-// ).then(async (storeEntityConnection) => {
-//   if (storeEntityConnection.edges.length > 0) return storeEntityConnection;
-//
-//   // If there are no themes present when searching globally, add the default light and dark themes.
-//   if (Object.keys(opts).length < 1) {
-//     await addTheme(context, SYSTEM_USER, defaultLightTheme);
-//     await addTheme(context, SYSTEM_USER, defaultDarkTheme);
-//   }
-//
-//   return pageEntitiesConnection<BasicStoreEntityTheme>(
-//     context,
-//     SYSTEM_USER,
-//     [ENTITY_TYPE_THEME],
-//     opts,
-//   );
-// });
-
-// export const deleteTheme = async (
-//   context: AuthContext,
-//   user: AuthUser,
-//   id: string,
-// ) => {
-//   const deleted = await deleteElementById(
-//     context,
-//     user,
-//     id,
-//     ENTITY_TYPE_THEME,
-//   );
-
-// await publishUserAction({
-//   user,
-//   event_type: 'mutation',
-//   event_scope: 'delete',
-//   event_access: 'extended',
-//   message: `deletes theme \`${deleted.name}\``,
-//   context_data: {
-//     id: deleted.id,
-//     entity_type: ENTITY_TYPE_THEME,
-//     input: deleted,
-//   },
-// });
-
-//   return notify(BUS_TOPICS[ENTITY_TYPE_THEME].DELETE_TOPIC, deleted, user).then(() => id);
-// };
-
-// export const editTheme = async (
-//   context: AuthContext,
-//   user: AuthUser,
-//   id: string,
-//   input: EditInput[],
-// ) => {
-//   // Check if new name conflicts with an existing theme's name
-//   const updatedName = input.find(({ key }) => key === 'name')?.value?.[0];
-//   if (updatedName && typeof updatedName === 'string') {
-//     await checkExistingTheme(context, updatedName, id);
-//   }
-//
-//   const { element } = await updateAttribute(
-//     context,
-//     user,
-//     id,
-//     ENTITY_TYPE_THEME,
-//     input,
-//   );
-//
-//   await publishUserAction({
-//     user,
-//     event_type: 'mutation',
-//     event_scope: 'update',
-//     event_access: 'administration',
-//     message: `update theme with id:\`${element.id}\``,
-//     context_data: {
-//       id: element.id,
-//       entity_type: ENTITY_TYPE_THEME,
-//       input,
-//     },
-//   });
-//
-//   return notify(BUS_TOPICS[ENTITY_TYPE_THEME].EDIT_TOPIC, element, user);
-// };
-
-// const convertThemeManifestIds = (manifest: string) => {
-//   const parsedManifest = JSON.parse(fromBase64(manifest) ?? '{}');
-//
-//   // Disable system default flag on export, so importing these themes don't
-//   // produce undeletable themes.
-//   try {
-//     if (parsedManifest?.system_default) {
-//       parsedManifest.system_default = false;
-//     }
-//   } catch {
-//     logApp.error('[THEME] Failed to disable system default flag in exported theme');
-//   }
-//
-//   if (parsedManifest && isNotEmptyField(parsedManifest)) {
-//     return toBase64(JSON.stringify(parsedManifest)) as string;
-//   }
-//   return manifest;
-// };
-//
-// export const generateThemeExportConfiguration = async (
-//   theme: BasicStoreEntityTheme,
-// ) => {
-//   const generatedManifest = convertThemeManifestIds(theme.manifest);
-//   const exportConfiguration = {
-//     openCTI_version: pjson.version,
-//     type: 'theme',
-//     configuration: {
-//       name: theme.name,
-//       manifest: generatedManifest,
-//     },
-//   };
-//   return JSON.stringify(exportConfiguration);
-// };
-//
-// export const themeImport = async (
-//   context: AuthContext,
-//   user: AuthUser,
-//   file: Promise<FileHandle>,
-// ) => {
-//   const parsedData = await extractContentFrom(file);
-//   checkConfigurationImport('theme', parsedData);
-//   const mappedData = {
-//     openCTI_version: parsedData.openCTI_version,
-//     type: parsedData.type,
-//     name: parsedData.configuration.name,
-//     manifest: parsedData.configuration.manifest,
-//   };
-//   await checkExistingTheme(context, mappedData.name);
-//   const importThemeCreation = await createEntity(context, user, mappedData, ENTITY_TYPE_THEME);
-//   const themeId = importThemeCreation.id;
-//   await publishUserAction({
-//     user,
-//     event_type: 'mutation',
-//     event_scope: 'create',
-//     event_access: 'extended',
-//     message: `import ${importThemeCreation.name} theme`,
-//     context_data: {
-//       id: themeId,
-//       entity_type: ENTITY_TYPE_THEME,
-//       input: importThemeCreation,
-//     },
-//   });
-//   await notify(BUS_TOPICS[ENTITY_TYPE_THEME].ADDED_TOPIC, importThemeCreation, user);
-//   return importThemeCreation;
-// };
