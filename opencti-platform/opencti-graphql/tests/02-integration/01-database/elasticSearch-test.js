d281b4f1df5f940818eec23461c52db75dd95903
Angelique Jard <angelique.jard@filigran.io>
2025-10-09 12:19:40 +0200

    Revert migration test part

--- opencti-platform/opencti-graphql/tests/02-integration/01-database/elasticSearch-test.js
+++ opencti-platform/opencti-graphql/tests/02-integration/01-database/elasticSearch-test.js
@@ -450,8 +450,7 @@ describe('Elasticsearch pagination', () => {
     expect(entityTypeMap.get('Tracking-Number')).toBe(1);
     expect(entityTypeMap.get('User')).toBe(TESTING_USERS.length + 1);
     expect(entityTypeMap.get('Vocabulary')).toBe(VOCABULARY_COUNT);
-    expect(entityTypeMap.get('MigrationStatus')).toBe(1);
-    expect(data.edges.length, `EntityMap content:${JSON.stringify(entityTypeMap)}`).toEqual(542 + TESTING_ORGS.length + TESTING_USERS.length + TESTING_ROLES.length + TESTING_GROUPS.length);
+    expect(data.edges.length, `EntityMap content:${JSON.stringify(entityTypeMap)}`).toEqual(541 + TESTING_ORGS.length + TESTING_USERS.length + TESTING_ROLES.length + TESTING_GROUPS.length);
     const filterBaseTypes = R.uniq(R.map((e) => e.node.base_type, data.edges));
     expect(filterBaseTypes.length).toEqual(1);
     expect(R.head(filterBaseTypes)).toEqual('ENTITY');
@@ -599,8 +598,7 @@ describe('Elasticsearch pagination', () => {
     expect(entityTypeMap.get('Tracking-Number')).toBe(1);
     expect(entityTypeMap.get('User')).toBe(TESTING_USERS.length + 1);
     expect(entityTypeMap.get('Vocabulary')).toBe(VOCABULARY_COUNT);
-    expect(entityTypeMap.get('MigrationStatus')).toBe(1);
-    expect(data.edges.length).toEqual(551);
+    expect(data.edges.length).toEqual(550);
   });
   it('should entity paginate with field exist filter', async () => {
     const filters = {
@@ -737,8 +735,7 @@ describe('Elasticsearch pagination', () => {
     expect(entityTypeMap.get('Label')).toBe(13);
     expect(entityTypeMap.get('Kill-Chain-Phase')).toBe(2);
     expect(entityTypeMap.get('External-Reference')).toBe(7);
-    expect(entityTypeMap.get('MigrationStatus')).toBe(1);
-    expect(data.edges.length).toEqual(562);
+    expect(data.edges.length).toEqual(561);
     const createdDates = R.map((e) => e.node.created, data.edges);
     let previousCreatedDate = null;
     for (let index = 0; index < createdDates.length; index += 1) {
