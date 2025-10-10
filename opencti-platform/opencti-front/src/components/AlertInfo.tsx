edb687f48768af7c76dcd244bf1a3dd5db7800fb
Octave <octave.albert@filigran.io>
2025-10-10 19:36:47 +0200

    [frontend] Alert component can use all severity levels

--- opencti-platform/opencti-front/src/components/AlertInfo.tsx
+++ opencti-platform/opencti-front/src/components/AlertInfo.tsx
@@ -1,27 +1,27 @@
-import Alert from '@mui/material/Alert';
-import Typography from '@mui/material/Typography';
-import React, { CSSProperties, ReactNode } from 'react';
-import { useTheme } from '@mui/styles';
-import type { Theme } from './Theme';
+import Alert from "@mui/material/Alert";
+import type { AlertProps } from "@mui/material/Alert";
+import Typography from "@mui/material/Typography";
+import React, { CSSProperties, ReactNode } from "react";
+import { useTheme } from "@mui/styles";
+import type { Theme } from "./Theme";
 
 type AlertInfoProps = {
-  content: string | ReactNode
-  style?: CSSProperties
+  content: string | ReactNode;
+  style?: CSSProperties;
+  severity?: AlertProps["severity"];
 };
 
-const AlertInfo = ({ content, style }: AlertInfoProps) => {
+const AlertInfo = ({ content, style, severity = "info" }: AlertInfoProps) => {
   const theme = useTheme<Theme>();
 
   return (
     <div style={style}>
       <Alert
-        severity="info"
+        severity={severity}
         variant="outlined"
         style={{ padding: `0 ${theme.spacing(1)}` }}
       >
-        <Typography variant={'body2'}>
-          {content}
-        </Typography>
+        <Typography variant={"body2"}>{content}</Typography>
       </Alert>
     </div>
   );
