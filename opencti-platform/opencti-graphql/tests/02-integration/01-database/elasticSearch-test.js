d8df3fb2e7157e22d28f4767f7f3fa56760ae1e2
Gwendoline Favre-Felix <gwendoline.favre-felix@filigran.io>
2025-10-13 16:50:28 +0200

    [backend] Fix counter (#12190)

--- opencti-platform/opencti-graphql/tests/02-integration/01-database/elasticSearch-test.js
+++ opencti-platform/opencti-graphql/tests/02-integration/01-database/elasticSearch-test.js
@@ -600,7 +600,7 @@ describe('Elasticsearch pagination', () => {
     expect(entityTypeMap.get('Tracking-Number')).toBe(1);
     expect(entityTypeMap.get('User')).toBe(TESTING_USERS.length + 1);
     expect(entityTypeMap.get('Vocabulary')).toBe(VOCABULARY_COUNT);
-    expect(data.edges.length).toEqual(552);
+    expect(data.edges.length).toEqual(554);
   });
   it('should entity paginate with field exist filter', async () => {
     const filters = {
@@ -738,7 +738,7 @@ describe('Elasticsearch pagination', () => {
     expect(entityTypeMap.get('Label')).toBe(13);
     expect(entityTypeMap.get('Kill-Chain-Phase')).toBe(2);
     expect(entityTypeMap.get('External-Reference')).toBe(7);
-    expect(data.edges.length).toEqual(563);
+    expect(data.edges.length).toEqual(565);
     const createdDates = R.map((e) => e.node.created, data.edges);
     let previousCreatedDate = null;
     for (let index = 0; index < createdDates.length; index += 1) {
