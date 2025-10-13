52446f61460e1c6512b6bc3ae4b72e8994c1c8f3
Esrevi Huynh <esrevi.huynh@filigran.io>
2025-10-13 10:47:23 +0200

    [frontend] should fix tests (#12190)

--- opencti-platform/opencti-front/src/utils/hooks/useAuth.test.ts
+++ opencti-platform/opencti-front/src/utils/hooks/useAuth.test.ts
@@ -11,6 +11,44 @@ describe('Hook: useAuth', () => {
     platformModuleHelpers: { platformModuleHelpers: 'platformModuleHelpers' },
     settings: { settings: 'settings' },
     about: { version: '6.7.17' },
+    themes:
+      {
+        edges: [
+          {
+            node: {
+              id: 'aa4294eb-7f02-45f3-81c0-e52268021cd3',
+              name: 'Light',
+              theme_background: '#f8f8f8',
+              theme_accent: '#dfdfdf',
+              theme_paper: '#ffffff',
+              theme_nav: '#ffffff',
+              theme_primary: '#001bda',
+              theme_secondary: '#0c7e69',
+              theme_text_color: '#000000',
+              theme_logo: '',
+              theme_logo_collapsed: '',
+              theme_logo_login: '' },
+          },
+          {
+            node: {
+              id: 'b9e9766f-467c-4b1e-a4cd-dbf600e139be',
+              name: 'Dark',
+              theme_background: '#161616',
+              theme_accent: '#0f1e38',
+              theme_paper: '#09101e',
+              theme_nav: '#070d19',
+              theme_primary: '#0fbcff',
+              theme_secondary: '#00f1bd',
+              theme_text_color: '#ffffff',
+              theme_logo: '',
+              theme_logo_collapsed: '',
+              theme_logo_login: '',
+            },
+          }],
+        me: {
+          theme: 'b9e9766f-467c-4b1e-a4cd-dbf600e139be',
+        },
+      },
   } as unknown as UserContextType;
 
   it('should throw an error if "me" undefined', () => {
