ccfbe04b7ecc4a22504d3dacccb7de18a4db80da
Cathia Archidoit <cathia.archidoit@filigran.io>
2025-10-13 11:03:57 +0200

    [frontend] try e2e fix

--- opencti-platform/opencti-front/tests_e2e/model/DashboardWidgets.pageModel.ts
+++ opencti-platform/opencti-front/tests_e2e/model/DashboardWidgets.pageModel.ts
@@ -85,8 +85,9 @@ export default class DashboardWidgetsPageModel {
     await this.filters.addFilter('Label', 'e2e');
     await this.validateFilters();
     await this.titleField.fill('List of malwares');
-    const orderMode = new SelectFieldPageModel(this.page, 'Sort mode', false);
-    await orderMode.selectOption('Desc');
+    await this.page.getByRole('combobox', { name: 'Sort mode' }).click();
+    const list = this.page.getByRole('listbox', { name: 'Sort mode' });
+    return list.getByText('Desc', { exact: true }).click();
     await this.createWidget();
   }
 
@@ -99,8 +100,9 @@ export default class DashboardWidgetsPageModel {
     await this.filters.addFilter('Label', 'e2e');
     await this.validateFilters();
     await this.titleField.fill('Timeline of malwares');
-    const orderMode = new SelectFieldPageModel(this.page, 'Sort mode', false);
-    await orderMode.selectOption('Desc');
+    await this.page.getByRole('combobox', { name: 'Sort mode' }).click();
+    const list = this.page.getByRole('listbox', { name: 'Sort mode' });
+    return list.getByText('Desc', { exact: true }).click();
     await this.createWidget();
   }
 
