a8989ca5ff35fd5394bb64db2c14ea66eade8e67
Cathia Archidoit <cathia.archidoit@filigran.io>
2025-10-14 16:09:48 +0200

    [backend] use marked instead of showdown

--- opencti-platform/opencti-graphql/src/utils/NotificationTool.ts
+++ opencti-platform/opencti-graphql/src/utils/NotificationTool.ts
@@ -1,14 +1,8 @@
-import showdown from 'showdown';
+import { marked } from 'marked';
 
 export default class NotificationTool {
-  public static markdownToHtml(markdownText: string): string {
-    const converter = new showdown.Converter();
-    return converter.makeHtml(markdownText);
-  }
-
   // eslint-disable-next-line class-methods-use-this
   m2h(markdownText: string) : string {
-    const converter = new showdown.Converter();
-    return converter.makeHtml(markdownText);
+    return marked(markdownText) as string;
   }
 }
