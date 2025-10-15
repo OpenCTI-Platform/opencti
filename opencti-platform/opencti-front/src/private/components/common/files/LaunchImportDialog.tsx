305a783d99dffb49c1225b8c03a99f03c4fbfd58
Florian Delemarre <florian.delemarre@filigran.io>
2025-10-15 15:26:53 +0200

    fix lint

--- opencti-platform/opencti-front/src/private/components/common/files/LaunchImportDialog.tsx
+++ opencti-platform/opencti-front/src/private/components/common/files/LaunchImportDialog.tsx
@@ -13,6 +13,7 @@ import { fileManagerAskJobImportMutation } from '@components/common/files/FileMa
 import { PreloadedQuery, usePreloadedQuery } from 'react-relay';
 import { ImportWorksDrawerQuery, ImportWorksDrawerQuery$data } from '@components/common/files/__generated__/ImportWorksDrawerQuery.graphql';
 import { fileWorksQuery } from '@components/common/files/ImportWorksDrawer';
+import AuthorizedMembersField, { AuthorizedMembersFieldValue } from '@components/common/form/AuthorizedMembersField';
 import { ImportWorkbenchesContentFileLine_file$data } from '@components/data/import/__generated__/ImportWorkbenchesContentFileLine_file.graphql';
 import { ImportFilesContentFileLine_file$data } from '@components/data/import/__generated__/ImportFilesContentFileLine_file.graphql';
 import { commitMutation, defaultCommitMutation } from '../../../../relay/environment';
@@ -20,10 +21,7 @@ import { FieldOption, fieldSpacingContainerStyle } from '../../../../utils/field
 import { resolveHasUserChoiceParsedCsvMapper } from '../../../../utils/csvMapperUtils';
 import SelectField from '../../../../components/fields/SelectField';
 import { useFormatter } from '../../../../components/i18n';
-import stopEvent from '../../../../utils/domEvent';
-import AuthorizedMembersField, { AuthorizedMembersFieldValue } from '@components/common/form/AuthorizedMembersField';
 import useAuth from '../../../../utils/hooks/useAuth';
-import Switch from '@mui/material/Switch';
 
 interface LaunchImportDialogProps {
   file: ImportWorkbenchesContentFileLine_file$data | ImportFilesContentFileLine_file$data;
