813ba74a1de903672ce3d3918a91c063b9466d89
Jeremy Cloarec <159018898+JeremyCloarec@users.noreply.github.com>
2025-10-16 10:52:40 +0200

    [backend] delete work when deleting task & verify that work exists in worker (#12815)

--- opencti-worker/src/push_handler.py
+++ opencti-worker/src/push_handler.py
@@ -55,9 +55,14 @@ class PushHandler:  # pylint: disable=too-many-instance-attributes
             self.api.set_draft_id(data.get("draft_id"))
             self.api.set_synchronized_upsert_header(data.get("synchronized", False))
             self.api.set_previous_standard_header(data.get("previous_standard"))
-
-            # Execute the import
             work_id = data.get("work_id")
+            # Check if work is still valid
+            if work_id is not None:
+                is_work_alive = self.api.work.get_is_work_alive(work_id)
+                # If work no longer exists, bundle can be acked without doing anything
+                if not is_work_alive:
+                    return "ack"
+            # Execute the import
             types = (
                 data["entities_types"]
                 if "entities_types" in data and len(data["entities_types"]) > 0
