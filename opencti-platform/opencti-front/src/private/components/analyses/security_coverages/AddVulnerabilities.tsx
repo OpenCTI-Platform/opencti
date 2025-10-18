08fc5f177ccf52a7796323ee6aeae950603b9b3c
Samuel Hassine <samuel.hassine@filigran.io>
2025-10-18 07:56:53 +0400

    [backend/frontend] Enhance

---
+++ opencti-platform/opencti-front/src/private/components/analyses/security_coverages/AddVulnerabilities.tsx
@@ -0,0 +1,111 @@
+import React, { FunctionComponent, useState } from 'react';
+import { Theme } from '@mui/material/styles/createTheme';
+import makeStyles from '@mui/styles/makeStyles';
+import IconButton from '@mui/material/IconButton';
+import { Add } from '@mui/icons-material';
+import Drawer from '../../common/drawer/Drawer';
+import { useFormatter } from '../../../../components/i18n';
+import SearchInput from '../../../../components/SearchInput';
+import { QueryRenderer } from '../../../../relay/environment';
+import AddVulnerabilitiesLines, { addVulnerabilitiesLinesQuery } from './AddVulnerabilitiesLines';
+
+// Deprecated - https://mui.com/system/styles/basics/
+// Do not use it for new code.
+const useStyles = makeStyles<Theme>(() => ({
+  createButton: {
+    float: 'left',
+    marginTop: -15,
+  },
+}));
+
+interface AddVulnerabilitiesProps {
+  securityCoverage: {
+    id: string;
+  };
+  securityCoverageVulnerabilities: Array<{
+    node: {
+      id: string;
+    };
+  }>;
+}
+
+const AddVulnerabilities: FunctionComponent<AddVulnerabilitiesProps> = ({
+  securityCoverage,
+  securityCoverageVulnerabilities,
+}) => {
+  const classes = useStyles();
+  const { t_i18n } = useFormatter();
+  const [open, setOpen] = useState(false);
+  const [search, setSearch] = useState('');
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setSearch('');
+  };
+
+  const handleSearch = (keyword: string) => {
+    setSearch(keyword);
+  };
+
+  return (
+    <>
+      <IconButton
+        color="primary"
+        aria-label="Add"
+        onClick={handleOpen}
+        classes={{ root: classes.createButton }}
+        size="large"
+      >
+        <Add fontSize="small" />
+      </IconButton>
+      <Drawer
+        open={open}
+        onClose={handleClose}
+        title={t_i18n('Add vulnerabilities')}
+        header={(
+          <div
+            style={{
+              marginLeft: 'auto',
+              marginRight: '20px',
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <SearchInput
+              variant="thin"
+              onSubmit={handleSearch}
+            />
+          </div>
+        )}
+      >
+        {open && (
+          <QueryRenderer
+            query={addVulnerabilitiesLinesQuery}
+            variables={{
+              search,
+              count: 20,
+            }}
+            render={({ props }: { props: any }) => {
+              if (props) {
+                return (
+                  <AddVulnerabilitiesLines
+                    securityCoverage={securityCoverage}
+                    securityCoverageVulnerabilities={securityCoverageVulnerabilities}
+                    data={props}
+                  />
+                );
+              }
+              return <div />;
+            }}
+          />
+        )}
+      </Drawer>
+    </>
+  );
+};
+
+export default AddVulnerabilities;
---
+++ opencti-platform/opencti-front/src/private/components/analyses/security_coverages/SecurityCoverageSecurityPlatforms.tsx
@@ -0,0 +1,173 @@
+import React, { FunctionComponent } from 'react';
+import { filter } from 'ramda';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { Link } from 'react-router-dom';
+import IconButton from '@mui/material/IconButton';
+import { LinkOff } from '@mui/icons-material';
+import { LockPattern } from 'mdi-material-ui';
+import { createFragmentContainer, graphql } from 'react-relay';
+import { ListItemButton } from '@mui/material';
+import { RecordSourceSelectorProxy } from 'relay-runtime';
+import { commitMutation } from '../../../../relay/environment';
+import { useFormatter } from '../../../../components/i18n';
+import FieldOrEmpty from '../../../../components/FieldOrEmpty';
+import AddSecurityPlatforms from './AddSecurityPlatforms';
+
+const removeMutation = graphql`
+  mutation SecurityCoverageSecurityPlatformsRelationDeleteMutation(
+    $fromId: StixRef!
+    $toId: StixRef!
+    $relationship_type: String!
+  ) {
+    stixCoreRelationshipDelete(
+      fromId: $fromId
+      toId: $toId
+      relationship_type: $relationship_type
+    )
+  }
+`;
+
+interface SecurityPlatformNode {
+  id: string;
+  name: string;
+  description?: string;
+  parent_types: string[];
+}
+
+interface SecurityPlatformRelationshipNode {
+  id: string;
+  to: SecurityPlatformNode;
+}
+
+interface SecurityCoverageSecurityPlatformsProps {
+  securityCoverage: {
+    id: string;
+    name: string;
+    parent_types: string[];
+    entity_type: string;
+    securityPlatforms: {
+      edges: Array<{
+        node: SecurityPlatformRelationshipNode;
+      }>;
+    };
+  };
+}
+
+const SecurityCoverageSecurityPlatformsComponent: FunctionComponent<SecurityCoverageSecurityPlatformsProps> = ({
+  securityCoverage,
+}) => {
+  const { t_i18n } = useFormatter();
+
+  const removeSecurityPlatform = (securityPlatformEdge: { node: SecurityPlatformRelationshipNode }) => {
+    commitMutation({
+      mutation: removeMutation,
+      variables: {
+        fromId: securityCoverage.id,
+        toId: securityPlatformEdge.node.to.id,
+        relationship_type: 'has-covered',
+      },
+      updater: (store: RecordSourceSelectorProxy) => {
+        const node = store.get(securityCoverage.id);
+        if (node) {
+          const securityPlatforms = node.getLinkedRecord('securityPlatforms');
+          if (securityPlatforms) {
+            const edges = securityPlatforms.getLinkedRecords('edges');
+            const newEdges = filter(
+              (n) => n?.getLinkedRecord('node')?.getValue('id')
+                !== securityPlatformEdge.node.id,
+              edges,
+            );
+            securityPlatforms.setLinkedRecords(newEdges, 'edges');
+          }
+        }
+      },
+    });
+  };
+
+  return (
+    <div style={{ marginTop: 20 }}>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <Typography variant="h3" gutterBottom={true}>
+          {t_i18n('Security Platforms')}
+        </Typography>
+        <AddSecurityPlatforms
+          securityCoverage={securityCoverage}
+          securityCoverageSecurityPlatforms={securityCoverage.securityPlatforms.edges}
+        />
+      </div>
+      <div className="clearfix" />
+      <List style={{ marginTop: -10 }}>
+        <FieldOrEmpty source={securityCoverage.securityPlatforms.edges}>
+          {securityCoverage.securityPlatforms.edges.map((securityPlatformEdge) => {
+            const securityPlatform = securityPlatformEdge.node.to;
+            return (
+              <ListItem
+                key={securityPlatformEdge.node.id}
+                dense={true}
+                divider={true}
+                disablePadding={true}
+                secondaryAction={
+                  <IconButton
+                    aria-label="Remove"
+                    onClick={() => removeSecurityPlatform(securityPlatformEdge)}
+                    size="large"
+                  >
+                    <LinkOff/>
+                  </IconButton>
+                }
+              >
+                <ListItemButton
+                  component={Link}
+                  to={`/dashboard/entities/security_platforms/${securityPlatform.id}`}
+                >
+                  <ListItemIcon>
+                    <LockPattern color="primary"/>
+                  </ListItemIcon>
+                  <ListItemText primary={securityPlatform.name}/>
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </FieldOrEmpty>
+      </List>
+    </div>
+  );
+};
+
+const SecurityCoverageSecurityPlatforms = createFragmentContainer(
+  SecurityCoverageSecurityPlatformsComponent,
+  {
+    securityCoverage: graphql`
+      fragment SecurityCoverageSecurityPlatforms_securityCoverage on SecurityCoverage {
+        id
+        name
+        parent_types
+        entity_type
+        securityPlatforms: stixCoreRelationships(
+          relationship_type: "has-covered"
+          toTypes: ["SecurityPlatform"]
+        ) {
+          edges {
+            node {
+              id
+              to {
+                ... on SecurityPlatform {
+                  id
+                  parent_types
+                  name
+                  description
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+  },
+);
+
+export default SecurityCoverageSecurityPlatforms;
