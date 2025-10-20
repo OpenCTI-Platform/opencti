63931bcbb64211e084cd2c224919a707b9a448e1
Landry Trebon <landry.trebon@filigran.io>
2025-10-20 10:29:00 +0200

    [backend] ROLLBACK poc filters events transition

--- opencti-platform/opencti-graphql/src/utils/filtering/filtering-stix/stix-testers.ts
+++ opencti-platform/opencti-graphql/src/utils/filtering/filtering-stix/stix-testers.ts
@@ -44,9 +44,7 @@ import {
   SEVERITY_FILTER,
   TYPE_FILTER,
   WORKFLOW_FILTER,
-  RELATION_IN_PIR_FILTER,
   PATTERN_TYPE_FILTER,
-  EVENT_TRANSITION_AFTER_FILTER
 } from '../filtering-constants';
 import type { Filter } from '../../../generated/graphql';
 import { STIX_RESOLUTION_MAP_PATHS } from '../filtering-resolution';
@@ -54,8 +52,6 @@ import { extractStixRepresentative } from '../../../database/stix-representative
 import { type AuthorizedMember, isUserInAuthorizedMember } from '../../access';
 import type { AuthUser } from '../../../types/user';
 import { UnsupportedError } from '../../../config/errors';
-import { RELATION_IN_PIR } from '../../../schema/internalRelationship';
-import type { UpdateEvent } from '../../../types/event';
 
 //-----------------------------------------------------------------------------------
 // Testers for each possible filter.
@@ -385,14 +381,6 @@ export const testRelationToTypes = (stix: any, filter: Filter) => {
   return false;
 };
 
-export const testRelationInPir = (stix: any, filter: Filter) => {
-  if (stix.type === ABSTRACT_INTERNAL_RELATIONSHIP && stix.relationship_type === RELATION_IN_PIR) {
-    const stixValues: string[] = toValidArray(stix.target_ref);
-    return testStringFilter(filter, stixValues);
-  }
-  return false;
-};
-
 /**
  * REPRESENTATIVE
  */
@@ -468,38 +456,6 @@ export const testCvssSeverity = (stix: any, filter: Filter) => {
   return testStringFilter(filter, value);
 };
 
-export const testEventTransitionAfter = (_: any, filter: Filter, updateContext?: UpdateEvent['context']) => {
-  const patch = updateContext?.patch ?? [];
-  const results: boolean[] = [];
-  filter.values.forEach((patchFilter: Filter) => {
-    const [attributeKey] = patchFilter.key; // TODO in this case key should be an array with 1 element
-    const patchOfFilter = patch.find((p) => {
-      return p.path === `/${attributeKey}`
-        || p.path === `/extensions/${STIX_EXT_OCTI}/${attributeKey}`;
-    }); // TODO can we have several patch with same path?
-    if (!patchOfFilter || patchOfFilter.op !== 'replace') { // TODO by defautl op = replace, so pass if op is undefined ?
-      results.push(false);
-    } else {
-      const valueToCheck = patchOfFilter.value; // TODO check it is never an array
-      switch (typeof valueToCheck) {
-        case 'boolean':
-          results.push(testBooleanFilter(patchFilter, valueToCheck));
-          break;
-        case 'string':
-          results.push(testStringFilter(patchFilter, [valueToCheck]));
-          break;
-        case 'number':
-          results.push(testNumericFilter(patchFilter, valueToCheck));
-          break;
-        default:
-          results.push(false);
-          break;
-      }
-    }
-  });
-  return filter.mode === 'and' ? results.every((r) => r) : results.some((r) => r);
-};
-
 /**
  * TODO: This mapping could be given by the schema, like we do with stix converters
  */
@@ -546,6 +502,4 @@ export const FILTER_KEY_TESTERS_MAP: Record<string, TesterFunction> = {
   [RELATION_TO_FILTER]: testRelationTo,
   [RELATION_TO_TYPES_FILTER]: testRelationToTypes,
   [REPRESENTATIVE_FILTER]: testRepresentative,
-  [RELATION_IN_PIR_FILTER]: testRelationInPir,
-  [EVENT_TRANSITION_AFTER_FILTER]: testEventTransitionAfter, // filter on update events
 };
