a8f648b3a7c6bb632e098bf87f397a35fb8bca88
Angelique Jard <angelique.jard@filigran.io>
2025-10-30 11:17:53 +0100

    [backend] change SSO absolute redirection (#13011)

--- opencti-platform/opencti-graphql/src/http/httpPlatform.js
+++ opencti-platform/opencti-graphql/src/http/httpPlatform.js
@@ -35,16 +35,15 @@ import { setCookieError } from './httpUtils';
 import { getChatbotProxy } from './httpChatbotProxy';
 
 export const sanitizeReferer = (refererToSanitize) => {
+  if (!refererToSanitize) return '/';
   const base = getBaseUrl();
-  if (!refererToSanitize) return base;
-
   const resolvedUrl = new URL(refererToSanitize, base).toString();
   if (resolvedUrl === base || resolvedUrl.startsWith(`${base}/`)) {
     // same domain URL accept the redirection
-    return resolvedUrl;
+    return refererToSanitize;
   }
   logApp.info('Error auth provider callback : url has been altered', { url: refererToSanitize });
-  return base;
+  return '/';
 };
 
 const extractRefererPathFromReq = (req) => {
