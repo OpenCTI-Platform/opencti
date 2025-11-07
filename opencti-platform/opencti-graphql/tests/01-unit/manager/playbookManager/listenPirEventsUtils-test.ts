3b852dd38112f4db1ed8696b9e5441f0e45404bf
Marie Torbay <marie.torbay@filigran.io>
2025-11-07 11:55:09 +0100

    Updated listenPirEventsUtils unit tests

--- opencti-platform/opencti-graphql/tests/01-unit/manager/playbookManager/listenPirEventsUtils-test.ts
+++ opencti-platform/opencti-graphql/tests/01-unit/manager/playbookManager/listenPirEventsUtils-test.ts
@@ -7,6 +7,7 @@ import * as playbookUtils from '../../../../src/manager/playbookManager/playbook
 import * as stixRelationship from '../../../../src/schema/stixRelationship';
 import * as stixFiltering from '../../../../src/utils/filtering/filtering-stix/stix-filtering';
 import * as middleware from '../../../../src/database/middleware';
+import * as middlewareLoader from '../../../../src/database/middleware-loader';
 import * as playbookExecutor from '../../../../src/manager/playbookManager/playbookExecutor';
 import { RELATION_IN_PIR } from '../../../../src/schema/internalRelationship';
 
@@ -45,59 +46,28 @@ describe('listenPirEventsUtils', () => {
     data: {
       id: 'internal-relationship--id-3',
       scope: 'internal',
-      spec_version: '2.1',
+      // spec_version: '2.1',
       type: 'create',
       data: { relationship_type: RELATION_IN_PIR, },
       extensions: { 'extension-definition--id-4':
         {
-          extension_type: 'new-sro',
+          // extension_type: 'new-sro',
           id: 'id-5',
           type: 'in-pir',
-          created_at: '2025-10-21T08:28:06.407Z',
-          updated_at: '2025-10-21T08:28:06.407Z',
-          is_inferred: false,
-          creator_ids: ['id-6'],
-          source_value: 'malware 3',
-          source_ref: 'id-7',
-          source_type: 'Malware',
           source_ref_pir_refs: ['id-1'],
-          target_value: 'test patch',
-          target_ref: 'id-1',
-          target_type: 'Pir',
-          pir_score: 40,
-          pir_explanation: [
-            {
-              dependencies: [{ element_id: 'id-8' }],
-              criterion: {
-                weight: 1,
-                filters: {
-                  mode: 'and',
-                  filters: [{
-                    key: ['entity_type'],
-                    values: ['targets'],
-                    operator: 'eq',
-                    mode: 'or' }, {
-                    key: ['toId'],
-                    values: ['id-9'],
-                    operator: 'eq',
-                    mode: 'or' }],
-                  filterGroups: []
-                }
-              }
-            },
-            { dependencies: [{ element_id: 'id-10' }],
-              criterion: {
-                weight: 1,
-                filters: {
-                  mode: 'and',
-                  filters: [{
-                    key: ['entity_type'],
-                    values: ['targets'],
-                    operator: 'eq',
-                    mode: 'or' }, { key: ['toId'], values: ['id-11'], operator: 'eq', mode: 'or' }],
-                  filterGroups: [] } } }] } },
-      source_ref: 'malware--id-12',
-      target_ref: 'pir--id-13' } } as unknown as SseEvent<StreamDataEvent>;
+          target_ref: 'id-1'
+        } },
+    } } as unknown as SseEvent<StreamDataEvent>;
+
+  const randomEventNotInPir = { ...randomEventInPir, data: { ...randomEventInPir.data, data: { relationship_type: 'not-pir-relationship' }, } } as unknown as SseEvent<StreamDataEvent>;
+
+  const randomUpdateEventOnEntity = { data: {
+    type: 'update',
+    scope: 'external',
+    data: {
+      id: 'malware--id',
+    }
+  } } as unknown as SseEvent<StreamDataEvent>;
 
   const randomInstance = {
     id: 'id-14',
@@ -108,18 +78,8 @@ describe('listenPirEventsUtils', () => {
   };
 
   const randomPlaybook = {
-    _index: 'opencti_internal_objects-000001',
     _id: 'id-15',
     id: 'id-15',
-    sort: ['playbook--id-16'],
-    standard_id: 'playbook--id-16',
-    internal_id: 'id-15',
-    parent_types: ['Basic-Object', 'Internal-Object'],
-    playbook_start: 'id-14',
-    i_attributes: [[Object], [Object], [Object]],
-    confidence: 100,
-    playbook_running: true,
-    description: '',
     playbook_definition: '{"nodes":[{"id":"id-14","name":"Listen PIR events","position":{"x":0,"y":0},"component_id":"PLAYBOOK_DATA_STREAM_PIR","configuration":"{\\"inPirFilters\\":[{\\"label\\":\\"test patch\\",\\"value\\":\\"id-1\\",\\"type\\":\\"Pir\\"}],\\"create\\":true,\\"delete\\":true,\\"filters\\":\\"{\\\\\\"mode\\\\\\":\\\\\\"and\\\\\\",\\\\\\"filters\\\\\\":[{\\\\\\"key\\\\\\":[\\\\\\"entity_type\\\\\\"],\\\\\\"operator\\\\\\":\\\\\\"eq\\\\\\",\\\\\\"values\\\\\\":[\\\\\\"Malware\\\\\\"],\\\\\\"mode\\\\\\":\\\\\\"or\\\\\\"}],\\\\\\"filterGroups\\\\\\":[]}\\"}"},{"id":"id-17","name":"Send to notifier","position":{"x":0,"y":300},"component_id":"PLAYBOOK_NOTIFIER_COMPONENT","configuration":"{\\"notifiers\\":[\\"id-18\\"],\\"authorized_members\\":[{\\"label\\":\\"admin@opencti.io\\",\\"value\\":\\"id-19\\",\\"type\\":\\"User\\"}]}"},{"id":"id-20","name":"Log data in standard output","position":{"x":0,"y":150},"component_id":"PLAYBOOK_LOGGER_COMPONENT","configuration":"{\\"level\\":\\"debug\\"}"}],"links":[{"id":"id-21","from":{"id":"id-20","port":"out"},"to":{"id":"id-17"}},{"id":"id-22","from":{"id":"id-14","port":"out"},"to":{"id":"id-20"}}]}',
     entity_type: 'Playbook',
     base_type: 'ENTITY',
@@ -166,7 +126,7 @@ describe('listenPirEventsUtils', () => {
     });
   });
 
-  describe('When isEventMatchesPir is called with PIR list that match filters', () => {
+  describe('When isEventMatchesPir is called with an event in Pir relationship, and a PIR list that match filters', () => {
     it('should return true', async () => {
       vi.spyOn(stixFiltering, 'isStixMatchFilterGroup').mockResolvedValue(true);
 
@@ -180,7 +140,7 @@ describe('listenPirEventsUtils', () => {
     });
   });
 
-  describe('When isEventMatchesPir is called with PIR list that does not match filters', () => {
+  describe('When isEventMatchesPir is called with an event in Pir relationship, and PIR list that does not match filters', () => {
     it('should return false', async () => {
       vi.spyOn(stixFiltering, 'isStixMatchFilterGroup').mockResolvedValue(false);
 
@@ -191,6 +151,52 @@ describe('listenPirEventsUtils', () => {
       );
       expect(result).toBeFalsy();
       expect(stixFiltering.isStixMatchFilterGroup).toHaveBeenCalled();
+    });
+  });
+
+  describe('When isEventMatchesPir is called with an event update on entity, and is in a PIR that matches filters', () => {
+    it('should return true', async () => {
+      vi.spyOn(stixFiltering, 'isStixMatchFilterGroup').mockResolvedValue(false);
+      vi.spyOn(middlewareLoader, 'storeLoadById').mockResolvedValue({ pir_information: [{ pir_id: 'id-2' }] } as unknown as BasicStoreEntityPlaybook);
+
+      const result = await isEventMatchesPir(
+        randomContext,
+        randomUpdateEventOnEntity.data,
+        randomPirList,
+      );
+      expect(result).toBeTruthy();
+      expect(stixFiltering.isStixMatchFilterGroup).not.toHaveBeenCalled();
+      expect(middlewareLoader.storeLoadById).toHaveBeenCalled();
+    });
+  });
+
+  describe('When isEventMatchesPir is called with an event update on entity, and is in a PIR that does not match filters', () => {
+    it('should return false', async () => {
+      vi.spyOn(stixFiltering, 'isStixMatchFilterGroup').mockResolvedValue(false);
+      vi.spyOn(middlewareLoader, 'storeLoadById').mockResolvedValue({ pir_information: [{ pir_id: 'not good pir id' }] } as unknown as BasicStoreEntityPlaybook);
+
+      const result = await isEventMatchesPir(
+        randomContext,
+        randomUpdateEventOnEntity.data,
+        randomPirList,
+      );
+      expect(result).toBeFalsy();
+      expect(stixFiltering.isStixMatchFilterGroup).not.toHaveBeenCalled();
+      expect(middlewareLoader.storeLoadById).toHaveBeenCalled();
+    });
+  });
+
+  describe('When isEventMatchesPir is called with an event that is not a in pir relationship and not an update en entity', () => {
+    it('should return false', async () => {
+      vi.spyOn(stixFiltering, 'isStixMatchFilterGroup').mockResolvedValue(false);
+
+      const result = await isEventMatchesPir(
+        randomContext,
+        randomEventNotInPir.data,
+        randomPirList,
+      );
+      expect(result).toBeFalsy();
+      expect(stixFiltering.isStixMatchFilterGroup).not.toHaveBeenCalled();
     });
   });
 
@@ -228,7 +234,7 @@ describe('listenPirEventsUtils', () => {
         vi.spyOn(middleware, 'stixLoadById').mockResolvedValue(mockEntity);
         await listenPirEvents(
           randomContext,
-          { ...randomEventInPir, data: { ...randomEventInPir.data, data: { relationship_type: 'not-pir-relationship' }, } } as unknown as SseEvent<StreamDataEvent>,
+          randomEventNotInPir,
           randomInstance,
           randomPlaybook
         );
