f02ec93b9fbc21ffb7186139d674167906628006
Gwendoline Favre-Felix <gwendoline.favre-felix@filigran.io>
2025-11-12 10:53:23 +0100

    [frontend] Add e2e on Observations/Infrastructures (#13014)

--- opencti-platform/opencti-front/tests_e2e/model/infrastructureDetails.pageModel.ts
+++ opencti-platform/opencti-front/tests_e2e/model/infrastructureDetails.pageModel.ts
@@ -5,11 +5,27 @@ export default class InfrastructureDetailsPageModel {
   tabs = new SDOTabs(this.page);
   constructor(private page: Page) {}
 
+  getInfrastructureDetailsPage() {
+    return this.page.getByTestId('infrastructure-details-page');
+  }
+
   getPage() {
     return this.page.getByTestId('infrastructure-overview');
   }
 
   getTitle(name: string) {
     return this.page.getByRole('heading', { name });
+  }
+
+  getKnowledgeTab() {
+    return this.page.getByRole('tab', { name: 'Knowledge' }).click();
+  }
+
+  getVictimologyTab() {
+    return this.page.getByRole('menuitem', { name: 'Victimology' }).click();
+  }
+
+  getCreateRelationshipButton() {
+    return this.page.getByRole('button', { name: 'Create Relationship' });
   }
 }
