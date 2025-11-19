355f303cd497061d3d98344eb90f6308ba90c4e3
Raphael Bourgeois <raphael.bourgeois@filigran.io>
2025-11-19 16:22:07 +0100

    add built in email template for onboarding new users

---
+++ opencti-platform/opencti-graphql/src/migrations/1763562671886-add-missing-built-in-onbarding-email-template.js
@@ -0,0 +1,17 @@
+import { executionContext, SYSTEM_USER } from '../utils/access';
+import { logApp } from '../config/conf';
+import {addEmailTemplate} from "../modules/emailTemplate/emailTemplate-domain";
+import {DEFAULT_EMAIL_TEMPLATE_INPUT} from "../database/default-email-template-input";
+
+const message = '[MIGRATION] Add built in onboarding email template';
+export const up = async (next) => {
+  logApp.info(`${message} > started`);
+  const context = executionContext('migration');
+  await addEmailTemplate(context, SYSTEM_USER, DEFAULT_EMAIL_TEMPLATE_INPUT);
+  logApp.info(`${message} > done.`);
+  next();
+};
+
+export const down = async (next) => {
+  next();
+};
