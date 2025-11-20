73b4b319b5b87b52b5d2a9166c3b6d282cae7a8d
Florian Delemarre <florian.delemarre@filigran.io>
2025-11-20 12:05:07 +0100

    update eslint config

--- opencti-platform/opencti-front/.eslintrc.js
+++ opencti-platform/opencti-front/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
-    'plugin:oxlint/recommended'
+    'plugin:oxlint/recommended',
   ],
   settings: {
     react: {
