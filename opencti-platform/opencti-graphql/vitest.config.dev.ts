6206f2f148bb7517d19576fd868f8555e26fa107
A. Jard <angelique.jard@filigran.io>
2025-11-21 09:47:07 +0100

    [backend] Fix taxii server error management (#12668)(#8946)

--- opencti-platform/opencti-graphql/vitest.config.dev.ts
+++ opencti-platform/opencti-graphql/vitest.config.dev.ts
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { buildTestConfig } from './vitest.config.test';
 
-export default buildTestConfig(['tests/(01|02|03|05|06)-*/**/*-test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']);
-// export default buildTestConfig(['tests/(02)-*/**/(loader|filterGroup|grouping|workspace)*-test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']);
+export default buildTestConfig(['tests/(01|02|03|04|05|06|07)-*/**/*-test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']);
+// to run one test use yarn test:dev:init ; yarn test:dev:resume <test-name>
