6085844e73ae621407ba2a9a72c2124f6de0b892
Angelique Jard <angelique.jard@filigran.io>
2025-11-25 09:22:44 +0100

    [backend] Refactor api-test folders (#13317)

--- opencti-platform/opencti-graphql/vitest.config.ci-rules-and-others.ts
+++ opencti-platform/opencti-graphql/vitest.config.ci-rules-and-others.ts
@@ -1,3 +1,3 @@
 import { buildIntegrationTestConfig } from './vitest.config.integration';
 
-export default buildIntegrationTestConfig(['tests/(02|05|06|07)-*/**/*-test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']);
+export default buildIntegrationTestConfig(['tests/(02|20|30|99)-*/**/*-test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']);
