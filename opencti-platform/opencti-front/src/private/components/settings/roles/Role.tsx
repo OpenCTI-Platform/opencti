29d9d52f3998382bd3eee45e7470c7ba3cf7f81c
Florian Delemarre <florian.delemarre@filigran.io>
2025-11-26 18:14:39 +0100

    display drafts capabilities in role

--- opencti-platform/opencti-front/src/private/components/settings/roles/Role.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/roles/Role.tsx
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import { ListItemButton } from '@mui/material';
+import EEChip from '@components/common/entreprise_edition/EEChip';
 import { useFormatter } from '../../../../components/i18n';
 import { Role_role$data, Role_role$key } from './__generated__/Role_role.graphql';
 import useQueryLoading from '../../../../utils/hooks/useQueryLoading';
@@ -138,6 +139,24 @@ const Role = ({
                 {queryRef && (
                   <React.Suspense>
                     <CapabilitiesList queryRef={queryRef} role={role} />
+                  </React.Suspense>
+                )}
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={6}></Grid>
+        <Grid item xs={6}>
+          <Typography variant="h4" gutterBottom={true}>
+            {t_i18n('Capabilities in Draft')}
+            <EEChip feature={t_i18n('Capabilities in Draft')} />
+          </Typography>
+          <Paper classes={{ root: classes.paper }} variant="outlined">
+            <Grid container={true} spacing={3}>
+              <Grid item xs={12} style={{ paddingTop: 10 }}>
+                {queryRef && (
+                  <React.Suspense>
+                    <CapabilitiesList queryRef={queryRef} role={role} isCapabilitiesInDraft />
                   </React.Suspense>
                 )}
               </Grid>
