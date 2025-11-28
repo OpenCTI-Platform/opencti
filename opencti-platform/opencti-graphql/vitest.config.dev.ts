e0f098216a08063d16fd43853c18c182a83a7151
Angelique Jard <angelique.jard@filigran.io>
2025-11-28 15:01:08 +0100

    I forgot one refactor

--- opencti-platform/opencti-graphql/vitest.config.dev.ts
+++ opencti-platform/opencti-graphql/vitest.config.dev.ts
@@ -1,5 +1,5 @@
  
-import { buildTestConfig } from './vitest.config.integration';
+import { buildIntegrationTestConfig } from './vitest.config.integration';
 
-export default buildTestConfig(['tests/(01|02|03|04|05|06|07)-*/**/*-test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']);
+export default buildIntegrationTestConfig(['tests/(01|02|03|04|05|06|07)-*/**/*-test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']);
 // to run one test use yarn test:dev:init ; yarn test:dev:resume <test-name>
