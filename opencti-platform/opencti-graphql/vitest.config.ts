913ddb71663a47361f2e36fe95e0e3be9c903255
Angelique Jard <angelique.jard@filigran.io>
2025-11-28 11:44:31 +0100

    Resplit tests

--- opencti-platform/opencti-graphql/vitest.config.ts
+++ opencti-platform/opencti-graphql/vitest.config.ts
@@ -1,9 +1,9 @@
-/* eslint-disable import/no-extraneous-dependencies */
+ 
 import { defineConfig } from 'vitest/config';
 import graphql from '@rollup/plugin-graphql';
 import type { PluginOption } from 'vite';
 
-const buildTestConfig = (include: string[]) => defineConfig({
+export const buildTestConfig = (include: string[]) => defineConfig({
   plugins: [graphql() as PluginOption],
   test: {
     include,
