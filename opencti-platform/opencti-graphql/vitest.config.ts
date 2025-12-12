80d004fa16a4b5fd0d5440fa219d89d940253e3d
Angelique Jard <angelique.jard@filigran.io>
2025-12-12 15:11:11 +0100

    [ci] Revert vitest to v3 and back to poolOption

--- opencti-platform/opencti-graphql/vitest.config.ts
+++ opencti-platform/opencti-graphql/vitest.config.ts
@@ -14,8 +14,7 @@ export const buildTestConfig = (include: string[]) => defineConfig({
       include: ['src/**'],
       exclude: ['src/generated/**', 'src/migrations/**', 'src/stixpattern/**', 'src/python/**', '*.md'],
       reporter: ['text', 'json', 'html'],
-    },
-    maxWorkers: 5,
+    }
   },
 });
 
