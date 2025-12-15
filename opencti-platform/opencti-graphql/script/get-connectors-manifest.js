47049248cc9032784076bf146cbb644a5286d560
Xavier Fournet <461943+xfournet@users.noreply.github.com>
2025-12-15 19:56:08 +0100

    [backend] Allow to customize URL and headers for manifest fetch during build (#13069)

--- opencti-platform/opencti-graphql/script/get-connectors-manifest.js
+++ opencti-platform/opencti-graphql/script/get-connectors-manifest.js
@@ -3,6 +3,10 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const getSchemaURL = () => {
+  if (process.env.OPENCTI_MANIFEST_URL) {
+    return process.env.OPENCTI_MANIFEST_URL;
+  }
+
   if (process.env.TAG_VERSION) {
     return `https://raw.githubusercontent.com/OpenCTI-Platform/connectors/refs/tags/${process.env.TAG_VERSION}/manifest.json`;
   }
@@ -10,6 +14,11 @@ const getSchemaURL = () => {
   return 'https://raw.githubusercontent.com/OpenCTI-Platform/connectors/refs/heads/master/manifest.json';
 };
 // use tags instead of master if env from circle TAG is set
+
+const getFetchHeaders = () => {
+  const headersEnv = process.env.OPENCTI_MANIFEST_HEADERS;
+  return headersEnv ? JSON.parse(headersEnv) : {};
+};
 
 const OUTPUT_DIR = '../src/__generated__';
 const OUTPUT_FILE = 'opencti-manifest.json';
@@ -23,7 +32,9 @@ async function getConnectorManifest() {
   try {
     // fetch file
     console.info(`➡️ Fetching manifest from: ${schemaUrl}`);
-    const res = await fetch(schemaUrl);
+    const res = await fetch(schemaUrl, {
+      headers: getFetchHeaders(),
+    });
     if (!res.ok) throw new Error(`HTTP ${res.status}`);
     const data = await res.text();
 
