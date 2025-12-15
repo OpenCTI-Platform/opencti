eb92a71ba78bcf5f67219cb4042df2bf61fc842c
Cathia Archidoit <cathia.archidoit@filigran.io>
2025-12-15 11:03:33 +0100

    [backend] loadParticipants

--- opencti-platform/opencti-graphql/src/database/members.ts
+++ opencti-platform/opencti-graphql/src/database/members.ts
@@ -1,6 +1,8 @@
 import { filterMembersUsersWithUsersOrgs } from '../utils/access';
 import type { AuthContext, AuthUser } from '../types/user';
 import type { BasicStoreEntity } from '../types/store';
+import { loadThroughDenormalized } from '../resolvers/stix';
+import { INPUT_PARTICIPANT } from '../schema/general';
 
 export const loadCreators = async (
   context: AuthContext,
@@ -12,4 +14,16 @@ export const loadCreators = async (
     return [];
   }
   return filterMembersUsersWithUsersOrgs(context, user, creators);
+};
+
+export const loadParticipants = async (
+  context: AuthContext,
+  user: AuthUser,
+  object: BasicStoreEntity,
+) => {
+  const participants = await loadThroughDenormalized(context, user, object, INPUT_PARTICIPANT, { sortBy: 'user_email' });
+  if (!participants) {
+    return [];
+  }
+  return filterMembersUsersWithUsersOrgs(context, user, participants);
 };
