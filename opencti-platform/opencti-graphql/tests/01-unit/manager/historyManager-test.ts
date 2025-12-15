0ea057da314bfadc848a4fe4168a54cb359aea6d
marie flores <marie.flores@filigran.io>
2025-12-15 18:09:40 +0100

    [backend] add test

--- opencti-platform/opencti-graphql/tests/01-unit/manager/historyManager-test.ts
+++ opencti-platform/opencti-graphql/tests/01-unit/manager/historyManager-test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from 'vitest';
 import { historyMessage } from '../../../src/manager/historyManager';
+import type { Change } from '../../../src/types/event';
 
 describe('historyMessage tests', () => {
   it('should generate history message for Description update', () => {
@@ -26,5 +27,30 @@ describe('historyMessage tests', () => {
     }];
     const message = historyMessage('update', changes);
     expect(message).toEqual('updates description in Description - backdoor in Malware types');
+  });
+  it('should generate history message for multiple update', () => {
+    const changes = [{
+      field: 'Description',
+      previous: [],
+      new: ['description'],
+    },
+    {
+      field: 'Malware types',
+      previous: ['backdoor', 'bootkit'],
+      new: ['backdoor'],
+      added: [],
+      removed: ['bootkit'],
+    },
+    {
+      field: 'Confidence',
+      previous: [58],
+      new: [52],
+    },
+    {
+      field: 'Workflow status',
+      previous: ['status1'],
+      new: ['status2'] }];
+    const message = historyMessage('update', changes as Change[]);
+    expect(message).toEqual('updates description in Description - backdoor in Malware types - 52 in Confidence - status2 in Workflow status and 1 more operations');
   });
 });
