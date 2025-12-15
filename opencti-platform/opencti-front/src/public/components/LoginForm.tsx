8d3d53064ab7c12b66f60d59fc638ac08151b154
Esrevi Huynh <esrevi.huynh@filigran.io>
2025-12-15 17:50:10 +0100

    [frontend] add selected state (#13460)

--- opencti-platform/opencti-front/src/public/components/LoginForm.tsx
+++ opencti-platform/opencti-front/src/public/components/LoginForm.tsx
@@ -10,7 +10,6 @@ import Button from '@mui/material/Button';
 import { Theme } from '@mui/material/styles/createTheme';
 import { useFormatter } from '../../components/i18n';
 import useApiMutation from '../../utils/hooks/useApiMutation';
-import { Button } from '@mui/material';
 
 const loginMutation = graphql`
   mutation LoginFormMutation($input: UserLoginInput!) {
@@ -102,7 +101,7 @@ const LoginForm: FunctionComponent<LoginFormProps> = ({ onClickForgotPassword, e
               style={{ marginTop: theme.spacing(3) }}
             >
               {t_i18n('Sign in')}
-            </Button>             
+            </Button>
           </Form>
         )}
       </Formik>
