d0cac8cc6aef874f660845d3babfe0c14cf6d52c
Cathia Archidoit <cathia.archidoit@filigran.io>
2025-12-16 16:27:23 +0100

    [backend] runMigration api

--- opencti-platform/opencti-graphql/src/modules/migration/migration-domain.ts
+++ opencti-platform/opencti-graphql/src/modules/migration/migration-domain.ts
@@ -1,8 +1,17 @@
 import type { AuthContext, AuthUser } from '../../types/user';
 import { retrieveMigration } from '../../database/migration';
+import { logApp, logMigration } from '../../config/conf';
+import { MigrationSet } from 'migrate';
 
-export const runMigration = async (context: AuthContext, user: AuthUser, migrationName: string) => {
-  const migration = await retrieveMigration(migrationName);
-  console.log('migration', migration);
-  return 'DONE';
+export const runMigration = async (context: AuthContext, user: AuthUser, migrationFileName: string) => {
+  const migration = await retrieveMigration(migrationFileName);
+  const migrationSet = new MigrationSet(migration);
+  migrationSet.up((migrationError) => {
+    if (migrationError) {
+      logApp.error(`[MIGRATION] Migration ${migrationFileName} up error`, { cause: migrationError });
+      return 'ERROR';
+    }
+    logMigration.info(`[MIGRATION] Migration ${migrationFileName} successfully run`);
+    return 'DONE';
+  });
 };
