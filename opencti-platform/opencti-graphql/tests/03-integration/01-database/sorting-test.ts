e5baf06dbeb400872971b15db9d73ed6dde7a50a
Marie Torbay <marie.torbay@filigran.io>
2025-12-19 09:57:54 +0100

    Added EE mock to sorting-test

--- opencti-platform/opencti-graphql/tests/03-integration/01-database/sorting-test.ts
+++ opencti-platform/opencti-graphql/tests/03-integration/01-database/sorting-test.ts
@@ -2,8 +2,12 @@ import { describe, expect, it, vi } from 'vitest';
 import { buildElasticSortingForAttributeCriteria } from '../../../src/utils/sorting';
 import { SYSTEM_USER } from '../../../src/utils/access';
 import { testContext } from '../../utils/testQuery';
+import * as entrepriseEdition from '../../../src/enterprise-edition/ee';
 
 describe('Sorting utilities', () => {
+  // Activate EE for this test
+  vi.spyOn(entrepriseEdition, 'checkEnterpriseEdition').mockResolvedValue();
+  vi.spyOn(entrepriseEdition, 'isEnterpriseEdition').mockResolvedValue(true);
   let sorting;
   it('buildElasticSortingForAttributeCriteria throws on error if pir sorting and user has not the rights', async () => {
     sorting = async () => buildElasticSortingForAttributeCriteria(testContext, SYSTEM_USER, 'pir_score', 'asc', 'fakePirId');
