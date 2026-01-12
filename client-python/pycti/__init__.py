97ca75ecb6b12751c5269d8bc868d65e3a2d705f
Filigran Automation <automation@filigran.io>
2026-01-12 13:33:42 +0000

    [backend/worker] Release 6.9.7

--- client-python/pycti/__init__.py
+++ client-python/pycti/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "6.9.6"
+__version__ = "6.9.7"
 
 from .api.opencti_api_client import OpenCTIApiClient
 from .api.opencti_api_connector import OpenCTIApiConnector
