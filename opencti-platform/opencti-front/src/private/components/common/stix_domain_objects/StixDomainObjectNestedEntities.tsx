973008137c75000aaab7fa3d8ba317b19723b9ff
Archidoit <75783086+Archidoit@users.noreply.github.com>
2026-01-19 13:49:15 +0100

    [frontend] Nested objects list with restricted objects in entities (#14026)

---
+++ opencti-platform/opencti-front/src/private/components/common/stix_domain_objects/StixDomainObjectNestedEntities.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import StixNestedRefRelationshipCreationFromEntityContainer from '../stix_nested_ref_relationships/StixNestedRefRelationshipCreationFromEntityContainer';
+import { useFormatter } from '../../../../components/i18n';
+import Security from '../../../../utils/Security';
+import { KNOWLEDGE_KNUPDATE } from '../../../../utils/hooks/useGranted';
+import Loader, { LoaderVariant } from '../../../../components/Loader';
+import useQueryLoading from '../../../../utils/hooks/useQueryLoading';
+import { StixDomainObjectNestedEntitiesLinesQuery, StixDomainObjectNestedEntitiesLinesQuery$variables } from './__generated__/StixDomainObjectNestedEntitiesLinesQuery.graphql';
+import StixDomainObjectNestedEntitiesLines, { stixDomainObjectNestedEntitiesLinesQuery } from './StixDomainObjectNestedEntitiesLines';
+
+interface StixDomainObjectNestedEntitiesProps {
+  entityId: string;
+  entityType: string;
+}
+
+const StixDomainObjectNestedEntities = ({
+  entityId,
+  entityType,
+}: StixDomainObjectNestedEntitiesProps) => {
+  const { t_i18n } = useFormatter();
+
+  const paginationOptions = {
+    fromOrToId: entityId,
+    search: '',
+    orderBy: null,
+    orderMode: 'desc',
+  };
+  const queryPaginationOptions = { ...paginationOptions, count: 25 } as StixDomainObjectNestedEntitiesLinesQuery$variables;
+
+  const queryRef = useQueryLoading<StixDomainObjectNestedEntitiesLinesQuery>(
+    stixDomainObjectNestedEntitiesLinesQuery,
+    queryPaginationOptions,
+  );
+  return (
+    <div style={{ marginTop: 20 }}>
+      <Typography variant="h4" gutterBottom={true} style={{ float: 'left' }}>
+        {t_i18n('Nested objects')}
+      </Typography>
+      <Security
+        needs={[KNOWLEDGE_KNUPDATE]}
+        placeholder={<div style={{ height: 29 }} />}
+      >
+        <StixNestedRefRelationshipCreationFromEntityContainer
+          paginationOptions={paginationOptions}
+          entityId={entityId}
+          variant="inLine"
+          entityType={entityType}
+        />
+      </Security>
+      <div className="clearfix" />
+      <List style={{ marginTop: -10 }}>
+        {queryRef && (
+          <React.Suspense fallback={<Loader variant={LoaderVariant.container} />}>
+            <StixDomainObjectNestedEntitiesLines
+              stixDomainObjectId={entityId}
+              paginationOptions={queryPaginationOptions}
+              queryRef={queryRef}
+            />
+          </React.Suspense>
+        )}
+      </List>
+    </div>
+  );
+};
+
+export default StixDomainObjectNestedEntities;
