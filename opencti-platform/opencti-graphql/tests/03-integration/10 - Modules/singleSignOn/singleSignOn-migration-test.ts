339f8c1dd7ea54538afbfcd8084260de29613454
marie flores <marie.flores@filigran.io>
2026-01-21 10:02:43 +0100

    [backend] reactivat cert migration

--- opencti-platform/opencti-graphql/tests/03-integration/10 - Modules/singleSignOn/singleSignOn-migration-test.ts
+++ opencti-platform/opencti-graphql/tests/03-integration/10 - Modules/singleSignOn/singleSignOn-migration-test.ts
@@ -898,7 +898,7 @@ describe('Migration of SSO environment test coverage', () => {
       });
     });
   });
-  describe.todo('Dry run of CERT migrations', () => {
+  describe('Dry run of CERT migrations', () => {
     it('should CERT minimal configuration works', async () => {
       const configuration = {
         cert_minimal: {
