09205b9067685b4145df09787b691cbd77b988fc
Julien Richard <julien.richard@filigran.io>
2026-01-23 21:13:02 +0100

    Update opencti-platform/opencti-graphql/src/database/middleware.js
    Co-authored-by: Xavier Fournet <461943+xfournet@users.noreply.github.com>

--- opencti-platform/opencti-graphql/src/database/middleware.js
+++ opencti-platform/opencti-graphql/src/database/middleware.js
@@ -3430,7 +3430,7 @@ const createEntityRaw = async (context, user, rawInput, type, opts = {}) => {
   } catch (e) {
     // In case of insufficient confidence level, don't reject and continue to upsert
     // as upsert have a complex strategy about confidence that doesn't reject everything
-    if (e?.extensions?.data?.doc_code === INSUFFICIENT_CONFIDENCE_LEVEL) {
+    if (rawInput.update !== false && e?.extensions?.data?.doc_code === INSUFFICIENT_CONFIDENCE_LEVEL) {
       logApp.warn('Merging stopped because of user confidence level, applying upsert', { cause: e });
       // Try to execute the method forcing update to false, prevent auto merging.
       return await internalCreateEntityRaw(context, user, { ...rawInput, update: false }, type, opts);
