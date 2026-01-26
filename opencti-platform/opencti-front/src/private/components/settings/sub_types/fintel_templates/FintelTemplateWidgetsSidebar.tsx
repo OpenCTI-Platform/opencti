cd91b9b473d2b2bb4936c5f70b74d684efa51945
Landry Trebon <landry.trebon@filigran.io>
2026-01-26 15:51:07 +0100

    fix zindex

--- opencti-platform/opencti-front/src/private/components/settings/sub_types/fintel_templates/FintelTemplateWidgetsSidebar.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/sub_types/fintel_templates/FintelTemplateWidgetsSidebar.tsx
@@ -218,7 +218,7 @@ const FintelTemplateWidgetsSidebar: FunctionComponent<FintelTemplateWidetsSideba
       width: FINTEL_TEMPLATE_SIDEBAR_WIDTH,
       padding: `${theme.spacing(2)} 0`,
       paddingTop: `calc(${theme.spacing(2)} +  ${settingsMessagesBannerHeight}px)`,
-      zIndex: 7,
+      zIndex: theme.zIndex.appBar - 1,
     },
   };
 
