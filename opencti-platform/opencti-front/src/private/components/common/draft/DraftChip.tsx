e1fdcd2b6a5a1ed6b95b2212ad1dbf1253a42080
Esrevi Huynh <esrevi.huynh@filigran.io>
2026-01-27 17:31:58 +0100

    fix lint

--- opencti-platform/opencti-front/src/private/components/common/draft/DraftChip.tsx
+++ opencti-platform/opencti-front/src/private/components/common/draft/DraftChip.tsx
@@ -9,32 +9,10 @@ export const getDraftModeColor = (theme: Theme) => {
   return (theme.palette.warning as SimplePaletteColorOptions)?.main ?? theme.palette.primary.main;
 };
 
-export const DraftChip = ({ style }: { style?: React.CSSProperties }) => {
+export const DraftChip = () => {
   const { t_i18n } = useFormatter();
   const theme = useTheme<Theme>();
   const draftColor = getDraftModeColor(theme);
-  // return (
-  //   <div
-  //     style={{
-  //       fontSize: 'xx-small',
-  //       textTransform: 'uppercase',
-  //       fontWeight: 500,
-  //       height: 20,
-  //       display: 'inline-flex',
-  //       justifyContent: 'center',
-  //       alignItems: 'center',
-  //       marginLeft: theme.spacing(0.5),
-  //       padding: `0 ${theme.spacing(0.5)}`,
-  //       borderRadius: theme.borderRadius,
-  //       border: `1px solid ${draftColor}`,
-  //       color: draftColor,
-  //       backgroundColor: 'transparent',
-  //       ...style,
-  //     }}
-  //   >
-  //     {t_i18n('Draft')}
-  //   </div>
-  // );
 
   return (
     <Tag
