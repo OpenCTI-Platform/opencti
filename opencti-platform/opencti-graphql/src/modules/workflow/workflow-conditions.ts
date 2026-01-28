2f93767a1b5a9bb7b04c239bddf7309bb692ec66
Octave <octave.albert@filigran.io>
2026-01-28 13:52:07 +0100

    [backend] add conditions registry and engine tests

---
+++ opencti-platform/opencti-graphql/src/modules/workflow/workflow-conditions.ts
@@ -0,0 +1,15 @@
+import { BYPASS } from '../../utils/access';
+import type { Context } from './workflow-types';
+
+export type ConditionFunction<TContext extends Context = Context> = (ctx: TContext, params?: any) => Promise<boolean> | boolean;
+
+export const ConditionRegistry: Record<string, ConditionFunction> = {
+  // generic conditions examples:
+  'is-admin': (ctx) => {
+    return ctx.user.capabilities?.some((c) => c?.name === BYPASS) || false;
+  },
+  'has-capability': (ctx, params) => {
+    const capability = params?.capability;
+    return ctx.user?.capabilities?.some((c: any) => c.name === capability) || false;
+  },
+};
