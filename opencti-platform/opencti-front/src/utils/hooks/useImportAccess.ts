a8310675383be1530e68f09f733be3a89284ebc2
Florian Delemarre <florian.delemarre@gmail.com>
2026-01-29 09:43:34 +0100

    [frontend/backend] Remove `CAPABILITIES_IN_DRAFT` feature flag (#10599)

--- opencti-platform/opencti-front/src/utils/hooks/useImportAccess.ts
+++ opencti-platform/opencti-front/src/utils/hooks/useImportAccess.ts
@@ -1,11 +1,8 @@
 import useGranted, { getCapabilitiesName, KNOWLEDGE, KNOWLEDGE_KNASKIMPORT } from './useGranted';
 import useAuth from './useAuth';
-import useHelper from './useHelper';
 
 const useImportAccess = () => {
   const { me } = useAuth();
-  const { isFeatureEnable } = useHelper();
-  const isCapabilitiesInDraftEnabled = isFeatureEnable('CAPABILITIES_IN_DRAFT');
 
   // Has global import (KNOWLEDGE_KNASKIMPORT in base capabilities)
   const hasImportBaseCapability = useGranted([KNOWLEDGE_KNASKIMPORT]);
@@ -25,9 +22,7 @@ const useImportAccess = () => {
   const hasAnyKnowledgeCapability = [...userCapabilities, ...userCapabilitiesInDraft]
     .some((cap) => cap.includes(KNOWLEDGE));
 
-  const hasOnlyAccessToImportDraftTab = isCapabilitiesInDraftEnabled
-    && !hasImportBaseCapability
-    && hasAnyKnowledgeCapability;
+  const hasOnlyAccessToImportDraftTab = !hasImportBaseCapability && hasAnyKnowledgeCapability;
 
   return {
     isForcedImportToDraft,
