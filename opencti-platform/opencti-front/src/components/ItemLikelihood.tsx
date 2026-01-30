de57a6e5b6e13bf9b5319e81ee3a32622a01deb2
Cathia Archidoit <cathia.archidoit@filigran.io>
2026-01-30 15:31:38 +0100

    [frontend] item severity and priority

---
+++ opencti-platform/opencti-front/src/components/ItemLikelihood.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Tag from '@common/tag/Tag';
+import { useTheme } from '@mui/material/styles';
+
+interface ItemLikelihoodProps {
+  likelihood?: number;
+}
+
+const ItemLikelihood = ({ likelihood }: ItemLikelihoodProps) => {
+  const theme = useTheme();
+  if (!likelihood) {
+    return (
+      <>-</>
+    );
+  }
+  if (likelihood <= 20) {
+    return (
+      <Tag
+        color={theme.palette.severity?.critical}
+        label={`${likelihood} / 100`}
+      />
+    );
+  }
+  if (likelihood <= 50) {
+    return (
+      <Tag
+        color={theme.palette.severity?.high}
+        label={`${likelihood} / 100`}
+      />
+    );
+  }
+  if (likelihood <= 75) {
+    return (
+      <Tag
+        color={theme.palette.severity?.info}
+        label={`${likelihood} / 100`}
+      />
+    );
+  }
+  if (likelihood <= 100) {
+    return (
+      <Tag
+        color={theme.palette.severity?.low}
+        label={`${likelihood} / 100`}
+      />
+    );
+  }
+  return (
+    <Tag
+      color={theme.palette.common.white}
+      label={`${likelihood} / 100`}
+    />
+  );
+};
+
+export default ItemLikelihood;
