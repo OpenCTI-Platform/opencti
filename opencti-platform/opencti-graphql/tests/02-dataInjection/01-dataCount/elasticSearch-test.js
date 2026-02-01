4150a07cc8b7acf19129787176b12b02833efad1
Samuel Hassine <samuel.hassine@filigran.io>
2026-02-01 23:06:15 +0100

    Fix

--- opencti-platform/opencti-graphql/tests/02-dataInjection/01-dataCount/elasticSearch-test.js
+++ opencti-platform/opencti-graphql/tests/02-dataInjection/01-dataCount/elasticSearch-test.js
@@ -741,7 +741,8 @@ describe('Elasticsearch pagination', () => {
     expect(entityTypeMap.get('Kill-Chain-Phase')).toBe(entitiesCounter.KillChainPhase);
     expect(entityTypeMap.get('External-Reference')).toBe(entitiesCounter.ExternalReference);
     expect(entityTypeMap.get('EmailTemplate')).toBe(entitiesCounter.EmailTemplate);
-    expect(data.edges.length).toEqual(221 + entitiesCounter.Vocabulary);
+    expect(entityTypeMap.get('RetentionRule')).toBe(entitiesCounter.RetentionRule);
+    expect(data.edges.length).toEqual(223 + entitiesCounter.Vocabulary);
     const createdDates = R.map((e) => e.node.created, data.edges);
     let previousCreatedDate = null;
     for (let index = 0; index < createdDates.length; index += 1) {
