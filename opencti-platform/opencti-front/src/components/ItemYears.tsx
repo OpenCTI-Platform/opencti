899397505326847b181a4df3679837d8d87e7e59
Cathia Archidoit <cathia.archidoit@filigran.io>
2026-02-02 15:22:56 +0100

    fix lint

--- opencti-platform/opencti-front/src/components/ItemYears.tsx
+++ opencti-platform/opencti-front/src/components/ItemYears.tsx
@@ -4,25 +4,25 @@ import Chip from '@mui/material/Chip';
 import { EMPTY_VALUE } from '../utils/String';
 
 interface ItemYearsProps {
-  years: string,
-  variant?: string,
-  disabled?: boolean,
+  years: string;
+  variant?: string;
+  disabled?: boolean;
 }
 
 const ItemYears = ({ years, variant, disabled }: ItemYearsProps) => {
   const chipStyle: CSSProperties = variant === 'inList'
     ? {
-      fontSize: 12,
-      lineHeight: '12px',
-      height: 20,
-      marginRight: 15,
-    }
+        fontSize: 12,
+        lineHeight: '12px',
+        height: 20,
+        marginRight: 15,
+      }
     : {
-      fontSize: 12,
-      lineHeight: '12px',
-      height: 25,
-      marginRight: 7,
-    };
+        fontSize: 12,
+        lineHeight: '12px',
+        height: 25,
+        marginRight: 7,
+      };
   return (
     <Chip
       sx={chipStyle}
