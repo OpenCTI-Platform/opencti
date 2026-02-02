95f5e8c26bc959ddb7410106426c8029eadde130
Landry Trebon <33682259+lndrtrbn@users.noreply.github.com>
2026-02-02 17:49:21 +0100

    [frontend] Fix connector cards (#13303)

---
+++ opencti-platform/opencti-front/src/components/common/card/CardStatistic.tsx
@@ -0,0 +1,42 @@
+import { useTheme } from '@mui/styles';
+import { Theme } from '../../Theme';
+import Card from './Card';
+import { Stack, Typography } from '@mui/material';
+
+interface CardStatisticProps {
+  label: string;
+  value: string;
+}
+
+const CardStatistic = ({ label, value }: CardStatisticProps) => {
+  const theme = useTheme<Theme>();
+
+  const valueStyle = {
+    fontSize: 40,
+    lineHeight: 1,
+    fontWeight: 600,
+  };
+
+  return (
+    <Card sx={{ paddingY: 2 }}>
+      <Stack height="100%" justifyContent="space-between">
+        <Typography
+          color={theme.palette.text.light}
+          variant="body2"
+          gutterBottom
+        >
+          {label}
+        </Typography>
+
+        <div
+          data-testid={`card-stat-${label}`}
+          style={valueStyle}
+        >
+          {value}
+        </div>
+      </Stack>
+    </Card>
+  );
+};
+
+export default CardStatistic;
