194dc04487719e39492b1e5bea7f0146ae1afa9b
Landry Trebon <landry.trebon@filigran.io>
2026-02-02 12:01:05 +0100

    fix diamond model

--- opencti-platform/opencti-front/src/private/components/common/stix_domain_objects/diamond/types/nodes/NodeContainer.tsx
+++ opencti-platform/opencti-front/src/private/components/common/stix_domain_objects/diamond/types/nodes/NodeContainer.tsx
@@ -1,10 +1,9 @@
-import React, { FunctionComponent, ReactNode } from 'react';
-import { useTheme } from '@mui/styles';
+import { FunctionComponent, ReactNode } from 'react';
 import Button from '@common/button/Button';
 import { Link } from 'react-router-dom';
 import { Handle, Position } from 'reactflow';
 import { useFormatter } from 'src/components/i18n';
-import type { Theme } from '../../../../../../../components/Theme';
+import Card from '../../../../../../../components/common/card/Card';
 
 interface NodeContainerProps {
   children: ReactNode;
@@ -20,26 +19,19 @@ const NodeContainer: FunctionComponent<NodeContainerProps> = ({
   position,
 }) => {
   const { t_i18n } = useFormatter();
-  const theme = useTheme<Theme>();
 
   return (
-    <div style={{
-      position: 'relative',
-      border: theme.palette.mode === 'dark'
-        ? '1px solid rgba(255, 255, 255, 0.12)'
-        : '1px solid rgba(0, 0, 0, 0.12)',
-      borderRadius: '4px',
-      backgroundColor: theme.palette.background.paper,
-      width: '400px',
-      height: `${height}px`,
-      paddingBottom: '25px',
-    }}
+    <Card
+      sx={{
+        position: 'relative',
+        width: '400px',
+        height: `${height}px`,
+      }}
     >
       <div style={{
         width: '100%',
-        height: '100%',
+        height: 'calc(100% - 24px)',
         overflowY: 'auto',
-        padding: '20px',
       }}
       >
         {children}
@@ -48,26 +40,7 @@ const NodeContainer: FunctionComponent<NodeContainerProps> = ({
         component={Link}
         to={link}
         size="small"
-        sx={{
-          position: 'absolute',
-          left: 0,
-          bottom: 0,
-          width: '100%',
-          height: '25px',
-          color: theme.palette.primary.main,
-          borderTopLeftRadius: 0,
-          borderTopRightRadius: 0,
-          backgroundColor:
-            theme.palette.mode === 'dark'
-              ? 'rgba(255, 255, 255, .1)'
-              : 'rgba(0, 0, 0, .1)',
-          '&:hover': {
-            backgroundColor:
-              theme.palette.mode === 'dark'
-                ? 'rgba(255, 255, 255, .2)'
-                : 'rgba(0, 0, 0, .2)',
-          },
-        }}
+        variant="secondary"
         className="nodrag nopan"
       >
         {t_i18n('View all')}
@@ -78,7 +51,7 @@ const NodeContainer: FunctionComponent<NodeContainerProps> = ({
         position={position}
         isConnectable={false}
       />
-    </div>
+    </Card>
   );
 };
 
