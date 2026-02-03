39566847d57c8143a861877e812cc2ffc3a0ff29
Cathia Archidoit <cathia.archidoit@filigran.io>
2026-02-03 11:19:46 +0100

    [backend] add test for loadCreator and loadCreators

--- opencti-platform/opencti-graphql/tests/03-integration/01-database/members-test.ts
+++ opencti-platform/opencti-graphql/tests/03-integration/01-database/members-test.ts
@@ -1,16 +1,44 @@
-import { describe, expect, it } from 'vitest';
-import { loadCreator } from '../../../src/database/members';
-import { ADMIN_USER, testContext } from '../../utils/testQuery';
-import { SYSTEM_USER } from '../../../src/utils/access';
+import { describe, expect, it, beforeAll } from 'vitest';
+import { loadCreator, loadCreators } from '../../../src/database/members';
+import { ADMIN_USER, USER_EDITOR } from '../../utils/testQuery';
+import { executionContext, PIR_MANAGER_USER, SYSTEM_USER } from '../../../src/utils/access';
+import type { BasicStoreEntity } from '../../../src/types/store';
+import { computeLoaders } from '../../../src/http/httpAuthenticatedContext';
 
 describe('Members loader function testing', () => {
-  it('Should load creator return SYSTEM_USER if creator id is undefined or creator user doesnt exist', async () => {
-    let creator = await loadCreator(testContext, ADMIN_USER, undefined);
+  const execContext = executionContext('test', ADMIN_USER);
+  beforeAll(() => {
+    execContext.batch = computeLoaders(execContext, ADMIN_USER);
+  });
+
+  it('Should load creator return SYSTEM_USER if creator id is undefined or corresponds to no user', async () => {
+    let creator = await loadCreator(execContext, ADMIN_USER, undefined);
     expect(creator.name).toEqual(SYSTEM_USER.name);
     expect(creator.id).toEqual(SYSTEM_USER.id);
 
-    creator = await loadCreator(testContext, ADMIN_USER, 'fake-userId');
+    creator = await loadCreator(execContext, ADMIN_USER, 'fake-userId');
     expect(creator.name).toEqual(SYSTEM_USER.name);
     expect(creator.id).toEqual(SYSTEM_USER.id);
+  });
+
+  it('Should load creator', async () => {
+    let creator = await loadCreator(execContext, ADMIN_USER, PIR_MANAGER_USER.id);
+    expect(creator.name).toEqual(PIR_MANAGER_USER.name);
+    expect(creator.id).toEqual(PIR_MANAGER_USER.id);
+
+    creator = await loadCreator(execContext, ADMIN_USER, USER_EDITOR.id);
+    expect(creator.name).toEqual(USER_EDITOR.email);
+  });
+
+  it('Should load creators return SYSTEM_USER if creator id corresponds to no user', async () => {
+    const creators = await loadCreators(execContext, ADMIN_USER, { creator_id: 'fakeUserId' } as BasicStoreEntity);
+    expect(creators.length).toEqual(1);
+    expect(creators[0].name).toEqual(SYSTEM_USER.name);
+  });
+
+  it('Should load creators', async () => {
+    const creators = await loadCreators(execContext, ADMIN_USER, { creator_id: USER_EDITOR.id } as BasicStoreEntity);
+    expect(creators.length).toEqual(1);
+    expect(creators[0].name).toEqual(USER_EDITOR.email);
   });
 });
