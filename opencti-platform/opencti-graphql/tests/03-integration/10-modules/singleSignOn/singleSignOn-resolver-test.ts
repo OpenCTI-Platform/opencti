6e5fc4bc18e22df79834f9440384e4d5089e3a22
marie flores <marie.flores@filigran.io>
2026-02-11 16:32:50 +0100

    [backend] remove encrypted type from returned values in tests

--- opencti-platform/opencti-graphql/tests/03-integration/10-modules/singleSignOn/singleSignOn-resolver-test.ts
+++ opencti-platform/opencti-graphql/tests/03-integration/10-modules/singleSignOn/singleSignOn-resolver-test.ts
@@ -15,7 +15,7 @@ import { queryAsAdminWithSuccess, queryAsUserIsExpectedError, queryAsUserIsExpec
 import { ADMIN_USER, testContext, USER_PARTICIPATE, USER_SECURITY } from '../../../utils/testQuery';
 import { deleteElementById } from '../../../../src/database/middleware';
 import { ENTITY_TYPE_SINGLE_SIGN_ON } from '../../../../src/modules/singleSignOn/singleSignOn-types';
-import { ENCRYPTED_TYPE, TO_ENCRYPT_TYPE } from '../../../../src/modules/singleSignOn/singleSignOn-domain';
+import { TO_ENCRYPT_TYPE } from '../../../../src/modules/singleSignOn/singleSignOn-domain';
 
 export const SINGLE_SIGN_ON_LIST_QUERY = gql`
     query singleSignOns($first: Int $filters: FilterGroup) {
@@ -182,16 +182,6 @@ describe('Single Sign On CRUD coverage', () => {
       expect(callbackUrlData.value).toBe('http://myopencti/auth/samlTestDomain/callback');
       const issuerData = configurationData.find((config) => config.key === 'issuer') as ConfigurationTypeInput;
       expect(issuerData.value).toBe('issuer');
-
-      // should be encrypted because in AUTH_SECRET_LIST list
-      const privateKeyData = configurationData.find((config) => config.key === 'privateKey') as ConfigurationTypeInput;
-      expect(privateKeyData.value).not.toBe('myPK');
-      expect(privateKeyData.type).toBe(ENCRYPTED_TYPE);
-
-      // this one is encrypted because enter as 'secret' by user
-      const customSecretData = configurationData.find((config) => config.key === 'mySecret') as ConfigurationTypeInput;
-      expect(customSecretData.value).not.toBe('Ilove;Mint');
-      expect(customSecretData.type).toBe(ENCRYPTED_TYPE);
     });
 
     it('should create another single sign on entity', async () => {
