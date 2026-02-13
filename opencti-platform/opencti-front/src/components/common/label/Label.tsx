e76dc798dd0d8db4405673ec09d1afcb1446b069
Francois GRUNERT <francois.grunert@filigran.io>
2026-02-13 12:17:59 +0100

    [frontend] spacing under confidence level title (#14443)

--- opencti-platform/opencti-front/src/components/common/label/Label.tsx
+++ opencti-platform/opencti-front/src/components/common/label/Label.tsx
@@ -6,12 +6,14 @@ import { PropsWithSx } from '../../../utils/props';
 
 interface LabelProps extends PropsWithChildren, PropsWithSx {
   action?: ReactNode;
+  variant?: 'default' | 'primary';
 }
 
 const Label = ({
   children,
   action,
   sx,
+  variant = 'default',
 }: LabelProps) => {
   const theme = useTheme<Theme>();
 
@@ -21,14 +23,17 @@ const Label = ({
     ...sx,
   };
 
+  const titleStyle = variant === 'default'
+    ? { color: theme.palette.text.light, textTransform: 'capitalize' }
+    : { color: theme.palette.text.primary, textTransform: 'none' };
+
   const titleSx: SxProps = {
     marginBottom: 0,
-    textTransform: 'capitalize',
-    color: theme.palette.text.light,
     lineHeight: '21px',
     fontSize: '12px',
     fontWeight: 400,
     fontFamily: '"IBM Plex Sans", sans-serif',
+    ...titleStyle,
   };
 
   return (
