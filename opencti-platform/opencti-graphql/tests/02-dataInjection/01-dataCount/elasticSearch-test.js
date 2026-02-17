eed65d5b3c103c7a6f892d863a090f2ed60bdfbf
Xavier Fournet <461943+xfournet@users.noreply.github.com>
2026-02-17 20:35:46 +0100

    [frontend] fix integration tests

--- opencti-platform/opencti-graphql/tests/02-dataInjection/01-dataCount/elasticSearch-test.js
+++ opencti-platform/opencti-graphql/tests/02-dataInjection/01-dataCount/elasticSearch-test.js
@@ -40,7 +40,6 @@ import { RELATION_USES } from '../../../src/schema/stixCoreRelationship';
 import { buildAggregationRelationFilter } from '../../../src/database/middleware-loader';
 import { mapCountPerEntityType, mapEdgesCountPerEntityType } from '../../utils/domainQueryHelper';
 import { entitiesCounter, entitiesCounterTotal, relationsCounter } from './entityCountHelper';
-import { ENTITY_TYPE_AUTHENTICATION_PROVIDER } from '../../../src/modules/authenticationProvider/authenticationProvider-types';
 
 const elWhiteUser = async () => {
   const opts = { types: ['Marking-Definition'], connectionFormat: false };
