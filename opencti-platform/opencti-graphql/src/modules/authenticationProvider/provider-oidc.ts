bae353329d358bef028be6c5e190f062d989e10e
Xavier Fournet <461943+xfournet@users.noreply.github.com>
2026-02-17 13:59:15 +0100

    [backend] add relevant information in OIDC log in order to help mapping

--- opencti-platform/opencti-graphql/src/modules/authenticationProvider/provider-oidc.ts
+++ opencti-platform/opencti-graphql/src/modules/authenticationProvider/provider-oidc.ts
@@ -51,7 +51,16 @@ export const createOpenIdStrategy = async (logger: AuthenticationProviderLogger,
 
   const verify: VerifyFunction = async (tokens, verified: AuthenticateCallback) => {
     try {
-      logger.info('Successfully logged on IdP');
+      logger.info(
+        'Successfully logged on IdP',
+        {
+          tokens: {
+            access_token: tokens.access_token ? jwtDecode(tokens.access_token) : undefined,
+            id_token: tokens.id_token ? jwtDecode(tokens.id_token) : undefined,
+            refresh_token: tokens.refresh_token ? jwtDecode(tokens.refresh_token) : undefined,
+          },
+          scope: tokens.scope,
+        });
 
       const user_info = memoize(async () => {
         const sub = tokens.claims()?.sub;
