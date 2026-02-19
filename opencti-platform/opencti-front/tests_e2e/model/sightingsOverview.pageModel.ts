3a47dfc294f8e4dd69c5fc0c9d98eb8a81a7fda9
Sébastien Wauquier <sebastien.wauquier@filigran.io>
2026-02-19 16:23:29 +0100

    [chore/front] Remove unused pageModels in e2e tests

--- opencti-platform/opencti-front/tests_e2e/model/sightingsOverview.pageModel.ts
+++
@@ -1,9 +0,0 @@
-import { Page } from '@playwright/test';
-
-export default class SightingsOverviewPage {
-  constructor(private page: Page) {}
-
-  getPage() {
-    return this.page.getByTestId('sightings-overview');
-  }
-}
