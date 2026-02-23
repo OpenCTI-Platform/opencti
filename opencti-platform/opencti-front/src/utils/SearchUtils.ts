98cf6e1b34c18e7700c4b8824207d1cd28591a43
Sébastien Wauquier <sebastien.wauquier@filigran.io>
2026-02-23 12:54:41 +0100

    [chore/front] Fix unlisted deps

--- opencti-platform/opencti-front/src/utils/SearchUtils.ts
+++ opencti-platform/opencti-front/src/utils/SearchUtils.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { NavigateFunction } from 'react-router/dist/lib/hooks';
+import type { NavigateFunction } from 'react-router';
 import { emptyFilterGroup } from './filters/filtersUtils';
 
 export const handleSearchByKeyword = (searchKeyword: string, searchScope: string, navigate: NavigateFunction) => {
