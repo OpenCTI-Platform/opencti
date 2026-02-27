b15296d2a13bd84825f1785738941ef3f047f77f
Marie Flores <73853856+marieflorescontact@users.noreply.github.com>
2026-02-27 09:38:51 +0100

    [frontend] Fix promoted checkbox not showing in container observables screen (#14598)

--- opencti-platform/opencti-front/src/private/components/common/containers/ContainerStixCyberObservables.tsx
+++ opencti-platform/opencti-front/src/private/components/common/containers/ContainerStixCyberObservables.tsx
@@ -337,6 +337,7 @@ const ContainerStixCyberObservablesComponent: FunctionComponent<
             exportContext={{ entity_id: container.id, entity_type: 'Stix-Cyber-Observable' }}
             availableEntityTypes={['Stix-Cyber-Observable']}
             searchContextFinal={{ entityTypes: ['Stix-Cyber-Observable'] }}
+            container={container}
             createButton={(
               <Security needs={[KNOWLEDGE_KNUPDATE]}>
                 <ContainerAddStixCoreObjectsInLine
