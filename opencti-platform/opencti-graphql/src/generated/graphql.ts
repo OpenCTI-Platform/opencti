58cb90e07448a104c158cfcb0f2bf299fb0a92c4
Florian Delemarre <florian.delemarre@gmail.com>
2026-02-27 09:23:52 +0100

    [backend] fix schema

--- opencti-platform/opencti-graphql/src/generated/graphql.ts
+++ opencti-platform/opencti-graphql/src/generated/graphql.ts
@@ -36342,7 +36342,7 @@ export type WorkTracking = {
 export type WorkflowActionConfig = {
   __typename?: 'WorkflowActionConfig';
   mode: WorkflowActionMode;
-  params?: Maybe<Scalars['String']['output']>;
+  params?: Maybe<Scalars['JSON']['output']>;
   type: Scalars['String']['output'];
 };
 
@@ -50565,7 +50565,7 @@ export type WorkTrackingResolvers<ContextType = any, ParentType extends Resolver
 
 export type WorkflowActionConfigResolvers<ContextType = any, ParentType extends ResolversParentTypes['WorkflowActionConfig'] = ResolversParentTypes['WorkflowActionConfig']> = ResolversObject<{
   mode?: Resolver<ResolversTypes['WorkflowActionMode'], ParentType, ContextType>;
-  params?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  params?: Resolver<Maybe<ResolversTypes['JSON']>, ParentType, ContextType>;
   type?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
 }>;
 
