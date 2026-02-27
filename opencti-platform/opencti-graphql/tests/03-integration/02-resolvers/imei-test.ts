3f2b2c2d66f14ff989afb257ac9cf71270bfa0eb
Landry Trebon <landry.trebon@filigran.io>
2026-02-27 14:56:50 +0100

    [backend] add tests on relationship

--- opencti-platform/opencti-graphql/tests/03-integration/02-resolvers/imei-test.ts
+++ opencti-platform/opencti-graphql/tests/03-integration/02-resolvers/imei-test.ts
@@ -1,5 +1,5 @@
 import gql from 'graphql-tag';
-import { describe, expect, it } from 'vitest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 import { queryAsAdmin } from '../../utils/testQuery';
 import { queryAsAdminWithSuccess } from '../../utils/testQueryHelper';
 
@@ -11,9 +11,29 @@ const READ_QUERY = gql`
   }
 `;
 
+const CREATE_REL_QUERY = gql`
+  mutation StixDomainRelationAdd($input: StixCoreRelationshipAddInput!) {
+    stixCoreRelationshipAdd(input: $input) {
+      id
+      fromType
+      toType
+    }
+  }
+`;
+
 const CREATE_MUTATION = gql`
-  mutation StixCyberObservableAdd($type: String!, $input: IMEIAddInput) {
-    stixCyberObservableAdd(type: $type, IMEI: $input) {
+  mutation StixCyberObservableAdd(
+    $type: String!,
+    $imei: IMEIAddInput,
+    $iccid: ICCIDAddInput,
+    $macAddress: MacAddrAddInput,
+  ) {
+    stixCyberObservableAdd(
+      type: $type,
+      IMEI: $imei,
+      ICCID: $iccid,
+      MacAddr: $macAddress,
+    ) {
       id
       observable_value
     }
@@ -43,49 +63,116 @@ describe('SCO IMEI resolver standard behavior', () => {
   let internalId: string;
 
   it('should not create invalid IMEI', async () => {
-    const response = await queryAsAdmin({
+    const { errors } = await queryAsAdmin({
       query: CREATE_MUTATION,
       variables: {
         type: 'IMEI',
-        input: { value: 'ABC123' },
+        imei: { value: 'ABC123' },
       },
     });
-    expect(response?.errors?.[0].message).toEqual('Observable is not correctly formatted');
+    expect(errors?.[0].message).toEqual('Observable is not correctly formatted');
   });
 
   it('should create IMEI', async () => {
-    const response = await queryAsAdmin({
+    const { data } = await queryAsAdmin({
       query: CREATE_MUTATION,
       variables: {
         type: 'IMEI',
-        input: { value: '112222223333334' },
+        imei: { value: '112222223333334' },
       },
     });
-    expect(response.data?.stixCyberObservableAdd).not.toBeNull();
-    expect(response.data?.stixCyberObservableAdd.observable_value).toEqual('112222223333334');
-    internalId = response.data?.stixCyberObservableAdd.id ?? '';
+    expect(data?.stixCyberObservableAdd).not.toBeNull();
+    expect(data?.stixCyberObservableAdd.observable_value).toEqual('112222223333334');
+    internalId = data?.stixCyberObservableAdd.id ?? '';
   });
 
   it('should not update invalid IMEI', async () => {
-    const response = await queryAsAdmin({
+    const { errors } = await queryAsAdmin({
       query: UPDATE_MUTATION,
       variables: {
         id: internalId,
         input: { key: 'value', value: 'ABC123' },
       },
     });
-    expect(response.errors?.[0].message).toEqual('Observable of is not correctly formatted');
+    expect(errors?.[0].message).toEqual('Observable of is not correctly formatted');
   });
 
   it('should update IMEI', async () => {
-    const response = await queryAsAdminWithSuccess({
+    const { data } = await queryAsAdminWithSuccess({
       query: UPDATE_MUTATION,
       variables: {
         id: internalId,
         input: { key: 'value', value: '112222223333335' },
       },
     });
-    expect(response.data?.stixCyberObservableEdit.fieldPatch.observable_value).toEqual('112222223333335');
+    expect(data?.stixCyberObservableEdit.fieldPatch.observable_value).toEqual('112222223333335');
+  });
+
+  describe('Relationships section', () => {
+    let macAddressId: string;
+    let iccidInternalId: string;
+
+    beforeAll(async () => {
+      const { data: dataIccid } = await queryAsAdmin({
+        query: CREATE_MUTATION,
+        variables: {
+          type: 'ICCID',
+          iccid: { value: '123456789012345999' },
+        },
+      });
+      const { data: dataMacAddress } = await queryAsAdmin({
+        query: CREATE_MUTATION,
+        variables: {
+          type: 'Mac-Addr',
+          macAddress: { value: '00:1b:63:84:45:e6' },
+        },
+      });
+      iccidInternalId = dataIccid?.stixCyberObservableAdd.id ?? '';
+      macAddressId = dataMacAddress?.stixCyberObservableAdd.id ?? '';
+    });
+
+    afterAll(async () => {
+      await queryAsAdmin({
+        query: DELETE_MUTATION,
+        variables: { id: iccidInternalId },
+      });
+      await queryAsAdmin({
+        query: DELETE_MUTATION,
+        variables: { id: macAddressId },
+      });
+    });
+
+    it('should create a rel "uses" between IMEI and ICCID', async () => {
+      const { data } = await queryAsAdminWithSuccess({
+        query: CREATE_REL_QUERY,
+        variables: {
+          input: {
+            fromId: internalId,
+            toId: iccidInternalId,
+            relationship_type: 'uses',
+          },
+        },
+      });
+      expect(data?.stixCoreRelationshipAdd).not.toBeNull();
+      expect(data?.stixCoreRelationshipAdd.fromType).toEqual('IMEI');
+      expect(data?.stixCoreRelationshipAdd.toType).toEqual('ICCID');
+    });
+
+    it('should create a rel "has" between IMEI and Mac-Addr', async () => {
+      const { data } = await queryAsAdminWithSuccess({
+        query: CREATE_REL_QUERY,
+        variables: {
+          input: {
+            fromId: internalId,
+            toId: macAddressId,
+            relationship_type: 'has',
+          },
+        },
+      });
+      expect(data?.stixCoreRelationshipAdd).not.toBeNull();
+      expect(data?.stixCoreRelationshipAdd.fromType).toEqual('IMEI');
+      expect(data?.stixCoreRelationshipAdd.toType).toEqual('Mac-Addr');
+    });
   });
 
   it('should delete IMEI', async () => {
