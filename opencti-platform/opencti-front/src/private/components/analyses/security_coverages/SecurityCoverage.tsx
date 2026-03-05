d1cac1f5d885b86041839a74d94b403b7155a543
Marine LM <marine.lemezo@filigran.io>
2026-03-05 14:29:46 +0100

    [frontend] Improve Security Coverage Overview (#4529)

--- opencti-platform/opencti-front/src/private/components/analyses/security_coverages/SecurityCoverage.tsx
+++ opencti-platform/opencti-front/src/private/components/analyses/security_coverages/SecurityCoverage.tsx
@@ -1,8 +1,6 @@
 import React, { Suspense } from 'react';
 import { graphql, PreloadedQuery, useFragment, usePreloadedQuery } from 'react-relay';
-import Grid from '@mui/material/Grid';
-import makeStyles from '@mui/styles/makeStyles';
-import { Theme } from '@mui/material/styles/createTheme';
+import Grid from '@mui/material/Grid2';
 import SecurityCoverageDetails from './SecurityCoverageDetails';
 import StixDomainObjectOverview from '../../common/stix_domain_objects/StixDomainObjectOverview';
 import StixCoreObjectExternalReferences from '../external_references/StixCoreObjectExternalReferences';
@@ -13,14 +11,8 @@ import useQueryLoading from '../../../../utils/hooks/useQueryLoading';
 import { SecurityCoverage_securityCoverage$key } from './__generated__/SecurityCoverage_securityCoverage.graphql';
 import { SecurityCoverageKillChainsQuery } from './__generated__/SecurityCoverageKillChainsQuery.graphql';
 import Loader, { LoaderVariant } from '../../../../components/Loader';
-
-// Deprecated - https://mui.com/system/styles/basics/
-// Do not use it for new code.
-const useStyles = makeStyles<Theme>(() => ({
-  gridContainer: {
-    marginBottom: 20,
-  },
-}));
+import SecurityCoverageTestedEntitiesChart from './SecurityCoverageTestedEntitiesChart';
+import SecurityCoverageMainInfo from './SecurityCoverageMainInfo';
 
 const securityCoverageKillChainsQuery = graphql`
   query SecurityCoverageKillChainsQuery {
@@ -93,13 +85,8 @@ const securityCoverageFragment = graphql`
     duration
     type_affinity
     platforms_affinity
-    coverage_last_result
-    coverage_valid_from
-    coverage_valid_to
-    coverage_information {
-      coverage_name
-      coverage_score
-    }
+    ...SecurityCoverageMainInfo_securityCoverage
+    ...SecurityCoverageTestedEntitiesChart_securityCoverage
     ...SecurityCoverageDetails_securityCoverage
     ...SecurityCoverageAttackPatternsFragment
   }
@@ -114,43 +101,44 @@ const SecurityCoverageComponent = ({
   data,
   killChainsQueryRef,
 }: SecurityCoverageComponentProps) => {
-  const classes = useStyles();
   const securityCoverage = useFragment(securityCoverageFragment, data);
   const dataKillChains = usePreloadedQuery(securityCoverageKillChainsQuery, killChainsQueryRef);
   return (
-    <>
-      <Grid
-        container={true}
-        spacing={3}
-        classes={{ container: classes.gridContainer }}
-      >
-        <Grid item xs={6}>
-          <SecurityCoverageDetails securityCoverage={securityCoverage} />
-        </Grid>
-        <Grid item xs={6}>
-          <StixDomainObjectOverview stixDomainObject={securityCoverage} />
-        </Grid>
+    <Grid container={true} spacing={3}>
+      <Grid size={6}>
+        <SecurityCoverageMainInfo securityCoverage={securityCoverage} />
       </Grid>
-      <SecurityCoverageAttackPatterns
-        data={securityCoverage}
-        dataKillChains={dataKillChains}
-      />
-      <Grid
-        container={true}
-        spacing={3}
-        style={{ marginTop: 10 }}
-      >
-        <Grid item xs={6}>
-          <StixCoreObjectExternalReferences stixCoreObjectId={securityCoverage.id} />
-        </Grid>
-        <Grid item xs={6}>
-          <StixCoreObjectLatestHistory stixCoreObjectId={securityCoverage.id} />
-        </Grid>
-        <Grid item xs={12}>
-          <StixCoreObjectOrStixCoreRelationshipNotes stixCoreObjectOrStixCoreRelationshipId={securityCoverage.id} />
-        </Grid>
+      <Grid size={6}>
+        <SecurityCoverageTestedEntitiesChart securityCoverage={securityCoverage} />
       </Grid>
-    </>
+
+      <Grid size={6}>
+        <SecurityCoverageDetails securityCoverage={securityCoverage} />
+      </Grid>
+
+      <Grid size={6}>
+        <StixDomainObjectOverview stixDomainObject={securityCoverage} />
+      </Grid>
+
+      <Grid size={12}>
+        <SecurityCoverageAttackPatterns
+          data={securityCoverage}
+          dataKillChains={dataKillChains}
+        />
+      </Grid>
+
+      <Grid size={6}>
+        <StixCoreObjectExternalReferences stixCoreObjectId={securityCoverage.id} />
+      </Grid>
+
+      <Grid size={6}>
+        <StixCoreObjectLatestHistory stixCoreObjectId={securityCoverage.id} />
+      </Grid>
+
+      <Grid size={12}>
+        <StixCoreObjectOrStixCoreRelationshipNotes stixCoreObjectOrStixCoreRelationshipId={securityCoverage.id} />
+      </Grid>
+    </Grid>
   );
 };
 
