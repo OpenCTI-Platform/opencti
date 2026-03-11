31a05fbd653f3b1913a6ecad31be30ffce5925e6
Marie Flores <73853856+marieflorescontact@users.noreply.github.com>
2026-03-11 14:31:37 +0100

    [backend] STIX 2.0 converter: Arsenal (#8832)

--- opencti-platform/opencti-graphql/src/modules/channel/channel-converter.ts
+++ opencti-platform/opencti-graphql/src/modules/channel/channel-converter.ts
@@ -1,9 +1,10 @@
 import { STIX_EXT_OCTI } from '../../types/stix-2-1-extensions';
 import { buildStixDomain } from '../../database/stix-2-1-converter';
-import type { StixChannel, StoreEntityChannel } from './channel-types';
-import { cleanObject } from '../../database/stix-converter-utils';
+import { ENTITY_TYPE_CHANNEL, type Stix2Channel, type StixChannel, type StoreEntityChannel } from './channel-types';
+import { assertType, cleanObject } from '../../database/stix-converter-utils';
+import { buildStixDomain as buildStixDomain2 } from '../../database/stix-2-0-converter';
 
-const convertChannelToStix = (instance: StoreEntityChannel): StixChannel => {
+export const convertChannelToStix_2_1 = (instance: StoreEntityChannel): StixChannel => {
   const stixDomainObject = buildStixDomain(instance);
   return {
     ...stixDomainObject,
@@ -20,4 +21,14 @@ const convertChannelToStix = (instance: StoreEntityChannel): StixChannel => {
   };
 };
 
-export default convertChannelToStix;
+export const convertChannelToStix_2_0 = (instance: StoreEntityChannel): Stix2Channel => {
+  assertType(ENTITY_TYPE_CHANNEL, instance.entity_type);
+  const channel = buildStixDomain2(instance);
+  return {
+    ...channel,
+    name: instance.name,
+    description: instance.description,
+    channel_types: instance.channel_types,
+    aliases: instance.x_opencti_aliases ?? [],
+  };
+};
--- opencti-platform/opencti-graphql/tests/01-unit/database/instances-stix-2-0-converter/containers/report.ts
+++ opencti-platform/opencti-graphql/tests/01-unit/database/instances-stix-2-0-converter/containers/report.ts
@@ -5,14 +5,14 @@ export const REPORT_INSTANCE = {
   _id: '0c38a734-3150-468f-bf38-8dc1f937a1b3',
   id: '0c38a734-3150-468f-bf38-8dc1f937a1b3',
   sort: [
-    1750948419653
+    1750948419653,
   ],
   name: 'Report STIX 2.0',
   description: 'description',
   content: '<p>some content for my report about &nbsp;Disco Team Threat Actor Group</p>',
   published: '2025-06-26T14:32:10.000Z',
   report_types: [
-    'internal-report'
+    'internal-report',
   ],
   x_opencti_reliability: 'A - Completely reliable',
   confidence: 100,
@@ -21,7 +21,7 @@ export const REPORT_INSTANCE = {
   internal_id: '0c38a734-3150-468f-bf38-8dc1f937a1b3',
   standard_id: 'report--87de3e34-b9a2-551d-a42f-d25a13d4ad0f',
   creator_id: [
-    '88ec0c6a-13ce-5e39-b486-354fe4a7084f'
+    '88ec0c6a-13ce-5e39-b486-354fe4a7084f',
   ],
   x_opencti_stix_ids: [],
   created_at: '2025-06-26T14:33:39.653Z',
@@ -36,7 +36,7 @@ export const REPORT_INSTANCE = {
     'Stix-Object',
     'Stix-Core-Object',
     'Stix-Domain-Object',
-    'Container'
+    'Container',
   ],
   x_opencti_files: [
     {
@@ -45,7 +45,7 @@ export const REPORT_INSTANCE = {
       version: '2025-06-26T14:33:39.655Z',
       mime_type: 'application/json',
       file_markings: [
-        '2cc876e4-ae90-497b-a4ae-16c762f0d03f'
+        '2cc876e4-ae90-497b-a4ae-16c762f0d03f',
       ],
       objectMarking: [
         {
@@ -53,39 +53,39 @@ export const REPORT_INSTANCE = {
           _id: '2cc876e4-ae90-497b-a4ae-16c762f0d03f',
           id: '2cc876e4-ae90-497b-a4ae-16c762f0d03f',
           sort: [
-            1749546753947
+            1749546753947,
           ],
           entity_type: 'Marking-Definition',
           internal_id: '2cc876e4-ae90-497b-a4ae-16c762f0d03f',
           standard_id: 'marking-definition--4cdff7eb-acb8-543f-8573-829eb9fe8b34',
-          base_type: 'ENTITY'
-        }
-      ]
-    }
+          base_type: 'ENTITY',
+        },
+      ],
+    },
   ],
   i_attributes: [
     {
       updated_at: '2025-06-26T14:36:51.468Z',
       user_id: '88ec0c6a-13ce-5e39-b486-354fe4a7084f',
       confidence: 100,
-      name: 'content'
+      name: 'content',
     },
     {
       updated_at: '2025-06-26T14:36:58.233Z',
       user_id: '88ec0c6a-13ce-5e39-b486-354fe4a7084f',
       confidence: 100,
-      name: 'objects'
+      name: 'objects',
     },
     {
       updated_at: '2025-06-26T14:36:37.671Z',
       user_id: '88ec0c6a-13ce-5e39-b486-354fe4a7084f',
       confidence: 100,
-      name: 'content_mapping'
-    }
+      name: 'content_mapping',
+    },
   ],
   content_mapping: 'eyJkaXNjbyB0ZWFtIHRocmVhdCBhY3RvciBncm91cCI6InRocmVhdC1hY3Rvci0tZmQ2YjBlNmYtOTZlMC01NjhkLWJhMjQtOGExNDBkMDQyOGNkIn0',
   'object-assignee': [
-    '0ff0750e-4d91-425d-b44c-b69269dead0b'
+    '0ff0750e-4d91-425d-b44c-b69269dead0b',
   ],
   objectAssignee: [
     {
@@ -93,7 +93,7 @@ export const REPORT_INSTANCE = {
       _id: '0ff0750e-4d91-425d-b44c-b69269dead0b',
       id: '0ff0750e-4d91-425d-b44c-b69269dead0b',
       sort: [
-        1750152527269
+        1750152527269,
       ],
       user_email: 'marie.flores@filigran.io',
       name: 'marie',
@@ -110,21 +110,21 @@ export const REPORT_INSTANCE = {
       external: false,
       personal_notifiers: [
         'f4ee7b33-006a-4b0d-b57d-411ad288653d',
-        '44fcf1f4-8e31-4b31-8dbc-cd6993e1b822'
+        '44fcf1f4-8e31-4b31-8dbc-cd6993e1b822',
       ],
       confidence: 100,
       entity_type: 'User',
       internal_id: '0ff0750e-4d91-425d-b44c-b69269dead0b',
       standard_id: 'user--20e40687-5a83-5a19-ba58-ca14e88fdbd1',
       creator_id: [
-        '88ec0c6a-13ce-5e39-b486-354fe4a7084f'
+        '88ec0c6a-13ce-5e39-b486-354fe4a7084f',
       ],
       created_at: '2025-06-17T09:28:47.269Z',
       updated_at: '2025-06-17T09:28:47.332Z',
       base_type: 'ENTITY',
       parent_types: [
         'Basic-Object',
-        'Internal-Object'
+        'Internal-Object',
       ],
       modified: '2025-06-17T09:28:47.332Z',
       i_relation: {
@@ -132,7 +132,7 @@ export const REPORT_INSTANCE = {
         _id: '292d401e-df4f-47ff-89f0-174cd7e52553',
         id: '292d401e-df4f-47ff-89f0-174cd7e52553',
         sort: [
-          'relationship-meta--156b01cb-45cf-45da-9907-7ae046617024'
+          'relationship-meta--156b01cb-45cf-45da-9907-7ae046617024',
         ],
         base_type: 'RELATION',
         internal_id: '292d401e-df4f-47ff-89f0-174cd7e52553',
@@ -150,9 +150,9 @@ export const REPORT_INSTANCE = {
         toName: 'marie',
         toType: 'User',
         target_ref: 'user--temporary',
-        relationship_type: 'object-assignee'
-      }
-    }
+        relationship_type: 'object-assignee',
+      },
+    },
   ],
   'created-by': '51bca270-ec82-4b04-bd0a-d78274c70d92',
   createdBy: {
@@ -160,10 +160,10 @@ export const REPORT_INSTANCE = {
     _id: '51bca270-ec82-4b04-bd0a-d78274c70d92',
     id: '51bca270-ec82-4b04-bd0a-d78274c70d92',
     sort: [
-      1749547918224
+      1749547918224,
     ],
     x_opencti_stix_ids: [
-      'identity--d37acc64-4a6f-4dc2-879a-a4c138d0a27f'
+      'identity--d37acc64-4a6f-4dc2-879a-a4c138d0a27f',
     ],
     name: 'John Doe',
     description: null,
@@ -184,7 +184,7 @@ export const REPORT_INSTANCE = {
     internal_id: '51bca270-ec82-4b04-bd0a-d78274c70d92',
     standard_id: 'identity--072d8aaa-93a9-5ded-89e4-1ad87d8b91c6',
     creator_id: [
-      '88ec0c6a-13ce-5e39-b486-354fe4a7084f'
+      '88ec0c6a-13ce-5e39-b486-354fe4a7084f',
     ],
     created_at: '2025-06-10T09:31:58.224Z',
     updated_at: '2025-06-10T09:31:58.237Z',
@@ -195,14 +195,14 @@ export const REPORT_INSTANCE = {
       'Stix-Object',
       'Stix-Core-Object',
       'Stix-Domain-Object',
-      'Identity'
+      'Identity',
     ],
     i_relation: {
       _index: 'opencti_stix_meta_relationships-000001',
       _id: '11616813-abc0-4fbf-9b35-31793f814ff8',
       id: '11616813-abc0-4fbf-9b35-31793f814ff8',
       sort: [
-        'relationship-meta--1de7f69c-c687-423b-b15f-0f58e45fc0ca'
+        'relationship-meta--1de7f69c-c687-423b-b15f-0f58e45fc0ca',
       ],
       base_type: 'RELATION',
       internal_id: '11616813-abc0-4fbf-9b35-31793f814ff8',
@@ -220,12 +220,12 @@ export const REPORT_INSTANCE = {
       toName: 'John Doe',
       toType: 'Individual',
       target_ref: 'identity--temporary',
-      relationship_type: 'created-by'
-    }
+      relationship_type: 'created-by',
+    },
   },
   object: [
     '5a3ff5f1-66ab-46c8-b5ce-67aebfaee6f5',
-    'c808561d-ed59-4d54-914a-5d090eceafd6'
+    'c808561d-ed59-4d54-914a-5d090eceafd6',
   ],
   objects: [
     {
@@ -233,23 +233,23 @@ export const REPORT_INSTANCE = {
       _id: '5a3ff5f1-66ab-46c8-b5ce-67aebfaee6f5',
       id: '5a3ff5f1-66ab-46c8-b5ce-67aebfaee6f5',
       sort: [
-        1749547930988
+        1749547930988,
       ],
       x_opencti_stix_ids: [
-        'threat-actor--dfaa8d77-07e2-4e28-b2c8-92e9f7b04428'
+        'threat-actor--dfaa8d77-07e2-4e28-b2c8-92e9f7b04428',
       ],
       name: 'Disco Team Threat Actor Group',
       description: 'This organized threat actor group operates to create profit from all types of crime.',
       aliases: [
-        'Equipo del Discoteca'
+        'Equipo del Discoteca',
       ],
       threat_actor_types: [
-        'crime-syndicate'
+        'crime-syndicate',
       ],
       first_seen: '1970-01-01T00:00:00.000Z',
       last_seen: '5138-11-16T09:46:40.000Z',
       goals: [
-        'Steal Credit Card Information'
+        'Steal Credit Card Information',
       ],
       sophistication: 'expert',
       resource_level: 'organization',
@@ -266,12 +266,12 @@ export const REPORT_INSTANCE = {
       internal_id: '5a3ff5f1-66ab-46c8-b5ce-67aebfaee6f5',
       standard_id: 'threat-actor--fd6b0e6f-96e0-568d-ba24-8a140d0428cd',
       creator_id: [
-        '88ec0c6a-13ce-5e39-b486-354fe4a7084f'
+        '88ec0c6a-13ce-5e39-b486-354fe4a7084f',
       ],
       created_at: '2025-06-10T09:32:10.988Z',
       updated_at: '2025-06-10T09:32:11.003Z',
       i_aliases_ids: [
-        'threat-actor--e91d383c-d377-5e7b-8386-36cf8ce110f9'
+        'threat-actor--e91d383c-d377-5e7b-8386-36cf8ce110f9',
       ],
       base_type: 'ENTITY',
       parent_types: [
@@ -279,14 +279,14 @@ export const REPORT_INSTANCE = {
         'Stix-Object',
         'Stix-Core-Object',
         'Stix-Domain-Object',
-        'Threat-Actor'
+        'Threat-Actor',
       ],
       i_relation: {
         _index: 'opencti_stix_meta_relationships-000001',
         _id: '48103821-876d-446d-8368-8b4246111164',
         id: '48103821-876d-446d-8368-8b4246111164',
         sort: [
-          'relationship-meta--3eab20a6-7c94-4adf-a283-e828d6c16ac1'
+          'relationship-meta--3eab20a6-7c94-4adf-a283-e828d6c16ac1',
         ],
         base_type: 'RELATION',
         internal_id: '48103821-876d-446d-8368-8b4246111164',
@@ -304,15 +304,15 @@ export const REPORT_INSTANCE = {
         toName: 'Disco Team Threat Actor Group',
         toType: 'Threat-Actor-Group',
         target_ref: 'threat-actor--temporary',
-        relationship_type: 'object'
-      }
+        relationship_type: 'object',
+      },
     },
     {
       _index: 'opencti_stix_cyber_observables-000001',
       _id: 'c808561d-ed59-4d54-914a-5d090eceafd6',
       id: 'c808561d-ed59-4d54-914a-5d090eceafd6',
       sort: [
-        1749548017333
+        1749548017333,
       ],
       x_opencti_score: 50,
       x_opencti_description: 'Basic credential',
@@ -322,10 +322,10 @@ export const REPORT_INSTANCE = {
       internal_id: 'c808561d-ed59-4d54-914a-5d090eceafd6',
       standard_id: 'credential--4a194ac7-4aef-57d5-9a64-1312da4e604a',
       creator_id: [
-        '88ec0c6a-13ce-5e39-b486-354fe4a7084f'
+        '88ec0c6a-13ce-5e39-b486-354fe4a7084f',
       ],
       x_opencti_stix_ids: [
-        'credential--8e0a834a-c233-5c50-8186-adde10a69f02'
+        'credential--8e0a834a-c233-5c50-8186-adde10a69f02',
       ],
       created_at: '2025-06-10T09:33:37.333Z',
       updated_at: '2025-06-10T09:33:37.345Z',
@@ -334,18 +334,18 @@ export const REPORT_INSTANCE = {
         'Basic-Object',
         'Stix-Object',
         'Stix-Core-Object',
-        'Stix-Cyber-Observable'
+        'Stix-Cyber-Observable',
       ],
       modified: '2025-06-10T09:33:37.345Z',
       draft_ids: [
-        '2c0ff00c-f892-4f22-b863-58de30313461'
+        '2c0ff00c-f892-4f22-b863-58de30313461',
       ],
       i_relation: {
         _index: 'opencti_stix_meta_relationships-000001',
         _id: 'c92261ad-d17e-4ab0-a752-fd33aa9ccb86',
         id: 'c92261ad-d17e-4ab0-a752-fd33aa9ccb86',
         sort: [
-          'relationship-meta--901a7eac-e855-483d-ad6b-b46a3fb992ea'
+          'relationship-meta--901a7eac-e855-483d-ad6b-b46a3fb992ea',
         ],
         base_type: 'RELATION',
         internal_id: 'c92261ad-d17e-4ab0-a752-fd33aa9ccb86',
@@ -363,12 +363,12 @@ export const REPORT_INSTANCE = {
         toName: 'azerty',
         toType: 'Credential',
         target_ref: 'credential--temporary',
-        relationship_type: 'object'
-      }
-    }
+        relationship_type: 'object',
+      },
+    },
   ],
   'external-reference': [
-    '746847b8-f5d0-4c82-a7a2-70649ac9b135'
+    '746847b8-f5d0-4c82-a7a2-70649ac9b135',
   ],
   externalReferences: [
     {
@@ -376,7 +376,7 @@ export const REPORT_INSTANCE = {
       _id: '746847b8-f5d0-4c82-a7a2-70649ac9b135',
       id: '746847b8-f5d0-4c82-a7a2-70649ac9b135',
       sort: [
-        1749547864832
+        1749547864832,
       ],
       x_opencti_stix_ids: [],
       source_name: 'capec',
@@ -390,7 +390,7 @@ export const REPORT_INSTANCE = {
       internal_id: '746847b8-f5d0-4c82-a7a2-70649ac9b135',
       standard_id: 'external-reference--4a67461d-68b8-5a27-996f-a8e30578cb56',
       creator_id: [
-        '88ec0c6a-13ce-5e39-b486-354fe4a7084f'
+        '88ec0c6a-13ce-5e39-b486-354fe4a7084f',
       ],
       created_at: '2025-06-10T09:31:04.832Z',
       updated_at: '2025-06-10T09:31:04.832Z',
@@ -398,14 +398,14 @@ export const REPORT_INSTANCE = {
       parent_types: [
         'Basic-Object',
         'Stix-Object',
-        'Stix-Meta-Object'
+        'Stix-Meta-Object',
       ],
       i_relation: {
         _index: 'opencti_stix_meta_relationships-000001',
         _id: 'e1e203d5-b892-4d1d-ac64-63007c86e605',
         id: 'e1e203d5-b892-4d1d-ac64-63007c86e605',
         sort: [
-          'relationship-meta--9c2089a8-dadf-454d-beeb-963ca14ed6a3'
+          'relationship-meta--9c2089a8-dadf-454d-beeb-963ca14ed6a3',
         ],
         base_type: 'RELATION',
         internal_id: 'e1e203d5-b892-4d1d-ac64-63007c86e605',
@@ -423,12 +423,12 @@ export const REPORT_INSTANCE = {
         toName: 'capec (CAPEC-163)',
         toType: 'External-Reference',
         target_ref: 'external-reference--temporary',
-        relationship_type: 'external-reference'
-      }
-    }
+        relationship_type: 'external-reference',
+      },
+    },
   ],
   'object-marking': [
-    '2cc876e4-ae90-497b-a4ae-16c762f0d03f'
+    '2cc876e4-ae90-497b-a4ae-16c762f0d03f',
   ],
   objectMarking: [
     {
@@ -436,7 +436,7 @@ export const REPORT_INSTANCE = {
       _id: '2cc876e4-ae90-497b-a4ae-16c762f0d03f',
       id: '2cc876e4-ae90-497b-a4ae-16c762f0d03f',
       sort: [
-        1749546753947
+        1749546753947,
       ],
       definition_type: 'PAP',
       definition: 'PAP:CLEAR',
@@ -447,7 +447,7 @@ export const REPORT_INSTANCE = {
       internal_id: '2cc876e4-ae90-497b-a4ae-16c762f0d03f',
       standard_id: 'marking-definition--4cdff7eb-acb8-543f-8573-829eb9fe8b34',
       creator_id: [
-        '6a4b11e1-90ca-4e42-ba42-db7bc7f7d505'
+        '6a4b11e1-90ca-4e42-ba42-db7bc7f7d505',
       ],
       x_opencti_stix_ids: [],
       created_at: '2025-06-10T09:12:33.947Z',
@@ -458,14 +458,14 @@ export const REPORT_INSTANCE = {
       parent_types: [
         'Basic-Object',
         'Stix-Object',
-        'Stix-Meta-Object'
+        'Stix-Meta-Object',
       ],
       i_relation: {
         _index: 'opencti_stix_meta_relationships-000001',
         _id: 'da363089-412c-452a-95d2-ed8146e94eca',
         id: 'da363089-412c-452a-95d2-ed8146e94eca',
         sort: [
-          'relationship-meta--b98b7725-b5a6-4d40-bd4b-f261f278d401'
+          'relationship-meta--b98b7725-b5a6-4d40-bd4b-f261f278d401',
         ],
         base_type: 'RELATION',
         internal_id: 'da363089-412c-452a-95d2-ed8146e94eca',
@@ -483,12 +483,12 @@ export const REPORT_INSTANCE = {
         toName: 'PAP:CLEAR',
         toType: 'Marking-Definition',
         target_ref: 'marking-definition--temporary',
-        relationship_type: 'object-marking'
-      }
-    }
+        relationship_type: 'object-marking',
+      },
+    },
   ],
   'object-participant': [
-    '0ff0750e-4d91-425d-b44c-b69269dead0b'
+    '0ff0750e-4d91-425d-b44c-b69269dead0b',
   ],
   objectParticipant: [
     {
@@ -496,7 +496,7 @@ export const REPORT_INSTANCE = {
       _id: '0ff0750e-4d91-425d-b44c-b69269dead0b',
       id: '0ff0750e-4d91-425d-b44c-b69269dead0b',
       sort: [
-        1750152527269
+        1750152527269,
       ],
       user_email: 'marie.flores@filigran.io',
       name: 'marie',
@@ -513,21 +513,21 @@ export const REPORT_INSTANCE = {
       external: false,
       personal_notifiers: [
         'f4ee7b33-006a-4b0d-b57d-411ad288653d',
-        '44fcf1f4-8e31-4b31-8dbc-cd6993e1b822'
+        '44fcf1f4-8e31-4b31-8dbc-cd6993e1b822',
       ],
       confidence: 100,
       entity_type: 'User',
       internal_id: '0ff0750e-4d91-425d-b44c-b69269dead0b',
       standard_id: 'user--20e40687-5a83-5a19-ba58-ca14e88fdbd1',
       creator_id: [
-        '88ec0c6a-13ce-5e39-b486-354fe4a7084f'
+        '88ec0c6a-13ce-5e39-b486-354fe4a7084f',
       ],
       created_at: '2025-06-17T09:28:47.269Z',
       updated_at: '2025-06-17T09:28:47.332Z',
       base_type: 'ENTITY',
       parent_types: [
         'Basic-Object',
-        'Internal-Object'
+        'Internal-Object',
       ],
       modified: '2025-06-17T09:28:47.332Z',
       i_relation: {
@@ -535,7 +535,7 @@ export const REPORT_INSTANCE = {
         _id: 'ca4b31b7-b269-4bba-bd99-30c24803bb4b',
         id: 'ca4b31b7-b269-4bba-bd99-30c24803bb4b',
         sort: [
-          'relationship-meta--d742800b-e75c-4559-b175-060da45c7977'
+          'relationship-meta--d742800b-e75c-4559-b175-060da45c7977',
         ],
         base_type: 'RELATION',
         internal_id: 'ca4b31b7-b269-4bba-bd99-30c24803bb4b',
@@ -553,12 +553,12 @@ export const REPORT_INSTANCE = {
         toName: 'marie',
         toType: 'User',
         target_ref: 'user--temporary',
-        relationship_type: 'object-participant'
-      }
-    }
+        relationship_type: 'object-participant',
+      },
+    },
   ],
   'object-label': [
-    '8e221bf3-bc58-4636-bf14-b23c89502fe1'
+    '8e221bf3-bc58-4636-bf14-b23c89502fe1',
   ],
   objectLabel: [
     {
@@ -566,7 +566,7 @@ export const REPORT_INSTANCE = {
       _id: '8e221bf3-bc58-4636-bf14-b23c89502fe1',
       id: '8e221bf3-bc58-4636-bf14-b23c89502fe1',
       sort: [
-        1749548104126
+        1749548104126,
       ],
       x_opencti_stix_ids: [],
       value: 'report',
@@ -576,7 +576,7 @@ export const REPORT_INSTANCE = {
       internal_id: '8e221bf3-bc58-4636-bf14-b23c89502fe1',
       standard_id: 'label--a87ca076-f2b8-5374-a25b-103df133d9d3',
       creator_id: [
-        '88ec0c6a-13ce-5e39-b486-354fe4a7084f'
+        '88ec0c6a-13ce-5e39-b486-354fe4a7084f',
       ],
       created_at: '2025-06-10T09:35:04.126Z',
       updated_at: '2025-06-10T09:35:04.126Z',
@@ -586,14 +586,14 @@ export const REPORT_INSTANCE = {
       parent_types: [
         'Basic-Object',
         'Stix-Object',
-        'Stix-Meta-Object'
+        'Stix-Meta-Object',
       ],
       i_relation: {
         _index: 'opencti_stix_meta_relationships-000001',
         _id: '7cea2ff8-18d0-4928-9154-6c462e855c66',
         id: '7cea2ff8-18d0-4928-9154-6c462e855c66',
         sort: [
-          'relationship-meta--f3f75d36-7b65-4e17-9fae-88973d56888d'
+          'relationship-meta--f3f75d36-7b65-4e17-9fae-88973d56888d',
         ],
         base_type: 'RELATION',
         internal_id: '7cea2ff8-18d0-4928-9154-6c462e855c66',
@@ -611,10 +611,10 @@ export const REPORT_INSTANCE = {
         toName: 'report',
         toType: 'Label',
         target_ref: 'label--temporary',
-        relationship_type: 'object-label'
-      }
-    }
-  ]
+        relationship_type: 'object-label',
+      },
+    },
+  ],
 } as unknown as StoreEntity;
 
 export const EXPECTED_REPORT = {
@@ -628,30 +628,30 @@ export const EXPECTED_REPORT = {
   name: 'Report STIX 2.0',
   description: 'description',
   report_types: [
-    'internal-report'
+    'internal-report',
   ],
   published: '2025-06-26T14:32:10.000Z',
   x_opencti_workflow_id: 'b29268cc-bb75-4f28-96c9-4cb48e549dff',
   labels: [
-    'report'
+    'report',
   ],
   external_references: [
     {
       source_name: 'capec',
       description: 'spear phishing',
-      external_id: 'CAPEC-163'
-    }
+      external_id: 'CAPEC-163',
+    },
   ],
   x_opencti_id: '0c38a734-3150-468f-bf38-8dc1f937a1b3',
   x_opencti_type: 'Report',
   type: 'report',
   created_by_ref: 'identity--072d8aaa-93a9-5ded-89e4-1ad87d8b91c6',
   object_marking_refs: [
-    'marking-definition--4cdff7eb-acb8-543f-8573-829eb9fe8b34'
+    'marking-definition--4cdff7eb-acb8-543f-8573-829eb9fe8b34',
   ],
   object_refs: [
     'threat-actor--fd6b0e6f-96e0-568d-ba24-8a140d0428cd',
-    'credential--4a194ac7-4aef-57d5-9a64-1312da4e604a'
+    'credential--4a194ac7-4aef-57d5-9a64-1312da4e604a',
   ],
   x_opencti_files: [
     {
