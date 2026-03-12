431e9f429fc523dc30946da5bd9146c2d7ee4f42
marie flores <marie.flores@filigran.io>
2026-03-12 15:12:16 +0100

    [backend] migrate Stream component to dataTable

--- opencti-platform/opencti-front/src/private/components/data/stream/StreamCollectionCreation.tsx
+++ opencti-platform/opencti-front/src/private/components/data/stream/StreamCollectionCreation.tsx
@@ -1,235 +1,236 @@
-import React, { FunctionComponent } from 'react';
 import { Field, Form, Formik } from 'formik';
-import Button from '@common/button/Button';
-import * as Yup from 'yup';
 import { graphql } from 'react-relay';
-import { ConnectionHandler, RecordProxy, RecordSourceSelectorProxy } from 'relay-runtime';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
+import * as Yup from 'yup';
+import { RecordSourceSelectorProxy } from 'relay-runtime';
+import Box from '@mui/material/Box';
 import Alert from '@mui/material/Alert';
 import AlertTitle from '@mui/material/AlertTitle';
-import Box from '@mui/material/Box';
-import makeStyles from '@mui/styles/makeStyles';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
 import { FormikConfig } from 'formik/dist/types';
-import { commitMutation } from '../../../../relay/environment';
-import TextField from '../../../../components/TextField';
-import Filters from '../../common/lists/Filters';
-import { emptyFilterGroup, serializeFilterGroupForBackend, stixFilters } from '../../../../utils/filters/filtersUtils';
-import FilterIconButton from '../../../../components/FilterIconButton';
-import { FieldOption, fieldSpacingContainerStyle } from '../../../../utils/field';
-import ObjectMembersField from '../../common/form/ObjectMembersField';
-import Drawer, { DrawerControlledDialProps } from '../../common/drawer/Drawer';
-import useFiltersState from '../../../../utils/filters/useFiltersState';
-import { PaginationOptions } from '../../../../components/list_lines';
-import type { Theme } from '../../../../components/Theme';
+import Button from '@common/button/Button';
+import Drawer, { DrawerControlledDialProps } from '@components/common/drawer/Drawer';
+import FormButtonContainer from '@common/form/FormButtonContainer';
 import { useFormatter } from '../../../../components/i18n';
+import TextField from '../../../../components/TextField';
+import ObjectMembersField from '../../common/form/ObjectMembersField';
+import Filters from '../../common/lists/Filters';
+import FilterIconButton from '../../../../components/FilterIconButton';
+import { fieldSpacingContainerStyle, FieldOption } from '../../../../utils/field';
+import { emptyFilterGroup, serializeFilterGroupForBackend, stixFilters } from '../../../../utils/filters/filtersUtils';
+import useFiltersState from '../../../../utils/filters/useFiltersState';
+import { insertNode } from '../../../../utils/store';
+import useApiMutation from '../../../../utils/hooks/useApiMutation';
+import { handleErrorInForm } from '../../../../relay/environment';
 import CreateEntityControlledDial from '../../../../components/CreateEntityControlledDial';
-import FormButtonContainer from '../../../../components/common/form/FormButtonContainer';
-import { useTheme } from '@mui/material/styles';
+import { FilterGroup } from '../../../../utils/filters/filtersHelpers-types';
+import { StreamCollectionCreationMutation } from './__generated__/StreamCollectionCreationMutation.graphql';
+import { StreamLinesPaginationQuery$variables } from '../__generated__/StreamLinesPaginationQuery.graphql';
 
-interface StreamCollectionCreationProps {
-  paginationOptions: PaginationOptions;
-}
-
-interface StreamCollectionCreationForm {
-  authorized_members: FieldOption[];
-  stream_public: boolean;
-  name: string;
-  description: string;
-}
-// Deprecated - https://mui.com/system/styles/basics/
-// Do not use it for new code.
-const useStyles = makeStyles<Theme>(() => ({
-  alert: {
-    width: '100%',
-    marginTop: 20,
-  },
-  message: {
-    width: '100%',
-    overflow: 'hidden',
-  },
-}));
-
-const StreamCollectionCreationMutation = graphql`
-    mutation StreamCollectionCreationMutation($input: StreamCollectionAddInput!) {
-        streamCollectionAdd(input: $input) {
-            ...StreamLine_node
-        }
+export const streamCollectionCreationMutation = graphql`
+  mutation StreamCollectionCreationMutation($input: StreamCollectionAddInput!) {
+    streamCollectionAdd(input: $input) {
+      id
+      name
+      description
+      ...StreamLine_node
     }
+  }
 `;
 
-const streamCollectionCreationValidation = (requiredSentence: string) => Yup.object().shape({
-  name: Yup.string().required(requiredSentence),
-  description: Yup.string().nullable(),
-  stream_public: Yup.bool(),
-  authorized_members: Yup.array().nullable(),
-});
+interface StreamCollectionFormProps {
+  updater: (store: RecordSourceSelectorProxy, key: string) => void;
+  onReset?: () => void;
+  onCompleted?: () => void;
+  filters: FilterGroup;
+  helpers: ReturnType<typeof useFiltersState>[1];
+}
 
-const sharedUpdater = (store: RecordSourceSelectorProxy, userId: string, paginationOptions: PaginationOptions, newEdge: RecordProxy) => {
-  const userProxy = store.get(userId);
-  if (userProxy) {
-    const conn = ConnectionHandler.getConnection(
-      userProxy,
-      'Pagination_streamCollections',
-      paginationOptions,
-    );
-    ConnectionHandler.insertEdgeBefore(conn as RecordProxy, newEdge);
-  }
-};
+export interface StreamCollectionCreationFormValues {
+  name: string;
+  description: string;
+  stream_public: boolean;
+  authorized_members: FieldOption[];
+}
 
-const CreateStreamCollectionControlledDial = (props: DrawerControlledDialProps) => (
-  <CreateEntityControlledDial
-    entityType="StreamCollection"
-    {...props}
-  />
-);
-
-const StreamCollectionCreation: FunctionComponent<StreamCollectionCreationProps> = ({ paginationOptions }) => {
-  const [filters, helpers] = useFiltersState(emptyFilterGroup);
-  const classes = useStyles();
+const StreamCollectionCreationForm = ({
+  updater,
+  onReset,
+  onCompleted,
+  filters,
+  helpers,
+}: StreamCollectionFormProps) => {
   const { t_i18n } = useFormatter();
-  const theme = useTheme();
 
-  const onSubmit: FormikConfig<StreamCollectionCreationForm>['onSubmit'] = (values, { setSubmitting, resetForm }) => {
+  const [commit] = useApiMutation<StreamCollectionCreationMutation>(streamCollectionCreationMutation);
+
+  const onSubmit: FormikConfig<StreamCollectionCreationFormValues>['onSubmit'] = (
+    values,
+    { setSubmitting, setErrors, resetForm },
+  ) => {
     const jsonFilters = serializeFilterGroupForBackend(filters);
     const authorized_members = values.authorized_members.map(({ value }) => ({
       id: value,
       access_right: 'view',
     }));
-    commitMutation({
-      mutation: StreamCollectionCreationMutation,
+
+    commit({
       variables: {
-        input: { ...values, filters: jsonFilters, authorized_members },
+        input: {
+          name: values.name,
+          description: values.description,
+          stream_public: values.stream_public,
+          authorized_members,
+          filters: jsonFilters,
+        },
       },
-      updater: (store: RecordSourceSelectorProxy) => {
-        const payload = store.getRootField('streamCollectionAdd');
-        const newEdge = payload?.setLinkedRecord(payload, 'node');
-        const container = store.getRoot();
-        sharedUpdater(
-          store,
-          container.getDataID(),
-          paginationOptions,
-          newEdge as RecordProxy,
-        );
+      updater: (store) => {
+        if (updater) {
+          updater(store, 'streamCollectionAdd');
+        }
+      },
+      onError: (error) => {
+        handleErrorInForm(error, setErrors);
+        setSubmitting(false);
       },
       onCompleted: () => {
         setSubmitting(false);
         resetForm();
+        if (onCompleted) {
+          onCompleted();
+        }
       },
-      optimisticUpdater: undefined,
-      optimisticResponse: undefined,
-      onError: undefined,
-      setSubmitting,
     });
   };
+
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required(t_i18n('This field is required')),
+    description: Yup.string().nullable(),
+    stream_public: Yup.bool(),
+    authorized_members: Yup.array().nullable(),
+  });
+
+  return (
+    <Formik<StreamCollectionCreationFormValues>
+      initialValues={{
+        name: '',
+        description: '',
+        stream_public: false,
+        authorized_members: [],
+      }}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+      onReset={onReset}
+    >
+      {({ submitForm, handleReset, isSubmitting, setFieldValue, values }) => (
+        <Form>
+          <div style={{ display: 'flex', flexDirection: 'column', gap: 20 }}>
+            <Field
+              component={TextField}
+              variant="standard"
+              name="name"
+              label={t_i18n('Name')}
+              fullWidth
+            />
+            <Field
+              component={TextField}
+              variant="standard"
+              name="description"
+              label={t_i18n('Description')}
+              fullWidth
+            />
+            <Alert
+              icon={false}
+              severity="warning"
+              variant="outlined"
+              sx={{ width: '100%', position: 'relative' }}
+            >
+              <AlertTitle>
+                {t_i18n('Make this stream public and available to anyone')}
+              </AlertTitle>
+              <FormControlLabel
+                control={(
+                  <Switch
+                    checked={values.stream_public}
+                    onChange={(_, checked) => setFieldValue('stream_public', checked)}
+                  />
+                )}
+                label={t_i18n('Public stream')}
+              />
+              {!values.stream_public && (
+                <ObjectMembersField
+                  label="Accessible for"
+                  style={fieldSpacingContainerStyle}
+                  helpertext={t_i18n('Leave the field empty to grant all authenticated users')}
+                  multiple
+                  name="authorized_members"
+                />
+              )}
+            </Alert>
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+              <Filters
+                availableFilterKeys={stixFilters}
+                helpers={helpers}
+                searchContext={{ entityTypes: ['Stix-Core-Object', 'stix-core-relationship', 'Stix-Filtering'] }}
+              />
+            </Box>
+            <FilterIconButton
+              filters={filters}
+              helpers={helpers}
+              redirection
+              searchContext={{ entityTypes: ['Stix-Core-Object', 'stix-core-relationship'] }}
+            />
+          </div>
+          <FormButtonContainer>
+            <Button
+              variant="secondary"
+              onClick={handleReset}
+              disabled={isSubmitting}
+            >
+              {t_i18n('Cancel')}
+            </Button>
+            <Button
+              onClick={submitForm}
+              disabled={isSubmitting}
+            >
+              {t_i18n('Create')}
+            </Button>
+          </FormButtonContainer>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+const StreamCollectionCreation = ({ paginationOptions }: { paginationOptions: StreamLinesPaginationQuery$variables }) => {
+  const { t_i18n } = useFormatter();
+  const [filters, helpers] = useFiltersState(emptyFilterGroup);
+
+  const updater = (store: RecordSourceSelectorProxy) => insertNode(
+    store,
+    'Pagination_streamCollections',
+    paginationOptions,
+    'streamCollectionAdd',
+  );
+
+  const CreateStreamControlledDial = (props: DrawerControlledDialProps) => (
+    <CreateEntityControlledDial entityType="StreamCollection" {...props} />
+  );
 
   return (
     <Drawer
       title={t_i18n('Create a stream')}
-      controlledDial={CreateStreamCollectionControlledDial}
+      controlledDial={CreateStreamControlledDial}
       onClose={helpers.handleClearAllFilters}
     >
       {({ onClose }) => (
-        <Formik
-          initialValues={{
-            name: '',
-            description: '',
-            stream_public: false,
-            authorized_members: [] as FieldOption[],
-          }}
-          validationSchema={streamCollectionCreationValidation(t_i18n('This field is required'))}
-          onSubmit={onSubmit}
+        <StreamCollectionCreationForm
+          updater={updater}
+          onCompleted={onClose}
           onReset={onClose}
-        >
-          {({
-            submitForm,
-            handleReset,
-            isSubmitting,
-            setFieldValue,
-            values,
-          }) => (
-            <Form>
-              <Field
-                component={TextField}
-                variant="standard"
-                name="name"
-                label={t_i18n('Name')}
-                fullWidth={true}
-              />
-              <Field
-                component={TextField}
-                variant="standard"
-                name="description"
-                label={t_i18n('Description')}
-                fullWidth={true}
-                style={{ marginTop: 20 }}
-              />
-              <Alert
-                icon={false}
-                classes={{ root: classes.alert, message: classes.message }}
-                severity="warning"
-                variant="outlined"
-                style={{ position: 'relative' }}
-              >
-                <AlertTitle>
-                  {t_i18n('Make this stream public and available to anyone')}
-                </AlertTitle>
-                <FormControlLabel
-                  control={<Switch />}
-                  style={{ marginLeft: 1 }}
-                  name="stream_public"
-                  onChange={(_, checked) => setFieldValue('stream_public', checked)}
-                  label={t_i18n('Public stream')}
-                />
-                {!values.stream_public && (
-                  <ObjectMembersField
-                    label="Accessible for"
-                    style={fieldSpacingContainerStyle}
-                    helpertext={t_i18n('Leave the field empty to grant all authenticated users')}
-                    multiple={true}
-                    name="authorized_members"
-                  />
-                )}
-              </Alert>
-              <Box sx={{
-                marginTop: '20px',
-                display: 'flex',
-                alignItems: 'center',
-                gap: theme.spacing(1),
-                marginBottom: theme.spacing(1),
-              }}
-              >
-                <Filters
-                  availableFilterKeys={stixFilters}
-                  helpers={helpers}
-                  searchContext={{ entityTypes: ['Stix-Core-Object', 'stix-core-relationship', 'Stix-Filtering'] }}
-                />
-              </Box>
-              <FilterIconButton
-                filters={filters}
-                helpers={helpers}
-                redirection
-                searchContext={{ entityTypes: ['Stix-Core-Object', 'stix-core-relationship'] }}
-                entityTypes={['Stix-Core-Object', 'stix-core-relationship', 'Stix-Filtering']}
-              />
-              <FormButtonContainer>
-                <Button
-                  variant="secondary"
-                  onClick={handleReset}
-                  disabled={isSubmitting}
-                >
-                  {t_i18n('Cancel')}
-                </Button>
-                <Button
-                  onClick={submitForm}
-                  disabled={isSubmitting}
-                >
-                  {t_i18n('Create')}
-                </Button>
-              </FormButtonContainer>
-            </Form>
-          )}
-        </Formik>
+          filters={filters}
+          helpers={helpers}
+        />
       )}
     </Drawer>
   );
