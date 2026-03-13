97cc665a679a6184d777f3cb855018dcc4e4890b
Valentin Bouzin <valentin.bouzin@filigran.io>
2026-03-13 16:01:09 +0100

    [frontend] list item button updated to keep the right design  (#14846)

--- opencti-platform/opencti-front/src/private/components/settings/groups/Group.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/groups/Group.tsx
@@ -211,33 +211,26 @@ const Group = ({ groupData }: { groupData: Group_group$key }) => {
                   {t_i18n('Default dashboard')}
                 </Label>
                 <FieldOrEmpty source={group.default_dashboard}>
-                  <List>
-                    <ListItem
-                      dense={true}
-                      divider={true}
-                      secondaryAction={!canAccessDashboard && (
-                        <Tooltip
-                          title={t_i18n(
-                            'You need to authorize this group to access this dashboard in the permissions of the workspace.',
-                          )}
-                        >
-                          <WarningOutlined color="warning" />
-                        </Tooltip>
+                  <ListItemButton
+                    component={Link}
+                    dense
+                    divider
+                    to={`/dashboard/workspaces/dashboards/${group.default_dashboard?.id}`}
+                  >
+                    <ListItemIcon>
+                      <ItemIcon type="Dashboard" />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={truncate(group.default_dashboard?.name, 40)}
+                    />
+                    <Tooltip
+                      title={t_i18n(
+                        'You need to authorize this group to access this dashboard in the permissions of the workspace.',
                       )}
                     >
-                      <ListItemButton
-                        component={Link}
-                        to={`/dashboard/workspaces/dashboards/${group.default_dashboard?.id}`}
-                      >
-                        <ListItemIcon>
-                          <ItemIcon type="Dashboard" />
-                        </ListItemIcon>
-                        <ListItemText
-                          primary={truncate(group.default_dashboard?.name, 40)}
-                        />
-                      </ListItemButton>
-                    </ListItem>
-                  </List>
+                      <WarningOutlined color="warning" />
+                    </Tooltip>
+                  </ListItemButton>
                 </FieldOrEmpty>
               </Grid>
               <Grid item xs={12}>
