ea8d40d8465fd78f2754c59f96d5c14884e314f4
Ludovic Bouges <ludovic.bouges@filigran.io>
2026-03-16 18:59:49 +0100

    refactor(dataSharing): enhance type safety and improve resolver return types for stream and taxii collections

--- opencti-platform/opencti-graphql/src/modules/dataSharing/streamCollection-resolver.ts
+++ opencti-platform/opencti-graphql/src/modules/dataSharing/streamCollection-resolver.ts
@@ -1,4 +1,5 @@
-import type { EditContext, EditInput, Resolvers, RedisStreamInfo } from '../../generated/graphql';
+import type { EditContext, EditInput, Resolvers, StreamCollection, StreamCollectionConnection, StreamCollectionEditMutations, RedisStreamInfo } from '../../generated/graphql';
+import type { BasicStoreEntity } from '../../types/store';
 import {
   createStreamCollection,
   findById,
@@ -14,22 +15,22 @@ import { fetchStreamInfo } from '../../database/stream/stream-handler';
 
 const streamCollectionResolvers: Resolvers = {
   Query: {
-    streamCollection: (_, { id }, context) => findById(context, context.user, id),
-    streamCollections: (_, args, context) => findStreamCollectionPaginated(context, context.user, args),
+    streamCollection: (_, { id }, context) => findById(context, context.user, id) as unknown as Promise<StreamCollection>,
+    streamCollections: (_, args, context) => findStreamCollectionPaginated(context, context.user, args) as unknown as Promise<StreamCollectionConnection>,
     redisStreamInfo: () => fetchStreamInfo() as unknown as Promise<RedisStreamInfo>,
   },
   StreamCollection: {
-    authorized_members: (stream, _, context) => getAuthorizedMembers(context, context.user, stream),
+    authorized_members: (stream, _, context) => getAuthorizedMembers(context, context.user, stream as unknown as BasicStoreEntity),
     consumers: (stream) => getStreamCollectionConsumers(stream.id),
   },
   Mutation: {
-    streamCollectionAdd: (_, { input }, context) => createStreamCollection(context, context.user, input),
+    streamCollectionAdd: (_, { input }, context) => createStreamCollection(context, context.user, input) as unknown as Promise<StreamCollection>,
     streamCollectionEdit: (_, { id }, context) => ({
       delete: () => streamCollectionDelete(context, context.user, id),
       fieldPatch: ({ input }: { input: EditInput[] }) => streamCollectionEditField(context, context.user, id, input),
       contextPatch: ({ input }: { input: EditContext }) => streamCollectionEditContext(context, context.user, id, input),
       contextClean: () => streamCollectionCleanContext(context, context.user, id),
-    }) as any,
+    }) as unknown as StreamCollectionEditMutations,
   },
 };
 
