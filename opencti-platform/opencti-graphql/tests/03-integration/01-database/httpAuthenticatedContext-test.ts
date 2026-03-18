aad716ff6deeb017799f982d665a15dae9a8b270
Souad Hadjiat <souad.hadjiat@filigran.io>
2026-03-18 16:47:52 +0100

    [backend] add tests

--- opencti-platform/opencti-graphql/tests/03-integration/01-database/httpAuthenticatedContext-test.ts
+++ opencti-platform/opencti-graphql/tests/03-integration/01-database/httpAuthenticatedContext-test.ts
@@ -1,7 +1,10 @@
-import { describe, expect, it } from 'vitest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 import { createAuthenticatedContext } from '../../../src/http/httpAuthenticatedContext';
-import { ADMIN_API_TOKEN, getAuthUser, USER_EDITOR } from '../../utils/testQuery';
+import { ADMIN_API_TOKEN, getAuthUser, testContext, USER_EDITOR } from '../../utils/testQuery';
 import { OPENCTI_ADMIN_UUID } from '../../../src/schema/general';
+import { TokenDuration } from '../../../src/generated/graphql';
+import { addUserToken, revokeUserToken } from '../../../src/modules/user/user-domain';
+import type { AuthUser } from '../../../src/types/user';
 
 describe('Testing createAuthenticatedContext', () => {
   it('should create executeContext with synchronizedUpsert=false for bypass user without synchronized-upsert headers', async () => {
@@ -93,32 +96,89 @@ describe('Testing createAuthenticatedContext', () => {
     const executeContext = await createAuthenticatedContext(req, res, 'api-test');
     expect(executeContext).toBeDefined();
     expect(executeContext.user).toBeDefined();
-    console.log('executeContext', JSON.stringify(executeContext));
     expect(executeContext.user?.id).toBe(editorUser.id);
     expect(executeContext.user?.origin).toBeDefined();
     expect(executeContext.synchronizedUpsert).toBe(true);
   });
-  // don't know how I can test this yet, since I don't have user_editor api token.
-  it.skip('should create executeContext with synchronizedUpsert=false for non bypass user and synchronized-upsert headers', async () => {
-    const user = await getAuthUser(USER_EDITOR.id);
-    const token = user.api_tokens[0];
-    const res = {};
-    const req = {
-      header: (header: string) => {
-        return header; // fake response to make conf.getRequestAuditHeaders work
-      },
-      headers: {
-        ['synchronized-upsert']: 'true',
-        authorization: 'Bearer ' + token,
-      },
-      socket: {
-        remoteAddress: '128.0.0.1',
-      },
-    };
-    const executeContext = await createAuthenticatedContext(req, res, 'api-test');
-    expect(executeContext).toBeDefined();
-    expect(executeContext.user).toBeDefined();
-    expect(executeContext.user?.origin).toBeDefined();
-    expect(executeContext.synchronizedUpsert).toBe(false); // not bypass, no full sync
+  describe('Testing editor context', () => {
+    let editorUserToken: { token_id: string; plaintext_token?: string; masked_token?: string; expires_at?: string | null };
+    let editorUser: AuthUser;
+    beforeAll(async () => {
+      editorUser = await getAuthUser(USER_EDITOR.id);
+      const input = {
+        name: 'Editor Test Token',
+        duration: TokenDuration.Days_30,
+      };
+      // create a token for userEditor
+      editorUserToken = await addUserToken(testContext, editorUser, input);
+    });
+    afterAll(async () => {
+      editorUser = await getAuthUser(USER_EDITOR.id); // fetch the user again to get its tokens
+      // revoke created token
+      await revokeUserToken(testContext, editorUser, editorUserToken.token_id);
+    });
+    it('should not create executeContext for non bypass user and synchronized-upsert=true header', async () => {
+      const token = editorUserToken.plaintext_token;
+      const res = {};
+      const req = {
+        header: (header: string) => {
+          return header; // fake response to make conf.getRequestAuditHeaders work
+        },
+        headers: {
+          ['synchronized-upsert']: 'true',
+          authorization: 'Bearer ' + token,
+        },
+        socket: {
+          remoteAddress: '128.0.0.1',
+        },
+      };
+      const executeContext = await createAuthenticatedContext(req, res, 'api-test');
+      // context is created without user
+      expect(executeContext).toBeDefined();
+      expect(executeContext.user).toBeUndefined();
+    });
+    it('should create executeContext with synchronizedUpsert=false for non bypass user with synchronized-upsert=false headers', async () => {
+      const token = editorUserToken.plaintext_token;
+      const res = {};
+      const req = {
+        header: (header: string) => {
+          return header; // fake response to make conf.getRequestAuditHeaders work
+        },
+        headers: {
+          ['synchronized-upsert']: 'false',
+          authorization: 'Bearer ' + token,
+        },
+        socket: {
+          remoteAddress: '128.0.0.1',
+        },
+      };
+      const executeContext = await createAuthenticatedContext(req, res, 'api-test');
+      expect(executeContext).toBeDefined();
+      expect(executeContext.user).toBeDefined();
+      expect(executeContext.user?.id).toBe(editorUser.id);
+      expect(executeContext.user?.origin).toBeDefined();
+      expect(executeContext.synchronizedUpsert).toBe(false); // not bypass, no full sync
+    });
+    it('should create executeContext with synchronizedUpsert=false for non bypass user without synchronized-upsert headers', async () => {
+      const token = editorUserToken.plaintext_token;
+      const res = {};
+      const req = {
+        header: (header: string) => {
+          return header; // fake response to make conf.getRequestAuditHeaders work
+        },
+        headers: {
+          authorization: 'Bearer ' + token,
+        },
+        socket: {
+          remoteAddress: '128.0.0.1',
+        },
+      };
+      const executeContext = await createAuthenticatedContext(req, res, 'api-test');
+      expect(executeContext).toBeDefined();
+      expect(executeContext.user).toBeDefined();
+      expect(executeContext.user?.id).toBe(editorUser.id);
+      expect(executeContext.user?.origin).toBeDefined();
+      expect(executeContext.synchronizedUpsert).toBe(false); // not bypass, no full sync
+    });
   });
 });
