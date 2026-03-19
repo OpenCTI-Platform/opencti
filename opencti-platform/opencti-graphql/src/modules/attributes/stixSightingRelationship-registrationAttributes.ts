fbd6bb5b7677a881a1ca9615d11f43e93c4d5b79
Archidoit <75783086+Archidoit@users.noreply.github.com>
2026-03-19 17:27:26 +0100

    [backend] Sightings: x_opencti_negative field upsert (#14978)

--- opencti-platform/opencti-graphql/src/modules/attributes/stixSightingRelationship-registrationAttributes.ts
+++ opencti-platform/opencti-graphql/src/modules/attributes/stixSightingRelationship-registrationAttributes.ts
@@ -11,7 +11,7 @@ export const stixSightingRelationshipsAttributes: Array<AttributeDefinition> = [
   { name: 'first_seen', label: 'First seen', type: 'date', mandatoryType: 'customizable', editDefault: true, multiple: false, upsert: true, isFilterable: true },
   { name: 'last_seen', label: 'Last seen', type: 'date', mandatoryType: 'customizable', editDefault: true, multiple: false, upsert: true, isFilterable: true },
   { name: 'description', label: 'Description', type: 'string', format: 'text', mandatoryType: 'customizable', editDefault: true, multiple: false, upsert: true, isFilterable: true },
-  { name: 'x_opencti_negative', label: 'False positive', type: 'boolean', mandatoryType: 'customizable', editDefault: true, multiple: false, upsert: false, isFilterable: true },
+  { name: 'x_opencti_negative', label: 'False positive', type: 'boolean', mandatoryType: 'customizable', editDefault: true, multiple: false, upsert: true, isFilterable: true },
   workflowId,
   { ...connections, isFilterable: true },
 ];
