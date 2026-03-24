8ef59d719f256a01e60aefce6fb7bc13ca3dcea1
FrancoisGrunert <francois.grunert@filigran.io>
2026-03-24 16:01:50 +0100

    [frontend/backend] add filters on drafts (#14058)

--- opencti-platform/opencti-graphql/src/modules/draftWorkspace/draftWorkspace.ts
+++ opencti-platform/opencti-graphql/src/modules/draftWorkspace/draftWorkspace.ts
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import { ABSTRACT_INTERNAL_OBJECT, ABSTRACT_STIX_CORE_OBJECT } from '../../schema/general';
 import { type ModuleDefinition, registerDefinition } from '../../schema/module';
-import { authorizedMembers, authorizedMembersActivationDate, createdAt, creators, draftChange } from '../../schema/attribute-definition';
+import { authorizedMembers, authorizedMembersActivationDate, createdAt, creators, draftChange, refreshedAt, updatedAt } from '../../schema/attribute-definition';
 import { ENTITY_TYPE_DRAFT_WORKSPACE, type StixDraftWorkspace, type StoreEntityDraftWorkspace } from './draftWorkspace-types';
 import convertDraftWorkspaceToStix from './draftWorkspace-converter';
 import { getDraftStatuses } from './draftStatuses';
@@ -25,16 +25,18 @@ const DRAFT_WORKSPACE_DEFINITION: ModuleDefinition<StoreEntityDraftWorkspace, St
     { key: 'basicInformation', width: 6, label: 'Basic information' },
   ],
   attributes: [
-    createdAt,
+    { ...createdAt, isFilterable: false },
     { name: 'name', label: 'Draft name', type: 'string', format: 'short', mandatoryType: 'external', editDefault: true, multiple: false, upsert: false, isFilterable: true },
-    { name: 'description', label: 'Description', type: 'string', format: 'text', editDefault: true, mandatoryType: 'customizable', multiple: false, upsert: true, isFilterable: true },
-    { name: 'entity_id', label: 'Related entity', type: 'string', format: 'id', entityTypes: [ABSTRACT_STIX_CORE_OBJECT], mandatoryType: 'internal', editDefault: false, multiple: false, upsert: false, isFilterable: true },
+    { name: 'description', label: 'Description', type: 'string', format: 'text', editDefault: true, mandatoryType: 'customizable', multiple: false, upsert: true, isFilterable: false },
+    { name: 'entity_id', label: 'Related entity', type: 'string', format: 'id', entityTypes: [ABSTRACT_STIX_CORE_OBJECT], mandatoryType: 'internal', editDefault: false, multiple: false, upsert: false, isFilterable: false },
     { name: 'draft_status', label: 'Draft status', type: 'string', format: 'enum', values: getDraftStatuses(), mandatoryType: 'internal', editDefault: false, multiple: false, upsert: true, isFilterable: true },
     { name: 'validation_work_id', label: 'Validation work', type: 'string', format: 'id', entityTypes: [ENTITY_TYPE_WORK], mandatoryType: 'internal', editDefault: false, multiple: false, upsert: true, isFilterable: false },
     { ...draftChange, isFilterable: false },
     { ...creators },
     { ...authorizedMembers, editDefault: true },
     { ...authorizedMembersActivationDate },
+    { ...updatedAt, isFilterable: false },
+    { ...refreshedAt, isFilterable: false },
   ],
   relations: [],
   relationsRefs: [createdBy, objectAssignee, objectParticipant],
