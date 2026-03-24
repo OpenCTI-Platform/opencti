b5a79d8e3ab8be340395417ab67f4a4674ccbac0
Florian Delemarre <florian.delemarre@gmail.com>
2026-03-24 10:48:19 +0100

    [frontend] fix hook usage

--- opencti-platform/opencti-front/src/private/components/common/stix_core_objects/StixCoreObjectContent.jsx
+++ opencti-platform/opencti-front/src/private/components/common/stix_core_objects/StixCoreObjectContent.jsx
@@ -26,7 +26,7 @@ import withRouter from '../../../../utils/compat_router/withRouter';
 import CKEditor from '../../../../components/CKEditor';
 import { htmlToPdf } from '../../../../utils/htmlToPdf/htmlToPdf';
 import HtmlDisplay from '../../../../components/HtmlDisplay';
-import { typesWithFintelTemplates } from '../../../../utils/hooks/useAttributes';
+import useAttributes from '../../../../utils/hooks/useAttributes';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `${APP_BASE_PATH}/static/ext/pdf.worker.mjs`;
 
@@ -465,7 +465,7 @@ class StixCoreObjectContentComponent extends Component {
   }
 
   render() {
-    const { classes, stixCoreObject, t, currentMode } = this.props;
+    const { classes, stixCoreObject, t, currentMode, typesWithFintelTemplates } = this.props;
     const {
       currentFileId,
       totalPdfPageNumber,
@@ -487,7 +487,7 @@ class StixCoreObjectContentComponent extends Component {
     const { innerHeight } = window;
     const height = innerHeight - 320;
     const isContentCompatible = isContainerWithContent(stixCoreObject.entity_type);
-    const hasFintelTemplates = typesWithFintelTemplates.includes(stixCoreObject.entity_type);
+    const hasFintelTemplates = (typesWithFintelTemplates ?? []).includes(stixCoreObject.entity_type);
     return (
       <div className={classes.container} data-testid="sco-content-page">
         <StixCoreObjectContentFiles
@@ -746,6 +746,7 @@ StixCoreObjectContentComponent.propTypes = {
   classes: PropTypes.object,
   t: PropTypes.func,
   currentMode: PropTypes.string,
+  typesWithFintelTemplates: PropTypes.array,
 };
 
 export const stixCoreObjectContentRefetchQuery = graphql`
@@ -934,9 +935,19 @@ const StixCoreObjectContent = createRefetchContainer(
   stixCoreObjectContentRefetchQuery,
 );
 
+const withAttributes = (Component) => {
+  const WithAttributes = (props) => {
+    const { typesWithFintelTemplates } = useAttributes();
+    return <Component {...props} typesWithFintelTemplates={typesWithFintelTemplates} />;
+  };
+  WithAttributes.displayName = `WithAttributes(${Component.displayName || Component.name || 'Component'})`;
+  return WithAttributes;
+};
+
 export default R.compose(
   inject18n,
   withTheme,
   withRouter,
   withStyles(styles),
+  withAttributes,
 )(StixCoreObjectContent);
