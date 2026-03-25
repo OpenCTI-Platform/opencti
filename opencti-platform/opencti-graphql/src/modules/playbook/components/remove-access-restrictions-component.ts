fcf7ba215bb09d774ae2561e7d814f03caa96c2e
Landry Trebon <33682259+lndrtrbn@users.noreply.github.com>
2026-03-25 16:47:12 +0100

    [backend] Playbook - Bundle scope: component Remove access restrictions (#14283)

--- opencti-platform/opencti-graphql/src/modules/playbook/components/remove-access-restrictions-component.ts
+++ opencti-platform/opencti-graphql/src/modules/playbook/components/remove-access-restrictions-component.ts
@@ -1,6 +1,6 @@
 import type { JSONSchemaType } from 'ajv';
 import * as jsonpatch from 'fast-json-patch';
-import { type PlaybookComponent } from '../playbook-types';
+import { playbookBundleElementsToApply, type PlaybookBundleElementsToApply, type PlaybookComponent } from '../playbook-types';
 import { INPUT_AUTHORIZED_MEMBERS } from '../../../schema/general';
 import { generateInternalType } from '../../../schema/schemaUtils';
 import { STIX_EXT_OCTI } from '../../../types/stix-2-1-extensions';
@@ -10,14 +10,23 @@ import { AUTHORIZED_MEMBERS_SUPPORTED_ENTITY_TYPES } from '../../../utils/author
 import { applyOperationFieldPatch } from '../playbook-utils';
 
 export interface RemoveAccessRestrictionsConfiguration {
-  all: boolean;
+  applyToElements: PlaybookBundleElementsToApply;
 }
 const PLAYBOOK_REMOVE_ACCESS_RESTRICTIONS_COMPONENT_SCHEMA: JSONSchemaType<RemoveAccessRestrictionsConfiguration> = {
   type: 'object',
   properties: {
-    all: { type: 'boolean', $ref: 'Remove access restrictions on all elements included in the bundle', default: false },
+    applyToElements: {
+      type: 'string',
+      default: playbookBundleElementsToApply.onlyMain.value,
+      $ref: 'Apply to',
+      oneOf: [
+        { const: playbookBundleElementsToApply.onlyMain.value, title: playbookBundleElementsToApply.onlyMain.title },
+        { const: playbookBundleElementsToApply.allElements.value, title: playbookBundleElementsToApply.allElements.title },
+        { const: playbookBundleElementsToApply.allExceptMain.value, title: playbookBundleElementsToApply.allExceptMain.title },
+      ],
+    },
   },
-  required: [],
+  required: ['applyToElements'],
 };
 export const PLAYBOOK_REMOVE_ACCESS_RESTRICTIONS_COMPONENT: PlaybookComponent<RemoveAccessRestrictionsConfiguration> = {
   id: 'PLAYBOOK_REMOVE_ACCESS_RESTRICTIONS_COMPONENT',
@@ -30,12 +39,16 @@ export const PLAYBOOK_REMOVE_ACCESS_RESTRICTIONS_COMPONENT: PlaybookComponent<Re
   configuration_schema: PLAYBOOK_REMOVE_ACCESS_RESTRICTIONS_COMPONENT_SCHEMA,
   schema: async () => PLAYBOOK_REMOVE_ACCESS_RESTRICTIONS_COMPONENT_SCHEMA,
   executor: async ({ dataInstanceId, playbookNode, bundle }) => {
-    const { all } = playbookNode.configuration;
+    const { applyToElements } = playbookNode.configuration;
     const patchOperations = [];
     for (let index = 0; index < bundle.objects.length; index += 1) {
       const element = bundle.objects[index];
       const internalType = generateInternalType(element);
-      if (AUTHORIZED_MEMBERS_SUPPORTED_ENTITY_TYPES.includes(internalType) && (all || element.id === dataInstanceId)) {
+      const all = applyToElements === playbookBundleElementsToApply.allElements.value;
+      const onlyMain = applyToElements === playbookBundleElementsToApply.onlyMain.value && element.id === dataInstanceId;
+      const exceptMain = applyToElements === playbookBundleElementsToApply.allExceptMain.value && element.id !== dataInstanceId;
+      const shouldTakeObject = all || onlyMain || exceptMain;
+      if (AUTHORIZED_MEMBERS_SUPPORTED_ENTITY_TYPES.includes(internalType) && shouldTakeObject) {
         const patchValue = {
           op: EditOperation.Replace,
           path: `/objects/${index}/extensions/${STIX_EXT_OCTI}/restricted_members`,
