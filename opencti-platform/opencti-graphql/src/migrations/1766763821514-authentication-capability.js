d54c21dec2bb960e8a5687fb5f14e915d316e7d3
Souad Hadjiat <souad.hadjiat@filigran.io>
2026-03-26 11:47:33 +0100

    [backend] use logMigration for all recent migrations (#15195)

--- opencti-platform/opencti-graphql/src/migrations/1766763821514-authentication-capability.js
+++ opencti-platform/opencti-graphql/src/migrations/1766763821514-authentication-capability.js
@@ -1,19 +1,19 @@
 import { addCapability } from '../domain/grant';
-import { logApp } from '../config/conf';
+import { logMigration } from '../config/conf';
 import { executionContext, SYSTEM_USER } from '../utils/access';
 
 const message = '[MIGRATION] add the new SETAUTH capability';
 
 export const up = async (next) => {
+  logMigration.info(`${message} > started`);
   const context = executionContext('migration');
-
   await addCapability(context, SYSTEM_USER, {
     name: 'SETTINGS_SETAUTH',
     description: 'Manage authentication',
     attribute_order: 3220,
   });
 
-  logApp.info(`${message} > done`);
+  logMigration.info(`${message} > done`);
   next();
 };
 
