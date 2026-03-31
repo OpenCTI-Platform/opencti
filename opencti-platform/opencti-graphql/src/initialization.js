e1963424284e7f1ec9a3edad5d8404cd0556ed9e
Angelique Jard <angelique.jard@filigran.io>
2026-03-31 13:03:51 +0200

    Testing new codecov config

--- opencti-platform/opencti-graphql/src/initialization.js
+++ opencti-platform/opencti-graphql/src/initialization.js
@@ -39,26 +39,15 @@ export const checkFeatureFlags = () => {
 // Check every dependency
 export const checkSystemDependencies = async () => {
   logApp.info('[OPENCTI] Checking dependencies statuses');
-  // Check if elasticsearch is available
-  logApp.info('[CHECK] checking if Search engine is alive');
-  await searchEngineInit();
-  // Check if minio is here
-  logApp.info('[CHECK] Search engine ok, checking if File storage is alive');
-  await storageInit();
-  // Check if RabbitMQ is here and create the logs exchange/queue
-  logApp.info('[CHECK] File storage ok, checking if RabbitMQ is alive');
-  await rabbitMQIsAlive();
-  logApp.info('[CHECK] RabbitMQ ok, checking if Redis is alive');
-  // Check if redis is here
-  await redisInit();
-  logApp.info('[CHECK] Redis ok, checking if SMTP is alive');
-  // Check if SMTP is here
-  await smtpIsAlive();
-  // Check if Python is available
-  logApp.info('[CHECK] SMTP done, checking if python is available');
   const context = executionContext('system_dependencies');
-  await checkPythonAvailability(context, SYSTEM_USER);
-  logApp.info('[CHECK] Python3 is available');
+  const checkDependenciesPromises = [];
+  checkDependenciesPromises.push(searchEngineInit());
+  checkDependenciesPromises.push(storageInit());
+  checkDependenciesPromises.push(rabbitMQIsAlive());
+  checkDependenciesPromises.push(redisInit());
+  checkDependenciesPromises.push(smtpIsAlive());
+  checkDependenciesPromises.push(checkPythonAvailability(context, SYSTEM_USER));
+  await Promise.all(checkDependenciesPromises);
   return true;
 };
 
