a114e29460f013225714e44d35fa93d1c6f17c88
Jeremy Cloarec <jeremy.cloarec@filigran.io>
2026-04-01 16:09:01 +0200

    [backend] fix

--- opencti-platform/opencti-graphql/src/manager/taskManager.js
+++ opencti-platform/opencti-graphql/src/manager/taskManager.js
@@ -264,7 +264,7 @@ const sendResultToQueue = async (context, user, task, objects, opts = {}) => {
     await buildAndSendBundle(context, user, task, objects, opts);
   } else {
     for (let index = 0; index < objects.length; index += 1) {
-      const splitObject = objects[index]fi;
+      const splitObject = objects[index];
       await buildAndSendBundle(context, user, task, [splitObject], opts);
     }
   }
