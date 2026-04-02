76fed33e0327e2e5b925cd7a6f3a2be89bb23370
Marie Torbay <marie.torbay@filigran.io>
2026-04-02 12:01:14 +0200

    [backend] Playbook - Bundle scope: Component extract observables from indicators (#14283)

--- opencti-platform/opencti-graphql/src/modules/playbook/components/create-observable-component.ts
+++ opencti-platform/opencti-graphql/src/modules/playbook/components/create-observable-component.ts
@@ -1,6 +1,6 @@
 import * as R from 'ramda';
 import type { JSONSchemaType } from 'ajv';
-import { type PlaybookComponent } from '../playbook-types';
+import { playbookBundleElementsToApply, type PlaybookBundleElementsToApply, type PlaybookComponent } from '../playbook-types';
 import type { StoreCommon } from '../../../types/store';
 import { generateInternalId, generateStandardId, idGenFromData } from '../../../schema/identifier';
 import { now } from '../../../utils/format';
@@ -13,21 +13,30 @@ import { RELATION_BASED_ON } from '../../../schema/stixCoreRelationship';
 import type { StixRelation } from '../../../types/stix-2-1-sro';
 import { extractValidObservablesFromIndicatorPattern } from '../../../utils/syntax';
 import { type StixIndicator } from '../../indicator/indicator-types';
-import { extractBundleBaseElement } from '../playbook-utils';
+import { extractBundleBaseElement, isBundleElementInScope } from '../playbook-utils';
 import { convertStoreToStix_2_1 } from '../../../database/stix-2-1-converter';
 import { pushAll } from '../../../utils/arrayUtil';
 
 interface CreateObservableConfiguration {
-  all: boolean;
+  applyToElements: PlaybookBundleElementsToApply;
   wrap_in_container: boolean;
 }
 const PLAYBOOK_CREATE_OBSERVABLE_COMPONENT_SCHEMA: JSONSchemaType<CreateObservableConfiguration> = {
   type: 'object',
   properties: {
-    all: { type: 'boolean', $ref: 'Create observables from all indicators in the bundle', default: false },
+    applyToElements: {
+      type: 'string',
+      default: playbookBundleElementsToApply.onlyMain.value,
+      $ref: 'Apply to',
+      oneOf: [
+        { const: playbookBundleElementsToApply.onlyMain.value, title: playbookBundleElementsToApply.onlyMain.title },
+        { const: playbookBundleElementsToApply.allElements.value, title: playbookBundleElementsToApply.allElements.title },
+        { const: playbookBundleElementsToApply.allExceptMain.value, title: playbookBundleElementsToApply.allExceptMain.title },
+      ],
+    },
     wrap_in_container: { type: 'boolean', $ref: 'If main entity is a container, wrap observables in container', default: false },
   },
-  required: [],
+  required: ['applyToElements'],
 };
 export const PLAYBOOK_CREATE_OBSERVABLE_COMPONENT: PlaybookComponent<CreateObservableConfiguration> = {
   id: 'PLAYBOOK_CREATE_OBSERVABLE_COMPONENT',
@@ -40,17 +49,19 @@ export const PLAYBOOK_CREATE_OBSERVABLE_COMPONENT: PlaybookComponent<CreateObser
   configuration_schema: PLAYBOOK_CREATE_OBSERVABLE_COMPONENT_SCHEMA,
   schema: async () => PLAYBOOK_CREATE_OBSERVABLE_COMPONENT_SCHEMA,
   executor: async ({ playbookNode, dataInstanceId, bundle }) => {
-    const { all, wrap_in_container } = playbookNode.configuration;
+    const { applyToElements, wrap_in_container } = playbookNode.configuration;
     const baseData = extractBundleBaseElement(dataInstanceId, bundle);
-    const indicators = [baseData];
-    if (all) {
-      pushAll(indicators, bundle.objects);
-    }
+    // const indicators = [baseData];
+    // if (all) {
+    //   pushAll(indicators, bundle.objects.filter((object) => object.id !== dataInstanceId));
+    // }
+
+    const elementsToApply = bundle.objects.filter((object) => isBundleElementInScope(object, applyToElements, dataInstanceId));
     const { type: baseDataType } = baseData.extensions[STIX_EXT_OCTI];
     const isBaseDataAContainer = isStixDomainObjectContainer(baseDataType);
     const objectsToPush: StixObject[] = [];
-    for (let indexIndicator = 0; indexIndicator < indicators.length; indexIndicator += 1) {
-      const indicator = indicators[indexIndicator] as StixIndicator;
+    for (let indexIndicator = 0; indexIndicator < elementsToApply.length; indexIndicator += 1) {
+      const indicator = elementsToApply[indexIndicator] as StixIndicator;
       if (indicator.type === 'indicator') {
         const observables = extractValidObservablesFromIndicatorPattern(indicator.pattern);
         for (let indexObservable = 0; indexObservable < observables.length; indexObservable += 1) {
