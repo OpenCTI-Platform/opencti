69309cd63b1f65c2e35f5a6354cad8cb5bae7193
Celine Sebe <celine.sebe@filigran.io>
2026-04-14 14:28:32 +0200

    [frontend] revert changes

--- opencti-platform/opencti-front/src/public/workers/graphql.worker.js
+++ opencti-platform/opencti-front/src/public/workers/graphql.worker.js
@@ -1,5 +1,5 @@
 // Re-export GraphQL language worker for Monaco.
 // This wrapper exists so that Vite's worker bundling treats this as a project file
 // (fully controlled bundling) rather than a bare node_modules reference.
-
+// eslint-disable-next-line import/no-extraneous-dependencies
 import 'monaco-graphql/esm/graphql.worker.js';
