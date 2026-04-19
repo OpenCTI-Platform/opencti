a18900468991632668c319f6e7ba332d247bd652
Sébastien Wauquier <sebastien.wauquier@filigran.io>
2026-04-19 02:25:00 +0200

    [frontend] Add a custom view

---
+++ opencti-platform/opencti-front/src/private/components/settings/sub_types/custom_views/useCustomViewAdd.ts
@@ -0,0 +1,42 @@
+import { graphql } from 'react-relay';
+import { useState } from 'react';
+import useApiMutation from '../../../../../utils/hooks/useApiMutation';
+import { useCustomViewAdd_Mutation } from './__generated__/useCustomViewAdd_Mutation.graphql';
+import { useCustomViewsData } from '../../../custom_views/useCustomViews';
+
+const customViewAddMutation = graphql`
+  mutation useCustomViewAdd_Mutation($input: CustomViewAddInput!) {
+    customViewAdd(input: $input) {
+      id
+    }
+  }
+`;
+
+/**
+ * Hook handling Custom view creation logic
+ */
+const useCustomViewAdd = () => {
+  const [mutating, setMutating] = useState(false);
+  const [commitAddMutation] = useApiMutation<useCustomViewAdd_Mutation>(customViewAddMutation);
+  const { refetchCustomViews } = useCustomViewsData();
+
+  const mutation: typeof commitAddMutation = ({ variables, onCompleted, onError }) => {
+    setMutating(true);
+    commitAddMutation({
+      variables,
+      onError: (error) => {
+        setMutating(false);
+        onError?.(error);
+      },
+      onCompleted: (...args) => {
+        setMutating(false);
+        onCompleted?.(...args);
+        refetchCustomViews();
+      },
+    });
+  };
+
+  return [mutation, mutating] as const;
+};
+
+export default useCustomViewAdd;
