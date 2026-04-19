112d58fff46a9c55c99d2e0e4c4583a4ab119446
Sébastien Wauquier <sebastien.wauquier@filigran.io>
2026-04-20 01:23:03 +0200

    [frontend] Duplicate a custom view

---
+++ opencti-platform/opencti-front/src/private/components/settings/sub_types/custom_views/CustomViewDuplicationDialog.tsx
@@ -0,0 +1,140 @@
+import { FunctionComponent, UIEvent, useMemo, useState } from 'react';
+import { graphql, useFragment } from 'react-relay';
+import { Link } from 'react-router-dom';
+import Button from '@common/button/Button';
+import Dialog from '@common/dialog/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import TextField from '@mui/material/TextField';
+import { useFormatter } from '../../../../../components/i18n';
+import { handleError, MESSAGING$ } from '../../../../../relay/environment';
+import stopEvent from '../../../../../utils/domEvent';
+import useApiMutation from '../../../../../utils/hooks/useApiMutation';
+import { useCustomViewsData } from '../../../custom_views/useCustomViewsData';
+import { CustomViewDuplicationDialog_DuplicateMutation } from './__generated__/CustomViewDuplicationDialog_DuplicateMutation.graphql';
+import { CustomViewDuplicationDialog_Fragment$data, CustomViewDuplicationDialog_Fragment$key } from './__generated__/CustomViewDuplicationDialog_Fragment.graphql';
+
+const customViewDuplicationFragment = graphql`
+  fragment CustomViewDuplicationDialog_Fragment on CustomView {
+    name
+    description
+    manifest
+    targetEntityType
+  }
+`;
+
+interface CustomViewDuplicationDialogProps {
+  data: CustomViewDuplicationDialog_Fragment$key;
+  displayDuplicate: boolean;
+  duplicating: boolean;
+  handleCloseDuplicate: () => void;
+  setDuplicating: (value: boolean) => void;
+}
+
+const duplicateMutation = graphql`
+  mutation CustomViewDuplicationDialog_DuplicateMutation(
+    $input: CustomViewDuplicateInput!
+  ) {
+    customViewDuplicate(input: $input) {
+      id
+      targetEntityType
+    }
+  }
+`;
+const CustomViewDuplicationDialog: FunctionComponent<
+  CustomViewDuplicationDialogProps
+> = ({
+  data,
+  duplicating,
+  setDuplicating,
+  displayDuplicate,
+  handleCloseDuplicate,
+}) => {
+  const { t_i18n } = useFormatter();
+  const customView = useFragment(customViewDuplicationFragment, data);
+  const { refetchCustomViews } = useCustomViewsData();
+
+  const duplicatedCustomViewInitialName = useMemo(
+    () => `${customView.name} - ${t_i18n('copy')}`,
+    [t_i18n, customView.name],
+  );
+  const [newName, setNewName] = useState(duplicatedCustomViewInitialName);
+  const [commitDuplicateCustomView] = useApiMutation<CustomViewDuplicationDialog_DuplicateMutation>(
+    duplicateMutation,
+  );
+  const submitDashboardDuplication = (
+    e: UIEvent,
+    sourceCustomView: CustomViewDuplicationDialog_Fragment$data,
+  ) => {
+    stopEvent(e);
+    commitDuplicateCustomView({
+      variables: {
+        input: {
+          name: sourceCustomView.name,
+          description: sourceCustomView.description,
+          manifest: sourceCustomView.manifest,
+          target_entity_type: sourceCustomView.target_entity_type,
+        },
+      },
+      onError: (error) => {
+        handleError(error);
+      },
+      onCompleted: (result) => {
+        handleCloseDuplicate();
+        refetchCustomViews();
+        MESSAGING$.notifySuccess(
+          <span>
+            {t_i18n('The custom view has been duplicated. You can manage it')}{' '}
+            <Link
+              to={`/dashboard/settings/customization/entity_types/${result.customViewDuplicate?.target_entity_type}/custom-views/${result.customViewDuplicate?.id}`}
+            >
+              {t_i18n('here')}
+            </Link>
+            .
+          </span>,
+        );
+      },
+    });
+  };
+
+  const handleSubmitDuplicate = (e: UIEvent, submittedNewName: string) => {
+    setDuplicating(true);
+    submitDashboardDuplication(e, { ...customView, name: submittedNewName });
+  };
+
+  return (
+    <Dialog
+      open={displayDuplicate}
+      onClose={handleCloseDuplicate}
+      fullWidth={true}
+      title={t_i18n('Duplicate the custom view')}
+    >
+      <TextField
+        error={!newName}
+        autoFocus
+        margin="dense"
+        id="duplicated_dashboard_name"
+        label={t_i18n('New name')}
+        type="text"
+        fullWidth
+        variant="standard"
+        helperText={!newName ? `${t_i18n('This field is required')}` : ''}
+        defaultValue={newName}
+        onChange={(event) => {
+          event.preventDefault();
+          setNewName(event.target.value);
+        }}
+      />
+      <DialogActions>
+        <Button variant="secondary" onClick={() => handleCloseDuplicate()}>{t_i18n('Cancel')}</Button>
+        <Button
+          onClick={(e) => handleSubmitDuplicate(e, newName)}
+          disabled={duplicating || !newName}
+        >
+          {t_i18n('Duplicate')}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default CustomViewDuplicationDialog;
---
+++ opencti-platform/opencti-front/src/private/components/settings/sub_types/custom_views/CustomViewKebabMenu.tsx
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+import { graphql, useFragment } from 'react-relay';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MoreVert from '@mui/icons-material/MoreVert';
+import ToggleButton from '@mui/material/ToggleButton';
+import { useFormatter } from '../../../../../components/i18n';
+import CustomViewDuplicationDialog from './CustomViewDuplicationDialog';
+import type { CustomViewKebabMenu_customView$key } from './__generated__/CustomViewKebabMenu_customView.graphql';
+
+const kebabMenuFragment = graphql`
+  fragment CustomViewKebabMenu_customView on CustomView {
+    id
+    name
+    targetEntityType
+    ...CustomViewDuplicationDialog_Fragment
+  }
+`;
+
+interface CustomViewKebabMenuProps {
+  data: CustomViewKebabMenu_customView$key;
+}
+
+const noop = () => {};
+
+const useDuplicate = (onDuplicate = noop) => {
+  const [displayDuplicate, setDisplayDuplicate] = useState(false);
+  const handleCloseDuplicate = () => setDisplayDuplicate(false);
+  const [duplicating, setDuplicating] = useState(false);
+  const handleDuplication = () => {
+    onDuplicate();
+    setDisplayDuplicate(true);
+  };
+
+  return {
+    displayDuplicate,
+    setDisplayDuplicate,
+    handleCloseDuplicate,
+    duplicating,
+    setDuplicating,
+    handleDuplication,
+  };
+};
+
+const CustomViewKebabMenu = ({ data }: CustomViewKebabMenuProps) => {
+  const customView = useFragment(kebabMenuFragment, data);
+  const { t_i18n } = useFormatter();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const {
+    displayDuplicate,
+    duplicating,
+    setDuplicating,
+    handleDuplication,
+    handleCloseDuplicate,
+  } = useDuplicate(handleClose);
+  return (
+    <div>
+      <ToggleButton
+        aria-label={t_i18n('Popover of custom view actions')}
+        value="popover"
+        size="small"
+        color="primary"
+        id="custom-view-kebab-button"
+        aria-controls={open ? 'custom-view-kebab-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        sx={{ height: '100%' }}
+      >
+        <MoreVert color="primary" fontSize="small" />
+      </ToggleButton>
+      <Menu
+        id="custom-view-kebab-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        slotProps={{
+          list: {
+            'aria-labelledby': 'workspace-kebab-button',
+          },
+        }}
+      >
+        <MenuItem onClick={handleDuplication}>{t_i18n('Duplicate the custom view')}</MenuItem>
+      </Menu>
+      <CustomViewDuplicationDialog
+        data={customView}
+        displayDuplicate={displayDuplicate}
+        handleCloseDuplicate={handleCloseDuplicate}
+        duplicating={duplicating}
+        setDuplicating={setDuplicating}
+      />
+    </div>
+  );
+};
+
+export default CustomViewKebabMenu;
