c4f52ca4abe5ecd0143552e1e3ccc92753cc1c04
Tiffaine HERVY <tiffaine.hervy@filigran.io>
2026-04-22 16:00:06 +0200

    [frontend] - feat(Registration): Fix modal display #15639

--- opencti-platform/opencti-front/src/private/components/settings/xtm-hub/ProcessLoader.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/xtm-hub/ProcessLoader.tsx
@@ -15,10 +15,12 @@ const ProcessLoader: FunctionComponent<ProcessLoaderProps> = ({
   return (
     <Box
       sx={{
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
+        height: '100%',
+        minHeight: 100,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
         zIndex: 1,
       }}
     >
