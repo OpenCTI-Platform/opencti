b962d96f37c4bd5ed6935b762a468cfb2c48caf0
Gwendoline Favre-Felix <gwendoline.favre-felix@filigran.io>
2026-04-22 16:23:30 +0200

    [backend/frontend] WIP dashboard auto-reload and donut widget refetch (#14448)
    # Conflicts:
    #	opencti-platform/opencti-front/src/components/dashboard/DashboardEntitiesViz.tsx
    #	opencti-platform/opencti-front/src/private/components/common/stix_core_objects/StixCoreObjectsDonut.jsx
    #	opencti-platform/opencti-front/src/private/components/workspaces/WorkspaceCreation.tsx
    #	opencti-platform/opencti-front/src/private/components/workspaces/dashboards/Dashboard.jsx

--- opencti-platform/opencti-graphql/src/generated/graphql.ts
+++ opencti-platform/opencti-graphql/src/generated/graphql.ts
@@ -37873,6 +37873,7 @@ export type Workspace = BasicObject & InternalObject & {
   objects?: Maybe<StixObjectOrStixRelationshipRefConnection>;
   owner?: Maybe<Creator>;
   parent_types: Array<Scalars['String']['output']>;
+  refresh_rate?: Maybe<Scalars['Int']['output']>;
   refreshed_at?: Maybe<Scalars['DateTime']['output']>;
   standard_id: Scalars['String']['output'];
   tags?: Maybe<Array<Scalars['String']['output']>>;
@@ -37905,6 +37906,7 @@ export type WorkspaceAddInput = {
   description?: InputMaybe<Scalars['String']['input']>;
   investigated_entities_ids?: InputMaybe<Array<InputMaybe<Scalars['String']['input']>>>;
   name: Scalars['String']['input'];
+  refresh_rate?: InputMaybe<Scalars['Int']['input']>;
   tags?: InputMaybe<Array<Scalars['String']['input']>>;
   type: Scalars['String']['input'];
 };
@@ -52526,6 +52528,7 @@ export type WorkspaceResolvers<ContextType = any, ParentType extends ResolversPa
   objects?: Resolver<Maybe<ResolversTypes['StixObjectOrStixRelationshipRefConnection']>, ParentType, ContextType, Partial<WorkspaceObjectsArgs>>;
   owner?: Resolver<Maybe<ResolversTypes['Creator']>, ParentType, ContextType>;
   parent_types?: Resolver<Array<ResolversTypes['String']>, ParentType, ContextType>;
+  refresh_rate?: Resolver<Maybe<ResolversTypes['Int']>, ParentType, ContextType>;
   refreshed_at?: Resolver<Maybe<ResolversTypes['DateTime']>, ParentType, ContextType>;
   standard_id?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   tags?: Resolver<Maybe<Array<ResolversTypes['String']>>, ParentType, ContextType>;
