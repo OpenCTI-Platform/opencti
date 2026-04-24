ade38de691ceb5bdf271310b22735f0b0965d451
Sébastien Wauquier <sebastien.wauquier@filigran.io>
2026-04-24 14:53:31 +0200

    Remove useless Grid usage

--- opencti-platform/opencti-front/src/private/components/settings/sub_types/custom_views/CustomViewsSettings.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/sub_types/custom_views/CustomViewsSettings.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import Grid from '@mui/material/Grid';
 import Card from '@common/card/Card';
 import Tooltip from '@mui/material/Tooltip';
 import { Add as AddIcon } from '@mui/icons-material';
@@ -19,28 +18,26 @@ const CustomViewsSettings = () => {
 
   return (
     <>
-      <Grid item xs={12}>
-        <Card
-          title={t_i18n('Custom Views')}
-          sx={{
-          // Compensate existing top padding from data table header
-          //  to avoid the feeling of having too much empty space.
-            pt: 2,
-          }}
-          action={(
-            <Tooltip title={t_i18n('Create a new custom view')}>
-              <IconButton
-                onClick={() => setDrawerOpen(true)}
-                size="small"
-              >
-                <AddIcon fontSize="small" color="primary" />
-              </IconButton>
-            </Tooltip>
-          )}
-        >
-          <CustomViewsSettingsDataTable targetType={subType.id} />
-        </Card>
-      </Grid>
+      <Card
+        title={t_i18n('Custom Views')}
+        sx={{
+        // Compensate existing top padding from data table header
+        //  to avoid the feeling of having too much empty space.
+          pt: 2,
+        }}
+        action={(
+          <Tooltip title={t_i18n('Create a new custom view')}>
+            <IconButton
+              onClick={() => setDrawerOpen(true)}
+              size="small"
+            >
+              <AddIcon fontSize="small" color="primary" />
+            </IconButton>
+          </Tooltip>
+        )}
+      >
+        <CustomViewsSettingsDataTable targetType={subType.id} />
+      </Card>
       <CustomViewFormDrawer
         isOpen={isDrawerOpen}
         entityType={subType.id}
