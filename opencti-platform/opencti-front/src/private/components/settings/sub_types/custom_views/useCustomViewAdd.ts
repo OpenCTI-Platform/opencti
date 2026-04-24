d19fe9604e7bb482d30a595933510aee3fbe41b9
Sébastien Wauquier <sebastien.wauquier@filigran.io>
2026-04-24 14:53:15 +0200

    Remove useless mutating state

--- opencti-platform/opencti-front/src/private/components/settings/sub_types/custom_views/useCustomViewAdd.ts
+++ opencti-platform/opencti-front/src/private/components/settings/sub_types/custom_views/useCustomViewAdd.ts
@@ -1,5 +1,4 @@
 import { graphql } from 'react-relay';
-import { useState } from 'react';
 import useApiMutation from '../../../../../utils/hooks/useApiMutation';
 import { useCustomViewAdd_Mutation } from './__generated__/useCustomViewAdd_Mutation.graphql';
 import { useCustomViewsData } from '../../../custom_views/useCustomViewsData';
@@ -16,27 +15,21 @@ const customViewAddMutation = graphql`
  * Hook handling Custom view creation logic
  */
 const useCustomViewAdd = () => {
-  const [mutating, setMutating] = useState(false);
   const [commitAddMutation] = useApiMutation<useCustomViewAdd_Mutation>(customViewAddMutation);
   const { refetchCustomViews } = useCustomViewsData();
-
   const mutation: typeof commitAddMutation = ({ variables, onCompleted, onError }) => {
-    setMutating(true);
     commitAddMutation({
       variables,
       onError: (error) => {
-        setMutating(false);
         onError?.(error);
       },
       onCompleted: (...args) => {
-        setMutating(false);
         onCompleted?.(...args);
         refetchCustomViews();
       },
     });
   };
-
-  return [mutation, mutating] as const;
+  return mutation;
 };
 
 export default useCustomViewAdd;
