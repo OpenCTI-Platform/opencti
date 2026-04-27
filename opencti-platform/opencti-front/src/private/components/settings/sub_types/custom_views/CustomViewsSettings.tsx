3fa4e32fc87c33579df5ab47e659a0e5508bc47c
Sébastien Wauquier <sebastien.wauquier@filigran.io>
2026-04-27 17:09:22 +0200

    [frontend][backend] Add a custom view (#13389)

--- opencti-platform/opencti-front/src/private/components/settings/sub_types/custom_views/CustomViewsSettings.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/sub_types/custom_views/CustomViewsSettings.tsx
@@ -1,8 +1,12 @@
-import Grid from '@mui/material/Grid';
+import { useState } from 'react';
 import Card from '@common/card/Card';
+import Tooltip from '@mui/material/Tooltip';
+import { Add as AddIcon } from '@mui/icons-material';
+import IconButton from '@common/button/IconButton';
 import { useFormatter } from '../../../../../components/i18n';
 import { useSubTypeOutletContext } from '../SubTypeOutletContext';
 import CustomViewsSettingsDataTable from './CustomViewsSettingsDataTable';
+import CustomViewFormDrawer from './CustomViewFormDrawer';
 
 /**
  * Custom Views settings page.
@@ -10,20 +14,38 @@ import CustomViewsSettingsDataTable from './CustomViewsSettingsDataTable';
 const CustomViewsSettings = () => {
   const { t_i18n } = useFormatter();
   const { subType } = useSubTypeOutletContext();
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
 
   return (
-    <Grid item xs={12}>
+    <>
       <Card
         title={t_i18n('Custom Views')}
         sx={{
-          // Compensate existing top padding from data table header
-          //  to avoid the feeling of having too much empty space.
+        // Compensate existing top padding from data table header
+        //  to avoid the feeling of having too much empty space.
           pt: 2,
         }}
+        action={(
+          <Tooltip title={t_i18n('Create a new custom view')}>
+            <IconButton
+              onClick={() => setDrawerOpen(true)}
+              size="small"
+            >
+              <AddIcon fontSize="small" color="primary" />
+            </IconButton>
+          </Tooltip>
+        )}
       >
         <CustomViewsSettingsDataTable targetType={subType.id} />
       </Card>
-    </Grid>
+      <CustomViewFormDrawer
+        isOpen={isDrawerOpen}
+        entityType={subType.id}
+        onClose={() => {
+          setDrawerOpen(false);
+        }}
+      />
+    </>
   );
 };
 
