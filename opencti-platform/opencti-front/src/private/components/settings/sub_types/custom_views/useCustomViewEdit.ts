bda875a9cea0a033bcc3fbe80eab30dced7b4827
Sébastien Wauquier <sebastien.wauquier@filigran.io>
2026-04-29 16:44:21 +0200

    [frontend][backend] Edit custom views (#13389)

---
+++ opencti-platform/opencti-front/src/private/components/settings/sub_types/custom_views/useCustomViewEdit.ts
@@ -0,0 +1,46 @@
+import { graphql } from 'react-relay';
+import { useState } from 'react';
+import useApiMutation from '../../../../../utils/hooks/useApiMutation';
+import { useCustomViewEdit_Mutation } from './__generated__/useCustomViewEdit_Mutation.graphql';
+import { useCustomViewsData } from '@components/custom_views/useCustomViewsData';
+
+const customViewEditMutation = graphql`
+  mutation useCustomViewEdit_Mutation($id: ID!, $input: [EditInput!]!) {
+    customViewEdit(id: $id, input: $input) {
+      id
+      name
+      description
+      path
+    }
+  }
+`;
+
+/**
+ * Hook handling Custom view edition.
+ * To edit dashboard-related content use useCustomViewDashboardEdit.
+ */
+const useCustomViewEdit = () => {
+  const [mutating, setMutating] = useState(false);
+  const [commitEditMutation] = useApiMutation<useCustomViewEdit_Mutation>(customViewEditMutation);
+  const { refetchCustomViews } = useCustomViewsData();
+
+  const mutation: typeof commitEditMutation = ({ variables, onCompleted, onError }) => {
+    setMutating(true);
+    commitEditMutation({
+      variables,
+      onError: (error) => {
+        setMutating(false);
+        onError?.(error);
+      },
+      onCompleted: (...args) => {
+        setMutating(false);
+        onCompleted?.(...args);
+        refetchCustomViews();
+      },
+    });
+  };
+
+  return [mutation, mutating] as const;
+};
+
+export default useCustomViewEdit;
