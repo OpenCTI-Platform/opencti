ad0aa2f8dc6afc7b66e0927902560d3096f32e60
Sébastien Wauquier <sebastien.wauquier@filigran.io>
2026-05-05 11:36:52 +0200

    Address PR comment https://github.com/OpenCTI-Platform/opencti/pull/15823/changes#r3186991120

--- opencti-platform/opencti-front/src/components/SlugRedirectHandler.tsx
+++ opencti-platform/opencti-front/src/components/SlugRedirectHandler.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 
 const EXTRACT_UUID_FROM_SEGMENT = (segment: string) => {
-  const results = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.exec(segment);
+  const results = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.exec(segment);
   if (results && results[0]) {
     return results[0];
   }
