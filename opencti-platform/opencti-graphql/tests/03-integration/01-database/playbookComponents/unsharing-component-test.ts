c3ee332f108bdcd1d17808e60a6d2f6797a14d01
Jeremy Cloarec <jeremy.cloarec@filigran.io>
2026-05-06 16:33:55 +0200

    [backend] test fix

--- opencti-platform/opencti-graphql/tests/03-integration/01-database/playbookComponents/unsharing-component-test.ts
+++ opencti-platform/opencti-graphql/tests/03-integration/01-database/playbookComponents/unsharing-component-test.ts
@@ -169,7 +169,7 @@ describe('PLAYBOOK_UNSHARING_COMPONENT', () => {
       const ext = result.bundle.objects[0].extensions[STIX_EXT_OCTI];
 
       // granted_refs should be removed
-      expect(ext.granted_refs).toBeUndefined();
+      expect(ext.granted_refs).toBe([]);
 
       // upsert operation should be added
       if (!ext.opencti_upsert_operations || !ext.opencti_upsert_operations[0]) {
