6ca9c46f5c84695e60c68236c59fbcb4b1f6d4a0
Sébastien Wauquier <sebastien.wauquier@filigran.io>
2026-05-07 17:13:32 +0200

    Avoid using the UserContext to host custom views preloaded data

--- opencti-platform/opencti-front/src/private/components/custom_views/useCustomViewsData.ts
+++ opencti-platform/opencti-front/src/private/components/custom_views/useCustomViewsData.ts
@@ -1,16 +1,20 @@
+import { useContext } from 'react';
 import { useRefetchableFragment } from 'react-relay';
-import useAuth from '../../../utils/hooks/useAuth';
 import type { useCustomViews_data$key } from './__generated__/useCustomViews_data.graphql';
 import type { UseCustomViewsRefetchQuery } from './__generated__/UseCustomViewsRefetchQuery.graphql';
 import { customViewsFragment } from './useCustomViews';
+import { CustomViewsPreloadedDataContext } from './CustomViewsPreloadedDataContext';
 
 export const useCustomViewsData = () => {
-  const { queryData } = useAuth();
+  const { preloadedData } = useContext(CustomViewsPreloadedDataContext);
+  if (!preloadedData) {
+    throw new Error('CustomViews preloaded data is unavailable');
+  }
   const [data, refetch] = useRefetchableFragment<
     UseCustomViewsRefetchQuery,
     useCustomViews_data$key
-  >(customViewsFragment, queryData);
-  const allCustomViews = data.customViews.edges.map((e) => e.node) ?? [];
+  >(customViewsFragment, preloadedData);
+  const allCustomViews = data?.customViews.edges.map((e) => e.node) ?? [];
   const refetchCustomViews = () => refetch({}, { fetchPolicy: 'store-and-network' });
   return { allCustomViews, refetchCustomViews };
 };
