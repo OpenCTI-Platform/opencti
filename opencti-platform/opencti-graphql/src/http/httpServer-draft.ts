bf66a6df4f9699446fec45ae2e456efeeb3f2f81
Angelique Jard <angelique.jard@filigran.io>
2026-05-11 17:00:11 +0200

    Reduce PR size and take feedbacks on cache

--- opencti-platform/opencti-graphql/src/http/httpServer-draft.ts
+++ opencti-platform/opencti-graphql/src/http/httpServer-draft.ts
@@ -1,14 +1,23 @@
 import type { AuthContext } from '../types/user';
-import { findById as findDraftById } from '../modules/draftWorkspace/draftWorkspace-domain';
 import { DraftLockedError, FunctionalError } from '../config/errors';
 import { DRAFT_STATUS_OPEN } from '../modules/draftWorkspace/draftStatuses';
 import { userEditField } from '../domain/user';
+import { ENTITY_TYPE_DRAFT_WORKSPACE, type BasicStoreEntityDraftWorkspace } from '../modules/draftWorkspace/draftWorkspace-types';
+import { getEntitiesMapFromCache } from '../database/cache';
+import { isUserCanAccessStoreElement, SYSTEM_USER } from '../utils/access';
 
 export const checkDraftInContext = async (executeContext: AuthContext) => {
   // When context is in draft, we need to check draft status: if draft is not in an open status, it means that it is no longer possible to execute requests in this draft
   if (executeContext.draft_context) {
     if (executeContext.user) {
-      const draftWorkspace = await findDraftById(executeContext, executeContext.user, executeContext.draft_context);
+      const draftWorkspaces = await getEntitiesMapFromCache(executeContext, SYSTEM_USER, ENTITY_TYPE_DRAFT_WORKSPACE);
+      const draftWorkspace: BasicStoreEntityDraftWorkspace = draftWorkspaces.get(executeContext.draft_context) as BasicStoreEntityDraftWorkspace;
+
+      const isUserCanAccess = await isUserCanAccessStoreElement(executeContext, executeContext.user, draftWorkspace);
+      if (!isUserCanAccess) {
+        throw FunctionalError(`Draft ${executeContext.draft_context} cannot be found`);
+      }
+
       if (!draftWorkspace) {
         if (executeContext.user.draft_context === executeContext.draft_context) {
           // If user is stuck in an invalid draft, remove draft context from user
