58318691602b4e99a0715f28522861f98086023f
Marie Torbay <marie.torbay@filigran.io>
2026-05-11 11:36:37 +0200

    WIP

--- opencti-platform/opencti-graphql/tests/03-integration/01-database/playbook-domain-test.ts
+++ opencti-platform/opencti-graphql/tests/03-integration/01-database/playbook-domain-test.ts
@@ -1,9 +1,8 @@
 import { afterAll, beforeEach, describe, expect, it, vi } from 'vitest';
 import { findById, playbookDelete, playbookImport } from '../../../src/modules/playbook/playbook-domain';
-import * as fileToContent from '../../../src/utils/fileToContent';
 import type { AuthContext } from '../../../src/types/user';
 import { ADMIN_USER } from '../../utils/testQuery';
-import { open, type FileHandle } from 'fs/promises';
+import { open } from 'fs/promises';
 import { ENTITY_TYPE_PLAYBOOK } from '../../../src/modules/playbook/playbook-types';
 import { join } from 'path';
 
@@ -14,10 +13,7 @@ describe('playbook-domain', () => {
   });
 
   describe('playbookImport', () => {
-    const parsedDataOldVersionMock = { openCTI_version: '6.6.0', type: 'playbook', configuration: {} };
-    const parsedDataWrongTypeMock = { openCTI_version: '7.260428.0', type: 'random', configuration: {} };
     const contextMock = { id: 'context' } as unknown as AuthContext;
-    const fileMock = {} as unknown as Promise<FileHandle>;
 
     const playbookCreatedIds: string[] = [];
 
@@ -28,19 +24,21 @@ describe('playbook-domain', () => {
     });
 
     it('should throw error if playbook from a too old octi version', async () => {
-      vi.spyOn(fileToContent, 'extractContentFrom').mockResolvedValue(parsedDataOldVersionMock);
+      const filePath = join(__dirname, 'testData/imported-playbook-with-old-version.json');
+      const fileHandle = open(filePath);
 
-      await expect(playbookImport(contextMock, ADMIN_USER, fileMock)).rejects.toThrowError('Invalid version of the platform. Please upgrade your OpenCTI. Minimal version required: 6.7.14');
+      await expect(playbookImport(contextMock, ADMIN_USER, fileHandle)).rejects.toThrowError('Invalid version of the platform. Please upgrade your OpenCTI. Minimal version required: 6.7.14');
     });
 
     it('should throw error if parsedData is not of playbook type', async () => {
-      vi.spyOn(fileToContent, 'extractContentFrom').mockResolvedValue(parsedDataWrongTypeMock);
+      const filePath = join(__dirname, 'testData/imported-playbook-with-wrong-type.json');
+      const fileHandle = open(filePath);
 
-      await expect(playbookImport(contextMock, ADMIN_USER, fileMock)).rejects.toThrowError('Invalid import type, must be playbook');
+      await expect(playbookImport(contextMock, ADMIN_USER, fileHandle)).rejects.toThrowError('Invalid import type, must be playbook');
     });
 
     it('should update correctly a playbook imported with the old configuration', async () => {
-      const filePath = join(__dirname, 'playbookComponents/data/imported-playbook-with-old-scope.json');
+      const filePath = join(__dirname, 'testData/imported-playbook-with-old-scope.json');
       const fileHandle = open(filePath);
       const updatedPlaybookDefinition = '{"nodes":[{"id":"ce1413d0-d93b-45ae-9cda-24fea1ab67b7","name":"Listen knowledge events","position":{"x":0,"y":0},"component_id":"PLAYBOOK_INTERNAL_DATA_STREAM","configuration":"{\\"create\\":true,\\"update\\":true,\\"delete\\":false,\\"filters\\":\\"{\\\\\\"mode\\\\\\":\\\\\\"and\\\\\\",\\\\\\"filters\\\\\\":[],\\\\\\"filterGroups\\\\\\":[]}\\",\\"canEnrollManually\\":true}"},{"id":"01c5e873-df20-41db-805b-00ac26d0de88","name":"Apply predefined rule","position":{"x":0,"y":150},"component_id":"PLAYBOOK_RULE_COMPONENT","configuration":"{\\"rule\\":\\"resolve_container\\",\\"inferences\\":false}"},{"id":"78411f5e-e053-4e03-92c5-748845ec2de9","name":"Container wrapper","position":{"x":-100,"y":300},"component_id":"PLAYBOOK_CONTAINER_WRAPPER_COMPONENT","configuration":"{\\"actions\\":[],\\"applyWithFilters\\":\\"{\\\\\\"mode\\\\\\":\\\\\\"and\\\\\\",\\\\\\"filters\\\\\\":[],\\\\\\"filterGroups\\\\\\":[]}\\",\\"container_type\\":\\"Feedback\\",\\"applyToElements\\":\\"all-except-main\\"}"},{"id":"2853a108-7d66-4904-aefa-67259308338a","name":"Send for ingestion","position":{"x":-100,"y":450},"component_id":"PLAYBOOK_INGESTION_COMPONENT","configuration":"{}"}],"links":[]}';
 
