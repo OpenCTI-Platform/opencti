17572c23c6a374a55e73e5221c20e6b3a4aca45c
Marie Torbay <marie.torbay@filigran.io>
2026-05-13 17:39:03 +0200

    First commit

---
+++ opencti-platform/opencti-graphql/src/modules/securityCoverage/securityCoverageResult/securityCoverageResult-types.ts
@@ -0,0 +1,42 @@
+import type { BasicStoreEntity, StoreEntity } from '../../../types/store';
+import type { StixDomainObject, StixOpenctiExtensionSDO } from '../../../types/stix-2-1-common';
+import { STIX_EXT_OCTI } from '../../../types/stix-2-1-extensions';
+import type { BasicStoreEntitySecurityCoverage } from '../securityCoverage-types';
+
+export const ENTITY_TYPE_SECURITY_COVERAGE_RESULT = 'Security-Coverage-Result';
+export const RELATION_RESULT_OF = 'result-of';
+export const ATTRIBUTE_RESULT_OF = 'result_of_ref';
+export const INPUT_RESULT_OF = 'resultOf';
+
+export interface BasicStoreEntitySecurityCoverageResult extends BasicStoreEntity {
+  name: string;
+  external_uri?: string;
+  coverage_last_result?: string;
+  coverage_valid_from?: string;
+  coverage_valid_to?: string;
+  coverage_information?: { coverage_name: string; coverage_score: number }[];
+}
+
+export interface StoreEntitySecurityCoverageResult extends StoreEntity {
+  name: string;
+  external_uri?: string;
+  coverage_last_result?: string;
+  coverage_valid_from?: string;
+  coverage_valid_to?: string;
+  coverage_information?: { coverage_name: string; coverage_score: number }[];
+  [INPUT_RESULT_OF]: BasicStoreEntitySecurityCoverage;
+}
+
+export interface StixSecurityCoverageResult extends StixDomainObject {
+  name: string;
+  external_uri?: string;
+  coverage_last_result?: string;
+  coverage_valid_from?: string;
+  coverage_valid_to?: string;
+  coverage: { name: string; score: number }[];
+  covered: boolean;
+  [ATTRIBUTE_RESULT_OF]: string;
+  extensions: {
+    [STIX_EXT_OCTI]: StixOpenctiExtensionSDO;
+  };
+}
