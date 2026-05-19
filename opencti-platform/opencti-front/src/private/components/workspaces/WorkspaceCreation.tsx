664f3294bc3bbcbf8b790067c6fd56a86cdec090
Gwendoline Favre-Felix <gwendoline.favre-felix@filigran.io>
2026-05-19 11:40:49 +0200

    [frontend] WIP after rebase (#14448)
    # Conflicts:
    #	opencti-platform/opencti-front/src/private/components/workspaces/WorkspaceCreation.tsx
    
    # Conflicts:
    #	opencti-platform/opencti-front/src/private/components/workspaces/dashboards/CustomDashboard.tsx

--- opencti-platform/opencti-front/src/private/components/workspaces/WorkspaceCreation.tsx
+++ opencti-platform/opencti-front/src/private/components/workspaces/WorkspaceCreation.tsx
@@ -26,6 +26,8 @@ import { WorkspaceCreationImportMutation } from './__generated__/WorkspaceCreati
 import { WorkspacesLinesPaginationQuery$variables } from './__generated__/WorkspacesLinesPaginationQuery.graphql';
 import useDashboardImport from '../../../components/dashboard/import-export/useDashboardImport';
 import DashboardHiddenImportInput from '../../../components/dashboard/import-export/DashboardHiddenImportInput';
+import { Box, Switch } from '@mui/material';
+import MenuItem from '@mui/material/MenuItem';
 
 const workspaceMutation = graphql`
   mutation WorkspaceCreationMutation($input: WorkspaceAddInput!) {
@@ -50,12 +52,22 @@ const workspaceValidation = (t_i18n: (s: string) => string) => Yup.object().shap
 interface WorkspaceCreationForm {
   name: string;
   description: string;
+  autoReload: boolean;
+  refreshRate: number;
 }
 
 interface WorkspaceCreationProps {
   paginationOptions: WorkspacesLinesPaginationQuery$variables;
   type: string;
 }
+
+export const REFRESH_INTERVALS = [
+  { label: '1 minute', value: 60 },
+  { label: '5 minutes', value: 300 },
+  { label: '15 minutes', value: 900 },
+  { label: '30 minutes', value: 1800 },
+  { label: '1 hour', value: 3600 },
+];
 
 const WorkspaceCreation = ({ paginationOptions, type }: WorkspaceCreationProps) => {
   const { t_i18n } = useFormatter();
@@ -88,12 +100,16 @@ const WorkspaceCreation = ({ paginationOptions, type }: WorkspaceCreationProps) 
   const [commitCreationMutation] = useApiMutation(workspaceMutation);
 
   const onSubmit: FormikConfig<WorkspaceCreationForm>['onSubmit'] = (values, { setSubmitting, resetForm, setErrors }) => {
+    const refreshRateInSeconds = values.autoReload
+      ? values.refreshRate
+      : null;
     commitCreationMutation({
       variables: {
         input: {
           ...buildCreationFilesInput(),
           ...values,
           type,
+          refresh_rate: refreshRateInSeconds,
         },
       },
       updater: (store) => {
@@ -164,12 +180,14 @@ const WorkspaceCreation = ({ paginationOptions, type }: WorkspaceCreationProps) 
             initialValues={{
               name: '',
               description: '',
+              autoReload: false,
+              refreshRate: 60,
             }}
             validationSchema={workspaceValidation(t_i18n)}
             onSubmit={onSubmit}
             onReset={onClose}
           >
-            {({ submitForm, handleReset, isSubmitting }) => (
+            {({ submitForm, handleReset, isSubmitting, values, setFieldValue }) => (
               <Form>
                 <Field
                   component={TextField}
@@ -188,6 +206,34 @@ const WorkspaceCreation = ({ paginationOptions, type }: WorkspaceCreationProps) 
                   autoPersistOnBlur={false}
                   registerMarkdownImagesController={registerMarkdownImagesController}
                 />
+                {type === 'dashboard' && (
+                  <Box mt={2}>
+                    <Box display="flex" alignItems="center" justifyContent="space-between">
+                      <span>{t_i18n('Auto-reload')}</span>
+                      <Switch
+                        checked={values.autoReload}
+                        onChange={(e) => setFieldValue('autoReload', e.target.checked)}
+                      />
+                    </Box>
+                    {values.autoReload && (
+                      <Box mt={2}>
+                        <Field
+                          name="refreshRate"
+                          component={TextField}
+                          select
+                          fullWidth
+                          label={t_i18n('Interval')}
+                        >
+                          {REFRESH_INTERVALS.map((opt) => (
+                            <MenuItem key={opt.value} value={opt.value}>
+                              {opt.label}
+                            </MenuItem>
+                          ))}
+                        </Field>
+                      </Box>
+                    )}
+                  </Box>
+                )}
                 <FormButtonContainer>
                   <Button
                     variant="secondary"
