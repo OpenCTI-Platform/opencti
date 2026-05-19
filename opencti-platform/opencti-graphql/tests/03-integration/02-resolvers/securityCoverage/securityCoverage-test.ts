bf51c3e105dd294f9804e8d218f10670f23ae078
Marie Torbay <marie.torbay@filigran.io>
2026-05-19 16:26:50 +0200

    [backend] Add some tests to check security coverage creation
    corrected counter
    
    fix counter total
    
    fix counter

---
+++ opencti-platform/opencti-graphql/tests/03-integration/02-resolvers/securityCoverage/securityCoverage-test.ts
@@ -0,0 +1,55 @@
+import gql from 'graphql-tag';
+import { describe, expect, it } from 'vitest';
+import { queryAsAdmin } from '../../../utils/testQueryHelper';
+
+const CREATE_QUERY = gql`
+ mutation SecurityCoverageAdd($input: SecurityCoverageAddInput!) {
+    securityCoverageAdd(input: $input) {
+      name
+      coverage_last_result
+      coverage_valid_from
+      coverage_valid_to
+      coverage_information {
+        coverage_name
+        coverage_score
+      }
+      external_uri
+    }
+  }
+`;
+
+describe('SecurityCoverage resolver', () => {
+  it('should create SecurityCoverage with correct coverage information', async () => {
+    const SECURITY_COVERAGE = {
+      input: {
+        name: 'SC name',
+        objectCovered: 'report--a445d22a-db0c-4b5d-9ec8-e9ad0b6dbdd7',
+        auto_enrichment_disable: true,
+        coverage_last_result: '2023-08-06T11:39:36.949Z',
+        coverage_valid_from: '2023-07-06T11:39:36.949Z',
+        coverage_valid_to: '2023-12-06T11:39:36.949Z',
+        coverage_information: [{
+          coverage_name: 'prevention',
+          coverage_score: 10,
+        }],
+        external_uri: 'http://localhost/admin/scenarios/a2166709-be41-48bf-9ce1-51bb2fd3a131',
+      },
+    };
+
+    const securityCoverage = await queryAsAdmin({
+      query: CREATE_QUERY,
+      variables: SECURITY_COVERAGE,
+    });
+
+    const securityCoverageData = securityCoverage.data?.securityCoverageAdd;
+    console.log(securityCoverageData);
+    expect(securityCoverageData).toBeDefined();
+    expect(securityCoverageData.name).toEqual('SC name');
+    expect(securityCoverageData.coverage_last_result.toISOString()).toEqual('2023-08-06T11:39:36.949Z');
+    expect(securityCoverageData.coverage_valid_from.toISOString()).toEqual('2023-07-06T11:39:36.949Z');
+    expect(securityCoverageData.coverage_valid_to.toISOString()).toEqual('2023-12-06T11:39:36.949Z');
+    expect(securityCoverageData.external_uri).toEqual('http://localhost/admin/scenarios/a2166709-be41-48bf-9ce1-51bb2fd3a131');
+    expect(securityCoverageData.coverage_information[0].coverage_name).toEqual('prevention');
+    expect(securityCoverageData.coverage_information[0].coverage_score).toEqual(10);
+  });
+});
