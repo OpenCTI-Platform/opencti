23b68f704b9ac507bd54a536e110c0975f218ccf
Marie Torbay <marie.torbay@filigran.io>
2026-05-19 16:26:50 +0200

    [backend] Add some tests to check security coverage creation
    corrected counter
    
    fix counter total
    
    fix counter

--- opencti-platform/opencti-graphql/tests/03-integration/02-resolvers/stixDomainObject-test.js
+++ opencti-platform/opencti-graphql/tests/03-integration/02-resolvers/stixDomainObject-test.js
@@ -174,7 +174,7 @@ describe('StixDomainObject resolver standard behavior', () => {
       }
     `;
     const queryResult = await queryAsAdmin({ query: NUMBER_QUERY });
-    expect(queryResult.data.stixDomainObjectsNumber.total).toEqual(106);
+    expect(queryResult.data.stixDomainObjectsNumber.total).toEqual(107);
   });
   it('should timeseries stixDomainObjects to be accurate', async () => {
     const TIMESERIES_QUERY = gql`
