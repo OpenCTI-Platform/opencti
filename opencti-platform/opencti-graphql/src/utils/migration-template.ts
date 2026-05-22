e62611b4af31fc43f482a44d59f75d671e0a5977
Angelique Jard <angelique.jard@filigran.io>
2026-05-22 09:01:20 +0200

    Also add total processing time in done log

--- opencti-platform/opencti-graphql/src/utils/migration-template.ts
+++ opencti-platform/opencti-graphql/src/utils/migration-template.ts
@@ -2,13 +2,16 @@ import { logMigration } from '../config/conf';
 
 const message = '[MIGRATION] migration title';
 
-export const up = async (next) => {
+export const up = async (next: any) => {
+  const startTime = Date.now();
   logMigration.info(`${message} > started`);
   // do your migration
-  logMigration.info(`${message} > done`);
+  // see src/migration/README.md for best practices
+  // if there is a loop, add a logMigration.info to display expected number and progress (ex: "processing 2/100 Indicators")
+  logMigration.info(`${message} > done in ${Date.now() - startTime} ms`);
   next();
 };
 
-export const down = async (next) => {
+export const down = async (next: any) => {
   next();
 };
