021da54d37ca347409538a2d68084f65516efbd3
Gwendoline Favre-Felix <gwendoline.favre-felix@filigran.io>
2026-05-26 13:52:52 +0200

    [Frontend] Bookmark widget refetch (#14448)

--- opencti-platform/opencti-front/src/private/components/common/stix_core_objects/StixCoreObjectsList.tsx
+++ opencti-platform/opencti-front/src/private/components/common/stix_core_objects/StixCoreObjectsList.tsx
@@ -457,7 +457,6 @@ interface StixCoreObjectsListProps {
   popover?: ReactNode;
   dataSelection: Widget['dataSelection'];
   widgetId: string;
-
   host?: WidgetHost;
   config: DashboardConfig;
   refreshRate?: number | null;
