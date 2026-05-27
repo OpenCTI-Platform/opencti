a2aaff543e3f33be6b41fe58179fde97bd29e9ea
Kevin Nunes <kevin.nunes@filigran.io>
2026-05-27 11:46:29 +0200

    [backend] Fix vitest config include patterns (#16200)

--- opencti-platform/opencti-graphql/vitest.config.dev.ts
+++ opencti-platform/opencti-graphql/vitest.config.dev.ts
@@ -1,4 +1,4 @@
 import { buildIntegrationTestConfig } from './vitest.config.integration';
 
-export default buildIntegrationTestConfig(['tests/**/*-test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']);
+export default buildIntegrationTestConfig(['**/*-test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']);
 // to run one test use yarn test:dev:init ; yarn test:dev:resume <test-name>
