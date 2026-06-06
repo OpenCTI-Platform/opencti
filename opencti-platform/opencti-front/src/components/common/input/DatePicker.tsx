d7eadc4858e6b1c513289fd4a369f02279c8ab96
Armin <arminkheirkhahan@gmail.com>
2026-06-06 12:30:02 +0330

    Reapply "Merge pull request #43 from plantir/feature/jalali-datepicker"
    This reverts commit e8a381635cbbdeadb321c50fe06db2d08d8a6fd6.

--- opencti-platform/opencti-front/src/components/common/input/DatePicker.tsx
+++ opencti-platform/opencti-front/src/components/common/input/DatePicker.tsx
@@ -1,20 +1,36 @@
 import React from 'react';
 import { DatePicker as MuiDatePicker, DatePickerProps } from '@mui/x-date-pickers/DatePicker';
 import { useTheme } from '@mui/material/styles';
+import { useIntl } from 'react-intl';
 import { Theme } from '../../Theme';
+import { resolveDatePickerFormat } from '../../../utils/datePickerConfig';
 
 const DatePicker: React.FC<DatePickerProps<Date>> = ({
   slotProps,
+  format,
   ...datePickerProps
 }) => {
   const theme = useTheme<Theme>();
+  const { locale } = useIntl();
   const { value } = datePickerProps;
+  const resolvedFormat = resolveDatePickerFormat(locale, format);
 
   return (
     <MuiDatePicker
       {...datePickerProps}
+      format={resolvedFormat}
       slotProps={{
         ...slotProps,
+        nextIconButton: {
+          sx: {
+            transform: theme.direction === 'rtl' ? 'rotate(180deg)' : 'none',
+          },
+        },
+        previousIconButton: {
+          sx: {
+            transform: theme.direction === 'rtl' ? 'rotate(180deg)' : 'none',
+          },
+        },
         textField: {
           size: 'small',
           ...slotProps?.textField,
@@ -22,7 +38,9 @@ const DatePicker: React.FC<DatePickerProps<Date>> = ({
             '& .MuiOutlinedInput-root': {
               backgroundColor: theme.palette.background.secondary,
               '& fieldset': {
-                borderColor: value ? theme.palette.border.secondary : 'transparent',
+                borderColor: value
+                  ? theme.palette.border.secondary
+                  : 'transparent',
               },
               '&:hover fieldset, &.Mui-focused fieldset': {
                 borderColor: theme.palette.border.secondary,
