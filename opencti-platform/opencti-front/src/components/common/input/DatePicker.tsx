e8a381635cbbdeadb321c50fe06db2d08d8a6fd6
Armin <arminkheirkhahan@gmail.com>
2026-06-06 12:05:13 +0330

    Revert "Merge pull request #43 from plantir/feature/jalali-datepicker"
    This reverts commit 09f89b331d30aefd75de182074598ea7157be049, reversing
    changes made to 602132cde1d3e4f72be9f63fb93604cd2efa17dc.

--- opencti-platform/opencti-front/src/components/common/input/DatePicker.tsx
+++ opencti-platform/opencti-front/src/components/common/input/DatePicker.tsx
@@ -1,36 +1,20 @@
 import React from 'react';
 import { DatePicker as MuiDatePicker, DatePickerProps } from '@mui/x-date-pickers/DatePicker';
 import { useTheme } from '@mui/material/styles';
-import { useIntl } from 'react-intl';
 import { Theme } from '../../Theme';
-import { resolveDatePickerFormat } from '../../../utils/datePickerConfig';
 
 const DatePicker: React.FC<DatePickerProps<Date>> = ({
   slotProps,
-  format,
   ...datePickerProps
 }) => {
   const theme = useTheme<Theme>();
-  const { locale } = useIntl();
   const { value } = datePickerProps;
-  const resolvedFormat = resolveDatePickerFormat(locale, format);
 
   return (
     <MuiDatePicker
       {...datePickerProps}
-      format={resolvedFormat}
       slotProps={{
         ...slotProps,
-        nextIconButton: {
-          sx: {
-            transform: theme.direction === 'rtl' ? 'rotate(180deg)' : 'none',
-          },
-        },
-        previousIconButton: {
-          sx: {
-            transform: theme.direction === 'rtl' ? 'rotate(180deg)' : 'none',
-          },
-        },
         textField: {
           size: 'small',
           ...slotProps?.textField,
@@ -38,9 +22,7 @@ const DatePicker: React.FC<DatePickerProps<Date>> = ({
             '& .MuiOutlinedInput-root': {
               backgroundColor: theme.palette.background.secondary,
               '& fieldset': {
-                borderColor: value
-                  ? theme.palette.border.secondary
-                  : 'transparent',
+                borderColor: value ? theme.palette.border.secondary : 'transparent',
               },
               '&:hover fieldset, &.Mui-focused fieldset': {
                 borderColor: theme.palette.border.secondary,
