0391e75096bd6d7fc90a432fc0b383b39152a48d
scarletmerlin123 <242427742+scarletmerlin123@users.noreply.github.com>
2026-06-10 07:23:32 -0500

    [frontend] extract creating offset time item into function to reduce duplication

--- opencti-platform/opencti-front/src/private/components/TimeoutLock.tsx
+++ opencti-platform/opencti-front/src/private/components/TimeoutLock.tsx
@@ -30,6 +30,13 @@ const timeoutLockRefreshQuery = graphql`
     }
   }
 `;
+const createTimeWithOffset = (): number => {
+  return Date.now() - ONE_SECOND;
+};
+
+const createOffsetTimeItem = (): { startDate: Date; startDateEpoch: number } => {
+  return { startDate: new Date(), startDateEpoch: createTimeWithOffset() };
+};
 
 /**
  * Handles various state changes for the timeout counting functionality.
@@ -37,7 +44,7 @@ const timeoutLockRefreshQuery = graphql`
 const timeoutReducer = (state: TimeoutState, action: Action): TimeoutState => {
   const { idleLimit, sessionLimit } = state;
   let { idleCount, startDate, startDateEpoch } = state;
-  const offsetTime = Date.now() - ONE_SECOND;
+  const offsetTimeItem = createOffsetTimeItem();
   if (idleCount === null) {
     idleCount = sessionLimit;
   }
@@ -49,8 +56,7 @@ const timeoutReducer = (state: TimeoutState, action: Action): TimeoutState => {
     idleCount = sessionLimit - (Date.now() - timeoutData.startDateEpoch) / ONE_SECOND;
   } else {
     // If timeout start not yet initialize, setup the local storage
-    const newTimeItem = { startDate: new Date(), startDateEpoch: offsetTime };
-    localStorage.setItem('lockoutTracker', JSON.stringify(newTimeItem));
+    localStorage.setItem('lockoutTracker', JSON.stringify(offsetTimeItem));
     idleCount -= 1;
   }
   // Handle actions
@@ -61,14 +67,13 @@ const timeoutReducer = (state: TimeoutState, action: Action): TimeoutState => {
     return state;
   }
   if (action.type === 'reset timeout') {
-    const newTimeItem = { startDate: new Date(), startDateEpoch: offsetTime };
-    localStorage.setItem('lockoutTracker', JSON.stringify(newTimeItem));
+    localStorage.setItem('lockoutTracker', JSON.stringify(offsetTimeItem));
     return {
       idleLimit,
       sessionLimit,
       idleCount: sessionLimit,
-      startDate: newTimeItem.startDate,
-      startDateEpoch: newTimeItem.startDateEpoch,
+      startDate: offsetTimeItem.startDate,
+      startDateEpoch: offsetTimeItem.startDateEpoch,
     };
   }
   return state;
@@ -86,7 +91,7 @@ const TimeoutLock: React.FunctionComponent = () => {
     sessionLimit,
     idleCount: null,
     startDate: new Date(),
-    startDateEpoch: Date.now() - ONE_SECOND,
+    startDateEpoch: createTimeWithOffset(),
   });
   const [resetCounter, triggerReset] = useState(false);
   const interval = useRef<NodeJS.Timeout | null>(null);
