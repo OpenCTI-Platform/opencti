cceecad054207fdf6ea702aca2bea92d3e009250
Marie Torbay <marie.torbay@filigran.io>
2026-06-16 12:14:40 +0200

    Brainstorm

--- opencti-platform/opencti-graphql/src/migrations/1781526374393-split-security-coverage-results.ts
+++ opencti-platform/opencti-graphql/src/migrations/1781526374393-split-security-coverage-results.ts
@@ -27,12 +27,15 @@ export const up = async (next: (error?: Error) => void) => {
   logMigration.info(`${message} > started`);
 
   // Step 1 -> Retrieve all SecurityCoverages
+  // TODO : add a callback system to avoid getting too many SC
   const allSecurityCoverages = await fullEntitiesList<OldSecurityCoverage>(
     context,
     SYSTEM_USER,
     [ENTITY_TYPE_SECURITY_COVERAGE],
   );
   logMigration.info(`${message} > ${allSecurityCoverages.length} SecurityCoverages found`);
+
+  // TODO : get all has-covered relationships
 
   // Step 2 -> Create a SecurityCoverageResult for each SecurityCoverage containing results data.
   for (const sc of allSecurityCoverages) {
@@ -51,6 +54,7 @@ export const up = async (next: (error?: Error) => void) => {
 
     // Want to create a result if there is results data,
     // Or an external_uri is set because we can have a result instantiate by OpenAEV but without data yet.
+    // TODO : add condition if there is has-covered relationship => create a SCR
     if (external_uri || (coverage_information ?? []).length > 0) {
       const securityCoverageResultInput = {
         name: `Result of ${sc.name}`,
@@ -74,11 +78,38 @@ export const up = async (next: (error?: Error) => void) => {
         securityCoverageResultInput,
         ENTITY_TYPE_SECURITY_COVERAGE_RESULT,
       );
+
+      // TODO : a bulk update + split in groups (see line 124 of clean-deprecated-rels)
+
+      // Find the has-covered relationships with the SC id
+
+      // 1rst op : relation => filter on  "role": "has-covered_from"
+      // In relationship : replace "connections" :
+      // "internal_id" : replace SC id with SCR id
+      // "name" : replace SC name with SCR name
+      // "types" : replace "Security-Coverage" with "Security-Coverage-Result" ENTITY_TYPE_SECURITY_COVERAGE_RESULT
+
+      // 2nd op : In SCR : "rel_has-covered.internal_id" : list of has-covered entities TO BE ADDED (to)
+      // get SCR id
+      // add list of has-covered entities to "rel_has-covered.internal_id"
+
+      // 3rd op : In entity : "rel_has-covered.internal_id" : list of SC to be replaced with corresponding SCR
+
+      // 4rth op : In SC : remove attributes
+      // "rel_has-covered.internal_id" : list of has-covered entities TO BE REMOVED (to)
+      // + all other attributes :
+      // 'coverage_information'
+      // 'coverage_last_result'
+      // 'coverage_valid_from'
+      // 'coverage_valid_to'
+      // 'external_uri'
+
       logMigration.info(`${message} > SCR ${result?.standard_id} created for SC ${sc.standard_id}`);
     }
   }
 
   // Step 3 -> Remove old attributes.
+  // TODO : create a bulk update to call query only once (see clean-deprecated-rels)
   await elUpdateByQueryForMigration(
     `${message} > Clean old attributes`,
     READ_INDEX_STIX_DOMAIN_OBJECTS,
