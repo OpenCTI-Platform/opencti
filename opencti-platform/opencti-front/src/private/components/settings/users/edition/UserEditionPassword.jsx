cf2f155a08b15a4742044faa3c03d63b717e895a
Celine Sebe <celine.sebe@filigran.io>
2026-06-18 17:37:46 +0200

    refacto userEdition

--- opencti-platform/opencti-front/src/private/components/settings/users/edition/UserEditionPassword.jsx
+++ opencti-platform/opencti-front/src/private/components/settings/users/edition/UserEditionPassword.jsx
@@ -1,27 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import * as PropTypes from 'prop-types';
 import { graphql, createFragmentContainer } from 'react-relay';
 import { Formik, Form, Field } from 'formik';
-import { compose } from 'ramda';
 import * as Yup from 'yup';
-import withStyles from '@mui/styles/withStyles';
 import { Stack } from '@mui/material';
 import FormHelperText from '@mui/material/FormHelperText';
 import Button from '@common/button/Button';
 import { commitMutation, MESSAGING$ } from '../../../../../relay/environment';
-import inject18n from '../../../../../components/i18n';
 import TextField from '../../../../../components/TextField';
 import PasswordPolicies from '../../../common/form/PasswordPolicies';
-
-const styles = (theme) => ({
-  buttons: {
-    marginTop: 20,
-    textAlign: 'right',
-  },
-  button: {
-    marginLeft: theme.spacing(2),
-  },
-});
 
 const userMutationFieldPatch = graphql`
   mutation UserEditionPasswordFieldPatchMutation(
@@ -43,37 +30,42 @@ const userValidation = (t) => Yup.object().shape({
     .required(t('This field is required')),
 });
 
-class UserEditionPasswordComponent extends Component {
-  formatExpiryDate(value) {
-    if (!value) return null;
-    const date = new Date(value);
-    if (Number.isNaN(date.getTime())) return null;
-    return new Intl.DateTimeFormat('fr-FR', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    }).format(date);
-  }
+const formatExpiryDate = (value) => {
+  if (!value) return null;
+  const date = new Date(value);
+  if (Number.isNaN(date.getTime())) return null;
+  return new Intl.DateTimeFormat('fr-FR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }).format(date);
+};
 
-  handleForcePasswordChange() {
+const UserEditionPasswordComponent = ({ classes, t, theme, user }) => {
+  const external = user.external === true;
+  const isLocked = user.account_status === 'Locked';
+  const formattedExpiry = formatExpiryDate(user.password_valid_until);
+  const initialValues = { password: '', confirmation: '' };
+
+  const handleForcePasswordChange = () => {
     commitMutation({
       mutation: userMutationFieldPatch,
       variables: {
-        id: this.props.user.id,
+        id: user.id,
         input: { key: 'password_valid_until', value: [new Date().toISOString()] },
       },
       onCompleted: () => {
         MESSAGING$.notifySuccess('Password change will be required at next login');
       },
     });
-  }
+  };
 
-  onSubmit(values, { setSubmitting, resetForm }) {
+  const onSubmit = (values, { setSubmitting, resetForm }) => {
     const field = { key: 'password', value: values.password };
     commitMutation({
       mutation: userMutationFieldPatch,
       variables: {
-        id: this.props.user.id,
+        id: user.id,
         input: field,
       },
       setSubmitting,
@@ -83,43 +75,19 @@ class UserEditionPasswordComponent extends Component {
         resetForm();
       },
     });
-  }
+  };
 
-  render() {
-    const { classes, t } = this.props;
-    const external = this.props.user.external === true;
-    const isLocked = this.props.user.account_status === 'Locked';
-    const formattedExpiry = this.formatExpiryDate(this.props.user.password_valid_until);
-    const initialValues = { password: '', confirmation: '' };
-    return (
-      <Formik
-        enableReinitialize={true}
-        initialValues={initialValues}
-        validationSchema={userValidation(t)}
-        onSubmit={this.onSubmit.bind(this)}
-      >
-        {({ submitForm, isSubmitting }) => (
-          <Form style={{ marginTop: this.props.theme.spacing(2) }}>
-            <Stack sx={{ gap: 2.5 }}>
-              <PasswordPolicies />
-              <Field
-                component={TextField}
-                variant="standard"
-                name="password"
-                label={t('Password')}
-                type="password"
-                fullWidth={true}
-              />
-              <Field
-                component={TextField}
-                variant="standard"
-                name="confirmation"
-                label={t('Confirmation')}
-                type="password"
-                fullWidth={true}
-              />
-            </Stack>
-            <PasswordPolicies style={{ marginBottom: 20 }} />
+  return (
+    <Formik
+      enableReinitialize={true}
+      initialValues={initialValues}
+      validationSchema={userValidation(t)}
+      onSubmit={onSubmit}
+    >
+      {({ submitForm, isSubmitting }) => (
+        <Form style={{ marginTop: theme.spacing(2) }}>
+          <Stack sx={{ gap: 2.5 }}>
+            <PasswordPolicies />
             <Field
               component={TextField}
               variant="standard"
@@ -135,37 +103,36 @@ class UserEditionPasswordComponent extends Component {
               label={t('Confirmation')}
               type="password"
               fullWidth={true}
-              style={{ marginTop: 20 }}
             />
-            <div className={classes.buttons}>
-              {!external && !isLocked && (
-                <Button
-                  variant="secondary"
-                  onClick={this.handleForcePasswordChange.bind(this)}
-                  classes={{ root: classes.button }}
-                >
-                  {t('Force password change')}
-                </Button>
-              )}
+          </Stack>
+          <div style={{ marginTop: 20, textAlign: 'right' }}>
+            {!external && !isLocked && (
               <Button
-                onClick={submitForm}
-                disabled={isSubmitting}
+                variant="secondary"
+                onClick={handleForcePasswordChange}
                 classes={{ root: classes.button }}
               >
-                {t('Update')}
+                {t('Force password change')}
               </Button>
-            </div>
-            {formattedExpiry && (
-              <FormHelperText style={{ marginTop: 8 }}>
-                {`Expiry: ${formattedExpiry}`}
-              </FormHelperText>
             )}
-          </Form>
-        )}
-      </Formik>
-    );
-  }
-}
+            <Button
+              onClick={submitForm}
+              disabled={isSubmitting}
+              style={{ marginLeft: theme.spacing(2) }}
+            >
+              {t('Update')}
+            </Button>
+          </div>
+          {formattedExpiry && (
+            <FormHelperText style={{ marginTop: 8 }}>
+              {`Expiry: ${formattedExpiry}`}
+            </FormHelperText>
+          )}
+        </Form>
+      )}
+    </Formik>
+  );
+};
 
 UserEditionPasswordComponent.propTypes = {
   classes: PropTypes.object,
@@ -190,7 +157,4 @@ const UserEditionPassword = createFragmentContainer(
   },
 );
 
-export default compose(
-  inject18n,
-  withStyles(styles, { withTheme: true }),
-)(UserEditionPassword);
+export default UserEditionPassword;
