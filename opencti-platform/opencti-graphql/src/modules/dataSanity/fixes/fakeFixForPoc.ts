367d118a918886d8a574d2931cbc57b49956f1ae
Angelique Jard <angelique.jard@filigran.io>
2026-06-26 07:39:54 +0200

    Add translations and fix style

--- opencti-platform/opencti-graphql/src/modules/dataSanity/fixes/fakeFixForPoc.ts
+++ opencti-platform/opencti-graphql/src/modules/dataSanity/fixes/fakeFixForPoc.ts
@@ -1,7 +1,7 @@
 import type { AuthContext } from '../../../types/user';
 import { logApp } from '../../../config/conf';
 
-export const fakeFixForPoc = async (context: AuthContext) => {
+export const fakeFixForPoc = async (_context: AuthContext) => {
   logApp.info('fakeFixForPoc execution');
   return {
     message: 'This is a fake fix for proof of concept purposes.',
