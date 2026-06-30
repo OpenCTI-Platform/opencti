c55f5a2459438a702360a7a732dedc2ca2cc3037
Ludovic Bouges <ludovic.bouges@filigran.io>
2026-06-30 17:14:06 +0200

    feat(backend): add CustomFieldDefinition module and feature flag
    - Add custom_fields feature flag in conf.js
    - Create CustomFieldDefinition module (types, domain, GraphQL schema,
      resolvers, converter, registration)
    - Support 5 field types: integer, string, boolean, date, select
    - In-memory cache loaded at boot, refreshed on CRUD
    - CRUD APIs with SETTINGS_SETCUSTOMIZATION auth
    - Immutable fields: field_type and name cannot be changed after creation
    - Add custom_field_values to BasicStoreEntity store type
    
    Refs: #6177

---
+++ opencti-platform/opencti-graphql/src/modules/customField/custom-field-converter.ts
@@ -0,0 +1,30 @@
+import { buildStixDomain } from '../../database/stix-2-1-converter';
+import { STIX_EXT_OCTI } from '../../types/stix-2-1-extensions';
+import type { StixCustomFieldDefinition, StoreEntityCustomFieldDefinition } from './custom-field-types';
+import { cleanObject } from '../../database/stix-converter-utils';
+
+const convertCustomFieldDefinitionToStix = (instance: StoreEntityCustomFieldDefinition): StixCustomFieldDefinition => {
+  const customFieldDefinition = buildStixDomain(instance);
+  return {
+    ...customFieldDefinition,
+    name: instance.name,
+    description: instance.description,
+    label: instance.label,
+    field_type: instance.field_type,
+    entity_types: instance.entity_types,
+    mandatory: instance.mandatory,
+    multiple: instance.multiple,
+    default_value: instance.default_value,
+    min_value: instance.min_value,
+    max_value: instance.max_value,
+    select_options: instance.select_options,
+    extensions: {
+      [STIX_EXT_OCTI]: cleanObject({
+        ...customFieldDefinition.extensions[STIX_EXT_OCTI],
+        extension_type: 'new-sdo',
+      }),
+    },
+  };
+};
+
+export default convertCustomFieldDefinitionToStix;
