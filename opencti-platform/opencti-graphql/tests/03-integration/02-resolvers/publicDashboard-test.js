9a6579fa8ea6b27368a5b9f4cd1aca9bc6782b99
Cathia Archidoit <cathia.archidoit@filigran.io>
2026-06-30 11:52:55 +0200

    [backend] fix tests

--- opencti-platform/opencti-graphql/tests/03-integration/02-resolvers/publicDashboard-test.js
+++ opencti-platform/opencti-graphql/tests/03-integration/02-resolvers/publicDashboard-test.js
@@ -831,7 +831,7 @@ describe('PublicDashboard resolver', () => {
           const { publicStixRelationshipsMultiTimeSeries } = data;
           const attacksData = publicStixRelationshipsMultiTimeSeries[0].data;
           expect(attacksData.length).toEqual(1);
-          expect(attacksData[0].value).toEqual(4);
+          expect(attacksData[0].value).toEqual(7);
         });
 
         it('should return the data for API: SCO Distribution', async () => {
