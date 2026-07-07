f08351f4b7a34932dba98c4ce954c90de74ace28
Xavier Fournet <461943+xfournet@users.noreply.github.com>
2026-07-07 22:27:58 +0200

    refactor(config): simplify path resolution by enabling tsconfigPaths

--- opencti-platform/opencti-front/vite.config.ts
+++ opencti-platform/opencti-front/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as path from 'node:path';
 import relay from 'vite-plugin-relay';
 import monacoEditorPluginImport from 'vite-plugin-monaco-editor';
 
 // Handle ESM/CJS interop for vite-plugin-monaco-editor
 const monacoEditorPlugin = (monacoEditorPluginImport as unknown as {default: typeof monacoEditorPluginImport}).default;
 
-const basePath = "";
+const basePath = '';
 
 const backProxy = (ws = false) => ({
   target: process.env.BACK_END_URL ?? 'http://localhost:4000',
@@ -34,27 +33,22 @@ export default defineConfig({
   },
 
   resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, './src/private/components'),
-      'src': path.resolve(__dirname, './src'),
-      '@common': path.resolve(__dirname, './src/components/common')
-    },
+    tsconfigPaths: true,
     extensions: ['.tsx', '.jsx', '.ts', '.js', '.json'],
   },
 
   plugins: [
     {
       name: 'html-transform',
-      enforce: "pre",
+      enforce: 'pre',
       apply: 'serve',
-      transformIndexHtml(html) {
-        return html.replace(/%BASE_PATH%/g, basePath)
+      transformIndexHtml: (html) =>
+        html.replace(/%BASE_PATH%/g, basePath)
           .replace(/%APP_SCRIPT_SNIPPET%/g,  '')
-          .replace(/%APP_TITLE%/g, "OpenCTI Dev")
-          .replace(/%APP_DESCRIPTION%/g, "OpenCTI Development platform")
+          .replace(/%APP_TITLE%/g, 'OpenCTI Dev')
+          .replace(/%APP_DESCRIPTION%/g, 'OpenCTI Development platform')
           .replace(/%APP_FAVICON%/g, `${basePath}/assets/static/favicon.png`)
-          .replace(/%APP_MANIFEST%/g, `${basePath}/assets/static/manifest.json`)
-      }
+          .replace(/%APP_MANIFEST%/g, `${basePath}/assets/static/manifest.json`),
     },
     react(),
     relay,
@@ -67,7 +61,7 @@ export default defineConfig({
           entry: 'monaco-graphql/esm/graphql.worker.js',
         },
       ],
-    })
+    }),
   ],
 
   server: {
