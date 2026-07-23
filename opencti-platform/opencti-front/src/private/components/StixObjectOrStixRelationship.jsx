b2cb10570718cc8c9f54da77e984bce998f1a45a
Marie Torbay <marie.torbay@filigran.io>
2026-07-23 17:21:07 +0200

    Refacto of StixObjectOrStixRelationship from class to function

--- opencti-platform/opencti-front/src/private/components/StixObjectOrStixRelationship.jsx
+++ opencti-platform/opencti-front/src/private/components/StixObjectOrStixRelationship.jsx
@@ -1,22 +1,10 @@
-import React, { Component } from 'react';
-import * as PropTypes from 'prop-types';
-import { Navigate } from 'react-router-dom';
-import { compose } from 'ramda';
-import withStyles from '@mui/styles/withStyles';
+import { Navigate, useParams } from 'react-router-dom';
 import { graphql } from 'react-relay';
-import inject18n from '../../components/i18n';
 import { QueryRenderer } from '../../relay/environment';
 import { resolveLink } from '../../utils/Entity';
 import Loader from '../../components/Loader';
 import ErrorNotFound from '../../components/ErrorNotFound';
-import withRouter from '../../utils/compat_router/withRouter';
-
-const styles = () => ({
-  container: {
-    margin: 0,
-    padding: 0,
-  },
-});
+import makeStyles from '@mui/styles/makeStyles';
 
 export const stixObjectOrStixRelationshipStixObjectOrStixRelationshipQuery = graphql`
   query StixObjectOrStixRelationshipStixObjectOrStixRelationshipQuery(
@@ -96,93 +84,88 @@ export const stixObjectOrStixRelationshipStixObjectOrStixRelationshipQuery = gra
   }
 `;
 
-class StixObjectOrStixRelationship extends Component {
-  render() {
-    const {
-      classes,
-      params: { id },
-    } = this.props;
-    return (
-      <div className={classes.container}>
-        <QueryRenderer
-          query={stixObjectOrStixRelationshipStixObjectOrStixRelationshipQuery}
-          variables={{ id }}
-          render={({ props }) => {
-            if (props) {
-              if (props.stixObjectOrStixRelationship) {
-                let redirectLink;
-                const { stixObjectOrStixRelationship } = props;
-                const fromRestricted = stixObjectOrStixRelationship.from === null;
-                const toRestricted = stixObjectOrStixRelationship.to === null;
-                if (
-                  stixObjectOrStixRelationship.relationship_type
-                  === 'stix-sighting-relationship'
-                ) {
-                  if (!toRestricted) {
-                    redirectLink = `${resolveLink(
-                      stixObjectOrStixRelationship.to.entity_type,
-                    )}/${
-                      stixObjectOrStixRelationship.to.id
-                    }/knowledge/sightings/${stixObjectOrStixRelationship.id}`;
-                  } else {
-                    redirectLink = !fromRestricted
-                      ? `${resolveLink(
-                        stixObjectOrStixRelationship.from.entity_type,
-                      )}/${
-                        stixObjectOrStixRelationship.from.id
-                      }/knowledge/sightings/${stixObjectOrStixRelationship.id}`
-                      : undefined;
-                  }
-                } else if (stixObjectOrStixRelationship.relationship_type) {
-                  if (stixObjectOrStixRelationship.from?.relationship_type) {
-                    redirectLink = !toRestricted
-                      ? `${resolveLink(
-                        stixObjectOrStixRelationship.to.entity_type,
-                      )}/${
-                        stixObjectOrStixRelationship.to.id
-                      }/knowledge/relations/${stixObjectOrStixRelationship.id}`
-                      : undefined;
-                  } else if (!fromRestricted) {
-                    redirectLink = `${resolveLink(
+const useStyles = makeStyles({
+  container: {
+    margin: 0,
+    padding: 0,
+  },
+});
+
+const StixObjectOrStixRelationship = () => {
+  const classes = useStyles();
+  const { id } = useParams();
+
+  return (
+    <div className={classes.container}>
+      <QueryRenderer
+        query={stixObjectOrStixRelationshipStixObjectOrStixRelationshipQuery}
+        variables={{ id }}
+        render={({ props }) => {
+          if (props) {
+            if (props.stixObjectOrStixRelationship) {
+              let redirectLink;
+              const { stixObjectOrStixRelationship } = props;
+              const fromRestricted = stixObjectOrStixRelationship.from === null;
+              const toRestricted = stixObjectOrStixRelationship.to === null;
+              if (
+                stixObjectOrStixRelationship.relationship_type
+                === 'stix-sighting-relationship'
+              ) {
+                if (!toRestricted) {
+                  redirectLink = `${resolveLink(
+                    stixObjectOrStixRelationship.to.entity_type,
+                  )}/${
+                    stixObjectOrStixRelationship.to.id
+                  }/knowledge/sightings/${stixObjectOrStixRelationship.id}`;
+                } else {
+                  redirectLink = !fromRestricted
+                    ? `${resolveLink(
                       stixObjectOrStixRelationship.from.entity_type,
                     )}/${
                       stixObjectOrStixRelationship.from.id
-                    }/knowledge/relations/${stixObjectOrStixRelationship.id}`;
-                  } else {
-                    redirectLink = !toRestricted
-                      ? `${resolveLink(
-                        stixObjectOrStixRelationship.to.entity_type,
-                      )}/${
-                        stixObjectOrStixRelationship.to.id
-                      }/knowledge/relations/${stixObjectOrStixRelationship.id}`
-                      : undefined;
-                  }
-                } else {
+                    }/knowledge/sightings/${stixObjectOrStixRelationship.id}`
+                    : undefined;
+                }
+              } else if (stixObjectOrStixRelationship.relationship_type) {
+                if (stixObjectOrStixRelationship.from?.relationship_type) {
+                  redirectLink = !toRestricted
+                    ? `${resolveLink(
+                      stixObjectOrStixRelationship.to.entity_type,
+                    )}/${
+                      stixObjectOrStixRelationship.to.id
+                    }/knowledge/relations/${stixObjectOrStixRelationship.id}`
+                    : undefined;
+                } else if (!fromRestricted) {
                   redirectLink = `${resolveLink(
-                    stixObjectOrStixRelationship.entity_type,
-                  )}/${stixObjectOrStixRelationship.id}`;
+                    stixObjectOrStixRelationship.from.entity_type,
+                  )}/${
+                    stixObjectOrStixRelationship.from.id
+                  }/knowledge/relations/${stixObjectOrStixRelationship.id}`;
+                } else {
+                  redirectLink = !toRestricted
+                    ? `${resolveLink(
+                      stixObjectOrStixRelationship.to.entity_type,
+                    )}/${
+                      stixObjectOrStixRelationship.to.id
+                    }/knowledge/relations/${stixObjectOrStixRelationship.id}`
+                    : undefined;
                 }
-                if (redirectLink) {
-                  return <Navigate exact from={`/id/${id}`} to={redirectLink} replace={true} />;
-                }
+              } else {
+                redirectLink = `${resolveLink(
+                  stixObjectOrStixRelationship.entity_type,
+                )}/${stixObjectOrStixRelationship.id}`;
               }
-              return <ErrorNotFound />;
+              if (redirectLink) {
+                return <Navigate exact from={`/id/${id}`} to={redirectLink} replace={true} />;
+              }
             }
-            return <Loader />;
-          }}
-        />
-      </div>
-    );
-  }
-}
-
-StixObjectOrStixRelationship.propTypes = {
-  params: PropTypes.object,
-  navigate: PropTypes.func,
+            return <ErrorNotFound />;
+          }
+          return <Loader />;
+        }}
+      />
+    </div>
+  );
 };
 
-export default compose(
-  inject18n,
-  withRouter,
-  withStyles(styles),
-)(StixObjectOrStixRelationship);
+export default StixObjectOrStixRelationship;
