48b4a3c10fb084ebea60a9e9a44522cdbcc476ad
Cathia Archidoit <cathia.archidoit@filigran.io>
2026-07-28 12:15:04 +0200

    Fix

--- opencti-platform/opencti-graphql/src/config/errors.js
+++ opencti-platform/opencti-graphql/src/config/errors.js
@@ -194,9 +194,10 @@ export const LockTimeoutError = (data, reason) => error(TYPE_LOCK, reason ?? 'Ex
 });
 
 export const DRAFT_LOCKED_ERROR = 'DRAFT_LOCKED';
-export const DraftLockedError = (reason) => error(DRAFT_LOCKED_ERROR, reason ?? 'Draft is in a locked state, no request can be done within this draft', {
+export const DraftLockedError = (reason, data) => error(DRAFT_LOCKED_ERROR, reason ?? 'Draft is in a locked state, no request can be done within this draft', {
   http_status: 400,
   genre: CATEGORY_BUSINESS,
+  ...data,
 });
 
 export const WORK_NOT_ALIVE_ERROR = 'WORK_NOT_ALIVE';
