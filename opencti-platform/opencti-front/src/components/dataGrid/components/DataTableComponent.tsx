ae6151ee80d7730abf76d6bd8d168539c604f6d0
scarletmerlin123 <242427742+scarletmerlin123@users.noreply.github.com>
2026-07-29 12:56:08 -0500

    feat(accessibility): add grid roles to data table (#17407)

--- opencti-platform/opencti-front/src/components/dataGrid/components/DataTableComponent.tsx
+++ opencti-platform/opencti-front/src/components/dataGrid/components/DataTableComponent.tsx
@@ -293,6 +293,7 @@ const DataTableComponent = ({
     >
       {filtersComponent && <div>{filtersComponent}</div>}
       <div
+        role="grid"
         className="datatable-container"
         style={{ width: '100%', overflow: 'auto hidden' }}
         ref={tableRef}
