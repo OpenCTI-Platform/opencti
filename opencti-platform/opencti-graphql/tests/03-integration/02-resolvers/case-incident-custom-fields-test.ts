f4a9ad89f27353e1e5c611033e00507f7f0d7437
Ludovic Bouges <ludovic.bouges@filigran.io>
2026-07-30 17:03:12 +0200

    test(backend): cover select_values merge semantics for multi_select custom fields on upsert

--- opencti-platform/opencti-graphql/tests/03-integration/02-resolvers/case-incident-custom-fields-test.ts
+++ opencti-platform/opencti-graphql/tests/03-integration/02-resolvers/case-incident-custom-fields-test.ts
@@ -172,3 +172,108 @@ describe('Custom fields wired on Case-Incident', () => {
     expect(deleteCustomFieldResult.errors).toBeUndefined();
   });
 });
+
+describe('Custom fields multi_select upsert on Case-Incident', () => {
+  let multiSelectFieldId: string;
+  let multiSelectFieldName: string;
+  let caseIncidentId: string;
+
+  const CASE_INCIDENT_MULTI_SELECT_ADD_QUERY = gql`
+    mutation CaseIncidentAddForMultiSelectCustomFieldTest($input: CaseIncidentAddInput!) {
+      caseIncidentAdd(input: $input) {
+        id
+        customFieldValues {
+          field_id
+          field_name
+          select_values
+        }
+      }
+    }
+  `;
+
+  const CASE_INCIDENT_MULTI_SELECT_FIELD_PATCH_QUERY = gql`
+    mutation CaseIncidentFieldPatchForMultiSelectCustomFieldTest($id: ID!, $input: [EditInput]!) {
+      stixDomainObjectEdit(id: $id) {
+        fieldPatch(input: $input) {
+          id
+          ... on CaseIncident {
+            customFieldValues {
+              field_id
+              field_name
+              select_values
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  it('should create a multi_select custom field definition attached to Case-Incident', async () => {
+    const addResult = await queryAsAdmin({
+      query: CUSTOM_FIELD_ADD_QUERY,
+      variables: {
+        input: {
+          name: 'x_opencti_cf_test_case_incident_tags',
+          label: 'Test Case Incident Tags',
+          field_type: 'multi_select',
+          select_options: ['alpha', 'bravo', 'charlie'],
+        },
+      },
+    });
+    expect(addResult.errors).toBeUndefined();
+    multiSelectFieldId = addResult.data?.customFieldDefinitionAdd.id;
+    multiSelectFieldName = addResult.data?.customFieldDefinitionAdd.name;
+    expect(multiSelectFieldId).toBeDefined();
+
+    const attachResult = await queryAsAdmin({
+      query: CUSTOM_FIELD_ADD_ENTITY_TYPE_QUERY,
+      variables: { id: multiSelectFieldId, entityType: 'Case-Incident', mandatory: false },
+    });
+    expect(attachResult.errors).toBeUndefined();
+  });
+
+  it('should create a Case-Incident with an initial selection', async () => {
+    const createResult = await queryAsAdmin({
+      query: CASE_INCIDENT_MULTI_SELECT_ADD_QUERY,
+      variables: {
+        input: {
+          name: 'Case Incident with multi_select custom field',
+          customFieldValues: [
+            { field_id: multiSelectFieldId, field_name: multiSelectFieldName, select_values: ['alpha'] },
+          ],
+        },
+      },
+    });
+    expect(createResult.errors).toBeUndefined();
+    caseIncidentId = createResult.data?.caseIncidentAdd.id;
+    const values = createResult.data?.caseIncidentAdd.customFieldValues;
+    expect(values).toHaveLength(1);
+    expect(values[0].select_values).toEqual(['alpha']);
+  });
+
+  it('should merge (not replace) select_values when an ADD patch selects an additional option', async () => {
+    const patchResult = await queryAsAdmin({
+      query: CASE_INCIDENT_MULTI_SELECT_FIELD_PATCH_QUERY,
+      variables: {
+        id: caseIncidentId,
+        input: {
+          key: 'custom_field_values',
+          value: [{ field_id: multiSelectFieldId, field_name: multiSelectFieldName, select_values: ['bravo'] }],
+          operation: 'add',
+        },
+      },
+    });
+    expect(patchResult.errors).toBeUndefined();
+    const values = patchResult.data?.stixDomainObjectEdit.fieldPatch.customFieldValues;
+    expect(values).toHaveLength(1);
+    expect(values[0].select_values).toEqual(expect.arrayContaining(['alpha', 'bravo']));
+    expect(values[0].select_values).toHaveLength(2);
+  });
+
+  it('should cleanup the Case-Incident and the multi_select custom field definition', async () => {
+    const deleteCaseResult = await queryAsAdmin({ query: CASE_INCIDENT_DELETE_QUERY, variables: { id: caseIncidentId } });
+    expect(deleteCaseResult.errors).toBeUndefined();
+    const deleteCustomFieldResult = await queryAsAdmin({ query: CUSTOM_FIELD_DELETE_QUERY, variables: { id: multiSelectFieldId } });
+    expect(deleteCustomFieldResult.errors).toBeUndefined();
+  });
+});
