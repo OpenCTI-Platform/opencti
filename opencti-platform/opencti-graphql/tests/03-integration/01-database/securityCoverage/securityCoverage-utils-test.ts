25cbff3dfbafc5b27e9a54df3b2333ed3a95954d
Landry Trebon <landry.trebon@filigran.io>
2026-08-04 16:21:59 +0200

    fix tests again

--- opencti-platform/opencti-graphql/tests/03-integration/01-database/securityCoverage/securityCoverage-utils-test.ts
+++ opencti-platform/opencti-graphql/tests/03-integration/01-database/securityCoverage/securityCoverage-utils-test.ts
@@ -1,24 +1,44 @@
-import { describe, expect, it } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
 import { transformHasCoveredFromId } from '../../../../src/modules/securityCoverage/securityCoverage-utils';
 import { SYSTEM_USER } from '../../../../src/utils/access';
-import { testContext } from '../../../utils/testQuery';
+import { ADMIN_USER, testContext } from '../../../utils/testQuery';
+import { addSecurityCoverage, listSecurityCoverageResults, securityCoverageDelete } from '../../../../src/modules/securityCoverage/securityCoverage-domain';
+import type { BasicStoreEntitySecurityCoverage } from '../../../../src/modules/securityCoverage/securityCoverage-types';
+import type { BasicStoreEntitySecurityCoverageResult } from '../../../../src/modules/securityCoverage/securityCoverageResult/securityCoverageResult-types';
 
 describe('Function transformHasCoveredFromId', () => {
+  let securityCoverage: BasicStoreEntitySecurityCoverage;
+  let result: BasicStoreEntitySecurityCoverageResult;
+
+  beforeEach(async () => {
+    securityCoverage = await addSecurityCoverage(testContext, ADMIN_USER, {
+      name: 'sc1',
+      objectCovered: 'report--a445d22a-db0c-4b5d-9ec8-e9ad0b6dbdd7',
+      auto_enrichment_disable: true,
+      external_uri: 'http://localhost/admin/scenarios/a2166709-be41-48bf-9ce1-51bb2fd3a999',
+    });
+    result = (await listSecurityCoverageResults(testContext, ADMIN_USER, securityCoverage))[0];
+  });
+
+  afterEach(async () => {
+    await securityCoverageDelete(testContext, ADMIN_USER, securityCoverage.standard_id);
+  });
+
   it('should replace security coverage id by security coverage result id when valid external_uri', async () => {
     const newInput = await transformHasCoveredFromId(
       testContext,
       SYSTEM_USER,
       {
         relationship_type: 'has-covered',
-        external_uri: 'http://localhost/admin/scenarios/a2166709-be41-48bf-9ce1-51bb2fd3a175',
-        fromId: 'security-coverage--9a2d4fa8-403c-5e15-ad4b-0f2909885db8',
+        external_uri: 'http://localhost/admin/scenarios/a2166709-be41-48bf-9ce1-51bb2fd3a999',
+        fromId: securityCoverage.standard_id,
         toId: 'attack-pattern--2fc04aa5-48c1-49ec-919a-b88241ef1d17',
       },
     );
     expect(newInput).toEqual({
       relationship_type: 'has-covered',
-      external_uri: 'http://localhost/admin/scenarios/a2166709-be41-48bf-9ce1-51bb2fd3a175',
-      fromId: 'security-coverage-result--cd293e6b-384f-596d-b72d-4c47509553da',
+      external_uri: 'http://localhost/admin/scenarios/a2166709-be41-48bf-9ce1-51bb2fd3a999',
+      fromId: result.standard_id,
       toId: 'attack-pattern--2fc04aa5-48c1-49ec-919a-b88241ef1d17',
     });
   });
@@ -43,13 +63,13 @@ describe('Function transformHasCoveredFromId', () => {
       SYSTEM_USER,
       {
         relationship_type: 'has-covered',
-        fromId: 'security-coverage--c76bfcfe-2be5-500f-9b81-367457f1088f',
+        fromId: securityCoverage.standard_id,
         toId: 'attack-pattern--2fc04aa5-48c1-49ec-919a-b88241ef1d17',
       },
     );
     expect(newInput).toEqual({
       relationship_type: 'has-covered',
-      fromId: 'security-coverage-result--90727e33-b5b6-54f4-8d17-ed5c68288553',
+      fromId: result.standard_id,
       toId: 'attack-pattern--2fc04aa5-48c1-49ec-919a-b88241ef1d17',
     });
   });
