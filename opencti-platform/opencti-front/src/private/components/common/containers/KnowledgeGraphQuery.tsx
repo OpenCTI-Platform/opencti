3c45f4b6d6e35689bbdd8960192e5a0363a099bf
Florian Delemarre <florian.delemarre@gmail.com>
2026-08-04 12:13:17 +0200

    fix: revert falty changes

--- opencti-platform/opencti-front/src/private/components/common/containers/KnowledgeGraphQuery.tsx
+++ opencti-platform/opencti-front/src/private/components/common/containers/KnowledgeGraphQuery.tsx
@@ -1,155 +1,17 @@
 import { graphql } from 'react-relay';
 
 export const knowledgeGraphStixCoreObjectQuery = graphql`
-    query KnowledgeGraphQueryStixCoreObjectQuery($id: String!) {
-        stixCoreObject(id: $id) {
-            id
-            entity_type
-            parent_types
-            created_at
-            createdBy {
-                ... on Identity {
-                    id
-                    name
-                    entity_type
-                }
-            }
-            objectMarking {
-                id
-                definition_type
-                definition
-                x_opencti_order
-                x_opencti_color
-            }
-            ... on StixDomainObject {
-                created
-            }
-            ... on AttackPattern {
-                name
-                x_mitre_id
-            }
-            ... on Campaign {
-                name
-                first_seen
-                last_seen
-            }
-            ... on CourseOfAction {
-                name
-            }
-            ... on Note {
-                attribute_abstract
-                content
-            }
-            ... on ObservedData {
-                name
-                first_observed
-                last_observed
-            }
-            ... on Opinion {
-                opinion
-            }
-            ... on Report {
-                name
-                published
-            }
-            ... on Grouping {
-                name
-                description
-            }
-            ... on Individual {
-                name
-            }
-            ... on Organization {
-                name
-            }
-            ... on Sector {
-                name
-            }
-            ... on System {
-                name
-            }
-            ... on SecurityPlatform {
-                name
-            }
-            ... on SecurityCoverage {
-                name
-            }
-            ... on Indicator {
-                name
-                valid_from
-            }
-            ... on Infrastructure {
-                name
-            }
-            ... on IntrusionSet {
-                name
-                first_seen
-                last_seen
-            }
-            ... on Position {
-                name
-            }
-            ... on City {
-                name
-            }
-            ... on AdministrativeArea {
-                name
-            }
-            ... on Country {
-                name
-            }
-            ... on Region {
-                name
-            }
-            ... on Malware {
-                name
-                first_seen
-                last_seen
-            }
-            ... on MalwareAnalysis {
-                result_name
-            }
-            ... on ThreatActor {
-                name
-                entity_type
-                first_seen
-                last_seen
-            }
-            ... on Tool {
-                name
-            }
-            ... on Vulnerability {
-                name
-            }
-            ... on Incident {
-                name
-                first_seen
-                last_seen
-            }
-            ... on StixCyberObservable {
-                observable_value
-            }
-            ... on StixFile {
-                observableName: name
-            }
-            ... on Event {
-                name
-            }
-            ... on Case {
-                name
-            }
-            ... on Narrative {
-                name
-            }
-            ... on DataComponent {
-                name
-            }
-            ... on DataSource {
-                name
-            }
-            ... on Language {
-                name
-            }
+  query KnowledgeGraphQueryStixCoreObjectQuery($id: String!) {
+    stixCoreObject(id: $id) {
+      id
+      entity_type
+      parent_types
+      created_at
+      createdBy {
+        ... on Identity {
+          id
+          name
+          entity_type
         }
       }
       objectMarking {
@@ -204,6 +66,12 @@ export const knowledgeGraphStixCoreObjectQuery = graphql`
         name
       }
       ... on System {
+        name
+      }
+      ... on SecurityPlatform {
+        name
+      }
+      ... on SecurityCoverage {
         name
       }
       ... on Indicator {
