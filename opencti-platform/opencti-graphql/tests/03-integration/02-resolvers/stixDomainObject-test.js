3c78b1d2daf6273c66ff7ae4ef0c7208385de99e
Landry Trebon <33682259+lndrtrbn@users.noreply.github.com>
2026-08-11 14:30:47 +0200

    fix: counters (#14716)
    Co-authored-by: Souad Hadjiat <souad.hadjiat@filigran.io>

--- opencti-platform/opencti-graphql/tests/03-integration/02-resolvers/stixDomainObject-test.js
+++ opencti-platform/opencti-graphql/tests/03-integration/02-resolvers/stixDomainObject-test.js
@@ -174,7 +174,7 @@ describe('StixDomainObject resolver standard behavior', () => {
       }
     `;
     const queryResult = await queryAsAdmin({ query: NUMBER_QUERY });
-    expect(queryResult.data.stixDomainObjectsNumber.total).toEqual(108);
+    expect(queryResult.data.stixDomainObjectsNumber.total).toEqual(110);
   });
   it('should timeseries stixDomainObjects to be accurate', async () => {
     const TIMESERIES_QUERY = gql`
