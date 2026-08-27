279e9b4d7b05bba0cbf14db9cd4d36462eb8e32a
scarletmerlin123 <242427742+scarletmerlin123@users.noreply.github.com>
2026-08-27 16:05:02 -0500

    feat(accessibility) Update components to use List and ListItemButton instead of MenuList and MenuItem (#17938)
    feat(accessibility): update to remove margin from ListIemText
    
    feat(accessibility): update tests (#17938)

--- opencti-platform/opencti-front/tests_e2e/model/settingsTaxonomies.pageModel.ts
+++ opencti-platform/opencti-front/tests_e2e/model/settingsTaxonomies.pageModel.ts
@@ -15,7 +15,7 @@ export default class SettingsTaxonomiesPage {
   }
 
   async navigateRightMenu(menu: string) {
-    await this.page.getByRole('menuitem', { name: menu, exact: true }).click();
+    await this.page.getByRole('link', { name: menu, exact: true }).click();
   }
 
   getTaxonomiesPages(name: string) {
