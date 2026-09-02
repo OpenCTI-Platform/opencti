f9cc260ea21cb36328dece1dec97bddc5a921905
Cathia Archidoit <cathia.archidoit@filigran.io>
2026-09-02 15:34:47 +0200

    Note icon alignement in entity overview

--- opencti-platform/opencti-front/src/private/components/analyses/notes/StixCoreObjectOrStixCoreRelationshipNoteCard.tsx
+++ opencti-platform/opencti-front/src/private/components/analyses/notes/StixCoreObjectOrStixCoreRelationshipNoteCard.tsx
@@ -6,7 +6,7 @@ import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid2';
 import IconButton from '@common/button/IconButton';
 import { useTheme } from '@mui/styles';
-import { Stack, Box } from '@mui/material';
+import { Stack, Box, Tooltip } from '@mui/material';
 import { useFormatter } from '../../../../components/i18n';
 import NotePopover from './NotePopover';
 import { resolveLink } from '../../../../utils/Entity';
@@ -111,15 +111,16 @@ const StixCoreObjectOrStixCoreRelationshipNoteCard: FunctionComponent<
           </Typography>
         </Stack>
 
-        <div>
-          <IconButton
-            aria-label={t_i18n('Go to note')}
-            component={Link}
-            to={`/dashboard/analyses/notes/${note.id}`}
-            sx={{ mr: 1 }}
-          >
-            <OpenInNewOutlined fontSize="small" />
-          </IconButton>
+        <Stack direction="row" alignItems="center" spacing={1}>
+          <Tooltip title={t_i18n('Go to note')}>
+            <IconButton
+              aria-label={t_i18n('Go to note')}
+              component={Link}
+              to={`/dashboard/analyses/notes/${note.id}`}
+            >
+              <OpenInNewOutlined fontSize="small" />
+            </IconButton>
+          </Tooltip>
           <CollaborativeSecurity data={note} needs={[KNOWLEDGE_KNUPDATE]}>
             <NotePopover
               data={note}
@@ -127,7 +128,7 @@ const StixCoreObjectOrStixCoreRelationshipNoteCard: FunctionComponent<
               entityId={stixCoreObjectOrStixCoreRelationshipId}
             />
           </CollaborativeSecurity>
-        </div>
+        </Stack>
       </Stack>
 
       <Grid container spacing={3}>
