6b4068fa2e76a184087f5470580c172181da4223
Landry Trebon <landry.trebon@filigran.io>
2026-09-02 11:47:37 +0200

    Fix checkboxes in investigation filters

--- opencti-platform/opencti-front/src/components/graph/components/GraphToolbarOptionsList.tsx
+++ opencti-platform/opencti-front/src/components/graph/components/GraphToolbarOptionsList.tsx
@@ -1,7 +1,6 @@
 import List from '@mui/material/List';
 import ListItemText from '@mui/material/ListItemText';
 import Popover from '@mui/material/Popover';
-import React from 'react';
 import { ListItemButton } from '@mui/material';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import { Checkbox } from '@filigran/design-system';
@@ -44,12 +43,12 @@ function GraphToolbarOptionsList<T>({
               <ListItemIcon sx={{ minWidth: 0 }}>
                 <Checkbox
                   aria-label={getOptionText(option)}
+                  label={getOptionText(option)}
                   className="py-1"
                   checked={isOptionSelected(option)}
                 />
               </ListItemIcon>
             )}
-            <ListItemText primary={getOptionText(option)} />
           </ListItemButton>
         ))}
       </List>
