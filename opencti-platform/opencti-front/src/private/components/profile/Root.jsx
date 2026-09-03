225e0c1d77b2b6de8424696fd8894607218c9544
Valentin Stern <valentin.stern@filigran.io>
2026-09-03 15:41:13 +0200

    refactor(frontend): flatten notifications page (#18085)

--- opencti-platform/opencti-front/src/private/components/profile/Root.jsx
+++ opencti-platform/opencti-front/src/private/components/profile/Root.jsx
@@ -4,8 +4,6 @@ import { boundaryWrapper } from '../Error';
 import Notifications from './Notifications';
 import Profile from './Profile';
 import Triggers from './Triggers';
-import Alerts from './Alerts';
-import NewsFeed from './NewsFeed';
 
 const Root = () => (
   <Routes>
@@ -20,11 +18,7 @@ const Root = () => (
     <Route
       path="/notifications"
       element={boundaryWrapper(Notifications)}
-    >
-      <Route index element={<Navigate to="alerts" replace={true} />} />
-      <Route path="alerts" element={boundaryWrapper(Alerts)} />
-      <Route path="news-feed" element={boundaryWrapper(NewsFeed)} />
-    </Route>
+    />
     <Route
       path="/triggers"
       element={boundaryWrapper(Triggers)}
