24400969c1f501a90d308979d6f85f4d5efda58e
Bocognano Sarah <sarah.bocognano@filigran.io>
2026-09-15 14:31:39 +0200

    Add search bar and loadEntities

--- opencti-platform/opencti-graphql/src/generated/graphql.ts
+++ opencti-platform/opencti-graphql/src/generated/graphql.ts
@@ -10207,6 +10207,11 @@ export type GetMetrics = {
   total?: Maybe<Scalars['String']['output']>;
 };
 
+export type GlobalExportSelectionInput = {
+  entityType: Scalars['String']['input'];
+  ids?: InputMaybe<Array<Scalars['ID']['input']>>;
+};
+
 export type Group = BasicObject & InternalObject & {
   __typename?: 'Group';
   allowed_marking?: Maybe<Array<MarkingDefinition>>;
@@ -26052,6 +26057,7 @@ export type QueryFormsArgs = {
 
 export type QueryGlobalConfigurationExportArgs = {
   entityTypes: Array<Scalars['String']['input']>;
+  selections?: InputMaybe<Array<GlobalExportSelectionInput>>;
 };
 
 
@@ -40901,6 +40907,7 @@ export type ResolversTypes = ResolversObject<{
   Format: Format;
   FormsOrdering: FormsOrdering;
   GetMetrics: ResolverTypeWrapper<GetMetrics>;
+  GlobalExportSelectionInput: GlobalExportSelectionInput;
   Group: ResolverTypeWrapper<Omit<Group, 'allowed_marking' | 'default_dashboard' | 'default_marking' | 'editContext' | 'max_shareable_marking' | 'members' | 'roles'> & { allowed_marking?: Maybe<Array<ResolversTypes['MarkingDefinition']>>, default_dashboard?: Maybe<ResolversTypes['Workspace']>, default_marking?: Maybe<Array<ResolversTypes['DefaultMarking']>>, editContext?: Maybe<Array<ResolversTypes['EditUserContext']>>, max_shareable_marking: Array<ResolversTypes['MarkingDefinition']>, members?: Maybe<ResolversTypes['UserConnection']>, roles?: Maybe<ResolversTypes['RoleConnection']> }>;
   GroupAddInput: GroupAddInput;
   GroupConnection: ResolverTypeWrapper<Omit<GroupConnection, 'edges'> & { edges?: Maybe<Array<Maybe<ResolversTypes['GroupEdge']>>> }>;
@@ -42029,6 +42036,7 @@ export type ResolversParentTypes = ResolversObject<{
   FormSubmissionInput: FormSubmissionInput;
   FormSubmissionResponse: FormSubmissionResponse;
   GetMetrics: GetMetrics;
+  GlobalExportSelectionInput: GlobalExportSelectionInput;
   Group: Omit<Group, 'allowed_marking' | 'default_dashboard' | 'default_marking' | 'editContext' | 'max_shareable_marking' | 'members' | 'roles'> & { allowed_marking?: Maybe<Array<ResolversParentTypes['MarkingDefinition']>>, default_dashboard?: Maybe<ResolversParentTypes['Workspace']>, default_marking?: Maybe<Array<ResolversParentTypes['DefaultMarking']>>, editContext?: Maybe<Array<ResolversParentTypes['EditUserContext']>>, max_shareable_marking: Array<ResolversParentTypes['MarkingDefinition']>, members?: Maybe<ResolversParentTypes['UserConnection']>, roles?: Maybe<ResolversParentTypes['RoleConnection']> };
   GroupAddInput: GroupAddInput;
   GroupConnection: Omit<GroupConnection, 'edges'> & { edges?: Maybe<Array<Maybe<ResolversParentTypes['GroupEdge']>>> };
