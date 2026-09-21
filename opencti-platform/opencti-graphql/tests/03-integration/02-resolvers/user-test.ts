5b6e4af1cf369fc7027f4d4614e9079a62e2f5ed
Jeremy Cloarec <jeremy.cloarec@filigran.io>
2026-09-21 14:20:18 +0200

    post cherry pick fix

--- opencti-platform/opencti-graphql/tests/03-integration/02-resolvers/user-test.ts
+++ opencti-platform/opencti-graphql/tests/03-integration/02-resolvers/user-test.ts
@@ -1020,7 +1020,7 @@ describe('User has no settings capability and is organization admin query behavi
   it('Org admins should NOT update user_service_account without SETTINGS_SETACCESSES capability', async () => {
     // USER_EDITOR is an organization admin (VIRTUAL_ORGANIZATION_ADMIN) but has no SETTINGS_SETACCESSES capability.
     // Even for a user of its own administrated organization, editing user_service_account must be forbidden.
-    await queryAsUserIsExpectedForbidden(USER_EDITOR, {
+    await queryAsUserIsExpectedForbidden(USER_EDITOR.client, {
       query: UPDATE_QUERY,
       variables: { id: userInternalId, input: [{ key: 'user_service_account', value: [true] }] },
     });
